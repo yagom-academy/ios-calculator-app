96e2a22a48c1ee05ad0a07c38e216a779fcca169
wonhee <wonhee010@gmail.com>
2020-12-08 18:18:54 +0900

    feat: add decimalComma func

--- Calculator/Calculator/StringExtension.swift
+++ Calculator/Calculator/StringExtension.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+extension String {
+    func DecimalComma() -> String? {
+        let numberStringArray = self.split(separator: ".").map { value -> String in
+            return String(value)
+        }
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        
+        guard let decimalNumberString = numberStringArray.first,
+              let decimalNumber = Int(decimalNumberString),
+              let formattedNumber = numberFormatter.string(from: NSNumber(value: decimalNumber)) else {
+            return nil
+        }
+        
+        return formattedNumber + (numberStringArray.last ?? "")
+    }
+}
