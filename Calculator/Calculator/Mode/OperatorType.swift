1ce8364231ac8309c5bfe3e79bf76e515845ba8d
wonhee <wonhee010@gmail.com>
2020-12-09 01:52:23 +0900

    feat: add precedence property

--- Calculator/Calculator/Mode/OperatorType.swift
+++ Calculator/Calculator/Mode/OperatorType.swift
@@ -7,20 +7,49 @@
 
 import Foundation
 
-enum DecimalOperatorType: String, CaseIterable {
-    case divide = "/"
-    case multiple = "*"
-    case minus = "-"
-    case plus = "+"
+enum Operator: Int {
+    case leftShift, rightShift = 1
+    case divide, multiple, and, nand
+    case add, subtract, or, nor, xor
+    case not
 }
 
-enum BinaryOperatorType: String, CaseIterable {
+enum DecimalOperator: String {
+    case add = "+"
+    case subtract = "-"
+    case multiple = "*"
+    case divide = "/"
+    
+    var precedence: Int {
+        switch self {
+        case .add, .subtract:
+            return 3
+        case .multiple, .divide:
+            return 2
+        }
+    }
+}
+
+enum Binaryoperator: String {
     case leftShift = "<<"
     case rightShift = ">>"
-    case or = "OR"
-    case nor = "NOR"
-    case not = "NOT"
     case and = "AND"
     case nand = "NAND"
+    case or = "OR"
+    case nor = "NOR"
     case xor = "XOR"
+    case not = "NOT"
+    
+    var precedence: Int {
+        switch self {
+        case .leftShift, .rightShift:
+            return 1
+        case .and, .nand:
+            return 2
+        case .or, .nor, .xor:
+            return 3
+        case .not:
+            return 0
+        }
+    }
 }
