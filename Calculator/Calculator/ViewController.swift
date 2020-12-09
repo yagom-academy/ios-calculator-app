711976c0597887f234742b4ebd01e278458b29f0
wonhee <wonhee010@gmail.com>
2020-12-09 17:00:34 +0900

    feat: add test decimal

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -10,7 +10,26 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        do {
+            let test = ["3", "+", "2"]
+            let result = try DecimalCalculator.shared.calculate(test)
+            print("📚: \(result.value)")
+            
+            let test2 = ["3", "+", "2", "*", "2"]
+            let result2 = try DecimalCalculator.shared.calculate(test2)
+            print("📚: \(result2.value)")
+            
+            let test3 = ["3", "+", "2", "*", "2", "*", "-1"]
+            let result3 = try DecimalCalculator.shared.calculate(test3)
+            print("📚: \(result3.value)")
+            
+            let test4 = ["3", "+", "-2", "*", "2", "*", "-1"]
+            let result4 = try DecimalCalculator.shared.calculate(test4)
+            print("📚: \(result4.value)")
+            
+        } catch {
+            
+        }
     }
 }
 
