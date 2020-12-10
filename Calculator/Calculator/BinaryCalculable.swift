f7de77e64d696452fbfac8ea0b8cc9a1f9ce90e3
SM <hcooch2ch3@gmail.com>
2020-12-10 18:54:36 +0900

    feat: BinaryCalculable 프로토콜 구현

---
+++ Calculator/Calculator/BinaryCalculable.swift
@@ -0,0 +1,19 @@
+//
+//  BinaryCalculable.swift
+//  Calculator
+//
+//  Created by 임성민 on 2020/12/10.
+//
+
+import Foundation
+
+protocol BinaryCalculable {
+    func and()
+    func or()
+    func xor()
+    func nor()
+    func not()
+    func nand()
+    func leftShift()
+    func rightShift()
+}
