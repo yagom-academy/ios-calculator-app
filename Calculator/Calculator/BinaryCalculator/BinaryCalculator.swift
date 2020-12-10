4dbfebb48c861a990da1c98bee70df6f8e7a46ae
yeon <jryoun1@naver.com>
2020-12-10 17:28:39 +0900

    feat: BinaryCalculator BinaryCalculable protocol 채택을 extension으로 빼고, 해당 함수들 구현

--- Calculator/Calculator/BinaryCalculator/BinaryCalculator.swift
+++ Calculator/Calculator/BinaryCalculator/BinaryCalculator.swift
@@ -40,3 +40,84 @@ extension BinaryCalculator: BasicCalculable {
         calculateStack.removeAll()
     }
 }
+
+extension BinaryCalculator: BinaryCalculable {
+    func and(first: String, second: String) throws -> String {
+        var result: Int
+        guard let firstNumber = Int(first, radix: 2),
+              let secondNumber = Int(second, radix: 2) else {
+            throw CalculatorError.unknown
+        }
+        result = firstNumber & secondNumber
+        return String(result, radix: 2)
+    }
+    
+    func or(first: String, second: String) throws -> String {
+        var result: Int
+        guard let firstNumber = Int(first, radix: 2),
+              let secondNumber = Int(second, radix: 2) else {
+            throw CalculatorError.unknown
+        }
+        result = firstNumber | secondNumber
+        return String(result, radix: 2)
+    }
+    
+    func xor(first: String, second: String) throws -> String {
+        var result: Int
+        guard let firstNumber = Int(first, radix: 2),
+              let secondNumber = Int(second, radix: 2) else {
+            throw CalculatorError.unknown
+        }
+        result = firstNumber ^ secondNumber
+        return String(result, radix: 2)
+    }
+    
+    func nor(first: String, second: String) throws -> String {
+        var result: Int
+        guard let firstNumber = Int(first, radix: 2),
+              let secondNumber = Int(second, radix: 2) else {
+            throw CalculatorError.unknown
+        }
+        result = ~(firstNumber & secondNumber)
+        return String(result, radix: 2)
+    }
+    
+    func not(first: String) throws -> String {
+        var result: Int
+        guard let firstNumber = Int(first, radix: 2) else {
+            throw CalculatorError.unknown
+        }
+        result = ~firstNumber
+        return String(result, radix: 2)
+    }
+    
+    func nand(first: String, second: String) throws -> String {
+        var result: Int
+        guard let firstNumber = Int(first, radix: 2),
+              let secondNumber = Int(second, radix: 2) else {
+            throw CalculatorError.unknown
+        }
+        result = ~(firstNumber & secondNumber)
+        return String(result, radix: 2)
+    }
+    
+    func rightShift(first: String, second: String) throws -> String {
+        var result: Int
+        guard let firstNumber = Int(first, radix: 2),
+              let secondNumber = Int(second) else {
+            throw CalculatorError.unknown
+        }
+        result = firstNumber >> secondNumber
+        return String(result, radix: 2)
+    }
+    
+    func leftShift(first: String, second: String) throws -> String {
+        var result: Int
+        guard let firstNumber = Int(first, radix: 2),
+              let secondNumber = Int(second) else {
+            throw CalculatorError.unknown
+        }
+        result = firstNumber << secondNumber
+        return String(result, radix: 2)
+    }
+}
