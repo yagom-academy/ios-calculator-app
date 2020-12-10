b07eacdf8f5a2a085bf677c5a4d983f943149edd
innieminnie <rkddlsl1001@gmail.com>
2020-12-10 23:59:58 +0900

    feat: struct DecimalCalculator의 BasicCalculator프로토콜 채택 및 요구함수 구현

--- Calculator/Calculator/Calculator.swift
+++ Calculator/Calculator/Calculator.swift
@@ -7,9 +7,49 @@
 
 import Foundation
 
+private var addingStack = [Double]()
+
 protocol BasicCalculator {
     var displayedValue: String { get }
     func calculate(value: String, by opreator: String)
     func reset()
 }
 
+
+struct DecimalCalculator: BasicCalculator {
+    let operatorSet: Set<String> = ["+","-","*","/"]
+    var displayedValue: String {
+        get {
+            return String(addingStack.reduce(0){$0 + $1})
+        }
+    }
+    
+    func calculate(value: String, by tappedOperator: String = "+") {
+        guard operatorSet.contains(tappedOperator),
+              let operand = Double(value) else {
+            return
+        }
+        
+        switch tappedOperator {
+        case "+":
+            addingStack.append(operand)
+        case "-":
+            addingStack.append(-operand)
+        case "*":
+            if let multiplicand = addingStack.popLast() {
+                addingStack.append(multiplicand * operand)
+            }
+        case "/":
+            if let dividend = addingStack.popLast() {
+                addingStack.append(dividend / operand)
+            }
+        default:
+            return
+        }
+    }
+    
+    func reset() {
+        addingStack = []
+    }
+    
+}
