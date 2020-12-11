775eee88aa05036a877919088c6eee7409f3dc9c
SM <hcooch2ch3@gmail.com>
2020-12-11 10:43:40 +0900

    feat: ViewController에 테스트 코드 추가

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -7,12 +7,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var decimalCalculator = DecimalCalculator()
+    var binaryCalculator = BianryCalculator()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        print("1011 + 1111 & 1111 ~ = ")
+        try! binaryCalculator.enterNumber("1")
+        try! binaryCalculator.enterNumber("0")
+        try! binaryCalculator.enterNumber("1")
+        try! binaryCalculator.enterNumber("1")
+        try! binaryCalculator.add()
+        try! binaryCalculator.enterNumber("1")
+        try! binaryCalculator.enterNumber("1")
+        try! binaryCalculator.enterNumber("1")
+        try! binaryCalculator.enterNumber("1")
+        try! binaryCalculator.and()
+        try! binaryCalculator.enterNumber("1")
+        try! binaryCalculator.enterNumber("1")
+        try! binaryCalculator.enterNumber("1")
+        try! binaryCalculator.enterNumber("1")
+        try! binaryCalculator.not()
+        print(try! binaryCalculator.equal())
+        
+        print("10 - 3 = ")
+        try! decimalCalculator.enterNumber("1")
+        try! decimalCalculator.enterNumber("0")
+        try! decimalCalculator.subtract()
+        try! decimalCalculator.enterNumber("3")
+        print(try! decimalCalculator.equal())
     }
-
-
 }
 
