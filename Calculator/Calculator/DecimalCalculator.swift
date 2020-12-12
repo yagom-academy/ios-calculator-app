db7fd4b50c41bc9b37b89d5ddaac397a82328c62
odong-Tree <ss12357@naver.com>
2020-12-13 03:21:45 +0900

    feat: 이전 연산자를 스택에서 꺼내 연산하는 메서드 추가

--- Calculator/Calculator/DecimalCalculator.swift
+++ Calculator/Calculator/DecimalCalculator.swift
@@ -22,3 +22,32 @@ struct DecimalCalculator: BasicCalculatable {
         <#code#>
     }
 }
+
+// operator 관련 기능
+extension DecimalCalculator {
+    func operatePrev(_ prevOperator: Operator) {
+        let new: Double = numStack.pop() ?? 0
+        let old: Double = numStack.pop() ?? 0
+        let newValue: Double
+        
+        switch prevOperator {
+        case .multiple:
+            newValue = old * new
+        case .divide:
+            if new == 0 {
+                print("오류")
+                return
+            }
+            newValue = old / new
+        case .plus:
+            newValue = old + new
+        case .minus:
+            newValue = old - new
+        default:
+            print("오류")
+            return
+        }
+        
+        numStack.push(newValue)
+    }
+}
