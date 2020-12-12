9ab32d6a02783fe5a50f28014725b32d4babda5c
odong-Tree <ss12357@naver.com>
2020-12-13 03:10:40 +0900

    feat: 십진법 계산기 구조체 추가 및 내부에 연산자, 피연산자 스택 초기화

---
+++ Calculator/Calculator/DecimalCalculator.swift
@@ -0,0 +1,24 @@
+
+struct DecimalCalculator: BasicCalculatable {
+    
+    var numStack = Stack<Double>()
+    var operatorStack = Stack<Operator>()
+    
+    var current: Double = 0
+    
+    func plus() {
+        <#code#>
+    }
+    
+    func minus() {
+        <#code#>
+    }
+    
+    func reset() {
+        <#code#>
+    }
+    
+    func printResult() {
+        <#code#>
+    }
+}
