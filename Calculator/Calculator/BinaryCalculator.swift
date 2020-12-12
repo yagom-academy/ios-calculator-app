e57a5db7bad398007a12a5fe6003c7d1cc5fd5a4
odong-Tree <ss12357@naver.com>
2020-12-13 03:46:30 +0900

    feat: 2진 계산기 구조체 추가

---
+++ Calculator/Calculator/BinaryCalculator.swift
@@ -0,0 +1,20 @@
+
+struct BinaryCalculator: BasicCalculatable {
+    mutating func plus() {
+        <#code#>
+    }
+    
+    mutating func minus() {
+        <#code#>
+    }
+    
+    mutating func reset() {
+        <#code#>
+    }
+    
+    mutating func printResult() {
+        <#code#>
+    }
+    
+    
+}
