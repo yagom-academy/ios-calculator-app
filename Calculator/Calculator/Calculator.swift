fc57109c7cb8e7202aa9aeed31ac04e7b95961e8
innieminnie <rkddlsl1001@gmail.com>
2020-12-13 21:26:23 +0900

    feat: struct BinaryCalculator의 BasicCalculator프로토콜 채택 및 요구함수 구현

--- Calculator/Calculator/Calculator.swift
+++ Calculator/Calculator/Calculator.swift
@@ -7,41 +7,42 @@
 
 import Foundation
 
-private var addingStack = [Double]()
+private var decimalAdder = [Double]()
+private var binaryAdder = [Int]()
 
 protocol BasicCalculator {
+    var arithmeticOperatorSet: Set<String> { get }
     var displayedValue: String { get }
-    func calculate(value: String, by opreator: String)
+    func calculate(value: String, by tappedOperator: String)
     func reset()
 }
 
-
 struct DecimalCalculator: BasicCalculator {
-    let operatorSet: Set<String> = ["+","-","*","/"]
+    let arithmeticOperatorSet: Set<String> = ["+","-","*","/"]
     var displayedValue: String {
         get {
-            return String(addingStack.reduce(0){$0 + $1})
+            return String(decimalAdder.reduce(0){$0 + $1})
         }
     }
     
     func calculate(value: String, by tappedOperator: String = "+") {
-        guard operatorSet.contains(tappedOperator),
+        guard arithmeticOperatorSet.contains(tappedOperator),
               let operand = Double(value) else {
             return
         }
         
         switch tappedOperator {
         case "+":
-            addingStack.append(operand)
+            decimalAdder.append(operand)
         case "-":
-            addingStack.append(-operand)
+            decimalAdder.append(-operand)
         case "*":
-            if let multiplicand = addingStack.popLast() {
-                addingStack.append(multiplicand * operand)
+            if let multiplicand = decimalAdder.popLast() {
+                decimalAdder.append(multiplicand * operand)
             }
         case "/":
-            if let dividend = addingStack.popLast() {
-                addingStack.append(dividend / operand)
+            if let dividend = decimalAdder.popLast() {
+                decimalAdder.append(dividend / operand)
             }
         default:
             return
@@ -49,7 +50,74 @@ struct DecimalCalculator: BasicCalculator {
     }
     
     func reset() {
-        addingStack = []
+        decimalAdder = []
+    }
+}
+
+struct BinaryCalculator: BasicCalculator {
+    var displayedValue: String {
+        get {
+            return String(binaryAdder.reduce(0){$0 + $1})
+        }
+    }
+    let shiftOperatorSet: Set<String> = ["<<",">>"]
+    let arithmeticOperatorSet: Set<String> = ["+","-"]
+    let bitwiseOperatorSet: Set<String> = ["NOT", "AND", "OR","NOR","NAND","XOR"]
+    
+    func calculate(value: String, by tappedOperator: String = "+") {
+        guard let operand = Int(value) else {
+            return
+        }
+        
+        if shiftOperatorSet.contains(tappedOperator) {
+            switch tappedOperator {
+            case "<<":
+                    binaryAdder.append(operand << 1)
+            case ">>":
+                    binaryAdder.append(operand >> 1)
+            default:
+                return
+            }
+        } else if arithmeticOperatorSet.contains(tappedOperator) {
+            switch tappedOperator {
+            case "+":
+                binaryAdder.append(operand)
+            case "-":
+                binaryAdder.append(-operand)
+            default:
+                return
+            }
+        } else if bitwiseOperatorSet.contains(tappedOperator) {
+            switch tappedOperator {
+            case "NOT":
+                binaryAdder.append(~operand)
+            case "AND":
+                if let operatingValue = binaryAdder.popLast() {
+                    binaryAdder.append(operatingValue & operand)
+                }
+            case "OR":
+                if let operatingValue = binaryAdder.popLast() {
+                    binaryAdder.append(operatingValue | operand )
+                }
+            case "NOR":
+                if let operatingValue = binaryAdder.popLast() {
+                    binaryAdder.append(~(operatingValue | operand) )
+                }
+            case "NAND":
+                if let operatingValue = binaryAdder.popLast() {
+                    binaryAdder.append(~(operatingValue & operand) )
+                }
+            case "XOR":
+                if let operatingValue = binaryAdder.popLast() {
+                    binaryAdder.append(operatingValue ^ operand)
+                }
+            default:
+                return
+            }
+        }
     }
     
+    func reset() {
+        binaryAdder = []
+    }
 }
