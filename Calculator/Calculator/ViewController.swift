e984c835c2991b85d4e53fa3a0b950b602f33f24
wonhee <wonhee010@gmail.com>
2020-12-15 01:33:10 +0900

    feat: add binary calculator test code without calcaulate

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -10,23 +10,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        do {
-            
-            let decimal = DecimalCalculator()
-            let dR = try decimal.subtract(first: "13.4", second: "5.8")
-            print(dR)
-            
-            let binary = BinaryCalculator()
-            let result = try binary.calculate(["1010","~|","1111"])
-            let result2 = try binary.calculate(["1010", "~"])
-            print(result)
-            print(result2)
-        } catch {
-            
-        }
     }
-
 
 }
 
--- Calculator/CalculatorTests/CalculatorTests.swift
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -26,50 +26,90 @@ final class BinaryCalculatorTests: XCTestCase {
         let result = try binaryCalculator.add(first: "1111", second: "100001")
         XCTAssertEqual(result, "110000")
     }
+    func testAddWithNegative() throws {
+        let result = try binaryCalculator.add(first: "11111", second: "-1111")
+        XCTAssertEqual(result, "10000")
+    }
     
     func testSubtract() throws {
         let result = try binaryCalculator.subtract(first: "100001", second: "1111")
         XCTAssertEqual(result, "10010")
+    }
+    func testSubtractWithNegative() throws {
+        let result = try binaryCalculator.subtract(first: "11111", second: "-10000")
+        XCTAssertEqual(result, "101111")
     }
     
     func testAnd() throws {
         let result = try binaryCalculator.and(first: "100001", second: "1111")
         XCTAssertEqual(result, "1")
     }
+    func testAndWithNegative() throws {
+        let result = try binaryCalculator.and(first: "1000", second: "-1000")
+        XCTAssertEqual(result, "1000")
+    }
     
     func testOr() throws {
         let result = try binaryCalculator.or(first: "100001", second: "1111")
         XCTAssertEqual(result, "101111")
+    }
+    func testOrWithNegative() throws {
+        let result = try binaryCalculator.or(first: "1000", second: "-1000")
+        XCTAssertEqual(result, "-1000")
     }
     
     func testXor() throws {
         let result = try binaryCalculator.xor(first: "100001", second: "1111")
         XCTAssertEqual(result, "101110")
     }
+    func testXorWithNegative() throws {
+        let result = try binaryCalculator.xor(first: "1000", second: "-1000")
+        XCTAssertEqual(result, "-10000")
+    }
     
     func testNor() throws {
         let result = try binaryCalculator.nor(first: "100001", second: "1111")
         XCTAssertEqual(result, "-110000")
+    }
+    func testNorWithNegative() throws {
+        let result = try binaryCalculator.nor(first: "1000", second: "-1000")
+        XCTAssertEqual(result, "111")
     }
     
     func testNot() throws {
         let result = try binaryCalculator.not(first: "100110")
         XCTAssertEqual(result, "-100111")
     }
+    func testNotWithNegative() throws {
+        let result = try binaryCalculator.not(first: "-100110")
+        XCTAssertEqual(result, "100101")
+    }
     
     func testNand() throws {
         let result = try binaryCalculator.nand(first: "100001", second: "1111")
         XCTAssertEqual(result, "-10")
+    }
+    func testNandWithNegative() throws {
+        let result = try binaryCalculator.nand(first: "1000", second: "-1000")
+        XCTAssertEqual(result, "-1001")
     }
     
     func testRightShift() throws {
         let result = try binaryCalculator.rightShift(first: "1111")
         XCTAssertEqual(result, "111")
     }
+    func testRightShiftWithNegative() throws {
+        let result = try binaryCalculator.rightShift(first: "-1000")
+        XCTAssertEqual(result, "-100")
+    }
     
     func testLeftShift() throws {
         let result = try binaryCalculator.leftShift(first: "1111")
         XCTAssertEqual(result, "11110")
+    }
+    func testLeftShiftWithNegative() throws {
+        let result = try binaryCalculator.leftShift(first: "-1000")
+        XCTAssertEqual(result, "-10000")
     }
     
     func testRemoveAll() throws {
