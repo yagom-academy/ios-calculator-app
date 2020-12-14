fd05e08ba52e8d8bb64b1b8f25ed6813648293e0
GREENOVER <humains29@gmail.com>
2020-12-14 15:01:20 +0900

    feat: Operator 열거형 생성 및 정의

---
+++ Calculator/Calculator/Operator.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+enum Operator {
+    case addtion
+    case subtraction
+    case multiplication
+    case division
+    case and
+    case nand
+    case or
+    case xor
+    case nor
+    case leftShift
+    case RightShift
+}
