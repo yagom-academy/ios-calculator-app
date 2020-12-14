66db7328e2953cb11563853f5b572b4947d84a28
GREENOVER <humains29@gmail.com>
2020-12-14 17:43:39 +0900

    refactor: 계산 메서드 파라미터 언더스코어 추가 수정

--- Calculator/Calculator/DecimalCalculator.swift
+++ Calculator/Calculator/DecimalCalculator.swift
@@ -8,55 +8,36 @@ class DecimalCalculator: Calculator {
     
     func addition<Double>() -> Double {
         decimalNumber.push(element: inputNumber!)
+        checkOperator()
         
-        while operatationNumber.count != 0 {
-            let presentOperator = operatationNumber.pop()
-            operate(present: presentOperator)
-        }
-        operatationNumber.push(element: .addition)
         return resultNumber as! Double
     }
     
     func subtraction<Double>() -> Double {
         decimalNumber.push(element: inputNumber!)
+        checkOperator()
         
-        while operatationNumber.count != 0 {
-            let presentOperator = operatationNumber.pop()
-            operate(present: presentOperator)
-        }
-        operatationNumber.push(element: .subtraction)
         return resultNumber as! Double
     }
     
     func multiplication() -> Double {
         decimalNumber.push(element: inputNumber!)
+        checkOperator()
         
-        while operatationNumber.count != 0 {
-            let presentOperator = operatationNumber.pop()
-            operate(present: presentOperator)
-        }
-        operatationNumber.push(element: .multiplication)
         return resultNumber!
     }
     
     func division() -> Double {
         decimalNumber.push(element: inputNumber!)
+        checkOperator()
         
-        while operatationNumber.count != 0 {
-            let presentOperator = operatationNumber.pop()
-            operate(present: presentOperator)
-        }
-        operatationNumber.push(element: .division)
         return resultNumber!
     }
     
     func result<Double>() -> Double {
         decimalNumber.push(element: inputNumber!)
+        checkOperator()
         
-        while operatationNumber.count != 0 {
-            let presentOperator = operatationNumber.pop()
-            operate(present: presentOperator)
-        }
         return resultNumber as! Double
     }
     
@@ -66,7 +47,15 @@ class DecimalCalculator: Calculator {
         inputNumber = 0
     }
     
-    func operate(present: Operator) {
+    func checkOperator() {
+        while operatationNumber.count != 0 {
+            var presentOperator = operatationNumber.pop()
+            operate(presentOperator)
+        }
+        operatationNumber.push(element: .addition)
+    }
+    
+    func operate(_ present: Operator) {
         var backNumber: Double = decimalNumber.pop()
         var frontNumber: Double = decimalNumber.pop()
         
