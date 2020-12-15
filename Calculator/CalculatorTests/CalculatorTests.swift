608062ed2fff87231e21ba36a2f9c093ee094014
zziro95 <opers7878@gmail.com>
2020-12-15 16:28:01 +0900

    feat: TDD를 위한 Tests 파일 생성

---
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -0,0 +1,18 @@
+//
+//  CalculatorTests.swift
+//  CalculatorTests
+//
+//  Created by Jinho Choi on 2020/12/15.
+//
+
+import XCTest
+
+class CalculatorTests: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+}
