ba15661d56b34697b227f017e15505bc31cd8083
zziro95 <opers7878@gmail.com>
2020-12-15 18:11:09 +0900

    feat: Stack 타입 구현

---
+++ Calculator/Calculator/Stack.swift
@@ -0,0 +1,24 @@
+//
+//  Stack.swift
+//  Calculator
+//
+//  Created by Jinho Choi on 2020/12/15.
+//
+
+import Foundation
+
+class Stack<T> {
+    var stack: [T] = [T]()
+        
+    func push(value: T) {
+        stack.append(value)
+    }
+    
+    func pop() -> T {
+        return stack.removeLast()
+    }
+    
+    func peek() -> T? {
+        return stack.last
+    }
+}
