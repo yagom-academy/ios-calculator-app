6dfd6b0c73ca903cb1af371114298cebb69b54c1
odong-Tree <ss12357@naver.com>
2020-12-16 14:04:35 +0900

    style: error 열거형 이름 변경

--- Calculator/Calculator/BinaryCalculator.swift
+++ Calculator/Calculator/BinaryCalculator.swift
@@ -157,3 +157,4 @@ extension BinaryCalculator {
 }
 
 
+
--- Calculator/Calculator/Error_Enum.swift
+++ Calculator/Calculator/Error_Enum.swift
@@ -1,5 +1,5 @@
 
-enum Error: String {
+enum CalculatorError: String {
     case numStackisEmpty = "저장된 값이 없습니다."
     case operatorStackisEmpty = "연산자를 꺼낼 수 없습니다."
     case etc = "알 수 없는 에러입니다."
