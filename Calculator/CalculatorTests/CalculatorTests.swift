66208183964193ed42550be599d69fa6245aac7d
odong-Tree <ss12357@naver.com>
2020-12-20 12:16:28 +0900

    feat: test에 setup, teardown에 객체 생성, 제거 코드 작성

--- Calculator/CalculatorTests/CalculatorTests.swift
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -1,33 +1,16 @@
-//
-//  CalculatorTests.swift
-//  CalculatorTests
-//
-//  Created by 김동빈 on 2020/12/18.
-//
 
 import XCTest
 @testable import Calculator
 
 class CalculatorTests: XCTestCase {
+    private var decimal: DecimalCalculator!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        decimal = DecimalCalculator()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
+        decimal = nil
     }
 
 }
