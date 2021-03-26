ae7cbd2dec18b22e16e5caa5474bf45a7eece7e9
stevenkim18 <11882@naver.com>
2021-03-26 17:40:12 +0900

    [Refactor] 10진수 계산기 기능 메소드 분리 calculateUntilSatisfiedCondition() -> 조건을 만족할 때까지 스택 안에 있는 값으로 연산 reset() -> c 버튼 클릭시 모든 것 초기화 updateScreenNumber() -> 계산기 화면에 출력할 숫자 업데이트

--- Calculator/Calculator/DimecalCalcultor.swift
+++ Calculator/Calculator/DimecalCalcultor.swift
@@ -39,7 +39,7 @@ struct Stack<T> {
 class Calculator {
     var operands = Stack<Int>()
     var operators = Stack<String>()
-    var temp = 0 
+    var tempNumber = 0
     var screenNumber = "0"
 
     var operationFunctions: [String : (Int, Int) -> Int] = [:]
@@ -51,56 +51,59 @@ class Calculator {
         operationFunctions["/"] = { $0 / $1 }
     }
  
+    private func calculateUntilSatisfiedCondition() {
+        guard let operatorSign = operators.pop() else { return }
+        guard let operationFunction = operationFunctions[operatorSign] else { return }
+        guard let secondOperand = operands.pop() else { return }
+        guard let firstOperand = operands.pop() else { return }
+        let result: Int = operationFunction(firstOperand, secondOperand)
+        operands.push(result)
+    }
+
+    private func reset() {
+        operands.clear()
+        operators.clear()
+        tempNumber = 0
+        screenNumber = "0"
+    }
+
+    private func updateScrennNumber() {
+        guard let lastOperand = operands.top() else { return }
+        screenNumber = String(lastOperand)
+    }
+
 /* 채팅창
 neph 화이팅
+sj 
+
+
 */
+
     func receiveInput(buttonType: String) {
-        print("Receive input: \(buttonType)")
         switch buttonType {
             case "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" :
                 guard let intValue = Int(buttonType) else { return }
-                temp = temp * 10 + intValue
-                screenNumber = String(temp)
+                tempNumber = tempNumber * 10 + intValue
+                screenNumber = String(tempNumber)
             case "+", "-", "*", "/":
-                operands.push(temp) // 피연산자
+                operands.push(tempNumber) 
                 if buttonType == "+" || buttonType == "-" {
                     while !operators.isEmpty() {
-                        // 연산자 스택에 가장 상단에 있는 연산 기호 가져오기
-                        guard let operatorSign = operators.pop() else { print("operatorSign"); return }
-                        // 가져온 기호로 함수 저장
-                        guard let operationFunction = operationFunctions[operatorSign] else { print("operationFunction"); return }
-                        guard let secondOperand = operands.pop() else { print("secondOperand"); return }
-                        guard let firstOperand = operands.pop() else { print("firstOperand"); return }
-                        let result: Int = operationFunction(firstOperand, secondOperand)
-                        operands.push(result)
+                        calculateUntilSatisfiedCondition()
                     }
                 } else if buttonType == "*" || buttonType == "/" {
-                    // guard let lastOperator = operators.top() ??  else { return }
-                    // 연산자 스택에 가장 상단에 있는 연산 기호 가져오기
                     while operators.top() == "*" || operators.top() == "/" {
-                        guard let operatorSign = operators.pop() else { print("operatorSign"); return }
-                        // 가져온 기호로 함수 저장
-                        guard let operationFunction = operationFunctions[operatorSign] else { print("operationFunction"); return }
-                        guard let secondOperand = operands.pop() else { print("secondOperand"); return }
-                        guard let firstOperand = operands.pop() else { print("firstOperand"); return }
-                        let result: Int = operationFunction(firstOperand, secondOperand)
-                        operands.push(result)
+                        calculateUntilSatisfiedCondition()
                     }
                 }
-                guard let lastOperand = operands.top() else { return }
-                screenNumber = String(lastOperand)
+                updateScrennNumber()
                 operators.push(buttonType)
-                temp = 0
+                tempNumber = 0
             case "c":
-                operands.clear()
-                operators.clear()
-                temp = 0
-                screenNumber = "0"
+                reset()
             default:
                 print("input error")           
         }
-        print(operators)
-        print(operands)
     }
 }
 
