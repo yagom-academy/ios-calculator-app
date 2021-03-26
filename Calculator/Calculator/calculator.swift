f610e7d20a4d18fa9c04617ddadc48fa631f8c11
babywalnut <anfuza@naver.com>
2021-03-26 20:14:47 +0900

    refoctor
    class Divder -> class InputDataValidation
    class Calculate -> class PostfixCalculation
    func appendValue -> func appendAdditionalValue
    func transformInput -> func convertInput
    presentValue -> currentValue(func appendAdditionalValue's first parameter)
    justBeforeValue -> previousValue(func appendAdditionalValue's second parameter)
    index -> element(for loop)
    weight -> precedence(typealias(a.k.a Int))

--- Calculator/Calculator/calculator.swift
+++ Calculator/Calculator/calculator.swift
@@ -7,19 +7,28 @@
 
 import Foundation
 
-class Divider {
-    static let shardInstance = Divider()
+class InputDataValidation {
+    static let sharedInstance = InputDataValidation()
     var userInput:[String] = []
     let operators = ["*","/","+","-"]
     
     private init() {}
     
-    private func appendNewValue(_ presentValue: String, after justBeforeValue: String ) {
-        if operators.contains(justBeforeValue) {
-            userInput.append(presentValue)
+    private func appendAdditionalValue(_ currentValue: String, after previousValue: String ) {
+        if operators.contains(previousValue) {
+            if operators.contains(currentValue){
+                userInput.removeLast()
+                userInput.append(currentValue)
+            } else {
+                userInput.append(currentValue)
+            }
         } else {
-            userInput.popLast()
-            userInput.append(justBeforeValue + presentValue)
+            if operators.contains(currentValue) {
+                userInput.append(currentValue)
+            } else {
+                userInput.removeLast()
+                userInput.append(previousValue + currentValue)
+            }
         }
     }
     
@@ -31,41 +40,43 @@ class Divider {
         }
     }
     
-    func transformInput(input: String) {
-        if let lastUserInput = self.userInput.last {
-            appendNewValue(input, after: lastUserInput)
+    func convertInput(input: String) {
+        if let lastUserInput = userInput.last {
+            appendAdditionalValue(input, after: lastUserInput)
         } else {
             appendInitialValue(input)
         }
     }
 }
 
-class Calculate {
-    typealias weight = Int
+protocol Calculatable {
     
-    let operatorPriority: [String : weight] = ["*" : 3, "/" : 3, "+" : 2, "-" : 2, "(" : 1]
-    let operators = ["*","/","+","-"]
-//    var medianNotation: [String] = Divider.shardInstance.userInput
-    var medianNotation: [String] = ["3","+","2","*","5","-","6","/","9","+"]
+}
+
+class PostfixCalculation {
+    typealias precedence = Int
+    
+    let operatorPriority: [String : precedence] = ["*" : 3, "/" : 3, "+" : 2, "-" : 2, "(" : 1]
+    var medianNotation: [String] = InputDataValidation.sharedInstance.userInput
     var postfixNotation: [String] = []
     var operatorStack = Stack<String>()
-    lazy var firstOperand = Double()
-    lazy var secondOperand = Double()
+    var firstOperand = Double()
+    var secondOperand = Double()
     
     func calculatePostfixNotation() {
         var operandStack = Stack<Double>()
         
-        for index in postfixNotation {
-            if !operators.contains(index) {
-                guard let index = Double(index) else { return }
-                operandStack.push(index)
+        for element in postfixNotation {
+            if !operatorPriority.keys.contains(element) {
+                guard let numbers = Double(element) else { return }
+                operandStack.push(numbers)
             } else {
-                guard let firstPoppedValue = operandStack.pop() else { return }
+                guard let firstPoppedValue = operandStack.pop(),
+                      let secondPoppedValue = operandStack.pop() else { return }
                 firstOperand = firstPoppedValue.value
-                guard let secondPoppedValue = operandStack.pop() else { return }
                 secondOperand = secondPoppedValue.value
                 
-                switch index {
+                switch element {
                 case "*" :
                     operandStack.push(Double(secondOperand * firstOperand))
                 case "/" :
@@ -76,8 +87,7 @@ class Calculate {
                     operandStack.push(Double(secondOperand - firstOperand))
                 default:
                     return
-                
-                    }
+                }
             }
         }
         print(operandStack.peek()?.value)
@@ -91,14 +101,13 @@ class Calculate {
             while operatorPriority[peeked.value]! >= operatorPriority[index]! {
                 guard let popped = operatorStack.pop() else { break }
                 postfixNotation.append(popped.value)
-                print(operatorStack.peek()?.value)
             }
             operatorStack.push(index)
             }
         }
     
     private func pushStack(index: String) {
-        if operators.contains(index) {
+        if operatorPriority.keys.contains(index) {
             pushComparedOperator(index: index)
         } else {
             postfixNotation.append(index)
@@ -106,8 +115,8 @@ class Calculate {
     }
     
     func changeCalculate() {
-        if operators.contains(medianNotation.last!) {
-            medianNotation.popLast()
+        if operatorPriority.keys.contains(medianNotation.last!) {
+            medianNotation.removeLast()
         }
         for index in medianNotation {
             pushStack(index: index)
