1f8ae211e7234249705b0693c0193a3307890704
kane-young <lyw2100@naver.com>
2021-03-26 15:15:29 +0900

    Feat : 10진수, 2진수 프로토콜 <description> 각 프로토콜에 메서드 지정

--- Calculator/Calculator/Model/DecimalCalculatable.swift
+++ Calculator/Calculator/Model/DecimalCalculatable.swift
@@ -8,5 +8,8 @@
 import Foundation
 
 protocol DecimalCalculatable: Calculatable {
-    
+    func infixCalculate(firstNumber: Double, operatorSymbol: String, secondNumber: Double) -> String
+    func numberMoveToArray()
+    func calculate(using stackTopOperator: String)
+    func calculateHigherPrioritythan(_ input: String)
 }
