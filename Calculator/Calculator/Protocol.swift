1a6d4135a662153b367a8faa6436604514976ec5
babywalnut <anfuza@naver.com>
2021-03-27 18:19:30 +0900

    refactor : 2진수계산 구현 및 수정 - Binary calculation 구현 	- NAND, NOR, NOT 계산 오류수정 	- NAND, NOR 연산을 위해 CustomPrecedenceGroup 구현 - Decimal.swift 메서드를 논리와 흐름에 맞게 Naming 수정 및 정렬 - protocol(InputDataValidationProtocol, CalculationProtocol) 생성 및 채택

---
+++ Calculator/Calculator/Protocol.swift
@@ -0,0 +1,30 @@
+//
+//  Protocol.swift
+//  Calculator
+//
+//  Created by 김민성 on 2021/03/27.
+//
+
+import Foundation
+
+protocol InputDataValidationProtocol {
+    var userInput:[String] { get }
+    var operators:[String] { get }
+
+    func manageData(input: String)
+}
+
+protocol CalculationProtocol {
+
+    typealias precedence = Int
+
+    var operatorPriority: [String : precedence] { get }
+    var medianNotation: [String] { get }
+    var postfixNotation: [String] { get }
+    var operatorStack: Stack<String> { get }
+
+    func convertToPostfixNotation()
+
+
+}
+
