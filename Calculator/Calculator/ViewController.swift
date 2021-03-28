db0d2b9df1489c029f9807c79db624dda43d6eca
duckbok <duckbok22@gmail.com>
2021-03-28 11:27:59 +0900

    [Test] #1 계산기 테스트 메서드 추가

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -7,12 +7,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let decimalCalculator = DecimalCalculator.shared
+    let binaryCalculator = BinaryCalculator.shared
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    func testDecimalCalculator(by text: String) {
+        print("----Decimal----")
+        decimalCalculator.appendPostfixedList(contentsOf: text)
+        decimalCalculator.equal()
+    }
+    
+    func testBinaryCalculator(by text: String) {
+        print("----Binary----")
+        binaryCalculator.appendPostfixedList(contentsOf: text)
+        binaryCalculator.equal()
+    }
 }
-
