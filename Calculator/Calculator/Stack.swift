9824f98a6585e0aa50e45b9dd90ba31f26d6eeec
kangkyung <rudgns0508@naver.com>
2021-04-01 21:27:13 +0900

    Refactor: Stack의 필요없는변수 refactoring
    - positionOfElement -> array.endIndex
    - array.append, array.removeLast, array.removeAll로 대체

--- Calculator/Calculator/Stack.swift
+++ Calculator/Calculator/Stack.swift
@@ -8,40 +8,36 @@
 import Foundation
 
 class Stack {
-  // Array(repeating:count:) 메서드는 반복할 값 1개(repeating)가 지정된 횟수(count)만큼 배열에 넣어집니다.
-  private var array = Array(repeating: 0, count: 9)
-  private var positionOfElement = 0
+  private var array: [Int] = []
   
   private func checkCount() throws {
-    if positionOfElement >= 10
-    || positionOfElement < 0 {
+    if array.endIndex >= 10
+    || array.endIndex < 0 {
       throw StackError.exceedRangeOfStack
     }
   }
   
   func peek() -> Int? {
-    if positionOfElement < 1 {
+    if array.endIndex < 1 {
       return nil
     } else {
-      return array[positionOfElement - 1]
+      return array[array.endIndex - 1]
     }
   }
   
   func push(number: Int) throws {
     try checkCount()
     
-    array[positionOfElement] = number
-    positionOfElement += 1
+    array.append(number)
   }
   
   func pop() throws {
     try checkCount()
     
-    array[positionOfElement] = 0
-    positionOfElement -= 1
+    array.removeLast()
   }
   
   func Initialization() {
-    array = Array(repeating: 0, count: 9)
+    array.removeAll()
   }
 }
