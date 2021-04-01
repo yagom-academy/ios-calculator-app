45839a3502eb3b0f5fb03121f45fea4247255193
Neph <tony3322@naver.com>
2021-04-01 16:32:53 +0900

    [23] Feat : 버튼 속성 지정 및 색 변경 구현

--- Calculator/Calculator/CaculatorButton.swift
+++ Calculator/Calculator/CaculatorButton.swift
@@ -7,13 +7,32 @@
 
 import UIKit
 
+enum CustomButtonType {
+    case numberButton
+    case operatorButton
+    case functionButton
+}
+
 @IBDesignable
 class CaculatorButton: UIButton {
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
+    var customButtonType = CustomButtonType.numberButton
+    var isOn: Bool = false
+    
     override func draw(_ rect: CGRect) {
-        // Drawing code
         layer.cornerRadius = rect.height / 2
         clipsToBounds = true
+        
+        switch customButtonType {
+        case .numberButton:
+            layer.backgroundColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
+            tintColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        case .functionButton:
+            layer.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+            tintColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        case .operatorButton:
+            layer.backgroundColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
+            tintColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+        
     }
 }
--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -8,26 +8,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var numberLabel: UILabel!
-    @IBOutlet weak var zeroButton: NumberButton!
-    @IBOutlet weak var dotButton: NumberButton!
-    @IBOutlet weak var oneButton: NumberButton!
-    @IBOutlet weak var twoButton: NumberButton!
-    @IBOutlet weak var threeButton: NumberButton!
+    @IBOutlet weak var clearButton: CaculatorButton!
+    @IBOutlet weak var signToggleButton: CaculatorButton!
+    @IBOutlet weak var modeToggleButton: CaculatorButton!
     
+    @IBOutlet weak var zeroButton: CaculatorButton!
+    @IBOutlet weak var dotButton: CaculatorButton!
+    @IBOutlet weak var oneButton: CaculatorButton!
+    @IBOutlet weak var twoButton: CaculatorButton!
+    @IBOutlet weak var threeButton: CaculatorButton!
+    @IBOutlet weak var fourButton: CaculatorButton!
+    @IBOutlet weak var fiveButton: CaculatorButton!
+    @IBOutlet weak var sixButton: CaculatorButton!
+    @IBOutlet weak var sevenButton: CaculatorButton!
+    @IBOutlet weak var eightButton: CaculatorButton!
+    @IBOutlet weak var nineButton: CaculatorButton!
+    
+    @IBOutlet weak var equalButton: CaculatorButton!
+    @IBOutlet weak var subtractButton: CaculatorButton!
+    @IBOutlet weak var divideButton: CaculatorButton!
+    @IBOutlet weak var multiplyButton: CaculatorButton!
+    @IBOutlet weak var addButton: CaculatorButton!
+
     private var decimalMode = true
     private var decimalCalculator = DecimalCalculator()
     private var binaryCalculator = BinaryCalculator()
     
     @IBOutlet weak var numberField: UILabel!
     
-    @IBAction func touchUpNumber(_ sender: UIButton) {
-        numberField.text?.append(sender.currentTitle!)
+    @IBAction func touchUpNumberOrOperator(_ sender: CaculatorButton) {
+
+        switch sender.customButtonType {
+        case .numberButton:
+            numberField.text?.append(sender.currentTitle!)
+        case .operatorButton:
+            sender.layer.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+            sender.tintColor = #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1)
+        default:
+            return
+        }
+        
     }
-    
-    @IBAction func touchUpOperator(_ sender: Any) {
-    }
-    
+
     @IBAction func touchUpToggleSign(_ sender: Any) {
         do {
             if decimalMode {
@@ -44,9 +66,32 @@ class ViewController: UIViewController {
     
     @IBAction func touchUpToggleMode(_ sender: Any) {
         decimalMode.toggle()
-        dotButton.isHidden.toggle()
+        clearButton(sender)
         twoButton.isHidden.toggle()
         threeButton.isHidden.toggle()
+        dotButton.isHidden.toggle()
+        if decimalMode {
+            fourButton.setTitle("4", for: .normal)
+            fiveButton.setTitle("5", for: .normal)
+            sixButton.setTitle("6", for: .normal)
+            sevenButton.setTitle("7", for: .normal)
+            eightButton.setTitle("8", for: .normal)
+            nineButton.setTitle("9", for: .normal)
+        } else {
+            fourButton.customButtonType = .operatorButton
+            fiveButton.customButtonType = .operatorButton
+            sixButton.customButtonType = .operatorButton
+            sevenButton.customButtonType = .operatorButton
+            eightButton.customButtonType = .operatorButton
+            nineButton.customButtonType = .operatorButton
+            fourButton.setTitle("AND", for: .normal)
+            fiveButton.setTitle("NAND", for: .normal)
+            sixButton.setTitle("XOR", for: .normal)
+            sevenButton.setTitle("OR", for: .normal)
+            eightButton.setTitle("NOR", for: .normal)
+            nineButton.setTitle("NOT", for: .normal)
+        }
+        
     }
     
     @IBAction func clearButton(_ sender: Any) {
@@ -55,6 +100,17 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        clearButton.customButtonType = .functionButton
+        signToggleButton.customButtonType = .functionButton
+        modeToggleButton.customButtonType = .functionButton
+        divideButton.customButtonType = .operatorButton
+        multiplyButton.customButtonType = .operatorButton
+        subtractButton.customButtonType = .operatorButton
+        addButton.customButtonType = .operatorButton
+        equalButton.customButtonType = .operatorButton
+    }
+    
 }
