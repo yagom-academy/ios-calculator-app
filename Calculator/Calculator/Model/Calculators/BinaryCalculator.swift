0b87f49273e77e73ef7b11892870607d7769a282
ryan-son <ryan.son1002@gmail.com>
2021-04-01 18:29:23 +0900

    Test: calculate 메서드 테스트 케이스 추가

--- Calculator/Calculator/Model/Calculators/BinaryCalculator.swift
+++ Calculator/Calculator/Model/Calculators/BinaryCalculator.swift
@@ -8,5 +8,9 @@
 import Foundation
 
 //struct BinaryCalculator: Subtractable, Addable, TypeConvertible {
+//    typealias T = Int
+//    var stack = Stack<Int>()
+//    var userInputNumber: Int = 0
+//    var userInputOperator: Operator = .addition
 //    
 //}
