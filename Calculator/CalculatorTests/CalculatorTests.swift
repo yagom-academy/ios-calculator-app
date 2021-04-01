021e86c90720eb5348827600c63a80d464cc587b
dacodaco <imochi@naver.com>
2021-04-01 21:11:04 +0900

    Test: 아홉자리 수 테스트 시도

--- Calculator/CalculatorTests/CalculatorTests.swift
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -28,6 +28,7 @@ class CalculatorTests: XCTestCase {
         test_convertType_일을입력했을때_return값확인()
         test_convertType_플러스를입력했을때_return값확인()
         test_calculate_플러스를입력했을때_return값확인()
+        test_showTopOfStack_열자리수입력했을때_확인()
     }
     
     func test_devide_한자리정수_두개() {
@@ -50,6 +51,15 @@ class CalculatorTests: XCTestCase {
         XCTAssertEqual(testDecimalCalculator.calculate(operateSign: "+", operatedNumber: 1, operatingNumber: 1), 2)
     }
     
+    func test_showTopOfStack_열자리수입력했을때_확인() {
+        testDecimalCalculator.stack.push(10000000000.0)
 
+        print(testDecimalCalculator.stack.top!) // 어떤 목적으로 하는거에요?
+        XCTAssertEqual(print(testDecimalCalculator.showTopOfStack()), 1.0)
+    }
 }
-
+//  뭐가 안나와요?
+// testDecimalCalculator.showTopOfStack() 얘가 뭘 나타내는 앤데요?
+// 만들기만 했잖아요
+// 눌렀어요
+// 다시 한번 잘 생각해봐요 비교하려는 함수가 무얼 나타내고'는' 있는지
