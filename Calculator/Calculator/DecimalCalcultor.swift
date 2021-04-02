005f1040eee54444dc4005a21a13aa8e47d94f19
kio <un19451024@gmail.com>
2021-04-02 10:58:46 +0900

    [Refactor] receiveinput() 메서드명 변경 및 기능 분리 receiveInput() -> inputMathematicsSign() 기능 분리를 위한 inputFourRuleOperations(), inputEqualOperation(), pushOperand() 생성

--- Calculator/Calculator/DecimalCalcultor.swift
+++ Calculator/Calculator/DecimalCalcultor.swift
@@ -13,42 +13,42 @@ class DecimalCalculator {
     var tempNumber = ""
     var screenNumber = "0"
     var operationFunctions: [String : (Double, Double) -> Double] = [:]
-
+    
     init() {
         operationFunctions["+"] = { $0 + $1 }
         operationFunctions["-"] = { $0 - $1 }
         operationFunctions["x"] = { $0 * $1 }
         operationFunctions["/"] = { $0 / $1 }
     }
- 
+    
     private func calculate() {
         guard let operatorSign = operators.pop(),
-            let operationFunction = operationFunctions[operatorSign],
-            let secondOperand = operands.pop(),
-            let firstOperand = operands.pop() else { return }
+              let operationFunction = operationFunctions[operatorSign],
+              let secondOperand = operands.pop(),
+              let firstOperand = operands.pop() else { return }
         let result: Double = operationFunction(firstOperand, secondOperand)
         operands.push(result)
     }
-
+    
     func reset() {
         operands.clear()
         operators.clear()
         tempNumber = ""
         screenNumber = "0"
     }
-
+    
     private func updateScreenNumber() {
         guard let lastOperand = operands.top() else { return }
         screenNumber = String(lastOperand)
     }
-
+    
     func isExistDecimalPoint(_ buttonType: String) -> Bool {
         if buttonType == "." && tempNumber.contains(".") {
             return true 
         }
         return false
     }
-
+    
     func isOverFlowedNineDigits(_ buttonType: String) -> Bool {
         var digitCount = 0
         for number in tempNumber {
@@ -62,7 +62,7 @@ class DecimalCalculator {
         }
         return false
     }
-
+    
     func concatenateNumberOrDot(_ buttonType: String) {
         if isOverFlowedNineDigits(buttonType) {
             return
@@ -79,45 +79,59 @@ class DecimalCalculator {
         }
         tempNumber += buttonType
     }
-
+    
     func inputMathematicsSign(buttonText: String) {
         switch buttonText {
-            case "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "." :
-                concatenateNumberOrDot(buttonText)
-                screenNumber = (Double(tempNumber)?.applyNumberFormatter())!
-            case "+", "-", "x", "/":
-                guard let doubleValue = Double(tempNumber) else { return }
-                operands.push(doubleValue)
-                if buttonText == "+" || buttonText == "-" {
-                    while !operators.isEmpty() {
-                        calculate()
-                    }
-                } else if buttonText == "x" || buttonText == "/" {
-                    while operators.top() == "x" || operators.top() == "/" {
-                        calculate()
-                    }
-                }
-                updateScreenNumber()
-                operators.push(buttonText)
-                tempNumber = ""
-            case "c":
-                reset()
-            case "=":
-                guard let doubleValue = Double(tempNumber) else { return }
-                operands.push(doubleValue)
-                if operands.count == operators.count {
-                // TODO: 마지막 입력이 연산자 이고 그 다음 "="을 눌렀을 때 마지막 피연산자와 연산자를 프로퍼티로 저장
-                    operands.push(doubleValue)
-                }
-                // TODO: "="을 계속 눌렀을 때 처리
-                while !operators.isEmpty() {
-                    calculate()
-                }
-                updateScreenNumber()
-                tempNumber = String(operands.top()!)
-            default:
-                print("input error")
+        case "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "." :
+            concatenateNumberOrDot(buttonText)
+            screenNumber = (Double(tempNumber)?.applyNumberFormatter())!
+        case "+", "-", "x", "/":
+            inputFourRuleOperation(sign: buttonText)
+            updateScreenNumber()
+            operators.push(buttonText)
+            tempNumber = ""
+        case "c":
+            reset()
+        case "=":
+            inputEqualOperation()
+            updateScreenNumber()
+            tempNumber = String(operands.top()!)
+        default:
+            print("input error")
         }
+    }
+    
+    func inputFourRuleOperation(sign: String) {
+        pushOperand()
+        
+        if sign == "+" || sign == "-" {
+            while !operators.isEmpty() {
+                calculate()
+            }
+        } else if sign == "x" || sign == "/" {
+            while operators.top() == "x" || operators.top() == "/" {
+                calculate()
+            }
+        }
+    }
+    
+    func inputEqualOperation() {
+        pushOperand()
+        
+        // if operands.count == operators.count {
+        // // TODO: 마지막 입력이 연산자 이고 그 다음 "="을 눌렀을 때 마지막 피연산자와 연산자를 프로퍼티로 저장
+        //     operands.push(doubleValue)
+        // }
+        // TODO: "="을 계속 눌렀을 때 처리
+        
+        while !operators.isEmpty() {
+            calculate()
+        }
+    }
+    
+    func pushOperand() {
+        guard let doubleValue = Double(tempNumber) else { return }
+        operands.push(doubleValue)
     }
 }
 
