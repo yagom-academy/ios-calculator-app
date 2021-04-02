86a8658c1db24eedba76e8eab2bd67ca2cdaa00e
summer0817 <step33@naver.com>
2021-04-02 19:01:49 +0900

    Feat: playground에 아홉자리 수 제한 연습

--- Calculator/연습용.playground/Contents.swift
+++ Calculator/연습용.playground/Contents.swift
@@ -1,6 +1,10 @@
-
-let numbers: [Int] = [0, 1, 2, 3, 4]
-let doubleNumbers: [Int] = []
-
-doubleNumbers = numbers.map( $0 * 2 )
+ func convert(binaryNumber: Int) -> Int {
+     if binaryNumber < 1000000000 && binaryNumber > 11111111 {
+         //9자리인 경우
+     } else if binaryNumber < 100000000 {
+         //8자리 이하인 경우
+     } else {
+         //10자리 이상인 경우
+     }
+ }
 
