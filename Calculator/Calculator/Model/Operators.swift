2e1d69e2d22aaae12c09e82764475ce124643302
yoonju lee <rsyjlee@gmail.com>
2021-06-22 11:39:40 +0900

    feat: UserInput 클래스에 연산표기방식별 배열 추가

---
+++ Calculator/Calculator/Model/Operators.swift
@@ -0,0 +1,8 @@
+//
+//  Operators.swift
+//  Calculator
+//
+//  Created by 이윤주 on 2021/06/22.
+//
+
+import Foundation
---
+++ Calculator/Calculator/Model/UserInput.swift
@@ -0,0 +1,14 @@
+//
+//  UserInput.swift
+//  Calculator
+//
+//  Created by 이윤주 on 2021/06/22.
+//
+
+import Foundation
+
+class UserInput {
+    var infix: Array<String> = []
+    var postfix: Array<String> = []
+    var operators: Array<String> = []
+}
