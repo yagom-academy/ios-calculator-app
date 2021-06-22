bc8f7ff3f88b21910ff3aad8a200699713753115
Ellen <80380535+jcrescent61@users.noreply.github.com>
2021-06-22 11:04:38 +0900

    feat: UI 버튼 동작시 Label에 숫자입력 구현

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -7,12 +7,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var displayUserInputNumber: UILabel!
+    
+    var userInput: Bool = false
+    
+    @IBAction func numberButtonDidTap(_ sender: UIButton) {
+        if userInput {
+            let currentDisplay = displayUserInputNumber.text!
+            displayUserInputNumber.text = currentDisplay + sender.currentTitle!
+        } else {
+            displayUserInputNumber.text = sender.currentTitle!
+        }
+        userInput = true
+    }
+    
+    @IBAction func operatorButtonDidTap(_ sender: UIButton) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
-
