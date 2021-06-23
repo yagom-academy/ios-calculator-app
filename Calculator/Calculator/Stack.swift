513bf9752d0ba6cf8cd11242cdbfb700ba669243
홍정아 <isjoeyful@gmail.com>
2021-06-23 10:22:46 +0900

    feat: Stack 구조체 구현

---
+++ Calculator/Calculator/Stack.swift
@@ -0,0 +1,28 @@
+//
+//  Stack.swift
+//  Calculator
+//
+//  Created by 홍정아 on 2021/06/23.
+//
+
+import Foundation
+
+struct Stack<T> {
+    var stack: Array<T> = []
+    
+    var isEmpty: Bool {
+        return stack.isEmpty
+    }
+    
+    mutating func push(_ element: T) {
+        stack.append(element)
+    }
+    
+    mutating func pop() -> T? {
+        return stack.popLast()
+    }
+    
+    var top: T? {
+        return stack.last
+    }
+}
