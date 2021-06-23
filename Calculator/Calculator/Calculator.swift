5a459f8b81bd53dd484970888cd5f6a081a86f56
brian2931 <sem789456@gmail.com>
2021-06-23 15:44:36 +0900

    feat: popAndAppend()
    feat: popAndAppend function for refactoring

--- Calculator/Calculator/Calculator.swift
+++ Calculator/Calculator/Calculator.swift
@@ -13,31 +13,36 @@ class Calculator {
         let postfix: [String] = changeToPostfix(infix: infix)
         return result
     }
+    
     func changeToPostfix(infix: [String]) -> [String] {
         var temporarySignStack = Stack<String>()
         var postfix: [String] = []
         for element in infix {
-            if let _ = Int(element) {
+            if let _ = Double(element) {
                 postfix.append(element)
-                continue
+            } else {
+                popAndAppend(sign: element, from: &temporarySignStack, to: &postfix)
+                temporarySignStack.push(element)
             }
-            while !temporarySignStack.isEmpty
-                    && !hasHigherPriority(this: element, than: temporarySignStack.top!) {
-                let poppedSign = temporarySignStack.pop()
-                
-                postfix.append(poppedSign!)
-
-            }
-            temporarySignStack.push(element)
-
         }
+        
         while !temporarySignStack.isEmpty {
             let poppedSign = temporarySignStack.pop()
             postfix.append(poppedSign!)
         }
         return postfix
     }
+    
     func hasHigherPriority(this: String, than: String) -> Bool {
         return (this == "/" || this == "x") && (than == "+" || than == "-")
+    }
+    
+    func popAndAppend( sign: String, from temporarySignStack: inout Stack<String>, to postfix: inout [String]) {
+        while let topOfTemporarySignStcak = temporarySignStack.top,
+              !hasHigherPriority(this: sign, than: topOfTemporarySignStcak) {
+            if let poppedSign = temporarySignStack.pop() {
+                postfix.append(poppedSign)
+            }
+        }
     }
 }
