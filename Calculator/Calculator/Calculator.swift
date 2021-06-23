a2c13dcf786d048cba6d81f3497595a992bb96f1
홍정아 <isjoeyful@gmail.com>
2021-06-23 16:35:55 +0900

    refactor: hasHigherPriority() 로직 리팩토링
    - Operator 열거형에 연산자 우선순위 비교 메서드 구현
    - convertToOperator()에서 연산자를 Operator 열거형으로 반환하는 기능 구현
      - 에러처리 기능 구현
    - OperatorError 열거형 구현

--- Calculator/Calculator/Calculator.swift
+++ Calculator/Calculator/Calculator.swift
@@ -7,39 +7,81 @@
 
 import Foundation
 
+enum OperatorError: Error {
+    case unknownOperator
+}
+
+enum Operator {
+    static let lowPriority = 1
+    static let highPriority = 2
+    
+    case plus, minus
+    case multiply, divide
+
+    var priority: Int {
+        switch self {
+        case .plus, .minus:
+            return Operator.lowPriority
+        case .multiply, .divide:
+            return Operator.highPriority
+        }
+    }
+
+    static func >(lhs: Operator, rhs: Operator) -> Bool {
+        return lhs.priority > rhs.priority
+    }
+}
+
 class Calculator {
-    func calculate(infix: [String]) -> String {
+    func calculate(infix: [String]) throws -> String {
         var result: String = ""
-        let postfix: [String] = changeToPostfix(infix: infix)
+        let postfix: [String] = try changeToPostfix(infix: infix)
         return result
     }
+
+    func convertToOperator(string: String) throws -> Operator {
+        switch string {
+        case "+":
+            return .plus
+        case "-":
+            return .minus
+        case "×":
+            return .multiply
+        case "÷":
+            return .divide
+        default:
+            throw OperatorError.unknownOperator
+        }
+    }
     
-    func changeToPostfix(infix: [String]) -> [String] {
+    func changeToPostfix(infix: [String]) throws -> [String] {
         var temporarySignStack = Stack<String>()
         var postfix: [String] = []
         for element in infix {
             if let _ = Double(element) {
                 postfix.append(element)
             } else {
-                popAndAppend(sign: element, from: &temporarySignStack, to: &postfix)
+                try popAndAppend(sign: element, from: &temporarySignStack, to: &postfix)
                 temporarySignStack.push(element)
             }
         }
-        
+
         while !temporarySignStack.isEmpty {
             let poppedSign = temporarySignStack.pop()
             postfix.append(poppedSign!)
         }
         return postfix
     }
-    
-    func hasHigherPriority(this: String, than: String) -> Bool {
-        return (this == "/" || this == "x") && (than == "+" || than == "-")
+
+    func hasHigherPriority(this: String, than: String) throws -> Bool {
+        let currentOperator: Operator = try convertToOperator(string: this)
+        let thanOperator: Operator = try convertToOperator(string: than)
+        return currentOperator > thanOperator
     }
     
-    func popAndAppend( sign: String, from temporarySignStack: inout Stack<String>, to postfix: inout [String]) {
+    func popAndAppend( sign: String, from temporarySignStack: inout Stack<String>, to postfix: inout [String]) throws {
         while let topOfTemporarySignStcak = temporarySignStack.top,
-              !hasHigherPriority(this: sign, than: topOfTemporarySignStcak) {
+              try !hasHigherPriority(this: sign, than: topOfTemporarySignStcak) {
             if let poppedSign = temporarySignStack.pop() {
                 postfix.append(poppedSign)
             }
--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -11,14 +11,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
     }
 
-    @IBAction func random(_ sender: Any) {
-        let calculator = Calculator()
-        let postfix = calculator.calculate(infix: ["7", "+", "3", "x", "3", "+", "4"])
-        print(postfix)
-    }
     
 }
 
