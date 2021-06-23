1fad4b04d34c7811be5de0181560a79b6275d5e9
yoonju lee <rsyjlee@gmail.com>
2021-06-24 08:53:57 +0900

    feat: Operators의 연산자 우선순위 지정
    - priority 연산 프로퍼티를 통해 연산자에 우선순위를 지정했습니다.
    - Comparable을 채택해 Operators끼리 우선순위를 비교할 수 있도록 했습니다.

--- Calculator/Calculator/Model/Operators.swift
+++ Calculator/Calculator/Model/Operators.swift
@@ -7,13 +7,26 @@
 
 import Foundation
 
-struct Operators {
-    enum Operators: String, CaseIterable {
-        case plus = "+"
-        case minus = "-"
-        case multiply = "×"
-        case divide = "÷"
-        
-        
+enum Operators: String, Comparable {
+    case plus = "+"
+    case minus = "-"
+    case multiply = "×"
+    case divide = "÷"
+    
+    var operatorSymbol: String {
+        self.rawValue
+    }
+    
+    var priority: Int {
+        switch self {
+        case .plus, .minus:
+            return 10
+        case .multiply, .divide:
+            return 100
+        }
+    }
+    
+    static func < (lhs: Operators, rhs: Operators) -> Bool {
+        lhs.priority < rhs.priority
     }
 }
