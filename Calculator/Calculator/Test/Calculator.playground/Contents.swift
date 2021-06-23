89ffbfe2fd88db91a9af83bf0333f90b8188ca5d
jyoon54 <jiwonyoon@Jiwonui-MacBookPro.local>
2021-06-23 10:55:49 +0900

    feat: 0으로 나눌때 에러 던지도록 수정, calculatePostfix함수 내 강제 언래핑 일부 수정

--- Calculator/Calculator/Test/Calculator.playground/Contents.swift
+++ Calculator/Calculator/Test/Calculator.playground/Contents.swift
@@ -27,68 +27,75 @@ class Stack {
 import Foundation
 
 class Calculator {
-    var currentInput = ""
-    var infixNotation = [String]()
-    
-    func isPriorOperator(this first: String, to second: String) -> Bool {
-        return "*/".contains(first) && "+-".contains(second)
-    }
-    
-    func moveNonPriorOperator(from stack: Stack, to postfix: inout [String], than element: String) {
-        while let top = stack.top(), !isPriorOperator(this: element, to: top) {
-            guard let top = stack.pop() else {
-                continue
-            }
-            postfix.append(top)
-        }
-    }
+	var currentInput = ""
+	var infixNotation = [String]()
+	
+	func isPriorOperator(this first: String, to second: String) -> Bool {
+		return "*/".contains(first) && "+-".contains(second)
+	}
+	
+	func moveNonPriorOperator(from stack: Stack, to postfix: inout [String], than element: String) {
+		while let top = stack.top(), !isPriorOperator(this: element, to: top) {
+			guard let top = stack.pop() else {
+				continue
+			}
+			postfix.append(top)
+		}
+	}
 
-    func convertToPosfix() -> [String] {
-        let tempStack = Stack()
-        var postfix = [String]()
+	func convertToPosfix() -> [String] {
+		let tempStack = Stack()
+		var postfix = [String]()
 
-        for element in infixNotation {
-            if let _ = Double(element) {
-                postfix.append(element)
-                continue
-            }
-            moveNonPriorOperator(from: tempStack, to: &postfix, than: element)
-            tempStack.push(element: element)
-        }
-        while let top = tempStack.pop() {
-            postfix.append(top)
-        }
-        return postfix
-    }
+		for element in infixNotation {
+			if let _ = Double(element) {
+				postfix.append(element)
+				continue
+			}
+			moveNonPriorOperator(from: tempStack, to: &postfix, than: element)
+			tempStack.push(element: element)
+		}
+		while let top = tempStack.pop() {
+			postfix.append(top)
+		}
+		return postfix
+	}
 
-    func calculatePosfix() -> String {
-        let postfix = convertToPosfix()
-        let tempStack = Stack()
+	func calculatePosfix() throws -> String {
+		let postfix = convertToPosfix()
+		let tempStack = Stack()
 
-        for element in postfix {
-            if let _  = Double(element) {
-                tempStack.push(element: element)
-                continue
-            }
-            if let operand2 = Double(tempStack.pop()!), let operand1 = Double(tempStack.pop()!) {
-                var result: Double?
-                switch element {
-                case "+":
-                    result = operand1 + operand2
-                case "-":
-                    result = operand1 - operand2
-                case "*":
-                    result = operand1 * operand2
-                case "/":
-                    result = operand1 / operand2
-                default:
-                    break
-                }
-                tempStack.push(element: String(result!))
-            }
-        }
-        return tempStack.pop()!
-    }
+		for element in postfix {
+			if let _  = Double(element) {
+				tempStack.push(element: element)
+				continue
+			}
+			
+			guard let operand2 = tempStack.pop(), let operand1 = tempStack.pop() else {
+				continue
+			}
+			if let operand2 = Double(operand2), let operand1 = Double(operand1) {
+				var result: Double?
+				switch element {
+				case "+":
+					result = operand1 + operand2
+				case "-":
+					result = operand1 - operand2
+				case "*":
+					result = operand1 * operand2
+				case "/":
+					if operand2 == 0 {
+						throw ErrorCases.dividedByZero
+					}
+					result = operand1 / operand2
+				default:
+					break
+				}
+				tempStack.push(element: String(result!))
+			}
+		}
+		return tempStack.pop()!
+	}
 }
 
 var c = Calculator()
@@ -96,6 +103,7 @@ c.infixNotation = ["5","+","2","/","7","*","4"]
 let postfix = c.convertToPosfix()
 //["5", "2", "7", "/", "4", "*", "+"]
 print(postfix)
-let result = c.calculatePosfix()
+let result = try? c.calculatePosfix()
+
 print(result)
 
--- Calculator/Calculator/Test/Calculator_string.playground/Contents.swift
+++ Calculator/Calculator/Test/Calculator_string.playground/Contents.swift
@@ -1,54 +1,77 @@
 import Foundation
 
+import Foundation
+
+enum ErrorCases: Error {
+    case emptyStackAccess
+    case dividedByZero
+}
+
+class Stack {
+    var stack: [String] = []
+    
+    func isEmpty() -> Bool {
+        return stack.isEmpty
+    }
+    
+    func top() -> String? {
+        return stack.last
+    }
+    
+    func push(element: String) {
+        stack.append(element)
+    }
+    
+    func pop() -> String? {
+        return stack.removeLast()
+    }
+}
+
+
+
+
 class Calculator {
     var currentInput = ""
     var infixNotation = [String]()
     
-    func isPriorOperator(this first: String, to second: String) -> Bool {
+    func isPrior(operator first: String, to second: String) -> Bool {
         return "*/".contains(first) && "+-".contains(second)
     }
     
-    func moveNonPriorOperator(from stack: Stack, to postfix: inout [String], than element: String) {
-        while let top = stack.top(), !isPriorOperator(this: element, to: top) {
-            guard let top = stack.pop() else {
-                continue
-            }
+    func pushOperator(to stack: Stack, postfix: inout [String], element: String) {
+        while let top = stack.pop(), !isPrior(operator: element, to: top) {
             postfix.append(top)
         }
+        stack.push(element: element)
     }
 
     func convertToPosfix() -> [String] {
         let tempStack = Stack()
         var postfix = [String]()
-
+        
         for element in infixNotation {
             if let _ = Double(element) {
                 postfix.append(element)
                 continue
             }
-            moveNonPriorOperator(from: tempStack, to: &postfix, than: element)
-            tempStack.push(element: element)
+            pushOperator(to: tempStack, postfix: &postfix, element: element)
         }
         while let top = tempStack.pop() {
             postfix.append(top)
         }
         return postfix
     }
-
-    func calculatePosfix() throws -> String {
+    
+    func calculatePosfix() -> String {
         let postfix = convertToPosfix()
         let tempStack = Stack()
-
+        
         for element in postfix {
             if let _  = Double(element) {
                 tempStack.push(element: element)
                 continue
             }
-			
-			guard let operand2 = tempStack.pop(), let operand1 = tempStack.pop() else {
-				continue
-			}
-			if let operand2 = Double(operand2), let operand1 = Double(operand1) {
+            if let operand2 = Double(tempStack.pop()!), let operand1 = Double(tempStack.pop()!) {
                 var result: Double?
                 switch element {
                 case "+":
@@ -58,9 +81,6 @@ class Calculator {
                 case "*":
                     result = operand1 * operand2
                 case "/":
-					if operand2 == 0 {
-						throw ErrorCases.dividedByZero
-					}
                     result = operand1 / operand2
                 default:
                     break
@@ -70,4 +90,11 @@ class Calculator {
         }
         return tempStack.pop()!
     }
+    
 }
+
+var c = Calculator()
+c.infixNotation = ["5","+","2","/","7","*","4"]
+let result = c.calculatePosfix()
+
+print(result)
