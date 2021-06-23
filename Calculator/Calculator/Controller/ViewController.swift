6a92589bee2e6a8200131d43f9b46547085fbfc8
jgkim1008 <rlawnsrjs100@naver.com>
2021-06-23 14:48:25 +0900

    refactor : operator의 값을 비교하고 갱신하는 메쏘드 구현
    * 이전 입력된 operator의 값을 비교하여 같으면 추가하지 않고, 다르면 배열에 추가하는 메쏘드

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -8,18 +8,24 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let calculator = Calculator()
     var inputStorage: [String] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    func checkReasonableOperation(with: Calculator.Component){
-        switch with {
-        case .number:
-            inputStorage.last
-        default:
-            <#code#>
+    func updateChangableOperator(with: String) {
+        guard let lastElement = inputStorage.last,
+              let currentType = try? calculator.convertToComponentType(from: with),
+              currentType == .operator else {
+            inputStorage.append(with)
+            return
+        }
+        guard with == lastElement else {
+            let lastIndex = inputStorage.count-1
+            inputStorage[lastIndex] = with
+            return
         }
     }
     
@@ -28,61 +34,81 @@ class ViewController: UIViewController {
     }
     
     @IBAction func touchUpDoubleZeroButton(_ sender: UIButton) {
+        inputStorage.append("00")
     }
     
     @IBAction func touchUpOneButton(_ sender: UIButton) {
-        
+        inputStorage.append("1")
     }
     
     @IBAction func touchUpTwoButton(_ sender: UIButton) {
+        inputStorage.append("2")
     }
     
     @IBAction func touchUpThreeButton(_ sender: UIButton) {
+        inputStorage.append("3")
     }
     
     @IBAction func touchUpFourButton(_ sender: UIButton) {
+        inputStorage.append("4")
     }
     
     @IBAction func touchUpFiveButton(_ sender: UIButton) {
+        inputStorage.append("5")
     }
     
     @IBAction func touchUpSixButton(_ sender: UIButton) {
+        inputStorage.append("6")
     }
     
     @IBAction func touchUpSevenButton(_ sender: UIButton) {
+        inputStorage.append("7")
     }
     
     @IBAction func touchUpEightButton(_ sender: UIButton) {
+        inputStorage.append("8")
     }
     
     @IBAction func touchUpNineButton(_ sender: UIButton) {
+        inputStorage.append("9")
     }
     
     @IBAction func touchUpDotButton(_ sender: UIButton) {
+        inputStorage.append(".")
     }
     
     @IBAction func touchUpDivideButton(_ sender: UIButton) {
+        updateChangableOperator(with: "/")
     }
     
     @IBAction func touchUpMultiplyButton(_ sender: UIButton) {
+        updateChangableOperator(with: "*")
     }
     
     @IBAction func touchUpMinusButton(_ sender: UIButton) {
+        updateChangableOperator(with: "-")
     }
     
     @IBAction func touchUpPlusButton(_ sender: UIButton) {
+        updateChangableOperator(with: "+")
     }
     
     @IBAction func touchUpEqualButton(_ sender: UIButton) {
+        inputStorage.append("=")
     }
     
     @IBAction func touchUpAllClearButton(_ sender: UIButton) {
+        inputStorage.removeAll()
+        // 전부 초기화
     }
     
     @IBAction func touchUpClearEntryButton(_ sender: UIButton) {
+
+        // =,숫자일떄 해야할 내용
     }
     
     @IBAction func touchUpConvertSignButton(_ sender: UIButton) {
+        inputStorage.append("⁺⁄₋")
     }
     
 }
