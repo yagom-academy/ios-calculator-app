bce298a98cf2e65cac76bb9ac997c0b5d0696154
yaewonLee <yw990324@gmail.com>
2021-06-24 21:27:25 +0900

    feat : 제네릭 스택에 필요한 기능 구현

---
+++ Calculator/Calculator/Model/Stack.swift
@@ -0,0 +1,26 @@
+//
+//  Stack.swift
+//  Calculator
+//
+//  Created by 이예원 on 2021/06/24.
+//
+
+import Foundation
+
+struct Stack<T> {
+    private var stack = Array<T>()
+    
+    var isEmpty: Bool {
+        return stack.isEmpty
+    }
+    var top: T? {
+        return stack.last
+    }
+    
+    mutating func push(_ value: T) {
+        stack.append(value)
+    }
+    mutating func pop() -> T {
+        return stack.removeLast()
+    }
+}
