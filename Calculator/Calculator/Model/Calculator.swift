801c8b815e2783db5f2afd7108bbfce18e1cd852
yaewonLee <yw990324@gmail.com>
2021-06-24 21:42:07 +0900

    feat : Calculator 구조체 생성
    전위연산큐와 후위연산큐 생성

---
+++ Calculator/Calculator/Model/Calculator.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator.swift
+//  Calculator
+//
+//  Created by 이예원 on 2021/06/24.
+//
+
+import Foundation
+
+struct Calculator {
+    private var infixQueue = Queue<String>()
+    private var postfixQueue = Queue<String>()
+    
+    mutating func transformInfixToPostfix() {
+        
+    }
+}
