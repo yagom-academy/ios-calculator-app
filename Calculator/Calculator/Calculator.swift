7eead54352b14350937273186503a4785c4cc10a
hosinging <pdhou23@gmail.com>
2021-06-25 16:19:19 +0900

    feat: 후위 연산으로 바꿔주는 함수 converToPostfixNotation(of:) 구현 test: 테스트를 위한 메인 함수 추가

--- Calculator/Calculator/Calculator.swift
+++ Calculator/Calculator/Calculator.swift
@@ -26,8 +26,8 @@ enum Operator: String, Equatable, Comparable {
         lhs.priority < rhs.priority
     }
     
-    static func == (lhs: Operator, rhs: Operator) -> Bool {
-        lhs.priority == rhs.priority
+    static func >= (lhs: Operator, rhs: Operator) -> Bool {
+        lhs.priority >= rhs.priority
     }
 }
 
@@ -36,11 +36,52 @@ enum OperatorPriority: Int {
     case high = 1
 }
 
+enum CalculatorError: Error {
+    case unknown
+}
+
 class Calculator {
-    var infixArray: Array<Any> = []
+    var infixArray: Array<Any> = [5.0, Operator.plus, 2.0, Operator.division, 7.0, Operator.multiplication, 3.0]
     var stack = Stack<Any>()
     var postArray: Array<Any> = []
     
-    
+    func converToPosifixNotation(of originArray: Array<Any>) -> Array<Any> {
+        for item in originArray {
+            switch item {
+            case _ as Double:
+                postArray.append(item)
+            case let someOperator as Operator:
+                if stack.isEmpty {
+                    stack.push(element: someOperator)
+                    print(stack)
+                } else {
+                    guard let topOfStack = stack.top as? Operator else { return [1] }
+                    
+                    if topOfStack >= someOperator {
+                        guard let topOfStack2 = stack.pop() as? Operator else { return [2] }
+                        postArray.append(topOfStack2)
+                        stack.push(element: someOperator)
+                    } else {
+                        stack.push(element: someOperator)
+                    }
+                }
+            default: return [3]
+            }
+           
+        }
+        while stack.isEmpty == false {
+            guard let topOfStack = stack.pop() else {
+                return [5]
+            }
+            postArray.append(topOfStack)
+        }
+        return postArray
+    }
+}
 
+func main() {
+    let c = Calculator()
+    let a = c.converToPosifixNotation(of: c.infixArray)
+    print(a)
+    
 }
