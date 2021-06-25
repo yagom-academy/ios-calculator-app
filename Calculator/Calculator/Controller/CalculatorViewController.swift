f705b2aaf128ccd7b0579ab83846cf3469f229ba
KimWanki <seoulkwk95@gmail.com>
2021-06-26 00:25:59 +0900

    feat: NumberButton 타입 선언, 계산기 인스턴스 생성

---
+++ Calculator/Calculator/Controller/CalculatorViewController.swift
@@ -0,0 +1,40 @@
+//
+//  Calculator - ViewController.swift
+//  Created by yagom. 
+//  Copyright © yagom. All rights reserved.
+// 
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+    
+    enum NumberButton: CustomStringConvertible {
+        case one, two, three, four, five, six, seven,
+             eight, nine, zero, doubleZero
+        
+        var description: String {
+            switch self {
+            case .one: return "1"
+            case .two: return "2"
+            case .three: return "3"
+            case .four: return "4"
+            case .five: return "5"
+            case .six: return "6"
+            case .seven: return "7"
+            case .eight: return "8"
+            case .nine: return "9"
+            case .zero: return "0"
+            case .doubleZero: return "00"
+            }
+        }
+    }
+    var calculator = Calculator()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
