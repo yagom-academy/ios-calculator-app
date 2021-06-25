e5f43bc58a41d6fd27fd4c9ace1c4ef4e81694f3
YesCoach <sbsii1020@gmail.com>
2021-06-26 00:33:00 +0900

    feat: IBOutlet, IBAction 추가

--- Calculator/Calculator/Controller/CalculatorViewController.swift
+++ Calculator/Calculator/Controller/CalculatorViewController.swift
@@ -30,11 +30,58 @@ class CalculatorViewController: UIViewController {
     }
     var calculator = Calculator()
     
+    @IBOutlet var numberInputLabel: UILabel!
+    @IBOutlet var operatorInputLabel: UILabel!
+    @IBOutlet weak var CalculationStackView: UIStackView!
+    @IBOutlet weak var CalculationStackScrollView: UIScrollView!
+    
+    @IBAction func clickNumberOneButton(_ sender: UIButton) {
+    }
+    @IBAction func clickNumberTwoButton(_ sender: UIButton) {
+    }
+    @IBAction func clickNumberThreeButton(_ sender: UIButton) {
+    }
+    @IBAction func clickNumberFourButton(_ sender: UIButton) {
+    }
+    @IBAction func clickNumberFiveButton(_ sender: UIButton) {
+    }
+    @IBAction func clickNumberSixButton(_ sender: UIButton) {
+    }
+    @IBAction func clickNumberSevenButton(_ sender: UIButton) {
+    }
+    @IBAction func clickNumberEightButton(_ sender: UIButton) {
+    }
+    @IBAction func clickNumberNineButton(_ sender: UIButton) {
+    }
+    @IBAction func clickNumberZeroButton(_ sender: UIButton) {
+    }
+    @IBAction func clickNumberDoubleZeroButton(_ sender: UIButton) {
+    }
+    @IBAction func clickDotButton(_ sender: UIButton) {
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func clickPlusOperatorButton(_ sender: UIButton) {
+    }
+    @IBAction func clickMinusOperatorButton(_ sender: UIButton) {
+    }
+    @IBAction func clickMultiplyOperatorButton(_ sender: UIButton) {
+    }
+    @IBAction func clickDivideOperatorButton(_ sender: UIButton) {
+    }
+    @IBAction func clickEqualOperatorButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func clickAllClearButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func clickClearEntryButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func clickToggleSignButton(_ sender: UIButton) {
+    }
 }
-
