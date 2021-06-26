7d7c8ea78f1d2c99757d5d9109a2592c05e6c9db
hosinging <pdhou23@gmail.com>
2021-06-26 12:22:16 +0900

    refactor: 후위연산식 계산하는 calculatePostfix()리팩토링 refactor: Operator enum에 comaparable프로토콜 명시적 채택 취소 refactor: infixArray, postfixArray 빈 배열 생성하는 코드 수정 chore: convertToPostfixNotation()함수명 오타 수정 test: convertToPostfixNotation()함수 내 에러 리턴 내용 문자열 배열로 명시적 표현 test: convertToPostfixNotaion()함수와 calculatePostfix()함수 테스트하는 코드들 주석처리

--- Calculator/Calculator/Calculator.swift
+++ Calculator/Calculator/Calculator.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-enum Operator: String, Comparable {
+enum Operator: String {
     case plus = "+"
     case minus = "-"
     case multiplication = "*"
@@ -38,23 +38,23 @@ enum CalculatorError: Error {
 }
 
 class Calculator {
-    private var infixArray: Array<String> = []
+    private var infixArray = [String]()
     private var stack = Stack<String>()
-    private var postfixArray: Array<String> = []
+    private var postfixArray = [String]()
     
-    func converToPosifixNotation(of originArray: Array<String>) -> Array<String> {
-        for item in originArray {
-            if Double(item) != nil {
+    func converToPostfixNotation() -> [String] {
+        for item in infixArray {
+            if let _ = Double(item) {
                 postfixArray.append(item)
             } else {
                 if stack.isEmpty {
                     stack.push(element: item)
                 } else {
-                    guard let topOfStack = stack.top else { return ["1"] }
+                    guard let topOfStack = stack.top else { return ["Error"] }
                     
                     if topOfStack >= item {
-                        guard let topOfStack2 = stack.pop() else { return ["2"] }
-                        postfixArray.append(topOfStack2)
+                        guard let poppedValue = stack.pop() else { return ["Error"] }
+                        postfixArray.append(poppedValue)
                         stack.push(element: item)
                     } else {
                         stack.push(element: item)
@@ -64,7 +64,7 @@ class Calculator {
         }
         while stack.isEmpty == false {
             guard let topOfStack = stack.pop() else {
-                return ["5"]
+                return ["Error"]
             }
             postfixArray.append(topOfStack)
         }
@@ -80,36 +80,35 @@ class Calculator {
     
     func calculatePostfix() throws -> Double {
         for item in postfixArray {
-            if let item = item as? Double {
+            if let _ = Double(item) {
                 stack.push(element: item)
             } else {
-                guard let firstElement = stack.pop(), let secondElement = stack.pop() else { return 0.0}
-                guard let firstNumber = firstElement as? Double,
-                      let secondNumber = secondElement as? Double else { return 0.0 }
+                guard let firstElement = stack.pop(), let secondElement = stack.pop() else { return 0.0 }
+                guard let firstNumber = Double(firstElement),
+                      let secondNumber = Double(secondElement) else { return 0.0 }
                 
-                let `operator` = try? checkOperator(item as? String ?? "")
+                let `operator` = try? checkOperator(item)
                 
                 switch `operator` {
                 case .plus:
-                    stack.push(element: add(firstNumber, secondNumber))
+                    stack.push(element: String(add(firstNumber, secondNumber)))
                 case .minus:
-                    stack.push(element: substract(firstNumber, secondNumber))
+                    stack.push(element: String(substract(firstNumber, secondNumber)))
                 case .multiplication:
-                    stack.push(element: multiply(firstNumber, secondNumber))
+                    stack.push(element: String(multiply(firstNumber, secondNumber)))
                 case .division:
-                    let result = try? divide(firstNumber, secondNumber)
-                    stack.push(element: result as Any)
+                    let result = try divide(secondNumber, firstNumber)
+                    stack.push(element: String(result))
                 default:
                     break
                 }
             }
         }
-        guard let lastElement = stack.pop(), let result = Double(lastElement as? Substring ?? "") else {
+        guard let lastElement = stack.pop(), let result = Double(lastElement) else {
             throw CalculatorError.unknown
         }
         return result
     }
-    
 }
 
 extension Calculator: Calculatable {
@@ -133,8 +132,14 @@ extension Calculator: Calculatable {
     }
 }
 
-func main() {
-    let c = Calculator()
-    let a = c.converToPosifixNotation(of: ["5.0", Operator.plus.rawValue, "2.0", Operator.division.rawValue, "7.0", Operator.multiplication.rawValue, "3.0"])
-    print(a)
-}
+//MARK: - 테스트를 위한 메인함수
+//func main() {
+//    let c = Calculator()
+//    let a = c.converToPosifixNotation(of: ["5.0", Operator.plus.rawValue, "2.0", Operator.division.rawValue, "7.0", Operator.multiplication.rawValue, "3.0"])
+//    do {
+//      let d = try? c.calculatePostfix()
+//    print(d)
+//    }
+//
+//    print(a)
+//}
--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -8,10 +8,10 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        main()
+        //테스트 함수 실행을 위한 호출
+//        main()
     }
 }
 
