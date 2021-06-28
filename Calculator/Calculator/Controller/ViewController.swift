d3a49318b961bf772b1af34fe9efc4684eaa156b
Ldoy <leedoy7989@gmail.com>
2021-06-28 17:08:14 +0900

    refactor: updateChangableOperatorCalculator 메소드 수정, 접근제어자 수정 완료

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -7,113 +7,132 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    let calculator: Calculatorable = Calculator()
-    var inputStorage: [String] = []
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    
-    func updateChangableOperator(with: String) {
-        guard let lastElement = inputStorage.last,
-              let lastType = try? CalculatorComponent.convertToComponentType(from: lastElement),
-              lastType == .operator else {
-            inputStorage.append(with)
-            return
-        }
-        
-        guard with == lastElement else {
-            let lastIndex = inputStorage.count-1
-            inputStorage[lastIndex] = with
-            return
-        }
-    }
-    
-    @IBAction func touchUpZeroButton(_ sender: UIButton) {
-        inputStorage.append("0")
-    }
-    
-    @IBAction func touchUpDoubleZeroButton(_ sender: UIButton) {
-        inputStorage.append("00")
-    }
-    
-    @IBAction func touchUpOneButton(_ sender: UIButton) {
-        inputStorage.append("1")
-    }
-    
-    @IBAction func touchUpTwoButton(_ sender: UIButton) {
-        inputStorage.append("2")
-    }
-    
-    @IBAction func touchUpThreeButton(_ sender: UIButton) {
-        inputStorage.append("3")
-    }
-    
-    @IBAction func touchUpFourButton(_ sender: UIButton) {
-        inputStorage.append("4")
-    }
-    
-    @IBAction func touchUpFiveButton(_ sender: UIButton) {
-        inputStorage.append("5")
-    }
-    
-    @IBAction func touchUpSixButton(_ sender: UIButton) {
-        inputStorage.append("6")
-    }
-    
-    @IBAction func touchUpSevenButton(_ sender: UIButton) {
-        inputStorage.append("7")
-    }
-    
-    @IBAction func touchUpEightButton(_ sender: UIButton) {
-        inputStorage.append("8")
-    }
-    
-    @IBAction func touchUpNineButton(_ sender: UIButton) {
-        inputStorage.append("9")
-    }
-    
-    @IBAction func touchUpDotButton(_ sender: UIButton) {
-        inputStorage.append(".")
-    }
-    
-    @IBAction func touchUpDivideButton(_ sender: UIButton) {
-        updateChangableOperator(with: "/")
-    }
-    
-    @IBAction func touchUpMultiplyButton(_ sender: UIButton) {
-        updateChangableOperator(with: "*")
-    }
-    
-    @IBAction func touchUpMinusButton(_ sender: UIButton) {
-        updateChangableOperator(with: "-")
-    }
-    
-    @IBAction func touchUpPlusButton(_ sender: UIButton) {
-        updateChangableOperator(with: "+")
-    }
-    
-    @IBAction func touchUpEqualButton(_ sender: UIButton) {
-        let postfix = calculator.convertToPostfixExpression(fromInfix: inputStorage)
-        if let result = try? String(calculator.calculatePostfixExpression(postfix: postfix)) {
-            print(result)
-        } else {
-            print("NaN")
-        }
-    }
-    
-    @IBAction func touchUpAllClearButton(_ sender: UIButton) {
-        inputStorage.removeAll()
-    }
-    
-    @IBAction func touchUpClearEntryButton(_ sender: UIButton) {
-        // =,숫자일때 해야할 내용
-    }
-    
-    @IBAction func touchUpConvertSignButton(_ sender: UIButton) {
-        inputStorage.append("⁺⁄₋")
-    }
-    
+	
+	private let calculator: Calculatorable = Calculator()
+	private let zero = "0"
+	private var inputStorage: [String] = []
+	private var numberString = ""
+		
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+	
+	private func updateChangableOperator(with: String) {
+		guard let lastElement = inputStorage.last else {
+			inputStorage.append(zero)
+			inputStorage.append(with)
+			return
+		}
+		
+		if let lastType = try? CalculatorComponent.convertToComponentType(from: lastElement) {
+			if lastType == .operator && with != lastElement {
+				let lastIndex = inputStorage.count-1
+				inputStorage[lastIndex] = with
+			} else if lastType == .number {
+				inputStorage.append(numberString)
+				inputStorage.append(with)
+				numberString = ""
+			}
+		}
+	}
+	
+	@IBAction func touchUpZeroButton(_ sender: UIButton) {
+		numberString += "0"
+	}
+	
+	@IBAction func touchUpDoubleZeroButton(_ sender: UIButton) {
+		numberString += "00"
+	}
+	
+	@IBAction func touchUpOneButton(_ sender: UIButton) {
+		numberString += "1"
+	}
+	
+	@IBAction func touchUpTwoButton(_ sender: UIButton) {
+		numberString += "2"
+	}
+	
+	@IBAction func touchUpThreeButton(_ sender: UIButton) {
+		numberString += "3"
+	}
+	
+	@IBAction func touchUpFourButton(_ sender: UIButton) {
+		numberString += "4"
+	}
+	
+	@IBAction func touchUpFiveButton(_ sender: UIButton) {
+		numberString += "5"
+	}
+	
+	@IBAction func touchUpSixButton(_ sender: UIButton) {
+		numberString += "6"
+	}
+	
+	@IBAction func touchUpSevenButton(_ sender: UIButton) {
+		numberString += "7"
+	}
+	
+	@IBAction func touchUpEightButton(_ sender: UIButton) {
+		numberString += "8"
+	}
+	
+	@IBAction func touchUpNineButton(_ sender: UIButton) {
+		numberString += "9"
+	}
+	
+	@IBAction func touchUpDotButton(_ sender: UIButton) {
+		numberString += "."
+	}
+	
+	@IBAction func touchUpDivideButton(_ sender: UIButton) {
+		updateChangableOperator(with: "/")
+	}
+	
+	@IBAction func touchUpMultiplyButton(_ sender: UIButton) {
+		updateChangableOperator(with: "*")
+	}
+	
+	@IBAction func touchUpMinusButton(_ sender: UIButton) {
+		updateChangableOperator(with: "-")
+	}
+	
+	@IBAction func touchUpPlusButton(_ sender: UIButton) {
+		updateChangableOperator(with: "+")
+	}
+	
+	@IBAction func touchUpEqualButton(_ sender: UIButton) {
+		let postfix = calculator.convertToPostfixExpression(fromInfix: inputStorage)
+		if let result = try? String(calculator.calculatePostfixExpression(postfix: postfix)) {
+			print(result)
+		} else {
+			print("NaN")
+		}
+	}
+	
+	@IBAction func touchUpAllClearButton(_ sender: UIButton) {
+		inputStorage.removeAll()
+	}
+	
+	@IBAction func touchUpClearEntryButton(_ sender: UIButton) {
+		guard let lastValue = inputStorage.last,
+			  let lastType = try? CalculatorComponent.convertToComponentType(from: lastValue),
+			  lastType != .operator else {
+			return
+		}
+		
+		if lastType == .equalSign {
+			inputStorage.removeAll()
+		} else if lastType == .number {
+			inputStorage.popLast()
+		}
+	}
+	
+	@IBAction func touchUpConvertSignButton(_ sender: UIButton) {
+		// 1. 분기처리하기
+		// 2. 버튼을 누르면 * -1 를 넣기 ->
+		
+		inputStorage.append("⁺⁄₋")
+	}
+	
 }
 
--- Calculator/Calculator/Model/Calculator.swift
+++ Calculator/Calculator/Model/Calculator.swift
@@ -1,59 +1,67 @@
 import Foundation
 //
 struct Calculator: Calculatorable {
-    
-    enum ErrorCase: Error {
-        case dividedByZero
-    }
-    
-    enum AccessibleOperator: String {
-        case plus = "+", minus = "-", multiply = "*", divide = "/"
-    }
-    
-    private let numberFormatter = NumberFormatter()
-    
-    init() {
-        numberFormatter.numberStyle = .decimal
-        numberFormatter.roundingMode = .halfUp
-        numberFormatter.maximumSignificantDigits = 20
-        if let USLocale = NSLocale().displayName(forKey: .countryCode, value: "en_US") {
-            numberFormatter.locale = NSLocale(localeIdentifier: USLocale) as Locale
-        }
-    }
-    
-    func calculatePostfixExpression(postfix: [String]) throws -> Double {
-        var stack = Stack()
-        for element in postfix {
-            let elementType = AccessibleOperator.init(rawValue: element)
-            switch elementType {
-            case .plus:
-                let next = Double(stack.pop() ?? "0") ?? 0
-                let prev = Double(stack.pop() ?? "0") ?? 0
-                stack.push(element: String(prev + next))
-            case .minus:
-                let next = Double(stack.pop() ?? "0") ?? 0
-                let prev = Double(stack.pop() ?? "0") ?? 0
-                stack.push(element: String(prev - next))
-            case .multiply:
-                let next = Double(stack.pop() ?? "0") ?? 0
-                let prev = Double(stack.pop() ?? "0") ?? 0
-                stack.push(element: String(prev * next))
-            case .divide:
-                let next = Double(stack.pop() ?? "0") ?? 0
-                let prev = Double(stack.pop() ?? "0") ?? 0
-                if next == 0 {
-                    throw ErrorCase.dividedByZero
-                } else {
-                    stack.push(element: String(prev / next))
-                }
-            default:
-                stack.push(element: element)
-            }
-        }
-        return Double(stack.pop() ?? "0") ?? 0
-    }
-    
-    func formatting(number: Double) -> String? {
-        return numberFormatter.string(from: NSNumber(value: number))
-    }
+	
+	enum ErrorCase: Error {
+		case dividedByZero
+	}
+	
+	enum AccessibleOperator: String {
+		case plus = "+", minus = "-", multiply = "*", divide = "/"
+	}
+	
+	private let numberFormatter = NumberFormatter()
+	private let zero = "0"
+	private let maximumSignificantDigits = 20
+	private let currentLocale = "en_US"
+	
+	init() {
+		numberFormatter.numberStyle = .decimal
+		numberFormatter.roundingMode = .halfUp
+		numberFormatter.maximumSignificantDigits = maximumSignificantDigits
+		if let USLocale = NSLocale().displayName(forKey: .countryCode, value: currentLocale) {
+			numberFormatter.locale = NSLocale(localeIdentifier: USLocale) as Locale
+		}
+	}
+	
+	func calculatePostfixExpression(postfix: [String]) throws -> Double {
+		var stack = Stack()
+		var popedValueFromStack: Double {
+			return Double(stack.pop() ?? zero) ?? .zero
+		}
+		
+		for element in postfix {
+			let elementType = AccessibleOperator.init(rawValue: element)
+			switch elementType {
+			case .plus:
+				let next = popedValueFromStack
+				let prev = popedValueFromStack
+				stack.push(element: String(prev + next))
+			case .minus:
+				let next = popedValueFromStack
+				let prev = popedValueFromStack
+				stack.push(element: String(prev - next))
+			case .multiply:
+				let next = popedValueFromStack
+				let prev = popedValueFromStack
+				stack.push(element: String(prev * next))
+			case .divide:
+				let next = popedValueFromStack
+				let prev = popedValueFromStack
+				if next == .zero {
+					throw ErrorCase.dividedByZero
+				} else {
+					stack.push(element: String(prev / next))
+				}
+			default:
+				stack.push(element: element)
+			}
+		}
+		
+		return popedValueFromStack
+	}
+	
+	func formatting(number: Double) -> String? {
+		return numberFormatter.string(from: NSNumber(value: number))
+	}
 }
