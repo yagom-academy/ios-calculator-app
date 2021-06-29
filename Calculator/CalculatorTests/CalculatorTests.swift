5539ad5e8e2992b64b0bd174afa843087b3a1f7a
KimWanki <seoulkwk95@gmail.com>
2021-06-29 14:37:44 +0900

    refactor: CalculatorUnitTest give-when-then pattern 적용

--- Calculator/CalculatorTests/CalculatorTests.swift
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -20,86 +20,115 @@ class CalculatorTests: XCTestCase {
     }
 
     func test_계산기에_1더하기2더하기3연산시_6이나온다() throws {
-        calculator.enterExpression(operation: "", inputNumber: "1")
-        calculator.enterExpression(operation: "+", inputNumber: "2")
-        calculator.enterExpression(operation: "+", inputNumber: "3")
-        calculator.enterExpression(operation: "=", inputNumber: "")
-    
-        let plusResult = try calculator.calculate()
-        XCTAssertTrue(plusResult == "6")
-        print(plusResult)
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        //give
+        var input = ["", "1", "+", "2", "+", "3", "=", ""]
+        
+        //when
+        while !input.isEmpty {
+            calculator.enterExpression(operation: input.removeFirst(), inputNumber: input.removeFirst())
+        }
+        let calculationResult = try calculator.calculate()
+        
+        //then
+        print(calculationResult)
+        let expectedResult = "6"
+        XCTAssertTrue(calculationResult == expectedResult)
     }
     
     func test_계산기에_1곱하기2곱하기3연산시_6이나온다() throws {
-        calculator.enterExpression(operation: "", inputNumber: "1")
-        calculator.enterExpression(operation: "×", inputNumber: "2")
-        calculator.enterExpression(operation: "×", inputNumber: "3")
-        calculator.enterExpression(operation: "=", inputNumber: "")
+        //give
+        var input = ["", "1", "×", "2", "×", "3", "=", ""]
         
-        let multiplyResult = try calculator.calculate()
-        XCTAssertTrue(multiplyResult == "6")
-        print(multiplyResult)
+        //when
+        while !input.isEmpty {
+            calculator.enterExpression(operation: input.removeFirst(), inputNumber: input.removeFirst())
+        }
+        let calculationResult = try calculator.calculate()
+
+        //then
+        print(calculationResult)
+        let expectedResult = "6"
+        XCTAssertTrue(calculationResult == expectedResult)
     }
     
     func test_계산기에_1빼기2빼기3연산시_마이너스4가나온다() throws {
-        calculator.enterExpression(operation: "", inputNumber: "1")
-        calculator.enterExpression(operation: "-", inputNumber: "2")
-        calculator.enterExpression(operation: "-", inputNumber: "3")
-        calculator.enterExpression(operation: "=", inputNumber: "")
+        //give
+        var input = ["", "1", "-", "2", "-", "3", "=", ""]
+                
+        //when
+        while !input.isEmpty {
+            calculator.enterExpression(operation: input.removeFirst(), inputNumber: input.removeFirst())
+        }
+        let calculationResult = try calculator.calculate()
         
-        let minusResult = try calculator.calculate()
-        XCTAssertTrue(minusResult == "-4")
-        print(minusResult)
+        //then
+        print(calculationResult)
+        let expectedResult = "-4"
+        XCTAssertTrue(calculationResult == expectedResult)
     }
     
     func test_계산기에_1나누기2연산시_0p5가나온다() throws {
-        calculator.enterExpression(operation: "", inputNumber: "1")
-        calculator.enterExpression(operation: "÷", inputNumber: "2")
-        calculator.enterExpression(operation: "=", inputNumber: "")
+        //give
+        var input = ["", "1", "÷", "2", "=", ""]
         
-        let divideResult = try calculator.calculate()
-        print(divideResult)
-        XCTAssertTrue(divideResult == "0.5")
+        //when
+        while !input.isEmpty {
+            calculator.enterExpression(operation: input.removeFirst(), inputNumber: input.removeFirst())
+        }
+        let calculationResult = try calculator.calculate()
+
+        //then
+        print(calculationResult)
+        let expectedResult = "0.5"
+        XCTAssertTrue(calculationResult == expectedResult)
     }
 
     func test_계산기에_1곱하기3더하기2나누기2연산시_4가나온다() throws {
-        calculator.enterExpression(operation: "", inputNumber: "1")
-        calculator.enterExpression(operation: "×", inputNumber: "3")
-        calculator.enterExpression(operation: "+", inputNumber: "2")
-        calculator.enterExpression(operation: "÷", inputNumber: "2")
-        calculator.enterExpression(operation: "=", inputNumber: "")
+        //give
+        var input = ["", "1", "×", "3", "+", "2", "÷", "2", "=", ""]
         
-        let complexResult = try calculator.calculate()
-        print(complexResult)
-        XCTAssertTrue(complexResult == "4")
+        //when
+        while !input.isEmpty {
+            calculator.enterExpression(operation: input.removeFirst(), inputNumber: input.removeFirst())
+        }
+        let calculationResult = try calculator.calculate()
+
+        //then
+        print(calculationResult)
+        let expectedResult = "4"
+        XCTAssertTrue(calculationResult == expectedResult)
     }
     
     func test_계산기에_1곱하기3더하기2나누기0연산시_NaN이나온다() throws {
-        calculator.enterExpression(operation: "", inputNumber: "1")
-        calculator.enterExpression(operation: "×", inputNumber: "3")
-        calculator.enterExpression(operation: "+", inputNumber: "2")
-        calculator.enterExpression(operation: "÷", inputNumber: "0")
-        calculator.enterExpression(operation: "=", inputNumber: "")
+        //give
+        var input = ["", "1", "×", "3", "+", "2", "÷", "0", "=", ""]
         
-        let complexResult = try calculator.calculate()
-        print(complexResult)
-        XCTAssertTrue(complexResult == "NaN")
+        //when
+        while !input.isEmpty {
+            calculator.enterExpression(operation: input.removeFirst(), inputNumber: input.removeFirst())
+        }
+        let calculationResult = try calculator.calculate()
+
+        //then
+        print(calculationResult)
+        let expectedResult = "NaN"
+        XCTAssertTrue(calculationResult == expectedResult)
     }
     
     func test_계산기에_10p1더하기12p5나누기125더하기8빼기100곱하기13p8연산시_마이너스1361p8이나온다() throws {
-        calculator.enterExpression(operation: "", inputNumber: "10.1")
-        calculator.enterExpression(operation: "+", inputNumber: "12.5")
-        calculator.enterExpression(operation: "÷", inputNumber: "125")
-        calculator.enterExpression(operation: "+", inputNumber: "8")
-        calculator.enterExpression(operation: "-", inputNumber: "100")
-        calculator.enterExpression(operation: "×", inputNumber: "13.8")
-        calculator.enterExpression(operation: "=", inputNumber: "")
+        //give
+        var input = ["", "10.1", "+", "12.5", "÷", "125", "+", "8", "-", "100", "×", "13.8", "=", ""]
+          
+        //when
+        while !input.isEmpty {
+            calculator.enterExpression(operation: input.removeFirst(), inputNumber: input.removeFirst())
+        }
+        let calculationResult = try calculator.calculate()
         
-        let complexResult = try calculator.calculate()
-        print(complexResult)
-        XCTAssertTrue(complexResult == "-1361.8")
+        //then
+        print(calculationResult)
+        let expectedResult = "-1361.8"
+        XCTAssertTrue(calculationResult == expectedResult)
     }
     
     func testPerformanceExample() throws {
@@ -108,5 +137,4 @@ class CalculatorTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
-
 }
