dbb6d719f7d1869ac70435be93e3caf64d827436
jsim27 <simjh27@gmail.com>
2021-06-29 16:18:58 +0900

    feat: 스토리보드 버튼 액션 연결

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -1,7 +1,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBAction func acDidTap(_ sender: UIButton) {
+    }
+    @IBAction func ceDidTap(_ sender: UIButton) {
+    }
+    @IBAction func switchSignDidTap(_ sender: UIButton) {
+    }
+    @IBAction func divideDidTap(_ sender: UIButton) {
+    }
+    @IBAction func multiplyDidTap(_ sender: UIButton) {
+    }
+    @IBAction func minusDidTap(_ sender: UIButton) {
+    }
+    @IBAction func plusDidTap(_ sender: UIButton) {
+    }
+    @IBAction func equalDidTap(_ sender: UIButton) {
+    }
+    @IBAction func dotDidTap(_ sender: UIButton) {
+    }
+    @IBAction func doubleZeroDidTap(_ sender: UIButton) {
+    }
+    
+    @IBAction func numberDidTap(_ sender: UIButton) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
