6dc0ee61b359bdac5ec8f867ad8363c5b0b4087f
jyoon54 <jiwonyoon@Jiwonui-MacBookPro.local>
2021-06-29 22:47:27 +0900

    feat: reset함수 추가, 점 버튼 구현, updateCurrentValueLabel 함수 구현

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -2,35 +2,49 @@ import UIKit
 
 class ViewController: UIViewController {
     private var currentValue = "0"
+    private var infixNotation = [String]()
     
+    @IBOutlet weak var historyStackView: UIStackView!
     @IBOutlet weak var currentValueLabel: UILabel!
+    @IBOutlet weak var currentOperatorLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        reset()
         updateCurrentValueLabel()
     }
     
+    private func reset() {
+        currentValueLabel.text = "0"
+        currentOperatorLabel.text = ""
+        infixNotation = [String]()
+        for history in historyStackView.arrangedSubviews {
+            history.removeFromSuperview()
+        }
+        
+    }
     private func updateCurrentValueLabel() {
         currentValueLabel.text = currentValue
     }
     
     @IBAction func acDidTap(_ sender: UIButton) {
+        
     }
     @IBAction func ceDidTap(_ sender: UIButton) {
     }
     @IBAction func switchSignDidTap(_ sender: UIButton) {
     }
-    @IBAction func divideDidTap(_ sender: UIButton) {
-    }
-    @IBAction func multiplyDidTap(_ sender: UIButton) {
-    }
-    @IBAction func minusDidTap(_ sender: UIButton) {
-    }
-    @IBAction func plusDidTap(_ sender: UIButton) {
+    @IBAction func operatorDidTap(_ sender: UIButton) {
+        //×÷
+        
+        
     }
     @IBAction func equalDidTap(_ sender: UIButton) {
     }
     @IBAction func dotDidTap(_ sender: UIButton) {
+        guard !currentValue.contains(".") else { return }
+        currentValue += "."
+        updateCurrentValueLabel()
     }
     
     @IBAction func numberDidTap(_ sender: UIButton) {
@@ -40,7 +54,7 @@ class ViewController: UIViewController {
             guard input != "0" && input != "00" else { return }
                 currentValue = input
         } else {
-            currentValue = currentValue + input
+            currentValue += input
         }
         updateCurrentValueLabel()
     }
