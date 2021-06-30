dd0291f04976c62656c134faf64c77af0b5f2d9c
Ldoy <leedoy7989@gmail.com>
2021-06-30 15:17:47 +0900

    test: 테스트 코드 추가 및 이에 따른 Calculator의 calculate 기능 수정

--- Calculator/Calculator/Model/Calculator.swift
+++ Calculator/Calculator/Model/Calculator.swift
@@ -3,45 +3,45 @@ import Foundation
 let zero = "0"
 
 enum ErrorCase: Error {
-    case unknownInputCase
-    case dividedByZero
+	case unknownInputCase
+	case dividedByZero
 }
 
 enum AccessibleOperator: String {
-    case plus = "+", minus = "-", multiply = "*", divide = "/"
+	case plus = "+", minus = "-", multiply = "*", divide = "/"
 }
 
 enum CalculatorComponent {
-    case number
-    case `operator`
-    case equalSign
-    case allClear
-    case clearEntry
-    case signConverter
-    case dot
-    
-    static func convertToComponentType(from userInput: String) throws -> Self {
-        switch userInput {
-        case "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "00", "." :
-            return .number
-        case "/", "*", "+", "-" :
-            return .operator
-        case "=" :
-            return .equalSign
-        case "AC" :
-            return .allClear
-        case "CE" :
-            return .clearEntry
-        case "⁺⁄₋" :
-            return .signConverter
-        default:
-            throw ErrorCase.unknownInputCase
-        }
-    }
+	case number
+	case `operator`
+	case equalSign
+	case allClear
+	case clearEntry
+	case signConverter
+	case dot
+	
+	static func convertToComponentType(from userInput: String) throws -> Self {
+		switch userInput {
+		case "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "00", "." :
+			return .number
+		case "/", "*", "+", "-" :
+			return .operator
+		case "=" :
+			return .equalSign
+		case "AC" :
+			return .allClear
+		case "CE" :
+			return .clearEntry
+		case "⁺⁄₋" :
+			return .signConverter
+		default:
+			throw ErrorCase.unknownInputCase
+		}
+	}
 }
 
 protocol Calculatorable {
-	func calculate(input: [String]) -> String?
+	func calculate(input: [String]) -> String
 }
 
 protocol CalculatorablePostfix {
@@ -50,77 +50,77 @@ protocol CalculatorablePostfix {
 }
 
 extension CalculatorablePostfix {
-    private func checkPriority(A a: String, isLowerThenB b: String) -> Bool {
-        if ["+", "-"].contains(a), ["*", "/"].contains(b) {
-            return true
-        }
-        return false
-    }
-    
-    func makeInfixExpression(from: [String]) throws -> [String] {
-        var infixExpression = [String]()
-        var infixNumberString = ""
-        var converterCount = 0
-        
-        for userInput in from {
-            let userInputType = try? CalculatorComponent.convertToComponentType(from: userInput)
-            
-            switch userInputType {
-            case .number:
-                infixNumberString += userInput
-            case .operator:
-                converterCount = 0
-                infixExpression.append(infixNumberString)
-                infixExpression.append(userInput)
-                infixNumberString = ""
-            case .signConverter:
-                converterCount += 1
-                if converterCount % 2 == 1 {
-                    infixNumberString = AccessibleOperator.minus.rawValue + infixNumberString
-                } else {
-                    infixNumberString.remove(at: infixNumberString.startIndex)
-                }
-            default:
-                throw ErrorCase.unknownInputCase
-            }
-        }
-        
-        infixExpression.append(infixNumberString)
-        
-        return infixExpression
-    }
-    
-    func convertToPostfixExpression(fromInfix input: [String]) -> [String] {
-        var postfix = [String]()
-        var stack = Stack()
-        
-        for currentElement in input {
-            if let isNumber = Double(currentElement) {
-                postfix.append(currentElement)
-            } else {
-                if let lastElement = stack.pop() {
-                    if checkPriority(A: lastElement, isLowerThenB: currentElement) {
-                        stack.push(element: lastElement)
-                        stack.push(element: currentElement)
-                    } else {
-                        postfix.append(lastElement)
-                        stack.push(element: currentElement)
-                    }
-                    
-                } else {
-                    stack.push(element: currentElement)
-                }
-            }
-        }
-        
-        while stack.peek() != nil {
-            if let element = stack.pop() {
-                postfix.append(element)
-            }
-        }
-        
-        return postfix
-    }
+	private func checkPriority(A a: String, isLowerThenB b: String) -> Bool {
+		if ["+", "-"].contains(a), ["*", "/"].contains(b) {
+			return true
+		}
+		return false
+	}
+	
+	func makeInfixExpression(from: [String]) throws -> [String] {
+		var infixExpression = [String]()
+		var infixNumberString = ""
+		var converterCount = 0
+		
+		for userInput in from {
+			let userInputType = try? CalculatorComponent.convertToComponentType(from: userInput)
+			
+			switch userInputType {
+			case .number:
+				infixNumberString += userInput
+			case .operator:
+				converterCount = 0
+				infixExpression.append(infixNumberString)
+				infixExpression.append(userInput)
+				infixNumberString = ""
+			case .signConverter:
+				converterCount += 1
+				if converterCount % 2 == 1 {
+					infixNumberString = AccessibleOperator.minus.rawValue + infixNumberString
+				} else {
+					infixNumberString.remove(at: infixNumberString.startIndex)
+				}
+			default:
+				throw ErrorCase.unknownInputCase
+			}
+		}
+		
+		infixExpression.append(infixNumberString)
+		
+		return infixExpression
+	}
+	
+	func convertToPostfixExpression(fromInfix input: [String]) -> [String] {
+		var postfix = [String]()
+		var stack = Stack()
+		
+		for currentElement in input {
+			if let isNumber = Double(currentElement) {
+				postfix.append(currentElement)
+			} else {
+				if let lastElement = stack.pop() {
+					if checkPriority(A: lastElement, isLowerThenB: currentElement) {
+						stack.push(element: lastElement)
+						stack.push(element: currentElement)
+					} else {
+						postfix.append(lastElement)
+						stack.push(element: currentElement)
+					}
+					
+				} else {
+					stack.push(element: currentElement)
+				}
+			}
+		}
+		
+		while stack.peek() != nil {
+			if let element = stack.pop() {
+				postfix.append(element)
+			}
+		}
+		
+		return postfix
+	}
 	
 	func calculatePostfixExpression(postfix: [String]) throws -> Double {
 		var stack = Stack()
@@ -180,7 +180,7 @@ struct Calculator: Calculatorable, CalculatorablePostfix {
 	}
 	
 	func castToIntFrom(double: Double) -> Int? {
-		if double.truncatingRemainder(dividingBy: 1) == 0 {
+		if double.truncatingRemainder(dividingBy: 1) == .zero {
 			return Int(double)
 		} else {
 			return nil
@@ -197,15 +197,17 @@ struct Calculator: Calculatorable, CalculatorablePostfix {
 		}
 	}
 	
-	func calculate(input: [String]) -> String? {
+	func calculate(input: [String]) -> String {
+		let notANumber = "NaN"
+		
 		guard let infix = try? makeInfixExpression(from: input) else {
-			return nil
+			return notANumber
 		}
 		
 		let postfix = convertToPostfixExpression(fromInfix: infix)
 		
 		guard let calculatedValue = try? calculatePostfixExpression(postfix: postfix) else {
-			return "NaN"
+			return notANumber
 		}
 		
 		return castToStringFrom(number: calculatedValue)
--- Calculator/CalculatorTests/CalculatorTests.swift
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -12,14 +12,46 @@ class CalculatorTests: XCTestCase {
 
 	let sut: Calculatorable = Calculator()
 	
-	func test_test() {
-		XCTAssertEqual(5, Int(5.7))
-	}
-	
 	func test_1더하기2더하기3등호를입력하면_6이나온다() {
 		let input = ["1", "+", "2", "+", "3"]
 		let result = sut.calculate(input: input)
-		XCTAssertEqual(result!, "6")
+		XCTAssertEqual(result, "6")
 	}
 
+	func test_1곱하기2곱하기3등호를입력하면_6이나온다() {
+		let input = ["1", "*", "2", "*", "3"]
+		let result = sut.calculate(input: input)
+		XCTAssertEqual(result, "6")
+	}
+
+	func test_1빼기2빼기3등호를입력하면_마이너스4가나온다() {
+		let input = ["1", "-", "2", "-", "3"]
+		let result = sut.calculate(input: input)
+		XCTAssertEqual(result, "-4")
+	}
+
+	func test_1나누기2등호를입력하면_영점오가나온다() {
+		let input = ["1", "/", "2"]
+		let result = sut.calculate(input: input)
+		XCTAssertEqual(result, "0.5")
+	}
+
+	func test_1곱하기3더하기2나누기2등호를입력하면_4가나온다() {
+		let input = ["1", "*", "3", "+","2", "/", "2"]
+		let result = sut.calculate(input: input)
+		XCTAssertEqual(result, "4")
+	}
+
+	func test_1곱하기3더하기2나누기0등호를입력하면_NaN이나온다() {
+		let input = ["1", "*", "3", "+","2", "/", "0"]
+		let result = sut.calculate(input: input)
+		XCTAssertEqual(result, "NaN")
+	}
+
+//	func test_10점1더하기12점5나누기125더하기8빼기100곱하기13점8등호를입력하면_마이너스1361점8이나온다() {
+//		let input = ["1", "0", ".", "1", "+", "1", "2", ".", "5", "/", "1", "2", "5", "+", "8", "-", "1", "0", "0", "*", "1", "3", ".", "8"]
+//		let result = sut.calculate(input: input)
+//		XCTAssertEqual(result, "-1361.8")
+//	}
+	
 }
