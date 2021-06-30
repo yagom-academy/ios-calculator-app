c123f4920063e140ec6447814b83f01959401cd7
YongHoonJJo <starctak@gmail.com>
2021-06-30 20:10:24 +0900

    feat: 연산자 및 피연산자 버튼 IBAction 에 연결
    - touchUpOperandButton(_:) : 입력한 숫자를 보여주기 위한 inputNotation 에 값을 이어주는 기능 우선 적용
    - touchUpOperatorButton(_) : inputNotation 저장된 피연산자와 현재 입력된 연산자를 notation 배열에 담는 기능 적용
    - touchUpDotButton(_:) : 점 버튼 연결까지만 진행.

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -8,11 +8,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var notation: [String] = []
+    var inputNotation: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        notation = []
     }
 
-
+    @IBAction func touchUpOperandButton(_ sender: UIButton) {
+        guard let operandButtonNumber = sender.titleLabel?.text else {
+            return
+        }
+        
+        inputNotation = "\(inputNotation)\(operandButtonNumber)"
+    }
+    
+    @IBAction func touchUpOperatorButton(_ sender: UIButton) {
+        guard let operatorCase = sender.titleLabel?.text else {
+            return
+        }
+        notation.append(inputNotation)
+        notation.append(operatorCase)
+        inputNotation = ""
+    }
+    
+    @IBAction func touchUpDotButton(_ sender: UIButton) {
+        
+    }
 }
 
