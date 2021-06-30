07faac8a76ae0aea71b41f7a7f323b8678edb65d
YongwooKim <keepingitflow@gmail.com>
2021-06-30 10:23:45 +0900

    feat : 짝코딩_1  버튼 추가 및 스위치문 작성
    버튼 아울렛 추가, IBAction 추가

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -7,8 +7,51 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var btnZero: UIButton!
+    @IBOutlet weak var btnOne: UIButton!
+    @IBOutlet weak var btnTwo: UIButton!
+    @IBOutlet weak var btnThree: UIButton!
+    @IBOutlet weak var btnFour: UIButton!
+    @IBOutlet weak var btnFive: UIButton!
+    @IBOutlet weak var btnSix: UIButton!
+    @IBOutlet weak var btnSeven: UIButton!
+    @IBOutlet weak var btnEigth: UIButton!
+    @IBOutlet weak var btnNine: UIButton!
+    @IBOutlet weak var btnDoubleZero: UIButton!
+    @IBOutlet weak var mainStackView: UILabel!
+    
+    @IBOutlet weak var btnDot: UIButton!
+    let calculate = Calculator()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func pushNumberButton(_ sender: UIButton) {
+        switch sender {
+        case btnDoubleZero:
+            mainStackView.text! += "00"
+            calculate.pushNumberOrOperator(Operand(value: Double("00")))
+        case btnZero:
+            
+        case btnOne:
+            
+        case btnTwo:
+            
+        case btnThree:
+            
+        case btnFour:
+            
+        case btnFive:
+            
+        case btnSix:
+            
+        case btnSeven:
+            
+        case btnNine:
+            
+        default:
+            <#code#>
+        }
     }
 }
