fceb459a071ff49cc1e5008431fa39826d8951b4
Ellen <80380535+jcrescent61@users.noreply.github.com>
2021-06-30 19:30:10 +0900

    feat: Test 파일 생성

---
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -0,0 +1,16 @@
+//
+//  CalculatorTests.swift
+//  CalculatorTests
+//
+//  Created by Ellen on 2021/06/30.
+//
+
+import XCTest
+
+@testable import Calculator
+
+class CalculatorTests: XCTestCase {
+
+}
+
+
