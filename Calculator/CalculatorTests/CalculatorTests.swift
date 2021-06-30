4c7367fa9622b179f35f8e1d8877ba26e146d3ad
YongHoonJJo <starctak@gmail.com>
2021-06-30 14:42:15 +0900

    refactor: CalculatorTest 내의 테스트 함수 리뷰 반영
    - 성공/실패 케이스 분리
    - 함수 이름 given/when/then 패턴에 맞게 네이밍 수정

--- Calculator/CalculatorTests/CalculatorTests.swift
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -11,55 +11,87 @@ import Foundation
 
 class CalculatorTests: XCTestCase {
     
-    let sut: Calculator = Calculator()
+    var sut: Calculator = Calculator()
     
-    override func setUpWithError() throws {
+    override func setUp() {
+        super.setUp()
+        sut = Calculator()
         
     }
     
-    func test_덧셈_연산은_잘되는가()  {
+    func test_1더하기2더하기3이_준비되어서_runCalculator메서드가_호출되었을때_6이_된다()  {
         // given
         let testcase = ["1","+","2","+","3"]
         
         // when
         let result = sut.runCalculator(on: testcase)
         let expectedResult: Double = 6
-        let failedResult: Double = 5
         
         // then
         XCTAssertEqual(result, .success(expectedResult))
+    }
+    
+    func test_1더하기2더하기3이_준비되어서_runCalculator메서드가_호출되었을때_5는_아니다()  {
+        // given
+        let testcase = ["1","+","2","+","3"]
+        
+        // when
+        let result = sut.runCalculator(on: testcase)
+        let failedResult: Double = 5
+        
+        // then
         XCTAssertNotEqual(result, .success(failedResult))
     }
     
-    func test_곱셈_연산은_잘되는가() {
+    func test_1곱하기2곱하기3이_준비되어서_runCalculator메서드가_호출되었을때_6이_된다() {
         // given
         let testcase = ["1","*","2","*","3"]
         
         // when
         let result = sut.runCalculator(on: testcase)
         let expectedResult: Double = 6
-        let failedResult: Double = 5
         
         // then
         XCTAssertEqual(result, .success(expectedResult))
+    }
+    
+    func test_1곱하기2곱하기3이_준비되어서_runCalculator메서드가_호출되었을때_5는_아니다() {
+        // given
+        let testcase = ["1","*","2","*","3"]
+        
+        // when
+        let result = sut.runCalculator(on: testcase)
+        let failedResult: Double = 5
+        
+        // then
         XCTAssertNotEqual(result, .success(failedResult))
     }
 
-    func test_뺄셈_연산은_잘되는가() {
+    func test_1빼기2빼기3이_준비되어서_runCalculator메서드가_호출되었을때_마이너스4가_된다() {
         // given
         let testcase = ["1","-","2","-","3"]
         
         // when
         let result = sut.runCalculator(on: testcase)
         let expectedResult: Double = -4
-        let failedResult: Double = 5
         
         // then
         XCTAssertEqual(result, .success(expectedResult))
+    }
+    
+    func test_1빼기2빼기3이_준비되어서_runCalculator메서드가_호출되었을때_5는_아니다() {
+        // given
+        let testcase = ["1","-","2","-","3"]
+        
+        // when
+        let result = sut.runCalculator(on: testcase)
+        let failedResult: Double = 5
+        
+        // then
         XCTAssertNotEqual(result, .success(failedResult))
     }
 
-    func test_나눗셈_연산은_잘되는가() {
+    func test_1나누기2나누기3이_준비되어서_runCalculator메서드가_호출되었을때_1점6667이_된다() {
         // given
         let testcase = ["1","/","2","/","3"]
         
@@ -72,53 +104,99 @@ class CalculatorTests: XCTestCase {
         
         let rounedResult = round(unwrappedResult*100000)/100000
         let expectedResult: Double = 0.16667
-        let failedResult: Double = 0.16666
         
         // then
         XCTAssertEqual(rounedResult, expectedResult)
+    }
+    
+    func test_1나누기2나누기3이_준비되어서_runCalculator메서드가_호출되었을때_1점6666은_아니다() {
+        // given
+        let testcase = ["1","/","2","/","3"]
+        
+        // when
+        let result = sut.runCalculator(on: testcase)
+        
+        guard case .success(let unwrappedResult) = result else {
+            return
+        }
+        
+        let rounedResult = round(unwrappedResult*100000)/100000
+        let failedResult: Double = 0.16666
+        
+        // then
         XCTAssertNotEqual(rounedResult, failedResult)
     }
 
-    func test_사칙연산은_잘되는가() {
+    func test_1곱하기3더하기2나누기2가_준비되어서_runCalculator메서드가_호출되었을때_4가_된다() {
         // given
         let testcase = ["1","*","3","+","2","/","2"]
         
         // when
         let result = sut.runCalculator(on: testcase)
         let expectedResult: Double = 4
-        let failedResult: Double = 5
         
         // then
         XCTAssertEqual(result, .success(expectedResult))
+    }
+
+    func test_1곱하기3더하기2나누기2가_준비되어서_runCalculator메서드가_호출되었을때_5는_아니다() {
+        // given
+        let testcase = ["1","*","3","+","2","/","2"]
+        
+        // when
+        let result = sut.runCalculator(on: testcase)
+        let failedResult: Double = 5
+        
+        // then
         XCTAssertNotEqual(result, .success(failedResult))
     }
 
-    func test_0으로_나누었을때_에러가_발생하는가() {
+    
+    func test_1곱하기3더하기2나누기0가_준비되어서_runCalculator메서드가_호출되었을때_dividedByZero에러가_발생한다() {
         // given
         let testcase = ["1","*","3","+","2","/","0"]
         
         // when
         let result = sut.runCalculator(on: testcase)
         let expectedResult = CalculatorError.dividedByZero
-        let failedResult = CalculatorError.unknownError
         
         // then
         XCTAssertEqual(result, .failure(expectedResult))
-        XCTAssertNotEqual(result, .failure(failedResult))
+    }
+    
+    func test_1곱하기3더하기2나누기0가_준비되어서_runCalculator메서드가_호출되었을때_unknownError에러는_아니다() {
+        // given
+        let testcase = ["1","*","3","+","2","/","0"]
         
+        // when
+        let result = sut.runCalculator(on: testcase)
+        let failedResult = CalculatorError.unknownError
+        
+        // then
+        XCTAssertNotEqual(result, .failure(failedResult))
     }
 
-    func test_소수점_사칙연산은_잘되는가() {
+    func test_10점1더하기12점5나누기125더하기8빼기100곱하기13점8이_준비되어서_runCalculator메서드가_호출되었을때_마이너스1361점8이_된다() {
         // given
         let testcase = ["10.1","+","12.5","/","125","+","8","-","100","*","13.8"]
         
         // when
         let result = sut.runCalculator(on: testcase)
         let expectedResult: Double = -1361.8
-        let failedResult: Double = -1361.9
         
         // then
         XCTAssertEqual(result, .success(expectedResult))
+    }
+    
+    func test_10점1더하기12점5나누기125더하기8빼기100곱하기13점8이_준비되어서_runCalculator메서드가_호출되었을때_마이너스1361점9는_아니다() {
+        // given
+        let testcase = ["10.1","+","12.5","/","125","+","8","-","100","*","13.8"]
+        
+        // when
+        let result = sut.runCalculator(on: testcase)
+        let failedResult: Double = -1361.9
+        
+        // then
         XCTAssertNotEqual(result, .success(failedResult))
     }
 }
