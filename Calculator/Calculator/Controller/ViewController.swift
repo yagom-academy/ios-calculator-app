dab7e17541db3779076ccd56ad58c9a98c02f32b
ictechgy <ictechgy@gmail.com>
2021-07-01 18:48:49 +0900

    feat: Connect buttons and input value display labels among UI elements to ViewController

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -7,12 +7,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet private weak var operandInputLabel: UILabel!
+    @IBOutlet weak var operatorInputLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+}
 
-
+extension ViewController {
+    @IBAction func didTapOperandButton(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func didTapOperatorButton(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func didTapCalculateButton(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func didTapClearButton(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func didTapChangeSignButton(_ sender: UIButton) {
+        
+    }
 }
 
