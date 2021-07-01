a58d6304724b0f467bae5a2e8473c33a2ec6390f
taelee42 <ritmakid@gmail.com>
2021-07-01 16:46:49 +0900

    refactor: currentValue를 옵셔널로 바꾸고 연관된 동작들 변경

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-    private var currentValue = "0"
+    private var currentValue: String?
     private var infixNotation = [String]()
     
     @IBOutlet weak var historyStackView: UIStackView!
@@ -11,58 +11,76 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         reset()
-        updateCurrentValueLabel()
     }
     
     private func reset() {
-        currentValue = "0"
-        currentValueLabel.text = "0"
+        currentValue = nil
+        updateCurrentValueLabel()
         currentOperatorLabel.text = ""
         infixNotation = [String]()
         for history in historyStackView.arrangedSubviews {
             history.removeFromSuperview()
         }
-        
     }
+    
     private func updateCurrentValueLabel() {
+        guard let currentValue = currentValue else {
+            currentValueLabel.text = "0"
+            return
+        }
         currentValueLabel.text = currentValue
     }
     
     @IBAction func acDidTap(_ sender: UIButton) {
         reset()
     }
+    
     @IBAction func ceDidTap(_ sender: UIButton) {
         currentValue = "0"
         currentValueLabel.text = "0"
     }
+    
     @IBAction func switchSignDidTap(_ sender: UIButton) {
-        guard currentValue != "0" else { return }
+        guard let currentValue = currentValue, currentValue != "0" else { return }
         if let firstCharacter = currentValue.first, firstCharacter == "-" {
-            currentValue.removeFirst()
+            self.currentValue?.removeFirst()
         } else {
-            currentValue = "-" + currentValue
+            self.currentValue = "-" + currentValue
         }
         updateCurrentValueLabel()
     }
+    
     @IBAction func operatorDidTap(_ sender: UIButton) {
         //×÷
     }
+    
     @IBAction func equalDidTap(_ sender: UIButton) {
     }
+    
     @IBAction func dotDidTap(_ sender: UIButton) {
-        guard !currentValue.contains(".") else { return }
-        currentValue += "."
+        guard let currentValue = currentValue, !currentValue.contains(".") else { return }
+        self.currentValue = currentValue + "."
         updateCurrentValueLabel()
     }
     
     @IBAction func numberDidTap(_ sender: UIButton) {
         guard let input = sender.titleLabel?.text else { return }
-        
+        guard let currentValue = currentValue else {
+            self.currentValue = input
+            if input == "00" {
+                self.currentValue = "0"
+            }
+            updateCurrentValueLabel()
+            return
+        }
         if currentValue == "0" {
-            guard input != "0" && input != "00" else { return }
-                currentValue = input
+            if input == "00" {
+                self.currentValue = "0"
+            } else {
+                self.currentValue = input
+            }
         } else {
-            currentValue += input
+            self.currentValue = currentValue + input
         }
         updateCurrentValueLabel()
     }
