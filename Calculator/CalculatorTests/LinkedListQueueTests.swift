397b942b5e8214640e8e8a168e85bf308146b5cf
Ari <lee_ari95@icloud.com>
2021-11-08 21:13:07 +0900

    test: LinkedListQueue 생성 후 요소가 한개일 때 case 추가 #1

---
+++ Calculator/CalculatorTests/LinkedListQueueTests.swift
@@ -0,0 +1,53 @@
+//
+//  LinkedListQueueTests.swift
+//  CalculatorTests
+//
+//  Created by 이아리 on 2021/11/08.
+//
+
+import XCTest
+
+struct LinkedListQueue<T> {
+    private var list = LinkedList<T>()
+    
+    var count: Int {
+        return list.count
+    }
+    
+    var isEmpty: Bool {
+        return list.isEmpty
+    }
+    
+    var front: T? {
+        return list.first?.value
+    }
+    
+    func enqueue(_ element: T) {
+        list.append(element)
+    }
+    
+    func dequeue() -> T? {
+        if list.isEmpty { return nil }
+        return list.remove(at: 0)
+    }
+    
+}
+
+class LinkedListQueueTests: XCTestCase {
+    
+    func test_요소가한개일때() {
+        let queue = LinkedListQueue<Int>()
+        
+        queue.enqueue(123)
+        XCTAssertFalse(queue.isEmpty)
+        XCTAssertEqual(queue.count, 1)
+        XCTAssertEqual(queue.front, 123)
+        
+        let result = queue.dequeue()
+        XCTAssertEqual(result, 123)
+        XCTAssertTrue(queue.isEmpty)
+        XCTAssertEqual(queue.count, 0)
+        XCTAssertEqual(queue.front, nil)
+    }
+    
+}
