807feed1f56cbd80e452c26b55670b22d685c2cb
horeng2 <huski.a.a@gmail.com>
2021-11-08 17:57:41 +0900

    feat: CalculatorItemQueue 클래스 생성 및 itemQueue 변수 생성

---
+++ Calculator/Calculator/Calculator.swift
@@ -0,0 +1,16 @@
+//
+//  Calculator.swift
+//  Calculator
+//
+//  Created by 서녕 on 2021/11/08.
+//
+
+import Foundation
+
+class CalculatorItemQueue<T> {
+    var itemQueue = [T]
+    
+    func putToQueue<T>(_ value: T) {
+        
+    }
+}
