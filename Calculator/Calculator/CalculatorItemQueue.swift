91c4008c8e64be6f07f4284dbb49fd07a4b6c9c6
aCafela-coffee <seym7894@gmail.com>
2021-11-08 18:35:20 +0900

    feat: CalculatorItemQueue 추가

---
+++ Calculator/Calculator/CalculatorItemQueue.swift
@@ -0,0 +1,25 @@
+import UIKit
+struct CalculatorItemQueue {
+    var array: [CalculateItem] = []
+    
+    mutating func enqueue(_ value: CalculateItem) {
+        array.append(value)
+    }
+    
+    mutating func dequeue() -> CalculateItem? {
+        if array.isEmpty {
+            return nil
+        }
+        return array.removeFirst()
+    }
+}
+
+protocol CalculateItem { }
+
+struct NumberItem: CalculateItem {
+    var value: Double
+}
+
+struct OperatorItem: CalculateItem {
+    var value: (Double, Double) -> Double
+}
