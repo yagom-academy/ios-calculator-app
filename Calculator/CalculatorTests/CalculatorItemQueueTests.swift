56e92508a1676be8c42d447674bd57d3f8dbeef4
Ari <lee_ari95@icloud.com>
2021-11-08 22:31:09 +0900

    refactor: CalculatorItemQueueTests의 테스트코드를 수정 #1
    - LinkedListQueueTests와 동일하도록 테스트코드를 전반적으로 수정

--- Calculator/CalculatorTests/CalculatorItemQueueTests.swift
+++ Calculator/CalculatorTests/CalculatorItemQueueTests.swift
@@ -9,77 +9,48 @@ import XCTest
 
 class CalculatorItemQueueTests: XCTestCase {
 
-    var sut: CalculatorItemQueue<String>!
-
-    override func setUpWithError() throws {
-        sut = CalculatorItemQueue<String>()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        sut = nil
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-    
-    func test_enqueue() {
-        let input = "37"
+    func test_요소가한개일때() {
+        var queue = CalculatorItemQueue<Int>()
         
-        sut.enqueue(input)
+        queue.enqueue(123)
+        XCTAssertFalse(queue.isEmpty)
+        XCTAssertEqual(queue.count, 1)
         
-        XCTAssertEqual(sut.inBox, ["37"])
+        let result = queue.dequeue()
+        XCTAssertEqual(result, 123)
+        XCTAssertTrue(queue.isEmpty)
+        XCTAssertEqual(queue.count, 0)
     }
     
-    func test_enqueue_여러개추가하기() {
-        sut.enqueue("6")
-        sut.enqueue("3.5")
-        sut.enqueue("10.8")
+    func test_요소가두개일때() {
+        var queue = CalculatorItemQueue<Int>()
         
-        XCTAssertEqual(sut.inBox, ["6","3.5","10.8"])
+        queue.enqueue(123)
+        queue.enqueue(456)
+        XCTAssertFalse(queue.isEmpty)
+        XCTAssertEqual(queue.count, 2)
+        
+        let result1 = queue.dequeue()
+        XCTAssertEqual(result1, 123)
+        XCTAssertFalse(queue.isEmpty)
+        XCTAssertEqual(queue.count, 1)
+        
+        let result2 = queue.dequeue()
+        XCTAssertEqual(result2, 456)
+        XCTAssertTrue(queue.isEmpty)
+        XCTAssertEqual(queue.count, 0)
     }
     
-    func test_dequeue_빈큐를dequeue를하면_nil이나온다() {
-        let result = sut.dequeue()
+    func test_요소가두개일때_clear호출() {
+        var queue = CalculatorItemQueue<Int>()
         
-        XCTAssertEqual(result, nil)
+        queue.enqueue(123)
+        queue.enqueue(456)
+        XCTAssertFalse(queue.isEmpty)
+        XCTAssertEqual(queue.count, 2)
+        
+        queue.clear()
+        XCTAssertTrue(queue.isEmpty)
+        XCTAssertEqual(queue.count, 0)
     }
-    
-    func test_dequeue_enqueue후dequeue하면_값이나온다() {
-        sut.enqueue("23")
-        let result = sut.dequeue()
-        
-        XCTAssertEqual(result, "23")
-    }
-    
-    func test_clear() {
-        sut.enqueue("45")
-        sut.enqueue("54")
-        sut.enqueue("2")
-        
-        sut.clear()
-        
-        XCTAssertEqual(true, sut.inBox.isEmpty && sut.outBox.isEmpty)
-    }
-    
-    func test_count() {
-        sut.enqueue("3")
-        sut.enqueue("6")
-        sut.enqueue("10.3")
-        
-        let result = sut.count
-        
-        XCTAssertEqual(result, 3)
-    }
-    
-    func test_count가_0을반환하는지확인() {
-        let result = sut.count
-        
-        XCTAssertEqual(result, 0)
-    }
-    
-    func test_isEmpty() {
-        XCTAssertTrue(sut.isEmpty)
-        sut.enqueue("2")
-        XCTAssertFalse(sut.isEmpty)
-    }
-
 }
