9759486bf334d2d70c138ed0af0cd4e86fe969c3
horeng2 <huski.a.a@gmail.com>
2021-11-09 02:47:09 +0900

    chore: 값이 변경되지 않을 변수를 상수로 변경

--- Calculator/CalculatorTests/CalculatorTests.swift
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -17,20 +17,20 @@ class CalculatorTests: XCTestCase {
     }
 
     func test_1만_있는_큐에_get을_하면_빈_큐가_된다() {
-        var queue = CalculatorItemQueue<Int>(head: Node(value: 1))
+        let queue = CalculatorItemQueue<Int>(head: Node(value: 1))
         queue.getFromQueue()
         XCTAssertNil(queue.head?.value)
     }
 
     func test_1과_2가_있는_큐에_get을_하면_2가_남는다() {
-        var queue = CalculatorItemQueue<Int>(head: Node(value: 1, pointer: Node(value: 2)))
+        let queue = CalculatorItemQueue<Int>(head: Node(value: 1, pointer: Node(value: 2)))
         queue.getFromQueue()
         XCTAssertEqual(queue.head?.value, Optional(2))
         XCTAssertNil(queue.head?.pointer)
     }
 
     func test_비어있는_큐에는_getFromQueue_함수를_쓰더라도_아무변화가_없다() {
-        var queue = CalculatorItemQueue<Int>(head: nil)
+        let queue = CalculatorItemQueue<Int>(head: nil)
         queue.getFromQueue()
         XCTAssertNil(queue.head?.value)
     }
