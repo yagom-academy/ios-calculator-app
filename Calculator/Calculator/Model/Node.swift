f5008bc723d960fa486d6b9849750220ca301a5e
hoBahk <qudgh849@gmail.com>
2021-11-09 00:14:17 +0900

    feat: 노드 구현

--- Calculator/Calculator/Model/Node.swift
+++ Calculator/Calculator/Model/Node.swift
@@ -8,5 +8,11 @@
 import Foundation
 
 class Node<T> {
+    var data: T?
+    var next: Node?
     
+    init(data: T?, next: Node? = nil) {
+        self.data = data
+        self.next = next
+    }
 }
