b0722f849115be9a542da62078e1cd7ce2151661
junbangg <wnstjr4620@naver.com>
2021-11-09 02:15:56 +0900

    feat: LinkedList 구조체와 Node 클래스 추가

---
+++ Calculator/Model/LinkedList.swift
@@ -0,0 +1,38 @@
+//
+//  LinkedList.swift
+//  Calculator
+//
+//  Created by Jun Bang on 2021/11/09.
+//
+
+import Foundation
+
+protocol CalculateItem {
+    
+}
+
+class Node {
+    var item: CalculateItem
+    var next: Node?
+    
+    init(item: CalculateItem) {
+        self.item = item
+    }
+}
+
+struct LinkedList {
+    private var head: Node?
+    
+    var isNotEmpty: Bool {
+        return head != nil
+    }
+
+    mutating func append(_ item: CalculateItem) {
+        let newNode = Node(item: item)
+        if let head = head {
+            head.next = newNode
+        } else {
+            head = newNode
+        }
+    }
+}
