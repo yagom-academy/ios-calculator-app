1f7263db6f6cc3a277aaa9603d2723ea8b324de9
namu <simjh27@gmail.com>
2021-11-08 22:46:35 +0900

    test: Unit Test 타깃 추가

---
+++ Calculator/CauculatorItemQueueTests/CauculatorItemQueueTests.swift
@@ -0,0 +1,20 @@
+//
+//  CauculatorItemQueueTests.swift
+//  CauculatorItemQueueTests
+//
+//  Created by Jae-hoon Sim on 2021/11/08.
+//
+
+import XCTest
+
+class CauculatorItemQueueTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+}
