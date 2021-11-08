074d12e52a8abe17a3d933479bc35c9119cc7eed
yim2627 <yim2627@gmail.com>
2021-11-09 00:37:16 +0900

    refactor: 기존 unitTest 삭제후 새로 생성

---
+++ Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
@@ -0,0 +1,16 @@
+//
+//  CalculatorItemQueueTests.swift
+//  CalculatorItemQueueTests
+//
+//  Created by 임지성 on 2021/11/09.
+//
+
+import XCTest
+
+class CalculatorItemQueueTests: XCTestCase {
+    let queue = CalculatorItemQueue()
+    
+
+
+
+}
