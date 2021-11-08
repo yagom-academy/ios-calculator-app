06cb3e441a2e1e220fb48fe40547310cd42938c6
hwangjeha <hyhpwang@gmail.com>
2021-11-08 23:31:21 +0900

    feat: CalculatorItemQueueTests 유닛 테스트 파일 생성

---
+++ Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
@@ -0,0 +1,20 @@
+//
+//  CalculatorItemQueueTests.swift
+//  CalculatorItemQueueTests
+//
+//  Created by 황제하 on 2021/11/08.
+//
+
+import XCTest
+
+class CalculatorItemQueueTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        
+    }
+
+    override func tearDownWithError() throws {
+        
+    }
+    
+}
