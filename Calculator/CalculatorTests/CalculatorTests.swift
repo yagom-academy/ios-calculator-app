69ec080b35501db8564ecc960fb733e96f433b61
zoe <kny0651@naver.com>
2021-11-09 11:15:34 +0900

    test: dequeue메서드를 위한 테스트케이스 작성

--- Calculator/CalculatorTests/CalculatorTests.swift
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -17,4 +17,27 @@ class CalculatorTests: XCTestCase {
         let result = calculateItemQueue.items.last
         XCTAssertEqual(result, 11)
     }
+    
+    //MARK: - dequeue
+    func test_빈배열일때_dequeue메서드를호출하면_nil을반환하는지() {
+        var calculateItemQueue = CalculateItemQueue<Int>()
+        let result = calculateItemQueue.dequeue()
+        XCTAssertNil(result)
+    }
+    
+    func test_배열에_1_2_3이있을때_dequeue메서드를호출하면_1을반환하는지() {
+        var calculateItemQueue = CalculateItemQueue(items: [1, 2, 3])
+        if let output = calculateItemQueue.dequeue() {
+            XCTAssertEqual(output, 1)
+        }
+    }
+    
+    func test_배열에_enqueue메서드로_1_2_3을넣고_dequeue메서드를호출했을때_1을반환하는지() {
+        var calculateItemQueue = CalculateItemQueue<Int>()
+        calculateItemQueue.enqueue(element: 1)
+        calculateItemQueue.enqueue(element: 2)
+        calculateItemQueue.enqueue(element: 3)
+        let output = calculateItemQueue.dequeue()
+        XCTAssertEqual(output, 1)
+    }
 }
