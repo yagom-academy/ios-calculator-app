2d1098d6e2d645df46092635eddd880c7b52a54c
just1103 <hyojuson1103@gmail.com>
2021-11-09 17:50:32 +0900

    feat: removeFirst 함수 test case 추가 #1

--- Calculator/CalculatorTests/CalculatorTests.swift
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -12,4 +12,25 @@ class CalculatorTests: XCTestCase {
         queue.append(value: 2)
         XCTAssertEqual(queue.scanAllValue(), [1, 2])
     }
+    
+    func test_1이_있는_큐를_removeFirst하면_빈_큐가되고_제거한값을_반환한다() {
+        let queue = CalculatorItemQueue<Int>(head: Node(value: 1))
+        let removedValue = queue.removeFirst()
+        XCTAssertTrue(queue.scanAllValue().isEmpty)
+        XCTAssertEqual(removedValue, 1)
+    }
+
+    func test_1과2가_있는_큐를_removeFirst하면_2가_남고_제거한값1을_반환한다() {
+        let queue = CalculatorItemQueue<Int>(head: Node(value: 1, next: Node(value: 2)))
+        let removedValue = queue.removeFirst()
+        XCTAssertEqual(queue.scanAllValue(), [2])
+        XCTAssertEqual(removedValue, 1)
+    }
+    
+    func test_1과2와3이_있는_큐를_removeFirst하면_2와3이_남고_제거한값1을_반환한다() {
+        let queue = CalculatorItemQueue<Int>(head: Node(value: 1, next: Node(value: 2, next: Node(value: 3))))
+        let removedValue = queue.removeFirst()
+        XCTAssertEqual(queue.scanAllValue(), [2, 3])
+        XCTAssertEqual(removedValue, 1)
+    }
 }
