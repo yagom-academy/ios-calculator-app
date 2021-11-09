cbd0472a7deeb92bb67d04836a34441df96a577c
Jager-yoo <y73447jh@gmail.com>
2021-11-09 19:12:18 +0900

    feat: 사칙연산자 열거형 MathOperator 별도 파일로 생성 #2

---
+++ Calculator/Calculator/Model/MathOperator.swift
@@ -0,0 +1,15 @@
+//
+//  MathOperator.swift
+//  Calculator
+//
+//  Created by 예거 on 2021/11/09.
+//
+
+import Foundation
+
+enum MathOperator {
+    case add
+    case subtract
+    case divide
+    case multiply
+}
