d7c9f290b8cd0838953054790767a317c2ca0ef6
forestjae <forestsj30@gmail.com>
2021-11-09 20:46:45 +0900

    test: CalculatorItemQueueTests 유닛 테스트 추가

---
+++ Calculator/CalculatorTests/CalculatorItemQueueTests.swift
@@ -0,0 +1,21 @@
+//
+//  CalculatorItemQueueTests.swift
+//  CalculatorTests
+//
+//  Created by 이승재 on 2021/11/09.
+//
+
+import XCTest
+
+class CalculatorItemQueueTests: XCTestCase {
+
+    
+    func test_enqueueAndDequeue() {
+        
+    }
+    
+    func test_enqueueAndRemoveLast() {
+        
+    }
+    
+}
