cbd72ad2aa9b0b08746191cc72de4de371a587bc
forestjae <forestsj30@gmail.com>
2021-11-09 17:37:23 +0900

    feat: 리스트 자료구조형 직접 구현을 위한 Node 클래스 추가

---
+++ Calculator/Calculator/Model/List.swift
@@ -0,0 +1,18 @@
+//
+//  List.swift
+//  Calculator
+//
+//  Created by 이승재 on 2021/11/09.
+//
+
+import Foundation
+
+class Node<T> {
+    var data: T?
+    var next: Node?
+    
+    init(data: T?, next: Node? = nil) {
+        self.data = data
+        self.next = next
+    }
+}
