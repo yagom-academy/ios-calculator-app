78de589a94a9256ce4a3cc16470a9348b7967d2b
yanghojoon <yanghojoon8487@gmail.com>
2021-11-09 15:57:32 +0900

    refactor: CalculatorItemQueue를 제네릭 타입으로 수정 #1 - CalculatorItemQueue에 각각 Double, String 타입을 가질 경우 타입 변환 문제

--- Calculator/Calculator/Model/Calculator.swift
+++ Calculator/Calculator/Model/Calculator.swift
@@ -12,29 +12,10 @@ protocol CalculateItem {
     
 }
 
-struct CalculatorItemQueue: CalculateItem {
-    var numbers = CalculatorQueue<Double>().queue
-    var operators = CalculatorQueue<String>().queue
+struct CalculatorItemQueue<Element>: CalculateItem {
+    var array: [Element] = []
     
-    mutating func appendItem<T>(_ input: T) {
-        if input is Double {
-            numbers.append(input as! Double)
-        } else if input is String {
-            operators.append(input as! String)
-        } else if input is Int {
-            let intInput = input as! Int
-            
-            numbers.append(intInput.doubleValue)
-        }
-    }
-}
-
-struct CalculatorQueue<Element>: CalculateItem {
-    var queue: [Element] = []
-}
-
-extension Int {
-    var doubleValue: Double {
-        return round(Double(self))
+    mutating func appendItem(_ item: Element) {
+        array.append(item)
     }
 }
