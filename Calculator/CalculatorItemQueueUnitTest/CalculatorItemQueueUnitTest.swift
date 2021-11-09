5a1cd8987e4b6e4f47db54d12d6ee2a76d7d20a4
SIWONKIM <siyeon0616@gmail.com>
2021-11-09 23:28:30 +0900

    feat: test_1이_있는_큐에_2를_push하면_1과_2가_남는다() 테스트 코드 작성

--- Calculator/CalculatorItemQueueUnitTest/CalculatorItemQueueUnitTest.swift
+++ Calculator/CalculatorItemQueueUnitTest/CalculatorItemQueueUnitTest.swift
@@ -19,10 +19,16 @@ class CalculatorItemQueueUnitTest: XCTestCase {
         sut = nil
     }
 
-    func test_큐에_1을_넣었을때_1이_잘들어가는지() {
+    func test_빈_큐에_1을_push하면_1이_남는다() {
         let input = 1
         let result = sut.push(input)
         XCTAssertEqual(result, [1])
     }
-
+    
+    func test_1이_있는_큐에_2를_push하면_1과_2가_남는다() {
+        sut.calculatorItems = [1]
+        let input = 2
+        let result = sut.push(input)
+        XCTAssertEqual(result, [1,2])
+    }
 }
