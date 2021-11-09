eaed085462cc4a30ac899f64b4f3eb87eb06fd48
zoe <kny0651@naver.com>
2021-11-09 10:29:29 +0900

    feat: CalculateItemQueue 구조체 구현

---
+++ Calculator/Calculator/CalculateItemQueue.swift
@@ -0,0 +1,20 @@
+//
+//  CalculateItemQueue.swift
+//  Calculator
+//
+//  Created by 권나영 on 2021/11/09.
+//
+
+import Foundation
+
+struct CalculateItemQueue<T> {
+   var items: [T] = []
+   
+   mutating func enqueue(element: T) {
+       items.append(element)
+   }
+   mutating func dequeue() -> T? {
+       if items.isEmpty { return nil }
+       return items.removeFirst()
+   }
+}
