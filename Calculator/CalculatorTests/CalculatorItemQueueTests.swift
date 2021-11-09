26bbbba0a6d5de00027f7d4317217673437c3974
smart8612 <smart8612@gmail.com>
2021-11-09 20:56:13 +0900

    feat: CalculatorItemQueue dequeue 기능 테스트 실패 코드 구현

--- Calculator/CalculatorTests/CalculatorItemQueueTests.swift
+++ Calculator/CalculatorTests/CalculatorItemQueueTests.swift
@@ -20,5 +20,17 @@ class CalculatorItemQueueTests: XCTestCase {
         // then
         XCTAssertEqual(result, 1)
     }
+    
+    func test_비어있는큐에1_2_3넣고dequeue하면2반환() throws {
+        // given
+        var queue = CalculatorItemQueue<Int>()
+        queue.enqueue(1)
+        queue.enqueue(2)
+        queue.enqueue(3)
+        // when
+        let result = queue.dequeue()
+        // then
+        XCTAssertEqual(result, 2)
+    }
 
 }
