3275c987f110082c65ae7e4718403c70e5dad731
SIWONKIM <siyeon0616@gmail.com>
2021-11-09 16:23:37 +0900

    feat: CalculatorItemQueue 구조체, CalculateItem 프로토콜, Test Target 추가

---
+++ Calculator/Calculator/CalculatorItemQueue.swift
@@ -0,0 +1,16 @@
+//
+//  CalculatorItemQueue.swift
+//  Calculator
+//
+//  Created by si won kim on 2021/11/09.
+//
+
+import Foundation
+
+protocol CalculateItem {
+    
+}
+
+struct CalculatorItemQueue: CalculateItem {
+    
+}
