3b05cc9585eaa84ef9be7fbfa5255c0904a22ed6
ICS-Asan <carpediem1015@gmail.com>
2021-11-09 14:41:58 +0900

    test: 각 item inbox가 비어있는지 test #4

--- Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
+++ Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
@@ -9,6 +9,14 @@ import XCTest
 
 class CalculatorItemQueueTests: XCTestCase {
 
-
+    func test_연산자_inbox가_비어있는지() {
+        let calculatorOperator = OperatorQueue()
+        XCTAssertTrue(calculatorOperator.queue.inbox.isEmpty)
+    }
+    
+    func test_숫자_inbox가_비어있는지() {
+        let calculatorNumber = NumberQueue()
+        XCTAssertTrue(calculatorNumber.queue.inbox.isEmpty)
+    }
 
 }
