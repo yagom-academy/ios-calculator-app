66d5788a0dc69e9ed64b090c7368428eecc4b0dd
forestjae <forestsj30@gmail.com>
2021-11-09 20:28:34 +0900

    feat: 계산요소들이 담길 CalculatorItemQueue 구조체 추가

---
+++ Calculator/Calculator/Model/CalculatorItemQueue.swift
@@ -0,0 +1,27 @@
+//
+//  CalculatorItemQueue.swift
+//  Calculator
+//
+//  Created by 이승재 on 2021/11/09.
+//
+
+import Foundation
+
+struct CalculatorItemQueue<T> {
+    var items: LinkedList<T>
+    var isEmpty: Bool {
+        return items.isEmpty
+    }
+    
+    func enqueue(_ item: T) {
+        
+    }
+    
+    func dequeue() {
+        
+    }
+    
+    func removeLast() {
+        
+    }
+}
