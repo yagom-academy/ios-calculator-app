54195f82e2fcd2e9ecf38f9d807fb71560f0186b
wooyani77 <wooyani77@gmail.com>
2021-11-09 18:30:53 +0900

    test: enQueue() 메서드의 test case 추가

--- Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
+++ Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
@@ -10,12 +10,19 @@ import XCTest
 
 class CalculatorItemQueueTests: XCTestCase {
     
-    func test_enQueue() {
+    func test_enQueue에_요소를_추가했을때_잘들어가는지() {
         var queue = CalculatorItemQueue<Int>()
-        
         queue.enQueue(1)
         
         XCTAssertEqual(queue.enQueueArray, [1])
     }
-
+    
+    func test_enQueue에_여러개의_요소를_추가했을때_잘들어가는지() {
+        var queue = CalculatorItemQueue<Int>()
+        queue.enQueue(1)
+        queue.enQueue(2)
+        queue.enQueue(3)
+        
+        XCTAssertEqual(queue.enQueueArray, [1, 2, 3])
+    }
 }
