e5a15c0bf7af1363fac34860c19501d496335844
Jager-yoo <y73447jh@gmail.com>
2021-11-09 16:46:10 +0900

    test: 큐타입 구현을 위한 테스트 케이스 작성 #3

--- Calculator/CalculatorTests/CalculatorTests.swift
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -8,5 +8,64 @@
 import XCTest
 
 class CalculatorTests: XCTestCase {
-    // 테스트 케이스 입력
+    
+    func test_숫자배열에_넣는_5가_Double_타입인지() {
+        var calculatorTest = CalculatorItemQueue()
+        calculatorTest.enQueueNumberArray(item: 5)
+        
+        let expectation: Double = 5
+        let result = calculatorTest.numberArray[0]
+        
+        XCTAssertEqual(expectation, result)
+    }
+    
+    func test_숫자배열에_넣는_음수가_Double_타입인지() {
+        var calculatorTest = CalculatorItemQueue()
+        calculatorTest.enQueueNumberArray(item: -2)
+        
+        let expectation: Double = -2
+        let result = calculatorTest.numberArray[0]
+        
+        XCTAssertEqual(expectation, result)
+    }
+    
+    func test_숫자배열에_넣는_0이_Double_타입인지() {
+        var calculatorTest = CalculatorItemQueue()
+        calculatorTest.enQueueNumberArray(item: 0)
+        
+        let expectation: Double = 0
+        let result = calculatorTest.numberArray[0]
+        
+        XCTAssertEqual(expectation, result)
+    }
+    
+    func test_연산자배열에_add케이스_넣어지는지() {
+        var calculatorTest = CalculatorItemQueue()
+        calculatorTest.enQueueMathOperator(item: .add)
+        
+        let expectation: MathOperator = .add
+        let result = calculatorTest.mathOperatorArray[0]
+        
+        XCTAssertEqual(expectation, result)
+    }
+    
+    func test_두배열이_모두_비워지는지() {
+        var calculatorTest = CalculatorItemQueue()
+        
+        calculatorTest.enQueueNumberArray(item: 1)
+        calculatorTest.enQueueNumberArray(item: -2)
+        calculatorTest.enQueueNumberArray(item: 3.3)
+        calculatorTest.enQueueNumberArray(item: 0)
+        
+        calculatorTest.enQueueMathOperator(item: .add)
+        calculatorTest.enQueueMathOperator(item: .subtract)
+        calculatorTest.enQueueMathOperator(item: .multiply)
+        calculatorTest.enQueueMathOperator(item: .divide)
+        
+        calculatorTest.clearBothArrays()
+        
+        let result = calculatorTest.numberArray.isEmpty && calculatorTest.mathOperatorArray.isEmpty
+        
+        XCTAssertTrue(result)
+    }
 }
