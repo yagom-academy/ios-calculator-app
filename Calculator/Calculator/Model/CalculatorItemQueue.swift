fc8b0ea0ee6864f2a292a5b4ae227dcf67f3d1aa
Jager-yoo <y73447jh@gmail.com>
2021-11-09 19:33:57 +0900

    feat: 구조체 CalculateItemQueue 큐타입 구현 #2

--- Calculator/Calculator/Model/CalculatorItemQueue.swift
+++ Calculator/Calculator/Model/CalculatorItemQueue.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+
+struct CalculatorItemQueue {
+    var numberArray: [Double] = []
+    var mathOperatorArray: [MathOperator] = []
+
+    mutating func enQueueNumberArray(item: Double) {
+        numberArray.append(item)
+    }
+    
+    mutating func enQueueMathOperator(item: MathOperator) {
+        mathOperatorArray.append(item)
+    }
+    
+    mutating func deQueueNumberArray() -> Double? {
+        switch numberArray.isEmpty {
+        case true:
+            return nil
+        case false:
+            return numberArray.removeFirst()
+        }
+    }
+    
+    mutating func deQueueMathOperatorArray() -> MathOperator? {
+        switch mathOperatorArray.isEmpty {
+        case true:
+            return nil
+        case false:
+            return mathOperatorArray.removeFirst()
+        }
+    }
+    
+    mutating func clearBothArrays() {
+        numberArray.removeAll()
+        mathOperatorArray.removeAll()
+    }
+}
