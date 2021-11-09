a8a0294eba6e89765ba7cf1e777965f0a112846c
Kim-EunsooSilver <graysilver18@naver.com>
2021-11-09 13:49:15 +0900

    feat: LinkedList, Node 클래스 구현

--- Calculator/Calculator/Model/LinkedList/LinkedList.swift
+++ Calculator/Calculator/Model/LinkedList/LinkedList.swift
@@ -7,7 +7,33 @@
 
 import Foundation
 
-class LinkedList {
+class LinkedList<T> {
+    class Node<T> {
+        var value: T
+        var next: Node<T>?
+        
+        init(value: T) {
+          self.value = value
+        }
+    }
+    
+    private var head: Node<T>?
+    private var tail: Node<T>?
+    
+    var isEmpty: Bool {
+        return head == nil
+    }
+    
+    var first: Node<T>? {
+        guard let headValue = head else {
+            return nil
+        }
+        return headValue
+    }
+    
+    var last: Node<T>? {
+        return tail
+    }
     
     
     
