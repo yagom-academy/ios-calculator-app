81c69c26f7a6020cc0530acb6328391f45bbf27c
hwangjeha <hyhpwang@gmail.com>
2021-11-09 12:05:47 +0900

    feat: LinkedList를 테스트하기 위한 LinkedListTests 타겟 추가

---
+++ Calculator/LinkedListTests/LinkedListTests.swift
@@ -0,0 +1,21 @@
+//
+//  LinkedListTests.swift
+//  LinkedListTests
+//
+//  Created by 황제하 on 2021/11/09.
+//
+
+import XCTest
+@testable import UnitTestSample
+
+class LinkedListTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+}
