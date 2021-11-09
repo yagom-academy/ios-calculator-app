721c3e3a383ca1ebd8b2c17351249e9cd2e9e83d
aCafela-coffee <seym7894@gmail.com>
2021-11-09 14:36:23 +0900

    test: Add queue test

--- Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
+++ Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
@@ -13,4 +13,18 @@ class CalculatorItemQueueTests: XCTestCase {
         try super.tearDownWithError()
         sut = nil
     }
+    
+    func test_enqueue() {
+        sut.enqueue(item: NumberItem(20))
+        let result = sut.items.count
+        XCTAssertEqual(result, 1)
+    }
+    
+    func test_dequeue_and_type_casting() {
+        sut.enqueue(item: NumberItem(20))
+        guard let result = sut.dequeue() as? NumberItem else {
+            return
+        }
+        XCTAssertEqual(result, 20)
+    }
 }
