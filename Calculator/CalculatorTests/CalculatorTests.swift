2c169c59cffde8fe4ecf6f9891d768a368e8f581
zoe <kny0651@naver.com>
2021-11-09 11:04:57 +0900

    test: enqueue메서드를 위한 테스트케이스 작성

--- Calculator/CalculatorTests/CalculatorTests.swift
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -8,5 +8,13 @@
 import XCTest
 
 class CalculatorTests: XCTestCase {
-
+    //MARK: - enqueue
+    func test_enqueue메서드로_1_2_3을더했을때_배열의마지막요소가_3인지() {
+        var calculateItemQueue = CalculateItemQueue<Int>()
+        calculateItemQueue.enqueue(element: 1)
+        calculateItemQueue.enqueue(element: 2)
+        calculateItemQueue.enqueue(element: 3)
+        let result = calculateItemQueue.items.last
+        XCTAssertEqual(result, 11)
+    }
 }
