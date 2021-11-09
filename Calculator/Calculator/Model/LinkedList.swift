80fb3fc149450fcf0c05d4f1d3e94b0eb66975b3
ChaminLee <trueman9512@naver.com>
2021-11-09 14:07:49 +0900

    feat: LinkedList 타입 추가
    - Queue타입 구현을 위해 LinkedList 구현
    - 값과 다음 노드의 정보를 갖는 Node 타입 추가
    - 새로운 노드를 추가하는 appendNewNode 메서드 추가
    - 가장 앞의 노드를 제거 후 반환하는 removeFirstNode 메서드 추가

---
+++ Calculator/Calculator/Model/LinkedList.swift
@@ -0,0 +1,83 @@
+//
+//  LinkedList.swift
+//  Calculator
+//
+//  Created by 이차민 on 2021/11/09.
+//
+
+import Foundation
+
+class LinkedList<Element> {
+    class ListNode<Element> {
+        var value: Element
+        var next: ListNode?
+        
+        init(value: Element) {
+            self.value = value
+        }
+    }
+    
+    typealias Node = ListNode<Element>
+    
+    var head: Node?
+    
+    var first: Node? {
+        return head
+    }
+    
+    var last: Node? {
+        guard var node = head else {
+            return nil
+        }
+        
+        while let next = node.next {
+            node = next
+        }
+        
+        return node
+    }
+    
+    var isEmpty: Bool {
+        guard head == nil else {
+            return true
+        }
+        
+        return false
+    }
+    
+    var count: Int {
+        guard var node = head else {
+            return 0
+        }
+        
+        var count = 1
+        
+        while let next = node.next {
+            node = next
+            count += 1
+        }
+        
+        return count
+    }
+    
+    func appendNewNode(value: Element) {
+        let newNode = Node(value: value)
+        
+        guard var lastNode = last else {
+            head = newNode
+            return
+        }
+        
+        lastNode.next = newNode
+    }
+    
+    func removeFirstNode() -> Node? {
+        if head == nil {
+            return nil
+        } else {
+            let firstElement = head
+            head = head?.next
+            return firstElement
+        }
+    }
+}
