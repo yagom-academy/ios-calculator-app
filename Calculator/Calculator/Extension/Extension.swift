83541ee9e28ce6d592751550161ea3131dc5507f
hoBahk <qudgh849@gmail.com>
2021-11-09 19:05:55 +0900

    refactor: extension 선언

---
+++ Calculator/Calculator/Extension/Extension.swift
@@ -0,0 +1,18 @@
+//
+//  Extension.swift
+//  Calculator
+//
+//  Created by 박병호 on 2021/11/09.
+//
+
+import Foundation
+
+extension Double: CalculateItem {
+    
+}
+
+extension String {
+    func split(with target: Character) -> [String] {
+        
+    }
+}
