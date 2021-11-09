8fb57c2c829437b13a3d84da53256aad66e3f99c
hoBahk <qudgh849@gmail.com>
2021-11-09 11:52:15 +0900

    refactor: LInkedList 함수 생성
    * LInkedList 함수 생성 후 기존의 CalculatorItemQueue의 구현을 옮김
    * CalculatorItemQueue에서는 LinkedList의 함수 메서드 불러와서 재사용

--- Calculator/Calculator/Model/CalculatorItemQueue.swift
+++ Calculator/Calculator/Model/CalculatorItemQueue.swift
@@ -8,49 +8,21 @@
 import Foundation
 
 struct CalculatorItemQueue<T>: CalculateItem {
-
-    var front: Node<T>?
-    
-    init(front: Node<T>? = nil) {
-        self.front = front
-    }
+    var queue = LinkedList<T>()
     
     mutating func enQueue(_ data: T?) {
-        var node = front
-        
-        if node == nil {
-            front = Node(data: data)
-            return
-        }
-        
-        while node?.next != nil {
-            node = node?.next
-        }
-        node?.next = Node(data: data)
+        queue.append(data)
     }
     
     mutating func deQueue() {
-        if front == nil {
-            return
-        }
-        front = front?.next
+        queue.deQueue()
     }
     
     mutating func removeAll() {
-        front = nil
+        queue.removeAll()
     }
     
     mutating func returnQueue() -> Array<T> {
-        var queue: Array<T> = []
-        var node = front
-        
-        while node?.data != nil {
-            guard let data = node?.data else {
-                return []
-            }
-            queue.append(data)
-            node = node?.next
-        }
-        return queue
+        return queue.returnQueue()
     }
 }
---
+++ Calculator/Calculator/Model/LinkedList.swift
@@ -0,0 +1,69 @@
+//
+//  Node.swift
+//  Calculator
+//
+//  Created by 박병호 on 2021/11/08.
+//
+
+import Foundation
+
+class Node<T> {
+    var data: T?
+    var next: Node?
+    
+    init(data: T?, next: Node? = nil) {
+        self.data = data
+        self.next = next
+    }
+}
+
+struct LinkedList<T> {
+    var front: Node<T>?
+    
+    init(front: Node<T>? = nil) {
+        self.front = front
+    }
+    
+    func isEmpty() -> Bool {
+        return front == nil
+    }
+    
+    mutating func append(_ data: T?) {
+        if isEmpty() {
+            front = Node(data: data)
+            return
+        }
+        
+        var node = front
+        
+        while node?.next != nil {
+            node = node?.next
+        }
+        node?.next = Node(data: data)
+    }
+    
+    mutating func deQueue() {
+        if isEmpty() {
+            return
+        }
+        front = front?.next
+    }
+    
+    mutating func removeAll() {
+        front = nil
+    }
+    
+    mutating func returnQueue() -> Array<T> {
+        var queue: Array<T> = []
+        var node = front
+        
+        while node?.data != nil {
+            guard let data = node?.data else {
+                return []
+            }
+            queue.append(data)
+            node = node?.next
+        }
+        return queue
+    }
+}
