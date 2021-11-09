3d9eb614e0d79470f691f8fa9d6e750fd89e06ea
just1103 <hyojuson1103@gmail.com>
2021-11-09 17:41:27 +0900

    feat: Node 클래스 추가 #1

--- Calculator/Calculator/CalculatorItemQueue.swift
+++ Calculator/Calculator/CalculatorItemQueue.swift
@@ -5,3 +5,13 @@ protocol CalculateItem {
 
 class CalculatorItemQueue: CalculateItem {
 }
+
+class Node<T> {
+    var value: T
+    var next: Node<T>?
+    
+    init(value: T, next: Node<T>? = nil) {
+        self.value = value
+        self.next = next
+    }
+}
