b2c21932cdf8c4b192f4d9343d9c16219462e793
JT <dd3557@naver.com>
2021-11-09 10:34:22 +0900

    feat: CalculatorItemQueue.swift 파일 생성 및 기본적인 코드 구현

---
+++ Calculator/Calculator/Model/CalculatorItemQueue.swift
@@ -0,0 +1,24 @@
+//
+//  CalculatorItemQueue.swift
+//  Calculator
+//
+//  Created by 김진태 on 2021/11/09.
+//
+
+import Foundation
+
+typealias List = Array
+
+protocol CalculateItem {
+}
+
+struct CalculatorItemQueue<T>: CalculateItem {
+    private let items: List<T>
+    
+    func insert(_ item: T) {
+    }
+    
+    func delete() {
+        
+    }
+}
