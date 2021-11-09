0733f2ba2039fd30d52bea9391fa122f0f79118b
wooyani77 <wooyani77@gmail.com>
2021-11-09 17:22:10 +0900

    test: enQueue 메서드 test 코드 추가

--- Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
+++ Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
@@ -6,27 +6,16 @@
 //
 
 import XCTest
+@testable import Calculator
 
 class CalculatorItemQueueTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func test_enQueue() {
+        var queue = CalculatorItemQueue<Int>()
+        
+        queue.enQueue(1)
+        
+        XCTAssertEqual(queue.enQueueArray, [1])
     }
 
 }
