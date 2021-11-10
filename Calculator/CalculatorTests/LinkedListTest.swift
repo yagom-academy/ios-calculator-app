50000fcb77d8a8240e84229f92315dad5fc9007f
ChaminLee <trueman9512@naver.com>
2021-11-10 11:26:30 +0900

    test: LinkedList 테스트 코드 수정
    - appendNewNode -> appendNode
    - 테스트 코드의 네이밍을 명확하게 만들고, 한글로 수정
    - 일부 Equtable을 채택하지 않으므로 값이 비교되지 않는 부분은 XCTAssertNil이나 다른 로직으로 확인

--- Calculator/CalculatorTests/LinkedListTest.swift
+++ Calculator/CalculatorTests/LinkedListTest.swift
@@ -8,65 +8,60 @@
 import XCTest
 
 class LinkedListTest: XCTestCase {
-    func test_first_element_in_linkedList() {
-        let list = LinkedList<Any>()
-        list.appendNewNode(value: 3)
-        list.appendNewNode(value: "+")
-        list.appendNewNode(value: 4)
-        XCTAssertEqual(3, list.first?.value as? Int)
+    func test_링크드리스트에_두_개의_데이터를_추가한_이후_첫_번째_노드의_값이_잘_반환되는지() {
+        let list = LinkedList<Double>()
+        list.appendNode(value: 3.0)
+        list.appendNode(value: 4.0)
+        XCTAssertEqual(3, list.first?.value)
     }
-    
-    func test_last_element_in_linkedList() {
-        let list = LinkedList<Any>()
-        list.appendNewNode(value: 3)
-        list.appendNewNode(value: "+")
-        list.appendNewNode(value: 4)
-        XCTAssertEqual(4, list.last?.value as? Int)
+
+    func test_링크드리스트에_두_개의_데이터를_추가한_이후_마지막_노드의_값이_잘_반환되는지() {
+        let list = LinkedList<Double>()
+        list.appendNode(value: 3.0)
+        list.appendNode(value: 4.0)
+        XCTAssertEqual(4, list.last?.value)
     }
-    
-    func test_next_element_in_linkedList() {
-        let list = LinkedList<Any>()
-        list.appendNewNode(value: 3)
-        list.appendNewNode(value: "+")
-        list.appendNewNode(value: 4)
-        XCTAssertEqual("+", list.first?.next?.value as? String)
+
+    func test_링크드리스트에서_첫_번째의_노드가_다음_노드와_잘_연결되었는지() {
+        let list = LinkedList<Double>()
+        list.appendNode(value: 3.0)
+        list.appendNode(value: 4.0)
+        XCTAssertEqual(4, list.first?.next?.value)
     }
-    
-    func test_linkedList_is_empty() {
-        let list = LinkedList<Any>()
-        list.appendNewNode(value: 3)
-        list.appendNewNode(value: "+")
-        list.appendNewNode(value: 4)
+
+    func test_링크드리스트가_비어있는지() {
+        let list = LinkedList<Double>()
+        list.appendNode(value: 3.0)
+        list.appendNode(value: 4.0)
         let isEmpty = list.isEmpty
         XCTAssertEqual(false, isEmpty)
     }
-    
-    func test_append_linkedList() {
-        let list = LinkedList<Any>()
-        list.appendNewNode(value: 3)
-        list.appendNewNode(value: "+")
-        list.appendNewNode(value: 4)
-        XCTAssertEqual(3, list.count)
+
+    func test_링크드리스트에_데이터를_추가한_후_개수가_맞는지() {
+        let list = LinkedList<Double>()
+        list.appendNode(value: 3.0)
+        list.appendNode(value: 4.0)
+        XCTAssertEqual(2, list.count)
     }
-    
-    func test_remove_in_empty_linkedList() {
-        let list = LinkedList<Any>()
+
+    func test_비어있는_링크드리스트에서_첫_번째_노드를_제거하는_경우_nil이_나오는지() {
+        let list = LinkedList<Double>()
+        let result = list.removeFirstNode()
+        XCTAssertNil(result)
+    }
+
+    func test_비어있지_않은_링크드리스트에서_첫_번째_노드를_제거하는_경우_다음_노드가_첫_번째_노드가_되는지() {
+        let list = LinkedList<Double>()
+        list.appendNode(value: 3.0)
+        list.appendNode(value: 4.0)
         list.removeFirstNode()
-        XCTAssertEqual(nil, list.first?.value as? Int)
+        XCTAssertEqual(4, list.first?.value)
     }
-    
-    func test_remove_in_not_empty_linkedList() {
-        let list = LinkedList<Any>()
-        list.appendNewNode(value: 3)
-        list.appendNewNode(value: "+")
-        list.removeFirstNode()
-        XCTAssertEqual("+", list.first?.value as? String)
-    }
-    
-    func test_remove_all_nodes_in_linkedList() {
-        let list = LinkedList<Any>()
-        list.appendNewNode(value: 3)
-        list.appendNewNode(value: "+")
+
+    func test_링크드리스트의_모든_노드들을_삭제한_후_개수가_맞는지() {
+        let list = LinkedList<Double>()
+        list.appendNode(value: 3.0)
+        list.appendNode(value: 4.0)
         list.removeAllNodes()
         XCTAssertEqual(0, list.count)
     }
