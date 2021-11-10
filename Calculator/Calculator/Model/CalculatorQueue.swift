f724bd6ac7213177349d8c2a7aa34a0b84a6c517
oksunwoo <oksunwo@naver.com>
2021-11-10 19:22:52 +0900

    refactor: dequeue메소드에서 nil을 반환하도록 수정
    - 배열이 비었는지 비어있지 않은지 판단하는 isEmpty 프로퍼티 추가
    - 그에 따른 enqueue메소드 수정

--- Calculator/Calculator/Model/CalculatorQueue.swift
+++ Calculator/Calculator/Model/CalculatorQueue.swift
@@ -13,12 +13,18 @@ protocol CalculatorItem {
 
 struct CalculatorItemQueue <T> where T: CalculatorItem {
     var operationStorage: [T] = []
+    var isEmpty: Bool = true
     
     mutating func enqueue(operation: T) {
         operationStorage.append(operation)
+        self.isEmpty = false
     }
     
     mutating func dequeue() -> T? {
+        if self.isEmpty {
+            return nil
+        }
+        
         let operation = operationStorage.removeFirst()
         return operation
     }
