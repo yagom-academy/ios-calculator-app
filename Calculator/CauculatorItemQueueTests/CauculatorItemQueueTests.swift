e128c7bf1f546c73d942adb5f244eb7fa4525324
namu <simjh27@gmail.com>
2021-11-10 14:16:55 +0900

    test: CalculatorItemQueueTests isEmpty test case 추가

--- Calculator/CauculatorItemQueueTests/CauculatorItemQueueTests.swift
+++ Calculator/CauculatorItemQueueTests/CauculatorItemQueueTests.swift
@@ -15,25 +15,24 @@ class CauculatorItemQueueTests: XCTestCase {
         try super.setUpWithError()
         sut = CalculatorItemQueue<Int>()
     }
-
     override func tearDownWithError() throws {
         try super.tearDownWithError()
         sut = nil
     }
     
+    // MARK:- enqueue(_ item:) tests
     func test_enqueue호출시_4를전달하면_list에4가추가되는지() {
         //given
         let input = 4
-        
         //when
         sut.enqueue(input)
         let result = sut.front
-        
         //then
         XCTAssertEqual(result, 4)
         XCTAssertEqual(sut.count, 1)
     }
-
+    
+    // MARK:- dequeue() tests
     func test_dequeue호출시_빈배열일시_nil을반환하는지() {
         // given
         //when
@@ -41,7 +40,6 @@ class CauculatorItemQueueTests: XCTestCase {
         //then
         XCTAssertEqual(result, nil)
     }
-    
     func test_4가있는큐에서_dequeue호출시_4를반환하고_list가비워지는지() {
         //given
         sut.enqueue(4)
@@ -51,7 +49,6 @@ class CauculatorItemQueueTests: XCTestCase {
         XCTAssertEqual(result, 4)
         XCTAssertTrue(sut.isEmpty)
     }
-    
     func test_4와7이있는큐에서_dequeue호출시_4를반환하고_list에7이남는지() {
         //given
         sut.enqueue(4)
@@ -61,5 +58,22 @@ class CauculatorItemQueueTests: XCTestCase {
         //then
         XCTAssertEqual(result, 4)
         XCTAssertTrue(sut.count == 1 && sut.front == 7)
+    }
+    
+    // MARK:- isEmpty tests
+    func test_빈큐에서_isEmpty를요청하면_true반환하는지() {
+        //given
+        //when
+        let result = sut.isEmpty
+        //then
+        XCTAssertTrue(result)
+    }
+    func test_4가있는큐에서_isEmpty를요청하면_false반환하는지() {
+        //given
+        sut.enqueue(4)
+        //when
+        let result = sut.isEmpty
+        //then
+        XCTAssertFalse(result)
     }
 }
