c857e6a37ea86bbd4ee9f660edb39264dd92a6f3
Kim-EunsooSilver <graysilver18@naver.com>
2021-11-10 20:58:39 +0900

    test: 피드백 반영으로 인한 테스트 함수 수정

--- Calculator/CalculatorTests/CalculatorItemQueueTests.swift
+++ Calculator/CalculatorTests/CalculatorItemQueueTests.swift
@@ -10,66 +10,85 @@ import XCTest
 class CalculatorTests: XCTestCase {
 
     func test_기존_연산큐에_데이터가_없을때_계산기가_입력받은_1와_더하기_연산자는_연산큐에_쌓이는가() {
-        let calculatorItemQueue = CalculatorItemQueue()
+        let operands = CalculatorItemQueue<Double>()
+        let operators = CalculatorItemQueue<String>()
         
-        calculatorItemQueue.enqueue(1.0, "+")
+        operands.enqueue(1.0)
+        operators.enqueue("+")
         
-        XCTAssertEqual(1.0, calculatorItemQueue.computingValueList.first?.value)
-        XCTAssertEqual("+", calculatorItemQueue.operatorList.first?.value)
+        XCTAssertEqual(1.0, operands.queue.first?.value)
+        XCTAssertEqual("+", operators.queue.first?.value)
     }
     
     func test_기존_연산큐에_데이터가_있을때_제대로_쌓이는가() {
-        let calculatorItemQueue = CalculatorItemQueue()
-        
-        calculatorItemQueue.enqueue(1.0, "+")
-        calculatorItemQueue.enqueue(2.0, "-")
-        calculatorItemQueue.enqueue(3.0, "/")
-        
-        XCTAssertEqual(1.0, calculatorItemQueue.computingValueList.first?.value)
-        XCTAssertEqual("+", calculatorItemQueue.operatorList.first?.value)
-        
-        XCTAssertEqual(3.0, calculatorItemQueue.computingValueList.last?.value)
-        XCTAssertEqual("/", calculatorItemQueue.operatorList.last?.value)
-        
+        let operands = CalculatorItemQueue<Double>()
+        let operators = CalculatorItemQueue<String>()
+
+        operands.enqueue(1.0)
+        operators.enqueue("+")
+        operands.enqueue(2.0)
+        operators.enqueue("-")
+        operands.enqueue(3.0)
+        operators.enqueue("/")
+
+        XCTAssertEqual(1.0, operands.queue.first?.value)
+        XCTAssertEqual("+", operators.queue.first?.value)
+        XCTAssertEqual(3.0, operands.queue.last?.value)
+        XCTAssertEqual("/", operators.queue.last?.value)
     }
     
+
     func test_연산큐에_데이터가_존재할때_dequeue_기능이_잘_되는가() {
-        let calculatorItemQueue = CalculatorItemQueue()
-        
-        calculatorItemQueue.enqueue(1.0, "+")
-        calculatorItemQueue.enqueue(2.0, "-")
-        calculatorItemQueue.enqueue(3.0, "/")
-        
-        let firstDequeued = calculatorItemQueue.dequeue()
-        
-        
-        XCTAssertEqual(1.0, firstDequeued.0)
-        XCTAssertEqual("+", firstDequeued.1)
-        
-        XCTAssertEqual(2.0, calculatorItemQueue.computingValueList.first?.value)
-        XCTAssertEqual("-", calculatorItemQueue.operatorList.first?.value)
+        let operands = CalculatorItemQueue<Double>()
+        let operators = CalculatorItemQueue<String>()
+
+        operands.enqueue(1.0)
+        operators.enqueue("+")
+        operands.enqueue(2.0)
+        operators.enqueue("-")
+        operands.enqueue(3.0)
+        operators.enqueue("/")
+
+        let firstDequeuedOperands = operands.dequeue()
+        let firstDequeuedOperators = operators.dequeue()
+
+        XCTAssertEqual(1.0, firstDequeuedOperands)
+        XCTAssertEqual("+", firstDequeuedOperators)
+
+        XCTAssertEqual(2.0, operands.queue.first?.value)
+        XCTAssertEqual("-", operators.queue.first?.value)
     }
     
     func test_연산큐에_있는_모든_데이터를_지우는_기능이_잘_되는가() {
-        let calculatorItemQueue = CalculatorItemQueue()
-        
-        calculatorItemQueue.enqueue(1.0, "+")
-        calculatorItemQueue.enqueue(2.0, "-")
-        calculatorItemQueue.enqueue(3.0, "/")
-        
-        XCTAssertEqual(1.0, calculatorItemQueue.computingValueList.first?.value)
-        XCTAssertEqual("+", calculatorItemQueue.operatorList.first?.value)
-        XCTAssertEqual(3.0, calculatorItemQueue.computingValueList.last?.value)
-        XCTAssertEqual("/", calculatorItemQueue.operatorList.last?.value)
-        
-        calculatorItemQueue.resetQueue()
-        
-        XCTAssertEqual(nil, calculatorItemQueue.computingValueList.first?.value)
-        XCTAssertEqual(nil, calculatorItemQueue.operatorList.first?.value)
-        XCTAssertEqual(nil, calculatorItemQueue.computingValueList.last?.value)
-        XCTAssertEqual(nil, calculatorItemQueue.operatorList.last?.value)
-        
+        let operands = CalculatorItemQueue<Double>()
+        let operators = CalculatorItemQueue<String>()
+
+        operands.enqueue(1.0)
+        operators.enqueue("+")
+        operands.enqueue(2.0)
+        operators.enqueue("-")
+        operands.enqueue(3.0)
+        operators.enqueue("/")
+
+        XCTAssertEqual(1.0, operands.queue.first?.value)
+        XCTAssertEqual("+", operators.queue.first?.value)
+        XCTAssertEqual(3.0, operands.queue.last?.value)
+        XCTAssertEqual("/", operators.queue.last?.value)
+
+        operands.resetQueue()
+        operators.resetQueue()
+
+        XCTAssertEqual(nil, operands.queue.first?.value)
+        XCTAssertEqual(nil, operators.queue.first?.value)
+        XCTAssertEqual(nil, operands.queue.first?.value)
+        XCTAssertEqual(nil, operators.queue.first?.value)
+
     }
+    
+
+
+
+    
     
     
 }
