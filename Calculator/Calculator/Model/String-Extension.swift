4de10bc0cbc433218ddc481eaba6f69995a23cbf
forestjae <forestsj30@gmail.com>
2021-11-10 17:51:44 +0900

    feat: String타입의 익스텐션 부를 구현하는 String-Extension 파일 추가

---
+++ Calculator/Calculator/Model/String-Extension.swift
@@ -0,0 +1,14 @@
+//
+//  String-Extension.swift
+//  Calculator
+//
+//  Created by 이승재 on 2021/11/10.
+//
+
+import Foundation
+
+extension String {
+    func split(with target: Character) -> [String] {
+        return []
+    }
+}
