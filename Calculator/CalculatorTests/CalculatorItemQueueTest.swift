2526234dfc4a1095c312365bf4b07cc694e52cb8
ChaminLee <trueman9512@naver.com>
2021-11-10 10:38:28 +0900

    test: CalculatorItemQueue 테스트 코드 수정
    - Generics 활용을 위한 타입 명시 Any -> Double
    - 테스트 코드 네이밍을 명확하게 만들고, 한글로 수정

--- Calculator/CalculatorTests/CalculatorItemQueueTest.swift
+++ Calculator/CalculatorTests/CalculatorItemQueueTest.swift
@@ -8,45 +8,40 @@
 import XCTest
 
 class CalculatorItemQueueTest: XCTestCase {
-    func test_enqueue_in_queue() {
-        let queue = CalculatorItemQueue<Any>()
+    func test_큐에_데이터를_2개_넣었을_떄_큐의_개수가_2인지() {
+        let queue = CalculatorItemQueue<Double>()
         queue.enqueue(3.5)
-        queue.enqueue("+")
         queue.enqueue(4.5)
-        XCTAssertEqual(3, queue.items.count)
+        XCTAssertEqual(2, queue.items.count)
     }
     
-    func test_dequeue_in_empty_queue() {
-        let queue = CalculatorItemQueue<Any>()
-        queue.dequeue()
-        XCTAssertEqual(nil, queue.items.first?.value as? Double)
+    func test_비어있는_큐에서_데이터를_빼내려고_할때_nil이_반환되는지() {
+        let queue = CalculatorItemQueue<Double>()
+        XCTAssertEqual(nil, queue.dequeue())
     }
     
-    func test_dequeue_in_not_empty_queue() {
-        let queue = CalculatorItemQueue<Any>()
+    func test_비어있지_않은_큐에서_데이터를_빼내려고_할때_정상적으로_앞에_요소가_제거되는지() {
+        let queue = CalculatorItemQueue<Double>()
         queue.enqueue(3.5)
-        queue.enqueue("+")
         queue.enqueue(4.5)
         queue.dequeue()
-        queue.dequeue()
-        XCTAssertEqual(4.5, queue.items.first?.value as? Double)
+        XCTAssertEqual(4.5, queue.items.first?.value)
     }
     
-    func test_dequeue_in_not_empty_queue_with_return_value() {
-        let queue = CalculatorItemQueue<Any>()
+    func test_비어있지_않은_큐에서_데이터를_빼내려고_할때_정상적으로_앞에_요소가_제거되고_반환되는지() {
+        let queue = CalculatorItemQueue<Double>()
         queue.enqueue(3.5)
-        queue.enqueue("+")
         queue.enqueue(4.5)
-        queue.dequeue()
         let value = queue.dequeue()
-        XCTAssertEqual("+", value as? String)
+        XCTAssertEqual(3.5, value)
+        XCTAssertEqual(1, queue.items.count)
     }
     
-    func test_removeAll_in_queue() {
-        let queue = CalculatorItemQueue<Any>()
+    func test_큐의_모든_데이터를_삭제하는_경우_모두_삭제가_되는지() {
+        let queue = CalculatorItemQueue<Double>()
         queue.enqueue(3.5)
-        queue.enqueue("+")
         queue.enqueue(4.5)
+        queue.enqueue(5.5)
         queue.removeAll()
         XCTAssertEqual(0, queue.items.count)
     }
