3dd9dd58803b8a995de0c275eb63f884c5a7657e
wooyani77 <wooyani77@gmail.com>
2021-11-10 18:32:19 +0900

    refactor: 전반적인 네이밍 수정

--- Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
+++ Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
@@ -12,42 +12,42 @@ class CalculatorItemQueueTests: XCTestCase {
     
     func test_enQueue에_요소를_추가했을때_잘들어가는지() {
         var queue = CalculatorItemQueue<Int>()
-        queue.enQueue(1)
+        queue.enQueueElement(1)
         
-        XCTAssertEqual(queue.enQueueArray, [1])
+        XCTAssertEqual(queue.enQueueElements, [1])
     }
     
     func test_enQueue에_여러개의_요소를_추가했을때_잘들어가는지() {
         var queue = CalculatorItemQueue<Int>()
-        queue.enQueue(1)
-        queue.enQueue(2)
-        queue.enQueue(3)
+        queue.enQueueElement(1)
+        queue.enQueueElement(2)
+        queue.enQueueElement(3)
         
-        XCTAssertEqual(queue.enQueueArray, [1, 2, 3])
+        XCTAssertEqual(queue.enQueueElements, [1, 2, 3])
     }
     
     func test_deQueue를_실행했을때_빈배열이면_nil을_반환하는지() {
         var queue = CalculatorItemQueue<Int>()
-        queue.deQueue()
+        queue.deQueueFirstElement()
         
-        XCTAssertNil(queue.deQueue())
+        XCTAssertNil(queue.deQueueFirstElement())
     }
     
     func test_enQueue에_요소한개를_추가하고_deQueue했을때_빠지는지() {
         var queue = CalculatorItemQueue<Int>()
-        queue.enQueue(1)
-        queue.deQueue()
+        queue.enQueueElement(1)
+        queue.deQueueFirstElement()
         
-        XCTAssertNil(queue.deQueue())
+        XCTAssertNil(queue.deQueueFirstElement())
     }
     
     func test_enQueue에_요소여러개를_추가하고_deQueue했을때_마지막요소가_빠지는지() {
         var queue = CalculatorItemQueue<Int>()
-        queue.enQueue(1)
-        queue.enQueue(2)
-        queue.enQueue(3)
+        queue.enQueueElement(1)
+        queue.enQueueElement(2)
+        queue.enQueueElement(3)
         
-        if let result = queue.deQueue() {
+        if let result = queue.deQueueFirstElement() {
             XCTAssertEqual(result, 1)
         }
     }
