03117cd17cd874ed36573b05fcb5b796d51a70f0
Ari <lee_ari95@icloud.com>
2021-11-11 01:36:04 +0900

    test: 연산자 갯수에 따라 곱셈 연산을 하는 case 추가 #3

--- Calculator/CalculatorTests/FormulaTests.swift
+++ Calculator/CalculatorTests/FormulaTests.swift
@@ -180,4 +180,38 @@ class FormulaTests: XCTestCase {
         XCTAssertEqual(result, -2.0)
     }
     
+    func test_곱셈연산해보기_피연산자한개() {
+        var formula = Formula()
+        
+        formula.operands.enqueue(2.0)
+        formula.operators.enqueue("*")
+        let result = formula.result()
+        
+        XCTAssertEqual(result, 0)
+    }
+    
+    func test_곱셈연산해보기_피연산자두개() {
+        var formula = Formula()
+        
+        formula.operands.enqueue(2.0)
+        formula.operators.enqueue("*")
+        formula.operands.enqueue(2.0)
+        formula.operators.enqueue("*")
+        let result = formula.result()
+        
+        XCTAssertEqual(result, 4.0)
+    }
+    
+    func test_곱셈연산해보기_피연산자세개() {
+        var formula = Formula()
+        
+        formula.operands.enqueue(2.0)
+        formula.operators.enqueue("*")
+        formula.operands.enqueue(2.0)
+        formula.operators.enqueue("*")
+        formula.operands.enqueue(2.0)
+        let result = formula.result()
+        
+        XCTAssertEqual(result, 8.0)
+    }
 }
