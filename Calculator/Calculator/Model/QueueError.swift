9114ed1c5abb4441341b27186b544505515bf7aa
Ari <lee_ari95@icloud.com>
2021-11-11 01:25:48 +0900

    feat: CalculatorError 내부에 description 프로퍼티 추가 #4

--- Calculator/Calculator/Model/QueueError.swift
+++ Calculator/Calculator/Model/QueueError.swift
@@ -10,4 +10,13 @@ import Foundation
 enum CalculatorError: Error {
     case queueNotFound
     case wrongOperator
+    
+    var description: String {
+        switch self {
+        case .queueNotFound:
+            return "큐가 비어있습니다."
+        case .wrongOperator:
+            return "잘못된 연산자입니다."
+        }
+    }
 }
