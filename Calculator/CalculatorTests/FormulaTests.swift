ad12c6fd30501c9b2061d36cacddecf92bded31e
Ari <lee_ari95@icloud.com>
2021-11-11 01:43:14 +0900

    refactor: Operator를 Model group으로 이동 #4

--- Calculator/CalculatorTests/FormulaTests.swift
+++ Calculator/CalculatorTests/FormulaTests.swift
@@ -13,45 +13,6 @@ extension String {
     }
 }
 
-enum Operator: Character, CalculateItem, CaseIterable {
-    case add = "+"
-    case subtract = "-"
-    case divide = "/"
-    case multiply = "*"
-    
-    func calculate(lhs: Double, rhs: Double) -> Double {
-        switch self {
-        case .add:
-            return add(lhs: lhs, rhs: rhs)
-        case .divide:
-            return didvde(lhs: lhs, rhs: rhs)
-        case .multiply:
-            return multiply(lhs: lhs, rhs: rhs)
-        case .subtract:
-            return subtract(lhs: lhs, rhs: rhs)
-        }
-    }
-    
-    private func add(lhs: Double, rhs: Double) -> Double {
-        lhs + rhs
-    }
-    
-    private func subtract(lhs: Double, rhs: Double) -> Double {
-        lhs - rhs
-    }
-    
-    private func didvde(lhs: Double, rhs: Double) -> Double {
-        if rhs == .zero {
-            return .nan
-        }
-        return lhs / rhs
-    }
-    
-    private func multiply(lhs: Double, rhs: Double) -> Double {
-        lhs * rhs
-    }
-}
-
 struct Formula {
     var operands = CalculatorItemQueue<Double>()
     var operators = CalculatorItemQueue<Character>()
