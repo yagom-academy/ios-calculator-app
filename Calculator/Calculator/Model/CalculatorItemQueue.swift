a7933dcdb2ba1a9062a85e7f1df93d37ed5f1a1d
YohanBlessYou <duddn5906@naver.com>
2021-11-11 15:55:59 +0900

    revert: 계산기 큐 구조변경을 위해 STEP1 코드 revert
    - 게산기 큐에 숫자와 연산자를 별도로 저장할 수 있도록 구현하기 위해 revert

--- Calculator/Calculator/Model/CalculatorItemQueue.swift
+++ Calculator/Calculator/Model/CalculatorItemQueue.swift
@@ -1,34 +1,5 @@
 import Foundation
 
 struct CalculatorItemQueue {
-    var queue: [CalculateItem] = []
-    
-    mutating func enqueue(_ item: CalculateItem) {
-        queue.append(item)
-    }
-    
-    mutating func allClear() {
-        self.queue = []
-    }
-    
-    func calculate() -> Double {
-        var result: Double = 0
-        
-        for item in queue {
-            switch item {
-            case let adder as Adder:
-                result = adder.calculate(originalValue: result)
-            case let subtractor as Subtractor:
-                result = subtractor.calculate(originalValue: result)
-            case let multiplier as Multiplier:
-                result = multiplier.calculate(originalValue: result)
-            case let divider as Divider:
-                result = divider.calculate(originalValue: result)
-            default:
-                break
-            }
-        }
-        
-        return result
-    }
+ 
 }
