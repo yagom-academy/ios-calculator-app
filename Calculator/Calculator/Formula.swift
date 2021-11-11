9920b0f10a37d521041bd31fea3f827376e53a66
horeng2 <huski.a.a@gmail.com>
2021-11-12 04:02:54 +0900

    feat: extension ExpressionParser 선언

--- Calculator/Calculator/Formula.swift
+++ Calculator/Calculator/Formula.swift
@@ -1,13 +1,13 @@
 import Foundation
 
-struct Formula<Element> {
-    var operands = CalculatorItemQueue<Element>()
-    var operators = CalculatorItemQueue<Element>()
+struct Formula {
+    var operands = CalculatorItemQueue<Double>()
+    var operators = CalculatorItemQueue<String>()
     
-    func result() -> Double {
-
-
-    }
+//    func result() -> Double {
+//
+//
+//    }
 }
 
 extension String {
@@ -16,3 +16,32 @@ extension String {
         return splitedString
     }
 }
+
+
+enum ExpressionParser {
+    static let operatorEnumArray = Operator.allCases.map { String($0.rawValue) }
+    
+    func parse(from input: String) -> Formula {
+        var formula = Formula()
+        
+        let inputCharacters = Array(input).map { String($0) }
+        let operandsArray = componentsByOperators(from: input)
+        let operatorArray = inputCharacters.filter { ExpressionParser.operatorEnumArray.contains($0) == true }
+        
+        for operandIndex in 0...operandsArray.count {
+            formula.operands.insertToQueue(Double(operandsArray[operandIndex]) ?? 0)
+        }
+        
+        for operatorIndex in 0...operatorArray.count {
+            formula.operators.insertToQueue(operatorArray[operatorIndex])
+        }
+         //입력받은 값 피연산자or연산자를 두개의 큐 중 적절한 큐에 저장
+        return formula
+    }
+
+    private func componentsByOperators(from input: String) -> [String] {
+        return input.components(separatedBy: ["+","-","/","*"])
+        
+    }
+}
+
