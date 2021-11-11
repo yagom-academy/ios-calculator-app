7bc29e3b177d98974b738dc302b4e4b86a6cdeee
horeng2 <huski.a.a@gmail.com>
2021-11-12 01:01:39 +0900

    feat: extension String 선언

--- Calculator/Calculator/Formula.swift
+++ Calculator/Calculator/Formula.swift
@@ -1,12 +1,18 @@
 import Foundation
 
-struct Formula {
+struct Formula<Element> {
     var operands = CalculatorItemQueue<Element>()
     var operators = CalculatorItemQueue<Element>()
     
     func result() -> Double {
-        
-        
+
+
     }
 }
 
+extension String {
+    func split(with target: Character) -> [String] {
+        let splitedString = self.split(separator: target).map { String($0) }
+        return splitedString
+    }
+}
