de1461ff9dcc6d9f14fcfe45f606e575a2111540
YohanBlessYou <duddn5906@naver.com>
2021-11-11 16:56:03 +0900

    test: 계산기 큐 테스트코드 추가

--- Calculator/CalculatorTests/CalculatorTests.swift
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -9,4 +9,44 @@ class CalculatorTests: XCTestCase {
         try super.setUpWithError()
         testQueue = CalculatorItemQueue()
     }
+    
+    func test_basicOperandQueueing() {
+        testQueue.enqueue(1)
+        
+        let firstOperand = testQueue.firstNode?.value as? Double
+        
+        XCTAssertEqual(firstOperand, 1)
+    }
+    
+    func test_operandsQueueing() {
+        testQueue.enqueue(1)
+        testQueue.enqueue(2)
+        testQueue.enqueue(3)
+        testQueue.enqueue(4)
+        testQueue.enqueue(5)
+        testQueue.dequeue()
+        testQueue.dequeue()
+        testQueue.dequeue()
+        
+        let operands = testQueue.allItems().map {
+            return $0 as? Double
+        }
+        
+        XCTAssertEqual(operands, [4,5])
+    }
+    
+    func test_operatorsQueueing() {
+        testQueue.enqueue(Operator.add)
+        testQueue.enqueue(Operator.subtract)
+        testQueue.enqueue(Operator.divide)
+        testQueue.enqueue(Operator.multiply)
+        testQueue.dequeue()
+        testQueue.enqueue(Operator.add)
+        
+        let operators = testQueue.allItems().map {
+            return $0 as? Operator
+        }
+        
+        XCTAssertEqual(operators, [.subtract,.divide,.multiply,.add])
+    }
 }
