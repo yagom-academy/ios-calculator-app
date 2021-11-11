2543ae8ee49af74bde85810caa99e63e3db8c3f0
Ari <lee_ari95@icloud.com>
2021-11-11 15:50:05 +0900

    test: ExpressionParser 연산자와 숫자가 세개씩 나오는지 확인하는 case 추가 #3

--- Calculator/CalculatorTests/ExpressionParserTests.swift
+++ Calculator/CalculatorTests/ExpressionParserTests.swift
@@ -59,4 +59,11 @@ class ExpressionParserTests: XCTestCase {
         XCTAssertTrue(input.operators.count == 2)
         XCTAssertTrue(input.operators.count == 2)
     }
+    
+    func test_연산자와숫자가_세개씩() {
+        let input = ExpressionParser.parse(from: "12.123+12+12+")
+        
+        XCTAssertTrue(input.operands.count == 3)
+        XCTAssertTrue(input.operators.count == 3)
+    }
 }
