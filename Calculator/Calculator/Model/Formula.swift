540558ae1aa9d10516a3454c41542859c9f51700
YohanBlessYou <duddn5906@naver.com>
2021-11-11 23:13:12 +0900

    feat: Formula 타입 구현

--- Calculator/Calculator/Model/Formula.swift
+++ Calculator/Calculator/Model/Formula.swift
@@ -4,7 +4,14 @@ struct Formula {
     var operands = CalculatorItemQueue()
     var operators = CalculatorItemQueue()
     
-//    func result() -> Double {
-//        
-//    }
+    mutating func result() -> Double {
+        var result = operands.dequeue() as? Double ?? 0
+        
+        while let `operator` = operators.dequeue() as? Operator,
+              let operand = operands.dequeue() as? Double {
+            result = `operator`.calculate(lhs: result, rhs: operand)
+        }
+        
+        return result
+    }
 }
