fb85ba716a00929631a62d937c39a90195b39e59
ChaminLee <trueman9512@naver.com>
2021-11-11 15:39:18 +0900

    feat: Operator 타입의 케이스에 따라 사칙연산을 실행하는 calculate 메서드 추가

--- Calculator/Calculator/Model/Operator.swift
+++ Calculator/Calculator/Model/Operator.swift
@@ -13,6 +13,19 @@ enum Operator: Character, CaseIterable, CalculateItem {
     case divide = "/"
     case multiply = "*"
     
+    func calculate(lhs: Double, rhs: Double) -> Double {
+        switch self {
+        case .add:
+            return add(lhs: lhs, rhs: rhs)
+        case .subtract:
+            return subtract(lhs: lhs, rhs: rhs)
+        case .divide:
+            return divide(lhs: lhs, rhs: rhs)
+        case .multiply:
+            return multiply(lhs: lhs, rhs: rhs)
+        }    
+    }
+    
     private func add(lhs: Double, rhs: Double) -> Double {
         return lhs + rhs
     }
