c89cc2b75b138863f0961aa2cc4a12c107c22996
Ari <lee_ari95@icloud.com>
2021-11-12 01:27:55 +0900

    refactor: ExpressionParser를 Model group으로 이동 #4

---
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -0,0 +1,25 @@
+//
+//  ExpressionParser.swift
+//  Calculator
+//
+//  Created by Ari on 2021/11/12.
+//
+
+import Foundation
+
+enum ExpressionParser {
+    static func parse(from input: String) -> Formula {
+        let operands = componentsByOperators(from: input)
+                        .compactMap{ Double($0) }
+        let operators = input.map{ $0 }
+                        .compactMap{ Operator(rawValue: $0) }
+        return Formula(operands: CalculatorItemQueue(operands),
+                       operators: CalculatorItemQueue(operators))
+    }
+    
+    private static func componentsByOperators(from input: String) -> [String] {
+        let sliceInputValue = input.split()
+        let operands = sliceInputValue.filter { Double($0.description) != nil }
+        return operands
+    }
+}
--- Calculator/CalculatorTests/ExpressionParserTests.swift
+++ Calculator/CalculatorTests/ExpressionParserTests.swift
@@ -7,24 +7,6 @@
 
 import XCTest
 
-
-enum ExpressionParser {
-    static func parse(from input: String) -> Formula {
-        let operands = componentsByOperators(from: input)
-                        .compactMap{ Double($0) }
-        let operators = input.map{ $0 }
-                        .compactMap{ Operator(rawValue: $0) }
-        return Formula(operands: CalculatorItemQueue(operands),
-                       operators: CalculatorItemQueue(operators))
-    }
-    
-    private static func componentsByOperators(from input: String) -> [String] {
-        let sliceInputValue = input.split()
-        let operands = sliceInputValue.filter { Double($0.description) != nil }
-        return operands
-    }
-}
-
 class ExpressionParserTests: XCTestCase {
 
     func test_연산자와숫자가_한개씩() {
