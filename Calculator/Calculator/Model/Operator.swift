6100b33942f4c79e4007b1df625d70387f06611c
namu <simjh27@gmail.com>
2021-11-11 17:26:25 +0900

    feat: Operator 타입 구현, 덧셈 test 작성

---
+++ Calculator/Calculator/Model/Operator.swift
@@ -0,0 +1,41 @@
+//
+//  Operator.swift
+//  Calculator
+//
+//  Created by Jae-hoon Sim on 2021/11/11.
+//
+
+import Foundation
+
+enum Operator: Character, CaseIterable, CalculateItem {
+    case add = "+"
+    case subtract = "-"
+    case divide = "÷"
+    case multiply = "×"
+    
+    func calculate(lhs: Double, rhs: Double) -> Double {
+        switch self {
+        case .add:
+            return add(lhs: lhs, rhs: rhs)
+        case .subtract:
+            return subtract(lhs: lhs, rhs: rhs)
+        case .divide:
+            return divide(lhs: lhs, rhs: rhs)
+        case .multiply:
+            return multiply(lhs: lhs, rhs: rhs)
+        }
+    }
+    
+    private func add(lhs: Double, rhs: Double) -> Double {
+        lhs + rhs
+    }
+    private func subtract(lhs: Double, rhs: Double) -> Double {
+        lhs - rhs
+    }
+    private func divide(lhs: Double, rhs: Double) -> Double {
+        lhs / rhs
+    }
+    private func multiply(lhs: Double, rhs: Double) -> Double {
+        lhs * rhs
+    }
+}
---
+++ Calculator/OperatorTests/OperatorTests.swift
@@ -0,0 +1,39 @@
+//
+//  OperatorTests.swift
+//  OperatorTests
+//
+//  Created by Jae-hoon Sim on 2021/11/11.
+//
+
+import XCTest
+@testable import Calculator
+
+class OperatorTests: XCTestCase {
+    var sut: Operator!
+    
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+    }
+
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+        sut = nil
+    }
+
+    func test_4와9를더했을때_13이나오는지() {
+        //given
+        sut = Operator.add
+        //when
+        let result = sut.calculate(lhs: 4.0, rhs: 9.0)
+        //then
+        XCTAssertEqual(result, 13.0)
+    }
+    func test_4와마이너스9를더했을때_마이너스5가나오는지() {
+        //given
+        sut = Operator.add
+        //when
+        let result = sut.calculate(lhs: 4.0, rhs: -9.0)
+        //then
+        XCTAssertEqual(result, -5.0)
+    }
+}
