9e47d451f567d002926101c31682c35d54191214
yanghojoon <yanghojoon8487@gmail.com>
2021-11-12 01:18:16 +0900

    test: 연산자 열거형에 대한 테스트 코드 추가 #7

---
+++ Calculator/CalculatorTests/OperatorTests.swift
@@ -0,0 +1,86 @@
+//
+//  OperatorTests.swift
+//  CalculatorTests
+//
+//  Created by 양호준 on 2021/11/11.
+//
+
+import XCTest
+
+class OperatorTests: XCTestCase {
+    var add: Operator!
+    var subtract: Operator!
+    var divide: Operator!
+    var multiply: Operator!
+
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        add = Operator.add
+        subtract = Operator.subtract
+        divide = Operator.divide
+        multiply = Operator.multiply
+    }
+
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+        add = nil
+        subtract = nil
+        divide = nil
+        multiply = nil
+    }
+    
+    func test_더하기일때_덧셈이_잘_계산되는지() {
+        let value = add.calculate(lhs: 2, rhs: 4)
+        XCTAssertEqual(value, 6)
+    }
+    
+    func test_더하기일때_음수가_있어도_덧셈이_잘_계산되는지() {
+        let value = add.calculate(lhs: -2, rhs: 7)
+        XCTAssertEqual(value, 5)
+    }
+    
+    func test_소수의_덧셈도_잘_계산되는지() {
+        let value = add.calculate(lhs: 5.5, rhs: 4.5)
+        XCTAssertEqual(value, 10)
+    }
+    
+    func test_빼기일때_뺄셈이_잘_계산되는지() {
+        let value = subtract.calculate(lhs: 10, rhs: 5)
+        XCTAssertEqual(value, 5)
+    }
+    
+    func test_빼기일때_음수가_있어도_뺄셈이_잘_계산되는지() {
+        let value = subtract.calculate(lhs: -2, rhs: 5)
+        XCTAssertEqual(value, -7)
+    }
+    
+    func test_소수의_뺄셈도_잘_계산되는지() {
+        let value = subtract.calculate(lhs: 5.5, rhs: 4.5)
+        XCTAssertEqual(value, 1)
+    }
+    
+    func test_나누기일때_나눗셈이_잘_계산되는지() {
+        let value = divide.calculate(lhs: 10, rhs: 5)
+        XCTAssertEqual(value, 2)
+    }
+    
+    func test_소수의_나눗셈도_잘_계산되는지() {
+        let value = divide.calculate(lhs: 5.5, rhs: 5.5)
+        XCTAssertEqual(value, 1)
+    }
+    
+    func test_나누기일때_결과가_소수인_나눗셈도_잘_계산되는지() {
+        let value = divide.calculate(lhs: 5, rhs: 10)
+        XCTAssertEqual(value, 0.5)
+    }
+    
+    func test_곱하기일때_곱셈이_잘_계산되는지() {
+        let value = multiply.calculate(lhs: 2, rhs: 5)
+        XCTAssertEqual(value, 10)
+    }
+    
+    func test_소수의_곱셈도_잘_계산되는지() {
+        let value = multiply.calculate(lhs: 5, rhs: 0.5)
+        XCTAssertEqual(value, 2.5)
+    }
+}
