61609e6b57b9b29759b99e00116af461a6460ba2
SIWONKIM <siyeon0616@gmail.com>
2021-11-11 14:45:12 +0900

    chore: 테스트 코드에 given, when, then 주석 추가

--- Calculator/CalculatorItemQueueUnitTest/CalculatorItemQueueUnitTest.swift
+++ Calculator/CalculatorItemQueueUnitTest/CalculatorItemQueueUnitTest.swift
@@ -21,53 +21,75 @@ class CalculatorItemQueueUnitTest: XCTestCase {
     }
 
     func test_빈_큐에_1을_push하면_1이_남는다() {
+        //given
         let input: Character = "1"
-        sut.enqueue(input)
         
+        //when
+        sut.enqueue(input)
         guard let result = sut.dequeue() else { return }
+        
+        //then
         XCTAssertEqual(result, "1")
     }
     
     func test_빈_큐에_1과_2를_enqueue하면_1과_2가_남는다() {
+        //given
         let firstInput: Character = "1"
-        sut.enqueue(firstInput)
         let secondInput: Character = "2"
-        sut.enqueue(secondInput)
         
+        //when
+        sut.enqueue(firstInput)
+        sut.enqueue(secondInput)
         guard let firstResult = sut.dequeue() else { return }
         guard let secondResult = sut.dequeue() else { return }
         
+        //then
         XCTAssertEqual(firstResult, "1")
         XCTAssertEqual(secondResult, "2")
     }
     
     func test_빈_큐에_더하기를_push하면_더하기가_남는다() {
+        //given
         let input: Character = "+"
+        
+        //when
         sut.enqueue(input)
         guard let result = sut.dequeue() else { return }
-
+        
+        //then
         XCTAssertEqual(result, "+")
     }
     
     func test_1과_2와_3이_있는_큐를_pop하면_1이_나온다() {
+        //given
         let firstInput: Character = "1"
-        sut.enqueue(firstInput)
         let secondInput: Character = "2"
-        sut.enqueue(secondInput)
         let thirdInput: Character = "3"
-        sut.enqueue(thirdInput)
         
+        //when
+        sut.enqueue(firstInput)
+        sut.enqueue(secondInput)
+        sut.enqueue(thirdInput)
         guard let result = sut.dequeue() else { return }
         
+        //then
         XCTAssertEqual(result, "1")
     }
     
     func test_빈_큐에_pop을_하면_에러가_난다() {
+        //when
         let result = sut.dequeue()
+        
+        //then
         XCTAssertNil(result)
     }
 
     func test_1과_2가_있는_큐의_모든_요소를_제거하면_빈_큐가_된다() {
+        //when
         sut.removeAll()
+        
+        //then
+        let result = sut.dequeue()
+        XCTAssertNil(result)
     }
 }
