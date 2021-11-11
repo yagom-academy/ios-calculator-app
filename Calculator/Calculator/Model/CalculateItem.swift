4310c90dce3fdedfd537973b1072b56117b8e46f
SIWONKIM <siyeon0616@gmail.com>
2021-11-12 01:04:35 +0900

    feat: CalculateItem 프로토콜을 채택하는 타입 생성
    Double을 확장하고 Operator 열거형에 CalculateItem을 채택하여 계산기의 피연산자와 연산자의 타입을 생성해주었고 그에 맞게 테스트 코드 수정

--- Calculator/Calculator/Model/CalculateItem.swift
+++ Calculator/Calculator/Model/CalculateItem.swift
@@ -4,6 +4,33 @@ protocol CalculateItem {
     
 }
 
-extension Character: CalculateItem {
+extension Double: CalculateItem {
     
+}
+
+enum Operator: Character, CaseIterable, CalculateItem {
+    case add = "+"
+    case subtract = "-"
+    case divide = "/"
+    case multiply = "*"
+    
+    func calculate() -> Double {
+        return 0.1
+    }
+    
+    func add() -> Double {
+        return 0.1
+    }
+    
+    func subtract() -> Double {
+        return 0.1
+    }
+    
+    func divide() -> Double {
+        return 0.1
+    }
+    
+    func multiply() -> Double {
+        return 0.1
+    }
 }
