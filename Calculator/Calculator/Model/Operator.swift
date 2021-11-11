58d229b08353fbd95c602469e62ae618c9960145
ChaminLee <trueman9512@naver.com>
2021-11-11 15:34:11 +0900

    feat: Operator 타입 추가
    - add, subtract, divide, multiply 케이스 및 메서드 추가
    - divide 메서드의 경우 0으로 나누는 케이스를 대비해 .nan 리턴하도록 구현

---
+++ Calculator/Calculator/Model/Operator.swift
@@ -0,0 +1,35 @@
+//
+//  Operator.swift
+//  Calculator
+//
+//  Created by 이차민 on 2021/11/11.
+//
+
+import Foundation
+
+enum Operator: Character, CaseIterable, CalculateItem {
+    case add = "+"
+    case subtract = "-"
+    case divide = "/"
+    case multiply = "*"
+    
+    private func add(lhs: Double, rhs: Double) -> Double {
+        return lhs + rhs
+    }
+    
+    private func subtract(lhs: Double, rhs: Double) -> Double {
+        return lhs - rhs
+    }
+    
+    private func divide(lhs: Double, rhs: Double) -> Double {
+        if rhs == 0.0 {
+            return .nan
+        }
+        
+        return lhs / rhs
+    }
+    
+    private func multiply(lhs: Double, rhs: Double) -> Double {
+        return lhs * rhs
+    }
+}
