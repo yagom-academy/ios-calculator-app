de7f5dac2bb82d6d8b9449992f02def191f2a88e
just1103 <hyojuson1103@gmail.com>
2021-11-11 13:26:33 +0900

    test: LinkedList 테스트 코드 네이밍 수정 #2

--- Calculator/CalculatorTests/CalculatorItemQueueTests.swift
+++ Calculator/CalculatorTests/CalculatorItemQueueTests.swift
@@ -1,4 +1,9 @@
 import XCTest
 
 class CalculatorItemQueueTests: XCTestCase {
+    func test_빈_큐에_1을_append하면_1이_남는다() {
+        let linkedList = LinkedList<Double>(head: nil)
+        linkedList.append(value: 1)
+        XCTAssertEqual(linkedList.scanAllValue(), [1])
+    }
 }
