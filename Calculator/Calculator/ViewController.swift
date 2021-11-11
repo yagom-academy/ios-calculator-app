ee78813c52171e9433386753f5c7c29d4bd2c701
junbangg <wnstjr4620@naver.com>
2021-11-11 17:29:48 +0900

    chore: 실수로 남긴 코드 제거

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -8,7 +8,6 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var linkedList = LinkedList<Any>()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
