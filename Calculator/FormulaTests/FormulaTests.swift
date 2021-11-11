e989f92c378135de0c7c8494304e8f86c2bd969c
hwangjeha <hyhpwang@gmail.com>
2021-11-11 18:34:38 +0900

    feat: Formula 구조체, FormulaTests 타겟 추가

---
+++ Calculator/FormulaTests/FormulaTests.swift
@@ -0,0 +1,19 @@
+//
+//  FormulaTests.swift
+//  FormulaTests
+//
+//  Created by 황제하 on 2021/11/11.
+//
+
+import XCTest
+
+class FormulaTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        
+    }
+
+    override func tearDownWithError() throws {
+        
+    }
+}
---
+++ Calculator/Model/Formula.swift
@@ -0,0 +1,17 @@
+//
+//  Formula.swift
+//  Calculator
+//
+//  Created by 황제하 on 2021/11/11.
+//
+
+import Foundation
+
+struct Formula {
+    let operands: CalculatorItemQueue
+    let operators: CalculatorItemQueue
+    
+    func result() -> Double {
+        return 0
+    }
+}
