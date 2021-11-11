eb1d52f9670f4be37174419755191631973654ed
yim2627 <yim2627@gmail.com>
2021-11-11 19:48:45 +0900

    feat: Operator 열거형 내부 케이스 및 메소드 구현 #2
    UML에 맞춰 각 케이스와 메소드 구현
    아직 완벽 구현 X

--- Calculator/Calculator/Model/Operator.swift
+++ Calculator/Calculator/Model/Operator.swift
@@ -8,6 +8,29 @@
 import Foundation
 
 enum Operator: Character, CaseIterable {
+    case add = "+"
+    case substract = "-"
+    case divide = "/"
+    case multiply = "×"
 
+    func calculate(lhs: Double, rhs: Double) -> Double {
+        return 0.0
+    }
+
+    private func add(lhs: Double, rhs: Double) -> Double {
+        return 0.0
+    }
+
+    private func substract(lhs: Double, rhs: Double) -> Double {
+        return 0.0
+    }
+
+    private func divide(lhs: Double, rhs: Double) -> Double {
+        return 0.0
+    }
+
+    private func multiply(lhs: Double, rhs: Double) -> Double {
+        return 0.0
+    }
 }
 
