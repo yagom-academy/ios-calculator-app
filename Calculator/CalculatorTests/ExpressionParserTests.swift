e2a83e641f3ccfebfe2f8ac9970a6eb19e8212ca
forestjae <forestsj30@gmail.com>
2021-11-11 10:44:45 +0900

    test: ExpressionParser 테스트 메소드 추가

--- Calculator/CalculatorTests/ExpressionParserTests.swift
+++ Calculator/CalculatorTests/ExpressionParserTests.swift
@@ -9,6 +9,8 @@ import XCTest
 
 class ExpressionParserTests: XCTestCase {
 
+    var sampleString: String = ""
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -17,6 +19,28 @@ class ExpressionParserTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
+    func testParsingFromString() {
+        sampleString = "1.7+3.8-21.9/41.0*310.7"
+        var formula: Formula = ExpressionParser.parase(from: sampleString)
+        do {
+            let result = try formula.result()
+            XCTAssertEqual(result, ((1.7 + 3.8) - 21.9) / 41.0 * 310.7 )
+        } catch let error {
+            print(error.localizedDescription)
+            XCTFail()
+        }
+    }
+    
+    func testParsingFromInvaildString() {
+        sampleString = "+1.7+3.8-21.9/41.0**310.7"
+        var formula: Formula = ExpressionParser.parase(from: sampleString)
+        do {
+            let result = try formula.result()
+            XCTAssertEqual(result, ((1.7 + 3.8) - 21.9) / 41.0 * 310.7 )
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
     
 
 }
