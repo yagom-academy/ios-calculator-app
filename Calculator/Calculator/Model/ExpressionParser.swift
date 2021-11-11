605a441ecf609872ddac6e65a881ddadd2d3dffe
forestjae <forestsj30@gmail.com>
2021-11-11 17:40:38 +0900

    fix: parse함수가 negative값에서 subtract연산자를 가져오는 문제 수정

--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -8,12 +8,12 @@
 import Foundation
 
 enum ExpressionParser {
-    static let operatorSet = Operator.allCases.map({ String($0.rawValue) }).reduce("", { $0 + $1 })
+    static let operatorSet = Operator.allCases.map({ String($0.rawValue) })
     
     static func parse(from input: String) -> Formula {
         let operationComponents: [String] = ExpressionParser.componentsByOperators(from: input)
         let operands = operationComponents.compactMap { convertToDouble(from:$0) }
-        let operators = operationComponents.reduce("", { $0 + $1 } ).compactMap { Operator(rawValue: $0) }
+        let operators = operationComponents.filter { operatorSet.contains($0) }.reduce("", { $0 + $1 }).compactMap { Operator(rawValue: $0) }
         
         var operandsQueue = CalculatorItemQueue<Double>()
         var operatorsQueue = CalculatorItemQueue<Operator>()
