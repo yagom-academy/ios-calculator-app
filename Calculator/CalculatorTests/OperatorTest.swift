389ad2f9ac835ba93d023126191ba2b8fe19cded
ChaminLee <trueman9512@naver.com>
2021-11-11 15:29:34 +0900

    test: Operator 테스트 코드 추가
    - 덧셈, 곱셈, 뺄셈, 니눗셈에 대해 양의 실수끼리, 양/음의 실수, 음의 실수끼리의 연산을 테스트
    - 나눗셈의 경우 0으로 나누는 경우 NaN이 나오는지 테스트하는 메서드 추가

---
+++ Calculator/CalculatorTests/OperatorTest.swift
@@ -0,0 +1,127 @@
+//
+//  OperatorTest.swift
+//  CalculatorTests
+//
+//  Created by 이차민 on 2021/11/11.
+//
+
+import XCTest
+
+class OperatorTest: XCTestCase {
+    func test_양의_실수_두_개를_더하는_경우_정상적으로_연산되는지() {
+        guard let calculator = Operator(rawValue: "+") else {
+            return
+        }
+        
+        let result = calculator.calculate(lhs: 3.0, rhs: 4.5)
+        XCTAssertEqual(7.5, result)
+    }
+    
+    func test_양의_실수와_음의_실수를_더하는_경우_정상적으로_연산되는지() {
+        guard let calculator = Operator(rawValue: "+") else {
+            return
+        }
+        
+        let result = calculator.calculate(lhs: 3.0, rhs: -4.5)
+        XCTAssertEqual(-1.5, result)
+    }
+    
+    func test_음의_실수와_음의_실수를_더하는_경우_정상적으로_연산되는지() {
+        guard let calculator = Operator(rawValue: "+") else {
+            return
+        }
+        
+        let result = calculator.calculate(lhs: -3.0, rhs: -4.5)
+        XCTAssertEqual(-7.5, result)
+    }
+    
+    func test_양의_실수_두_개를_빼는_경우_정상적으로_연산되는지() {
+        guard let calculator = Operator(rawValue: "-") else {
+            return
+        }
+        
+        let result = calculator.calculate(lhs: 3.0, rhs: 4.5)
+        XCTAssertEqual(-1.5, result)
+    }
+    
+    func test_양의_실수와_음의_실수를_빼는_경우_정상적으로_연산되는지() {
+        guard let calculator = Operator(rawValue: "-") else {
+            return
+        }
+        
+        let result = calculator.calculate(lhs: 3.0, rhs: -4.5)
+        XCTAssertEqual(7.5, result)
+    }
+    
+    func test_음의_실수와_음의_실수를_빼는_경우_정상적으로_연산되는지() {
+        guard let calculator = Operator(rawValue: "-") else {
+            return
+        }
+        
+        let result = calculator.calculate(lhs: -3.0, rhs: -4.5)
+        XCTAssertEqual(1.5, result)
+    }
+    
+    func test_양의_실수_두_개를_곱하는_경우_정상적으로_연산되는지() {
+        guard let calculator = Operator(rawValue: "*") else {
+            return
+        }
+        
+        let result = calculator.calculate(lhs: 3.0, rhs: 4.0)
+        XCTAssertEqual(12.0, result)
+    }
+    
+    func test_양의_실수와_음의_실수를_곱하는_경우_정상적으로_연산되는지() {
+        guard let calculator = Operator(rawValue: "*") else {
+            return
+        }
+        
+        let result = calculator.calculate(lhs: 3.0, rhs: -4.0)
+        XCTAssertEqual(-12.0, result)
+    }
+    
+    func test_음의_실수와_음의_실수를_곱하는_경우_정상적으로_연산되는지() {
+        guard let calculator = Operator(rawValue: "*") else {
+            return
+        }
+        
+        let result = calculator.calculate(lhs: -3.0, rhs: -4.0)
+        XCTAssertEqual(12.0, result)
+    }
+    
+    func test_양의_우측_값이_양의_좌측_값을_나누는_경우_정상적으로_연산되는지() {
+        guard let calculator = Operator(rawValue: "/") else {
+            return
+        }
+        
+        let result = try calculator.calculate(lhs: 3.0, rhs: 1.5)
+        XCTAssertEqual(2.0, result)
+    }
+    
+    func test_양의_우측_값이_음의_좌측_값을_나누는_경우_정상적으로_연산되는지() {
+        guard let calculator = Operator(rawValue: "/") else {
+            return
+        }
+        
+        let result = calculator.calculate(lhs: -3.0, rhs: 1.5)
+        XCTAssertEqual(-2.0, result)
+    }
+    
+    func test_음의_우측_값이_음의_좌측_값을_나누는_경우_정상적으로_연산되는지() {
+        guard let calculator = Operator(rawValue: "/") else {
+            return
+        }
+        
+        let result = calculator.calculate(lhs: -3.0, rhs: -1.5)
+        XCTAssertEqual(2.0, result)
+    }
+    
+    func test_0으로_값을_나누는_경우_값이_nan인지g() {
+        guard let calculator = Operator(rawValue: "/") else {
+            return
+        }
+        
+        let result = calculator.calculate(lhs: -3.0, rhs: 0.0)
+        XCTAssertTrue(result.isNaN)
+    }
+}
