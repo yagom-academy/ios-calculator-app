11cfece3381185add62bff22dc2ec6d75aaaaf68
SIWONKIM <siyeon0616@gmail.com>
2021-11-12 11:10:33 +0900

    feat: Formula, ExpressionParser 타입 추가 #1

--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -1,1 +1,12 @@
 import Foundation
+
+enum ExpressionParser {
+    func parse(from input: String) -> Formula {
+        return Formula()
+    }
+    
+    func componentsByOperators(from input: String) -> [String] {
+        return ["aa"]
+    }
+}
+
--- Calculator/Calculator/Model/Formula.swift
+++ Calculator/Calculator/Model/Formula.swift
@@ -1,1 +1,10 @@
 import Foundation
+
+struct Formula {
+    var operands: CalculatorItemQueue<Double> = CalculatorItemQueue<Double>()
+    var operators: CalculatorItemQueue<Operator> = CalculatorItemQueue<Operator>()
+    
+    func result() -> Double {
+        return 0.1
+    }
+}
