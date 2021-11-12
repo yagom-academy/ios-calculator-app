743494c42ce2f254f85b7e4e9ec93fadb04ee060
SIWONKIM <siyeon0616@gmail.com>
2021-11-12 13:56:03 +0900

    feat: 연산자와 피연산자를 이용해 연산을 하는 Formula 구조체의 result() 메서드 초기 구현

--- Calculator/Calculator/Model/Formula.swift
+++ Calculator/Calculator/Model/Formula.swift
@@ -4,7 +4,13 @@ struct Formula {
     var operands: CalculatorItemQueue<Double> = CalculatorItemQueue<Double>()
     var operators: CalculatorItemQueue<Operator> = CalculatorItemQueue<Operator>()
     
-    func result() -> Double {
-        return 9
+    mutating func result() -> Double? {
+        //Operator의 연산자 함수들을 사용하여 계산결과 도출하기
+        guard let lhs = operands.dequeue() else { return nil }
+        guard let rhs = operands.dequeue() else { return nil }
+        let `operator` = operators.dequeue()
+        let calculateResult = `operator`?.calculate(lhs: lhs, rhs: rhs)
+        
+        return calculateResult
     }
 }
--- Calculator/CalculatorItemQueueUnitTest/FormulaUnitTest.swift
+++ Calculator/CalculatorItemQueueUnitTest/FormulaUnitTest.swift
@@ -10,14 +10,13 @@ class FormulaUnitTest: XCTestCase {
         sut = nil
     }
 
-    func test_2더하기_3더하기_4의_결과는_9가_나온다() {
+    func test_2더하기_3의_결과는_5가_나온다() {
         //given
         var numbers = CalculatorItemQueue<Double>()
         var operators = CalculatorItemQueue<Operator>()
         numbers.enqueue(2)
         numbers.enqueue(3)
-        numbers.enqueue(4)
-        operators.enqueue(.add)
+
         operators.enqueue(.add)
         
         var firstFormula = Formula(operands: numbers, operators: operators)
@@ -26,7 +25,7 @@ class FormulaUnitTest: XCTestCase {
         let result = firstFormula.result()
         
         //then
-        XCTAssertEqual(result, 9)
+        XCTAssertEqual(result, 5)
     }
 
 }
