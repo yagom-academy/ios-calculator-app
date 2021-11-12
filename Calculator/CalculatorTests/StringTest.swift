0cf18eb1d73458395adb89b7bf86f6b49a41a1d5
ChaminLee <trueman9512@naver.com>
2021-11-12 11:29:23 +0900

    test: String 테스트 코드 수정 및 케이스 추가

--- Calculator/CalculatorTests/StringTest.swift
+++ Calculator/CalculatorTests/StringTest.swift
@@ -9,7 +9,12 @@ import XCTest
 
 class StringTest: XCTestCase {
     func test_공백을_기준으로_피연샨자와_연산자가_잘_나뉘는지() {
-        var testString = "1.0 + 32.4 - -3.2 / 4.0 * 7.2"
+        let testString = "1.0 + 32.4 - -3.2 / 4.0 * 7.2"
         XCTAssertEqual(["1.0","+","32.4","-","-3.2","/","4.0","*","7.2"], testString.split(with: " "))
+    }
+    
+    func test_쉼표를_기준으로_피연샨자와_연산자가_잘_나뉘는지() {
+        let testString = "1.0,+,32.4,-,-3.2,/,4.0,*,7.2"
+        XCTAssertEqual(["1.0","+","32.4","-","-3.2","/","4.0","*","7.2"], testString.split(with: ","))
     }
 }
