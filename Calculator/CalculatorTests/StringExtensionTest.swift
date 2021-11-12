6420211e989c70497d280128b660366dfa3d6d39
ICS-Asan <carpediem1015@gmail.com>
2021-11-13 01:41:03 +0900

    chore: StringExtensionTest 테스트명 _누락부분 추가 #9

--- Calculator/CalculatorTests/StringExtensionTest.swift
+++ Calculator/CalculatorTests/StringExtensionTest.swift
@@ -9,25 +9,25 @@ import XCTest
 
 class StringExtensionTest: XCTestCase {
     
-    func test_add연산자_1개_정수2개_숫자분리 {
+    func test_add연산자_1개_정수_2개_숫자분리 {
         let testString = "12+15"
         let result = testString.split(with: Operator.add.rawValue)
         XCTAssertEqual(result, ["12", "15"])
     }
     
-    func test_subtract연산자_1개_정수2개_숫자분리 {
+    func test_subtract연산자_1개_정수_2개_숫자분리 {
         let testString = "12_15"
         let result = testString.split(with: Operator.subtract.rawValue)
         XCTAssertEqual(result, ["12", "15"])
     }
     
-    func test_divide연산자_1개_정수2개_숫자분리 {
+    func test_divide연산자_1개_정수_2개_숫자분리 {
         let testString = "12/15"
         let result = testString.split(with: Operator.divide.rawValue)
         XCTAssertEqual(result, ["12", "15"])
     }
     
-    func test_multiply연산자_1개_정수2개_숫자분리 {
+    func test_multiply연산자_1개_정수_2개_숫자분리 {
         let testString = "12×15"
         let result = testString.split(with: Operator.multiply.rawValue)
         XCTAssertEqual(result, ["12", "15"])
