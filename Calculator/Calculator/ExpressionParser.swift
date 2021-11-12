514af8321afdd27a278024e055419a8497bde491
horeng2 <huski.a.a@gmail.com>
2021-11-13 01:38:06 +0900

    feat: 테스트코드 분리 및 수정

--- Calculator/Calculator/ExpressionParser.swift
+++ Calculator/Calculator/ExpressionParser.swift
@@ -1,23 +1,23 @@
 import Foundation
 
 enum ExpressionParser {
-    func parse(from input: String) -> Formula {
+    static func parse(from input: String) -> Formula {
         let operatorEnumArray = Operator.allCases.map { String($0.rawValue) }
         let inputCharacters = Array(input).map { String($0) }
         let inputOperator = inputCharacters.filter { operatorEnumArray.contains($0) == true }
         let inputOperands = componentsByOperators(from: input)
         
         let formula = Formula()
-        for operandIndex in 0...inputOperands.count {
+        for operandIndex in 0...inputOperands.count - 1 {
             formula.operands.insertToQueue(Double(inputOperands[operandIndex]) ?? 0)
         }
-        for operatorIndex in 0...inputOperator.count {
+        for operatorIndex in 0...inputOperator.count - 1 {
             formula.operators.insertToQueue(Character(inputOperator[operatorIndex]))
         }
         return formula
     }
 
-    private func componentsByOperators(from input: String) -> [String] {
+    private static func componentsByOperators(from input: String) -> [String] {
         var splitedWithTarget: [String]
         var joinedString = input
         for operatorCase in Operator.allCases {
--- Calculator/CalculatorTests/CalculatorTests.swift
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -2,11 +2,41 @@ import XCTest
 
 class CalculatorTests: XCTestCase {
 
-    func test_빈큐에_delete를_하면_에러발생() {
-        let cal = CalculatorItemQueue<Double>()
-        try? cal.deleteFromQueue()
-        XCTAssertNoThrow(ErrorCase.emptyQueue)
+    func test_빈_큐에_1을_put하면_1이_남는다() {
+        let queue = CalculatorItemQueue<Int>()
+        queue.insertToQueue(1)
+        XCTAssertEqual(queue.linkedList.head?.nodeValue, 1)
+        XCTAssertNil(queue.linkedList.head?.pointer)
     }
-    
-    
+
+    func test_1이_있는_큐에_2를_put하면_1과_2가_남는다() {
+        let queue = CalculatorItemQueue<Int>()
+        queue.linkedList.head = Node(1)
+        queue.insertToQueue(2)
+        XCTAssertEqual(queue.linkedList.head?.nodeValue, 1)
+        XCTAssertEqual(queue.linkedList.head?.pointer?.nodeValue, 2)
+        XCTAssertNil(queue.linkedList.head?.pointer?.pointer)
+    }
+
+    func test_1만_있는_큐에_get을_하면_빈_큐가_된다() {
+        let queue = CalculatorItemQueue<Int>()
+        queue.linkedList.head = Node(1)
+        try! queue.deleteFromQueue()
+        XCTAssertNil(queue.linkedList.head?.nodeValue)
+    }
+    func test_1과_2가_있는_큐에_get을_하면_2가_남는다() {
+        let queue = CalculatorItemQueue<Int>()
+        queue.linkedList.head = Node(1)
+        queue.linkedList.head?.pointer = Node(2)
+        try! queue.deleteFromQueue()
+        XCTAssertEqual(queue.linkedList.head?.nodeValue, 2)
+        XCTAssertNil(queue.linkedList.head?.pointer)
+    }
+
+    func test_빈큐에_deleteFromQueue하면_emptyQueue오류발생() {
+        let queue = CalculatorItemQueue<Int>()
+        XCTAssertNil(queue.linkedList.head?.nodeValue)
+        try? queue.deleteFromQueue()
+        XCTAssertThrowsError(ErrorCase.emptyQueue)
+    }
 }
