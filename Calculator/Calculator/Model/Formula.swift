7a8dede564ffa40eff3c7f869b39110b81a58121
namu <simjh27@gmail.com>
2021-11-12 17:41:41 +0900

    feat: Formula 타입 프로퍼티,메서드 작성

--- Calculator/Calculator/Model/Formula.swift
+++ Calculator/Calculator/Model/Formula.swift
@@ -8,5 +8,10 @@
 import Foundation
 
 struct Formula {
+    var operands: CalculatorItemQueue<Double>
+    var operators: CalculatorItemQueue<Operator>
     
+    func result() -> Double {
+        return Double()
+    }
 }
