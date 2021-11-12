6409844e98440fcebbe61004147f6104b41ca34c
Kim-EunsooSilver <graysilver18@naver.com>
2021-11-12 17:59:20 +0900

    test: 프로젝트 명세 중 요구사항 케이스 작성

--- Calculator/CalculatorTests/FormulaTests.swift
+++ Calculator/CalculatorTests/FormulaTests.swift
@@ -22,4 +22,72 @@ class FormulaTests: XCTestCase {
         
         XCTAssertEqual(3.0, calculationResult)
     }
+    
+    func test_1_더하기_2_더하기_3의_결과가_6인가() {
+        let formula = Formula()
+        
+        formula.operands.enqueue(1.0)
+        formula.operands.enqueue(2.0)
+        formula.operands.enqueue(3.0)
+        formula.operators.enqueue(Operator.add)
+        formula.operators.enqueue(Operator.add)
+        
+        let calculationResult = formula.result()
+        
+        XCTAssertEqual(6.0, calculationResult)
+    }
+    
+    func test_2_더하기_3_곱하기_3_빼기_1의_결과가_14인가() {
+        let formula = Formula()
+        
+        formula.operands.enqueue(2.0)
+        formula.operands.enqueue(3.0)
+        formula.operands.enqueue(3.0)
+        formula.operands.enqueue(1.0)
+        formula.operators.enqueue(Operator.add)
+        formula.operators.enqueue(Operator.multiply)
+        formula.operators.enqueue(Operator.substract)
+        
+        let calculationResult = formula.result()
+        
+        XCTAssertEqual(14.0, calculationResult)
+    }
+    
+    func test_3_나누기_3_더하기_2_빼기_1의_결과가_2인가() {
+        let formula = Formula()
+        
+        formula.operands.enqueue(3.0)
+        formula.operands.enqueue(3.0)
+        formula.operands.enqueue(2.0)
+        formula.operands.enqueue(1.0)
+        formula.operators.enqueue(Operator.divide)
+        formula.operators.enqueue(Operator.add)
+        formula.operators.enqueue(Operator.substract)
+        
+        let calculationResult = formula.result()
+        
+        XCTAssertEqual(2.0, calculationResult)
+    }
+    
+    func test_1_더하기_2_빼기_3_곱하기_2_빼기_3_나누기_6의_결과가_마이너스0점5인가() {
+        let formula = Formula()
+        
+        formula.operands.enqueue(1.0)
+        formula.operands.enqueue(2.0)
+        formula.operands.enqueue(3.0)
+        formula.operands.enqueue(2.0)
+        formula.operands.enqueue(3.0)
+        formula.operands.enqueue(6.0)
+        formula.operators.enqueue(Operator.add)
+        formula.operators.enqueue(Operator.substract)
+        formula.operators.enqueue(Operator.multiply)
+        formula.operators.enqueue(Operator.substract)
+        formula.operators.enqueue(Operator.divide)
+        
+        
+        let calculationResult = formula.result()
+        
+        XCTAssertEqual(-0.5, calculationResult)
+    }
+    
 }
