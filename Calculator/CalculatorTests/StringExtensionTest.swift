5d64d3b8cb6414e326328684ee90e88131ace177
ICS-Asan <carpediem1015@gmail.com>
2021-11-13 02:37:28 +0900

    chore: StringExtensionTest 테스트 호출시 ()누락부분 추가 #9

--- Calculator/CalculatorTests/StringExtensionTest.swift
+++ Calculator/CalculatorTests/StringExtensionTest.swift
@@ -9,73 +9,73 @@ import XCTest
 
 class StringExtensionTest: XCTestCase {
     
-    func test_add연산자_1개_정수_2개_숫자분리 {
+    func test_add연산자_1개_정수_2개_숫자분리() {
         let testString = "12+15"
         let result = testString.split(with: Operator.add.rawValue)
         XCTAssertEqual(result, ["12", "15"])
     }
     
-    func test_subtract연산자_1개_정수_2개_숫자분리 {
+    func test_subtract연산자_1개_정수_2개_숫자분리() {
         let testString = "12_15"
         let result = testString.split(with: Operator.subtract.rawValue)
         XCTAssertEqual(result, ["12", "15"])
     }
     
-    func test_divide연산자_1개_정수_2개_숫자분리 {
+    func test_divide연산자_1개_정수_2개_숫자분리() {
         let testString = "12/15"
         let result = testString.split(with: Operator.divide.rawValue)
         XCTAssertEqual(result, ["12", "15"])
     }
     
-    func test_multiply연산자_1개_정수_2개_숫자분리 {
+    func test_multiply연산자_1개_정수_2개_숫자분리() {
         let testString = "12×15"
         let result = testString.split(with: Operator.multiply.rawValue)
         XCTAssertEqual(result, ["12", "15"])
     }
     
-    func test_add연산자_2개_정수_3개_숫자분리 {
+    func test_add연산자_2개_정수_3개_숫자분리() {
         let testString = "12+15+23"
         let result = testString.split(with: Operator.add.rawValue)
         XCTAssertEqual(result, ["12", "15", "23"])
     }
     
-    func test_subtract연산자_2개_정수_3개_숫자분리 {
+    func test_subtract연산자_2개_정수_3개_숫자분리() {
         let testString = "12_15_23"
         let result = testString.split(with: Operator.subtract.rawValue)
         XCTAssertEqual(result, ["12", "15", "23"])
     }
     
-    func test_divide연산자_2개_정수_3개_숫자분리 {
+    func test_divide연산자_2개_정수_3개_숫자분리() {
         let testString = "12/15/23"
         let result = testString.split(with: Operator.divide.rawValue)
         XCTAssertEqual(result, ["12", "15", "23"])
     }
     
-    func test_multiply연산자_2개_정수_3개_숫자분리 {
+    func test_multiply연산자_2개_정수_3개_숫자분리() {
         let testString = "12×15×23"
         let result = testString.split(with: Operator.multiply.rawValue)
         XCTAssertEqual(result, ["12", "15", "23"])
     }
     
-    func test_add연산자_1개_실수_1개_음수_1개_숫자분리 {
+    func test_add연산자_1개_실수_1개_음수_1개_숫자분리() {
         let testString = "1.2+-15"
         let result = testString.split(with: Operator.add.rawValue)
         XCTAssertEqual(result, ["1.2", "-15"])
     }
     
-    func test_subtract연산자_1개_실수_1개_음수_1개_숫자분리 {
+    func test_subtract연산자_1개_실수_1개_음수_1개_숫자분리() {
         let testString = "1.2_-15"
         let result = testString.split(with: Operator.subtract.rawValue)
         XCTAssertEqual(result, ["1.2", "-15"])
     }
     
-    func test_divide연산자_1개_실수_1개_음수_1개_숫자분리 {
+    func test_divide연산자_1개_실수_1개_음수_1개_숫자분리() {
         let testString = "1.2/-15"
         let result = testString.split(with: Operator.divide.rawValue)
         XCTAssertEqual(result, ["1.2", "-15"])
     }
     
-    func test_multiply연산자_1개_실수_1개_음수_1개_숫자분리 {
+    func test_multiply연산자_1개_실수_1개_음수_1개_숫자분리() {
         let testString = "1.2×-15"
         let result = testString.split(with: Operator.multiply.rawValue)
         XCTAssertEqual(result, ["1.2", "-15"])
