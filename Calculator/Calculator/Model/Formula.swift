1064ff668062b16d1fe4e8549cf07c394a149d38
Kim-EunsooSilver <graysilver18@naver.com>
2021-11-12 17:57:39 +0900

    feat: Formula operators, operands, result() 구현

--- Calculator/Calculator/Model/Formula.swift
+++ Calculator/Calculator/Model/Formula.swift
@@ -8,5 +8,27 @@
 import Foundation
 
 struct Formula {
+    var operands = CalculatorItemQueue<Double>()
+    var operators = CalculatorItemQueue<Operator>()
+    
+    
+    func result() -> Double {
+        
+        var result: Double = 0.0
+        
+        guard let initialValue = operands.dequeue() else {
+            return 0.0
+        }
+        
+        result = result + initialValue
+        
+        while operands.queue.first != nil {
+            result = operators.dequeue()?.calculate(lhs: result,
+                                                     rhs: operands.dequeue() ?? 0.0) ?? 0.0
+        }
+        
+        return result
+        
+    }
     
 }
