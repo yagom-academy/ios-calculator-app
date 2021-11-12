284f7749975cec2ea7327579f701dd85648795f8
yanghojoon <yanghojoon8487@gmail.com>
2021-11-12 16:01:45 +0900

    test: Formula 구조체의 테스트 코드 추가 #7 - 덧셈이 제대로 작동하는지 구현

--- Calculator/CalculatorTests/FormulaTests.swift
+++ Calculator/CalculatorTests/FormulaTests.swift
@@ -7,4 +7,24 @@
 
 import XCTest
 
+class FormulaTests: XCTestCase {
+    var sut: Formula!
 
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        sut = Formula()
+    }
+
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+        sut = nil
+    }
+    
+    func test_연산자_1개_숫자_2개가_있을_경우_계산이_잘_되는지() {
+        sut.operands.appendItem(1)
+        sut.operands.appendItem(2)
+        sut.operators.appendItem(.add)
+        
+        XCTAssertEqual(try sut.result(), 3)
+    }
+}
