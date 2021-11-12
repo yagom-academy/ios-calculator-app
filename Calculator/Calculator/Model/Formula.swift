47f0d2551eea619495093ce98e2a9da02ab863e9
ChaminLee <trueman9512@naver.com>
2021-11-12 10:58:05 +0900

    refactor: Formula 타입 내 result 메서드 로직 변경
    - 반복되는 구문 제거
    - 에러를 던질 수 있도록 변경

--- Calculator/Calculator/Model/Formula.swift
+++ Calculator/Calculator/Model/Formula.swift
@@ -11,44 +11,22 @@ struct Formula {
     var operands: CalculatorItemQueue<Double>
     var operators: CalculatorItemQueue<Operator>
     
-    func result() -> Double {
-        guard operands.items.count > 1 && operators.items.count > 0 else {
-            return operands.items.first?.value ?? 0.0
-        }
-        
+    func result() throws -> Double {
         var calculationResult: Double = 0.0
         
         guard let firstOperand = operands.dequeue() else {
-            return 0.0
+            throw QueueError.queueIsEmpty
         }
         
-        guard let secondOperand = operands.dequeue() else {
-            return 0.0
-        }
-        
-        guard let arithmetic = operators.dequeue() else {
-            return 0.0
-        }
-            
-        let currentCalculationResult = arithmetic.calculate(lhs: firstOperand, rhs: secondOperand)
-    
-        if currentCalculationResult.isNaN {
-            return .nan
-        }
-        
-        calculationResult += currentCalculationResult
-        
-        guard operands.items.count > 0 && operators.items.count > 0 else {
-            return calculationResult
-        }
+        calculationResult = firstOperand
         
         while operands.items.count > 0 && operators.items.count > 0 {
             guard let nextOperand = operands.dequeue() else {
-                return 0.0
+                throw QueueError.queueIsEmpty
             }
             
             guard let nextArithmetic = operators.dequeue() else {
-                return 0.0
+                throw QueueError.queueIsEmpty
             }
             
             let nextCalculationResult = nextArithmetic.calculate(lhs: calculationResult, rhs: nextOperand)
