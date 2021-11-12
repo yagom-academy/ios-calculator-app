6f94f0ea9abf1ae8bd2c8a5817c0d3750d662c91
yeahg-dev <yeahg.dev@gmail.com>
2021-11-13 01:27:19 +0900

    test: test code내 커멘트 삭제

--- Calculator/CalculatorTests/CalculatorTests.swift
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -22,65 +22,51 @@ class CalculatorTests: XCTestCase {
     }
 
     func test_enqueue호출시_queue는_첫번째요소로_1을_저장하는배열이된다() {
-        //expectation
         calculatorItemQueue.enqueue(1.0)
         
-        //result
         XCTAssertNotNil(calculatorItemQueue.testableQueue)
         XCTAssertEqual(calculatorItemQueue.testableQueue[0] as! Double, 1.0)
     }
     
     func test_enqueue호출시_queue는_첫번째요소로_plus를_저장하는배열이된다() {
-        //expectation
         calculatorItemQueue.enqueue(Operator.plus)
 
-        //result
         XCTAssertNotNil(calculatorItemQueue.testableQueue)
     }
 
     func test_enqueue호출시_queue는_1과_plus를_저장하는배열이된다() {
-        //expctation
         calculatorItemQueue.enqueue(1)
         calculatorItemQueue.enqueue(Operator.plus)
 
-        //result
         XCTAssertEqual(calculatorItemQueue.testableQueue.count, 2)
         XCTAssertEqual(calculatorItemQueue.testableQueue[0] as! Double, 1.0)
         XCTAssertEqual(calculatorItemQueue.testableQueue[1] as! Operator, Operator.plus)
     }
 
     func test_dequeue호출시_queue는_빈배열이된다() {
-        //expectation
         calculatorItemQueue.enqueue(1)
         calculatorItemQueue.dequeue()
 
-        //result
         XCTAssertTrue(calculatorItemQueue.testableQueue.isEmpty)
     }
 
     func test_queue가_빈배열일때_removeFisrst는_실행되지않는다() {
-        //expectation
         calculatorItemQueue.dequeue()
 
-        //result
         XCTAssertNoThrow(calculatorItemQueue.dequeue())
     }
 
     func test_removeAllItems호출시_queue는_빈배열이된다() {
-        //expectation
         calculatorItemQueue.enqueue(1)
         calculatorItemQueue.enqueue(Operator.minus)
         calculatorItemQueue.removeAllItems()
 
-        //result
         XCTAssertTrue(calculatorItemQueue.testableQueue.isEmpty)
     }
 
     func test_queue가_빈배열일때_removeAllItems호출하면_오류를던지지않는다() {
-        //expectation
         calculatorItemQueue.removeAllItems()
 
-        //result
         XCTAssertNoThrow(calculatorItemQueue.removeAllItems())
     }
 }
