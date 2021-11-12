26b7c399f153b3df779bb1412e747e8bde3cc805
hwangjeha <hyhpwang@gmail.com>
2021-11-12 11:50:46 +0900

    feat: 숫자 여러개와 여러개의 연산자가 입력됐을때 result함수의 결과가 맞게 나오는지 테스트 추가

--- Calculator/CalculatorTests/FormulaTests.swift
+++ Calculator/CalculatorTests/FormulaTests.swift
@@ -48,4 +48,35 @@ class FormulaTests: XCTestCase {
         // then
         XCTAssertEqual(result, number1 + number2)
     }
+    
+    func test_숫자_여러개와_여러개의_연산자가_입력됐을때_result함수의_결과가_맞게_나오는지(){
+        // given
+        let number1 = 2.0
+        let number2 = 3.0
+        let number3 = 1.0
+        let number4 = 5.0
+        let number5 = 4.0
+        
+        let operator1: Operator = .add
+        let operator2: Operator = .subtract
+        let operator3: Operator = .multiply
+        let operator4: Operator = .divide
+        
+        // when
+        sut.operands.enqueue(value: number1)
+        sut.operands.enqueue(value: number2)
+        sut.operands.enqueue(value: number3)
+        sut.operands.enqueue(value: number4)
+        sut.operands.enqueue(value: number5)
+                
+        sut.operators.enqueue(value: operator1)
+        sut.operators.enqueue(value: operator2)
+        sut.operators.enqueue(value: operator3)
+        sut.operators.enqueue(value: operator4)
+        
+        let result = sut.result()
+                
+        // then
+        XCTAssertEqual(result, 5.0)
+    }
 }
