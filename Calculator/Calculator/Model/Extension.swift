f4e07d7afdee6b01bcad209ff663faa2e30a7525
July911 <min95911@nate.com>
2021-11-12 15:40:58 +0900

    feat : 기본타입인 String 에 extension으로 split함수 추가

--- Calculator/Calculator/Model/Extension.swift
+++ Calculator/Calculator/Model/Extension.swift
@@ -1,4 +1,12 @@
 
 import Foundation
 
+extension Double {
+    
+}
 
+extension String {
+    func split(with target: Character) -> [String] {
+        self.components(separatedBy: String(target)).map{ String($0) }
+    }
+}
