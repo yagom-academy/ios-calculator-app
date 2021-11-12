909704dedfbec866303dc36de63948d2f7a6358f
ChaminLee <trueman9512@naver.com>
2021-11-12 13:09:05 +0900

    feat: ExpressionParse 타입 추가
    - 입력된 연산식을 구분하기 위한 구분자 separator 추가
    - 입력된 연산식에서 피연산자만 분리해내기 위한 componentsByOperators 메서드 추가- 입력된 연산식에서 피연산자와 연산자를 분리하여 저장하고 Formula 인스턴스를 만드는 parse 메서드 추가

---
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -0,0 +1,40 @@
+//
+//  ExpressionParser.swift
+//  Calculator
+//
+//  Created by 이차민 on 2021/11/12.
+//
+
+import Foundation
+
+enum ExpressionParser {
+    static private let separator: Character = " "
+    
+    static func parse(from input: String) -> Formula {
+        let operands = CalculatorItemQueue<Double>()
+        let operators = CalculatorItemQueue<Operator>()
+
+        let operatorStringsSet = Set(Operator.allCases.map { String($0.rawValue) })
+        
+        componentsByOperators(from: input)
+            .compactMap { Double($0) }
+            .forEach { number in
+                operands.enqueue(number)
+            }
+            
+        input.split(with: separator)
+            .filter { operatorStringsSet.contains($0) }
+            .compactMap { Operator(rawValue: Character($0)) }
+            .forEach { arithmetic in
+                operators.enqueue(arithmetic)
+            }
+            
+        return Formula(operands: operands, operators: operators)
+    }
+    
+    static private func componentsByOperators(from input: String) -> [String] {
+        return input.split(with: separator)
+            .compactMap { Double($0) }
+            .map { String($0) }
+    }
+}
