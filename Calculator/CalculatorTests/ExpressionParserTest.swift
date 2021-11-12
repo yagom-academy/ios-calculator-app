5590f3eb3d1f8cac7fc6351be9310d5175222230
ChaminLee <trueman9512@naver.com>
2021-11-12 14:10:56 +0900

    test: ExpressionParser 테스트 코드 추가

--- Calculator/CalculatorTests/ExpressionParserTest.swift
+++ Calculator/CalculatorTests/ExpressionParserTest.swift
@@ -8,18 +8,18 @@
 import XCTest
 
 class ExpressionParserTest: XCTestCase {
-    var testString: String = ""
+    var expression: String = ""
     
     override func setUp() {
-        testString = "1.0 + 32.4 - -3.2 / 4.0 * 7.2"
+        expression = "1.0 + 32.4 - -3.2 / 4.0 * 7.2"
     }
     
     override func tearDown() {
-        testString = ""
+        expression = ""
     }
     
     func test_입력받은_문자열에서_피연산자와_연산자를_구분하여_Formula_타입의_인스턴스를_만들고_있는지() {
-        let formula = ExpressionParser.parse(from: testString)
+        let formula = ExpressionParser.parse(from: expression)
         
         [1.0, 32.4, -3.2, 4.0, 7.2].forEach { number in
             XCTAssertEqual(number, formula.operands.dequeue())
@@ -28,5 +28,16 @@ class ExpressionParserTest: XCTestCase {
         [Operator.add, Operator.subtract, Operator.divide, Operator.multiply].forEach { arithmetic in
             XCTAssertEqual(arithmetic, formula.operators.dequeue())
         }
+    }
+    
+    func test_추출해낸_Formula_인스턴스가_정상적인_연산을_하는지() {
+        let formula = ExpressionParser.parse(from: expression)
+        XCTAssertEqual((1.0 + 32.4 - (-3.2)) / 4.0 * 7.2, try formula.result())
+    }
+    
+    func test_0으로_중간에_나누는_경우_nan이_연산결과로_반환되는지() {
+        let divideByZeroExpression = "1.0 + 32.4 - -3.2 / 0.0 * 7.2"
+        let formula = ExpressionParser.parse(from: divideByZeroExpression)
+        XCTAssertTrue(try formula.result().isNaN)
     }
 }
