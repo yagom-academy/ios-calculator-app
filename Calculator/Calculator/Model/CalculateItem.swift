5b5f31a9f1ca6bfd79f752fdc1f9a0ef79ceda63
llghdud921 <llghdud921@naver.com>
2021-11-12 11:39:20 +0900

    feat: operater에 rawValue 추가

---
+++ Calculator/Calculator/Model/CalculateItem.swift
@@ -0,0 +1,10 @@
+//
+//  calculateItem.swift
+//  Calculator
+//
+//  Created by 이호영 on 2021/11/12.
+//
+
+import Foundation
+
+protocol CalculateItem { }
---
+++ Calculator/Calculator/Model/Operator.swift
@@ -0,0 +1,23 @@
+//
+//  CalculatorItem.swift
+//  Calculator
+//
+//  Created by 이호영 on 2021/11/09.
+//
+
+import Foundation
+
+enum Operator: Character, CaseIterable {
+    
+    case add = "+"
+    case substract = "-"
+    case multiple = "*"
+    case divide = "/"
+}
+
+extension Operator {
+    
+}
+
+
+
