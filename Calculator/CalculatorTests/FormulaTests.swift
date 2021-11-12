44dc2ccd48e719b73f824d8826ea9dace6094482
Kim-EunsooSilver <graysilver18@naver.com>
2021-11-12 15:19:08 +0900

    test: FormulaTests - test_1_더하기_2의_결과가_3인가 케이스 작성

--- Calculator/CalculatorTests/FormulaTests.swift
+++ Calculator/CalculatorTests/FormulaTests.swift
@@ -6,7 +6,17 @@
 //
 
 import XCTest
+@testable import Calculator
 
 class FormulaTests: XCTestCase {
-    
+
+    func test_1_더하기_2의_결과가_3인가() {
+        var formula = Formula()
+        formula.operands.enqueue(1.0)
+        formula.operators.enqueue(Operator.add)
+        
+        var calculationResult = formula.result()
+        
+        XCTAssertEqual(3.0, calculationResult)
+    }
 }
