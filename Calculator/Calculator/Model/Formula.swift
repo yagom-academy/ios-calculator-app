42806eb0c6f078f41f54ba207af1e576300a9c1a
yanghojoon <yanghojoon8487@gmail.com>
2021-11-12 16:08:17 +0900

    feat: 계산을 진행하는 Formula 구조체 코드 구현 #5

--- Calculator/Calculator/Model/Formula.swift
+++ Calculator/Calculator/Model/Formula.swift
@@ -7,4 +7,38 @@
 
 import Foundation
 
-
+struct Formula {
+    var operands: CalculatorItemQueue<Double>
+    var operators: CalculatorItemQueue<Operator>
+    
+    init(operands: CalculatorItemQueue<Double> = CalculatorItemQueue<Double>(),
+         operators: CalculatorItemQueue<Operator> = CalculatorItemQueue<Operator>()) {
+        self.operands = operands
+        self.operators = operators
+    }
+    
+    mutating func result() throws -> Double {
+        let firstOperand = operands.array[0]
+        let firstOperator = operators.array[0]
+        var result = 0.0
+        
+        do {
+            let nextOperand = try operands.takeOutItem()
+            result = firstOperator.calculate(lhs: firstOperand, rhs: nextOperand[0])
+        } catch {
+            throw QueueError.emptyArray
+        }
+        
+        while try operators.takeOutItem().isEmpty == false {
+            do {
+                let nextOperand = try operands.takeOutItem()
+                let nextOperator = try operators.takeOutItem()
+                result = nextOperator[0].calculate(lhs: result, rhs: nextOperand[0])
+            } catch {
+                throw QueueError.emptyArray
+            }
+        }
+        
+        return result
+    }
+}
