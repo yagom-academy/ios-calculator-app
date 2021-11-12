bc6bb244bb7e13cd032a86fab659babd2971512c
SIWONKIM <siyeon0616@gmail.com>
2021-11-12 18:55:44 +0900

    feat: Formula 구조체의 result() 메서드 기능 추가구현 #2
    기존에는 더하기 기능만 가능했다면, 빼기도 테스트 코드 추가해서 실험해봄

--- Calculator/Calculator/Model/Formula.swift
+++ Calculator/Calculator/Model/Formula.swift
@@ -5,21 +5,17 @@ struct Formula {
     var operators: CalculatorItemQueue<Operator> = CalculatorItemQueue<Operator>()
     
     mutating func result() -> Double? {
-        //Operator의 연산자 함수들을 사용하여 계산결과 도출하기
-        
-        for i in 0...operands.countItems()-2 {
-            guard let lhs = operands.dequeue() else { return nil }
+        var result: Double = 0
+        for _ in 0...`operands`.countItems()-1 {
+            let lhs = result
             guard let rhs = operands.dequeue() else { return nil }
             
             let `operator` = operators.dequeue()
             guard let calculateResult = `operator`?.calculate(lhs: lhs, rhs: rhs) else { return nil }
-            
-            operands.enqueue(calculateResult)
+            result = calculateResult
         }
         
-        let result = operands.dequeue()
-        
-        
+        print(result)
         return result
     }
 }
--- Calculator/CalculatorItemQueueUnitTest/FormulaUnitTest.swift
+++ Calculator/CalculatorItemQueueUnitTest/FormulaUnitTest.swift
@@ -20,34 +20,57 @@ class FormulaUnitTest: XCTestCase {
         numbers.enqueue(3)
 
         operators.enqueue(.add)
-        
+        operators.enqueue(.add)
+
         var firstFormula = Formula(operands: numbers, operators: operators)
-        
+
         //when
         let result = firstFormula.result()
-        
+
         //then
         XCTAssertEqual(result, 5)
     }
-    
+
     func test_2더하기_3더하기_4의_결과는_9가_나온다() {
         //given
         numbers.enqueue(2)
         numbers.enqueue(3)
         numbers.enqueue(4)
-        
+
         operators.enqueue(.add)
         operators.enqueue(.add)
-        
+        operators.enqueue(.add)
+
         var secondFormula = Formula(operands: numbers, operators: operators)
-        
+
         //when
         let result = secondFormula.result()
-        
+
         //then
         XCTAssertEqual(result, 9)
     }
-    
+
+    func test_2더하기_3더하기_4빼기_3의_결과는_6이_나온다() {
+        //given
+        numbers.enqueue(2)
+        numbers.enqueue(3)
+        numbers.enqueue(4)
+        numbers.enqueue(3)
+        
+        operators.enqueue(.add)
+        operators.enqueue(.add)
+        operators.enqueue(.add)
+        operators.enqueue(.subtract)
+
+        var thirdFormula = Formula(operands: numbers, operators: operators)
+
+        //when
+        let result = thirdFormula.result()
+
+        //then
+        XCTAssertEqual(result, 6)
+
+    }
 
 }
 
