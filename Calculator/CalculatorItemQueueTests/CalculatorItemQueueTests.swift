49ed53e328a1934c4fdae8e17dd512ad252ddf73
ICS-Asan <carpediem1015@gmail.com>
2021-11-12 17:59:38 +0900

    refactor: 테스트 코드 내부 queue 호출 방법 수정 #4 #5
    - STEP1 피드백을 반영하여 코드가 수정되어 호출 방법을 변경

--- Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
+++ Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
@@ -10,127 +10,127 @@ import XCTest
 class CalculatorItemQueueTests: XCTestCase {
 
     func test_연산자_inbox가_비어있는지() {
-        let calculatorOperator = OperatorQueue()
-        XCTAssertTrue(calculatorOperator.queue.inbox.isEmpty)
+        let formula = Formula()
+        XCTAssertTrue(formula.operands.inbox.isEmpty)
     }
     
     func test_숫자_inbox가_비어있는지() {
-        let calculatorNumber = NumberQueue()
-        XCTAssertTrue(calculatorNumber.queue.inbox.isEmpty)
+        let formula = Formula()
+        XCTAssertTrue(formula.operators.inbox.isEmpty)
     }
     
     func test_연산자_outbox가_비어있는지() {
-        let calculatorOperator = OperatorQueue()
-        XCTAssertTrue(calculatorOperator.queue.outbox.isEmpty)
+        let formula = Formula()
+        XCTAssertTrue(formula.operators.outbox.isEmpty)
     }
     
     func test_숫자_outbox가_비어있는지() {
-        let calculatorNumber = NumberQueue()
-        XCTAssertTrue(calculatorNumber.queue.outbox.isEmpty)
+        let formula = Formula()
+        XCTAssertTrue(formula.operands.outbox.isEmpty)
     }
     
     func test_연산자_inbox에_add추가() {
-        var calculatorOperator = OperatorQueue()
-        calculatorOperator.queue.enqueue(.add)
-        XCTAssertEqual(calculatorOperator.queue.inbox, [.add])
+        var formula = Formula()
+        formula.operators.enqueue(.add)
+        XCTAssertEqual(formula.operators.inbox, [.add])
     }
     
     func test_연산자_inbox에_add_multiply_추가() {
-        var calculatorOperator = OperatorQueue()
-        calculatorOperator.queue.enqueue(.add)
-        calculatorOperator.queue.enqueue(.multiply)
-        XCTAssertEqual(calculatorOperator.queue.inbox, [.add, .multiply])
+        var formula = Formula()
+        formula.operators.enqueue(.add)
+        formula.operators.enqueue(.multiply)
+        XCTAssertEqual(formula.operators.inbox, [.add, .multiply])
     }
     
     func test_연산자_inbox에_add_multiply_추가_첫번째_요소_제거_및_확인() {
-        var calculatorOperator = OperatorQueue()
-        calculatorOperator.queue.enqueue(.add)
-        calculatorOperator.queue.enqueue(.multiply)
-        let result = calculatorOperator.queue.dequeue()
+        var formula = Formula()
+        formula.operators.enqueue(.add)
+        formula.operators.enqueue(.multiply)
+        let result = formula.operators.dequeue()
         XCTAssertEqual(result, .add)
     }
     
     func test_숫자_inbox에_123추가() {
-        var calculatorNumber = NumberQueue()
-        calculatorNumber.queue.enqueue(123)
-        XCTAssertEqual(calculatorNumber.queue.inbox, [123])
+        var formula = Formula()
+        formula.operands.enqueue(123)
+        XCTAssertEqual(formula.operands.inbox, [123])
     }
     
     func test_숫자_inbox에_123_124_추가() {
-        var calculatorNumber = NumberQueue()
-        calculatorNumber.queue.enqueue(123)
-        calculatorNumber.queue.enqueue(124)
-        XCTAssertEqual(calculatorNumber.queue.inbox, [123, 124])
+        var formula = Formula()
+        formula.operands.enqueue(123)
+        formula.operands.enqueue(124)
+        XCTAssertEqual(formula.operands.inbox, [123, 124])
     }
     
     func test_숫자_inbox에_123_124_추가_첫번째_요소_제거_및_확인() {
-        var calculatorNumber = NumberQueue()
-        calculatorNumber.queue.enqueue(123)
-        calculatorNumber.queue.enqueue(124)
-        let result = calculatorNumber.queue.dequeue()
+        var formula = Formula()
+        formula.operands.enqueue(123)
+        formula.operands.enqueue(124)
+        let result = formula.operands.dequeue()
         XCTAssertEqual(result, 123)
     }
     
     func test_숫자_inbox에_1_dot_23_추가() {
-        var calculatorNumber = NumberQueue()
-        calculatorNumber.queue.enqueue(1.23)
-        XCTAssertEqual(calculatorNumber.queue.inbox, [1.23])
+        var formula = Formula()
+        formula.operands.enqueue(1.23)
+        XCTAssertEqual(formula.operands.inbox, [1.23])
     }
     
     func test_숫자_inbox에_1_dot_23_1_dot_24_추가() {
-        var calculatorNumber = NumberQueue()
-        calculatorNumber.queue.enqueue(1.23)
-        calculatorNumber.queue.enqueue(1.24)
-        XCTAssertEqual(calculatorNumber.queue.inbox, [1.23, 1.24])
+        var formula = Formula()
+        formula.operands.enqueue(1.23)
+        formula.operands.enqueue(1.24)
+        XCTAssertEqual(formula.operands.inbox, [1.23, 1.24])
     }
     
     func test_숫자_inbox에_1_dot_23_1_dot_24_추가_첫번째_요소_제거_및_확인() {
-        var calculatorNumber = NumberQueue()
-        calculatorNumber.queue.enqueue(1.23)
-        calculatorNumber.queue.enqueue(1.24)
-        let result = calculatorNumber.queue.dequeue()
+        var formula = Formula()
+        formula.operands.enqueue(1.23)
+        formula.operands.enqueue(1.24)
+        let result = formula.operands.dequeue()
         XCTAssertEqual(result, 1.23)
     }
     
     func test_숫자_inbox에_1_dot_23_124_추가() {
-        var calculatorNumber = NumberQueue()
-        calculatorNumber.queue.enqueue(1.23)
-        calculatorNumber.queue.enqueue(124)
-        XCTAssertEqual(calculatorNumber.queue.inbox, [1.23, 124])
+        var formula = Formula()
+        formula.operands.enqueue(1.23)
+        formula.operands.enqueue(124)
+        XCTAssertEqual(formula.operands.inbox, [1.23, 124])
     }
     
     func test_숫자_inbox에_123_1_dot_24_추가_첫번째_요소_제거_및_확인() {
-        var calculatorNumber = NumberQueue()
-        calculatorNumber.queue.enqueue(123)
-        calculatorNumber.queue.enqueue(1.24)
-        let result = calculatorNumber.queue.dequeue()
+        var formula = Formula()
+        formula.operands.enqueue(123)
+        formula.operands.enqueue(1.24)
+        let result = formula.operands.dequeue()
         XCTAssertEqual(result, 123)
     }
     
     func test_숫자_inbox에_음수_1_dot_23_124_추가() {
-        var calculatorNumber = NumberQueue()
-        calculatorNumber.queue.enqueue(-1.23)
-        calculatorNumber.queue.enqueue(124)
-        XCTAssertEqual(calculatorNumber.queue.inbox, [-1.23, 124])
+        var formula = Formula()
+        formula.operands.enqueue(-1.23)
+        formula.operands.enqueue(124)
+        XCTAssertEqual(formula.operands.inbox, [-1.23, 124])
     }
     
     func test_숫자_inbox에_음수_123_1_dot_24_추가_첫번째_요소_제거_및_확인() {
-        var calculatorNumber = NumberQueue()
-        calculatorNumber.queue.enqueue(-123)
-        calculatorNumber.queue.enqueue(1.24)
-        let result = calculatorNumber.queue.dequeue()
+        var formula = Formula()
+        formula.operands.enqueue(-123)
+        formula.operands.enqueue(1.24)
+        let result = formula.operands.dequeue()
         XCTAssertEqual(result, -123)
     }
     
     func test_연산자_queue가_비어있을때_dequeue시_nil_반환() {
-        var calculatorOperator = OperatorQueue()
-        let result = calculatorOperator.queue.dequeue()
+        var formula = Formula()
+        let result = formula.operators.dequeue()
         XCTAssertNil(result)
     }
     
     func test_숫자_queue가_비어있을때_dequeue시_nil_반환() {
-        var calculatorNumber = NumberQueue()
-        let result = calculatorNumber.queue.dequeue()
+        var formula = Formula()
+        let result = formula.operators.dequeue()
         XCTAssertNil(result)
     }
 
