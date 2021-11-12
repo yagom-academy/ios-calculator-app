399171f8df8af02e05e613e92d62c4996704877f
hwangjeha <hyhpwang@gmail.com>
2021-11-12 11:43:38 +0900

    feat: 숫자 두개와 더하기가 입력됐을때 result함수의 결과가 맞게 나오는지 테스트 추가

--- Calculator/CalculatorTests/FormulaTests.swift
+++ Calculator/CalculatorTests/FormulaTests.swift
@@ -24,9 +24,28 @@ class FormulaTests: XCTestCase {
         
         // when
         sut.operands.enqueue(value: input)
+        
         let result = sut.result()
                 
         // then
         XCTAssertEqual(result, input)
+    }
+    
+    func test_숫자_두개와_더하기가_입력됐을때_result함수의_결과가_맞게_나오는지(){
+        // given
+        let number1 = 2.0
+        let number2 = 3.0
+        let `operator`: Operator = .add
+        
+        // when
+        sut.operands.enqueue(value: number1)
+        sut.operands.enqueue(value: number2)
+        
+        sut.operators.enqueue(value: `operator`)
+        
+        let result = sut.result()
+                
+        // then
+        XCTAssertEqual(result, number1 + number2)
     }
 }
--- Calculator/Model/Formula.swift
+++ Calculator/Model/Formula.swift
@@ -12,6 +12,23 @@ struct Formula {
     let operators: CalculatorItemQueue<Operator> = CalculatorItemQueue<Operator>()
     
     func result() -> Double {
-        return self.operands.dequeue() ?? 0
+        guard let firstNumber = operands.dequeue() else {
+            return 0
+        }
+
+        var result: Double = firstNumber
+
+        while operands.isEmpty == false {
+            guard let `operator` = operators.dequeue() else {
+                return 0
+            }
+            guard let number = operands.dequeue() else {
+                return 0
+            }
+
+            result = `operator`.calculate(lhs: result, rhs: number)
+        }
+        
+        return result
     }
 }
