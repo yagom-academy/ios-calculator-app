0251845e4fba78d82f3721863c3c82e8be749c0c
Kim-EunsooSilver <graysilver18@naver.com>
2021-11-13 01:23:28 +0900

    feat: ExpressionParser - parse 구현

--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -9,6 +9,18 @@ import Foundation
 
 enum ExpressionParser {
     
+    static func parse(from input: String) -> Formula {
+        let formula = Formula()
+        
+        _ = componentsByOperators(from: input).compactMap { Double($0) }
+                                                .map { formula.operands.enqueue($0) }
+        
+        _ = componentsByOperators(from: input).compactMap { Operator(rawValue: Character($0)) }
+                                                .map { formula.operators.enqueue($0) }
+        
+        return formula
+    }
+    
     static private func componentsByOperators(from input: String) -> [String] {
         input.split(with: " ")
     }
