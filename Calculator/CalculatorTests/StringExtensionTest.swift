10563421ce030b53aa08decaeadc87f9cbc0f3a7
ICS-Asan <carpediem1015@gmail.com>
2021-11-13 01:37:08 +0900

    test: split메서드 연산자2개 정수3개인 경우 숫자분리 test #9

--- Calculator/CalculatorTests/StringExtensionTest.swift
+++ Calculator/CalculatorTests/StringExtensionTest.swift
@@ -33,4 +33,28 @@ class StringExtensionTest: XCTestCase {
         XCTAssertEqual(result, ["12", "15"])
     }
     
+    func test_add연산자_2개_정수_3개_숫자분리 {
+        let testString = "12+15+23"
+        let result = testString.split(with: Operator.add.rawValue)
+        XCTAssertEqual(result, ["12", "15", "23"])
+    }
+    
+    func test_subtract연산자_2개_정수_3개_숫자분리 {
+        let testString = "12_15_23"
+        let result = testString.split(with: Operator.subtract.rawValue)
+        XCTAssertEqual(result, ["12", "15", "23"])
+    }
+    
+    func test_divide연산자_2개_정수_3개_숫자분리 {
+        let testString = "12/15/23"
+        let result = testString.split(with: Operator.divide.rawValue)
+        XCTAssertEqual(result, ["12", "15", "23"])
+    }
+    
+    func test_multiply연산자_2개_정수_3개_숫자분리 {
+        let testString = "12×15×23"
+        let result = testString.split(with: Operator.multiply.rawValue)
+        XCTAssertEqual(result, ["12", "15", "23"])
+    }
+    
 }
