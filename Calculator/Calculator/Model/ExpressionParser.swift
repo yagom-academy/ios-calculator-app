93883f76ac34b604023f9b6a33b3e28a1aabea45
ICS-Asan <carpediem1015@gmail.com>
2021-11-14 02:30:12 +0900

    feat: componentsByOperators 타입 메서드 및 테스트 계획 변경 #8

--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -9,7 +9,15 @@ import Foundation
 
 enum ExpressionParser {
     
-    static func componentsByOneOperator(from input: [String], with target: Character) -> [String] {
+    static func componentsByOperators(from input: String) -> [String] {
+        var result = [input]
+        for target in Operator.allCases {
+            result = componentsByOneOperator(from: result, with: target.rawValue)
+        }
+        return result
+    }
+    
+    private static func componentsByOneOperator(from input: [String], with target: Character) -> [String] {
         var result: [String] = []
         for formula in input {
             result += formula.split(with: target)
