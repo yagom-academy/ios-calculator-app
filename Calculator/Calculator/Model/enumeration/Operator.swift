2bcebba1754c5770006c79ce2856dcfbf9ff78e2
Jager-yoo <y73447jh@gmail.com>
2021-11-13 18:25:57 +0900

    feat: Operator 열거형의 계산 메서드 구현 #7

--- Calculator/Calculator/Model/enumeration/Operator.swift
+++ Calculator/Calculator/Model/enumeration/Operator.swift
@@ -10,6 +10,35 @@ import Foundation
 enum Operator: CalculateItem {
     case add
     case subtract
-    case divide
     case multiply
+    case divide
+    
+    func calculate(_ lhs: Double, _ rhs: Double) -> Double {
+        switch self {
+        case .add:
+            return add(lhs, rhs)
+        case .subtract:
+            return subtract(lhs, rhs)
+        case .multiply:
+            return multiply(lhs, rhs)
+        case .divide:
+            return divide(lhs, rhs)
+        }
+    }
+    
+    private func add(_ lhs: Double, _ rhs: Double) -> Double {
+        return lhs + rhs
+    }
+    
+    private func subtract(_ lhs: Double, _ rhs: Double) -> Double {
+        return lhs - rhs
+    }
+    
+    private func multiply(_ lhs: Double, _ rhs: Double) -> Double {
+        return lhs * rhs
+    }
+    
+    private func divide(_ lhs: Double, _ rhs: Double) -> Double {
+            return lhs / rhs
+    }
 }
