adc295b0e8628bb53f9b0a05ac6d7dd1d7a55d49
yim2627 <yim2627@gmail.com>
2021-11-13 18:38:56 +0900

    test: parse호출시 연산자와 피연산자가 정상적으로 분리되는지 요소의 개수로 확인 #3

--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -9,10 +9,26 @@ import Foundation
 
 enum ExpressionParser {
     static func parse(from input: String) -> Formula {
-        return Formula(operators: CalculatorItemQueue(), operands: CalculatorItemQueue())
+        let operands = componentsByOperators(from: input)
+                        .compactMap{ Double($0) }
+        
+        let operators = input.filter{ inputSlice in
+            Double(inputSlice.description) == nil
+        }.compactMap{ Operator(rawValue: $0) }
+        
+        return Formula(operators: CalculatorItemQueue(operators), operands: CalculatorItemQueue(operands))
     }
     
     private static func componentsByOperators(from input: String) -> [String] {
-        return [""]
+        var inputArray = [input]
+        var tempArray: [String] = []
+        Operator.allCases.forEach { `operator` in
+            for input in inputArray {
+                tempArray += input.split(with: `operator`.rawValue)
+            }
+            inputArray = tempArray
+            tempArray.removeAll()
+        }
+        return inputArray
     }
 }
--- Calculator/CalculatorTests/ExpressionParserTests.swift
+++ Calculator/CalculatorTests/ExpressionParserTests.swift
@@ -15,9 +15,125 @@ class ExpressionParserTests: XCTestCase {
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        try super.tearDownWithError()
+        sut = nil
+    }
+    
+    func test_parse호출시_input이연산자1개숫자2개일때_모든Operator가정상적으로분리되는지() {
+        // Given
+        var input = "12+33"
+        
+        // When
+        var result = sut.parse(from: input)
+        
+        // Then
+        XCTAssertEqual(result.operands.count, 2)
+        XCTAssertEqual(result.operators.count, 1)
+        
+        // Given
+        input = "12-33"
+        
+        // When
+        result = sut.parse(from: input)
+        
+        // Then
+        XCTAssertEqual(result.operands.count, 2)
+        XCTAssertEqual(result.operators.count, 1)
+        
+        // Given
+        input = "12×33"
+        
+        // When
+        result = sut.parse(from: input)
+        
+        // Then
+        XCTAssertEqual(result.operands.count, 2)
+        XCTAssertEqual(result.operators.count, 1)
+        
+        // Given
+        input = "12/33"
+        
+        // When
+        result = sut.parse(from: input)
+        
+        // Then
+        XCTAssertEqual(result.operands.count, 2)
+        XCTAssertEqual(result.operators.count, 1)
     }
 
-
-
+    func test_parse호출시_input이연산자2개숫자3개일때_모든Operator가정상적으로분리되는지() {
+        // Given
+        var input = "12+33-33"
+        
+        // When
+        var result = sut.parse(from: input)
+        
+        // Then
+        XCTAssertEqual(result.operands.count, 3)
+        XCTAssertEqual(result.operators.count, 2)
+        
+        // Given
+        input = "12-33/33"
+        
+        // When
+        result = sut.parse(from: input)
+        
+        // Then
+        XCTAssertEqual(result.operands.count, 3)
+        XCTAssertEqual(result.operators.count, 2)
+        
+        // Given
+        input = "12×33+33"
+        
+        // When
+        result = sut.parse(from: input)
+        
+        // Then
+        XCTAssertEqual(result.operands.count, 3)
+        XCTAssertEqual(result.operators.count, 2)
+        
+        // Given
+        input = "12/33×33"
+        
+        // When
+        result = sut.parse(from: input)
+        
+        // Then
+        XCTAssertEqual(result.operands.count, 3)
+        XCTAssertEqual(result.operators.count, 2)
+    }
+    
+    func test_parse호출시_input이연산자3개숫자4개일때_모든Operator가정상적으로분리되는지() {
+        // Given
+        var input = "12+33-33/13"
+        
+        // When
+        var result = sut.parse(from: input)
+        
+        // Then
+        XCTAssertEqual(result.operands.count, 4)
+        XCTAssertEqual(result.operators.count, 3)
+        
+        // Given
+        input = "12-33/33×492"
+        
+        // When
+        result = sut.parse(from: input)
+        
+        // Then
+        XCTAssertEqual(result.operands.count, 4)
+        XCTAssertEqual(result.operators.count, 3)
+    }
+    
+    func test_parse호출시_input이연산자4개숫자5개일때_모든Operator가정상적으로분리되는지() {
+        // Given
+        let input = "12+33-33/13×2322"
+        
+        // When
+        let result = sut.parse(from: input)
+        
+        // Then
+        XCTAssertEqual(result.operands.count, 5)
+        XCTAssertEqual(result.operators.count, 4)
+    }
 }
