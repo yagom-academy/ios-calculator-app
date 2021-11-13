77283f52334d5c9e847b51a000aa140666c44442
aCafela-coffee <seym7894@gmail.com>
2021-11-13 20:41:46 +0900

    test: ExpressionParserTests 수정

--- Calculator/CalculatorItemQueueTests/ExpressionParserTests.swift
+++ Calculator/CalculatorItemQueueTests/ExpressionParserTests.swift
@@ -3,31 +3,13 @@ import XCTest
 
 class ExpressionParserTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-}
-
-extension ExpressionParserTests {
-    func test_String_split() {
-        let input = "1+2+3"
-        let result = input.split(with: "+")
-        XCTAssertEqual(result, ["1", "+", "2", "+", "3"])
-    }
-    
-    func test_String_split_분리할_필요가_없을때() {
-        let input = "123"
-        let result = input.split(with: "+")
-        XCTAssertEqual(result, ["123"])
-    }
-    
-    func test_String_split_빈_문자열이_포함되는지() {
-        let input = "12+2++3"
-        let result = input.split(with: "+")
-        XCTAssertEqual(result, ["12", "+", "2", "+", "", "+", "3"])
+    func test_parse() {
+        let operands: [Double] = [1, 2, 3, 4, 5]
+        let operators = ["+", "-", "*", "/"].compactMap { Operator(rawValue: $0) }
+        var formula = Formula(operands: CalculatorItemQueue(operands),
+                              operators: CalculatorItemQueue(operators))
+        let input = "1+2-3*4/5"
+        var formulaFromText = ExpressionParser.parse(from: input)
+        XCTAssertEqual(formulaFromText.result(), formula.result())
     }
 }
