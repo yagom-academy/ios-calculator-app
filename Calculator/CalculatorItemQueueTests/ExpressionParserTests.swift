00795c3b99ce0b6893021c03ead61f416f67cc25
aCafela-coffee <seym7894@gmail.com>
2021-11-13 22:25:13 +0900

    test: ExpressionParserTests 테스트 추가

--- Calculator/CalculatorItemQueueTests/ExpressionParserTests.swift
+++ Calculator/CalculatorItemQueueTests/ExpressionParserTests.swift
@@ -12,4 +12,14 @@ class ExpressionParserTests: XCTestCase {
         var formulaFromText = ExpressionParser.parse(from: input)
         XCTAssertEqual(formulaFromText.result(), formula.result())
     }
+    
+    func test_parse_남는_숫자는_무시() {
+        let operands: [Double] = [1, 2, 3, 4, 5, 6]
+        let operators = ["+", "-", "*", "/"].compactMap { Operator(rawValue: $0) }
+        var formula = Formula(operands: CalculatorItemQueue(operands),
+                              operators: CalculatorItemQueue(operators))
+        let input = "1+2-3*4/5"
+        var formulaFromText = ExpressionParser.parse(from: input)
+        XCTAssertEqual(formulaFromText.result(), formula.result())
+    }
 }
