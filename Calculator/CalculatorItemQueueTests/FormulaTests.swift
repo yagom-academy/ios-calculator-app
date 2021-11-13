66f99de8bb0855039a270a92cc5d0f291537afe4
aCafela-coffee <seym7894@gmail.com>
2021-11-13 14:17:13 +0900

    text: FormulaTests 추가

---
+++ Calculator/CalculatorItemQueueTests/FormulaTests.swift
@@ -0,0 +1,48 @@
+import XCTest
+@testable import Calculator
+
+class FormulaTests: XCTestCase {
+    var sut: Formula!
+
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        sut = Formula()
+    }
+
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+        sut = nil
+    }
+    
+    func test_Formula_빈_result() {
+        let result = sut.result()
+        XCTAssertEqual(result, 0)
+    }
+    
+    func test_Formula_1더하기2는3() {
+        sut.operands.enqueue(1)
+        sut.operators.enqueue(Operator.add)
+        sut.operands.enqueue(2)
+        
+        let result = sut.result()
+        XCTAssertEqual(result, 3)
+    }
+    
+    func test_Formula_오퍼레이터가_하나_남는_경우_무시() {
+        sut.operands.enqueue(1)
+        sut.operators.enqueue(Operator.add)
+        sut.operands.enqueue(2)
+        sut.operators.enqueue(Operator.add)
+        
+        let result = sut.result()
+        XCTAssertEqual(result, 3)
+    }
+    
+    func test_Formula_오퍼레이터가_없으면_첫번째_숫자가_result() {
+        sut.operands.enqueue(1)
+        sut.operands.enqueue(2)
+        
+        let result = sut.result()
+        XCTAssertEqual(result, 1)
+    }
+}
