50acea511aea16602e7a54913dd9531133cbd07d
ICS-Asan <carpediem1015@gmail.com>
2021-11-14 02:46:15 +0900

    refactor: 테스트 계획 변경으로 인한 테스트 코드 수정 #9
    - testAry지역 변수 타입 및 할당값 수정
    - componentsByOneOperator->componentsByOperators 사용 메서드 변경

--- Calculator/CalculatorTests/ExpressionParserTest.swift
+++ Calculator/CalculatorTests/ExpressionParserTest.swift
@@ -8,59 +8,58 @@
 import XCTest
 
 class ExpressionParserTest: XCTestCase {
-    var sut: ExpressionParser.Type! = ExpressionParser.self
-    
-    func test_정수_2개_add연산자_1개인_배열_2개_숫자분리() {
-        let testAry = ["2+3", "12+466"]
-        let result = sut.componentsByOneOperator(from: testAry, with: Operator.add.rawValue)
+
+    func test_정수_4개_add연산자_2개_숫자분리() {
+        let testAry = "2+3+12+466"
+        let result = ExpressionParser.componentsByOperators(from: testAry)
         XCTAssertEqual(result, ["2", "3", "12", "466"])
     }
     
-    func test_정수_2개_subtract연산자_1개인_배열_2개_숫자분리() {
-        let testAry = ["2_3", "12_466"]
-        let result = sut.componentsByOneOperator(from: testAry, with: Operator.subtract.rawValue)
+    func test_정수_4개_subtract연산자_2개인_숫자분리() {
+        let testAry = "2_3_12_466"
+        let result = ExpressionParser.componentsByOperators(from: testAry)
         XCTAssertEqual(result, ["2", "3", "12", "466"])
     }
 
-    func test_정수_2개_divide연산자_1개인_배열_2개_숫자분리() {
-        let testAry = ["2/3", "12/466"]
-        let result = sut.componentsByOneOperator(from: testAry, with: Operator.divide.rawValue)
+    func test_정수_4개_divide연산자_2개인_숫자분리() {
+        let testAry = "2/3/12/466"
+        let result = ExpressionParser.componentsByOperators(from: testAry)
         XCTAssertEqual(result, ["2", "3", "12", "466"])
     }
     
     func test_정수_2개_multiply연산자_1개인_배열_2개_숫자분리() {
-        let testAry = ["2×3", "12×466"]
-        let result = sut.componentsByOneOperator(from: testAry, with: Operator.multiply.rawValue)
+        let testAry = "2×3×12×466"
+        let result = ExpressionParser.componentsByOperators(from: testAry)
         XCTAssertEqual(result, ["2", "3", "12", "466"])
     }
     
-    func test_음수_실수_각1개_add연산자_1개인_배열_2개_숫자분리() {
-        let testAry = ["2.22+-3.33", "1.2+-466"]
-        let result = sut.componentsByOneOperator(from: testAry, with: Operator.add.rawValue)
+    func test_음수_실수_각2개_add연산자_2개인_숫자분리() {
+        let testAry = "2.22+-3.33+1.2+-466"
+        let result = ExpressionParser.componentsByOperators(from: testAry)
         XCTAssertEqual(result, ["2.22", "-3.33", "1.2", "-466"])
     }
     
     func test_음수_실수_각1개_subtract연산자_1개인_배열_2개_숫자분리() {
-        let testAry = ["2.22_-3.33", "1.2_-466"]
-        let result = sut.componentsByOneOperator(from: testAry, with: Operator.subtract.rawValue)
+        let testAry = "2.22_-3.33_1.2_-466"
+        let result = ExpressionParser.componentsByOperators(from: testAry)
         XCTAssertEqual(result, ["2.22", "-3.33", "1.2", "-466"])
     }
 
     func test_음수_실수_각1개_divide연산자_1개인_배열_2개_숫자분리() {
-        let testAry = ["2.22/-3.33", "1.2/-466"]
-        let result = sut.componentsByOneOperator(from: testAry, with: Operator.divide.rawValue)
+        let testAry = "2.22/-3.33/1.2/-466"
+        let result = ExpressionParser.componentsByOperators(from: testAry)
         XCTAssertEqual(result, ["2.22", "-3.33", "1.2", "-466"])
     }
     
     func test_음수_실수_각1개_multiply연산자_1개인_배열_2개_숫자분리() {
-        let testAry = ["2.22×-3.33", "1.2×-466"]
-        let result = sut.componentsByOneOperator(from: testAry, with: Operator.multiply.rawValue)
+        let testAry = "2.22×-3.33×1.2×-466"
+        let result = ExpressionParser.componentsByOperators(from: testAry)
         XCTAssertEqual(result, ["2.22", "-3.33", "1.2", "-466"])
     }
     
     func test_정수_음수_실수_각1개_연산자_2개인_배열_2개_숫자분리() {
-        let testAry = ["2.22×-3.33×67", "1.2×-466×99"]
-        let result = sut.componentsByOneOperator(from: testAry, with: Operator.multiply.rawValue)
+        let testAry = "2.22×-3.33×67×1.2×-466×99"
+        let result = ExpressionParser.componentsByOperators(from: testAry)
         XCTAssertEqual(result, ["2.22", "-3.33", "67", "1.2", "-466", "99"])
     }
 
