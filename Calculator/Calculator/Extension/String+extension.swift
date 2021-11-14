5eec8aa8aa2b5b47eb3bf1a05c9ea1477d07b7f3
Ari <lee_ari95@icloud.com>
2021-11-14 23:50:55 +0900

    refactor: 불필요한 return문 삭제 #4

--- Calculator/Calculator/Extension/String+extension.swift
+++ Calculator/Calculator/Extension/String+extension.swift
@@ -9,6 +9,6 @@ import Foundation
 
 extension String {
     func split(with target: Character = " ") -> [String] {
-        return self.split(separator: target).map{ String($0) }
+        self.split(separator: target).map{ String($0) }
     }
 }
