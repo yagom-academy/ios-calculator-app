2d7677548795d63315b9d1038c746f7052735b76
JT <dd3557@naver.com>
2021-11-14 22:14:06 +0900

    feat: 구조체 Formula 타입 구현
    - 프로퍼티 operands와 operators 구현
    - 메소드 result 추가 및 내부 코드 임시 구현

---
+++ Calculator/Calculator/Model/Formula.swift
@@ -0,0 +1,17 @@
+//
+//  Formula.swift
+//  Calculator
+//
+//  Created by 김진태 on 2021/11/14.
+//
+
+import Foundation
+
+struct Formula {
+    let operands: CalculatorItemQueue<Double>
+    let operators: CalculatorItemQueue<Operator>
+    
+    func result() -> Double {
+        return 0.0
+    }
+}
