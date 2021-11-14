31c148d8aadffd85e2a0b7b6ab7f4773de9414bb
junbangg <wnstjr4620@naver.com>
2021-11-15 02:48:21 +0900

    REFACTOR: ExpressionParser 변수/상수명 수정

--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -9,12 +9,12 @@ import Foundation
 
 enum ExpressionParser {
     static func parse(from input: String) -> Formula {
-        let operatorCharacters: [Character] = ["+", "-", "*", "/"]
-        let operators = input.filter { operatorCharacters.contains($0) }
-            .compactMap{ Operator(rawValue: $0)}
+        let operatorRawValues = Operator.allCases.compactMap { $0.rawValue }
         let operands = componentsByOperators(from: input).compactMap { Double($0) }
-        let operatorQueue = CalculatorItemQueue(elements: operators)
+        let operators = input.filter { operatorRawValues.contains($0) }
+            .compactMap{ Operator(rawValue: $0)}
         let operandQueue = CalculatorItemQueue(elements: operands)
+        let operatorQueue = CalculatorItemQueue(elements: operators)
         return Formula(operands: operandQueue, operators: operatorQueue)
     }
     
@@ -22,9 +22,10 @@ enum ExpressionParser {
         let separators = CharacterSet(charactersIn: "+-*/")
         let separatedInputArray = input.components(separatedBy: separators).map { String($0) }
         let operatorRawValues = Operator.allCases.compactMap { $0.rawValue }
-        return operatorRawValues.reduce(separatedInputArray) { (array: [String], operatorCharacter: Character) in
+        let operandStringArray = operatorRawValues.reduce(separatedInputArray) { (array: [String], operatorCharacter: Character) in
             array.flatMap { $0.split(with: operatorCharacter) }
         }
+        return operandStringArray
     }
 }
 
