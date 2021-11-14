48d94e29643b91e7ee06bb52de539bc3c074fa27
namu <simjh27@gmail.com>
2021-11-15 03:45:03 +0900

    feat: 피연산자 큐 생성 메서드 분리

--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -9,21 +9,27 @@ import Foundation
 
 enum ExpressionParser {
     static func parse(from input: String) -> Formula {
-        var operandQueue = CalculatorItemQueue<Double>()
+        let operandQueue = makeOperandQueue(with: input)
         let operatorQueue = makeOperatorQueue(with: input)
-
+        
+        return Formula(operands: operandQueue, operators: operatorQueue)
+    }
+    
+    private static func componentsByOperators(from input: String) -> [String] {
+        Operator.allCases.reduce([input]) { (array, operator) in
+            array.flatMap { $0.split(with: `operator`.rawValue) }
+        }
+    }
+    
+    private static func makeOperandQueue(with input: String) -> CalculatorItemQueue<Double> {
+        var operandQueue = CalculatorItemQueue<Double>()
+        
         componentsByOperators(from: input).forEach {
             guard let converted = Double($0) else { return }
             operandQueue.enqueue(converted)
         }
         
-        return Formula(operands: operandQueue, operators: operatorQueue)
-    }
-
-    private static func componentsByOperators(from input: String) -> [String] {
-        Operator.allCases.reduce([input]) { (array, operator) in
-            array.flatMap { $0.split(with: `operator`.rawValue) }
-        }
+        return operandQueue
     }
     
     private static func makeOperatorQueue(with input: String) -> CalculatorItemQueue<Operator> {
