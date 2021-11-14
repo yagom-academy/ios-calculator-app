787afb1456d70e952c64272e6725957dce1e2ce9
junbangg <wnstjr4620@naver.com>
2021-11-14 22:12:57 +0900

    FEAT: ExpressionParser의 parse 를 검증하는 테스트의 helper 메서드 추가

--- Calculator/CalculatorTests/Tests/ExpressionParserTests.swift
+++ Calculator/CalculatorTests/Tests/ExpressionParserTests.swift
@@ -12,8 +12,34 @@ class ExpressionParserTests: XCTestCase {
     func testExpressionParserParse_givenMixedExpression_expectCorrectFormula() {
         let testOperands: [Double] = [10, 23, 4]
         let testOperators: [Operator] = [.add, .divide]
-        let mockFormula = createMockFormula(operands: testOperands, operators: testOperators)
+        let mockFormula = Formula(operands: createQueue(with: testOperands), operators: createQueue(with: testOperators))
         XCTAssertTrue(isSameFormula(formula: ExpressionParser.parse(from: "10+23/4"), otherFormula: mockFormula))
+    }
+    
+    private func createQueue<T>(with elements: [T]) -> CalculatorItemQueue<T> {
+        var queue = CalculatorItemQueue<T>()
+        for element in elements {
+            queue.enqueue(element)
+        }
+        return queue
+    }
+    
+    private func isSameFormula(formula: inout Formula, otherFormula: inout Formula) -> Bool {
+        let firstFormulaOperands = convertToArray(from: &formula.operands)
+        let firstFormulaOperators = convertToArray(from: &formula.operators)
+        let secondFormulaOperands = convertToArray(from: &formula.operands)
+        let secondFormulaOperators = convertToArray(from: &formula.operators)
+        return firstFormulaOperands == secondFormulaOperands && firstFormulaOperators == secondFormulaOperators
+    }
+    
+    private func convertToArray<T>(from queue: inout CalculatorItemQueue<T>) -> [T] {
+        var elements: [T] = []
+        while queue.isNotEmpty {
+            if let element = queue.dequeue() {
+                elements.append(element)
+            }
+        }
+        return elements
     }
     
 }
