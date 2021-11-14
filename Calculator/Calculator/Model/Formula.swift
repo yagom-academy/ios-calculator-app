a12abf8f0966ad115333fdc4f79b542810aaff10
ICS-Asan <carpediem1015@gmail.com>
2021-11-14 23:33:03 +0900

    feat: Formula내부 result메서드 추가 #8

--- Calculator/Calculator/Model/Formula.swift
+++ Calculator/Calculator/Model/Formula.swift
@@ -10,4 +10,14 @@ import Foundation
 struct Formula {
     var operands = CalculatorItemQueue<Double>()
     var operators = CalculatorItemQueue<Operator>()
+    
+    mutating func result() throws -> Double {
+        var result = try operands.dequeue()
+        while !operators.inbox.isEmpty {
+            result = try operators.dequeue().calculate(
+                lhs: result,
+                rhs: try operands.dequeue())
+        }
+        return result
+    }
 }
