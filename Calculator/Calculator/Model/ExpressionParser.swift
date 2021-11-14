697554178afe55391fcf8531981baed3eb453d74
July911 <min95911@nate.com>
2021-11-15 03:47:03 +0900

    test : ExpressionParser mock interface 생성 및 테스트 결과에 따른 함수 내부 수정

--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -2,30 +2,30 @@
 import Foundation
 
 enum ExpressionParser {
-    func parse(from input: String) {
-        
-        
-    }
+    static func parse(from input: String) {
     
-    private func componentsByOperators(from input: String) -> [String] {
-        let operators = Operation.allCases.map{ $0.rawValue }
-        var inputs: [String] = [input]
-        
-        for operand in operators {
-            let inputsCount = inputs.count
-            let indexOfFilteredByOperand: [Int] = ((0..<inputsCount).map { inputs[$0].contains(operand) ? $0 : nil}).compactMap { $0 }
-            let filteredInputsByHavingOperand: [String] = inputs.filter { $0.contains(operand) }
-            
-            for index in indexOfFilteredByOperand {
-                let notSplitedIndex = indexOfFilteredByOperand[index]
-                let splitedByOperand = filteredInputsByHavingOperand[index].split(with: operand)
-                inputs.insert(contentsOf: splitedByOperand.flatMap{ $0 }, at: notSplitedIndex)
-                inputs.remove(at: notSplitedIndex + splitedByOperand.count)
-            }
-        }
-        
-        return inputs
     }
+    func componentsByOperators(from input: String) -> [String] {
+       let operators = Operation.allCases.map{ $0.rawValue }
+       var inputs: [String] = [input]
+       
+       for operand in operators {
+           let inputsCount = inputs.count
+           let indexOfFilteredByOperand: [Int] = ((0..<inputsCount).map { inputs[$0].contains(operand) ? $0 : nil}).compactMap { $0 }
+           let filteredInputsByHavingOperand: [String] = inputs.filter { $0.contains(operand) }
+           var splitedCount = 0
+           
+           for index in indexOfFilteredByOperand {
+               splitedCount += 1
+               let splitedByOperand = filteredInputsByHavingOperand[splitedCount - 1 ].split(with: operand)
+               inputs.insert(contentsOf: splitedByOperand.flatMap{ $0 }, at: index)
+               inputs.remove(at: index + splitedByOperand.count)
+           }
+       }
+       
+       return inputs
+   }
+
 }
 
 
