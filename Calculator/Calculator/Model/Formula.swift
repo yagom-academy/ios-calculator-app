0217744afc798368c196ae1dc4070fb81e7e1ef3
junbangg <wnstjr4620@naver.com>
2021-11-14 16:04:57 +0900

    FEAT: formula.result 동작 확인하는 테스트 통과

---
+++ Calculator/Calculator/Model/Formula.swift
@@ -0,0 +1,24 @@
+//
+//  Formula.swift
+//  Calculator
+//
+//  Created by Jun Bang on 2021/11/14.
+//
+
+import Foundation
+
+struct Formula {
+    var operands: CalculatorItemQueue<Double>
+    var operators: CalculatorItemQueue<Operator>
+    
+    mutating func result() -> Double {
+        guard var resultValue = operands.dequeue() else {
+            return Double.nan
+        }
+        while let _operator = operators.dequeue(),
+              let operand = operands.dequeue() {
+            resultValue = _operator.calculate(lhs: resultValue, rhs: operand)
+        }
+        return resultValue
+    }
+}
--- Calculator/CalculatorTests/Tests/FormulaTests.swift
+++ Calculator/CalculatorTests/Tests/FormulaTests.swift
@@ -9,14 +9,14 @@ import XCTest
 @testable import Calculator
 
 class FormulaTests: XCTestCase {
-    var mockOperandCalculatorItemQueue!
-    var mockOperatorCalculatorItemQueue!
+    var mockOperandCalculatorItemQueue: CalculatorItemQueue<Double>!
+    var mockOperatorCalculatorItemQueue: CalculatorItemQueue<Operator>!
     var sut: Formula!
     
     override func setUpWithError() throws {
         mockOperandCalculatorItemQueue = CalculatorItemQueue<Double>()
         mockOperatorCalculatorItemQueue = CalculatorItemQueue<Operator>()
-        sut = Formula()
+        sut = Formula(operands: mockOperandCalculatorItemQueue, operators: mockOperatorCalculatorItemQueue)
     }
 
     override func tearDownWithError() throws {
@@ -27,9 +27,15 @@ class FormulaTests: XCTestCase {
     
     func testFormulaResult_given30Plus20Minus10_expect40() {
         let testOperators: [Operator] = [.add, .subtract]
-        setup(sut.operators, with: testOperators)
+        setup(queue: &sut.operators, with: testOperators)
         let testOperands: [Double] = [30, 20, 10]
-        setup(sut.operands, with: testOperands)
+        setup(queue: &sut.operands, with: testOperands)
         XCTAssertEqual(sut.result(), 40)
+    }
+    
+    private func setup<T>(queue: inout CalculatorItemQueue<T>, with testData: [T]) {
+        for data in testData {
+            queue.enqueue(data)
+        }
     }
 }
