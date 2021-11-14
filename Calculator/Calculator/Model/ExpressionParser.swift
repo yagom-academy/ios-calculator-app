a9a63da4b99311207c736d3b7433455b836851c3
llghdud921 <llghdud921@naver.com>
2021-11-15 04:27:59 +0900

    feat: componentsByOperator 메소드를 이용하여 parse 메소드 구현

--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -8,8 +8,14 @@
 import Foundation
 
 enum ExpressionParser {
-    static func parse(from input: String) {
-        componentByOperators(from: input)
+    static func parse(from input: String) -> Formula {
+        var operands = CalculatorItemQueue()
+        componentByOperators(from: input).compactMap { Double($0) }.forEach { operands.enqueue(number: $0) }
+        
+        var operators = CalculatorItemQueue()
+        Operator.allCases.filter{ componentByOperators(from: input).contains( String($0.rawValue)) }.forEach { operators.enqueue(operatorItem: $0) }
+        
+        return Formula(operands: operands, operators: operators)
     }
     
     private static func componentByOperators(from input: String) -> [String] {
