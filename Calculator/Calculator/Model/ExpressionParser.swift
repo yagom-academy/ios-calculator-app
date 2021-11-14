0c3da5bc96a9864442a2f640f758f8004e3f9d76
JT <dd3557@naver.com>
2021-11-14 22:37:20 +0900

    feat: Enumeration ExpressionParser 타입 추가 및 메소드 임시 구현

---
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -0,0 +1,18 @@
+//
+//  ExpressionParser.swift
+//  Calculator
+//
+//  Created by 김진태 on 2021/11/14.
+//
+
+import Foundation
+
+enum ExpressionParser {
+    func parse(from input: String) -> Formula {
+        return Formula()
+    }
+    
+    private func componentsByOperators(from input: String) -> [String] {
+        return []
+    }
+}
