7a5f6a3cab372b7c15e95b222a19a421339577aa
JT <dd3557@naver.com>
2021-11-14 21:23:49 +0900

    feat: Enumeration Operator 타입에 메소드 추가
    - UML에 나온 요구 사항에 맞추어 총 5개의 메소드 추가함
    - 외부에서 접근할 수 있는 1개의 메소드와 내부에서만 사용하는 4개의 메소드 추가함
    - 각 5개의 메소드는 제대로 구현되지 않음. 임시로 무조건 0.0을 return하도록 코드 작성

--- Calculator/Calculator/Model/Operator.swift
+++ Calculator/Calculator/Model/Operator.swift
@@ -12,4 +12,24 @@ enum Operator: Character, CaseIterable, CalculateItem {
     case subtract = "-"
     case divide = "/"
     case multiply = "*"
+    
+    func calculate(lhs: Double, rls: Double) -> Double {
+        return 0.0
+    }
+    
+    private func add(lhs: Double, rls: Double) -> Double {
+        return 0.0
+    }
+    
+    private func subtract(lhs: Double, rls: Double) -> Double {
+        return 0.0
+    }
+    
+    private func divide(lhs: Double, rls: Double) -> Double {
+        return 0.0
+    }
+    
+    private func multiply(lhs: Double, rls: Double) -> Double {
+        return 0.0
+    }
 }
