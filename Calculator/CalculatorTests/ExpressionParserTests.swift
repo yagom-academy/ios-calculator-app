010a2405b10b9d18056063cfd2089431ed98a1f3
zoe <kny0651@naver.com>
2021-11-14 23:45:14 +0900

    test: ExpressionParserTests 테스트 추가

--- Calculator/CalculatorTests/ExpressionParserTests.swift
+++ Calculator/CalculatorTests/ExpressionParserTests.swift
@@ -20,4 +20,20 @@ class ExpressionParserTests: XCTestCase {
         try super.tearDownWithError()
         sut = nil
     }
+    
+    func test_parse메서드로_피연산자가_잘나누어지는지() {
+        var operands = sut.parse(from: "1+2-3*4/5").operands
+        
+        [1.0, 2.0, 3.0, 4.0, 5.0].forEach { element in
+            XCTAssertEqual(element, try! operands.dequeue())
+        }
+    }
+    
+    func test_parse메서드오_연산자가_잘나누어지는지() {
+        var operators = sut.parse(from: "1+2-3*4/5").operators
+        
+        [Operator.add, Operator.subtract, Operator.multiply, Operator.divide].forEach { element in
+            XCTAssertEqual(element, try! operators.dequeue())
+        }
+    }
 }
