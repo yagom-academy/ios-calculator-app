f50c97b8e33d14f6c4de881c236a902b66529035
smart8612 <smart8612@gmail.com>
2021-11-14 14:52:38 +0900

    test: Operator 열거형 테스트 케이스 작성

---
+++ Calculator/CalculatorTests/OperatorTests.swift
@@ -0,0 +1,117 @@
+//
+//  OperatorTests.swift
+//  CalculatorTests
+//
+//  Created by JeongTaek Han on 2021/11/14.
+//
+
+import XCTest
+
+class OperatorTests: XCTestCase {
+    
+    var sutAddOperator: Operator!
+    var sutSubtractOperator: Operator!
+    var sutDivideOperator: Operator!
+    var sutMultiplyOperator: Operator!
+
+    override func setUpWithError() throws {
+        sutAddOperator = .add
+        sutSubtractOperator = .subtract
+        sutDivideOperator = .divide
+        sutMultiplyOperator = .multiply
+    }
+
+    override func tearDownWithError() throws {
+        sutAddOperator = nil
+        sutSubtractOperator = nil
+        sutDivideOperator = nil
+        sutMultiplyOperator = nil
+    }
+
+    func test_Double형_11점0과_Double형_9점99를_add_operator로_연산하면_Double형_20점99이_반환되는가() throws {
+        // given
+        let leftOperand = 11.00
+        let rightOperand = 9.99
+
+        // when
+        let result = try sutAddOperator.calculate(lhs: leftOperand, rhs: rightOperand)
+
+        // then
+        XCTAssertEqual(result, 20.99)
+    }
+
+    func test_Double형_0점2와_Double형_0점1을_add_operator로_연산하면_Double형_0점3이_반환되는가() throws {
+        // given
+        let leftOperand = 0.2
+        let rightOperand = 0.1
+
+        // when
+        let result = try sutAddOperator.calculate(lhs: leftOperand, rhs: rightOperand)
+
+        // then
+        XCTAssertEqual(result, 0.3)
+    }
+
+    func test_Double형_10와_Double형_8을_substract_operator로_연산하면_Double형_2점0가_반환되는가() throws {
+        // given
+        let leftOperand = 10.0
+        let rightOperand = 8.0
+
+        // when
+        let result = try sutSubtractOperator.calculate(lhs: leftOperand, rhs: rightOperand)
+
+        // then
+        XCTAssertEqual(result, 2.0)
+    }
+
+    func test_Double형_8와_Double형_10을_substract_operator로_연산하면_Double형_음수_2점0가_반환되는가() throws {
+        // given
+        let leftOperand = 8.0
+        let rightOperand = 10.0
+
+        // when
+        let result = try sutSubtractOperator.calculate(lhs: leftOperand, rhs: rightOperand)
+
+        // then
+        XCTAssertEqual(result, -2.0)
+    }
+
+
+    func test_Double형_80와_Double형_10을_divide_operator로_연산하면_Double형_8점0가_반환되는가() throws {
+        // given
+        let leftOperand = 80.0
+        let rightOperand = 10.0
+
+        // when
+        let result = try sutDivideOperator.calculate(lhs: leftOperand, rhs: rightOperand)
+
+        // then
+        XCTAssertEqual(result, 8.0)
+    }
+    
+    func test_Double형_80와_Double형_0점0을_divide_operator로_연산하면_notANumber오류가_반환되는가() throws {
+        // given
+        let leftOperand = 80.0
+        let rightOperand = 0.0
+        
+        // when
+        let result = try sutDivideOperator.calculate(lhs: leftOperand, rhs: rightOperand)
+        
+        // then
+        XCTAssertThrowsError(result)
+    }
+    
+    func test_Double형_80와_Double형_10을_multiply_operator로_연산하면_Double형_800점0가_반환되는가() throws {
+        // given
+        let leftOperand = 80.0
+        let rightOperand = 10.0
+
+        // when
+        let result = try sutMultiplyOperator.calculate(lhs: leftOperand, rhs: rightOperand)
+
+        // then
+        XCTAssertEqual(result, 800.0)
+    }
+
+    
+}
