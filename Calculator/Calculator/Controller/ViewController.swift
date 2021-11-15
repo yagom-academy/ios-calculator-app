0b5cfc04b4ae40f317686ca97c3011cece51af2e
Ari <lee_ari95@icloud.com>
2021-11-16 01:27:59 +0900

    feat: 추가 operand, operator Label, setUpFormulaLabel 메소드 #5

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -8,11 +8,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var operandLabel: UILabel!
+    @IBOutlet weak var operatorLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUpFormulaLabel()
     }
-
-
+    
+    private func setUpFormulaLabel() {
+        operandLabel.text = "0"
+        operatorLabel.text = ""
+    }
 }
 
