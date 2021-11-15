b6a4f56a90180a0d2a4faca735ce4a33fbdb3de5
yeha <dev.yeha@gmail.com>
2021-11-15 22:18:25 +0900

    test: CalculatorItemQueue의 Int, String 타입 검사 Test Cases 삭제 #1

--- Calculator/CalculatorTests/CalculatorItemQueueTests.swift
+++ Calculator/CalculatorTests/CalculatorItemQueueTests.swift
@@ -2,31 +2,17 @@
 //  CalculatorItemQueueTests.swift
 //  CalculatorTests
 //
-//  Created by yeha on 2021/11/09.
+//  Created by yeha on 2021/11/15.
 //
 
 import XCTest
 
 class CalculatorItemQueueTests: XCTestCase {
-    func test_큐에_Int_추가하기() {
-        var queue = CalculatorItemQueue<Int>()
-        queue.enqueue(1)
-        queue.enqueue(-1)
-        XCTAssertEqual(queue.items, [1, -1])
-    }
-    
     func test_큐에_Double_추가하기() {
         var queue = CalculatorItemQueue<Double>()
         queue.enqueue(1.0)
         queue.enqueue(-1.0)
         XCTAssertEqual(queue.items, [1.0, -1.0])
-    }
-    
-    func test_큐에_연산자_추가하기() {
-        var queue = CalculatorItemQueue<String>()
-        queue.enqueue("/")
-        queue.enqueue("*")
-        XCTAssertEqual(queue.items, ["/", "*"])
     }
     
     func test_큐에서_Double_삭제하기() {
