bf98d54e67e4d77d8e1fe82520be9cb0b58018f5
forestjae <forestsj30@gmail.com>
2021-11-15 16:23:14 +0900

    feat: StoryBoard의 버튼에 연결할 IBAction 생성 및 Action부분을 extension을 사용하여 분리

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -11,7 +11,31 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
-
 }
 
+// MARK: - Actions
+extension ViewController {
+    @IBAction private func touchUpDigit(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction private func touchUpOperator(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction private func touchUpAllClear(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction private func touchUpClearError(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction private func touchUpSignConversion(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction private func touchUPEqualSign(_ sender: UIButton) {
+        print(1 + 3)
+    }
+}
