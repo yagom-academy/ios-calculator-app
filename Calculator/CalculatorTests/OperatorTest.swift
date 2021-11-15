36b5f5e85fc6bbb96aa4696b4df4fc7f3ea91fd0
ICS-Asan <carpediem1015@gmail.com>
2021-11-15 14:37:09 +0900

    test: calculate메서드 정수2개 연산 #9

--- Calculator/CalculatorTests/OperatorTest.swift
+++ Calculator/CalculatorTests/OperatorTest.swift
@@ -9,4 +9,23 @@ import XCTest
 
 class OperatorTest: XCTestCase {
     
+    func test_정수_2개_add연산자_계산() {
+        let result = Operator.add.calculate(lhs: 4, rhs: 2)
+        XCTAssertEqual(result, 6)
+    }
+    
+    func test_정수_2개_subtract연산자_계산() {
+        let result = Operator.subtract.calculate(lhs: 4, rhs: 2)
+        XCTAssertEqual(result, 2)
+    }
+    
+    func test_정수_2개_divide연산자_계산() {
+        let result = Operator.divide.calculate(lhs: 4, rhs: 2)
+        XCTAssertEqual(result, 2)
+    }
+    
+    func test_정수_2개_multiply연산자_계산() {
+        let result = Operator.multiply.calculate(lhs: 4, rhs: 2)
+        XCTAssertEqual(result, 8)
+    }
 }
