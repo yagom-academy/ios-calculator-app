d45a0c62690f7df194e77b81aee290c4402be1e4
junbangg <wnstjr4620@naver.com>
2021-11-15 21:01:35 +0900

    REFACTOR: 고차함수 체이닝에 대한 줄바꿈 수정

--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -9,17 +9,21 @@ import Foundation
 
 enum ExpressionParser {
     static func parse(from input: String) -> Formula {
-        let operatorRawValues = Operator.allCases.compactMap { $0.rawValue }
-        let operators = input.filter { operatorRawValues.contains($0) }
+        let operatorRawValues = Operator.allCases
+            .compactMap { $0.rawValue }
+        let operators = input
+            .filter { operatorRawValues.contains($0) }
             .compactMap{ Operator(rawValue: $0)}
         let operatorQueue = CalculatorItemQueue(elements: operators)
         let operands = componentsByOperators(from: input).compactMap { Double($0) }
         let operandQueue = CalculatorItemQueue(elements: operands)
+        
         return Formula(operands: operandQueue, operators: operatorQueue)
     }
     
     private static func componentsByOperators(from input: String) -> [String] {
-        let operatorCharacters = Operator.allCases.compactMap { $0.rawValue }
+        let operatorCharacters = Operator.allCases
+            .compactMap { $0.rawValue }
         let initialArray = [input]
         let operandStringArray = operatorCharacters.reduce(initialArray) { (array: [String], operatorCharacter: Character) in
             array.flatMap { $0.split(with: operatorCharacter) }
@@ -30,7 +34,8 @@ enum ExpressionParser {
 
 extension String {
     func split(with target: Character) -> [String] {
-        return self.split(separator: target)
+        return self
+            .split(separator: target)
             .compactMap { String($0) }
     }
 }
