29481f1b667cc120185498728b35f0b6f24a8683
yeahg-dev <yeahg.dev@gmail.com>
2021-11-16 02:39:40 +0900

    feat: ExpressionParser 열거형 구현

---
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -0,0 +1,28 @@
+//
+//  ExpressionParser.swift
+//  Calculator
+//
+//  Created by 1 on 2021/11/15.
+//
+
+import Foundation
+
+enum ExpressionParser {
+
+    static func parse(from input: String) -> Formula {
+        let calcultorItems = ["➕","➖", "➗", "✖️"]
+        let stringOperators = input.filter { calcultorItems.contains(String($0))}
+        let stringOperands = componentsByOperator(from: input)
+        let operators = CalculatorItemQueue(stringOperators.map({Operator(rawValue: $0)!}))
+        let operands = CalculatorItemQueue(stringOperands.map({Double($0)!}))
+        return Formula(operands: operands, operators: operators)
+    }
+
+    static private func componentsByOperator(from input: String) -> [String] {
+        let initialArray = [input]
+        let components = Operator.allCases.reduce(initialArray){(array: [String], operators: Operator) -> [String] in
+            return array.flatMap{$0.split(with: operators.rawValue)}
+        }
+        return components
+    }
+}
