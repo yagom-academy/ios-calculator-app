443a17e009dc5b32e6aa45317108d77a0b7d2cee
llghdud921 <llghdud921@naver.com>
2021-11-15 18:42:43 +0900

    test: Formula테스트 파일 내 Formula옵셔널 변수 제거

--- Calculator/CalculatorTests/FormulaTests.swift
+++ Calculator/CalculatorTests/FormulaTests.swift
@@ -10,37 +10,29 @@ import XCTest
 
 class FormulaTests: XCTestCase {
     
-    var formula: Formula?
-
-    override func setUpWithError() throws {
-        formula = Formula()
-    }
-    
-    override func tearDownWithError() throws {
-        formula = nil
-    }
+    var formula: Formula = Formula()
 
     func test_formula내_연산자피연산자프로퍼티를_가지고_연산결과를_나타낸다() {
         formula = ExpressionParser.parse(from: "15*-1+0.4")
         
-        XCTAssertEqual(formula?.result(), -14.6)
+        XCTAssertEqual(formula.result(), -14.6)
     }
     
     func test_formula내_연산자피연산자가nil값이면_0을_나타낸다() {
         formula = ExpressionParser.parse(from: "")
         
-        XCTAssertEqual(formula?.result(), 0.0)
+        XCTAssertEqual(formula.result(), 0.0)
     }
     
     func test_formula내_연산자nil값이면_피연산자만을_나타낸다() {
         formula = ExpressionParser.parse(from: "0.2")
         
-        XCTAssertEqual(formula?.result(), 0.2)
+        XCTAssertEqual(formula.result(), 0.2)
     }
 
     func test_formula내_0으로나누는_연산이_포함되는_계산식은_nan결과를_나타낸다() {
-        formula = ExpressionParser.parse(from: "15*-1/0.0")
+        formula = ExpressionParser.parse(from: "-1/0.0")
         
-        XCTAssertEqual(formula?.result().isNaN, true)
+        XCTAssertTrue(formula.result().isNaN)
     }
 }
