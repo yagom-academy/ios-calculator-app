bfc9a47adfc4b491e763489a83ee6864e7f8076f
ChaminLee <trueman9512@naver.com>
2021-11-15 16:05:56 +0900

    test: 에러 네이밍 변경에 따른 테스트 코드 수정

--- Calculator/CalculatorTests/FomulaTest.swift
+++ Calculator/CalculatorTests/FomulaTest.swift
@@ -21,7 +21,7 @@ class FomulaTest: XCTestCase {
     
     func test_처음_연산할_때_피연산자가_없는_상태인_경우_에러를_반환하는지() throws {
         XCTAssertThrowsError(try formula.result()) { error in
-            XCTAssertEqual(error as? QueueError, QueueError.queueIsEmpty)
+            XCTAssertEqual(error as? DataExtractionError, DataExtractionError.hasNoElement)
         }
     }
     
