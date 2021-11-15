e3547d8923196ac8f74d08546ed307edcd76d55b
zoe <kny0651@naver.com>
2021-11-15 17:58:23 +0900

    refactor: 빼기연산자를 _(underScore)로 변경

--- Calculator/CalculatorTests/ExpressionParserTests.swift
+++ Calculator/CalculatorTests/ExpressionParserTests.swift
@@ -22,15 +22,15 @@ class ExpressionParserTests: XCTestCase {
     }
     
     func test_parse메서드로_피연산자가_잘나누어지는지() {
-        var operands = sut.parse(from: "1+2-3*4/5").operands
+        var operands = sut.parse(from: "1+2_3*4/5").operands
         
         [1.0, 2.0, 3.0, 4.0, 5.0].forEach { element in
             XCTAssertEqual(element, try! operands.dequeue())
         }
     }
     
-    func test_parse메서드오_연산자가_잘나누어지는지() {
-        var operators = sut.parse(from: "1+2-3*4/5").operators
+    func test_parse메서드로_연산자가_잘나누어지는지() {
+        var operators = sut.parse(from: "1+2_3*4/5").operators
         
         [Operator.add, Operator.subtract, Operator.multiply, Operator.divide].forEach { element in
             XCTAssertEqual(element, try! operators.dequeue())
