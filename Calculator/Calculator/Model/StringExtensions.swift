d0f3807e7839ba8645b4f549c5eab99a6544f8d9
yeha <dev.yeha@gmail.com>
2021-11-15 15:43:01 +0900

    feat: String extension 생성 #8

---
+++ Calculator/Calculator/Model/StringExtensions.swift
@@ -0,0 +1,14 @@
+//
+//  StringExtensions.swift
+//  Calculator
+//
+//  Created by yeha on 2021/11/15.
+//
+
+import Foundation
+
+extension String {
+    func split(with target: Character) -> [String] {
+        return ["1"]
+    }
+}
