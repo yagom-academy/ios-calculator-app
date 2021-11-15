99e3d51cee6352f04a0383dfe2ac6b3dac03de63
yeha <dev.yeha@gmail.com>
2021-11-15 20:08:29 +0900

    test: OperatorTests에 사칙연산 관련 Test Cases 추가 #9

--- Calculator/CalculatorTests/OperatorTests.swift
+++ Calculator/CalculatorTests/OperatorTests.swift
@@ -8,4 +8,44 @@
 import XCTest
 
 class OperatorTests: XCTestCase {
+    func test_rawValue() {
+        let operators = Operator.add.rawValue
+        XCTAssertEqual(operators, "+")
+    }
+    
+    func test_더하기_연산() {
+        let operators = Operator(rawValue: "+")
+        let result = operators?.calculate(1.1, 1.1)
+        XCTAssertEqual(result, 2.2)
+    }
+    
+    func test_음수_더하기_연산() {
+        let operators = Operator(rawValue: "+")
+        let result = operators?.calculate(-1.1, -1.1)
+        XCTAssertEqual(result, -2.2)
+    }
+    
+    func test_빼기_연산() {
+        let operators = Operator(rawValue: "-")
+        let result = operators?.calculate(1.1, 1.1)
+        XCTAssertEqual(result, 0.0)
+    }
+    
+    func test_음수_빼기_연산() {
+        let operators = Operator(rawValue: "-")
+        let result = operators?.calculate(1.1, -1.1)
+        XCTAssertEqual(result, 2.2)
+    }
+    
+    func test_나누기_연산() {
+        let operators = Operator(rawValue: "/")
+        let result = operators?.calculate(1.1, 1.1)
+        XCTAssertEqual(result, 1.0)
+    }
+    
+    func test_곱하기_연산() {
+        let operators = Operator(rawValue: "*")
+        let result = operators?.calculate(1.1, 0.0)
+        XCTAssertEqual(result, 0.0)
+    }
 }
