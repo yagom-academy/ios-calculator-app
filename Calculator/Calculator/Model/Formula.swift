85b4919f1b13fdd0a8fea445e63cb6c6374904e5
just1103 <hyojuson1103@gmail.com>
2021-11-16 03:17:04 +0900

    feat: FormulaError 열거형 및 Test Case 추가 #3

--- Calculator/Calculator/Model/Formula.swift
+++ Calculator/Calculator/Model/Formula.swift
@@ -4,15 +4,30 @@ struct Formula {
     var operands: CalculatorItemQueue<Double> = CalculatorItemQueue()
     var operators: CalculatorItemQueue<Operator> = CalculatorItemQueue()
     
-    mutating func result() -> Double {
+    mutating func result() throws -> Double { // TODO: do-catch 구문 필요
         var result: Double
-        guard let removedOperand = operands.dequeue() else { return 0.0 } // throw 처리 필요
+        guard let removedOperand = operands.dequeue() else {
+            throw FormulaError.queueIsEmpty
+        }
         
-        result = operators.scanAllValues().reduce(removedOperand) { 
-            guard let nextOperand = operands.dequeue() else { return 0.0 } // throw 처리 필요
+        result = try operators.scanAllValues().reduce(removedOperand) {
+            guard let nextOperand = operands.dequeue() else {
+                throw FormulaError.queueIsEmpty
+            }
             return $1.calculate(lhs: $0, rhs: nextOperand)
         }
         
         return result
+    }
+}
+
+enum FormulaError: Error, LocalizedError {
+    case queueIsEmpty
+    
+    var description: String {
+        switch self {
+        case .queueIsEmpty:
+            return "Dequeue 할 값이 존재하지 않습니다"
+        }
     }
 }
--- Calculator/CalculatorTests/FormulaTests.swift
+++ Calculator/CalculatorTests/FormulaTests.swift
@@ -1,17 +1,49 @@
 import XCTest
 
 class FormulaTests: XCTestCase {
-    func test_sampleString을_parse하고_반환된_Formula의_결과값은_4점545454545454545이다() {
+    func test_sampleString을_parse하고_반환된_Formula를_result하면_4점545454545454545이_반환된다() {
         let sampleString: String = "11+2-33*4/55+6"
         var formula = ExpressionParser.parse(from: sampleString)
-        let result = formula.result()
-        XCTAssertEqual(result, 4.545454545454545)
+        do {
+            let result = try formula.result()
+            XCTAssertEqual(result, 4.545454545454545)
+        } catch {
+            print(error)
+        }
     }
     
-    func test_sampleString을_parse하고_반환된_Formula의_결과값은_51점25이다() {
+    func test_sampleString을_parse하고_반환된_Formula를_result하면_51점25이_반환된다() {
         let sampleString: String = "1+2*3/4+55+3-9"
         var formula = ExpressionParser.parse(from: sampleString)
-        let result = formula.result()
-        XCTAssertEqual(result, 51.25)
+        do {
+            let result = try formula.result()
+            XCTAssertEqual(result, 51.25)
+        } catch {
+            print(error)
+        }
+    }
+    
+    func test_빈_String을_parse하고_반환된_Formula를_result하면_queueIsEmpty에러가_발생한다() {
+        let sampleString: String = ""
+        var formula = ExpressionParser.parse(from: sampleString)
+        do {
+            let _ = try formula.result()
+        } catch FormulaError.queueIsEmpty {
+            XCTAssertEqual(FormulaError.queueIsEmpty.description, "Dequeue 할 값이 존재하지 않습니다")
+        } catch  {
+            print(error)
+        }
+    }
+    
+    func test_1더하기_String을_parse하고_반환된_Formula를_result하면_queueIsEmpty에러가_발생한다() {
+        let sampleString: String = "1+"
+        var formula = ExpressionParser.parse(from: sampleString)
+        do {
+            let _ = try formula.result()
+        } catch FormulaError.queueIsEmpty {
+            XCTAssertEqual(FormulaError.queueIsEmpty.description, "Dequeue 할 값이 존재하지 않습니다")
+        } catch  {
+            print(error)
+        }
     }
 }
