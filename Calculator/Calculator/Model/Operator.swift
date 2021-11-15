fe1174e12502b8efaa8bd70ce847c293f28317fe
yeha <dev.yeha@gmail.com>
2021-11-15 20:11:49 +0900

    feat: Operator enum에 사칙연산 후 결과 반환하는 메서드 구현

--- Calculator/Calculator/Model/Operator.swift
+++ Calculator/Calculator/Model/Operator.swift
@@ -14,6 +14,31 @@ enum Operator: Character, CaseIterable, CalculateItem {
     case multiply = "*"
     
     func calculate(_ lhs: Double, _ rhs: Double) -> Double {
-        return 1.1
+        switch self {
+        case .add:
+            return add(lhs, rhs)
+        case .subtract:
+            return substract(lhs, rhs)
+        case .divide:
+            return divide(lhs, rhs)
+        case .multiply:
+            return multiply(lhs, rhs)
+        }
+    }
+    
+    private func add(_ lhs: Double, _ rhs: Double) -> Double {
+        return lhs + rhs
+    }
+    
+    private func substract(_ lhs: Double, _ rhs: Double) -> Double {
+        return lhs - rhs
+    }
+    
+    private func divide(_ lhs: Double, _ rhs: Double) -> Double {
+        return lhs / rhs
+    }
+    
+    private func multiply(_ lhs: Double, _ rhs: Double) -> Double {
+        return lhs * rhs
     }
 }
