695028d3ba02567e8cf4aea68ad413bd480f98ff
wooyani77 <wooyani77@gmail.com>
2021-11-16 02:07:40 +0900

    feat: extension 추가 #3
    - string을 분리하여 배열로 반환하는 split 메서드 추가

--- Calculator/Calculator/Extension/DoubleExtension.swift.swift
+++ Calculator/Calculator/Extension/DoubleExtension.swift.swift
@@ -7,3 +7,6 @@
 
 import Foundation
 
+extension Double: CalculateItem {
+  
+}
--- Calculator/Calculator/Extension/StringExtension.swift
+++ Calculator/Calculator/Extension/StringExtension.swift
@@ -7,4 +7,8 @@
 
 import Foundation
 
-
+extension String {
+    func split(with target: Character) -> [String] {
+        self.split(separator: target).map { String($0) }
+    }
+}
