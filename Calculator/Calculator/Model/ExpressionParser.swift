7f3e629a517ee6ce06448c8a5f194dab357ce21d
wooyani77 <wooyani77@gmail.com>
2021-11-16 02:23:59 +0900

    feat: ExpressionParser에 componentsByOperators() 추가 #3
    - 연산자를 기준으로 피연산자 문자열을 분리하기 위해 메서드 추가

--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+enum ExpressionParser {
+    static func componentsByOperators(from input: String) -> [String] {
+        var result: [String] = [input]
+        Operator.allCases.forEach { `operator` in
+            var splitedString: [String] = []
+            for input in result {
+                splitedString += input.split(with: `operator`.rawValue)
+                result = splitedString
+            }
+        }
+        return result
+    }
+}
