ee89f9f5dbac38f0c4a623301061bd374682e4f4
yeha <dev.yeha@gmail.com>
2021-11-15 16:35:19 +0900

    test: ExpressionParserTests 및 ExpressionParser enum 생성 #6

---
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -0,0 +1,16 @@
+//
+//  ExpressionParser.swift
+//  Calculator
+//
+//  Created by yeha on 2021/11/15.
+//
+
+import Foundation
+
+enum ExpressionParser {
+    func parse(from input: String) -> Formula {
+    }
+    
+    private func componentsByOperators(from input: String) -> [String] {
+    }
+}
---
+++ Calculator/CalculatorTests/ExpressionParserTests.swift
@@ -0,0 +1,11 @@
+//
+//  ExpressionParserTests.swift
+//  CalculatorTests
+//
+//  Created by yeha on 2021/11/15.
+//
+
+import XCTest
+
+class ExpressionParserTests: XCTestCase {
+}
