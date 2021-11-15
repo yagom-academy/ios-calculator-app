e481151aa111236b9a01008b9baa19ce8da03650
oksunwoo <oksunwo@naver.com>
2021-11-16 01:16:22 +0900

    test: 0.1과 0.4를 더했을 때의 테스트 실패

--- Calculator/Calculator/Model/Operator.swift
+++ Calculator/Calculator/Model/Operator.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+
+enum Operator: Character, CaseIterable, CalculatorItem {
+    case add = "+"
+    case subtract = "₋"
+    case divide = "/"
+    case multiply = "*"
+    
+    func calculate(lhs: Double, rhs: Double) -> Double {
+        var calculateResult: Double
+        switch self {
+        case .add:
+            calculateResult = add(lhs: lhs, rhs: rhs)
+        case .subtract:
+            calculateResult = subtract(lhs: lhs, rhs: rhs)
+        case .divide:
+            calculateResult = divide(lhs: lhs, rhs: rhs)
+        case .multiply:
+            calculateResult = multiply(lhs: lhs, rhs: rhs)
+        }
+        return calculateResult
+    }
+    private func add(lhs: Double, rhs: Double) -> Double {
+        return Double()
+    }
+    
+    private func subtract(lhs: Double, rhs: Double) -> Double {
+        return Double()
+    }
+    
+    private func divide(lhs: Double, rhs: Double) -> Double {
+        return Double()
+    }
+    
+    private func multiply(lhs: Double, rhs: Double) -> Double {
+        return Double()
+    }
+}
--- Calculator/CalculatorQueueTests/OperatorTests.swift
+++ Calculator/CalculatorQueueTests/OperatorTests.swift
@@ -6,27 +6,23 @@
 //
 
 import XCTest
-
+@testable import Calculator
 class OperatorTests: XCTestCase {
-
+    var sut: Operator!
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        try super.setUpWithError()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        try super.tearDownWithError()
+        sut = nil
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func test_Operator_add에_0점4_0점1을_더하면_0점5가_되는지 () {
+        sut = .add
+        let result = sut.calculate(lhs: 0.4, rhs: 0.1)
+        
+        XCTAssertEqual(result, 0.5)
     }
 
 }
