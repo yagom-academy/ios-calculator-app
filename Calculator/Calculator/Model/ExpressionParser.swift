774a034112fda23ad13f7c31802e604dec75bf93
junbangg <wnstjr4620@naver.com>
2021-11-15 22:32:20 +0900

    REFACTOR: ExpressionParser의 componentsByOperators 내에 있는 reduce 내의 리턴 타입 명시

--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -24,7 +24,7 @@ enum ExpressionParser {
         let initialArray = [input]
         let operatorCharacters = Operator.allCases
             .compactMap { $0.rawValue }
-        let operandStringArray = operatorCharacters.reduce(initialArray) { (array: [String], operatorCharacter: Character) in
+        let operandStringArray = operatorCharacters.reduce(initialArray) { (array: [String], operatorCharacter: Character) -> [String] in
             array.flatMap { $0.split(with: operatorCharacter) }
         }
         return operandStringArray
