ee5dbf8484c51ddfc2f24322c87c6df5d11f018c
Jager-yoo <y73447jh@gmail.com>
2021-11-16 16:14:14 +0900

    feat: ExpressionParser 열거형의 parse 메서드 구현 #8

--- Calculator/Calculator/Model/enumeration/ExpressionParser.swift
+++ Calculator/Calculator/Model/enumeration/ExpressionParser.swift
@@ -9,7 +9,17 @@ import Foundation
 
 enum ExpressionParser {
     
-    static func componentsByOperators(from input: String) -> [String] {
+    static func parse(from input: String) -> Formula {
+        let whiteSpace: Character = " "
+        let splitedInput = input.split(with: whiteSpace)
+        let rawValueOperators = componentsByOperators(from: input)
+        let operands = CalculatorItemQueue(queue: splitedInput.compactMap { Double($0) })
+        let operators = CalculatorItemQueue(queue: rawValueOperators.compactMap { Operator(rawValue: Character($0)) })
+        
+        return Formula(operands: operands, operators: operators)
+    }
+    
+    private static func componentsByOperators(from input: String) -> [String] {
         let operatorRawValues = Operator.allCases.map { $0.rawValue.description }
         let whiteSpace: Character = " "
         let splitedInput = input.split(with: whiteSpace)
