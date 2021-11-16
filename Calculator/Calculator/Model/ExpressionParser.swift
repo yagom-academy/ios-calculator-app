cb128f0a03dbe52f2fbd020f0d8ff8a1b2805c4a
JT <dd3557@naver.com>
2021-11-17 01:11:56 +0900

    refactor: ExpressionParser 타입의 componentsByOperators 메소드 수정

--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -31,15 +31,41 @@ enum ExpressionParser {
     
     private static func componentsByOperators(from input: String) -> [String] {
         let allOperatorSymbols = Operator.allCases.map { $0.rawValue }
+        let multiplyDivideSymbols = [Operator.multiply.rawValue, Operator.divide.rawValue]
+        let signs = [Operator.add.rawValue, Operator.subtract.rawValue]
+        let emptySpace = Character(" ")
+        
+        let multiplyDivideSymbolsWithSign = multiplyDivideSymbols.flatMap { operatorSymbol in
+            signs.map { sign in "\(operatorSymbol)\(sign)" }
+        }
         
         let inputWithoutWhitespace = input.components(separatedBy: .whitespacesAndNewlines).joined()
         
-        let components = allOperatorSymbols.reduce([inputWithoutWhitespace]) {
+        var components = multiplyDivideSymbolsWithSign.reduce([inputWithoutWhitespace]) {
+            (result: [String], operatorSymbolWithSign: String) in
+            guard let operatorSymbol = operatorSymbolWithSign.first else { return result }
+            guard let sign = operatorSymbolWithSign.last else { return result }
+            let operatorSymbolWithEmptySpaceAndSign = "\(emptySpace)\(operatorSymbol)\(emptySpace)\(sign)"
+            
+            return result
+                        .map {
+                            $0.replacingOccurrences(of: operatorSymbolWithSign, with: operatorSymbolWithEmptySpaceAndSign)
+                        }
+                        .flatMap { $0.components(separatedBy: "\(emptySpace)") }
+        }
+        
+        components = allOperatorSymbols.reduce(components) {
             (result: [String], operatorSymbol: Character) in
             return result
-                .flatMap { component -> [String] in
-                    return component.split(with: operatorSymbol)
-                }
+                        .flatMap { component -> [String] in
+                            guard let firstCharacter = component.first else { return [component] }
+                            let result = signs.contains(firstCharacter)
+                                            && component.count > 1
+                                            && component.filter { allOperatorSymbols.contains($0) }.count == 1
+                                                ? [component]
+                                                : component.split(with: operatorSymbol)
+                            return result
+                        }
         }
         
         return components
