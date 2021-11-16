c2f31b353f2786bd5cdceda3cfd1f6276d4d4bf5
smart8612 <smart8612@gmail.com>
2021-11-16 12:21:30 +0900

    test: Expression Parser 수식 띄어쓰기 방식의 수식을 이어적은 방식으로 수정

--- Calculator/CalculatorTests/CalculateParserTests/ExpressionParserTests.swift
+++ Calculator/CalculatorTests/CalculateParserTests/ExpressionParserTests.swift
@@ -23,7 +23,7 @@ class ExpressionParserTests: XCTestCase {
 
     func test_2_더하기_3_빼기_4_더하기_5의_parse_result로_6을_반환하는가() {
         // given
-        let form = "2 + 3 − 4 + 5"
+        let form = "2+3−4+5"
         
         // when
         do {
@@ -38,7 +38,7 @@ class ExpressionParserTests: XCTestCase {
     
     func test_2_더하기_3_곱하기_3_빼기_1의_result로_14를_반환하는가() {
         // given
-        let form = "2 + 3 × 3 − 1"
+        let form = "2+3×3−1"
         
         // when
         do {
@@ -54,7 +54,7 @@ class ExpressionParserTests: XCTestCase {
     
     func test_3_나누기_3_더하기_2_빼기_1의_result로_2를_반환하는가() {
         // given
-        let form = "3 ÷ 3 + 2 − 1"
+        let form = "3÷3+2−1"
         
         // when
         do {
@@ -70,7 +70,7 @@ class ExpressionParserTests: XCTestCase {
     
     func test_1_더하기_2_빼기_3_곱하기_2_빼기_3_나누기_6_의_result로_음수0점5를_반환하는가() {
         // given
-        let form = "1 + 2 − 3 × 2 − 3 ÷ 6"
+        let form = "1+2−3×2−3÷6"
         
         // when
         do {
@@ -86,7 +86,7 @@ class ExpressionParserTests: XCTestCase {
     
     func test_1_더하기_2_빼기_3_곱하기_2_빼기_음수3_나누기_6_의_result로_1점5를_반환하는가() {
         // given
-        let form = "1 + 2 − -3 × 2 − 3 ÷ 6"
+        let form = "1+2−-3×2−3÷6"
         
         // when
         do {
@@ -102,7 +102,7 @@ class ExpressionParserTests: XCTestCase {
     
     func test_1_더하기_음수2_빼기_3_곱하기_2_빼기_3_나누기_6_의_result로_음수11점0_나누기_6점0를_반환하는가() {
         // given
-        let form = "1 + -2 − 3 × 2 − 3 ÷ 6"
+        let form = "1+-2−3×2−3÷6"
         
         // when
         do {
@@ -118,7 +118,7 @@ class ExpressionParserTests: XCTestCase {
     
     func test_1_더하기_2_빼기_3_곱하기_음수2_빼기_3_나누기_6_의_result로_음수0점5를_반환하는가() {
         // given
-        let form = "1 + 2 − 3 × -2 − 3 ÷ 6"
+        let form = "1+2−3×-2−3÷6"
         
         // when
         do {
@@ -134,7 +134,7 @@ class ExpressionParserTests: XCTestCase {
     
     func test_1_더하기_2_빼기_3_곱하기_2_빼기_3_나누기_음수6_의_result로_0점5를_반환하는가() {
         // given
-        let form = "1 + 2 − 3 × 2 − 3 ÷ -6"
+        let form = "1+2−3×2−3÷-6"
         
         // when
         do {
@@ -150,7 +150,7 @@ class ExpressionParserTests: XCTestCase {
     
     func test_잘못된_연산자를_포함하여_1_더하기_2_빼기_3_곱하기_2_빼기_3_나누기_음수6_의_result로_오류를_반환하는가() {
         // given
-        let form = "1 + 2 − 3 * 2 − 3 / -6"
+        let form = "1+2−3*2−3/-6"
         
         // when
         // then
