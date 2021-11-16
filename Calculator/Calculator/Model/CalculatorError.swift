ef700c15b1c1d0b07d11a8c4be184dc94dd2ad07
Ari <lee_ari95@icloud.com>
2021-11-16 21:39:22 +0900

    refactor: CalculatorError 파일 이름 수정과 에러 case 추가 #5

--- Calculator/Calculator/Model/CalculatorError.swift
+++ Calculator/Calculator/Model/CalculatorError.swift
@@ -11,6 +11,7 @@ enum CalculatorError: Error {
     case queueNotFound
     case wrongOperator
     case wrongOperand
+    case notNumber
     
     var description: String {
         switch self {
@@ -20,6 +21,8 @@ enum CalculatorError: Error {
             return "잘못된 연산자입니다."
         case .wrongOperand:
             return "잘못된 숫자입니다."
+        case .notNumber:
+            return "NaN"
         }
     }
 }
--- Calculator/Calculator/Model/Formula.swift
+++ Calculator/Calculator/Model/Formula.swift
@@ -38,6 +38,9 @@ struct Formula {
         if operators.isEmpty == false {
             operators.clear()
         }
+        guard currentOperand.isNaN == false else {
+            throw CalculatorError.notNumber
+        }
         return currentOperand
     }
 }
