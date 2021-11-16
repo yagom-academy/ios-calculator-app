07f86ed532f2e60af1be3780820f52f664dd5629
Kim-EunsooSilver <graysilver18@naver.com>
2021-11-16 22:21:26 +0900

    refactor: touchUpNumberPadButton 함수 내부 프로퍼티 이름 수정

--- Calculator/Calculator/Controller/CalculatorViewController.swift
+++ Calculator/Calculator/Controller/CalculatorViewController.swift
@@ -10,27 +10,34 @@ class CalculatorViewController: UIViewController {
 
     @IBOutlet weak var operandLabel: UILabel!
     @IBOutlet weak var operatorLabel: UILabel!
+    @IBOutlet weak var calculationHistory: UIStackView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        reset()
+    }
+    
+    func reset() {
+        operandLabel.text = "0"
+        operatorLabel.text = ""
     }
     
     @IBAction func touchUpNumberPadButton(_ sender: UIButton) {
-        guard let txt = operandLabel.text else {
+        guard let operandLabelText = operandLabel.text else {
             return
         }
         
-        guard let a = sender.titleLabel?.text else {
+        guard let tapedNumber = sender.titleLabel?.text else {
             return
         }
         
-        if txt == "0" {
-            if a == "0" || a == "00" {
+        if operandLabelText == "0" {
+            if tapedNumber == "0" || tapedNumber == "00" {
                 return
             }
         }
         
-        operandLabel.text = txt + a
+        operandLabel.text = operandLabelText + tapedNumber
         
     }
 }
