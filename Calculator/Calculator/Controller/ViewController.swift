4ac474446445795077c65be9bf798ebfb4ccee6b
ChaminLee <trueman9512@naver.com>
2021-11-16 14:57:02 +0900

    feat: 피연산자 버튼 액션 추가
    - 피연산자 버튼에 따라 알맞는 문자를 반환하도록 하는 matchOperandButton 메서드 추가
    - 피연산자 버튼 터치시 실행할 액션 touchUpOperandButton 메서드 추가
    - 기존 수식에 .이 있을 경우 더 이상 .이 추가 되지 않도록 조건 추가

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -21,9 +21,54 @@ class ViewController: UIViewController {
     @IBOutlet weak var eightButton: UIButton!
     @IBOutlet weak var nineButton: UIButton!
     
+    @IBOutlet weak var expression: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+
+    @IBAction func touchUpOperandButton(_ sender: UIButton) {
+        guard let operand = matchOperandButton(sender: sender) else {
+            return
+        }
+        
+        guard operand != "." || expression.text?.contains(".") == false else {
+            return
+        }
+        
+        expression.text?.append(operand)
+    }
+
+    func matchOperandButton(sender: UIButton) -> String? {
+        switch sender {
+        case zeroButton:
+            return "0"
+        case doubleZeroButton:
+            return "00"
+        case dotButton:
+            return "."
+        case oneButton:
+            return "1"
+        case twoButton:
+            return "2"
+        case threeButton:
+            return "3"
+        case fourButton:
+            return "4"
+        case fiveButton:
+            return "5"
+        case sixButton:
+            return "6"
+        case sevenButton:
+            return "7"
+        case eightButton:
+            return "8"
+        case nineButton:
+            return "9"
+        default:
+            return nil
+        }
     }
 
 
