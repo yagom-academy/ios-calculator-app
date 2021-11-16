d2ea0b4b852ae053b4d2a9b4ddcbc6779672b2e6
smart8612 <smart8612@gmail.com>
2021-11-16 14:23:21 +0900

    feat: componentsByOperators 파싱 형식 변경으로 인한 코드 구현 수정
    1 + 2 − 3 × 2 − 3 ÷ -6 방식으로 입력받던 수식을 1+2−3×2−3÷-6 로 입력받아서 처리하는 방식으로 수정

--- Calculator/Calculator/Model/CalculateParser/ExpressionParser.swift
+++ Calculator/Calculator/Model/CalculateParser/ExpressionParser.swift
@@ -12,22 +12,18 @@ enum ExpressionParser {
         let operands = CalculatorItemQueue<Double>()
         let operators = CalculatorItemQueue<Operator>()
         
-        let splittedInput = componentsByOperators(from: input)
+        let parsedInput = try componentsByOperators(from: input)
         
-        splittedInput
+        parsedInput[0]
+            .split(with: " ")
             .compactMap { Double($0) }
             .forEach {
                 operands.enqueue($0)
             }
         
-        try splittedInput
-            .filter { Double($0) == nil }
-            .map {
-                guard let newOperator = Operator(rawValue: Character($0)) else {
-                    throw CalculatorError.unknownOperator
-                }
-                return newOperator
-            }
+        parsedInput[1]
+            .split(with: " ")
+            .compactMap { Operator(rawValue: Character($0)) }
             .forEach {
                 operators.enqueue($0)
             }
@@ -35,8 +31,44 @@ enum ExpressionParser {
         return Formula(operands: operands, operators: operators)
     }
     
-    private static func componentsByOperators(from input: String) -> [String] {
-        input.split(with: " ")
+    private static func componentsByOperators(from input: String) throws -> [String] {
+        var operands: [String] = []
+        var operators: [String] = []
+        
+        var defaultOperators: Set<String> = []
+        Operator.allCases.forEach { (each: Operator) -> Void in
+            defaultOperators.insert(String(each.rawValue))
+        }
+        
+        var temporaryOperand = ""
+        for eachCharacter in input {
+            let convertedCharacter = String(eachCharacter)
+            
+            // 마지막이라면
+            if input.last == eachCharacter {
+                temporaryOperand += convertedCharacter
+                operands.append(temporaryOperand)
+                continue
+            }
+            
+            // 피연산자에 해당된다면
+            if Double(convertedCharacter) != nil || convertedCharacter == "-" {
+                temporaryOperand += convertedCharacter
+                continue
+            }
+            
+            // 연산자에 해당된다면
+            if defaultOperators.contains(convertedCharacter) {
+                operands.append(temporaryOperand)
+                operators.append(convertedCharacter)
+                temporaryOperand = ""
+                continue
+            }
+            
+            throw CalculatorError.unknownOperator
+        }
+        
+        return [operands.joined(separator: " "), operators.joined(separator: " ")]
     }
 }
 
