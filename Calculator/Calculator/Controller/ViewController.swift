a2fe0194f2cd258cb128650823738e563ea85c1c
ChaminLee <trueman9512@naver.com>
2021-11-16 14:55:01 +0900

    feat: 피연산자 버튼을 나타내는 Outlet 추가

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -8,6 +8,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var zeroButton: UIButton!
+    @IBOutlet weak var doubleZeroButton: UIButton!
+    @IBOutlet weak var dotButton: UIButton!
+    @IBOutlet weak var oneButton: UIButton!
+    @IBOutlet weak var twoButton: UIButton!
+    @IBOutlet weak var threeButton: UIButton!
+    @IBOutlet weak var fourButton: UIButton!
+    @IBOutlet weak var fiveButton: UIButton!
+    @IBOutlet weak var sixButton: UIButton!
+    @IBOutlet weak var sevenButton: UIButton!
+    @IBOutlet weak var eightButton: UIButton!
+    @IBOutlet weak var nineButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
