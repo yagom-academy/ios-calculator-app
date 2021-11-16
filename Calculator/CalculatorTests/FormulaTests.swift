47aef8ee5e8952b530fa83eb9fafe46e256c077c
yanghojoon <yanghojoon8487@gmail.com>
2021-11-16 17:48:56 +0900

    refactor: UnitTest에서 강제 언래핑 제거 #8

--- Calculator/CalculatorTests/FormulaTests.swift
+++ Calculator/CalculatorTests/FormulaTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 
 class FormulaTests: XCTestCase {
-    var sut: Formula!
+    var sut: Formula?
 
     override func setUpWithError() throws {
         try super.setUpWithError()
@@ -21,69 +21,69 @@ class FormulaTests: XCTestCase {
     }
     
     func test_연산자_1개_숫자_2개가_있을_경우_더하기_계산이_잘_되는지() {
-        sut.operands.appendItem(1)
-        sut.operands.appendItem(2)
-        sut.operators.appendItem(.add)
+        sut?.operands.appendItem(1)
+        sut?.operands.appendItem(2)
+        sut?.operators.appendItem(.add)
         
-        XCTAssertEqual(try sut.result(), 3)
+        XCTAssertEqual(try sut?.result(), 3)
     }
     
     func test_연산자_1개_숫자_1개가_있을_경우_제대로_오류를_뱉는지() {
-        sut.operands.appendItem(1)
-        sut.operators.appendItem(.add)
+        sut?.operands.appendItem(1)
+        sut?.operators.appendItem(.add)
         
-        XCTAssertThrowsError(try sut.result())
+        XCTAssertThrowsError(try sut?.result())
     }
     
     func test_연산자_1개만_있을_경우_제대로_오류를_뱉는지() {
-        sut.operators.appendItem(.add)
+        sut?.operators.appendItem(.add)
         
-        XCTAssertThrowsError(try sut.result())
+        XCTAssertThrowsError(try sut?.result())
     }
     
     func test_숫자_1개만_있을_경우_제대로_오류를_뱉는지() {
-        sut.operands.appendItem(1)
+        sut?.operands.appendItem(1)
         
-        XCTAssertThrowsError(try sut.result())
+        XCTAssertThrowsError(try sut?.result())
     }
     
     func test_연산자_1개_숫자_2개가_있을_경우_빼기_계산이_잘_되는지() {
-        sut.operands.appendItem(2)
-        sut.operands.appendItem(1)
-        sut.operators.appendItem(.subtract)
+        sut?.operands.appendItem(2)
+        sut?.operands.appendItem(1)
+        sut?.operators.appendItem(.subtract)
         
-        XCTAssertEqual(try sut.result(), 1)
+        XCTAssertEqual(try sut?.result(), 1)
     }
     
     func test_연산자_1개_숫자_2개가_있을_경우_나누기_계산이_잘_되는지() {
-        sut.operands.appendItem(6)
-        sut.operands.appendItem(2)
-        sut.operators.appendItem(.divide)
+        sut?.operands.appendItem(6)
+        sut?.operands.appendItem(2)
+        sut?.operators.appendItem(.divide)
         
-        XCTAssertEqual(try sut.result(), 3)
+        XCTAssertEqual(try sut?.result(), 3)
     }
     
     func test_연산자_1개_숫자_2개가_있을_경우_곱하기_계산이_잘_되는지() {
-        sut.operands.appendItem(2)
-        sut.operands.appendItem(4)
-        sut.operators.appendItem(.multiply)
+        sut?.operands.appendItem(2)
+        sut?.operands.appendItem(4)
+        sut?.operators.appendItem(.multiply)
         
-        XCTAssertEqual(try sut.result(), 8)
+        XCTAssertEqual(try sut?.result(), 8)
     }
     
     func test_다양한_연산자와_숫자가_있을_경우_계산이_잘_되는지() {
-        sut.operands.appendItem(3)
-        sut.operands.appendItem(2)
-        sut.operands.appendItem(2)
-        sut.operands.appendItem(2)
-        sut.operands.appendItem(3)
-        sut.operands.appendItem(2)
-        sut.operators.appendItem(.add)
-        sut.operators.appendItem(.multiply)
-        sut.operators.appendItem(.divide)
-        sut.operators.appendItem(.add)
-        sut.operators.appendItem(.subtract)
+        sut?.operands.appendItem(3)
+        sut?.operands.appendItem(2)
+        sut?.operands.appendItem(2)
+        sut?.operands.appendItem(2)
+        sut?.operands.appendItem(3)
+        sut?.operands.appendItem(2)
+        sut?.operators.appendItem(.add)
+        sut?.operators.appendItem(.multiply)
+        sut?.operators.appendItem(.divide)
+        sut?.operators.appendItem(.add)
+        sut?.operators.appendItem(.subtract)
         
-        XCTAssertEqual(try sut.result(), 6)
+        XCTAssertEqual(try sut?.result(), 6)
     }
 }
--- Calculator/CalculatorTests/OperatorTests.swift
+++ Calculator/CalculatorTests/OperatorTests.swift
@@ -8,101 +8,80 @@
 import XCTest
 
 class OperatorTests: XCTestCase {
-    var add: Operator!
-    var subtract: Operator!
-    var divide: Operator!
-    var multiply: Operator!
-
-    override func setUpWithError() throws {
-        try super.setUpWithError()
-        add = Operator.add
-        subtract = Operator.subtract
-        divide = Operator.divide
-        multiply = Operator.multiply
-    }
-
-    override func tearDownWithError() throws {
-        try super.tearDownWithError()
-        add = nil
-        subtract = nil
-        divide = nil
-        multiply = nil
-    }
-    
     func test_더하기일때_덧셈이_잘_계산되는지() {
-        let value = add.calculate(lhs: 2, rhs: 4)
+        let value = Operator.calculate(.add)(lhs: 2, rhs: 4)
         
         XCTAssertEqual(value, 6.0)
     }
     
     func test_더하기일때_음수가_있어도_덧셈이_잘_계산되는지() {
-        let value = add.calculate(lhs: -2, rhs: 7)
+        let value = Operator.calculate(.add)(lhs: -2, rhs: 7)
         
         XCTAssertEqual(value, 5)
     }
     
     func test_소수의_덧셈도_잘_계산되는지() {
-        let value = add.calculate(lhs: 5.5, rhs: 4.5)
+        let value = Operator.calculate(.add)(lhs: 5.5, rhs: 4.5)
         
         XCTAssertEqual(value, 10)
     }
     
     func test_소수_간의_덧셈도_잘_계산되는지() {
-        let value = add.calculate(lhs: 0.5, rhs: 0.3)
+        let value = Operator.calculate(.add)(lhs: 0.5, rhs: 0.3)
         
         XCTAssertEqual(value, 0.8)
     }
     
     func test_빼기일때_뺄셈이_잘_계산되는지() {
-        let value = subtract.calculate(lhs: 10, rhs: 5)
+        let value = Operator.calculate(.subtract)(lhs: 7, rhs: 2)
         
         XCTAssertEqual(value, 5)
     }
     
     func test_빼기일때_음수가_있어도_뺄셈이_잘_계산되는지() {
-        let value = subtract.calculate(lhs: -2, rhs: 5)
+        let value = Operator.calculate(.subtract)(lhs: -2, rhs: 5)
         
         XCTAssertEqual(value, -7)
     }
     
     func test_소수의_뺄셈도_잘_계산되는지() {
-        let value = subtract.calculate(lhs: 5.5, rhs: 4.5)
+        let value = Operator.calculate(.subtract)(lhs: 5.5, rhs: 4.5)
         
         XCTAssertEqual(value, 1)
     }
     
     func test_나누기일때_나눗셈이_잘_계산되는지() {
-        let value = divide.calculate(lhs: 10, rhs: 5)
+        let value = Operator.calculate(.divide)(lhs: 10, rhs: 5)
         
         XCTAssertEqual(value, 2)
     }
     
     func test_소수의_나눗셈도_잘_계산되는지() {
-        let value = divide.calculate(lhs: 5.5, rhs: 5.5)
+        let value = Operator.calculate(.divide)(lhs: 5.5, rhs: 5.5)
         
         XCTAssertEqual(value, 1)
     }
     
     func test_나누기일때_결과가_소수인_나눗셈도_잘_계산되는지() {
-        let value = divide.calculate(lhs: 5, rhs: 10)
+        let value = Operator.calculate(.divide)(lhs: 5, rhs: 10)
         
         XCTAssertEqual(value, 0.5)
     }
     
     func test_곱하기일때_곱셈이_잘_계산되는지() {
-        let value = multiply.calculate(lhs: 2, rhs: 5)
+        let value = Operator.calculate(.multiply)(lhs: 2, rhs: 5)
         
         XCTAssertEqual(value, 10)
     }
     
     func test_소수의_곱셈도_잘_계산되는지() {
-        let value = multiply.calculate(lhs: 5, rhs: 0.5)
+        let value = Operator.calculate(.multiply)(lhs: 5, rhs: 0.5)
         
         XCTAssertEqual(value, 2.5)
     }
     
     func test_0으로_나눌_때_오류를_잘_던지는지() {
-        let value = divide.calculate(lhs: 5, rhs: 0)
+        let value = Operator.calculate(.divide)(lhs: 5, rhs: 0)
         
         XCTAssertTrue(value.isNaN)
     }
