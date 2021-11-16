5757bf9efeb635779cc45a0365fbbb70864b6162
yanghojoon <yanghojoon8487@gmail.com>
2021-11-16 15:03:11 +0900

    refactor: Formula의 프로퍼티 기본값 제거 #8 - 타입만 명시해주고 이니셜라이저를 통해 외부에서 값을 받을 수 있도록 수정

--- Calculator/Calculator/Model/Formula.swift
+++ Calculator/Calculator/Model/Formula.swift
@@ -6,8 +6,8 @@
 //
 
 struct Formula {
-    var operands = CalculatorItemQueue<Double>()
-    var operators = CalculatorItemQueue<Operator>()
+    var operands: CalculatorItemQueue<Double>
+    var operators: CalculatorItemQueue<Operator>
     
     mutating func result() throws -> Double {
         if operands.items == [] {
