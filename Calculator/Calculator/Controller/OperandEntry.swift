941522469071f17fcbcc48e4f2ed9cb12492ce1b
namu <simjh27@gmail.com>
2021-11-17 04:22:56 +0900

    feat: 일부버튼구현, OperandEntry수정

--- Calculator/Calculator/Controller/OperandEntry.swift
+++ Calculator/Calculator/Controller/OperandEntry.swift
@@ -7,49 +7,37 @@
 
 import UIKit
 
-protocol EntryDelegate {
-    var calculatorController: ViewController? { get set }
-    
-    func appendToEntry(_ input: String)
-}
-
-class OperandEntry {
-    var calculatorController: ViewController?
-    
-    private var entry = "0"
-    private var isPositive = true {
-        didSet {
-//            calculatorController
-        }
+struct OperandEntry {
+    var currentOperand: String {
+        isPositive ? entry : "-" + entry
     }
+    private var entry = "0"
+    private var isPositive = true
     private var isZeroState: Bool {
         switch entry {
-            case "", "0":
+            case "0":
                 return true
             default:
                 return false
         }
     }
-    
-    private func updateEntryLabel() {
-        
-    }
-}
-
-extension OperandEntry: EntryDelegate {
-    func appendToEntry(_ input: String) {
+    mutating func append(_ input: String) {
         switch input {
             case "0" where isZeroState,
                  "00" where isZeroState:
                 return
             default:
-                entry += input
+                entry = isZeroState ? input : entry + input
         }
     }
-    func toggleSign() {
+    mutating func replace(with result: String) {
+        entry = result
+    }
+    mutating func toggleSign() {
+        if isZeroState { return }
         isPositive.toggle()
     }
-    func emptyEntry() {
-        entry.removeAll()
+    mutating func clear() {
+        entry = "0"
     }
 }
--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -11,7 +11,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var currentOperatorLabel: UILabel!
     @IBOutlet weak var currentOperandLabel: UILabel!
     
-    private var inputFormula: String = ""
+    private var entry = OperandEntry()
+    private var formula: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,12 +21,21 @@ class ViewController: UIViewController {
     }
     
     @IBAction func allClearButtonDidTap(_ sender: UIButton) {
+        entry.clear()
+        updateOperandLabel()
     }
     @IBAction func clearEntryButtonDidTap(_ sender: UIButton) {
+        entry.clear()
+        updateOperandLabel()
     }
     @IBAction func toggleSignButtonDidTap(_ sender: UIButton) {
+        entry.toggleSign()
+        updateOperandLabel()
     }
-    @IBAction func operandButtonDidTap(_ sender: UIButton) {
+    @IBAction func numberButtonDidTap(_ sender: UIButton) {
+        guard let numer = sender.titleLabel?.text else { return }
+        entry.append(numer)
+        updateOperandLabel()
     }
     @IBAction func operatorButtonDidTap(_ sender: UIButton) {
     }
@@ -39,6 +49,14 @@ class ViewController: UIViewController {
         currentOperatorLabel.text = blank
         currentOperandLabel.text = zero
         verticalStackView.arrangedSubviews.forEach { $0.removeFromSuperview() }
+    }
+    
+    func updateOperandLabel() {
+        currentOperandLabel.text = entry.currentOperand
+    }
+    
+    func updateOperatorLabel(by operator: String) {
+        currentOperatorLabel.text = `operator`
     }
 }
 
