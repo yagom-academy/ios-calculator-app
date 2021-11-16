40107c2c93fdb21a5298a76aeaad21cde603aef9
yeha <dev.yeha@gmail.com>
2021-11-16 17:30:56 +0900

    refactor: 피연산자가 NaN이거나 0으로 나누는 경우 처리 #10

--- Calculator/Calculator/Model/Operator.swift
+++ Calculator/Calculator/Model/Operator.swift
@@ -27,18 +27,30 @@ enum Operator: Character, CaseIterable, CalculateItem {
     }
     
     private func add(_ lhs: Double, _ rhs: Double) -> Double {
+        if lhs.isNaN || rhs.isNaN {
+            return Double.nan
+        }
         return lhs + rhs
     }
     
     private func substract(_ lhs: Double, _ rhs: Double) -> Double {
+        if lhs.isNaN || rhs.isNaN {
+            return Double.nan
+        }
         return lhs - rhs
     }
     
     private func divide(_ lhs: Double, _ rhs: Double) -> Double {
+        if lhs.isNaN || rhs.isNaN || rhs == .zero {
+            return Double.nan
+        }
         return lhs / rhs
     }
     
     private func multiply(_ lhs: Double, _ rhs: Double) -> Double {
+        if lhs.isNaN || rhs.isNaN {
+            return Double.nan
+        }
         return lhs * rhs
     }
 }
