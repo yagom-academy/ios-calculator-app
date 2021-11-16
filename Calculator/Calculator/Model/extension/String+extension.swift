d4e1f4e9cc31c2f0369c22248ebb1a23b8abc6cd
Jager-yoo <y73447jh@gmail.com>
2021-11-16 13:33:03 +0900

    feat: split 메서드 구현 #5

---
+++ Calculator/Calculator/Model/extension/String+extension.swift
@@ -0,0 +1,15 @@
+//
+//  String+extension.swift
+//  Calculator
+//
+//  Created by 예거 on 2021/11/16.
+//
+
+import Foundation
+
+extension String {
+    
+    func split(with target: Character) -> [String] {
+        return self.components(separatedBy: target.description)
+    }
+}
