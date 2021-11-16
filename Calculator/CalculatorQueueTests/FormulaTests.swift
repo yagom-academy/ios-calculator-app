875bed192766fa37e09776c5b16e8e41711974cf
oksunwoo <oksunwo@naver.com>
2021-11-16 17:54:21 +0900

    test: 숫자 4개의 덧셈을 잘 수행하는지에 대한 테스트코드 추가

--- Calculator/CalculatorQueueTests/FormulaTests.swift
+++ Calculator/CalculatorQueueTests/FormulaTests.swift
@@ -8,14 +8,30 @@
 import XCTest
 
 class FormulaTests: XCTestCase {
-    var sut: Formula!
+    var formula = Formula()
+    
     override func setUpWithError() throws {
         try super.setUpWithError()
-        sut = Formula()
     }
 
     override func tearDownWithError() throws {
         try super.tearDownWithError()
-        sut = nil
+        formula.operators.removeAllItem()
+        formula.operands.removeAllItem()
+    }
+    
+    func test_숫자_4개의_덧셈을_잘_수행하는지() {
+        [10, 20, 30, 40].forEach { number in
+            formula.operands.enqueue(operation: number)
+        }
+        
+        for _ in 1...3 {
+            formula.operators.enqueue(operation: .add)
+        }
+        
+        let calculationResult = formula.result()
+        
+        XCTAssertEqual(100, calculationResult)
     }
 }
+
