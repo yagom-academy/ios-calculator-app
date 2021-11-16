0a4c980c2c067002f62e70029bfd1c12c5a127c0
namu <simjh27@gmail.com>
2021-11-16 20:37:06 +0900

    feat: 세로stackView IBOutlet 연결

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -7,10 +7,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var verticalStackView: UIStackView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
 }
 
