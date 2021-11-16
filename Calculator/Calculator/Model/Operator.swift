2eeb13a46cfb2138cdf02901ca62e975367acc37
Ari <lee_ari95@icloud.com>
2021-11-16 21:40:16 +0900

    refactor: 빼기 연산자의 rawValue를 버튼의 titleLabel text와 동일하게 수정 #5

--- Calculator/Calculator/Model/Operator.swift
+++ Calculator/Calculator/Model/Operator.swift
@@ -9,7 +9,7 @@ import Foundation
 
 enum Operator: Character, CalculateItem, CaseIterable {
     case add = "+"
-    case subtract = "-"
+    case subtract = "−"
     case divide = "÷"
     case multiply = "×"
     
