7b3ce576a5ad1185e692da4cb5e6cc5c4ec4e761
horeng2 <huski.a.a@gmail.com>
2021-11-17 18:18:47 +0900

    feat: 버튼을 눌렀을 때 버튼 유형별로 연산자, 피연산자가 관리되는 기능 구현

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -12,30 +12,45 @@ class ViewController: UIViewController {
     @IBOutlet var currentResult: UILabel!
 
     @IBOutlet var operandsButton: UIButton!
-    @IBOutlet var operatorsButtonL: UIButton!
+    @IBOutlet var operatorsButton: UIButton!
     @IBOutlet var acButton: UIButton!
     @IBOutlet var ceButton:
     UIButton!
     @IBOutlet var positiveOrNegativeButton: UIButton!
     
     @IBAction func hitButton(_ sender: UIButton) {
-        
+
+        switch sender {
+        case operandsButton:
+            //ispositive가 true면 양수 저장
+            inputOperands()
+        case operatorsButton:
+            inputOperators()
+        case acButton:
+            clearAllcalculation()
+        case ceButton:
+            clearCurrentcalculation()
+//        case positiveOrNegativeButton:
+//            isPositive()
+        default:
+            print("error")
+        }
     }
     
     func inputOperands() {
-        
+        //피연산자 큐에 입력
     }
     func inputOperators() {
-        
+        //연산자 큐에 입력
     }
     func isPositive() {
-        
+        //양수인지 반환
     }
     func clearAllcalculation() {
-    
+        //피연산자큐, 연산자큐 모두 비우기
     }
     func clearCurrentcalculation() {
-        
+        //마지막 피연산자 삭제
     }
     
     
