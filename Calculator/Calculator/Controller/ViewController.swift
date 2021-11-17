d03112158f3f83f8b025fc616522749ba24655dd
yim2627 <yim2627@gmail.com>
2021-11-17 16:31:30 +0900

    feat: StackView, StackView에 들어갈 Label 동적 생성 구현 #4

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -2,10 +2,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var numberLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    var operatorsLabel: UILabel {
+        let operatorsLabel = UILabel()
+        operatorsLabel.text = "Hi"
+        operatorsLabel.textColor = .white
+        operatorsLabel.textAlignment = .right
+        operatorsLabel.adjustsFontForContentSizeCategory = true
+        return operatorsLabel
+    }
+    var operandsLabel: UILabel {
+        let operandsLabel = UILabel()
+        operandsLabel.text = "Bye"
+        operandsLabel.textColor = .white
+        operandsLabel.textAlignment = .right
+        operandsLabel.adjustsFontForContentSizeCategory = true
+//        operandsLabel.sizeToFit()
+        return operandsLabel
+    }
+    
+    var formulaStackView: UIStackView {
+        let formulaStackView = UIStackView()
+        formulaStackView.axis = .horizontal
+        formulaStackView.spacing = 8
+        formulaStackView.distribution = .fill
+        formulaStackView.alignment = .firstBaseline
+        formulaStackView.addArrangedSubview(operatorsLabel)
+        formulaStackView.addArrangedSubview(operandsLabel)
+        return formulaStackView
+    }
 }
-
