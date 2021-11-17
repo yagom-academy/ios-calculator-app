faf2fc2248379079ef03325dc84632ca12e077e1
just1103 <hyojuson1103@gmail.com>
2021-11-17 19:58:20 +0900

    refactor: 프로젝트 요구사항에 따라 Operator static 메서드 수정 #4

--- Calculator/Calculator/Model/Operator.swift
+++ Calculator/Calculator/Model/Operator.swift
@@ -8,23 +8,23 @@ enum Operator: Character, CaseIterable, CalculateItem {
     
     func calculate(lhs: Double, rhs: Double) -> Double {
         switch self {
-        case .add: return Self.add(lhs: lhs, rhs: rhs)
-        case .subtract: return Self.subtract(lhs: lhs, rhs: rhs)
-        case .divide: return Self.divide(lhs: lhs, rhs: rhs)
-        case .multiply: return Self.multiply(lhs: lhs, rhs: rhs)
+        case .add: return self.add(lhs: lhs, rhs: rhs)
+        case .subtract: return self.subtract(lhs: lhs, rhs: rhs)
+        case .divide: return self.divide(lhs: lhs, rhs: rhs)
+        case .multiply: return self.multiply(lhs: lhs, rhs: rhs)
         }
     }
     
-    static private func add(lhs: Double, rhs: Double) -> Double {
+    private func add(lhs: Double, rhs: Double) -> Double {
         return lhs + rhs
     }
-    static private func subtract(lhs: Double, rhs: Double) -> Double {
+    private func subtract(lhs: Double, rhs: Double) -> Double {
         return lhs - rhs
     }
-    static private func divide(lhs: Double, rhs: Double) -> Double {
+    private func divide(lhs: Double, rhs: Double) -> Double {
         return lhs / rhs
     }
-    static private func multiply(lhs: Double, rhs: Double) -> Double {
+    private func multiply(lhs: Double, rhs: Double) -> Double {
         return lhs * rhs
     }
 }
