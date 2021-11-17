4ffe030df4602a738841fcf59b262d6bd71a8108
yanghojoon <yanghojoon8487@gmail.com>
2021-11-17 19:57:07 +0900

    feat: 숫자 버튼을 눌렀을 때 레이블에 반영되도록 구현 #9 - 0, 00을 눌렀을 때 함수 구현 - 숫자를 눌렀을 때 함수 구현 - .을 눌렀을 때 함수 구현

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -7,12 +7,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setOperandLabel()
     }
-
-
+    
+    @IBOutlet weak var operandLabel: UILabel?
+    @IBOutlet weak var operatorLabel: UILabel?
+    
+    var operandText = "0"
+    
+    func setOperandLabel() {
+        operandLabel?.text = operandText
+    }
+    
+    @IBAction func touchUpNumberButton(_ sender: UIButton) {
+        if operandText == "0" {
+            operandText = sender.currentTitle ?? "0"
+        } else {
+            operandText += sender.currentTitle ?? "0"
+        }
+        
+        operandLabel?.text = operandText
+    }
+    
+    @IBAction func touchUpZeroButton(_ sender: UIButton) {
+        if operandText == "0" {
+            operandText = "0"
+        } else {
+            operandText += sender.currentTitle ?? "0"
+        }
+        
+        operandLabel?.text = operandText
+    }
+    
+    @IBAction func touchUpDotButton(_ sender: UIButton) {
+        operandText += sender.currentTitle ?? "0"
+    }
 }
+
 
