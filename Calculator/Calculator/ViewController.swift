7ff468c74c9c18a8dddbc0cb914d2665900b7568
YohanBlessYou <duddn5906@naver.com>
2021-11-17 15:36:00 +0900

    feat: 기본구현 - 모든 버튼/레이블 IBOutlet 연결

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -7,12 +7,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var numberOneButton: UIButton!
+    @IBOutlet weak var numberTwoButton: UIButton!
+    @IBOutlet weak var numberThreeButton: UIButton!
+    @IBOutlet weak var numberFourButton: UIButton!
+    @IBOutlet weak var numberFiveButton: UIButton!
+    @IBOutlet weak var numberSixButton: UIButton!
+    @IBOutlet weak var numberSevenButton: UIButton!
+    @IBOutlet weak var numberEightButton: UIButton!
+    @IBOutlet weak var numberNineButton: UIButton!
+    @IBOutlet weak var numberZeroButton: UIButton!
+    @IBOutlet weak var numberZeroZeroButton: UIButton!
+    @IBOutlet weak var dotButton: UIButton!
+    
+    @IBOutlet weak var allClearButton: UIButton!
+    @IBOutlet weak var clearButton: UIButton!
+    @IBOutlet weak var togglePlusMinusButton: UIButton!
+    
+    @IBOutlet weak var resultButton: UIButton!
+    @IBOutlet weak var adderButton: UIButton!
+    @IBOutlet weak var subtractorButton: UIButton!
+    @IBOutlet weak var dividerButton: UIButton!
+    @IBOutlet weak var multiplierButton: UIButton!
+    
+    @IBOutlet weak var operatorLabel: UILabel!
+    @IBOutlet weak var currentValueLabel: UILabel!
+    
+    @IBOutlet weak var calculationListScrollView: UIScrollView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
