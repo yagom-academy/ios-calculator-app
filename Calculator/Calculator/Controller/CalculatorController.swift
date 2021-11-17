516563d4472bef5f3a5bcee6ca54edf6d09a38b8
Kim-EunsooSilver <graysilver18@naver.com>
2021-11-17 14:27:56 +0900

    feat: CalculatorController 파일 생성

---
+++ Calculator/Calculator/Controller/CalculatorController.swift
@@ -0,0 +1,12 @@
+//
+//  CalculatorController.swift
+//  Calculator
+//
+//  Created by Eunsoo KIM on 2021/11/17.
+//
+
+import Foundation
+
+class CalculatorController {
+    let formula = ExpressionParser.parse(from: "   ")
+}
