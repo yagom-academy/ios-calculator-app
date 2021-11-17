53d9c65aaf435e238e9dff84f8c9bb9f7d3d181e
junbangg <wnstjr4620@naver.com>
2021-11-17 16:57:30 +0900

    FEAT: 스토리보드의 숫자 버튼들을 ViewController 와 연결
    - UIButton의 associatedIdentifier로 구분
    - 버튼을 눌렀을때 currentNumberLabel 를 업데이트 시키도록 구현

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -8,11 +8,22 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet var currentNumberLabel: UILabel!
+    var currentNumberString = ""
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func numberPressed(_ sender: UIButton) {
+        guard let numberPressedString = sender.accessibilityIdentifier else {
+            return
+        }
+        currentNumberString += numberPressedString
+        update(self.currentNumberLabel, to: currentNumberString)
+    }
+    
+    private func update(_ label: UILabel, to data: String) {
+        label.text = data
+    }
 }
 
