6fa9acb61fee94bfd1fa7757d1fdbb97e00bfcb5
YohanBlessYou <duddn5906@naver.com>
2021-11-17 21:54:22 +0900

    feat: clear버튼 핸들러 구현

--- Calculator/Calculator/ButtonHandler/ClearButtonHandler.swift
+++ Calculator/Calculator/ButtonHandler/ClearButtonHandler.swift
@@ -5,16 +5,18 @@ struct ClearButtonHandler: ButtonActionDelegate {
     private init() { }
     
     func runActionInPhase0(viewController: ViewController, button: UIButton) {
-        
+        return
     }
     func runActionInPhase1(viewController: ViewController, button: UIButton) {
-        
+        viewController.valueLabel.text = "0"
+        viewController.currentPhase = .phase0
     }
     func runActionInPhase2(viewController: ViewController, button: UIButton) {
-        
+        return
     }
     func runActionInPhase3(viewController: ViewController, button: UIButton) {
-        
+        viewController.valueLabel.text = "0"
+        viewController.currentPhase = .phase2
     }
     func runActionInPhase4(viewController: ViewController, button: UIButton) {
         return
