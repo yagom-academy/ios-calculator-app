0ea3515ae148559c4c53bf67b796c87ba03a5f70
YohanBlessYou <duddn5906@naver.com>
2021-11-17 22:04:14 +0900

    feat: value부호 toggle버튼 핸들러 구현

--- Calculator/Calculator/ButtonHandler/TogglePlusMinusButtonHandler.swift
+++ Calculator/Calculator/ButtonHandler/TogglePlusMinusButtonHandler.swift
@@ -5,16 +5,28 @@ struct TogglePlusMinusButtonHandler: ButtonActionDelegate {
     private init() { }
     
     func runActionInPhase0(viewController: ViewController, button: UIButton) {
-        
+        return
     }
     func runActionInPhase1(viewController: ViewController, button: UIButton) {
+        let value = viewController.valueLabel.text ?? ""
         
+        if value.first == "-" {
+            viewController.valueLabel.text?.removeFirst()
+        } else {
+            viewController.valueLabel.text = "-" + value
+        }
     }
     func runActionInPhase2(viewController: ViewController, button: UIButton) {
-        
+        return
     }
     func runActionInPhase3(viewController: ViewController, button: UIButton) {
+        let value = viewController.valueLabel.text ?? ""
         
+        if value.first == "-" {
+            viewController.valueLabel.text?.removeFirst()
+        } else {
+            viewController.valueLabel.text = "-" + value
+        }
     }
     func runActionInPhase4(viewController: ViewController, button: UIButton) {
         return
