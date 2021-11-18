c8e734c82dedec63151bd5bb089e7d38eba67374
Kim-EunsooSilver <graysilver18@naver.com>
2021-11-18 15:39:36 +0900

    feat: CalculatorController 타입 구현 - formula 변수 선언 - insertFormual 함수 선언 - resetFormula 함수 선언 - calculate 함수 선언

--- Calculator/Calculator/Controller/CalculatorController.swift
+++ Calculator/Calculator/Controller/CalculatorController.swift
@@ -8,5 +8,37 @@
 import Foundation
 
 class CalculatorController {
-    let formula = ExpressionParser.parse(from: "   ")
+    private var formula = ""
+    
+    func insertFormula(`operator`: String, operand: String) {
+        formula = formula + `operator` + String(ExpressionParser.separator)
+                            + operand + String(ExpressionParser.separator)
+        print(formula)
+    }
+    
+    func resetFormula() {
+        formula = ""
+    }
+    
+    func calculate() -> String {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        numberFormatter.maximumSignificantDigits = 20
+        numberFormatter.roundingMode = .halfUp
+        
+        var calculatedResult = 0.0
+        
+        do {
+            calculatedResult = try ExpressionParser.parse(from: formula).result()
+        } catch {
+            return Formula.FormulaError.NaN.description
+        }
+        print(calculatedResult.description)
+        print(calculatedResult)
+        
+        guard let result = numberFormatter.string(from: NSNumber(value: calculatedResult)) else {
+            return ""
+        }
+        return result
+    }
 }
