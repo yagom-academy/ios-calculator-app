6f07bf531b9510fc77f792ff39f11deac147c9f6
smart8612 <smart8612@gmail.com>
2021-11-18 21:03:39 +0900

    feat: 계산 수식을 입력 받고 연산 상태를 구분해주는 초기 변수 구현

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -7,10 +7,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currentInputOperand: String = "0"
+    var currentInputOperator: String = ""
+    var mathExpression: [String] = []
+    var isEvaluated: Bool = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+    }
+    
+    func resetExpression() {
+        currentInputOperand = "0"
+        currentInputOperator = ""
+        mathExpression = []
+        isEvaluated = false
     }
 
 
