8b337fa26131dbff4b687909e331ec4cbb458eeb
Quokkaaa <rktrkdml@gmail.com>
2021-11-18 15:48:08 +0900

    feat: makeOperators와 makeOperands 메서드 구현

--- Calculator/Calculator/Model/Formula.swift
+++ Calculator/Calculator/Model/Formula.swift
@@ -22,11 +22,16 @@ enum ExpressionParser {
     }
     
     static func makeOperators(from input: String) -> [String] {
-        
+        let filteredOperators: String = input.filter { Operator(rawValue: $0) != nil }
+        let separatedOperators: [String] = filteredOperators.map({ String($0) })
+        return separatedOperators
     }
     
     static func makeOperands(from input: String) -> [String] {
-        
+        let operators = Operator.allCases
+        return operators.reduce([""]) { (array, `operator`) in
+            let separatedOperands = array.flatMap({$0.split(with: `operator`.rawValue)})
+            return separatedOperands
     }
 }
 
