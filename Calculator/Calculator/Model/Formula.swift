888e77ad98db904754af22dc7a9005fe80ced433
Quokkaaa <rktrkdml@gmail.com>
2021-11-18 15:42:53 +0900

    feat: Formula 구조체 및 프로퍼티와 인스턴스 생성
    * operands와 operators 프로퍼티 생성
    * result 메서드 생성

--- Calculator/Calculator/Model/Formula.swift
+++ Calculator/Calculator/Model/Formula.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+struct Formula {
+    var operands = CalculatorItemQueue<Double>()
+    var operators =  CalculatorItemQueue<Operator>()
+        
+    mutating func result() -> Double {
+
+    }
+}
