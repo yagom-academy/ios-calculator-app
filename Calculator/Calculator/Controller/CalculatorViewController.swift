d66f1e1276316a7f3c07d61d5ff4add3b937bd8d
oksunwoo <oksunwo@naver.com>
2021-11-19 00:46:13 +0900

    feat: StoryBoard 상의 버튼들을 viewContoller에 선언

--- Calculator/Calculator/Controller/CalculatorViewController.swift
+++ Calculator/Calculator/Controller/CalculatorViewController.swift
@@ -7,7 +7,22 @@
 import UIKit
 
 class CalculatorViewController: UIViewController {
-
+    @IBAction func touchUpAllClearButton(_ sender: Any) {
+    }
+    
+    @IBAction func touchUpClearEntryButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func touchUpChangeSignButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func touchUpEqualButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func touchUpDotButton(_ sender: UIButton) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
