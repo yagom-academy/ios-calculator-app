a4e5df5bf2c934ed7c8948a7dd685dc07dd2caa2
just1103 <hyojuson1103@gmail.com>
2021-11-18 09:39:56 +0900

    feat: 숫자 버튼 Action 함수 추가 #5

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -36,6 +36,31 @@ class ViewController: UIViewController {
     @IBAction func touchUpResultBtn(_ sender: UIButton) {
     }
     
+    @IBAction func touchUp0Btn(_ sender: UIButton) {
+    }
+    @IBAction func touchUp00Btn(_ sender: UIButton) {
+    }
+    @IBAction func touchUpDotBtn(_ sender: UIButton) {
+    }
+    @IBAction func touchUp1Btn(_ sender: UIButton) {
+    }
+    @IBAction func touchUp2Btn(_ sender: UIButton) {
+    }
+    @IBAction func touchUp3Btn(_ sender: UIButton) {
+    }
+    @IBAction func touchUp4Btn(_ sender: UIButton) {
+    }
+    @IBAction func touchUp5Btn(_ sender: UIButton) {
+    }
+    @IBAction func touchUp6Btn(_ sender: UIButton) {
+    }
+    @IBAction func touchUp7Btn(_ sender: UIButton) {
+    }
+    @IBAction func touchUp8Btn(_ sender: UIButton) {
+    }
+    @IBAction func touchUp9Btn(_ sender: UIButton) {
+    }
+    
 
 }
 
