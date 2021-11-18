5bc21dbddbe535da6726bbafa7517dffdc2f2948
yeahg-dev <yeahg.dev@gmail.com>
2021-11-18 21:46:41 +0900

    feat: Number 클래스 선언
    - value, isBiggerThan0 프로퍼티 선언
    - 이니셜라이저 구현

---
+++ Calculator/Calculator/Controller/Number.swift
@@ -0,0 +1,18 @@
+//
+//  Number.swift
+//  Calculator
+//
+//  Created by 1 on 2021/11/18.
+//
+
+import Foundation
+
+class Number {
+    var value: String
+    var isBiggerThan0: Bool
+    
+    init(value: String, isBiggerThan0: Bool) {
+        self.value = value
+        self.isBiggerThan0 = true
+    }
+}
