24c0e5b274d7c655246d8e5634f5099f13142b1e
just1103 <hyojuson1103@gmail.com>
2021-11-19 00:30:40 +0900

    refactor: touchUpResultBtn 함수 수정 #5

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -63,7 +63,7 @@ class ViewController: UIViewController {
         
     }
     @IBAction func touchUpResultBtn(_ sender: UIButton) {
-        var formula: Formula = ExpressionParser.parse(from: inputString)
+        var formula: Formula = ExpressionParser.parse(from: completeFormula.joined())
         do {
             let result: Double = try formula.result()
             operandLabel.text = "\(result)"
@@ -74,30 +74,7 @@ class ViewController: UIViewController {
         }
     }
     
-    @IBAction func touchUp0Btn(_ sender: UIButton) {
-    }
-    @IBAction func touchUp00Btn(_ sender: UIButton) {
-    }
-    @IBAction func touchUpDotBtn(_ sender: UIButton) {
-    }
-    @IBAction func touchUp1Btn(_ sender: UIButton) {
-        
-    }
-    @IBAction func touchUp2Btn(_ sender: UIButton) {
-    }
-    @IBAction func touchUp3Btn(_ sender: UIButton) {
-    }
-    @IBAction func touchUp4Btn(_ sender: UIButton) {
-    }
-    @IBAction func touchUp5Btn(_ sender: UIButton) {
-    }
-    @IBAction func touchUp6Btn(_ sender: UIButton) {
-    }
-    @IBAction func touchUp7Btn(_ sender: UIButton) {
-    }
-    @IBAction func touchUp8Btn(_ sender: UIButton) {
-    }
-    @IBAction func touchUp9Btn(_ sender: UIButton) {
+    @IBAction func touchUpOperandBtn(_ sender: UIButton) {
     }
     
 
