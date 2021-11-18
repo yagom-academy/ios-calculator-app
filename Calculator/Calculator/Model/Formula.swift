0662d31a51ba27b72dfbfa2b6c15c707ac982088
hoBahk <qudgh849@gmail.com>
2021-11-18 22:20:15 +0900

    refactor: Fomular 구조체 접근제어 설정

--- Calculator/Calculator/Model/Formula.swift
+++ Calculator/Calculator/Model/Formula.swift
@@ -8,8 +8,8 @@
 import Foundation
 
 struct Formula {
-    var operands: CalculatorItemQueue<Double>
-    var operators: CalculatorItemQueue<Operator>
+    private var operands: CalculatorItemQueue<Double>
+    private var operators: CalculatorItemQueue<Operator>
     
     init(operands: CalculatorItemQueue<Double>, operators: CalculatorItemQueue<Operator>) {
         self.operands = operands
--- Calculator/CalculatorTests/FormulaTest.swift
+++ Calculator/CalculatorTests/FormulaTest.swift
@@ -11,27 +11,30 @@ import XCTest
 class FormulaTest: XCTestCase {
     
     func test_큐에_1_더하기_2_순서로_enqueue하면_result는_3이다() throws {
-        let operators = CalculatorItemQueue<Operator>()
-        let operands = CalculatorItemQueue<Double>()
+        var operators = CalculatorItemQueue<Operator>()
+        var operands = CalculatorItemQueue<Double>()
+        
+        operands.enQueue(1)
+        operators.enQueue(.add)
+        operands.enQueue(2)
+        
         var formula = Formula(operands: operands, operators: operators)
         
-        formula.operands.enQueue(1)
-        formula.operators.enQueue(.add)
-        formula.operands.enQueue(2)
         
         try XCTAssertEqual(formula.result(), 3)
     }
     
     func test_큐에_1_더하기_2_나누기_3의_순서로_enqueue하면_result는_1이다() throws {
-        let operators = CalculatorItemQueue<Operator>()
-        let operands = CalculatorItemQueue<Double>()
-        var formula = Formula(operands: operands, operators: operators)
+        var operators = CalculatorItemQueue<Operator>()
+        var operands = CalculatorItemQueue<Double>()
         
-        formula.operands.enQueue(1)
-        formula.operators.enQueue(.add)
-        formula.operands.enQueue(2)
-        formula.operators.enQueue(.divide)
-        formula.operands.enQueue(3)
+        operands.enQueue(1)
+        operators.enQueue(.add)
+        operands.enQueue(2)
+        operators.enQueue(.divide)
+        operands.enQueue(3)
+        
+        var formula = Formula(operands: operands, operators: operators)
         
         try XCTAssertEqual(formula.result(), 1)
     }
