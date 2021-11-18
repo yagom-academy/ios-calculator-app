0b2b118e2bca99edb85347644d704373f963f79b
namu <simjh27@gmail.com>
2021-11-19 00:12:05 +0900

    feat: Calculator가 event를 받는 메서드 추가

--- Calculator/Calculator/Controller/Calculator.swift
+++ Calculator/Calculator/Controller/Calculator.swift
@@ -11,6 +11,37 @@ class Calculator {
     var delegate: CalculatorDelegate?
 }
 
+// MARK:- Receiving Events
+extension Calculator {
+    func allClearButtonDidTap() {
+        
+    }
+    func clearEntryButtonDidTap() {
+        
+    }
+    func toggleSignButtonDidTap() {
+        
+    }
+    func operatorButtonDidTap(operator: String) {
+        
+    }
+    func equalsButtonDidTap() {
+        
+    }
+    func dotButtonDidTap() {
+        
+    }
+    func zeroButtonDidTap() {
+        
+    }
+    func doubleZeroButtonDidTap() {
+        
+    }
+    func digitButtonDidTap(number: String) {
+        
+    }
+}
+
 protocol CalculatorDelegate {
     
 }
