81bf1472c03a52e328494626094ebfabfe919f3a
just1103 <hyojuson1103@gmail.com>
2021-11-18 09:22:18 +0900

    feat: UILabel 및 Action 함수 추가 #5

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -8,11 +8,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var processScrollView: UIStackView!
+    @IBOutlet weak var signLabel: UILabel!
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func touchUpAllClearBtn(_ sender: UIButton) {
+    }
+    @IBAction func touchUpClearEntryBtn(_ sender: UIButton) {
+    }
+    @IBAction func touchUpSignChangeBtn(_ sender: UIButton) {
+    }
+    
+    @IBAction func touchUpDivideOperator(_ sender: UIButton) {
+    }
+    @IBAction func touchUpMultiplyOperator(_ sender: UIButton) {
+    }
+    @IBAction func touchUpSubtractOperator(_ sender: UIButton) {
+    }
+    @IBAction func touchUpAddOperator(_ sender: UIButton) {
+    }
+    @IBAction func touchUpResultBtn(_ sender: UIButton) {
+    }
+    
 
 }
 
