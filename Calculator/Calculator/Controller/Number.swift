981c74d20217d06652d557e62cf930d0960ac2be
yeahg-dev <yeahg.dev@gmail.com>
2021-11-19 10:39:37 +0900

    feat: Number의 updateValue메서드 구현

--- Calculator/Calculator/Controller/Number.swift
+++ Calculator/Calculator/Controller/Number.swift
@@ -8,12 +8,26 @@
 import Foundation
 
 class Number {
-    var value: String
-    var isBiggerThan0: Bool
+    private (set) var value: String
+    private (set) var isBiggerThan0: Bool
     
     init(value: String = "0" , isBiggerThan0: Bool = true) {
         self.value = value
         self.isBiggerThan0 = true
+    }
+    
+    func updateValue(with input: String) {
+        if value.contains(".") && input == "." {
+            return
+        }
+        
+        if value == "0" && input == "." {
+            value = "0."
+        } else if value == "0" {
+            value = input
+        } else {
+            value += input
+        }
     }
     
     func reset() {
--- Calculator/CalculatorTests/Number_Tests.swift
+++ Calculator/CalculatorTests/Number_Tests.swift
@@ -23,4 +23,47 @@ class Number_Tests: XCTestCase {
         fakeLabel = sut.value
         XCTAssertEqual(fakeLabel, "0")
     }
+    
+    // MARK:- updateValue Tests
+    func test_value가0일때_updateValue호출시_input에_7을전달하면_label은_7이된다() {
+        sut.updateValue(with: "7")
+        fakeLabel = sut.value
+        XCTAssertEqual(fakeLabel, "7")
+    }
+    
+    func test_value가0일때_updateValue호출시_input에_0을전달하면_label은_0이된다() {
+        sut.updateValue(with: "0")
+        fakeLabel = sut.value
+        XCTAssertEqual(fakeLabel, "0")
+    }
+    
+    func test_value가0일때_input을_0_1_순서대로_updateValue호출하면_abel은_1이된다() {
+        sut.updateValue(with: "0")
+        sut.updateValue(with: "1")
+        fakeLabel = sut.value
+        XCTAssertEqual(fakeLabel, "1")
+    }
+    
+    func test_value가0일때_input을_0_점_3_순서대로_updateValue호출하면_label은_0점3이된다() {
+        sut.updateValue(with: "0")
+        sut.updateValue(with: ".")
+        sut.updateValue(with: "3")
+        fakeLabel = sut.value
+        XCTAssertEqual(fakeLabel, "0.3")
+    }
+    
+    func test_value가소수일때_updateValue호출시_input에_점을전달하면_label는_변하지않는다() {
+        let preExisting = Number(value: "0.3", isBiggerThan0: true)
+        preExisting.updateValue(with: ".")
+        fakeLabel = preExisting.value
+        XCTAssertEqual(fakeLabel, "0.3")
+    }
+    
+    func test_value가789일때_updateValue호출시_input에_점을전달하면_label은_789점이된다() {
+        let preExisting = Number(value: "789", isBiggerThan0: true)
+        preExisting.updateValue(with: ".")
+        fakeLabel = preExisting.value
+        XCTAssertEqual(fakeLabel, "789.")
+    }
+    
 }
