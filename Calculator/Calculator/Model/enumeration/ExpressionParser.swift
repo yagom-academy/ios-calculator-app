b716471ac7118c0383f843d7ac0ec121b5638dd2
Jager-yoo <y73447jh@gmail.com>
2021-11-19 13:22:42 +0900

    refactor: ExpressionParser 열거형에서 사용하는 고차함수의 중간 변수 보이도록 수정 #11

--- Calculator/Calculator/Model/enumeration/ExpressionParser.swift
+++ Calculator/Calculator/Model/enumeration/ExpressionParser.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import UIKit
 
 enum ExpressionParser {
     static let whiteSpace: Character = " "
@@ -14,17 +15,21 @@ enum ExpressionParser {
     
     static func parse(from input: String) -> Formula {
         let splitedInput = input.split(with: whiteSpace)
-        let rawValueOperators = componentsByOperators(from: input)
-        let operands = CalculatorItemQueue(queue: splitedInput.compactMap { Double($0) })
-        let operators = CalculatorItemQueue(queue: rawValueOperators.compactMap { Operator(rawValue: Character($0)) })
+        let filteredOperatorRawValues = componentsByOperators(from: input)
+        let operands = CalculatorItemQueue(queue: splitedInput.compactMap { operand in
+            return Double(operand) })
+        let operators = CalculatorItemQueue(queue: filteredOperatorRawValues.compactMap { operatorRawValue in
+            return Operator(rawValue: operatorRawValue)
+        })
         
         return Formula(operands: operands, operators: operators)
     }
     
     private static func componentsByOperators(from input: String) -> [String] {
-        let operatorRawValues = Operator.allCases.map { $0.rawValue.description }
         let splitedInput = input.split(with: whiteSpace)
         
-        return splitedInput.filter { operatorRawValues.contains($0) }
+        return splitedInput.filter { `operator` in
+            return Operator.operatorRawValues.contains(`operator`)
+        }
     }
 }
