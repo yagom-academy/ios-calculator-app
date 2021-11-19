df5095d5d3953e0e0f83411713a11d80d2c45526
yeha <dev.yeha@gmail.com>
2021-11-19 09:55:39 +0900

    chore: 각 버튼에 액션 생성 후 연결 및 CalculatorViewController로 네이밍 수정 #11

---
+++ Calculator/Calculator/Controller/CalculatorViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CalculatorViewController.swift
+//  Calculator
+//
+//  Created by yeha on 2021/11/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var presentOperandLable: UILabel!
+    @IBOutlet weak var presentOperatorLable: UILabel!
+    
+    @IBAction func didNumberButtonTap(sender: UIButton) {
+    }
+    @IBAction func didDecimalPointButtonTap (sender: UIButton) {
+    }
+    @IBAction func didOperatorButtonTap (sender: UIButton) {
+    }
+    @IBAction func didAllClearButtonTap (sender: UIButton) {
+    }
+    @IBAction func didClearEntryButtonTap (sender: UIButton) {
+    }
+    @IBAction func didChangeSignButtonTap (sender: UIButton) {
+    }
+    @IBAction func didEqualsSignButtonTap (sender: UIButton) {
+    }
+}
