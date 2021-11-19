5a6e957c37280ddca18d488c3440a9b57d57a4e1
ICS-Asan <carpediem1015@gmail.com>
2021-11-19 13:23:07 +0900

    refactor: Test 내부 divide,subtract기호 변경 #10
    - Operator RawValue 변경으로 인한 테스트 정상화

--- Calculator/CalculatorTests/ExpressionParserTest.swift
+++ Calculator/CalculatorTests/ExpressionParserTest.swift
@@ -16,13 +16,13 @@ class ExpressionParserTest: XCTestCase {
     }
     
     func test_정수_4개_subtract연산자_3개_숫자분리() {
-        let testAry = "2_3_12_466"
+        let testAry = "2−3−12−466"
         let result = ExpressionParser.parse(from: testAry).operands.inbox
         XCTAssertEqual(result, [2, 3, 12, 466])
     }
 
     func test_정수_4개_divide연산자_3개인_숫자분리() {
-        let testAry = "2/3/12/466"
+        let testAry = "2÷3÷12÷466"
         let result = ExpressionParser.parse(from: testAry).operands.inbox
         XCTAssertEqual(result, [2, 3, 12, 466])
     }
@@ -40,13 +40,13 @@ class ExpressionParserTest: XCTestCase {
     }
     
     func test_음수_실수_각2개_subtract연산자_3개_숫자분리() {
-        let testAry = "2.22_-3.33_1.2_-466"
+        let testAry = "2.22−-3.33−1.2−-466"
         let result = ExpressionParser.parse(from: testAry).operands.inbox
         XCTAssertEqual(result, [2.22, -3.33, 1.2, -466])
     }
 
     func test_음수_실수_각2개_divide연산자_3개_숫자분리() {
-        let testAry = "2.22/-3.33/1.2/-466"
+        let testAry = "2.22÷-3.33÷1.2÷-466"
         let result = ExpressionParser.parse(from: testAry).operands.inbox
         XCTAssertEqual(result, [2.22, -3.33, 1.2, -466])
     }
@@ -64,13 +64,13 @@ class ExpressionParserTest: XCTestCase {
     }
     
     func test_복합수식_숫자분리() {
-        let testAry = "11+1.2_13×14.990/15+16_-17×1.8/19.999"
+        let testAry = "11+1.2−13×14.990÷15+16−-17×1.8÷19.999"
         let result = ExpressionParser.parse(from: testAry).operands.inbox
         XCTAssertEqual(result, [11, 1.2, 13, 14.990, 15, 16, -17, 1.8, 19.999])
     }
     
     func test_복합수식_연산자분리() {
-        let testAry = "11+1.2_13×14.990/15+16_-17×1.8/19.999"
+        let testAry = "11+1.2−13×14.990÷15+16−-17×1.8÷19.999"
         let result = ExpressionParser.parse(from: testAry).operators.inbox
         XCTAssertEqual(result, [Operator.add, Operator.subtract, Operator.multiply, Operator.divide, Operator.add, Operator.subtract, Operator.multiply, Operator.divide])
     }
--- Calculator/CalculatorTests/FormulaTest.swift
+++ Calculator/CalculatorTests/FormulaTest.swift
@@ -9,7 +9,7 @@ import XCTest
 class FormulaTest: XCTestCase {
 
     func test_정수_수식_연산결과_확인() {
-        let input = "11+12_13×14/15+16_17×18/19"
+        let input = "11+12−13×14÷15+16−17×18÷19"
         var formula = ExpressionParser.parse(from: input)
         do {
             let result = try formula.result()
@@ -22,7 +22,7 @@ class FormulaTest: XCTestCase {
     }
     
     func test_정수_실수_음수_포함_수식_연산결과_확인() {
-        let input = "11+1.2_13×14.990/15+16_-17×1.8/19.999"
+        let input = "11+1.2−13×14.990÷15+16−-17×1.8÷19.999"
         var formula = ExpressionParser.parse(from: input)
         do {
             let result = try formula.result()
