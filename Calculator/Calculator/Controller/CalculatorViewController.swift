c0aab53a58fb0732f02c2fa7b3d02296471b9346
yeha <dev.yeha@gmail.com>
2021-11-19 10:18:17 +0900

    feat: 숫자 버튼의 터치 이벤트를 수신하여 임시 피연산자 큐에 넣는 기능 구현 #12

--- Calculator/Calculator/Controller/CalculatorViewController.swift
+++ Calculator/Calculator/Controller/CalculatorViewController.swift
@@ -9,10 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var inputOperand: String = "0"
+    var inputOperator: String = "0"
+    var inputOperationQueue: String = ""
+    
     @IBOutlet weak var presentOperandLable: UILabel!
     @IBOutlet weak var presentOperatorLable: UILabel!
     
     @IBAction func didNumberButtonTap(sender: UIButton) {
+        let buttonTitle = sender.currentTitle
+        if inputOperand != "0" {
+            inputOperand += buttonTitle ?? ""
+        } else if inputOperand == "0" && buttonTitle != "00" {
+            inputOperand = buttonTitle ?? ""
+        }
+        presentOperandLable.text! = inputOperand
+        inputOperationQueue += inputOperator
+        inputOperator = "0"
     }
     @IBAction func didDecimalPointButtonTap (sender: UIButton) {
     }
