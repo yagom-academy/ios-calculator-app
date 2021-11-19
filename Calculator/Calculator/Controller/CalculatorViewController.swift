83c7bff28bd0795b1abe74d6144b80e5c640cd44
ICS-Asan <carpediem1015@gmail.com>
2021-11-19 17:58:30 +0900

    refactor: tapOperatorBtn 내부 조건문 수정 #13
    - 하드코딩 방지를 위해 count==0 -> isEmpty로 변경

--- Calculator/Calculator/Controller/CalculatorViewController.swift
+++ Calculator/Calculator/Controller/CalculatorViewController.swift
@@ -86,7 +86,7 @@ class CalculatorViewController: UIViewController {
         guard let currentNumLabel = inputNumLabel.text,
              let inputOperator = sender.currentTitle else { return }
 
-        if formulaStackView.arrangedSubviews.count == 0,
+        if formulaStackView.arrangedSubviews.isEmpty,
           currentNumLabel == initialNumLabel {
             inputOperatorLabel.text = initialStringValue
         } else if currentNumLabel == initialNumLabel {
