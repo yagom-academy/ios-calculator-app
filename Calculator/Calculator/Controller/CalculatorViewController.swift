2495e7605a1cf3f2c40c171c46933389dbb51eac
ICS-Asan <carpediem1015@gmail.com>
2021-11-19 14:55:09 +0900

    feat: formulaStackView IBOutlet 변수 생성 및 Label연결 #13 #14

--- Calculator/Calculator/Controller/CalculatorViewController.swift
+++ Calculator/Calculator/Controller/CalculatorViewController.swift
@@ -10,6 +10,7 @@ class CalculatorViewController: UIViewController {
 
     @IBOutlet weak var inputNumLabel: UILabel!
     @IBOutlet weak var inputOperatorLabel: UILabel!
+    @IBOutlet weak var formulaStackView: UIStackView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
