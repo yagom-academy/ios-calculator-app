3264e522e8eb41078aea27687e335630494d9f68
ICS-Asan <carpediem1015@gmail.com>
2021-11-19 12:51:41 +0900

    refactor: FormulaTest내부 do-catch구문 적용 #10
    - result메서드가 에러를 던져준다는 것을 명확하게 하기 위해 테스트 구현 변경

--- Calculator/CalculatorTests/FormulaTest.swift
+++ Calculator/CalculatorTests/FormulaTest.swift
@@ -11,15 +11,27 @@ class FormulaTest: XCTestCase {
     func test_정수_수식_연산결과_확인() {
         let input = "11+12_13×14/15+16_17×18/19"
         var formula = ExpressionParser.parse(from: input)
-        let result = try! formula.result()
-        XCTAssertEqual(result, 7.894736842105266)
+        do {
+            let result = try formula.result()
+            XCTAssertEqual(result, 7.894736842105266)
+        } catch CalculatorError.emptyQueue {
+            print("연산을 할 수 없습니다.")
+        } catch {
+            print(error)
+        }
     }
     
     func test_정수_실수_음수_포함_수식_연산결과_확인() {
         let input = "11+1.2_13×14.990/15+16_-17×1.8/19.999"
         var formula = ExpressionParser.parse(from: input)
-        let result = try! formula.result()
-        XCTAssertEqual(result, 2.8981929096454824)
+        do {
+            let result = try formula.result()
+            XCTAssertEqual(result, 2.8981929096454824)
+        } catch CalculatorError.emptyQueue {
+            print("연산을 할 수 없습니다.")
+        } catch {
+            print(error)
+        }
     }
     
 }
