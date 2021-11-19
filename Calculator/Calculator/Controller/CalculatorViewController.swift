fac52683611254e96192b556735e74ed00ff44b5
ICS-Asan <carpediem1015@gmail.com>
2021-11-19 14:51:17 +0900

    feat: inputNumLabel IBOutlet 변수 생성 및 Label연결 #13 #14

--- Calculator/Calculator/Controller/CalculatorViewController.swift
+++ Calculator/Calculator/Controller/CalculatorViewController.swift
@@ -8,6 +8,8 @@ import UIKit
 
 class CalculatorViewController: UIViewController {
 
+    @IBOutlet weak var inputNumLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
