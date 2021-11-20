3b6a7c38e77ce786ff394f978488af3166394704
Jager-yoo <y73447jh@gmail.com>
2021-11-20 15:32:46 +0900

    feat: savedCalculatorItems 변수에 새로운 String 을 연결하는 메서드 구현 #10
    - savedCalculatorItems 변수를 초기화하는 메서드 구현
    - 숫자와 연산자 Label text 를 초기화하는 메서드 구현
    - 하드코딩을 지양하기 위해 2가지 상수 생성 (emptyString, defaultOperandLabel)

--- Calculator/Calculator/Controller/CalculatorViewController.swift
+++ Calculator/Calculator/Controller/CalculatorViewController.swift
@@ -8,8 +8,26 @@ import UIKit
 
 class CalculatorViewController: UIViewController {
     private var savedCalculatorItems: String = ""
+    private let emptyString: String = ""
+    private let defaultOperandLabel: String = "0"
     
     @IBOutlet weak var operandLabel: UILabel!
     @IBOutlet weak var operatorLabel: UILabel!
+    
+    private func saveCalculator(item: String) {
+        savedCalculatorItems += " \(item)"
+    }
+    
+    private func resetSavedCalculatorItems() {
+        savedCalculatorItems = emptyString
+    }
+    
+    private func resetOperandLable() {
+        operandLabel.text = defaultOperandLabel
+    }
+    
+    private func resetOperatorLable() {
+        operatorLabel.text = emptyString
+    }
 }
 
