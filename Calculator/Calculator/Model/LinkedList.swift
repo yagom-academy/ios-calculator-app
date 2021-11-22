bcae6f8de9ae3638588f5f944f5ec2ceeb679ad6
horeng2 <huski.a.a@gmail.com>
2021-11-22 16:31:47 +0900

    feat : 클래스타입 Node 생성 #6

--- Calculator/Calculator/Model/LinkedList.swift
+++ Calculator/Calculator/Model/LinkedList.swift
@@ -1,4 +1,11 @@
 import Foundation
 
-
-
+class Node<Element> {
+    let nodeValue: Element
+    var pointer: Node?
+    
+    init(nodeValue: Element, pointer: Node? = nil) {
+        self.nodeValue = nodeValue
+        self.pointer = pointer
+    }
+}
