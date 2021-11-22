9204f4e3c0635f1eaf18abb51b463666d8916b68
July911 <min95911@nate.com>
2021-11-22 16:42:02 +0900

    feat : 클래스 타입 LinkedList 구현 #6

--- Calculator/Calculator/Model/LinkedList.swift
+++ Calculator/Calculator/Model/LinkedList.swift
@@ -4,8 +4,34 @@ class Node<Element> {
     let nodeValue: Element
     var pointer: Node?
     
-    init(nodeValue: Element, pointer: Node? = nil) {
+    init(_ nodeValue: Element, pointer: Node? = nil) {
         self.nodeValue = nodeValue
         self.pointer = pointer
+    }
+}
+
+class LinkedList<Element> {
+    var head: Node<Element>?
+    
+    init(head: Node<Element>?) {
+        self.head = head
+    }
+    
+    func addNewNode(_ nodeValue: Element) {
+        if head == nil {
+            head = Node(nodeValue)
+            return
+        }
+        
+        var finderToLastNode: Node<Element>? = head
+        
+        while finderToLastNode?.pointer != nil {
+            finderToLastNode = finderToLastNode?.pointer
+        }
+        finderToLastNode?.pointer = Node(nodeValue)
+    }
+    
+    func deleteFirstNode() {
+        head = head?.pointer
     }
 }
