35424955fd14944aff94f2f23d8104333197e1f6
GOSARI <goeunrhdms@gmail.com>
2021-11-22 21:14:02 +0900

    test: CalculatorItemQueue Test 및 Double Extension 구현 #9

---
+++ Calculator/CalculatorTests/CalculatorItemQueue.swift
@@ -0,0 +1,48 @@
+//
+//  CalculatorTests.swift
+//  CalculatorTests
+//
+//  Created by 고은 on 2021/11/22.
+//
+
+import XCTest
+
+class CalculatorItemQueueTests: XCTestCase {
+    var sut: CalculatorItemQueue<Double>!
+   
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        sut = CalculatorItemQueue()
+    }
+
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+        sut = nil
+    }
+    
+    func test_enQueue에_요소를_추가했을때_잘들어가는지() {
+        sut.enQueueElement(1)
+        
+        XCTAssertEqual(sut.enQueueElements, [1])
+    }
+    
+    func test_enQueue에_여러개의_요소를_추가했을때_잘들어가는지() {
+        sut.enQueueElement(1)
+        sut.enQueueElement(2)
+        sut.enQueueElement(3)
+        
+        XCTAssertEqual(sut.enQueueElements, [1, 2, 3])
+    }
+    
+    func test_deQueue를_실행했을때_빈배열이면_에러를던지는지() {
+        XCTAssertThrowsError(try sut.deQueueFirstElement())
+    }
+    
+    func test_enQueue에_요소여러개를_추가하고_deQueue했을때_마지막요소가_빠지는지() {
+        sut.enQueueElement(1)
+        sut.enQueueElement(2)
+        sut.enQueueElement(3)
+        
+        XCTAssertEqual(try sut.deQueueFirstElement(), 1)
+    }
+}
