5a2acdcca30a3ce32b416b28c82ba1f38e76d9e5
Jager-yoo <y73447jh@gmail.com>
2021-11-22 20:50:51 +0900

    refactor: 불필요한 이니셜라이저 삭제 #17
    - Formula 구조체의 멤버와이즈 이니셜라이저와 역할이 중복돼서 삭제함
    - operationResult -> calculatedResult 변수 네이밍 수정

--- Calculator/Calculator/Model/Formula.swift
+++ Calculator/Calculator/Model/Formula.swift
@@ -11,17 +11,12 @@ struct Formula {
     var operands: CalculatorItemQueue<Double>
     var operators: CalculatorItemQueue<Operator>
 
-    init(operands: CalculatorItemQueue<Double>, operators: CalculatorItemQueue<Operator>) {
-        self.operands = operands
-        self.operators = operators
-    }
-
     mutating func result() -> Double {
-        guard var operationResult = operands.dequeue() else { return Double.nan }
+        guard var calculatedResult = operands.dequeue() else { return Double.nan }
         
         while let nextOperator = operators.dequeue(), let nextOperand = operands.dequeue() {
-            operationResult = nextOperator.calculate(operationResult, nextOperand)
+            calculatedResult = nextOperator.calculate(calculatedResult, nextOperand)
         }
-        return operationResult
+        return calculatedResult
     }
 }
