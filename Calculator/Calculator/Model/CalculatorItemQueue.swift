28553ebfbdec508ac89b969855fc603bd3836c49
SIWONKIM <siyeon0616@gmail.com>
2021-11-22 22:49:03 +0900

    feat: CalculatorItemQueue 및 관련 타입 구현 #11

---
+++ Calculator/Calculator/Model/CalculatorItemQueue.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+struct CalculatorItemQueue<T: CalculateItem> {
+    var calculatorItems: [T]
+    
+    mutating func enqueue(_ item: T) {
+        calculatorItems.append(item)
+    }
+    
+    mutating func dequeue() -> T? {
+        guard calculatorItems.isEmpty == false else {
+            return nil
+        }
+        
+        calculatorItems = calculatorItems.reversed()
+        let item = calculatorItems.removeLast()
+        calculatorItems = calculatorItems.reversed()
+        
+        return item
+    }
+    
+    mutating func removeAll() {
+        calculatorItems.removeAll()
+    }
+    
+    var numberOfItems: Int {
+        return calculatorItems.count
+    }
+}
