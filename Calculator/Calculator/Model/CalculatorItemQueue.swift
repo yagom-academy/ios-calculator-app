e2a5f3f9318077579e1cf56e589e420a8d4b5872
July911 <min95911@nate.com>
2021-11-22 16:57:09 +0900

    feat : 구조체타입 CalculatorItemQueue 구현 및 LinkedList 클래스 인스턴스 생성 #6

--- Calculator/Calculator/Model/CalculatorItemQueue.swift
+++ Calculator/Calculator/Model/CalculatorItemQueue.swift
@@ -1,1 +1,17 @@
 import Foundation
+
+struct CalculatorItemQueue<Element> {
+    var linkedList = LinkedList<Element>(head: nil)
+    
+    func insertToQueue(_ insertValue: Element) {
+        linkedList.addNewNode(insertValue)
+    }
+    
+    func deleteFromQueue() throws -> Element {
+        guard let deletedValue = linkedList.head?.nodeValue else {
+            throw CalculatorError.emptyQueue
+        }
+        linkedList.deleteFirstNode()
+        return deletedValue
+    }
+}
