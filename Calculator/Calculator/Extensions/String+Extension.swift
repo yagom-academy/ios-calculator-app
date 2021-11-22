15b7763608860cc65a3840c016e4a61835adbb3a
Jager-yoo <y73447jh@gmail.com>
2021-11-22 18:18:37 +0900

    refactor: String 익스텐션의 split 메서드 내부 구현 수정 #17
    - split 메서드와 map 고차함수 조합을 -> components 메서드로 변경

--- Calculator/Calculator/Extensions/String+Extension.swift
+++ Calculator/Calculator/Extensions/String+Extension.swift
@@ -5,10 +5,8 @@
 //  Created by yeha on 2021/11/15.
 //
 
-import Foundation
-
 extension String {
-    func split(with target: Character = " ") -> [String] {
-        return split(separator: target).map({ String($0) })
+    func split(with target: Character) -> [String] {
+        return components(separatedBy: target.description)
     }
 }
