470a8861480a8f9087f3a0d80472b47c8cc72efd
smart8612 <smart8612@gmail.com>
2021-11-23 17:48:02 +0900

    feat: ViewController 와 Calculator 모델을 연동하는 구현 추가
    버튼이 클릭되어 action 메서드가 호출되면 Calculator 모델을 통해 연산 로직이 동작함

--- Calculator/Calculator/Controller/CalculatorViewController.swift
+++ Calculator/Calculator/Controller/CalculatorViewController.swift
@@ -8,124 +8,46 @@ import UIKit
 
 
 class CalculatorViewController: UIViewController {
+    
+    var calculatorModel = Calculator()
+    
     @IBOutlet weak var formulaHistoryStackView: UIStackView!
     @IBOutlet weak var currentInputOperandLabel: UILabel!
     @IBOutlet weak var currentInputOperatorLabel: UILabel!
-    
-    var mathExpression: String = ""
-    var currentExpression: String = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        resetAllExpression()
-    }
-    
-    func updateCurrentInputLabel(from input: String) {
-        let historyStackLabel = UILabel()
-        historyStackLabel.text = input
-        historyStackLabel.textColor = .white
-        formulaHistoryStackView.addArrangedSubview(historyStackLabel)
-    }
-    
-    func clearEntry() {
-        currentExpression = ""
-        currentInputOperandLabel.text = currentExpression
-    }
-    
-    func changeSignNumber() {
-        guard var number = Double(currentExpression) else {
-            return
-        }
         
-        number = number * -1
-        currentExpression = String(number)
-        currentInputOperandLabel.text = currentExpression
     }
     
     @IBAction func touchUpAllClearButton(_ sender: UIButton) {
-        resetAllExpression()
+        calculatorModel.touchAllClearButton()
     }
     
     @IBAction func touchUpClearEntryButton(_ sender: UIButton) {
-        clearEntry()
+        calculatorModel.touchClearEntryButton()
     }
     
     @IBAction func touchUpChangeSignButton(_ sender: UIButton) {
-        changeSignNumber()
+        calculatorModel.touchSignChangeButton()
     }
     
     @IBAction func touchUpEqualButton(_ sender: UIButton) {
-        guard let beforeNumber = currentInputOperandLabel.text else {
-            return
-        }
-        
-        guard let beforeOperator = currentInputOperatorLabel.text else {
-            return
-        }
-        
-        mathExpression += beforeOperator
-        mathExpression += beforeNumber
-        var resultOfCalculate = ExpressionParser.parse(from: mathExpression)
-        
-        do {
-            mathExpression = try String(resultOfCalculate.result())
-            currentInputOperandLabel.text = mathExpression
-        } catch OperandsError.dividedByZero {
-            currentInputOperandLabel.text = "NaN"
-        } catch {
-            print(error)
-        }
-        mathExpression = ""
+        calculatorModel.touchEqualButton()
     }
     
     @IBAction func touchUpDotButton(_ sender: UIButton) {
+        calculatorModel.touchPointButton()
     }
     
     @IBAction func touchUpOperatorsButton(_ sender: UIButton) {
-        guard let beforeNumber = currentInputOperandLabel.text else {
-            return
-        }
-        
-        guard let beforeOperator = currentInputOperatorLabel.text else {
-            return
-        }
-        
-        guard let operation = sender.titleLabel?.text else {
-            return
-        }
-        
-        currentInputOperatorLabel.text = operation
-        
-        if beforeNumber == "0" || beforeNumber == "" {
-            
-        } else {
-            var historyStackExpression: String = ""
-            historyStackExpression += beforeOperator
-            historyStackExpression += beforeNumber
-            mathExpression += historyStackExpression
-            updateCurrentInputLabel(from: historyStackExpression)
-            currentExpression = ""
-            currentInputOperandLabel.text = "0"
-        }
+        guard let operatorSymbol = sender.titleLabel?.text else { return }
+        calculatorModel.touchOperatorButton(operatorSymbol)
     }
     
     @IBAction func touchUpOperandsButton(_ sender: UIButton) {
-        guard let number = sender.titleLabel?.text else {
-            return
-        }
-        
-        if number == "0" && currentInputOperandLabel.text == "0" {
-            currentInputOperandLabel.text = "0"
-        } else {
-            currentExpression += number
-            currentInputOperandLabel.text = currentExpression
-        }
+        guard let operandSymbol = sender.titleLabel?.text else { return }
+        calculatorModel.touchNumberButton(operandSymbol)
     }
     
-    private func resetAllExpression() {
-        currentInputOperandLabel.text = "0"
-        currentInputOperatorLabel.text = ""
-        currentExpression = ""
-        mathExpression = ""
-    }
 }
