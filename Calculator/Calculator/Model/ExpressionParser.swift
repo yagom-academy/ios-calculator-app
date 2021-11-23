d64486ee3e9991811a6a286594ae8ca21b8fb80e
Jager-yoo <y73447jh@gmail.com>
2021-11-23 15:45:47 +0900

    refactor: ExpressionParser 열거형 전반적인 수정 #17
    - 제시된 클래스 다이어그램을 준수하기 위해 parse, componentsByOperators 메서드로 구성

--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -9,46 +9,19 @@ import Foundation
 
 enum ExpressionParser {
     static func parse(from input: String) -> Formula {
-        return makeOperationQueue(from: input)
-    }
-    
-    private static func makeOperationQueue(from input: String) -> Formula {
-        let operatorQueue = insertOperatorsToQueue(from: input)
-        let operandQueue = insertOperandsToQueue(from: input)
+        let convertedOperand = input.split(with: " ")
+                                    .compactMap { operand in Double(operand) }
+        let convertedOperator = componentsByOperators(from: input)
+                                .compactMap { `operator` in Operator(symbol: `operator`) }
         
-        return Formula(operands: operandQueue, operators: operatorQueue)
-    }
-    
-    private static func insertOperatorsToQueue(from input: String) -> CalculatorItemQueue<Operator> {
-        var operatorQueue = CalculatorItemQueue<Operator>()
-        let allOperatorsRawValue = Operator.allCases.map { $0.rawValue }
-
-        input.filter { allOperatorsRawValue.contains($0) }.forEach {
-            guard let singleOperator = Operator(rawValue: $0) else { return }
-            operatorQueue.enqueue(singleOperator)
-        }
-        
-        return operatorQueue
-    }
-    
-    private static func insertOperandsToQueue(from input: String) -> CalculatorItemQueue<Double> {
-        var operandQueue = CalculatorItemQueue<Double>()
-        
-        componentsByOperators(from: input).forEach { singleOperand in
-            guard let convertedOperand = Double(singleOperand) else { return }
-            operandQueue.enqueue(convertedOperand)
-        }
-        
-        return operandQueue
+        return Formula(operands: CalculatorItemQueue(convertedOperand),
+                       operators: CalculatorItemQueue(convertedOperator))
     }
     
     private static func componentsByOperators(from input: String) -> [String] {
-        var componentsWithoutOperators: [String] = []
+        let symbols = Operator.provideSymbols()
         
-        componentsWithoutOperators = Operator.allCases.reduce([input]) { (splitInput, operatorInInput) in
-            splitInput.flatMap { $0.split(with: operatorInInput.rawValue) }
-        }
-        
-        return componentsWithoutOperators
+        return input.split(with: " ")
+                    .filter { eachString in symbols.contains(eachString) }
     }
 }
