ebc4b7ab2cfdf1b41c1500ceec5f59ef28b1c085
July911 <min95911@nate.com>
2021-11-23 11:20:32 +0900

    feat : 구조체타입 Formula 과 결과값을 반환해주는 result함수 구현 #6

--- Calculator/Calculator/Model/Formula.swift
+++ Calculator/Calculator/Model/Formula.swift
@@ -1,2 +1,30 @@
 
 import Foundation
+
+struct Formula {
+    var operands = CalculatorItemQueue<Double>()
+    var operators = CalculatorItemQueue<Character>()
+    
+    func result() -> Double {
+        guard var temporaryValue = try? operands.deleteFromQueue() else {
+            return .zero
+        }
+        
+        while operands.linkedList.head != nil {
+            do {
+                let currentOperand = try operands.deleteFromQueue()
+                let currentOperator = try operators.deleteFromQueue()
+                let operatorCase = Operator(rawValue: currentOperator)
+                temporaryValue += operatorCase?.calculate(lhs: temporaryValue, rhs: currentOperand) ?? .zero
+            }
+            catch let error as CalculatorError {
+                print(fatalError())
+            }
+            catch {
+                print("확인되지 않은 에러가 발생했습니다.")
+            }
+        }
+        return .zero
+    }
+}
+
