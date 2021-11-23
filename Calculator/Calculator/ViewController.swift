248ba1aa7d495d215bbe48122cccc8213270de1e
SIWONKIM <siyeon0616@gmail.com>
2021-11-23 17:51:54 +0900

    feat: ViewController에서 view 요소 초기화 기능 구현 #13

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -2,14 +2,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak private var numberLabel: UILabel!
+    @IBOutlet weak private var operandLabel: UILabel!
     @IBOutlet weak private var operatorLabel: UILabel!
     @IBOutlet weak private var expressionsStackView: UIStackView!
     @IBOutlet weak private var expressionScrollView: UIScrollView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        initializeView()
     }
-
+    
+    func initializeView() {
+        resetOperandLabel()
+        resetOperatorLabel()
+        removeSubviewsFromStackView()
+    }
+    
 }
 
+// MARK: - View Method
+extension ViewController {
+    private func removeSubviewsFromStackView() {
+        expressionsStackView.arrangedSubviews.forEach{$0.removeFromSuperview()}
+    }
+    
+    private func resetOperandLabel() {
+        operandLabel.text = ""
+    }
+    
+    private func resetOperatorLabel() {
+        operatorLabel.text = "0"
+    }
+}
