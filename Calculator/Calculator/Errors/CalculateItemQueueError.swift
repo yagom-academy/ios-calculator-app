50451d1d145dbfbef74e2e3eb3ecb0e071b0f3f8
hoBahk <qudgh849@gmail.com>
2021-11-23 10:57:05 +0900

    refactor: dequeue메소드 오류 처리 추가

---
+++ Calculator/Calculator/Errors/CalculateItemQueueError.swift
@@ -0,0 +1,12 @@
+//
+//  CalculateItemQueueError.swift
+//  Calculator
+//
+//  Created by 박병호 on 2021/11/23.
+//
+
+import Foundation
+
+enum CalculateItemQueueError: Error {
+    case queueIsEmpty
+}
