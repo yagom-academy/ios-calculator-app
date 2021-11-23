28cb797cd8a5481058eeec53efce29a176c357e3
horeng2 <huski.a.a@gmail.com>
2021-11-23 12:06:11 +0900

    feat : Extension 및 ExpressionParser 구현 #6

--- Calculator/Calculator/Model/Extension.swift
+++ Calculator/Calculator/Model/Extension.swift
@@ -1,1 +1,8 @@
 import Foundation
+
+extension String {
+    func split(with target: Character) -> [String] {
+        let splitedString = self.split(separator: target).map { String($0) }
+        return splitedString
+    }
+}
