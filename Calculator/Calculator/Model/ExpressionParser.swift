51a396f8addafb47dd40b5d70c0ea6b10be562b8
GOSARI <goeunrhdms@gmail.com>
2021-11-23 11:02:21 +0900

    feat: ExpressionParser의 componentsByOperators 메서드 추가 #9

---
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -0,0 +1,22 @@
+//
+//  ExpressionParser.swift
+//  Calculator
+//
+//  Created by 고은 on 2021/11/22.
+//
+
+import Foundation
+
+enum ExpressionParser {
+    private static func componentsByOperators(from input: String) -> [String] {
+            var result: [String] = [input]
+            Operator.allCases.forEach { targetOperator in
+                var splitedString: [String] = []
+                for input in result {
+                    splitedString += input.split(with: targetOperator.rawValue)
+                    result = splitedString
+                }
+            }
+            return result
+        }
+}
