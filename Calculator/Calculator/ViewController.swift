1927cef91d7afaf90f16fe159d0f7119d6816588
namu <simjh27@gmail.com>
2021-11-23 18:14:52 +0900

    feat: ExpressionStackView 동적 생성 #13

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -33,4 +33,36 @@ extension ViewController {
     private func resetOperatorLabel() {
         operatorLabel.text = "0"
     }
+    
+    private func addExpressionStackView() {
+        let `operator` = makeLabel(with: operatorLabel.text)
+        let operand = makeLabel(with: operandLabel.text)
+        let expressionStackView = makeExpressionStackView(operator: `operator`,
+                                                          operand: operand)
+        
+        expressionsStackView.addArrangedSubview(expressionStackView)
+    }
+    
+    private func makeExpressionStackView(operator: UILabel,
+                                         operand: UILabel) -> UIStackView {
+        let expression = UIStackView()
+        let spacing: CGFloat = 8
+        
+        expression.axis = .horizontal
+        expression.spacing = spacing
+        expression.addArrangedSubview(`operator`)
+        expression.addArrangedSubview(operand)
+        
+        return expression
+    }
+    
+    private func makeLabel(with text: String?) -> UILabel {
+        let label = UILabel()
+        
+        label.textColor = .white
+        label.font = .preferredFont(forTextStyle: .title3)
+        label.text = text
+        
+        return label
+    }
 }
