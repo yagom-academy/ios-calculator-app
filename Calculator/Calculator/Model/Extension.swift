b15383d13efcfcf0c83ceeb76dbf5227d35a1a30
July911 <min95911@nate.com>
2021-11-23 12:07:33 +0900

    file : Extension 파일 이름을 String+Extension으로 변경 #5

--- Calculator/Calculator/Model/Extension.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-extension String {
-    func split(with target: Character) -> [String] {
-        let splitedString = self.split(separator: target).map { String($0) }
-        return splitedString
-    }
-}
