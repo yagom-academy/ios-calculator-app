841be35c6083995eb622fffbe1c38d38566a24c5
oksunwoo <oksunwo@naver.com>
2021-11-23 18:05:06 +0900

    feat: updateCurrentInputLabel 메서드 추가
    - View를 띄울 수 있도록 각 메서드에 updateCurrentInputLabel 추가

--- Calculator/Calculator/Controller/CalculatorViewController.swift
+++ Calculator/Calculator/Controller/CalculatorViewController.swift
@@ -18,36 +18,48 @@ class CalculatorViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        updateCurrentInputLabel()
     }
     
     @IBAction func touchUpAllClearButton(_ sender: UIButton) {
         calculatorModel.touchAllClearButton()
+        updateCurrentInputLabel()
     }
     
     @IBAction func touchUpClearEntryButton(_ sender: UIButton) {
         calculatorModel.touchClearEntryButton()
+        updateCurrentInputLabel()
     }
     
     @IBAction func touchUpChangeSignButton(_ sender: UIButton) {
         calculatorModel.touchSignChangeButton()
+        updateCurrentInputLabel()
     }
     
     @IBAction func touchUpEqualButton(_ sender: UIButton) {
         calculatorModel.touchEqualButton()
+        updateCurrentInputLabel()
     }
     
     @IBAction func touchUpDotButton(_ sender: UIButton) {
         calculatorModel.touchPointButton()
+        updateCurrentInputLabel()
     }
     
     @IBAction func touchUpOperatorsButton(_ sender: UIButton) {
         guard let operatorSymbol = sender.titleLabel?.text else { return }
         calculatorModel.touchOperatorButton(operatorSymbol)
+        updateCurrentInputLabel()
     }
     
     @IBAction func touchUpOperandsButton(_ sender: UIButton) {
         guard let operandSymbol = sender.titleLabel?.text else { return }
         calculatorModel.touchNumberButton(operandSymbol)
+        updateCurrentInputLabel()
     }
     
+    private func updateCurrentInputLabel() {
+        currentInputOperandLabel.text = calculatorModel.currentInputOperand
+        currentInputOperatorLabel.text = calculatorModel.currentInputOperator
+    }
 }
