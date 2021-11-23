d11ceeed6afa11c5dbdeedaad26c3b353987bc0f
GOSARI <goeunrhdms@gmail.com>
2021-11-23 11:37:17 +0900

    test: ExpressionParser Tests 추가 #9

--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -8,26 +8,24 @@
 import Foundation
 
 enum ExpressionParser {
-    enum ExpressionParser {
-        static func parse(from input: String) -> Formula {
-            let operands = componentsByOperators(from: input).compactMap { Double($0) }
-            let operators = input.compactMap { Operator(rawValue: $0) }
-            return Formula(
-                operands: CalculatorItemQueue(operands),
-                operators: CalculatorItemQueue(operators)
-            )
-        }
-        
-        private static func componentsByOperators(from input: String) -> [String] {
-            var result: [String] = [input]
-            Operator.allCases.forEach { targetOperator in
-                var splitedString: [String] = []
-                for input in result {
-                    splitedString += input.split(with: targetOperator.rawValue)
-                    result = splitedString
-                }
+    static func parse(from input: String) -> Formula {
+        let operands = componentsByOperators(from: input).compactMap { Double($0) }
+        let operators = input.compactMap { Operator(rawValue: $0) }
+        return Formula(
+            operands: CalculatorItemQueue(operands),
+            operators: CalculatorItemQueue(operators)
+        )
+    }
+    
+    private static func componentsByOperators(from input: String) -> [String] {
+        var result: [String] = [input]
+        Operator.allCases.forEach { targetOperator in
+            var splitedString: [String] = []
+            for input in result {
+                splitedString += input.split(with: targetOperator.rawValue)
+                result = splitedString
             }
-            return result
         }
+        return result
     }
 }
---
+++ Calculator/CalculatorTests/ExpressionParserTests.swift
@@ -0,0 +1,48 @@
+//
+//  ExpressionParserTests.swift
+//  CalculatorTests
+//
+//  Created by 고은 on 2021/11/23.
+//
+
+import XCTest
+
+class ExpressionParserTests: XCTestCase {
+    var sut: ExpressionParser.Type!
+    
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        sut = ExpressionParser.self
+    }
+
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+        sut = nil
+    }
+    
+    //MARK: - componentsByOperators()
+    func test_parse메서드를_호출했을때_정수와_음수가섞인_피연산자가_반환되는지() {
+        var operands = sut.parse(from: "1+2_-3*4/5").operands
+        
+        [1.0, 2.0, -3.0, 4.0, 5.0].forEach { element in
+            XCTAssertEqual(element, try! operands.deQueueFirstElement())
+        }
+    }
+    
+    //MARK: - parse()
+    func test_parse메서드를_호출했을때_피연산자가_반환되는지() {
+        var operands = sut.parse(from: "10+3_7*5/2").operands
+        
+        [10.0, 3.0, 7.0, 5.0, 2.0].forEach { element in
+            XCTAssertEqual(element, try! operands.deQueueFirstElement())
+        }
+    }
+    
+    func test_parse메서드를_호출했을때_연산자가_반환되는지() {
+        var operators = sut.parse(from: "10+3_7*5/2").operators
+        
+        [Operator.add, Operator.subtract, Operator.multiply, Operator.divide].forEach { element in
+            XCTAssertEqual(element, try! operators.deQueueFirstElement())
+        }
+    }
+}
