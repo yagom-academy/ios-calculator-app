e242f65f5df827bc139acd65ed9d36b0a1211617
hwangjeha <hyhpwang@gmail.com>
2021-11-23 17:04:25 +0900

    feat: ViewController IBOutlet 추가 #13

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -2,11 +2,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak private var numberLabel: UILabel!
+    @IBOutlet weak private var operatorLabel: UILabel!
+    @IBOutlet weak private var expressionsStackView: UIStackView!
+    @IBOutlet weak private var expressionScrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
 
 }
 
