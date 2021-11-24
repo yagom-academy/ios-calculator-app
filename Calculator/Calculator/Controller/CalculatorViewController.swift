859e04cb79f37efb3ead0ab36dd0dd33ec8f03a5
llghdud921 <llghdud921@naver.com>
2021-11-24 11:03:34 +0900

    refactor: inputNumberLabel 접근제어자 수정으로 인해 extension 위치 수정 #32

--- Calculator/Calculator/Controller/CalculatorViewController.swift
+++ Calculator/Calculator/Controller/CalculatorViewController.swift
@@ -179,3 +179,31 @@ class CalculatorViewController: UIViewController {
     
 }
 
+// MARK: - stackView 생성
+
+extension CalculatorViewController {
+    
+    func createFormulaStack() -> UIStackView {
+        let formulaStack = FormulaStackView()
+        formulaStack.addArrangedSubview(operatorLabel)
+        formulaStack.addArrangedSubview(operandsLabel)
+        
+        return formulaStack
+    }
+    
+    private var operatorLabel: UILabel {
+        let operatorLabel = FormulaLabel()
+        operatorLabel.text = inputOperatorLabel.text
+        
+        return operatorLabel
+    }
+    
+    private var operandsLabel: UILabel {
+        let operandsLabel = FormulaLabel()
+        operandsLabel.text = inputNumberLabel.text
+        
+        return operandsLabel
+    }
+    
+}
+
