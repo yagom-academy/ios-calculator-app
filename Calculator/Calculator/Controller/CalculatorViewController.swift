40ea23428152c20cadb1cd250890eb1ba0840a5a
smart8612 <smart8612@gmail.com>
2021-11-24 17:40:22 +0900

    feat: StackView에 수식을 추가하는 기능 구현
    View 가 stateless 하지 않아서 이전의 수식 정보가 중복으로 입력되는 오류 존재함

--- Calculator/Calculator/Controller/CalculatorViewController.swift
+++ Calculator/Calculator/Controller/CalculatorViewController.swift
@@ -9,7 +9,12 @@ import UIKit
 
 class CalculatorViewController: UIViewController {
     
-    var calculatorModel = Calculator()
+    var calculatorModel = Calculator() {
+        didSet {
+            updateCurrentInputLabel()
+            addFormulaHistory()
+        }
+    }
     
     @IBOutlet weak var formulaHistoryScrollView: UIScrollView!
     @IBOutlet weak var formulaHistoryStackView: UIStackView!
@@ -25,44 +30,35 @@ class CalculatorViewController: UIViewController {
     
     @IBAction func touchUpAllClearButton(_ sender: UIButton) {
         calculatorModel.clearAllExpression()
-        updateCurrentInputLabel()
         removeAllFormulaHistory()
     }
     
     @IBAction func touchUpClearEntryButton(_ sender: UIButton) {
         calculatorModel.clearEntry()
-        updateCurrentInputLabel()
     }
     
     @IBAction func touchUpChangeSignButton(_ sender: UIButton) {
         calculatorModel.changeSignOfNumber()
-        updateCurrentInputLabel()
     }
     
     @IBAction func touchUpEqualButton(_ sender: UIButton) {
-        addFormulaHistory()
         formulaHistoryScrollView.scrollToBottom()
         calculatorModel.calculateAllExpression()
-        updateCurrentInputLabel()
     }
     
     @IBAction func touchUpDotButton(_ sender: UIButton) {
         calculatorModel.inputDot()
-        updateCurrentInputLabel()
     }
     
     @IBAction func touchUpOperatorsButton(_ sender: UIButton) {
         guard let operatorSymbol = sender.titleLabel?.text else { return }
-        addFormulaHistory()
         formulaHistoryScrollView.scrollToBottom()
         calculatorModel.inputOperator(operatorSymbol)
-        updateCurrentInputLabel()
     }
     
     @IBAction func touchUpOperandsButton(_ sender: UIButton) {
         guard let operandSymbol = sender.titleLabel?.text else { return }
         calculatorModel.inputNumber(operandSymbol)
-        updateCurrentInputLabel()
     }
     
     private func updateCurrentInputLabel() {
@@ -71,21 +67,23 @@ class CalculatorViewController: UIViewController {
     }
     
     private func addFormulaHistory() {
-        let stackView = UIStackView()
-        stackView.axis = .horizontal
-        stackView.spacing = 8
-        
-        let operatorLabel = UILabel()
-        operatorLabel.text = calculatorModel.currentInputOperator
-        operatorLabel.textColor = .white
-        
-        let operandLabel = UILabel()
-        operandLabel.text = calculatorModel.currentInputOperand
-        operandLabel.textColor = .white
-        
-        stackView.addArrangedSubview(operatorLabel)
-        stackView.addArrangedSubview(operandLabel)
-        formulaHistoryStackView.addArrangedSubview(stackView)
+        for each in calculatorModel.mathExpression {
+            let stackView = UIStackView()
+            stackView.axis = .horizontal
+            stackView.spacing = 8
+            
+            let operatorLabel = UILabel()
+            operatorLabel.text = each.operatorSymbole
+            operatorLabel.textColor = .white
+            
+            let operandLabel = UILabel()
+            operandLabel.text = each.operandNumber
+            operandLabel.textColor = .white
+            
+            stackView.addArrangedSubview(operatorLabel)
+            stackView.addArrangedSubview(operandLabel)
+            formulaHistoryStackView.addArrangedSubview(stackView)
+        }
     }
     
     private func removeAllFormulaHistory() {
