facfb54adaea78c28d1ffdc0d59628bee0379afc
yeha <dev.yeha@gmail.com>
2021-11-24 15:08:47 +0900

    chore: 스토리보드 화면 내 불필요한 문구 삭제 및 뷰컨트롤러 이름 오타 수정 #20

--- Calculator/Calculator/Controller/CalculatorViewController.swift
+++ Calculator/Calculator/Controller/CalculatorViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CalculatorViewController: UIViewController {
     
     var currentOperand: String = "0"
     var currentOperator: String = "0"
