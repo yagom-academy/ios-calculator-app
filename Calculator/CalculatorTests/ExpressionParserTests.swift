6f1de9e053df8b937c04b75cd6d574390eb3c283
YohanBlessYou <duddn5906@naver.com>
2021-11-25 21:52:56 +0900

    test: String, ExpressionParser, StringChecker 테스트코드 수정 #5

--- Calculator/CalculatorTests/ExpressionParserTests.swift
+++ Calculator/CalculatorTests/ExpressionParserTests.swift
@@ -1,78 +1,59 @@
-//
-//import XCTest
-//
-//@testable import Calculator
-//
-//class ExpressionParserTests: XCTestCase {
-//
-//    var successTestCases: [String] = []
-//    var failureTestCases: [String] = []
-//    
-//    override func setUpWithError() throws {
-//        failureTestCases.append("+ 123 + 123 + 123 +")
-//        failureTestCases.append("* 12 + / 123 - -")
-//        failureTestCases.append("123z")
-//        failureTestCases.append("12 + + 1")
-//        
-//        successTestCases.append("123 + 123 + 123")
-//        successTestCases.append("12 + 123 - 3 / 1 * 5")
-//        successTestCases.append("1")
-//        successTestCases.append("1 / 3")
-//    }
-//    
-//    func test_failureCase() {
-//        let failureAnswerList: [ExpressionParser.ParserError] = [.firstOrLastComponentIsNotOperand,
-//                                                          .firstOrLastComponentIsNotOperand,
-//                                                          .includingAbnormalCharacter,
-//                                                          .incorrectCountOfOperandsAndOperators]
-//        
-//        for index in 0..<failureTestCases.count {
-//            let failureResult = ExpressionParser.parse(from: failureTestCases[index])
-//            
-//            switch failureResult {
-//            case .failure(let error):
-//                XCTAssertEqual(error, failureAnswerList[index])
-//            default:
-//                break
-//            }
-//        }
-//    }
-//    
-//    func test_operandsTest() {
-//        let successAnswerList: [[Operator]] =  [[.add, .add],
-//                                                [.add,.subtract,.divide,.multiply],
-//                                                [],
-//                                                [.divide]]
-//        
-//        for index in 0..<successTestCases.count {
-//            let successResult = ExpressionParser.parse(from: successTestCases[index])
-//            
-//            switch successResult {
-//            case .success(let formula):
-//                XCTAssertEqual(formula.operatorsQueue.allOperators(), successAnswerList[index])
-//            default:
-//                break
-//            }
-//        }
-//    }
-//    
-//    func test_operatorsTest() {
-//        let successAnswerList: [[Double]] =  [[123, 123, 123],
-//                                       [12,123,3,1,5],
-//                                       [1],
-//                                       [1,3]]
-//        
-//        
-//        
-//        for index in 0..<successTestCases.count {
-//            let successResult = ExpressionParser.parse(from: successTestCases[index])
-//            
-//            switch successResult {
-//            case .success(let formula):
-//                XCTAssertEqual(formula.operandsQueue.allOperands(), successAnswerList[index])
-//            default:
-//                break
-//            }
-//        }
-//    }
-//}
+
+import XCTest
+
+@testable import Calculator
+
+class ExpressionParserTests: XCTestCase {
+
+    var successTestCases: [String] = []
+    var failureTestCases: [String] = []
+
+    override func setUpWithError() throws {
+        failureTestCases.append("+ 123 + 123 + 123 +")
+        failureTestCases.append("× 12 + ÷ 123 - -")
+        failureTestCases.append("123z")
+        failureTestCases.append("12 + + 1")
+        
+        successTestCases.append("123 + 123 + 123")
+        successTestCases.append("12 + 123 - 3 ÷ 1 × 5")
+        successTestCases.append("1")
+        successTestCases.append("1 ÷ 3")
+    }
+    
+    func test_failureCase() {
+        let failureAnswerList: [CalculatorError] = [.firstOrLastComponentIsNotOperand,
+                                                          .firstOrLastComponentIsNotOperand,
+                                                          .includingAbnormalCharacter,
+                                                          .incorrectCountOfOperandsAndOperators]
+        
+        for index in 0..<failureTestCases.count {
+            XCTAssertThrowsError(try ExpressionParser.parse(from: failureTestCases[index])) { error in
+                XCTAssertEqual(error as? CalculatorError, failureAnswerList[index])
+            }
+        }
+    }
+    
+    func test_operatorsTest() {
+        let successAnswerList: [[Operator]] =  [[.add, .add],
+                                                [.add,.subtract,.divide,.multiply],
+                                                [],
+                                                [.divide]]
+
+        for index in 0..<successTestCases.count {
+            XCTAssertEqual(try ExpressionParser.parse(from: successTestCases[index]).operatorsQueue.allItems(), successAnswerList[index])
+        }
+    }
+
+    func test_operandsTest() {
+        let successAnswerList: [[Double]] =  [[123, 123, 123],
+                                       [12,123,3,1,5],
+                                       [1],
+                                       [1,3]]
+
+
+
+        for index in 0..<successTestCases.count {
+            XCTAssertEqual(try ExpressionParser.parse(from: successTestCases[index]).operandsQueue.allItems(), successAnswerList[index])
+        }
+    }
+}
--- Calculator/CalculatorTests/StringCheckerTests.swift
+++ Calculator/CalculatorTests/StringCheckerTests.swift
@@ -1,50 +1,50 @@
-//import XCTest
-//
-//@testable import Calculator
-//
-//class StringCheckerTests: XCTestCase {
-//    var testCases: [String] = []
-//
-//    override func setUpWithError() throws {
-//        try super.setUpWithError()
-//        
-//        testCases.append("+ 123 + 123 + 123 +")
-//        testCases.append("* 12 + / 123 - -")
-//        testCases.append("")
-//        testCases.append("1")
-//        testCases.append("1 / 3")
-//        testCases.append("123z")
-//    }
-//
-//    func test_hasNotAbnormalCharacter() {
-//        let answerList = [ true,
-//                           true,
-//                           true,
-//                           true,
-//                           true,
-//                           false ]
-//        
-//        for index in 0..<testCases.count {
-//            XCTAssertEqual(ExpressionParser.StringChecker.hasOnlyValidCharacter(from: testCases[index]), answerList[index], "test: \(index) was failed")
-//        }
-//    }
-//    
-//    func test_firstOrLastComponentIsNotOperand() {
-//        
-//        let newTestCases: [[String]] = testCases.map {
-//            return $0.split(with: " ")
-//        }
-//        
-//        let answerList = [ false,
-//                           false,
-//                           false,
-//                           true,
-//                           true,
-//                           false ]
-//        
-//        for index in 0..<newTestCases.count {
-//            XCTAssertEqual(ExpressionParser.StringChecker.firstAndLastComponentAreOperands(from: newTestCases[index]), answerList[index], "test: \(index) was failed")
-//        }
-//    }
-//    
-//}
+import XCTest
+
+@testable import Calculator
+
+class StringCheckerTests: XCTestCase {
+    var testCases: [String] = []
+
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        
+        testCases.append("+ 123 + 123 + 123 +")
+        testCases.append("× 12 + ÷ 123 - -")
+        testCases.append("")
+        testCases.append("1")
+        testCases.append("1 ÷ 3")
+        testCases.append("123z")
+    }
+
+    func test_hasOnlyValidCharacter() {
+        let answerList = [ true,
+                           true,
+                           true,
+                           true,
+                           true,
+                           false ]
+        
+        for index in 0..<testCases.count {
+            XCTAssertEqual(ExpressionParser.StringChecker.hasOnlyValidCharacter(from: testCases[index]), answerList[index], "test: \(index) was failed")
+        }
+    }
+    
+    func test_firstAndLastComponentAreOperands() {
+        
+        let newTestCases: [[String]] = testCases.map {
+            return $0.split(with: " ")
+        }
+        
+        let answerList = [ false,
+                           false,
+                           false,
+                           true,
+                           true,
+                           false ]
+        
+        for index in 0..<newTestCases.count {
+            XCTAssertEqual(ExpressionParser.StringChecker.firstAndLastComponentAreOperands(from: newTestCases[index]), answerList[index], "test: \(index) was failed")
+        }
+    }
+    
+}
