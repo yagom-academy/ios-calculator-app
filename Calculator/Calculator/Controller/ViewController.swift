76aa7f72cce7852a2531929c98dc58ee4ddd5037
July911 <min95911@nate.com>
2021-11-25 16:10:09 +0900

    refactor : 중복되는 값 설정 삭제 #8

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -17,7 +17,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         temporaryOperandValues = [initialValue]
-        isOperatorEntered = false
         currentValueLabel.text = initialValue
     }
     
