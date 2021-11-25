e7946bc41a7c6d500244f0c37f19b82ae9e5cc6a
Jager-yoo <y73447jh@gmail.com>
2021-11-25 15:18:16 +0900

    refactor: tappedZeroButton 메서드 내부 구현 수정 #18
    - guard문의 하단에 조건문을 붙이고, if문을 삭제
    - '+=' 연산자를 사용하지 않고 String Interpolation 방법으로 Label text 할당

--- Calculator/Calculator/Controller/CalculatorViewController.swift
+++ Calculator/Calculator/Controller/CalculatorViewController.swift
@@ -87,13 +87,11 @@ class CalculatorViewController: UIViewController {
     }
     
     @IBAction func tappedZeroButton(_ button: UIButton) {
-        guard var operandLabelText = operandLabel.text,
-              let buttonTitle = button.currentTitle else { return }
+        guard let operandLabelText = operandLabel.text,
+              let buttonTitle = button.currentTitle,
+              operandLabelText != String.zero else { return }
         
-        if operandLabelText != String.zero {
-            operandLabelText += buttonTitle
-            operandLabel.text = operandLabelText
-        }
+        operandLabel.text = "\(operandLabelText)\(buttonTitle)"
     }
     
     @IBAction func tappedDecimalPointButton(_ button: UIButton) {
