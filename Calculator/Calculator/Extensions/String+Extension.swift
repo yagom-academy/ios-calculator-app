c2e381e30dbba8b2e9372fade778c76e56074dd4
yeha <dev.yeha@gmail.com>
2021-11-25 19:03:14 +0900

    refactor: String extension 내 연산 프로퍼티 생성 및 변경 #18
    - isNotEmpty 연산 프로퍼티 생성
    - hasMeaninglessDecimal 연산 프로퍼티로 변경

--- Calculator/Calculator/Extensions/String+Extension.swift
+++ Calculator/Calculator/Extensions/String+Extension.swift
@@ -6,16 +6,20 @@
 //
 
 extension String {
+    var isNotEmpty: Bool {
+        return !isEmpty
+    }
+    
+    var hasMeaninglessDecimal: Bool {
+        return self.hasSuffix(String.decimalPoint)
+            || (self.contains(String.decimalPoint) && self.hasSuffix(String.zero))
+    }
+    
     func split(with target: Character) -> [String] {
         return components(separatedBy: target.description)
     }
     
     func notContains(_ input: String) -> Bool {
         return !contains(Character(input))
-    }
-    
-    func hasMeaninglessDecimal() -> Bool {
-        return self.hasSuffix(String.decimalPoint)
-            || (self.contains(String.decimalPoint) && self.hasSuffix(String.zero))
     }
 }
