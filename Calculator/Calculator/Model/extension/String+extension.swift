9ef57594a11d11ef12001f095ed889c7a45f8371
Jager-yoo <y73447jh@gmail.com>
2021-11-28 17:34:29 +0900

    feat: String extension 에 인스턴스 프로퍼티 2개와 인스턴스 메서드 1개 추가 구현 #13
    - isNotEmpty 프로퍼티는 isEmpty 반대 결과를 Bool 타입으로 반환함
    - hasMeaninglessDecimal 프로퍼티는 String 타입의 인스턴스가 무의미한 소수값을 가지고 있는지 판단해 Bool 타입으로 반환함
    - notContains 메서드는 contains 메서드의 반대 결과를 Bool 타입으로 반환함

--- Calculator/Calculator/Model/extension/String+extension.swift
+++ Calculator/Calculator/Model/extension/String+extension.swift
@@ -6,8 +6,20 @@
 //
 
 extension String {
+    var isNotEmpty: Bool {
+        return !isEmpty
+    }
+    
+    var hasMeaninglessDecimal: Bool {
+        return self.hasSuffix(String.decimalPoint)
+            || (self.contains(String.decimalPoint) && self.hasSuffix(String.zero))
+    }
     
     func split(with target: Character) -> [String] {
-        return self.components(separatedBy: target.description)
+        return components(separatedBy: target.description)
+    }
+    
+    func notContains(_ input: String) -> Bool {
+        return !contains(Character(input))
     }
 }
