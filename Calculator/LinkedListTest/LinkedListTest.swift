648d41927b4cb6abd52e8bd798f58193cf00c0ca
dudu <whqtkf12@naver.com>
2022-03-15 00:25:30 +0900

    test: LinkedList를 Test하기 위한 테스트 파일 생성

---
+++ Calculator/LinkedListTest/LinkedListTest.swift
@@ -0,0 +1,22 @@
+//
+//  LinkedListTest.swift
+//  LinkedListTest
+//
+//  Created by DuDu on 2022/03/15.
+//
+
+import XCTest
+@testable import Calculator
+
+class LinkedListTest: XCTestCase {
+    var sut: LinkedList<Int>!
+    
+    override func setUpWithError() throws {
+        sut = LinkedList<Int>()
+    }
+
+    override func tearDownWithError() throws {
+        sut = nil
+    }
+    
+}
