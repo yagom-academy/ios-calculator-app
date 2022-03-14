a8d0849bcdd74a4e38b53f1bbd8c6766280a1a38
Mino777 <jomino7777@gmail.com>
2022-03-14 22:17:57 +0900

    chore: Unit Test Target 생성

---
+++ Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
@@ -0,0 +1,19 @@
+//
+//  CalculatorItemQueueTests.swift
+//  CalculatorItemQueueTests
+//
+//  Created by 조민호 on 2022/03/14.
+//
+
+import XCTest
+
+class CalculatorItemQueueTests: XCTestCase {
+
+    override func setUpWithError() throws {
+
+    }
+
+    override func tearDownWithError() throws {
+
+    }
+}
