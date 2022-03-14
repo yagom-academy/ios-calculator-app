8670e32dee829567c83742719dfdeb10b2ea487e
Tiana <97kth01@naver.com>
2022-03-14 23:39:48 +0900

    feat : LinkedList 타입

--- Calculator/Calculator/Model/LinkedList.swift
+++ Calculator/Calculator/Model/LinkedList.swift
@@ -13,3 +13,39 @@ class Node<T> {
         self.data = data
     }
 }
+
+class LinkedList<T> {
+    var head: Node<T>?
+    
+    init(head: Node<T>) {
+        self.head = head
+    }
+    
+    func isEmpty() -> Bool {
+        return head == nil
+    }
+    
+    func append(node: Node<T>) {
+        findLastNode()?.next = node
+    }
+    
+    func findLastNode() -> Node<T>? {
+        var currentNode = head
+        
+        while currentNode?.next != nil {
+            currentNode = currentNode?.next
+        }
+        return currentNode
+    }
+    
+    func removeFirst() -> T? {
+        let firstData = head?.data
+        
+        head = head?.next
+        return firstData
+    }
+    
+    func clear() {
+        head = nil
+    }
+}
