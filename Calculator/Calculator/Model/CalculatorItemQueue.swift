b9ebc456eabe98a1e388ee19677e31e659da5ef5
dudu <whqtkf12@naver.com>
2022-03-14 22:59:52 +0900

    add: CalculatorItemQueue 구조체 선언

---
+++ Calculator/Calculator/Model/CalculatorItemQueue.swift
@@ -0,0 +1,10 @@
+//
+//  CalculatorItemQueue.swift
+//  Calculator
+//
+//  Created by DuDu on 2022/03/14.
+//
+
+struct CalculatorItemQueue<Element> {
+    private var list = LinkedList<Element>()
+}
