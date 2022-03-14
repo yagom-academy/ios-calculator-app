c963f2ffd96150a920aa1ac4df680e6f2c9e6994
Lingo <llingo@kakao.com>
2022-03-14 19:56:02 +0900

    chore: 프로젝트 초기 환경설정
    스타일쉐어 가이드 컨벤션에 맞춰 설정 변경 (탭 사이즈: 2, 99줄, 줄바꿈 설정 등), 기본 폴더 구조 잡기
    plist 및 스토리보드 파일 Resource 폴더로 이동 및 설정, 테스트 타겟 생성
    
    close #2

---
+++ Calculator/Calculator/Application/SceneDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  Calculator - SceneDelegate.swift
+//  Created by yagom. 
+//  Copyright © yagom. All rights reserved.
+// 
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+  
+  var window: UIWindow?
+  
+  func scene(
+    _ scene: UIScene,
+    willConnectTo session: UISceneSession,
+    options connectionOptions: UIScene.ConnectionOptions
+  ) {
+    guard let _ = (scene as? UIWindowScene) else { return }
+  }
+}
+
---
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -0,0 +1,15 @@
+//
+//  CalculatorTests.swift
+//  CalculatorTests
+//
+//  Created by Lingo on 2022/03/14.
+//
+
+import XCTest
+
+class CalculatorTests: XCTestCase {
+  
+  override func setUpWithError() throws {}
+  
+  override func tearDownWithError() throws {}
+}
