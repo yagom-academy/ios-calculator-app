f64bb25e7e052b6bf26c247ad76b0f4eee206984
Marisol Mirim Kim <marisol.develop@gmail.com>
2022-03-15 23:35:28 +0900

    [move]: sort by MVC, [add]: enum Operator file
    - MVC로 파일 분류
    - Operator 열거형 파일 추가 및 구현

---
+++ Calculator/Calculator/Model/CalculatorItemQueue.swift
@@ -0,0 +1,16 @@
+//
+//  CalculatorItemQueue.swift
+//  Calculator
+//
+//  Created by Marisol Mirim Kim on 2022/03/15.
+//
+
+import Foundation
+
+struct CalculatorItemQueue {
+    
+}
+
+protocol CalculateItem {
+    
+}
