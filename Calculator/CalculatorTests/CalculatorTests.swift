7b682ab7e092fcf89ca6649d848c232c264911ad
Marisol Mirim Kim <marisol.develop@gmail.com>
2022-03-15 23:22:30 +0900

    [add]: add CalculatorTests file
    - TDD와 Unit Test를 위한 CalculatorTests파일 생성

---
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -0,0 +1,39 @@
+//
+//  CalculatorTests.swift
+//  CalculatorTests
+//
+//  Created by Marisol Mirim Kim on 2022/03/15.
+//
+
+import XCTest
+@testable import Calculator
+
+class CalculatorTests: XCTestCase {
+    
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+    }
+
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+    }
+
+    func test_enqueue() {
+        // given
+        
+        // when
+        
+        // then
+
+    }
+    
+    func test_dequeue() {
+        // given
+        
+        // when
+        
+        // then
+
+    }
+
+}
