ab45d1474380bc130811416795d66eb6725e6e40
xogns8696@gmail.com <onetool>
2022-03-16 05:27:53 +0900

    ✨[FEAT] : queue의 head를 초기화해주는 dequeue 함수 생성

--- Calculator/Model/CalculatorItemQueue.swift
+++ Calculator/Model/CalculatorItemQueue.swift
@@ -4,6 +4,7 @@
 //
 //  Created by 김태훈 on 2022/03/15.
 //
+import Foundation
 
 struct CalculatorItemQueue<T> {
     private var list = [T?]()
@@ -19,6 +20,20 @@ struct CalculatorItemQueue<T> {
     
     public mutating func enqueue(_ element: T) {
         return list.append(element)
+    }
+    
+    public mutating func dequeue() -> T? {
+        guard head <= list.count, let element = list[head] else {
+            if head > 100 {
+                list.removeFirst(head)
+                head = 0
+            }
+            
+            list[head] = nil
+            return list[head]
+            head += 1
+        }
+        return element
     }
 }
 
