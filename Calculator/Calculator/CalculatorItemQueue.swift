a3d8685bd952dafcab12b2d22ae82660fe1db860
saafaaari <111dltldnjs@naver.com>
2022-03-15 14:07:08 +0900

    add : CalculatorTests 테스트 파일 생성 및 프로토콜과 객체 생성
    CalculateItem 프로토콜 생성
    CalculatorItemQueue 타입 생성
    LinkdeList 타입 생성

---
+++ Calculator/Calculator/CalculatorItemQueue.swift
@@ -0,0 +1,21 @@
+//
+//  CalculatorItemQueue.swift
+//  Calculator
+//
+//  Created by 이시원 on 2022/03/15.
+//
+
+import Foundation
+
+protocol CalculateItem {
+    
+}
+
+class LinkdeList<T> {
+
+}
+
+class CalculatorItemQueue<T: CalculateItem> {
+
+
+}
---
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -0,0 +1,26 @@
+//
+//  CalculatorTests.swift
+//  CalculatorTests
+//
+//  Created by 이시원 on 2022/03/15.
+//
+
+import XCTest
+@testable import Calculator
+
+class CalculatorTests: XCTestCase {
+
+    
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+
+    }
+    
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+
+    }
+    
+
+    
+}
