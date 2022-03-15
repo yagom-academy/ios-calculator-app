a4cf67a5a6bd330e6b23484fab2df7f5509409cd
saafaaari <111dltldnjs@naver.com>
2022-03-15 14:14:10 +0900

    feat : Node타입 생성 및 LinkdeList타입 내부 Node타입 변수 추가

--- Calculator/Calculator/CalculatorItemQueue.swift
+++ Calculator/Calculator/CalculatorItemQueue.swift
@@ -15,7 +15,19 @@ extension Int: CalculateItem {
     
 }
 
+class Node<T> {
+    var data: T
+    var next: Node?
+    
+    init(data: T, next: Node? = nil) {
+        self.data = data
+        self.next = next
+    }
+}
+
 class LinkdeList<T> {
+    var head: Node<T>?
+    var tail: Node<T>?
 
 }
 
