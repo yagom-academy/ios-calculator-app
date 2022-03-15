43be9fbacbbc1fd33f476b27aa05907a4cb7d8fc
Quokkaa <rktrkdml@gmail.com>
2022-03-15 17:18:19 +0900

    feat: Node Type TestCodeTestCode메서드 추가
    * node메서드 초기화가 잘되는지 및 nil할당이 되는지

--- Calculator/CalculatorTests/CalculatorTests.swift
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -6,11 +6,21 @@ import XCTest
 @testable import Calculator
 
 final class CalculatorTests: XCTestCase {
-    var sut:
+    var sut: Node!
     override func setUpWithError() throws {
         try super.setUpWithError()
+        sut = Node(data: nil)
     }
     override func tearDownWithError() throws {
         try super.tearDownWithError()
+        sut = nil
+    }
+    
+    func test_nodeInitializer_isSuccess() {
+        let nodes = Node(data: nil, next: nil)
+        
+        let result = Node(data: sut.data, next: nil)
+        
+        XCTAssertEqual(result.data, nodes.data)
     }
 }
