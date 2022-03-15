ebb465692ac549ce5f8f6afa859bcb08110906ee
Lingo <llingo@kakao.com>
2022-03-15 11:30:02 +0900

    test: 테스트 함수의 네이밍과 코드를 더 명시적으로 변경 및 final 키워드 추가
    테스트 함수의 네이밍에서 불필요한 부분을 제거하고 내부 기능을 유지하되 불필요한 코드를 수정
    테스트 클래스 타입에 final 키워드를 추가

--- Calculator/CalculatorTests/CalculatorItemQueueTests.swift
+++ Calculator/CalculatorTests/CalculatorItemQueueTests.swift
@@ -8,9 +8,9 @@
 import XCTest
 @testable import Calculator
 
-class CalculatorItemQueueTests: XCTestCase {
+final class CalculatorItemQueueTests: XCTestCase {
   
-  var sut: CalculatorItemQueue<Double>!
+  private var sut: CalculatorItemQueue<Double>!
   
   override func setUpWithError() throws {
     try super.setUpWithError()
@@ -24,7 +24,7 @@ class CalculatorItemQueueTests: XCTestCase {
   
   // MARK: - enqueue()
   
-  func test_enqueue_elements의_개수가_1_증가해야한다() {
+  func test_enqueue_호출시_개수가_1_증가해야한다() {
     // given
     let input = self.sut.count
     
@@ -47,23 +47,22 @@ class CalculatorItemQueueTests: XCTestCase {
     XCTAssertEqual(output, [1.0])
   }
   
-  func test_enqueue_여러번하면_elements는_넣은_원소들의_배열을_반환해야한다() {
+  func test_enqueue_여러번_호출시_elements는_넣은_원소들의_배열을_반환해야한다() {
     // given
     self.sut.enqueue(1.0)
     self.sut.enqueue(2.0)
     self.sut.enqueue(3.0)
-    self.sut.enqueue(4.0)
     
     // when
     let output = self.sut.elements
     
     // then
-    XCTAssertEqual(output, [1.0, 2.0, 3.0, 4.0])
+    XCTAssertEqual(output, [1.0, 2.0, 3.0])
   }
   
   // MARK: - dequeue()
   
-  func test_dequeue_elements가_비어있을때_호출하면_nil이_반환되어야한다() {
+  func test_dequeue_큐가_비어있을때_호출시_nil을_반환해야한다() {
     // given
     let input = self.sut.count
     
@@ -75,7 +74,7 @@ class CalculatorItemQueueTests: XCTestCase {
     XCTAssertNil(output)
   }
   
-  func test_dequeue_elements_1_2_3이_들어있고_한번_호출시_맨_앞의_1이_반환되어야한다() {
+  func test_dequeue_큐에_1_2_3이_들어있고_호출시_맨_앞의_1을_반환해야한다() {
     // given
     self.sut.enqueue(1.0)
     self.sut.enqueue(2.0)
@@ -88,7 +87,7 @@ class CalculatorItemQueueTests: XCTestCase {
     XCTAssertEqual(output, 1.0)
   }
   
-  func test_dequeue_elements_1_2_3이_들어있고_한번_호출시_나머지가_2_3이_반환되어야한다() {
+  func test_dequeue_큐에_1_2_3이_들어있고_호출시_elements는_배열_2_3을_반환해야한다() {
     // given
     self.sut.enqueue(1.0)
     self.sut.enqueue(2.0)
@@ -104,7 +103,7 @@ class CalculatorItemQueueTests: XCTestCase {
   
   // MARK: - first
   
-  func test_first_호출시_원소가_존재하지_않으면_nil을_반환해야한다() {
+  func test_first_호출시_큐가_비어있다면_nil을_반환해야한다() {
     // given
     self.sut.clear()
     let input = self.sut.count
@@ -117,7 +116,7 @@ class CalculatorItemQueueTests: XCTestCase {
     XCTAssertNil(output)
   }
   
-  func test_first_호출시_원소가_존재하면_맨앞의_원소를_반환해야한다() {
+  func test_first_1_2_3이_들어있는_큐에서_호출시_맨앞의_원소_1을_반환해야한다() {
     // given
     self.sut.enqueue(1.0)
     self.sut.enqueue(2.0)
@@ -132,7 +131,7 @@ class CalculatorItemQueueTests: XCTestCase {
   
   // MARK: - clear()
   
-  func test_clear_호출시_elements가_빈_배열이_되어야한다() {
+  func test_clear_호출시_elements가_빈_배열을_반환해야한다() {
     // given
     self.sut.enqueue(1.0)
     self.sut.enqueue(2.0)
--- Calculator/CalculatorTests/CalculatorLinkedListTests.swift
+++ Calculator/CalculatorTests/CalculatorLinkedListTests.swift
@@ -8,9 +8,9 @@
 import XCTest
 @testable import Calculator
 
-class CalculatorLinkedListTests: XCTestCase {
+final class CalculatorLinkedListTests: XCTestCase {
   
-  var sut: CalculatorLinkedList<Double>!
+  private var sut: CalculatorLinkedList<Double>!
   
   override func setUpWithError() throws {
     try super.setUpWithError()
@@ -24,26 +24,22 @@ class CalculatorLinkedListTests: XCTestCase {
   
   // MARK: - append()
   
-  func test_append_data를_담아_호출할때_count가_1_증가해야한다() {
+  func test_append_호출시_개수가_1_증가해야한다() {
     // given
-    let data = 1.0
     let input = self.sut.count
     
     // when
-    self.sut.append(data)
+    self.sut.append(1.0)
     let output = self.sut.count
     
     // then
     XCTAssertEqual(output, input + 1)
   }
   
-  func test_append_count가_0에서_호출시_front와_rear가_nil이되면_안된다() {
-    // given
-    let data = 1.0
-    
-    // when
+  func test_append_리스트가_비어있을때_호출시_front와_rear가_nil이_아닌값을_반환해야한다() {
+    // given when
     let output = self.sut.count
-    self.sut.append(data)
+    self.sut.append(1.0)
     
     // then
     XCTAssertEqual(output, 0)
@@ -51,20 +47,17 @@ class CalculatorLinkedListTests: XCTestCase {
     XCTAssertNotNil(self.sut.rear)
   }
   
-  func test_append_count가_0에서_호출시_front와_rear가_nil이_아닌_같은_인스턴스를_참조해야한다() {
-    // given
-    let data = 1.0
-    
-    // when
-    self.sut.append(data)
+  func test_append_리스트가_비어있을때_호출시_front와_rear가_같은_인스턴스를_참조해야한다() {
+    // given when
+    let output = self.sut.count
+    self.sut.append(1.0)
     
     // then
-    XCTAssertNotNil(self.sut.front)
-    XCTAssertNotNil(self.sut.rear)
+    XCTAssertEqual(output, 0)
     XCTAssertTrue(self.sut.front === self.sut.rear)
   }
   
-  func test_append_count가_1일때_새로운_노드를_추가하면_rear가_front의_next와_같은_인스턴스를_참조해야한다() {
+  func test_append_리스트의_개수가_1일때_호출시_front의_next와_rear가_같은_인스턴스를_참조해야한다() {
     // given
     self.sut.append(1.0)
     let input = self.sut.count
@@ -79,7 +72,7 @@ class CalculatorLinkedListTests: XCTestCase {
     XCTAssertTrue(self.sut.front?.next === self.sut.rear)
   }
   
-  func test_append_count가_2일때_새로운_노드를_추가하면_rear가_front의_next와_같은_인스턴스를_참조해야한다() {
+  func test_append_리스트의_개수가_2일때_호출시_front의_next_next와_rear가_같은_인스턴스를_참조해야한다() {
     // given
     self.sut.append(1.0)
     self.sut.append(1.0)
@@ -97,34 +90,34 @@ class CalculatorLinkedListTests: XCTestCase {
   
   // MARK: - first
   
-  func test_first_프로퍼티를_호출할때_리스트가_비어있지_않을시_front의_data를_반환한다() {
+  func test_first_1을_넣어_리스트가_비어있지_않을때_호출시_front의_data_1을_반환해야한다() {
     // given
     self.sut.append(1.0)
     let input = self.sut.isEmpty
     
     // when
-    let data = self.sut.first
+    let output = self.sut.first
     
     // then
     XCTAssertFalse(input)
-    XCTAssertEqual(data, 1.0)
+    XCTAssertEqual(output, 1.0)
   }
   
-  func test_first_프로퍼티를_호출할때_리스트가_비어있을시_nil을_반환한다() {
+  func test_first_리스트가_비어있을때_호출시_nil을_반환해야한다() {
     // given
     let input = self.sut.isEmpty
     
     // when
-    let data = self.sut.first
+    let output = self.sut.first
     
     // then
     XCTAssertTrue(input)
-    XCTAssertNil(data)
+    XCTAssertNil(output)
   }
   
   // MARK: - removeFirst()
   
-  func test_removeFirst_호출시_리스트가_비어있다면_nil을_반환한다() {
+  func test_removeFirst_리스트가_비어있다면_호출시_nil을_반환해야한다() {
     // given
     let input = self.sut.isEmpty
     
@@ -136,7 +129,7 @@ class CalculatorLinkedListTests: XCTestCase {
     XCTAssertNil(output)
   }
   
-  func test_removeFirst_호출시_리스트가_비어있지_않다면_노드의_data를_반환한다() {
+  func test_removeFirst_1을_넣어_리스트가_비어있지_않을때_호출시_맨_앞_노드의_data_1을_반환해야한다() {
     // given
     self.sut.append(1.0)
     let input = self.sut.isEmpty
@@ -149,7 +142,7 @@ class CalculatorLinkedListTests: XCTestCase {
     XCTAssertEqual(output, 1.0)
   }
   
-  func test_removeFirst_호출시_기존_리스트의_개수가_1일_경우_count는_0을_반환하고_isEmpty는_true이다() {
+  func test_removeFirst_리스트의_개수가_1일때_호출시_count는_0을_isEmpty는_true를_반환해야한다() {
     // given
     self.sut.append(1.0)
     let inputCount = self.sut.count
@@ -165,7 +158,7 @@ class CalculatorLinkedListTests: XCTestCase {
     XCTAssertTrue(output)
   }
   
-  func test_removeFirst_호출시_기존_리스트의_개수가_2일_경우_count는_1을_반환한다() {
+  func test_removeFirst_리스트의_개수가_2일때_호출시_count는_1을_반환해야한다() {
     self.sut.append(1.0)
     self.sut.append(2.0)
     let inputCount = self.sut.count
@@ -179,55 +172,76 @@ class CalculatorLinkedListTests: XCTestCase {
     XCTAssertEqual(outputCount, 1)
   }
   
-  func test_removeAll_호출시_count가_0을_반환해야한다() {
+  func test_removeAll_리스트가_비어있지_않을때_호출시_count가_0을_반환해야한다() {
     // given
     self.sut.append(1.0)
     self.sut.append(2.0)
+    let input = self.sut.isEmpty
     
     // when
     self.sut.removeAll()
     let output = self.sut.count
     
     // then
+    XCTAssertFalse(input)
     XCTAssertEqual(output, 0)
   }
   
-  func test_removeAll_호출시_isEmpty가_true을_반환해야한다() {
+  func test_removeAll_리스트가_비어있지_않을때_호출시_isEmpty가_true을_반환해야한다() {
     // given
     self.sut.append(1.0)
     self.sut.append(2.0)
+    let input = self.sut.isEmpty
     
     // when
     self.sut.removeAll()
     let output = self.sut.isEmpty
     
     // then
+    XCTAssertFalse(input)
     XCTAssertTrue(output)
   }
   
-  func test_removeAll_호출시_front가_nil을_반환해야한다() {
+  func test_removeAll_리스트가_비어있지_않을때_호출시_front가_nil을_반환해야한다() {
     // given
     self.sut.append(1.0)
     self.sut.append(2.0)
+    let input = self.sut.isEmpty
     
     // when
     self.sut.removeAll()
     let output = self.sut.front
     
     // then
+    XCTAssertFalse(input)
     XCTAssertNil(output)
   }
   
-  func test_removeAll_호출시_rear가_nil을_반환해야한다() {
+  func test_removeAll_리스트가_비어있지_않을때_호출시_rear가_nil을_반환해야한다() {
     // given
     self.sut.append(1.0)
     self.sut.append(2.0)
+    let input = self.sut.isEmpty
     
     // when
     self.sut.removeAll()
     let output = self.sut.rear
     
     // then
+    XCTAssertFalse(input)
     XCTAssertNil(output)
+  }
+  
+  func test_removeAll_리스트가_비어있을때_호출시_front와_rear이_nil을_반환해야한다() {
+    // given
+    let input = self.sut.isEmpty
+    
+    // when
+    self.sut.removeAll()
+    
+    // then
+    XCTAssertTrue(input)
+    XCTAssertNil(self.sut.front)
+    XCTAssertNil(self.sut.rear)
   }
 }
