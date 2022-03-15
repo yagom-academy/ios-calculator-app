3841b2f10f393d1a759858dd6782d0a90ac60aef
Grumpy <sewoong2890@gmail.com>
2022-03-15 18:21:21 +0900

    feat: CalculatorItemQueue 구현
    class를 struct로 변경
    내부 메서드와 프로퍼티 구현

--- Calculator/Calculator/CalculatorItemQueue.swift
+++ Calculator/Calculator/CalculatorItemQueue.swift
@@ -1,19 +1,50 @@
 import Foundation
 
-class CalculatorItemQueue: CalculateItem {
+struct CalculatorItemQueue<T>: CalculateItem {
+    private var data = [T]()
     
-//    큐의 주요 기능들:
-//      func
-//    1. enqueue(): 큐의 맨 뒤에 새로운 요소를 추가
-//    2. dequeue(): 큐의 첫 번째 요소를 제거한 뒤 반환
-//    3. peek(): 큐의 첫 번째 요소를 반환하되, 제거하지는 않음
-//    4. clear(): 큐를 재설정해 빈 상태가 되게 함
-//    5. insert(_: atIndex): 큐의 특정 인덱스 위치에 요소를 삽입
-//    6. removeAtIndex(_): 큐의 특정 인덱스 위치에 있는 요소를 제거
-
-//      prop
-//    1. count: 큐에 있는 요소의 수를 반환
-//    2. isEmpty: 큐가 비어있으면 true를, 그렇지 않은 경우 false를 반환
-//    3. isFull: 큐가 꽉 차있으면 true를, 그렇지 않은 경우 false를 반환
-//    4. capacity: 큐 용량을 가져오거나 설정하는 프로퍼티
+    public mutating func enqueue(element: T) {
+        data.append(element)
+    }
+    
+    public mutating func dequeue() -> T? {
+        return data.removeFirst()
+    }
+    
+    public func peek() -> T? {
+        return data.first
+    }
+    
+    public mutating func clear() {
+        data.removeAll()
+    }
+    
+    public mutating func insert(_ element: T, at: Int) {
+        data.insert(element, at: at)
+    }
+    
+    public mutating func removeAtIndex(_ at: Int) {
+        data.remove(at: at)
+    }
+    
+    public var count: Int {
+        return data.count
+    }
+    
+    public var isEmpty: Bool {
+        return data.isEmpty
+    }
+    
+    public var isFull: Bool {
+        return data.count == data.capacity
+    }
+    
+    public var capacity: Int {
+        get {
+            return data.capacity
+        }
+        set {
+            data.reserveCapacity(newValue)
+        }
+    }
 }
