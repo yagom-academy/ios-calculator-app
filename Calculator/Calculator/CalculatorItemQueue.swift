1ea499b63b10a91b77d1e56d8690d4aaf89b6500
Grumpy <sewoong2890@gmail.com>
2022-03-15 18:06:27 +0900

    feat: 구현한 큐의 기능 작성

--- Calculator/Calculator/CalculatorItemQueue.swift
+++ Calculator/Calculator/CalculatorItemQueue.swift
@@ -2,4 +2,18 @@ import Foundation
 
 class CalculatorItemQueue: CalculateItem {
     
+//    큐의 주요 기능들:
+//      func
+//    1. enqueue(): 큐의 맨 뒤에 새로운 요소를 추가
+//    2. dequeue(): 큐의 첫 번째 요소를 제거한 뒤 반환
+//    3. peek(): 큐의 첫 번째 요소를 반환하되, 제거하지는 않음
+//    4. clear(): 큐를 재설정해 빈 상태가 되게 함
+//    5. insert(_: atIndex): 큐의 특정 인덱스 위치에 요소를 삽입
+//    6. removeAtIndex(_): 큐의 특정 인덱스 위치에 있는 요소를 제거
+
+//      prop
+//    1. count: 큐에 있는 요소의 수를 반환
+//    2. isEmpty: 큐가 비어있으면 true를, 그렇지 않은 경우 false를 반환
+//    3. isFull: 큐가 꽉 차있으면 true를, 그렇지 않은 경우 false를 반환
+//    4. capacity: 큐 용량을 가져오거나 설정하는 프로퍼티
 }
