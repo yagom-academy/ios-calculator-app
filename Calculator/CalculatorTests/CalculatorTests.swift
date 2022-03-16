4edc05d5436ac55f9cee25ed886a0d9aef94949b
Marisol Mirim Kim <marisol.develop@gmail.com>
2022-03-17 01:48:49 +0900

    [feat]: add test isEmpty&count method
    - isEmpty와 count를 테스트하는 메서드 추가

--- Calculator/CalculatorTests/CalculatorTests.swift
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -42,4 +42,33 @@ class CalculatorTests: XCTestCase {
         XCTAssertEqual(result, 2.0)
         XCTAssertEqual(queue, [nil])
     }
+    
+    func test_queue가_비어있을때_isEmpty가_true를리턴한다() {
+        // given
+        // when
+        let result = sut?.isEmpty
+        // then
+        XCTAssertEqual(result, true)
+    }
+    
+    func test_queue가_비어있지않을때_isEmpty가_false를리턴한다() {
+        // given
+        sut?.enqueue(1.0)
+        // when
+        let result = sut?.isEmpty
+        // then
+        XCTAssertEqual(result, false)
+    }
+    
+    func test_queue의count가_enqueue할때마다_업데이트된다() {
+        // given
+        let input1: Double = 1.0
+        let input2: Double = 2.0
+        // when
+        sut?.enqueue(input1)
+        sut?.enqueue(input2)
+        let result = sut?.count
+        // then
+        XCTAssertEqual(result, 2)
+    }
 }
