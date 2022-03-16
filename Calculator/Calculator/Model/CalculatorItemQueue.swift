d6f637e497f65b8669b1493d33e8f058c22076aa
doogie97 <chris_97@naver.com>
2022-03-16 12:07:08 +0900

    feat: dequeue함수 구현, refactor: count프로퍼티 수정

--- Calculator/Calculator/Model/CalculatorItemQueue.swift
+++ Calculator/Calculator/Model/CalculatorItemQueue.swift
@@ -3,11 +3,19 @@ import Foundation
 struct CalculatorItemQueue<item: CalculateItem> {
     private var enqueueList: [item] = []
     private var dequeueList: [item] = []
-    var count: Int { enqueueList.count }
+    var count: Int { dequeueList.isEmpty ? enqueueList.count : dequeueList.count }
     var first: item? { enqueueList.first }
     var last: item? { enqueueList.last }
     
     mutating func enqueue(_ item: item) {
         enqueueList.append(item)
+    }
+    
+    mutating func dequeue() -> item? {
+        if dequeueList.isEmpty {
+            dequeueList = enqueueList.reversed()
+            enqueueList.removeAll()
+        }
+        return dequeueList.popLast()
     }
 }
--- Calculator/CalculatorItemQueueTest/CalculatorItemQueueTest.swift
+++ Calculator/CalculatorItemQueueTest/CalculatorItemQueueTest.swift
@@ -12,6 +12,7 @@ class CalculatorItemQueueTest: XCTestCase {
         sut = nil
     }
     
+    //MARK: - enqueue
     func test_enqueue호출시_정상적으로추가가되는지() {
         //given
         let input: Double = 2
@@ -24,5 +25,32 @@ class CalculatorItemQueueTest: XCTestCase {
         //then
         XCTAssertEqual(sut.first, input)
         XCTAssertEqual(sut.last, input3)
+        XCTAssertEqual(sut.count, 3)
+    }
+    //MARK: - dequeue
+    func test_dequeue호출시_FIFO가정상적으로진행되는지() {
+        //given
+        let input: Double = 5
+        let input2: Double = 2
+        let input3: Double = 9
+        //when
+        sut.enqueue(input)
+        sut.enqueue(input2)
+        sut.enqueue(input3)
+        
+        let firstOut = sut.dequeue()
+        let secondOut = sut.dequeue()
+        let thirdOut = sut.dequeue()
+        //then
+        XCTAssertEqual(firstOut, input)
+        XCTAssertEqual(secondOut, input2)
+        XCTAssertEqual(thirdOut, input3)
+    }
+    
+    func test_dequeue호출시_배열이비어있을때_nil을반환하는지() {
+        //when
+        let result = sut.dequeue()
+        //then
+        XCTAssertNil(result)
     }
 }
