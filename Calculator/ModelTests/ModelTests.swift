cd61e13079b1335e94cab5fa9fe539d18a20784e
Red <seryprk@gmail.com>
2022-03-17 00:51:59 +0900

    ✏️chore : Unit test 주석 삭제

--- Calculator/ModelTests/ModelTests.swift
+++ Calculator/ModelTests/ModelTests.swift
@@ -19,43 +19,33 @@ class ModelTests: XCTestCase {
     }
 
     func test_enqueue_DoubleType을넣었을때() {
-        // given
         let numberValue: Double = 10
-        // when
         sut.enqueue(with: numberValue)
         guard let result = sut.queue[sut.head] as? Double else { return }
-        // then
         XCTAssertEqual(result, numberValue)
     }
     
     func test_enqueue_OperatorType을넣었을때() {
-        // given
         let operatorValue: Operator = .add
-        // when
         sut.enqueue(with: operatorValue)
         guard let result = sut.queue[sut.tail] as? Operator else { return }
-        // then
         XCTAssertEqual(result, operatorValue)
     }
     
     func test_dequeue_DoubleType을뺄때() {
-        // when
         sut.enqueue(with: 1.0)
         sut.enqueue(with: Operator.add)
         sut.enqueue(with: 2.0)
         guard let result = sut.dequeue() as? Double else { return }
-        // then
         XCTAssertEqual(result, 1.0)
     }
     
     func test_dequeue_DoubleType을넣었을때() {
-        // when
         sut.enqueue(with: 1.0)
         sut.enqueue(with: Operator.add)
         sut.enqueue(with: 2.0)
         _ = sut.dequeue()
         guard let result = sut.dequeue() as? Operator else { return }
-        // then
         XCTAssertEqual(result, Operator.add)
     }
     
