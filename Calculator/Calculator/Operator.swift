5ebf6331f909686b3ec4f0d740b78d7f2d523015
Eddy <kimkyunghun3@naver.com>
2022-03-16 16:13:34 +0900

    feat: Operator 열거형 정의

---
+++ Calculator/Calculator/Operator.swift
@@ -0,0 +1,15 @@
+//
+//  Operator.swift
+//  Calculator
+//
+//  Created by Eddy on 2022/03/16.
+//
+
+import Foundation
+
+enum Operator {
+    case add
+    case subtract
+    case multiply
+    case division
+}
