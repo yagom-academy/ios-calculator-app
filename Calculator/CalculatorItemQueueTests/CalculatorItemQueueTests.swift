8d3203d24bfd5cafed1291e8e70967661de4fcfb
Grumpy <sewoong2890@gmail.com>
2022-03-16 13:32:43 +0900

    feat: 테스트 케이스 수정
    1. test_dequeue_value_is_equal_to_inserted_value: dequeue값이 enqueue했던 값과 동일한지
    2. test_count_is_correct: count가 올바르게 값을 반환하는지
    3. test_clear_method_is_working_well: clear함수가 제대로 동작하는지
    4. test_peek_is_working_well: peek함수가 제대로 동작하는지
    5. test_insert_is_working_well: insert함수가 제대로 동작하는지
    6. test_remove_is_working_well: remove함수가 제대로 동작하는지

--- Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
+++ Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
@@ -2,82 +2,80 @@ import XCTest
 @testable import Calculator
 
 class CalculatorItemQueueTests: XCTestCase {
-    var sut: CalculatorItemQueue = CalculatorItemQueue<Any>()
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    var sut: CalculatorItemQueue = CalculatorItemQueue<Int>()
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func test_enqueue_into_queue() {
+    func test_dequeue_value_is_equal_to_inserted_value() {
         var queue = sut
         queue.enqueue(element: 1)
-        XCTAssertEqual(queue.count, 1)
+        queue.enqueue(element: 2)
+        queue.enqueue(element: 3)
+        
+        XCTAssertEqual(queue.dequeue(), 1)
+        XCTAssertEqual(queue.dequeue(), 2)
+        XCTAssertEqual(queue.dequeue(), 3)
     }
     
-    func test_enqueue_and_dequeue() {
-        var queue = sut
-        queue.enqueue(element: 10)
-        guard let dequeue = queue.dequeue() as? Int else { return }
-        XCTAssertEqual(dequeue, 10)
-    }
-    
-    func test_peek_equal_data() {
+    func test_count_is_correct() {
         var queue = sut
         queue.enqueue(element: 1)
-        guard let peek = queue.peek() else { return }
-        XCTAssertEqual(peek as! Int, 1)
+        queue.enqueue(element: 2)
+        queue.enqueue(element: 3)
+        queue.enqueue(element: 4)
+        queue.enqueue(element: 5)
+        
+        XCTAssertEqual(queue.count, 5)
     }
     
-    func test_count_change_after_peek() {
+    func test_clear_method_is_working_well() {
         var queue = sut
         queue.enqueue(element: 1)
-        queue.peek()
-        XCTAssertEqual(queue.count, 1)
-    }
-    
-    func test_count_change_after_clear() {
-        var queue = sut
-        queue.enqueue(element: 1)
+        queue.enqueue(element: 2)
+        queue.enqueue(element: 3)
+        queue.enqueue(element: 4)
+        queue.enqueue(element: 5)
+        
         queue.clear()
+        
         XCTAssertEqual(queue.count, 0)
     }
     
-    func test_queue_is_Empty() {
-        var queue = sut
-        XCTAssertEqual(queue.isEmpty, true)
-    }
-    
-    func test_queue_is_Empty_after_clear() {
+    func test_peek_is_working_well() {
         var queue = sut
         queue.enqueue(element: 1)
-        queue.clear()
-        XCTAssertEqual(queue.isEmpty, true)
+        
+        XCTAssertEqual(queue.peek(), 1)
+        XCTAssertEqual(queue.count, 1)
     }
     
-    func test_queue_can_push_lots_of_data() {
+    func test_insert_is_working_well() {
         var queue = sut
         queue.enqueue(element: 1)
-        queue.enqueue(element: "+")
+        queue.enqueue(element: 2)
         queue.enqueue(element: 3)
-        queue.enqueue(element: "=")
+        
+        queue.insert(4, at: 1)
+        
+        XCTAssertEqual(queue.count, 4)
+        XCTAssertEqual(queue.dequeue(), 1)
+        XCTAssertEqual(queue.dequeue(), 4)
+        XCTAssertEqual(queue.dequeue(), 2)
+        XCTAssertEqual(queue.dequeue(), 3)
     }
     
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
+    func test_remove_is_working_well() {
+        var queue = sut
+        queue.enqueue(element: 1)
+        queue.enqueue(element: 2)
+        queue.enqueue(element: 3)
+        queue.enqueue(element: 4)
+        queue.enqueue(element: 5)
+        
+        queue.removeAtIndex(3)
+        
+        XCTAssertEqual(queue.count, 4)
+        XCTAssertEqual(queue.dequeue(), 1)
+        XCTAssertEqual(queue.dequeue(), 2)
+        XCTAssertEqual(queue.dequeue(), 3)
+        XCTAssertEqual(queue.dequeue(), 5)
     }
-
 }
