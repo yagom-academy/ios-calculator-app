5184665ebf5876925e0742dbcf5dfb1ff2c8992f
Eddy <kimkyunghun3@naver.com>
2022-03-16 23:29:48 +0900

    Test: 테스트 케이스 추가

--- Calculator/CalculatorTests/CalculatorTests.swift
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -8,43 +8,60 @@
 import XCTest
 @testable import Calculator
 
-
 class CalculatorTests<T>: XCTestCase {
     var sut: QueueLinkedList<Int>!
-    var Operator: QueueLinkedList<String>!
     
     override func setUpWithError() throws {
         try super.setUpWithError()
         sut = QueueLinkedList<Int>()
-        Operator = QueueLinkedList<String>()
     }
 
     override func tearDownWithError() throws {
         try super.tearDownWithError()
         sut = nil
-        Operator = nil
     }
 
-    
-//    func test_deque_one_element() {
-//        let result = ["1"]
-//
-//        result = sut.dequeOperation()
-//
-//
-//        XCTAssertEqual(result, [""])
-//    }
-    
-    func test_count_how_many_elements_in_queue() {
-        var count = 0
+    func test_enque_one_element() {
+        sut.enqueue(1)
         
-        
-        
-        XCTAssertEqual(count, 0)
+        let result = sut.countNode()
+
+        XCTAssertEqual(result, 1)
     }
     
+    func test_enque_two_elements() {
+        sut.enqueue(1)
+        sut.enqueue(2)
+        
+        let result = sut.countNode()
+        
+        XCTAssertEqual(result, 2)
+    }
     
-    
-    
+    func test_enque_one_deque_one_element() {
+        sut.enqueue(1)
+        sut.dequeue()
+        
+        let result = sut.countNode()
 
+        XCTAssertEqual(result, 0)
+    }
+    
+    func test_all_reset() {
+        sut.enqueue(1)
+        sut.enqueue(2)
+        sut.resetAll()
+        
+        let result = sut.countNode()
+        
+        XCTAssertEqual(result, 0)
+    }
+    
+    func test_check_if_isEmpty() {
+        sut.enqueue(1)
+        
+        let result = sut.isEmpty
+        
+        XCTAssertFalse(result)
+    }
 }
