e2c520bd040b88bdc95897af03d8df1fd7a50edf
JoSH0318 <jsh9870@naver.com>
2022-03-16 12:01:51 +0900

    ✨[feat]: add calculate method in enum Operator

--- Calculator/Calculator/Model/Operator.swift
+++ Calculator/Calculator/Model/Operator.swift
@@ -12,4 +12,17 @@ enum Operator: CalculatorItem {
     case minus
     case divide
     case multiply
+    
+    func calculate(lhs: Double, rhs: Double) -> Double {
+        switch self {
+        case .plus:
+            return lhs + rhs
+        case .minus:
+            return lhs - rhs
+        case .divide:
+            return lhs / rhs
+        case .multiply:
+            return lhs * rhs
+        }
+    }
 }
