2112a0669d2906ad4276876dab0df4b9fb583bdf
JoSH0318 <jsh9870@naver.com>
2022-03-17 18:22:29 +0900

    ✅[chore]: modify XTTest code

--- Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
+++ Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
@@ -49,7 +49,7 @@ class CalculatorItemQueueTests: XCTestCase {
         // when
         let result = sut.dequeue()
         // then
-        XCTAssertEqual(result, nil)
+        XCTAssertNil(result)
     }
     
     func test_값이_있는_배열에_dequeue하면_정상반환하는가() {
