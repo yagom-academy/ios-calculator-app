1e3cd2a8ef587307b00bdcd65bb1af03ff4272f4
Grumpy <sewoong2890@gmail.com>
2022-03-17 17:31:15 +0900

    feat: 사칙연산 기능 구현

--- Calculator/Calculator/Operator.swift
+++ Calculator/Calculator/Operator.swift
@@ -8,18 +8,18 @@ enum Operator: Character, CaseIterable {
     }
     
     private func add(lhs: Double, rhs: Double) -> Double {
-        return 0.0
+        return lhs + rhs
     }
     
     private func subtract(lhs: Double, rhs: Double) -> Double {
-        return 0.0
+        return lhs - rhs
     }
     
     private func divide(lhs: Double, rhs: Double) -> Double {
-        return 0.0
+        return lhs / rhs
     }
     
     private func multiply(lhs: Double, rhs: Double) -> Double {
-        return 0.0
+        return lhs * rhs
     }
 }
