0a16d52ff0d850360f4c0ae5de7cc76c07e6b9fd
Mino777 <jomino7777@gmail.com>
2022-03-17 22:53:15 +0900

    chore: OperatorTests 생성

---
+++ Calculator/CalculatorItemQueueTests/OperatorTests.swift
@@ -0,0 +1,20 @@
+//
+//  OperatorTests.swift
+//  CalculatorItemQueueTests
+//
+//  Created by 조민호 on 2022/03/17.
+//
+
+import XCTest
+@testable import Calculator
+
+class OperatorTests: XCTestCase {
+    
+    override func setUpWithError() throws {
+        
+    }
+
+    override func tearDownWithError() throws {
+        
+    }
+}
