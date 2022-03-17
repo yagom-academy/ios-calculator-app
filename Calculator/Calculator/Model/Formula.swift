950d673dbf261e8e3269698c687191162560d8e5
JoSH0318 <jsh9870@naver.com>
2022-03-17 23:53:55 +0900

    📐[style]: modify result method style

--- Calculator/Calculator/Model/Formula.swift
+++ Calculator/Calculator/Model/Formula.swift
@@ -13,15 +13,20 @@ struct Formula {
     
     mutating func result() throws -> Double {
         var middleResult = operands.dequeue()
+        
         while !operands.isEmpty {
             guard let firstOperand = middleResult else {
                 throw CalculationError.unknownError }
+            
             guard let secondOperand = operands.dequeue() else {
                 throw CalculationError.unknownError }
+            
             middleResult = operators.dequeue()?.calculate(lhs: firstOperand, rhs: secondOperand)
         }
+        
         guard let result = middleResult else {
             throw CalculationError.unknownError }
+        
         return result
     }
 }
