adde33d34e007245e706a5e01d4b51e148139bca
saafaaari <111dltldnjs@naver.com>
2022-03-17 20:10:32 +0900

    feat : CalculatorItemQueue 테스트 완료

--- Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
+++ Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
@@ -6,27 +6,105 @@
 //
 
 import XCTest
+@testable import Calculator
 
 class CalculatorItemQueueTests: XCTestCase {
+    var mook: MookList<Int>!
+    var sut: CalculatorItemQueue<MookList<Int>>!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        mook = MookList<Int>()
+        sut = CalculatorItemQueue<MookList<Int>>(mook)
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
+    
+    func test_enqueue() {
+        // given
+        let testData = [1, 2, 3]
+        testData.forEach( sut.enqueue(_:) )
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // when
+
+        // then
+        XCTAssertEqual(testData, mook.dataA)
+    }
+    
+    func test_dequeue() {
+        // given
+        var testData = [1, 2, 3]
+        testData.forEach( sut.enqueue(_:) )
+        sut.dequeue()
+        testData.remove(at: 0)
+
+        // when
+
+        // then
+        XCTAssertEqual(testData, mook.dataA)
+    }
+    
+    func test_removeAll() {
+        // given
+        var testData = [1, 2, 3]
+        testData.forEach( sut.enqueue(_:) )
+        sut.removeAll()
+        testData.removeAll()
+
+        // when
+
+        // then
+        XCTAssertEqual(testData, mook.dataA)
+    }
+    
+    func test_isEmpty_Equal() {
+        // given
+        let testData = [1, 2, 3]
+        testData.forEach( sut.enqueue(_:) )
+
+        // when
+
+        // then
+        XCTAssertEqual(testData.isEmpty, sut.isEmpty)
+    }
+    
+    func test_count_Equal() {
+        // given
+        let testData = [1, 2, 3]
+        testData.forEach( sut.enqueue(_:) )
+
+        // when
+
+        // then
+        XCTAssertEqual(testData.count, sut.count)
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
 
+}
+
+class MookList<T>: List {
+    
+    var dataA: [T] = []
+    
+    var isEmpty: Bool { dataA.isEmpty }
+    
+    var count: Int { dataA.count }
+    
+    func append(_ data: T) {
+        dataA.append(data)
+    }
+    
+    func removeFirst() -> T? {
+        return dataA.remove(at: 0)
+    }
+    
+    func removeAll() {
+        dataA.removeAll()
+    }
+    
+
+    
+    
+    
 }
