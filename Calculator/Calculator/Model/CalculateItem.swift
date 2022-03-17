aacabed8bf114556cc4c04cb8e42907073919166
malrang <ruehd7924@gmail.com>
2022-03-17 22:57:03 +0900

    feat: Operator 열거형 구현

--- Calculator/Calculator/Model/CalculateItem.swift
+++ Calculator/Calculator/Model/CalculateItem.swift
@@ -9,6 +9,34 @@ import Foundation
 
 protocol CalculateItem {}
 
+enum Operator: Character, CaseIterable {
+    case add = "+"
+    case subtract = "-"
+    case divide = "/"
+    case multiply = "*"
+    
+    func calculate(lhs: Double, rhs: Double) -> Double {
+        return 0
+    }
+    
+    private func add(lhs: Double, rhs: Double) -> Double {
+        return 0
+    }
+    
+    private func subtract(lhs: Double, rhs: Double) -> Double {
+        return 0
+    }
+    
+    private func divide(lhs: Double, rhs: Double) -> Double {
+        return 0
+    }
+    
+    private func multiply(lhs: Double, rhs: Double) -> Double {
+        return 0
+    }
+    
+}
+
 extension Double: CalculateItem {}
 
 extension String {
