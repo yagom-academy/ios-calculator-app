5b46a3314fb165cb2096e2e8a315a78cbac5eb56
Donnie <seo_17@naver.com>
2022-03-17 22:09:33 +0900

    feat: result() 피연산자와 연산자를 가지고 결과물을 출력하는 메소드 구현
    * 현재는 주석처리

--- Calculator/Calculator/Model/Formula.swift
+++ Calculator/Calculator/Model/Formula.swift
@@ -10,4 +10,9 @@ import Foundation
 struct Formula<T: CalculateItem> {
     let operands: CalculatorItemQueue<T>
     let operators: CalculatorItemQueue<T>
+    
+//    func result() -> Double {
+//        // 피연산자 연산자 가지고 로직구현
+//        return
+//    }
 }
