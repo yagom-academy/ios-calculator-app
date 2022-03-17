1850228ecd372eb52f19d56423182f1d93fc8e5f
saafaaari <111dltldnjs@naver.com>
2022-03-17 18:32:22 +0900

    add : CalculatorItemQueueTests 파일 추가
    LinkdeList 테스트 완료

---
+++ Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
@@ -0,0 +1,32 @@
+//
+//  CalculatorItemQueueTests.swift
+//  CalculatorItemQueueTests
+//
+//  Created by 이시원 on 2022/03/17.
+//
+
+import XCTest
+
+class CalculatorItemQueueTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
--- Calculator/LinkdeListTests/LinkdeListTests.swift
+++ Calculator/LinkdeListTests/LinkdeListTests.swift
@@ -53,7 +53,7 @@ class LinkdeListTests: XCTestCase {
         XCTAssertEqual(result, 1)
     }
     
-    func test_append_1번_removeFirst_1번_return_true() {
+    func test_append_1번_removeFirst_1번_isEmpty_return_true() {
         // given
         sut.append(1)
         sut.removeFirst()
@@ -64,4 +64,83 @@ class LinkdeListTests: XCTestCase {
         // then
         XCTAssertTrue(result)
     }
+    
+    func test_append_0번_removeFirst_1번_return_nil() {
+        // given
+
+        // when
+        let result = sut.removeFirst()
+
+        // then
+        XCTAssertEqual(result, nil)
+    }
+    
+    func test_append_data_removeFirst_return_data_Equal_true() {
+        // given
+        let testData = 1
+        sut.append(testData)
+        
+
+        // when
+        let result = sut.removeFirst()
+
+        // then
+        XCTAssertEqual(result, testData)
+    }
+    
+    func test_append_3번_FisrtData_removeFirst_return_data_Equal_true() {
+        // given
+        let testData = [1, 2, 3]
+        testData.forEach(sut.append(_:))
+        
+
+        // when
+        let result = sut.removeFirst()
+
+        // then
+        XCTAssertEqual(result, testData[0])
+    }
+    
+    func test_append_3번_removeFirst_1번_count_return_2() {
+        // given
+        let testData = [1, 2, 3]
+        testData.forEach(sut.append(_:))
+        sut.removeFirst()
+        
+
+        // when
+        let result = sut.count
+
+        // then
+        XCTAssertEqual(result, 2)
+    }
+    
+    func test_append_3번_removeAll_isEmpty_return_true() {
+        // given
+        let testData = [1, 2, 3]
+        testData.forEach(sut.append(_:))
+        sut.removeAll()
+        
+
+        // when
+        let result = sut.isEmpty
+
+        // then
+        XCTAssertTrue(result)
+    }
+    
+    func test_append_3번_removeAll_count_return_0() {
+        // given
+        let testData = [1, 2, 3]
+        testData.forEach(sut.append(_:))
+        sut.removeAll()
+        
+
+        // when
+        let result = sut.count
+
+        // then
+        XCTAssertEqual(result, 0)
+    }
+    
 }
