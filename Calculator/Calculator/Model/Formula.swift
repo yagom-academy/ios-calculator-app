e16204be71c1e49051ff67b6e85b19cc574e5bd0
JoSH0318 <jsh9870@naver.com>
2022-03-17 23:45:51 +0900

    ✨[feat]: add result method in struct Formula

--- Calculator/Calculator/Model/Formula.swift
+++ Calculator/Calculator/Model/Formula.swift
@@ -10,4 +10,16 @@ import Foundation
 struct Formula {
     var operands = CalculatorItemQueue<Double>()
     var operators = CalculatorItemQueue<Operator>()
+    
+    mutating func result() -> Double {
+        var middleResult = operands.dequeue()
+        while !operands.isEmpty {
+            guard let firstOperand = middleResult else { break }
+            guard let secondOperand = operands.dequeue() else { break }
+            middleResult = operators.dequeue()?.calculate(lhs: firstOperand, rhs: secondOperand)
+        }
+        guard let result = middleResult else { return 0 }
+        return result
+    }
 }
+
