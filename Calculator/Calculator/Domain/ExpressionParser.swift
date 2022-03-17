f4425d7314a32348631dfa7fcbe3722e2bea4c0e
Lingo <llingo@kakao.com>
2022-03-18 01:48:18 +0900

    refactor: ExpressionParser의 parse 함수의 이중 들여쓰기를 제거
    ExpressionParser의 parse 함수의 이중 들여쓰기를 제거하고 함수형 프로그래밍 방식 적용
    
    cloase #8 #9

--- Calculator/Calculator/Domain/ExpressionParser.swift
+++ Calculator/Calculator/Domain/ExpressionParser.swift
@@ -8,14 +8,18 @@
 enum ExpressionParser {
   static func parse(from input: String) -> Formula {
     var formula = Formula()
-    let inputString = self.componentsByOperators(from: input)
-    inputString.forEach {
-      if let number = Double($0) {
-        formula.operands.enqueue(number)
-      } else if let operation = Operator(rawValue: Character($0)) {
-        formula.operators.enqueue(operation)
+    let result = self.componentsByOperators(from: input)
+    result
+      .compactMap { Double($0) }
+      .forEach {
+        formula.operands.enqueue($0)
       }
-    }
+    result
+      .filter { Double($0) == nil }
+      .compactMap { Operator(rawValue: Character($0)) }
+      .forEach {
+        formula.operators.enqueue($0)
+      }
     return formula
   }
   
