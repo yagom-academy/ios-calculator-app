80e0ac5d2c10a0ef0315feb8ee5e0e755b9e9170
Grumpy <sewoong2890@gmail.com>
2022-03-18 02:26:18 +0900

    feat: parse 함수 구현

--- Calculator/Calculator/ExpressionParser.swift
+++ Calculator/Calculator/ExpressionParser.swift
@@ -2,13 +2,25 @@ import Foundation
 
 enum ExpressionParser {
     public func parse(from input: String) -> Formula {
-        var tempQueue: CalculatorItemQueue = CalculatorItemQueue<Int>()
-        let formula = Formula(operands: tempQueue, operators: tempQueue)
+        var operands: CalculatorItemQueue = CalculatorItemQueue<Double>()
+        var operators: CalculatorItemQueue = CalculatorItemQueue<Character>()
+        let operatorPool:[Character] = ["+", "-", "/", "*"]
         
-        return formula
+        // 연산자(operators)와 피연산자(operands)를 분리해서 각각 큐에 담은 Formula return
+        let operandArray = componentsByOperators(from: input).compactMap({ Double($0) })
+        operandArray.forEach({
+            operands.enqueue(element: Double($0))
+        })
+        
+        let operatorArray = input.filter({ operatorPool.contains($0) })
+        operatorArray.forEach({
+            operators.enqueue(element: $0)
+        })
+        
+        return Formula(operands: operands, operators: operators)
     }
     
     private func componentsByOperators(from input: String) -> [String] {
-        return []
+        return input.components(separatedBy: ["+", "-", "/", "*"])
     }
 }
--- Calculator/Calculator/Formula.swift
+++ Calculator/Calculator/Formula.swift
@@ -1,10 +1,13 @@
 import Foundation
 
 struct Formula {
-    public var operands: CalculatorItemQueue<Int>
-    public var operators: CalculatorItemQueue<Int>
+    public var operands: CalculatorItemQueue<Double>
+    public var operators: CalculatorItemQueue<Character>
     
     public func result() -> Double {
+        //연산 결과 return
+        
+        
         return 0.0
     }
 }
