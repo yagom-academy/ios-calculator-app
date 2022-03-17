8bbac805c2dfc35c2be52f4f2ab310f7e236a23e
Grumpy <sewoong2890@gmail.com>
2022-03-17 17:29:06 +0900

    feat: String 타입을 확장하여 split 메서드 구현

--- Calculator/Calculator/CalculateItem.swift
+++ Calculator/Calculator/CalculateItem.swift
@@ -15,3 +15,9 @@ extension Double: CalculateItem {
 extension Character: CalculateItem {
     
 }
+
+extension String {
+    public func split(with target: Character) -> [String] {
+        return self.components(separatedBy: String(target))
+    }
+}
