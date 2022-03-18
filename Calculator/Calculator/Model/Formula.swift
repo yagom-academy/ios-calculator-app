907bd241f7cb318dc1c6c5e703f4ca9797070e5f
Mino777 <jomino7777@gmail.com>
2022-03-18 11:26:22 +0900

    test: test_operand가없는경우에result를호출할때_예상되는에러를반환해야한다 케이스 정의

--- Calculator/Calculator/Model/Formula.swift
+++ Calculator/Calculator/Model/Formula.swift
@@ -8,5 +8,16 @@
 import Foundation
 
 struct Formula {
+    private var operands: CalculatorItemQueue<Double>
+    private var operators: CalculatorItemQueue<Operator>
+
+    init(operands: CalculatorItemQueue<Double>,
+         operators: CalculatorItemQueue<Operator>) {
+        self.operands = operands
+        self.operators = operators
+    }
     
+    func result() -> Double {
+        
+    }
 }
--- Calculator/CalculatorItemQueueTests/FormulaTests.swift
+++ Calculator/CalculatorItemQueueTests/FormulaTests.swift
@@ -8,12 +8,37 @@
 import XCTest
 
 class FormulaTests: XCTestCase {
-
+    var sut: Formula!
+    var mockDoubleQueue: CalculatorItemQueue<Double>!
+    var mockOperatorQueue: CalculatorItemQueue<Operator>!
+    var mockOperand: [Double]!
+    var mockOperator: [Operator]!
+    
     override func setUpWithError() throws {
-        
+        sut = Formula(operands: mockDoubleQueue, operators: mockOperatorQueue)
+        mockOperand = [1.0, 2.0, 3.0, 4.0, 5.0]
+        mockOperator = [.add, .substract, .divide, .multiply]
     }
 
     override func tearDownWithError() throws {
+        sut = nil
+        mockDoubleQueue.clear()
+        mockOperatorQueue.clear()
+        mockOperand.removeAll()
+        mockOperator.removeAll()
+    }
+    
+    func test_operand가없는경우에result를호출할때_예상되는에러를반환해야한다() {
+        // given
+        mockOperator.forEach {
+            mockOperatorQueue.enqueue($0)
+        }
         
+        // when
+        // then
+        let expected: QueueError = .notFoundElement
+        XCTAssertThrowsError(try sut.result()) { error in
+            XCTAssertEqual(error as? QueueError, expected)
+        }
     }
 }
