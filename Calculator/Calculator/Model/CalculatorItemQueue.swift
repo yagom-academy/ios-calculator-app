1303497787d69b82b8d85ea80db379ac6dbec1d4
Red <seryprk@gmail.com>
2022-03-18 18:20:49 +0900

    💎feat : CalculatorItemQueue 의 init 생성

--- Calculator/Calculator/Model/CalculatorItemQueue.swift
+++ Calculator/Calculator/Model/CalculatorItemQueue.swift
@@ -16,6 +16,10 @@ struct CalculatorItemQueue<T: CalculateItem> {
     private(set) var head: Int = 0
     private(set) var tail: Int = 0
     
+    init(values: [T]) {
+        values.forEach { enqueue(with: $0)}
+    }
+    
     var isEmpty: Bool {
         return queue.isEmpty
     }
