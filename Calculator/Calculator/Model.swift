9da73892f979c27fb64ed6b06e45beb539c5d3b0
Red <seryprk@gmail.com>
2022-03-18 17:08:29 +0900

    💎feat : ExpressionParser의 parse 메서드 구현

--- Calculator/Calculator/Model.swift
+++ Calculator/Calculator/Model.swift
@@ -105,6 +105,23 @@ extension String {
 enum ExpressionParser {
     case scroll
     
+    func parse(form input: String) -> Formula {
+        var fomula = Formula()
+        let values = componentsByOperators(from: input)
+        
+        values.enumerated()
+            .filter { $0.offset % 2 == 0 }
+            .compactMap { Double($0.element) ?? .nan }
+            .forEach { fomula.operands.enqueue(with: $0) }
+        
+        values.enumerated()
+            .filter { $0.offset % 2 != 0 }
+            .compactMap { Operator(rawValue: Character($0.element)) }
+            .forEach { fomula.operators.enqueue(with: $0) }
+        
+        return fomula
+    }
+    
     func componentsByOperators(from input: String) -> [String] {
         return input.split(with: " ")
     }
