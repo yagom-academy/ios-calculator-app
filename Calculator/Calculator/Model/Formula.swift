bd1cfa9a2ccaddcb20ecc52eba8623ed50300a0e
marisol <marisol.develop@gmail.com>
2022-03-18 16:49:39 +0900

    [add]: add Formula.swift
    - Formula.swift 파일 추가
    - operands, operators 프로퍼티, result 메서드 추가

---
+++ Calculator/Calculator/Model/Formula.swift
@@ -0,0 +1,17 @@
+//
+//  Formula.swift
+//  Calculator
+//
+//  Created by marisol on 2022/03/18.
+//
+
+import Foundation
+
+struct Formula {
+    public var operands: CalculatorItemQueue<Double>
+    public var operators: CalculatorItemQueue<Operator>
+    
+    public func result() -> Double {
+        
+    }
+}
