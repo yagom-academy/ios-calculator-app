62e0a602e016fcb5ff4d985cf456426f098c2f8a
Tiana <97kth01@naver.com>
2022-03-18 15:00:56 +0900

    feat : ExpressionParser 구현

--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -6,23 +6,39 @@
 //
 
 enum ExpressionParser {
-//    func parse(from input: String) -> Formula {
-//        let operandsList = LinkedList<Double>()
-//        
-//        componentsByOperators(from: input)
-//            .compactMap { Double($0)}
-//            .map { Node(data: $0) }
-//            .forEach { operandsList.append(node: $0) }
-//        
-//        let operators = componentsByOperators(from: input)
-//        let operandsQueue = CalculatorItemQueue(linkedList: <#T##LinkedList<_>#>)
-//        
-//        return Formula(operands: operands, operators: operators)
-//    }
-//    
-//    private func componentsByOperators(from input: String) -> [String] {
-//        let operators = Operator.allCases
-//        
-//        return input.split(with: <#T##Character#>)
-//    }
+    static func parse(from input: String) -> Formula {
+        let operands = LinkedList<Double>()
+        let operators = LinkedList<Operator>()
+        
+        componentsByOperators(from: input)
+            .compactMap { Double($0) }
+            .map { Node(data: $0) }
+            .forEach { operands.append(node: $0) }
+        
+        input
+            .compactMap { Operator(rawValue: $0) }
+            .map { Node(data: $0) }
+            .forEach { operators.append(node: $0) }
+        
+        let operandQueue = CalculatorItemQueue(linkedList: operands)
+        let operatorQueue = CalculatorItemQueue(linkedList: operators)
+        
+        return Formula(operands: operandQueue, operators: operatorQueue)
+    }
+    
+    static private func componentsByOperators(from input: String) -> [String] {
+        var result: [String] = []
+        
+        input.forEach {
+            guard let _ = Operator(rawValue: $0) else {
+                result.append(String($0))
+                return
+            }
+            result.append("_")
+        }
+        
+        let stringResult: String = result.joined()
+        
+        return stringResult.split(with: "_")
+    }
 }
---
+++ Calculator/ExpressionParserTests/ExpressionParserTests.swift
@@ -0,0 +1,25 @@
+//
+//  ExpressionParserTests.swift
+//  ExpressionParserTests
+//
+//  Created by 김태현 on 2022/03/18.
+//
+
+import XCTest
+@testable import Calculator
+
+class ExpressionParserTests: XCTestCase {
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+    }
+
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+    }
+
+    func test_parse_formula_result확인() {
+        let formula = ExpressionParser.parse(from: "10*46-50")
+        
+        XCTAssertEqual(formula.result(), 410)
+    }
+}
