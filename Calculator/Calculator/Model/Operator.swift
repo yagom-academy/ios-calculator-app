696d6e615e84c7aab76de6f40d8d92612ca33d1c
Donnie <seo_17@naver.com>
2022-03-18 15:39:55 +0900

    feat: DivideError처리 구현

--- Calculator/Calculator/Model/Operator.swift
+++ Calculator/Calculator/Model/Operator.swift
@@ -7,20 +7,24 @@
 
 import Foundation
 
+enum DivideError: Error {
+    case divideByZero
+}
+
 enum Operator: Character, CaseIterable, CalculateItem {
     case add = "+"
     case subtract = "-"
     case divide = "/"
     case multiply = "*"
     
-    func calculate(lhs: Double, rhs: Double) -> Double {
+    func calculate(lhs: Double, rhs: Double) throws -> Double {
         switch self {
         case .add:
             return add(lhs, rhs)
         case .subtract:
             return subtract(lhs, rhs)
         case .divide:
-            return divide(lhs, rhs)
+            return try divide(lhs, rhs)
         case .multiply:
             return multiply(lhs, rhs)
         }
@@ -34,8 +38,11 @@ enum Operator: Character, CaseIterable, CalculateItem {
         return lhs - rhs
     }
     
-    private func divide(_ lhs: Double, _ rhs: Double) -> Double {
-        return lhs / rhs
+    private func divide(_ lhs: Double, _ rhs: Double) throws -> Double {
+        guard rhs != 0 else {
+            return lhs / rhs
+        }
+            throw DivideError.divideByZero
     }
     
     private func multiply(_ lhs: Double, _ rhs: Double) -> Double {
