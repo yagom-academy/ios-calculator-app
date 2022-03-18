585eb6144704081d02293da0cf87e5c1572ea569
Mino777 <jomino7777@gmail.com>
2022-03-18 10:59:08 +0900

    chore: FormulaTests 생성

---
+++ Calculator/CalculatorItemQueueTests/FormulaTests.swift
@@ -0,0 +1,19 @@
+//
+//  FormulaTests.swift
+//  CalculatorItemQueueTests
+//
+//  Created by 조민호 on 2022/03/18.
+//
+
+import XCTest
+
+class FormulaTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        
+    }
+
+    override func tearDownWithError() throws {
+        
+    }
+}
