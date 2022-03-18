0911b83b74880b29d6ce5ddbd55f5eb254f77853
JoSH0318 <jsh9870@naver.com>
2022-03-18 13:58:28 +0900

    ➕[add]: implement test codes in FormulaTests

--- Calculator/Tests/FormulaTests.swift
+++ Calculator/Tests/FormulaTests.swift
@@ -20,4 +20,48 @@ class FormulaTests: XCTestCase {
         try super.tearDownWithError()
         sut = nil
     }
+    
+    func test_add() {
+        // given
+        sut.operands.enqueue(1.0)
+        sut.operands.enqueue(2.0)
+        // when
+        sut.operators.enqueue(.add)
+        let result = sut.result()
+        // then
+        XCTAssertEqual(result, 3.0)
+    }
+    
+    func test_subtract() {
+        // given
+        sut.operands.enqueue(2.0)
+        sut.operands.enqueue(1.0)
+        // when
+        sut.operators.enqueue(.subtract)
+        let result = sut.result()
+        // then
+        XCTAssertEqual(result, 1.0)
+    }
+    
+    func test_divide() {
+        // given
+        sut.operands.enqueue(4.0)
+        sut.operands.enqueue(2.0)
+        // when
+        sut.operators.enqueue(.divide)
+        let result = sut.result()
+        // then
+        XCTAssertEqual(result, 2.0)
+    }
+    
+    func test_multiply() {
+        // given
+        sut.operands.enqueue(2.0)
+        sut.operands.enqueue(3.0)
+        // when
+        sut.operators.enqueue(.multiply)
+        let result = sut.result()
+        // then
+        XCTAssertEqual(result, 6.0)
+    }
 }
