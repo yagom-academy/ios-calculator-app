fac31d7912552cf4f57c2487e971aec98fe6f99d
JoSH0318 <jsh9870@naver.com>
2022-03-18 20:47:29 +0900

    ♻️[refactor]: func result() 내부 optional binding 변경

--- Calculator/Calculator/Model/Formula.swift
+++ Calculator/Calculator/Model/Formula.swift
@@ -12,7 +12,9 @@ struct Formula {
     var operators = CalculatorItemQueue<Operator>()
     
     mutating func result() -> Double {
-        var middleResult = operands.dequeue() ?? .nan
+        guard var middleResult = operands.dequeue() else {
+            return .nan
+        }
         
         while let operand = operands.dequeue(), let onceOperator = operators.dequeue() {
             middleResult = onceOperator.calculate(lhs: middleResult, rhs: operand)
