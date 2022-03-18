eeac5413eeea00b16cfb01b70cb53d810acb9a93
JoSH0318 <jsh9870@naver.com>
2022-03-18 16:11:08 +0900

    ✨[feat]: implement parse method

--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -6,8 +6,20 @@
 //
 
 import Foundation
-
+// input String = "1 + 2 - -3"
 enum ExpressionParser {
+    func parse(from input: String) -> Formula {
+        var formula = Formula()
+        componentsByOperator(from: input) // ["1", "+", "2", "-", "-3"]
+            .compactMap{ Double($0) } // [1, 2, -3]
+            .forEach{ formula.operands.enqueue($0) } // enqueue [1, 2, -3]
+        componentsByOperator(from: input) // ["1", "+", "2", "-", "-3"]
+            .filter{ Double($0) == nil } // ["+", "-"]
+            .compactMap{ Operator(rawValue: Character($0)) } // [.add, .subtract]
+            .forEach{ formula.operators.enqueue($0) } //dequeue [.add, .subtract]
+        return formula
+    }
+    
     private func componentsByOperator(from input: String) -> [String] {
         return input.split(with: " ")
     }
