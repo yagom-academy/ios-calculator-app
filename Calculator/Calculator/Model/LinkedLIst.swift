7c3ec136c9a9d4e2e7ed4007b59b26a5fadfafe7
GoMyungju171 <mooooongdu171@gmail.com>
2022-03-18 18:54:48 +0900

    feat: addNode, isEmpty 메서드 생성, tail변수 추가

---
+++ Calculator/Calculator/Model/LinkedLIst.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+struct LinkedList<T> {
+    private var head: LinkedListNode<T>?//첫번째노드를가리킬head
+    private var tail: LinkedListNode<T>?//마지막노드를가리킬tail
+    var count: Int = 0
+    
+    func isEmpty(data: T) -> Bool {
+        return self.head == nil
+    }
+    
+    mutating func addNode(data: T?) -> LinkedListNode<T>? {
+        if head == nil || tail == nil {
+            head = LinkedListNode.init(data: data)
+            tail = head
+            return head
+        }
+        let newNode = LinkedListNode.init(data: data)
+        tail?.next = newNode
+        newNode.prev = tail
+        tail = newNode
+        return tail
+    }
+}
--- Calculator/Calculator/Model/LinkedListNode.swift
+++ Calculator/Calculator/Model/LinkedListNode.swift
@@ -1,10 +1,13 @@
 import Foundation
 
 class LinkedListNode<T> {
-    var data: T
+    var data: T?
+    var prev: LinkedListNode?
     var next: LinkedListNode?
     
-    init(data: T) {
+    init(data: T?, prev: LinkedListNode? = nil, next: LinkedListNode? = nil) {
         self.data = data
+        self.prev = prev
+        self.next = next
     }
 }
