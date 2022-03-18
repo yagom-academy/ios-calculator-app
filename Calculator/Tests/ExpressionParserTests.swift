fb415aa644f79bc9dcad51f42c4566784df701b2
JoSH0318 <jsh9870@naver.com>
2022-03-18 16:35:15 +0900

    🔨[fix]: fix error because test file name

---
+++ Calculator/Tests/ExpressionParserTests.swift
@@ -0,0 +1,20 @@
+//
+//  ExpressionParserTests.swift
+//  Tests
+//
+//  Created by mmim.
+//
+
+import XCTest
+@testable import Calculator
+
+class ExpressionParserTests: XCTestCase {
+    func test_calculation() {
+        // given
+
+        // when
+        
+        // then
+        
+    }
+}
