2a7ec214a8d2b11319628af08c0c85d3d668250a
saafaaari <111dltldnjs@naver.com>
2022-03-18 16:03:22 +0900

    feat : Formula 타입 계산기능 함수 result 구현
    테스트 진행

--- Calculator/Calculator/Formula.swift
+++ Calculator/Calculator/Formula.swift
@@ -10,4 +10,15 @@ import Foundation
 struct Formula {
     let operands: CalculatorItemQueue<LinkdeList<Double>>
     let operators: CalculatorItemQueue<LinkdeList<Operator>>
+    
+    func result() -> Double {
+        guard var value: Double = operands.dequeue() else { return 0 }
+        while !operands.isEmpty {
+            guard let operand = operands.dequeue() else { return value }
+            guard let calculatedValue = operators.dequeue()?.calculate(lhs: value, rhs: operand) else { return value }
+            value = calculatedValue
+        }
+        
+        return value
+    }
 }
--- Calculator/CalculatorTests/FormulaTests.swift
+++ Calculator/CalculatorTests/FormulaTests.swift
@@ -10,13 +10,35 @@ import XCTest
 
 class FormulaTests: XCTestCase {
     var sut: Formula!
+    var operatorQueue: CalculatorItemQueue<LinkdeList<Operator>>!
+    var operandQueue: CalculatorItemQueue<LinkdeList<Double>>!
 
     override func setUpWithError() throws {
-        sut = Formula(operands: CalculatorItemQueue<LinkdeList<Double>>(LinkdeList<Double>()), operators: CalculatorItemQueue<LinkdeList<Operator>>(LinkdeList<Operator>()))
+        operatorQueue = CalculatorItemQueue<LinkdeList<Operator>>(LinkdeList<Operator>())
+        operandQueue = CalculatorItemQueue<LinkdeList<Double>>(LinkdeList<Double>())
+        sut = Formula(operands: operandQueue, operators: operatorQueue)
     }
 
     override func tearDownWithError() throws {
         sut = nil
+        operatorQueue = nil
+        operandQueue = nil
+        
+    }
+    
+    func test_result() {
+        // given
+        let operatorData = [Operator.add, Operator.subtract, Operator.divide, Operator.multiply]
+        let operandData = [1.0, 5.0, 3.0, 4.0, 6.0]
+        operatorData.forEach( operatorQueue.enqueue(_:) )
+        operandData.forEach(operandQueue.enqueue(_:))
+
+        // when
+        let result = sut.result()
+
+        // then
+        XCTAssertEqual(result, (1+5-3)/4*6)
+        
         
     }
     
