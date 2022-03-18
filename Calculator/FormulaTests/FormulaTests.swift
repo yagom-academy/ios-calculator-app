855164ffb6c46d0fbd621a11181544accf019b80
JoSH0318 <jsh9870@naver.com>
2022-03-18 13:40:23 +0900

    ➕[add]: add FormulaTests.swift for Unit Test

---
+++ Calculator/FormulaTests/FormulaTests.swift
@@ -0,0 +1,21 @@
+//
+//  FormulaTests.swift
+//  FormulaTests
+//
+//  Created by mmim.
+//
+
+import XCTest
+@testable import Calculator
+
+class FormulaTests: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+    
+
+}
