f967dd640a63cf5492c7a9107c1482b03954fccd
Grumpy <sewoong2890@gmail.com>
2022-03-18 20:21:31 +0900

    feat: 단위 테스트에서 error handling

--- Calculator/FormulaTests/FormulaTests.swift
+++ Calculator/FormulaTests/FormulaTests.swift
@@ -12,58 +12,74 @@ class FormulaTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    
+
     func test_add_12_and_23_equals_25() {
+        //given
         sut = Formula()
-        
         sut?.operands.enqueue(element: 12.0)
         sut?.operands.enqueue(element: 23.0)
         sut?.operators.enqueue(element: "+")
-        
-        guard let result: Double = sut?.result() else { return }
-        
-        XCTAssertEqual(result, 35.0)
+
+        //when
+        do {
+            guard let result: Double = try sut?.result() else { return }
+            
+            //then
+            XCTAssertEqual(result, 35.0)
+        } catch { }
     }
-    
+
     func test_add_minus_10_and_15_equals_5() {
+        //given
         sut = Formula()
-        
         sut?.operands.enqueue(element: -10.0)
         sut?.operands.enqueue(element: 15.0)
         sut?.operators.enqueue(element: "+")
         
-        guard let result: Double = sut?.result() else { return }
-        
-        XCTAssertEqual(result, 5.0)
+        //when
+        do {
+            guard let result: Double = try sut?.result() else { return }
+            
+            //then
+            XCTAssertEqual(result, 5.0)
+        } catch { }
     }
-    
+
     func test_divide_81_by_minus_9_equals_minus_9() {
+        //given
         sut = Formula()
-        
         sut?.operands.enqueue(element: 81.0)
         sut?.operands.enqueue(element: -9.0)
         sut?.operators.enqueue(element: "/")
-        
-        guard let result: Double = sut?.result() else { return }
-        
-        XCTAssertEqual(result, -9.0)
+
+        //when
+        do {
+            guard let result: Double = try sut?.result() else { return }
+            
+            //then
+            XCTAssertEqual(result, -9.0)
+        } catch { }
     }
-    
+
     func test_multiply_4_by_6_equals_24() {
+        //given
         sut = Formula()
-        
         sut?.operands.enqueue(element: 4.0)
         sut?.operands.enqueue(element: 6.0)
         sut?.operators.enqueue(element: "*")
         
-        guard let result: Double = sut?.result() else { return }
-        
-        XCTAssertEqual(result, 24.0)
+        //when
+        do {
+            guard let result: Double = try sut?.result() else { return }
+            
+            //then
+            XCTAssertEqual(result, 24.0)
+        } catch { }
     }
 
     func test_multiply_plus_divide_formula() {
+        //given
         sut = Formula()
-        
         sut?.operands.enqueue(element: 4.0)
         sut?.operands.enqueue(element: 6.0)
         sut?.operands.enqueue(element: 10.0)
@@ -71,10 +87,14 @@ class FormulaTests: XCTestCase {
         sut?.operators.enqueue(element: "*")
         sut?.operators.enqueue(element: "+")
         sut?.operators.enqueue(element: "/")
-        
-        guard let result: Double = sut?.result() else { return }
-        
-        XCTAssertEqual(result, 6.8)
+
+        //when
+        do {
+            guard let result: Double = try sut?.result() else { return }
+            
+            //then
+            XCTAssertEqual(result, 6.8)
+        } catch { }
     }
     
     func testPerformanceExample() throws {
--- Calculator/OperatorTests/OperatorTests.swift
+++ Calculator/OperatorTests/OperatorTests.swift
@@ -17,42 +17,67 @@ class OperatorTests: XCTestCase {
         sut = Operator.add
         
         //when
-        let result = sut?.calculate(lhs: 3.0, rhs: 5.0)
-        
-        //then
-        XCTAssertEqual(result, 8.0)
+        do {
+            let result = try sut?.calculate(lhs: 3.0, rhs: 5.0)
+            
+            //then
+            XCTAssertEqual(result, 8.0)
+        } catch {
+            
+        }
     }
     
     func test_subtract_7_from_10_equals_3() {
         //given
         sut = Operator.subtract
+        do {
+            //when
+            let result = try sut?.calculate(lhs: 10.0, rhs: 7.0)
+            
+            //then
+            XCTAssertEqual(result, 3.0)
+        } catch { }
         
-        //when
-        let result = sut?.calculate(lhs: 10.0, rhs: 7.0)
-        
-        //then
-        XCTAssertEqual(result, 3.0)
     }
 
     func test_divide_81_by_9_equals_9() {
         //given
         sut = Operator.divide
         
-        //when
-        let result = sut?.calculate(lhs: 81.0, rhs: 9.0)
+        do {
+            //when
+            let result = try sut?.calculate(lhs: 81.0, rhs: 9.0)
+            
+            //then
+            XCTAssertEqual(result, 9.0)
+        } catch { }
         
-        //then
-        XCTAssertEqual(result, 9.0)
     }
     
     func test_multiply_4_by_6_equals_24() {
         //given
         sut = Operator.multiply
+        do {
+            //when
+            let result = try sut?.calculate(lhs: 4.0, rhs: 6.0)
+            
+            //then
+            XCTAssertEqual(result, 24.0)
+        } catch { }
         
-        //when
-        let result = sut?.calculate(lhs: 4.0, rhs: 6.0)
+    }
+    
+    func test_divide_any_number_by_0_equals_nan() {
         
-        //then
-        XCTAssertEqual(result, 24.0)
+        //given
+        sut = Operator.divide
+        do {
+            //when
+            let result = try sut?.calculate(lhs: 1000.0, rhs: 0.0)
+            
+            //then
+            XCTAssertEqual(result?.isNaN, true)
+        } catch { }
+        
     }
 }
