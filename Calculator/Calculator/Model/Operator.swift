056b2a44075fea1cf48f0c60f2128ec1cad91002
marisol <marisol.develop@gmail.com>
2022-03-18 15:56:06 +0900

    [rename]: rename enum Operator cases
    - Operator 열거형의 case를 UML대로 수정

---
+++ Calculator/Calculator/Model/Operator.swift
@@ -0,0 +1,17 @@
+//
+//  Operators.swift
+//  Calculator
+//
+//  Created by Marisol Mirim Kim on 2022/03/15.
+//
+
+import Foundation
+
+enum Operator: Character, CaseIterable, CalculateItem {
+    case add = "+"
+    case subtract = "-"
+    case divide = "/"
+    case multiply = "*"
+}
+extension Operator {
+}
