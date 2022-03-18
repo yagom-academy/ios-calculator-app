5d2e82a3219dc2828c5c100c1422a8ef7380e55e
marisol <marisol.develop@gmail.com>
2022-03-18 16:11:17 +0900

    [feat]: add calculate() in extension Operator
    - calculate() 메서드와, calculate() 내부에서 호출되는 add(), subtract(), divide(), multiply() 메서드 구현

--- Calculator/Calculator/Model/Operator.swift
+++ Calculator/Calculator/Model/Operator.swift
@@ -14,4 +14,32 @@ enum Operator: Character, CaseIterable, CalculateItem {
     case multiply = "*"
 }
 extension Operator {
+    public func calculate(lhs: Double, rhs: Double) -> Double {
+        switch self {
+        case .add:
+            return add(lhs: lhs, rhs: rhs)
+        case .subtract:
+            return subtract(lhs: lhs, rhs: rhs)
+        case .divide:
+            return divide(lhs: lhs, rhs: rhs)
+        case .multiply:
+            return multiply(lhs: lhs, rhs: rhs)
+        }
+    }
+    
+    private func add(lhs: Double, rhs: Double) -> Double {
+        return lhs + rhs
+    }
+    
+    private func subtract(lhs: Double, rhs: Double) -> Double {
+        return lhs - rhs
+    }
+    
+    private func divide(lhs: Double, rhs: Double) -> Double {
+        return lhs / rhs
+    }
+    
+    private func multiply(lhs: Double, rhs: Double) -> Double {
+        return lhs * rhs
+    }
 }
