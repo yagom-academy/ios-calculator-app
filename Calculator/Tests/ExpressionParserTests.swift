4c281d2b67aca94689f900ccc29ae132b32e139a
JoSH0318 <jsh9870@naver.com>
2022-03-18 16:57:32 +0900

    ✨[feat]: implement test code

--- Calculator/Tests/ExpressionParserTests.swift
+++ Calculator/Tests/ExpressionParserTests.swift
@@ -11,10 +11,21 @@ import XCTest
 class ExpressionParserTests: XCTestCase {
     func test_calculation() {
         // given
-
+        let someInput = "1 + 2 - -3"
+        var expressionParser = ExpressionParser.parse(from: someInput)
         // when
-        
+        let result = expressionParser.result()
         // then
-        
+        XCTAssertEqual(result, 6.0)
+    }
+    
+    func test_calculation_연산자로_끝나는_경우() {
+        // given
+        let someInput = "1 + 2 - -3 +"
+        var expressionParser = ExpressionParser.parse(from: someInput)
+        // when
+        let result = expressionParser.result()
+        // then
+        XCTAssertEqual(result, 6.0)
     }
 }
