7aa7cc9da7d20bfa3abae0b9ca0086106c71412b
Taeangel <wngks2050@gmail.com>
2022-03-19 03:17:36 +0900

    feat: 요구조건 UML에 있는 타입과 메서드 프로퍼티 구현
    요구조건에 있는 ExpressionParser 열거형과 Formula 구조체 구현

---
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -0,0 +1,20 @@
+//
+//  ExpressionParser.swift
+//  Calculator
+//
+//  Created by song on 2022/03/19.
+//
+
+import Foundation
+
+extension String {
+  func split(with target: Character) {
+    //String배열 return
+  }
+}
+
+enum ExpressionParser {
+  func parse(from input: String) {
+    //Formula return
+  }
+}
---
+++ Calculator/Calculator/Model/Formula.swift
@@ -0,0 +1,17 @@
+//
+//  Formula.swift
+//  Calculator
+//
+//  Created by song on 2022/03/19.
+//
+
+import Foundation
+
+struct Formula {
+  var operands: CalculateItemQueue<Double>
+  var operators: CalculateItemQueue<Operator>
+  
+  func result() {
+    //Double return
+  }
+}
