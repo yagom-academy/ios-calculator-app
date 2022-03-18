7e3c596b8a6c7451dc4ccbad5a0c5f28534fe0ac
malrang <ruehd7924@gmail.com>
2022-03-18 22:19:47 +0900

    refactor: ExpressionParser parse 메서드 내부 수정

--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -10,15 +10,18 @@ import Foundation
 enum ExpressionParser {
     func parse(from input: String) -> Formula {
         let componentsString = componentsByOperators(from: input)
-        
         var values = LinkedQueue<Double>()
-        let convertDouble = componentsString.compactMap{ Double($0) }
-        convertDouble.forEach{ values.append(newNode: $0) }
-        
         var oper = LinkedQueue<Operator>()
-        let converteOper = componentsString.compactMap{ Operator(rawValue: Character($0)) }
-        converteOper.forEach{ oper.append(newNode: $0) }
-    
+        
+        componentsString.forEach{
+            if Double($0) != nil {
+                values.append(newNode: Double($0))
+            } else {
+                let convertOper = Operator(rawValue: Character($0))
+                oper.append(newNode: convertOper)
+            }
+        }
+        
         let calculator = Formula(operands: values, operators: oper)
         return calculator
     }
@@ -27,3 +30,4 @@ enum ExpressionParser {
         return input.split(with: " ")
     }
 }
+
