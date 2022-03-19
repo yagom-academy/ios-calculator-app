9220b6c876d06609f242357da026c50c32c3b9e5
dnwhd0112 <dnwhd0112@naver.com>
2022-03-20 06:17:28 +0900

    refactor: calculate 함수 변경
    매개변수 옵셔널로 변경 및 nil 값이 들어올시 nil 값 반환

--- Calculator/Modal/Operator.swift
+++ Calculator/Modal/Operator.swift
@@ -13,36 +13,39 @@ enum Operator: Character, CaseIterable, CalculateItem {
     case divide = "/"
     case multiply = "*"
     
-    func calculate(lhs: Double, rhs: Double) -> Double {
+    func calculate(lhs: Double?, rhs: Double?) -> Double? {
+        guard let doubleLhs = lhs, let doubleRhs = rhs else {
+            return nil
+        }
         switch self {
             
         case .add:
-            return add(lhs: lhs, rhs: rhs)
+            return add(lhs: doubleLhs, rhs: doubleRhs)
         case .subtract:
-            return subtract(lhs: lhs, rhs: rhs)
+            return subtract(lhs: doubleLhs, rhs: doubleRhs)
         case .divide:
-            return divide(lhs: lhs, rhs: rhs)
+            return divide(lhs: doubleLhs, rhs: doubleRhs)
         case .multiply:
-            return multiply(lhs: lhs, rhs: rhs)
+            return multiply(lhs: doubleLhs, rhs: doubleRhs)
         }
     }
     
-    private func add(lhs: Double, rhs: Double) -> Double {
+    private func add(lhs: Double, rhs: Double) -> Double? {
         return lhs + rhs
     }
     
-    private func subtract(lhs: Double, rhs: Double) -> Double {
+    private func subtract(lhs: Double, rhs: Double) -> Double? {
         return lhs - rhs
     }
     
-    private func divide(lhs: Double, rhs: Double) -> Double {
+    private func divide(lhs: Double, rhs: Double) -> Double? {
         if rhs == 0 {
-            return 0
+            return nil
         }
         return lhs / rhs
     }
     
-    private func multiply(lhs: Double, rhs: Double) -> Double {
+    private func multiply(lhs: Double, rhs: Double) -> Double? {
         return lhs * rhs
     }
 }
