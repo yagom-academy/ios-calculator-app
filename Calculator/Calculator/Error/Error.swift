6784cf61b4b0ba1bd4446a20241e80784afe1660
Quokkaa <rktrkdml@gmail.com>
2022-03-20 18:57:50 +0900

    feat: enum DevideError 생성
    * Error file추가

---
+++ Calculator/Calculator/Error/Error.swift
@@ -0,0 +1,9 @@
+//  Error.swift
+//  Calculator
+//  Created by LIMGAUI on 2022/03/20
+
+import Foundation
+
+enum DevideError: Error {
+    case isNaN(Double)
+}
--- Calculator/Calculator/Model/Operator.swift
+++ Calculator/Calculator/Model/Operator.swift
@@ -10,14 +10,14 @@ enum Operator: Character, CaseIterable, CalculateItem {
     case divide = "➗"
     case multiply = "✖️"
     
-    func calculate(lhs: Double, rhs: Double) -> Double {
+    func calculate(lhs: Double, rhs: Double) throws -> Double {
         switch self {
         case .add:
             return add(lhs: lhs, rhs: rhs)
         case .subtract:
             return subtract(lhs: lhs, rhs: rhs)
         case .divide:
-            return divide(lhs: lhs, rhs: rhs)
+            return try divide(lhs: lhs, rhs: rhs)
         case .multiply:
             return multiply(lhs: lhs, rhs: rhs)
         }
@@ -31,7 +31,10 @@ enum Operator: Character, CaseIterable, CalculateItem {
         return lhs-rhs
     }
     
-    private func divide(lhs: Double, rhs: Double) -> Double {
+    private func divide(lhs: Double, rhs: Double) throws -> Double {
+        if rhs == 0 {
+            throw DevideError.isNaN(.nan)
+        }
         return lhs/rhs
     }
     
