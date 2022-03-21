d5eefcff96cfa0d649452b8409937e731f34f9e8
doogie97 <chris_97@naver.com>
2022-03-21 11:51:04 +0900

    feat: result함수 구현 및 테스트 케이스 작성

--- Calculator/Calculator/Model/Formula.swift
+++ Calculator/Calculator/Model/Formula.swift
@@ -4,7 +4,17 @@ struct Formula {
     var operands = CalculatorItemQueue<Double>()
     var operations = CalculatorItemQueue<Operator>()
     
-    func result() -> Double {
-        return 0
+    mutating func result() -> Double {
+        guard var result = operands.dequeue() else { return 0}
+        while !operands.isEmpty {
+            guard let rhs = operands.dequeue() else { return result }
+            let pickedOperation = operations.dequeue()
+            for op in Operator.allCases {
+                if pickedOperation == op {
+                    result = op.calcuate(lhs: result, rhs: rhs)
+                }
+            }
+        }
+        return result
     }
 }
--- Calculator/FormulaTest/FormulaTest.swift
+++ Calculator/FormulaTest/FormulaTest.swift
@@ -22,14 +22,36 @@ class FormulaTest: XCTestCase {
     }
 
     //MARK: - result()
-    func test_result호출시_2와3과plus가있을때_5를반환하는지() {
+    func test_result호출시_6나누기3빼기6곱하기4진행시_음수16을반환하는지() {
         //given
-        sut.operands.enqueue(2)
+        sut.operands.enqueue(6)
         sut.operands.enqueue(3)
-        sut.operations.enqueue(.add)
+        sut.operations.enqueue(.divide)
+        sut.operands.enqueue(6)
+        sut.operations.enqueue(.subtract)
+        sut.operands.enqueue(4)
+        sut.operations.enqueue(.multiply)
         //when
         let result = sut.result()
         //then
-        XCTAssertEqual(result, 5)
+        XCTAssertEqual(result, -16)
+    }
+    
+    func test_result호출시_6인큐후연산자가없을경우_6을반환하는지() {
+        //given
+        sut.operands.enqueue(6)
+        //when
+        let result = sut.result()
+        //then
+        XCTAssertEqual(result, 6)
+    }
+    
+    func test_result호출시_연산자만있을경우_0을반환하는지() {
+        //given
+        sut.operations.enqueue(.divide)
+        //when
+        let result = sut.result()
+        //then
+        XCTAssertEqual(result, 0)
     }
 }
