a7b1217d344cf77a1daf9e2fae6d4859e147e2bf
Donnie <seo_17@naver.com>
2022-03-21 21:52:37 +0900

    feat: parse() 메소드 구현

--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -9,14 +9,19 @@ import Foundation
 
 enum ExpressionParser {
     
-//    static func parse(from input: String) {
-//        let splitSentence = componentsByOperators(from: input)
-//            
-//        if let aaa = Double($0), let vvv = Operator(rawValue: Character($0)) {
-//        
-//        }
-//            Formula(operands: aaa, operators: vvv)
-//    }
+    static func parse(from input: String) -> Formula {
+        let operandQueue = CalculatorItemQueue<Double>()
+        let operatorQueue = CalculatorItemQueue<Operator>()
+        
+        componentsByOperators(from: input)
+            .compactMap { Double($0) }
+            .forEach { operandQueue.enqueue($0) }
+        componentsByOperators(from: input)
+            .compactMap { Operator(rawValue: Character($0)) }
+            .forEach{ operatorQueue.enqueue($0)}
+        
+        return Formula(operands: operandQueue, operators: operatorQueue)
+    }
     
     static func componentsByOperators(from input: String) -> [String] {
         return input.split(with: " ")
