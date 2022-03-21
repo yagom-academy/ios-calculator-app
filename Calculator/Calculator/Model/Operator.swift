483ef8dffedc058a8db7e1497714183a0ecbadd9
minseong <msb0987@naver.com>
2022-03-21 23:49:40 +0900

    feat: Operator calculate 기능구현

--- Calculator/Calculator/Model/Operator.swift
+++ Calculator/Calculator/Model/Operator.swift
@@ -6,7 +6,18 @@ enum Operator: Character, CaseIterable {
     case divide = "/"
     case multiply = "*"
     
-//    func calculate(lhs: Double, rhs: Double) -> Double {}
+    func calculate(lhs: Double, rhs: Double) -> Double {
+        switch self {
+        case .add:
+            return add(lhs: lhs, rhs: rhs)
+        case .subtract:
+            return substract(lhs: lhs, rhs: rhs)
+        case .divide:
+            return divide(lhs: lhs, rhs: rhs)
+        case .multiply:
+            return multiply(lhs: lhs, rhs: rhs)
+        }
+    }
     
     private func add(lhs: Double, rhs: Double) -> Double {
         return lhs + rhs
