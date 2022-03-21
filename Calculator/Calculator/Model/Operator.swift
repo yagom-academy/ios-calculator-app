8ba1bb04657a809c13c9b52d6806b6100fa2635d
Donnie <seo_17@naver.com>
2022-03-21 19:54:37 +0900

    chore: argument label "by"를 추가하여 연산의 순서를 명시

--- Calculator/Calculator/Model/Operator.swift
+++ Calculator/Calculator/Model/Operator.swift
@@ -16,32 +16,32 @@ enum Operator: Character, CaseIterable, CalculateItem {
     func calculate(lhs: Double, rhs: Double) throws -> Double {
         switch self {
         case .add:
-            return add(lhs, rhs)
+            return add(lhs, by: rhs)
         case .subtract:
-            return subtract(lhs, rhs)
+            return subtract(lhs, by: rhs)
         case .divide:
-            return try divide(lhs, rhs)
+            return try divide(lhs, by: rhs)
         case .multiply:
-            return multiply(lhs, rhs)
+            return multiply(lhs, by: rhs)
         }
     }
 
-    private func add(_ lhs: Double, _ rhs: Double) -> Double {
+    private func add(_ lhs: Double, by rhs: Double) -> Double {
         return lhs + rhs
     }
     
-    private func subtract(_ lhs: Double, _ rhs: Double) -> Double {
+    private func subtract(_ lhs: Double, by rhs: Double) -> Double {
         return lhs - rhs
     }
     
-    private func divide(_ lhs: Double, _ rhs: Double) throws -> Double {
+    private func divide(_ lhs: Double, by rhs: Double) throws -> Double {
         if rhs == 0 {
             throw DivideError.divideByZero
         }
         return lhs / rhs
     }
     
-    private func multiply(_ lhs: Double, _ rhs: Double) -> Double {
+    private func multiply(_ lhs: Double, by rhs: Double) -> Double {
         return lhs * rhs
     }
 }
