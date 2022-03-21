43b0ab229abf755e5c5479c57a120997f41ff23e
Grumpy <sewoong2890@gmail.com>
2022-03-22 01:49:13 +0900

    refactor: Unit Tests 사용하지 않는 함수 제거, do ... try catch문 try? 로 변경

--- Calculator/FormulaTests/FormulaTests.swift
+++ Calculator/FormulaTests/FormulaTests.swift
@@ -4,31 +4,20 @@ import XCTest
 class FormulaTests: XCTestCase {
     var sut: Formula? = nil
     
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-
     func test_add_12_and_23_equals_25() {
         //given
         sut = Formula()
         sut?.operands.enqueue(element: 12.0)
         sut?.operands.enqueue(element: 23.0)
         sut?.operators.enqueue(element: "+")
-
+        
         //when
-        do {
-            guard let result: Double = try sut?.result() else { return }
-            
-            //then
-            XCTAssertEqual(result, 35.0)
-        } catch { }
+        guard let result: Double = try? sut?.result() else { return }
+        
+        //then
+        XCTAssertEqual(result, 35.0)
     }
-
+    
     func test_add_minus_10_and_15_equals_5() {
         //given
         sut = Formula()
@@ -37,30 +26,26 @@ class FormulaTests: XCTestCase {
         sut?.operators.enqueue(element: "+")
         
         //when
-        do {
-            guard let result: Double = try sut?.result() else { return }
-            
-            //then
-            XCTAssertEqual(result, 5.0)
-        } catch { }
+        guard let result: Double = try? sut?.result() else { return }
+        
+        //then
+        XCTAssertEqual(result, 5.0)
     }
-
+    
     func test_divide_81_by_minus_9_equals_minus_9() {
         //given
         sut = Formula()
         sut?.operands.enqueue(element: 81.0)
         sut?.operands.enqueue(element: -9.0)
         sut?.operators.enqueue(element: "/")
-
+        
         //when
-        do {
-            guard let result: Double = try sut?.result() else { return }
-            
-            //then
-            XCTAssertEqual(result, -9.0)
-        } catch { }
+        guard let result: Double = try? sut?.result() else { return }
+        
+        //then
+        XCTAssertEqual(result, -9.0)
     }
-
+    
     func test_multiply_4_by_6_equals_24() {
         //given
         sut = Formula()
@@ -69,14 +54,12 @@ class FormulaTests: XCTestCase {
         sut?.operators.enqueue(element: "*")
         
         //when
-        do {
-            guard let result: Double = try sut?.result() else { return }
-            
-            //then
-            XCTAssertEqual(result, 24.0)
-        } catch { }
+        guard let result: Double = try? sut?.result() else { return }
+        
+        //then
+        XCTAssertEqual(result, 24.0)
     }
-
+    
     func test_multiply_plus_divide_formula() {
         //given
         sut = Formula()
@@ -87,21 +70,27 @@ class FormulaTests: XCTestCase {
         sut?.operators.enqueue(element: "*")
         sut?.operators.enqueue(element: "+")
         sut?.operators.enqueue(element: "/")
-
+        
         //when
-        do {
-            guard let result: Double = try sut?.result() else { return }
-            
-            //then
-            XCTAssertEqual(result, 6.8)
-        } catch { }
+        guard let result: Double = try? sut?.result() else { return }
+        
+        //then
+        XCTAssertEqual(result, 6.8)
+        
     }
     
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
+    func test_divide_any_number_by_0_equals_nan() {
+        //given
+        sut = Formula()
+        sut?.operands.enqueue(element: 14.0)
+        sut?.operands.enqueue(element: 0.0)
+        sut?.operators.enqueue(element: "/")
+        
+        //when
+        guard let result: Double = try? sut?.result() else { return }
+        
+        //then
+        XCTAssertEqual(result.isNaN, true)
     }
-
+    
 }
