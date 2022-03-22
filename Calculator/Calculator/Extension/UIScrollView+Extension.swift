a44bd3393314244f0ea1e933d1399f0b57fd5d55
dudu <whqtkf12@naver.com>
2022-03-22 16:51:50 +0900

    feat: ScrollView의 srcollToBottom을 위한 extension 추가

---
+++ Calculator/Calculator/Extension/UIScrollView+Extension.swift
@@ -0,0 +1,29 @@
+//
+//  UIScrollView+Extension.swift
+//  Calculator
+//
+//  Created by DuDu on 2022/03/22.
+//
+
+import UIKit
+
+extension UIScrollView {
+    func scroll() {
+        DispatchQueue.main.async {[weak self] in
+            guard let self = self else {
+                return
+            }
+            
+            let bottomOffset = CGPoint(x: 0, y: self.contentSize.height - self.bounds.size.height + self.contentInset.bottom)
+            print(self.contentSize.height)
+            print(self.bounds.size.height)
+            print(self.contentInset.bottom)
+            
+            print(bottomOffset)
+            
+            if bottomOffset.y > 0 {
+                self.setContentOffset(bottomOffset, animated: true)
+            }
+        }
+    }
+}
