993bba1e85e927a5e1fd08c54caab4d831c6b2b0
Marisol Mirim Kim <marisol.develop@gmail.com>
2022-03-22 10:51:02 +0900

    [feat]: connect IBOutlet, IBOutletCollection to storyboard
    - 스토리보드에 아웃렛, 아웃렛 콜렉션 요소 연결

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -8,6 +8,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var currentNumberLabel: UILabel!
+    @IBOutlet weak var currentOperatorLabel: UILabel!
+    
+    @IBOutlet var numberButtons: [UIButton]!
+    @IBOutlet var operatorButtons: [UIButton]!
+    @IBOutlet weak var resultButton: UIButton!
+    @IBOutlet weak var acButton: UIButton!
+    @IBOutlet weak var ceButton: UIButton!
+    @IBOutlet weak var convertingSignButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
