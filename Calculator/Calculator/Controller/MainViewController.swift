1b460f3be5c023c17e7cf6477576252143f36c71
JoSH0318 <jsh9870@naver.com>
2022-03-22 20:24:52 +0900

    ♻️[refactor]: self.selectedOperatorLabel.text 초기값을 nil로 할당

--- Calculator/Calculator/Controller/MainViewController.swift
+++ Calculator/Calculator/Controller/MainViewController.swift
@@ -34,7 +34,7 @@ class MainViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.selectedOperatorLabel.text = ""
+        self.selectedOperatorLabel.text = nil
         self.selectedOperandLabel.text = "0"
     }
     
