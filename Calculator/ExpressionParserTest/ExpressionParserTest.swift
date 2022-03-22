d8903bcf366712ad53da6b3ad520331399173bfb
doogie97 <chris_97@naver.com>
2022-03-22 20:36:45 +0900

    refactor: testcase추가

--- Calculator/ExpressionParserTest/ExpressionParserTest.swift
+++ Calculator/ExpressionParserTest/ExpressionParserTest.swift
@@ -14,9 +14,18 @@ class ExpressionParserTest: XCTestCase {
     func test_parse호출시_from에계산식을넣으면_옳바른결과를바환하는지() {
         //given
         let input = " 3 + 4 × -2 ÷ 7"
+        let input2 = "3 + 4 × -2 ÷ 7"
+        let input3 = "3 + 4 × -2 ÷ 7 "
+        let input4 = " 3 + 4 × -2 ÷ 7 "
         //when
         var result = ExpressionParser.parse(from: input)
+        var result2 = ExpressionParser.parse(from: input2)
+        var result3 = ExpressionParser.parse(from: input3)
+        var result4 = ExpressionParser.parse(from: input4)
         //then
         XCTAssertEqual(result.result(), -2)
+        XCTAssertEqual(result2.result(), -2)
+        XCTAssertEqual(result3.result(), -2)
+        XCTAssertEqual(result4.result(), -2)
     }
 }
