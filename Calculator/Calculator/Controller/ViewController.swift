c88b08793a0339fa79f8a1dcf151672966651f49
dudu <whqtkf12@naver.com>
2022-03-22 16:20:03 +0900

    feat: 패드의 숫자, 점, 부호 버튼의 입력 구현

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -6,10 +6,12 @@
 import UIKit
 
 final class ViewController: UIViewController {
+    private var currentNumber: String = ""
+    
     @IBOutlet private var calculateLogStackView: UIStackView!
     
-    @IBOutlet private var currentOperator: UILabel!
-    @IBOutlet private var currentOperand: UILabel!
+    @IBOutlet private var numberLabel: UILabel!
+    @IBOutlet private var operatorLabel: UILabel!
     
     @IBOutlet private var oneZeroButton: UIButton!
     @IBOutlet private var twoZeroButton: UIButton!
@@ -30,23 +32,60 @@ final class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        numberLabel.text = "0"
     }
     
     @IBAction private func numberButtonDidTapped(_ sender: UIButton) {
+        let inputNumber = findNumber(of: sender)
         
+        if ["00","0"].contains(inputNumber) && numberLabel.text == "0" {
+            return
+        }
+        
+        currentNumber += inputNumber
+        numberLabel.text = currentNumber
     }
     
     @IBAction private func operatorButtonDidTapped(_ sender: UIButton) {
     }
     
     @IBAction private func dotButtonDidTapped(_ sender: Any) {
+        if currentNumber.contains(".") {
+            return
+        }
+        
+        if currentNumber.isEmpty {
+            currentNumber = "0"
+        }
+        
+        currentNumber += "."
+        numberLabel.text = currentNumber
     }
+    
     @IBAction private func acButtonDidTapped(_ sender: Any) {
     }
+    
     @IBAction private func ceButtonDidTapped(_ sender: Any) {
     }
+    
     @IBAction private func signButtonDidTapped(_ sender: Any) {
+        if numberLabel.text == "0" {
+            return
+        }
+        
+        guard let number = Double(currentNumber) else {
+            return
+        }
+        
+        if number < 0 {
+            currentNumber.removeFirst()
+        } else {
+            currentNumber.insert("-", at: currentNumber.startIndex)
+        }
+        
+        numberLabel.text = currentNumber
     }
+    
     @IBAction private func calculateButtonDidTapped(_ sender: Any) {
     }
     
