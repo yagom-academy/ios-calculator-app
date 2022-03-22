40d8cf9547843933fbf364b4203c1ff46b9d3c5d
GoMyungju171 <mooooongdu171@gmail.com>
2022-03-22 19:14:23 +0900

    refactor: count 변수 제거

--- Calculator/Calculator/Model/CalculatorItemQueue.swift
+++ Calculator/Calculator/Model/CalculatorItemQueue.swift
@@ -5,22 +5,18 @@ protocol CalculateItem {
 
 struct CalculatorItemQueue<T: CalculateItem> {
     private let linkedList = LinkedList<T>()
-    var count: Int
-    
+
     func isEmpty() -> Bool {
         return linkedList.isEmpty
     }
     
     mutating func enqueue(_ data: T) {
         linkedList.addNode(data: data)
-        count += 1
     }
     
     mutating func dequeue() -> T? {
         if linkedList.head == nil || linkedList.head?.next == nil {
-            count = 0
         } else {
-            count += 1
         }
         return linkedList.removeFirstNode()
     }
