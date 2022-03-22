d22e576afb9755d02c85bba2bd6fb547d3214057
Marisol Mirim Kim <marisol.develop@gmail.com>
2022-03-22 13:30:31 +0900

    [add]: add Number.swift file and enum Number
    - Number.swift 파일과 열거형 Number 추가

---
+++ Calculator/Calculator/Model/Number.swift
@@ -0,0 +1,22 @@
+//
+//  Number.swift
+//  Calculator
+//
+//  Created by Marisol Mirim Kim on 2022/03/22.
+//
+
+import Foundation
+
+enum Number: String, CaseIterable {
+    case one = "1"
+    case two = "2"
+    case three = "3"
+    case four = "4"
+    case five = "5"
+    case six = "6"
+    case seven = "7"
+    case eight = "8"
+    case nine = "9"
+    case zero = "0"
+    case doubleZero = "00"
+}
