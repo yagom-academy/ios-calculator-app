84b87f73ce21867295972633660481c4d8755c5d
Grumpy <sewoong2890@gmail.com>
2022-03-22 14:14:07 +0900

    feat: OperandButton 클래스 생성
    커스텀 클래스 생성 및 버튼과 클릭 이벤트 연결

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -6,13 +6,54 @@
 
 import UIKit
 
+class OperandButton: UIButton {
+    var value: String?
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setUpOperandValue()
+    }
+    
+    @IBOutlet var OperandButtons: [OperandButton]!
+    
+    @IBOutlet weak var OperandZeroButton: OperandButton!
+    @IBOutlet weak var OperandCoupleZeroButton: OperandButton!
+    @IBOutlet weak var OperandOneButton: OperandButton!
+    @IBOutlet weak var OperandTwoButton: OperandButton!
+    @IBOutlet weak var OperandThreeButton: OperandButton!
+    @IBOutlet weak var OperandFourButton: OperandButton!
+    @IBOutlet weak var OperandFiveButton: OperandButton!
+    @IBOutlet weak var OperandSixButton: OperandButton!
+    @IBOutlet weak var OperandSevenButton: OperandButton!
+    @IBOutlet weak var OperandEightButton: OperandButton!
+    @IBOutlet weak var OperandNineButton: OperandButton!
+    @IBOutlet weak var OperandDotButton: OperandButton!
+    
+    func setUpOperandValue() {
+        OperandZeroButton.value = "0"
+        OperandCoupleZeroButton.value = "00"
+        OperandOneButton.value = "1"
+        OperandTwoButton.value = "2"
+        OperandThreeButton.value = "3"
+        OperandFourButton.value = "4"
+        OperandFiveButton.value = "5"
+        OperandSixButton.value = "6"
+        OperandSevenButton.value = "7"
+        OperandEightButton.value = "8"
+        OperandNineButton.value = "9"
+        OperandDotButton.value = "."
     }
 
-
+    @IBAction func OperandButtonAction(_ sender: OperandButton) {
+        guard let input = sender.value else { return }
+        print(input)
+    }
+    
+    
+    
 }
 
