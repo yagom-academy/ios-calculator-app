20bc834d8f7bb52e2a444160148c6abad89da46f
Lingo <llingo@kakao.com>
2022-03-22 23:17:50 +0900

    feat: CalculatorViewController IBOutlet과 IBAction 연결
    메인 스토리보드의 CalculatorViewController 의 UI요소를 IBOutlet, IBAction으로 연결

--- Calculator/Calculator/Presentation/CalculatorViewController.swift
+++ Calculator/Calculator/Presentation/CalculatorViewController.swift
@@ -9,7 +9,20 @@ import UIKit
 
 final class CalculatorViewController: UIViewController {
   
+  @IBOutlet private weak var resultScrollView: UIScrollView!
+  @IBOutlet private weak var resultStackView: UIStackView!
+  @IBOutlet private weak var operatorLabel: UILabel!
+  @IBOutlet private weak var operandLabel: UILabel!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
   }
+  
+  @IBAction private func didTapKeyOperationButton(_ sender: UIButton) {}
+  @IBAction private func didTapOperatorButton(_ sender: UIButton) {}
+  @IBAction private func didTapNumberButton(_ sender: UIButton) {}
 }
+
+// MARK: - Private Extension
+
+private extension CalculatorViewController {}
