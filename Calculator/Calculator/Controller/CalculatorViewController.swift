4cdbb33d450c355d8aec0d3d0f4de508e86b134f
Mino777 <jomino7777@gmail.com>
2022-03-22 20:31:51 +0900

    refactor: ViewController 네이밍 수정 및 내부 영역 분리

---
+++ Calculator/Calculator/Controller/CalculatorViewController.swift
@@ -0,0 +1,26 @@
+//
+//  Calculator - CalculatorViewController.swift
+//  Created by yagom. 
+//  Copyright © yagom. All rights reserved.
+// 
+
+import UIKit
+
+final class CalculatorViewController: UIViewController {
+    // MARK: - IBOutlet
+    // MARK: - Property
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+// MARK: - IBAction
+extension CalculatorViewController {
+    
+}
+
+// MARK: - Method
+extension CalculatorViewController {
+    
+}
