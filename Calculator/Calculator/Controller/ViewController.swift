e119d8746cb2f01060de6fc690aaae5fff0617fd
doogie97 <chris_97@naver.com>
2022-03-23 09:59:13 +0900

    feat: setBasicStatus 함수 구현 및 viewDidLoad, AC버튼에 배치

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -7,12 +7,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var numberListStackView: UIStackView!
+    @IBOutlet weak var operationLabel: UILabel!
+    @IBOutlet weak var operandLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setBasicStatus()
     }
-
-
+//MARK: - IBAction
+    @IBAction func touchACButton(_ sender: UIButton) {
+        setBasicStatus()
+    }
+    
+//MARK: - Functions
+    func setBasicStatus() {
+        self.numberListStackView.subviews.forEach({$0.removeFromSuperview()})
+        self.operandLabel.text = "0"
+        self.operationLabel.text = ""
+    }
 }
 
