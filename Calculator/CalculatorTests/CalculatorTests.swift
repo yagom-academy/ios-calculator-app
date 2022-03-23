1215a01d80b3e4db2b625a47e4dad7c00a0d6b88
Donnie <seo_17@naver.com>
2022-03-23 12:30:17 +0900

    rename: sut1 -> sut 이름 수정

--- Calculator/CalculatorTests/CalculatorTests.swift
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -9,83 +9,83 @@ import XCTest
 @testable import Calculator
 
 class CalculatorTests: XCTestCase {
-    private var sut1: CalculatorItemQueue<String>!
+    private var sut: CalculatorItemQueue<String>!
 
     override func setUpWithError() throws {
         try super.setUpWithError()
-        sut1 = CalculatorItemQueue<String>()
+        sut = CalculatorItemQueue<String>()
     }
 
     override func tearDownWithError() throws {
         try super.tearDownWithError()
-        sut1 = nil
+        sut = nil
     }
     
     func test_큐에값1개enqueue후count호출시출력_1() {
-        sut1.enqueue("+")
+        sut.enqueue("+")
         
-        let result = sut1.count
+        let result = sut.count
         
         XCTAssertEqual(result, 1)
     }
     
     func test_값1개enqueue후dequeue후count호출시출력_0() {
-        sut1.enqueue("+")
-        _ = sut1.dequeue()
+        sut.enqueue("+")
+        _ = sut.dequeue()
 
-        let result = sut1.count
+        let result = sut.count
         
         XCTAssertEqual(result, 0)
     }
     
     func test_값1개enqueue후dequeue후isEmpty출력_true() {
-        sut1.enqueue("+")
-        _ = sut1.dequeue()
+        sut.enqueue("+")
+        _ = sut.dequeue()
         
-        let result = sut1.isEmpty
+        let result = sut.isEmpty
         
         XCTAssertTrue(result)
     }
     
     func test_값5개enqueue후removeAll호출시count출력_0_isEmpty호출시출력_true() {
-        sut1.enqueue("+")
-        sut1.enqueue("-")
-        sut1.removeAll()
+        sut.enqueue("+")
+        sut.enqueue("-")
+        sut.removeAll()
         
-        let result = sut1.count
-        let isEmpty = sut1.isEmpty
+        let result = sut.count
+        let isEmpty = sut.isEmpty
         
         XCTAssertEqual(result, 0)
         XCTAssertTrue(isEmpty)
     }
     
     func test_값5개enqueue후count호출시출력_5() {
-        sut1.enqueue("+")
-        sut1.enqueue("-")
-        sut1.enqueue("-")
-        sut1.enqueue("+")
-        sut1.enqueue("-")
+        sut.enqueue("+")
+        sut.enqueue("-")
+        sut.enqueue("-")
+        sut.enqueue("+")
+        sut.enqueue("-")
         
-        let result = sut1.count
+        let result = sut.count
         
         XCTAssertEqual(result, 5)
     }
     
     func test_값5개enqueue후dequeue호출시count출력_4() {
-        sut1.enqueue("+")
-        sut1.enqueue("-")
-        sut1.enqueue("+")
-        sut1.enqueue("-")
-        sut1.enqueue("+")
-        _ = sut1.dequeue()
+        sut.enqueue("+")
+        sut.enqueue("-")
+        sut.enqueue("+")
+        sut.enqueue("-")
+        sut.enqueue("+")
+        _ = sut.dequeue()
         
-        let result = sut1.count
+        let result = sut.count
         
         XCTAssertEqual(result, 4)
     }
     
     func test_값이없는데dequeue호출시출력_nil() {
-        let result = sut1.dequeue()
+        let result = sut.dequeue()
         
         XCTAssertNil(result)
     }
