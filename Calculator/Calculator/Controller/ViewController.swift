2c226e7cf98cfa272197e748caa1b01f13e28581
doogie97 <chris_97@naver.com>
2022-03-23 11:01:30 +0900

    feat: 숫자 버튼 터치시 숫자 입력 및 입력된 숫자 format변경 함수 구현

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -15,19 +15,34 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         setBasicStatus()
     }
-//MARK: - IBAction
+    //MARK: - IBAction
     @IBAction func touchACButton(_ sender: UIButton) {
         setBasicStatus()
     }
     
-//MARK: - Functions
+    @IBAction func touchCEButton(_ sender: UIButton) {
+        self.operandLabel.text = "0"
+    }
+    
+    @IBAction func touchNumberButton(_ sender: UIButton) {
+        guard let labelOperand = operandLabel.text else { return }
+        guard let inputNumber = sender.titleLabel?.text else { return }
+        operandLabel.text = changeNumberFormat(number: labelOperand + inputNumber)
+    }
+    //MARK: - Functions
     func setBasicStatus() {
         self.numberListStackView.subviews.forEach({$0.removeFromSuperview()})
         self.operandLabel.text = "0"
         self.operationLabel.text = ""
     }
-    @IBAction func touchCEButton(_ sender: UIButton) {
-        self.operandLabel.text = "0"
+    
+    func changeNumberFormat(number: String) -> String {
+        guard let number = Double(number.replacingOccurrences(of: ",", with: "")) else { return "" }
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        numberFormatter.maximumSignificantDigits = -2
+        guard let changedNumber = numberFormatter.string(from: NSNumber(value: number)) else { return "" }
+        return changedNumber
     }
 }
 
