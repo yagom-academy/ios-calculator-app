e706a02a28e787ef90f26ab2108bad53ad4ed811
malrang <ruehd7924@gmail.com>
2022-03-23 16:32:54 +0900

    feat: didTapOperand 입력받은 숫자를 레이블에 표기해주는 메서드 구현

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -7,12 +7,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var presentValue: UILabel!
+    var presentValueLabelText: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func didTapOperand(sender: UIButton) {
+        guard let buttonLabel = sender.titleLabel?.text else { return }
+        
+        presentValueLabelText.append(buttonLabel)
+        presentValue.text = buttonLabel
+    }
 }
-
