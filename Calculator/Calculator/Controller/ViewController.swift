6d8c33e5c931ae1a3d5e74e131a830f7af0db833
doogie97 <chris_97@naver.com>
2022-03-23 13:04:15 +0900

    feat: 연산자 버튼 기능 구현 및 이에 따른 코드 리팩토링
    var isFirst : Bool = true 수정 필요: 현재 연산자 누르고 숫자 입력후 CE버튼 누른 후 연산자 입력시 연산자 안바뀜

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -10,6 +10,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var numberListStackView: UIStackView!
     @IBOutlet weak var operationLabel: UILabel!
     @IBOutlet weak var operandLabel: UILabel!
+    var isRealZero: Bool = false
+    var isFirst : Bool = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,37 +24,60 @@ class ViewController: UIViewController {
     
     @IBAction func touchCEButton(_ sender: UIButton) {
         self.operandLabel.text = "0"
+        self.isRealZero = false
+        self.isFirst = true
     }
     
     @IBAction func touchChangeSignButton(_ sender: UIButton) {
-        guard let labelOperand = self.operandLabel.text else { return }
-        if labelOperand == "0" { return }
-        if labelOperand.contains("-") {
-            self.operandLabel.text = labelOperand.replacingOccurrences(of: "-", with: "")
+        guard let operandText = self.operandLabel.text else { return }
+        if operandText == "0" { return }
+        if operandText.contains("-") {
+            self.operandLabel.text = operandText.replacingOccurrences(of: "-", with: "")
         } else {
-            self.operandLabel.text = "-" + labelOperand
+            self.operandLabel.text = "-" + operandText
         }
     }
     
     @IBAction func touchNumberButton(_ sender: UIButton) {
-        guard let labelOperand = self.operandLabel.text else { return }
+        guard let operandText = self.operandLabel.text else { return }
         guard let inputNumber = sender.titleLabel?.text else { return }
-        self.operandLabel.text = self.changeNumberFormat(number: labelOperand + inputNumber)
+        self.operandLabel.text = self.showZeroAfterDot(number: operandText + inputNumber)
+        self.isRealZero = true
+        self.isFirst = false
     }
     @IBAction func touchDotButton(_ sender: UIButton) {
-        guard let labelOperand = self.operandLabel.text else { return }
-        if labelOperand.contains(".") { return }
-        self.operandLabel.text = labelOperand + "."
+        guard let operandText = self.operandLabel.text else { return }
+        if operandText.contains(".") { return }
+        self.operandLabel.text = operandText + "."
+    }
+    
+    @IBAction func touchOperationButton(_ sender: UIButton) {
+        guard let inputOperation = sender.titleLabel?.text else { return }
+        guard let operandText = self.operandLabel.text else { return }
+        guard let operationText = self.operationLabel.text else { return }
+        if isFirst == true { return }
+        self.operationLabel.text = inputOperation
+        guard isRealZero == true else { return }
+        let numberStackView = setStackView(operationText: operationText , operandText: changeNumberFormat(number: operandText))
+        self.numberListStackView.addArrangedSubview(numberStackView)
+        self.operandLabel.text = "0"
+        self.isRealZero = false
     }
     //MARK: - Functions
     func setBasicStatus() {
         self.numberListStackView.subviews.forEach({$0.removeFromSuperview()})
         self.operandLabel.text = "0"
         self.operationLabel.text = ""
+        self.isRealZero = false
+        self.isFirst = true
+    }
+    
+    func showZeroAfterDot(number: String) -> String {
+        if number.contains(".") && number.last == "0" { return number }
+        return changeNumberFormat(number: number)
     }
     
     func changeNumberFormat(number: String) -> String {
-        if number.contains(".") && number.last == "0" { return number}
         guard let number = Double(number.replacingOccurrences(of: ",", with: "")) else { return "" }
         let numberFormatter = NumberFormatter()
         numberFormatter.numberStyle = .decimal
@@ -61,18 +86,21 @@ class ViewController: UIViewController {
         return changedNumber
     }
     
-    func setStackView() -> UIStackView {
+    func setStackView(operationText: String, operandText: String) -> UIStackView {
         let stackView = UIStackView()
         stackView.axis = .horizontal
         stackView.alignment = .fill
         stackView.distribution = .fill
         stackView.spacing = 8
+        stackView.addArrangedSubview(setLabel(element: operationText))
+        stackView.addArrangedSubview(setLabel(element: operandText))
         return stackView
     }
     
     func setLabel(element: String) -> UILabel {
         let label = UILabel()
         label.text = element
+        label.textColor = .white
         return label
     }
 }
