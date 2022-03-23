b54399307762c4736794d672b92c6c2c4e15f059
Grumpy <sewoong2890@gmail.com>
2022-03-23 11:08:48 +0900

    feat: 계산내역 스택 뷰 추가, 스크롤 뷰로 스크롤 기능 추가

--- Calculator/Calculator/CalculatorViewController.swift
+++ Calculator/Calculator/CalculatorViewController.swift
@@ -6,16 +6,16 @@
 
 import UIKit
 
-
 class CalculatorViewController: UIViewController {
     static let zero: String = "0"
     static let empty: String = ""
     static let negativeSign: Character = "-"
+    static let nanResult: String = "NaN"
     
     private var formulaNotYetCalculated: String = empty
     private var calculator: Formula = Formula()
-    private var statusZeroFlag: Bool = true
-    
+    private var statusZero: Bool = true
+    private var formerOperator: String = empty
     private var inputtingOperand: String = zero {
         didSet {
             NumberLabel.text = inputtingOperand
@@ -63,11 +63,15 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var NumberLabel: UILabel!
     
     
+    @IBOutlet weak var HistoryScrollView: UIScrollView!
+    @IBOutlet weak var HistoryStackView: UIStackView!
+    
     public func setUpDefaultStatus() {
         clearFormula()
         setStatusZero()
         clearInputtingOperand()
         clearInputtingOperator()
+        formerOperator = ""
     }
     
     public func clearFormula() {
@@ -76,7 +80,7 @@ class CalculatorViewController: UIViewController {
     
     public func setStatusZero() {
         clearInputtingOperand()
-        statusZeroFlag = true
+        statusZero = true
     }
     public func clearInputtingOperand() {
         inputtingOperand = CalculatorViewController.zero
@@ -91,6 +95,7 @@ class CalculatorViewController: UIViewController {
         setUpOpertorButtonValue()
         setUpNumberFormat()
         setUpDefaultStatus()
+        setUpHistoryStackView()
     }
     
     public func setUpNumberFormat() {
@@ -121,6 +126,19 @@ class CalculatorViewController: UIViewController {
         OperatorDivideButton.value = " ÷ "
     }
     
+    public func setUpHistoryStackView() {
+        HistoryStackView.spacing = 8
+        HistoryStackView.distribution = .fillEqually
+        clearHistoryStackView()
+    }
+    
+    func clearHistoryStackView() {
+        for historyView in HistoryStackView.arrangedSubviews {
+            historyView.removeFromSuperview()
+            HistoryStackView.removeArrangedSubview(historyView)
+        }
+    }
+    
     @IBAction func OperandButtonAction(_ sender: OperandButton) {
         generateOperandNumber(sender)
         addOperatorToFormulaIfExists()
@@ -128,7 +146,7 @@ class CalculatorViewController: UIViewController {
     
     public func generateOperandNumber(_ sender: OperandButton) {
         guard let input = sender.value else { return }
-        if statusZeroFlag == true {
+        if statusZero == true {
             if sender == OperandZeroButton || sender == OperandCoupleZeroButton {
                 inputtingOperand = CalculatorViewController.zero
             } else if sender == OperandDotButton {
@@ -136,7 +154,7 @@ class CalculatorViewController: UIViewController {
             } else {
                 inputtingOperand = input
             }
-            statusZeroFlag = false
+            statusZero = false
         } else {
             inputtingOperand += input
         }
@@ -148,26 +166,35 @@ class CalculatorViewController: UIViewController {
             formulaNotYetCalculated += inputtingOperator
             clearInputtingOperator()
             OperatorLabel.text = lastInputtedOperator
+            formerOperator = lastInputtedOperator ?? ""
         }
+        
     }
     
     @IBAction func OperatorButtonAction(_ sender: OperatorButton) {
         guard let input = sender.value else { return }
         inputtingOperator = input
+        if statusZero {
+            return
+        }
+        
+        if HistoryStackView.arrangedSubviews.count < 1 {
+            insertHistoryInStackView(inputtingOperand)
+        } else {
+            insertHistoryInStackView(formerOperator + inputtingOperand)
+        }
         addOperandToFormula()
     }
     
     public func addOperandToFormula() {
-        if statusZeroFlag {
-            return
-        }
-        
         formulaNotYetCalculated += inputtingOperand
         setStatusZero()
+        formerOperator = inputtingOperand
     }
     
     @IBAction func allClearAction(_ sender: FunctionalButton) {
         setUpDefaultStatus()
+        clearHistoryStackView()
     }
     
     
@@ -189,7 +216,7 @@ class CalculatorViewController: UIViewController {
         if formulaNotYetCalculated.isEmpty {
             return
         }
-        
+        insertHistoryInStackView(formerOperator + inputtingOperand)
         formulaNotYetCalculated += inputtingOperand
         clearInputtingOperand()
         var parser = ExpressionParser.parse(from: formulaNotYetCalculated)
@@ -201,11 +228,36 @@ class CalculatorViewController: UIViewController {
     
     public func configureCalculateResultLabel(_ result: Double) {
         if result.isNaN {
-            NumberLabel.text = "NaN"
+            NumberLabel.text = CalculatorViewController.nanResult
         } else {
             guard let numberFormattedResult = numberFormatter.string(for: result) else { return }
             NumberLabel.text = numberFormattedResult
         }
+    }
+    
+    func insertHistoryInStackView(_ inputted: String) {
+        let stackView = historyStackView(inputted)
+        let offsetY = HistoryScrollView.contentSize.height - HistoryScrollView.bounds.height
+        HistoryStackView.addArrangedSubview(stackView)
+        
+        if(offsetY > 0) {
+            HistoryScrollView.setContentOffset(CGPoint(x: 0, y: (HistoryScrollView.contentSize.height - HistoryScrollView.bounds.height + 30)), animated: true)
+        }
+    }
+    
+    func historyStackView(_ inputted: String) -> UIStackView {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let formulaHistory = UILabel()
+        formulaHistory.text = inputted
+        formulaHistory.textColor = .white
+        formulaHistory.textAlignment = .right
+        
+        stackView.addArrangedSubview(formulaHistory)
+        
+        return stackView
     }
 }
 
