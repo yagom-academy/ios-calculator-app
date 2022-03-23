fb37c3088cc5864baf22743ba0a7b86c252f9d43
Donnie <seo_17@naver.com>
2022-03-23 22:15:30 +0900

    chore: Extension 파일 분리

--- Calculator/Calculator/Model/Extension/String+Extension.swift
+++ Calculator/Calculator/Model/Extension/String+Extension.swift
@@ -1,18 +1,12 @@
 //
-//  BasicTypesExtension.swift
+//  String+Extension.swift
 //  Calculator
 //
 //  Created by SeoDongyeon on 2022/03/17.
 //
 
-import Foundation
-
 extension String: CalculateItem {
     func split(with target: Character) -> [String] {
         return components(separatedBy: String(target))
     }
-}
-
-extension Double: CalculateItem {
-    // Empty
 }
