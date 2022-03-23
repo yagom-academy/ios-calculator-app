7258b443f1af64c244731fbff449faa907331dd5
minseong <msb0987@naver.com>
2022-03-23 16:24:40 +0900

    feat: Formula result() 기능 구현
    * operands큐와 operators큐에서 하나씩 pop해서 연산 후 결과값을 리턴

--- Calculator/Calculator/Model/Formula.swift
+++ Calculator/Calculator/Model/Formula.swift
@@ -4,7 +4,21 @@ struct Formula {
     var operands = CalculatorItemQueue<Double>()
     var operators =  CalculatorItemQueue<Operator>()
     
-//    func result() -> Double {
-//        return 0
-//    }
+    mutating func result() -> Double {
+        guard var sumTotal = operands.dequeue() else {
+            return 0
+        }
+        
+        while !(operands.isEmpty && operators.isEmpty) {
+            guard let operand = operands.dequeue(), let `operator` = operators.dequeue() else {
+                return 0
+            }
+            guard !(sumTotal.isNaN) else {
+                break
+            }
+            sumTotal = `operator`.calculate(lhs: sumTotal, rhs: operand)
+        }
+        
+        return sumTotal
+    }
 }
