39991d7e06acedd11ac8dac422d8344b34640f6e
JoSH0318 <jsh9870@naver.com>
2022-03-23 23:31:04 +0900

    ⚰️[del]: 필요없는 IBOutlet 제거

--- Calculator/Calculator/Controller/MainViewController.swift
+++ Calculator/Calculator/Controller/MainViewController.swift
@@ -16,24 +16,6 @@ class MainViewController: UIViewController {
     private var isZeroNone: Bool = true
     private var isFirstTime: Bool = true
     
-    @IBOutlet weak var oneButton: UIButton!
-    @IBOutlet weak var twoButton: UIButton!
-    @IBOutlet weak var threeButton: UIButton!
-    @IBOutlet weak var fourButton: UIButton!
-    @IBOutlet weak var fiveButton: UIButton!
-    @IBOutlet weak var sixButton: UIButton!
-    @IBOutlet weak var sevenButton: UIButton!
-    @IBOutlet weak var eightButton: UIButton!
-    @IBOutlet weak var nineButton: UIButton!
-    @IBOutlet weak var zeroButton: UIButton!
-    @IBOutlet weak var doubleZeroButton: UIButton!
-    @IBOutlet weak var dotButton: UIButton!
-    
-    @IBOutlet weak var addButton: UIButton!
-    @IBOutlet weak var subtractButton: UIButton!
-    @IBOutlet weak var divideButton: UIButton!
-    @IBOutlet weak var multiplyButton: UIButton!
-    
     @IBOutlet weak var operatorLabel: UILabel!
     @IBOutlet weak var operandLabel: UILabel!
     
