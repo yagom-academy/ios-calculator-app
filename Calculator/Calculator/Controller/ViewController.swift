4fb953d66f7ae132e9b3fe36e579a4c379d5218e
doogie97 <chris_97@naver.com>
2022-03-23 11:10:01 +0900

    feat: . 버튼 기능 구현, refactor: 소수점 뒤에 0이 들어올 때 조건 추가

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -25,9 +25,14 @@ class ViewController: UIViewController {
     }
     
     @IBAction func touchNumberButton(_ sender: UIButton) {
-        guard let labelOperand = operandLabel.text else { return }
+        guard let labelOperand = self.operandLabel.text else { return }
         guard let inputNumber = sender.titleLabel?.text else { return }
-        operandLabel.text = changeNumberFormat(number: labelOperand + inputNumber)
+        self.operandLabel.text = self.changeNumberFormat(number: labelOperand + inputNumber)
+    }
+    @IBAction func touchDotButton(_ sender: UIButton) {
+        guard let labelOperand = self.operandLabel.text else { return }
+        if labelOperand.contains(".") { return }
+        self.operandLabel.text = labelOperand + "."
     }
     //MARK: - Functions
     func setBasicStatus() {
@@ -37,6 +42,7 @@ class ViewController: UIViewController {
     }
     
     func changeNumberFormat(number: String) -> String {
+        if number.contains(".") && number.last == "0" { return number}
         guard let number = Double(number.replacingOccurrences(of: ",", with: "")) else { return "" }
         let numberFormatter = NumberFormatter()
         numberFormatter.numberStyle = .decimal
