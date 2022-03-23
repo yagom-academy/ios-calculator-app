4c0ae7ae014cfac0dce536894fdd36627ed6014c
Red <seryprk@gmail.com>
2022-03-23 21:26:14 +0900

    💎feat : clickCalculateButton 메서드 구현
    clickAC 메서드 구현, allOperation 변수 생성

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var currentOperatorLabel: UILabel!
     @IBOutlet weak var operationRecord: UIStackView!
     var touchedNumber: String = "0"
+    var allOperation: [String] = []
     
     @IBAction func clickNumberButton(_ sender: UIButton) {
         if currentNumberLabbel.text == "0" {
@@ -22,7 +23,10 @@ class ViewController: UIViewController {
     }
     
     @IBAction func clickOperatorButton(_ sender: UIButton) {
-        addNumberAndOperator(currentOperatorLabel.text ?? "", currentNumberLabbel.text ?? "")
+        if touchedNumber != "0" {
+            addNumberAndOperator(currentOperatorLabel.text ?? "", currentNumberLabbel.text ?? "")
+        }
+        //넘어갈 때 . 으로 끝나는 경우 .을 제거하고 화면에 띄울 것
         
         currentOperatorLabel.text = sender.currentTitle
     }
@@ -44,12 +48,31 @@ class ViewController: UIViewController {
     }
     
     @IBAction func clickPlusMimusSign(_ sender: UIButton) {
+        //입력된 숫자가 0인 경우 부호를 표시하지 않음!
         if touchedNumber.hasPrefix("-") == true {
             touchedNumber.remove(at: touchedNumber.startIndex)
         } else {
             touchedNumber.insert("-", at: touchedNumber.startIndex)
         }
         currentNumberLabbel.text = touchedNumber
+    }
+    
+    @IBAction func clickAC(_ sender: UIButton) {
+        operationRecord.subviews.forEach { $0.removeFromSuperview() }
+        touchedNumber = "0"
+        currentNumberLabbel.text = "0"
+    }
+
+    @IBAction func clickCalculateButton(_ sender: UIButton) {
+        addNumberAndOperator(currentOperatorLabel.text ?? "", currentNumberLabbel.text ?? "")
+        let mergedAllOperation = allOperation.joined(separator: " ")
+        var formula = ExpressionParser.parse(form: mergedAllOperation)
+        let result = formula.result()
+        
+        currentOperatorLabel.text = ""
+        currentNumberLabbel.text = "\(result)"
+        touchedNumber = "\(result)"
+        allOperation = []
     }
     
     func addNumberAndOperator(_ currentOperator: String, _ currentNumber: String) {
@@ -60,19 +83,24 @@ class ViewController: UIViewController {
         newStack.distribution = .fill
         newStack.spacing = 8
         
-        let numberLabel = UILabel()
-        numberLabel.textColor = .white
-        numberLabel.font = UIFont.preferredFont(forTextStyle: UIFont.TextStyle.title3)
-        
         let operatorLabel = UILabel()
         operatorLabel.textColor = .white
         operatorLabel.font = UIFont.preferredFont(forTextStyle: UIFont.TextStyle.title3)
+        
+        let numberLabel = UILabel()
+        numberLabel.textColor = .white
+        numberLabel.font = UIFont.preferredFont(forTextStyle: UIFont.TextStyle.title3)
         
         numberLabel.text = currentNumber
         operatorLabel.text = currentOperator
         newStack.addArrangedSubview(operatorLabel)
         newStack.addArrangedSubview(numberLabel)
         operationRecord.addArrangedSubview(newStack)
+        
+        if allOperation.isEmpty == false {
+            allOperation.append(currentOperator)
+        }
+        allOperation.append(currentNumber)
         
         touchedNumber = "0"
         currentNumberLabbel.text = "0"
