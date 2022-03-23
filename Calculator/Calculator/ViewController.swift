9770578dee559fd1fd24e36cf0723e3b1a44f01d
Red <seryprk@gmail.com>
2022-03-23 18:50:20 +0900

    💎feat : addNumberAndOperator 메서드 생성

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var currentNumberLabbel: UILabel!
     @IBOutlet weak var currentOperatorLabel: UILabel!
+    @IBOutlet weak var operationRecord: UIStackView!
     var touchedNumber: String = ""
     
     @IBAction func clickNumberButton(_ sender: UIButton) {
@@ -17,7 +18,36 @@ class ViewController: UIViewController {
     }
     
     @IBAction func clickOperatorButton(_ sender: UIButton) {
+        addNumberAndOperator(currentOperatorLabel.text ?? "", currentNumberLabbel.text ?? "")
+        
         currentOperatorLabel.text = sender.currentTitle
+        currentNumberLabbel.text = "0"
+    }
+    
+    func addNumberAndOperator(_ currentOperator: String, _ currentNumber: String) {
+        let newStack = UIStackView()
+        newStack.translatesAutoresizingMaskIntoConstraints = false
+        newStack.axis = .horizontal
+        newStack.alignment = .fill
+        newStack.distribution = .fill
+        newStack.spacing = 8
+        
+        let numberLabel = UILabel()
+        numberLabel.textColor = .white
+        numberLabel.font = UIFont.preferredFont(forTextStyle: UIFont.TextStyle.title3)
+        
+        let operatorLabel = UILabel()
+        operatorLabel.textColor = .white
+        operatorLabel.font = UIFont.preferredFont(forTextStyle: UIFont.TextStyle.title3)
+        
+        numberLabel.text = currentNumber
+        operatorLabel.text = currentOperator
+        newStack.addArrangedSubview(operatorLabel)
+        newStack.addArrangedSubview(numberLabel)
+        operationRecord.addArrangedSubview(newStack)
+        
+        touchedNumber = ""
+        currentNumberLabbel.text = ""
     }
     
     override func viewDidLoad() {
