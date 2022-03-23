252153e49cf22c7f65f8f6557ec092624b216b12
Red <seryprk@gmail.com>
2022-03-23 13:33:09 +0900

    💎feat : clickOperatorButton 메서드 구현

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -7,10 +7,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var currentNumberLabbel: UILabel!
+    @IBOutlet weak var currentOperatorLabel: UILabel!
     var touchedNumber: String = ""
     
     @IBAction func clickNumberButton(_ sender: UIButton) {
         touchedNumber += sender.currentTitle ?? ""
+        currentNumberLabbel.text = touchedNumber
+    }
+    
+    @IBAction func clickOperatorButton(_ sender: UIButton) {
+        currentOperatorLabel.text = sender.currentTitle
     }
     
     override func viewDidLoad() {
