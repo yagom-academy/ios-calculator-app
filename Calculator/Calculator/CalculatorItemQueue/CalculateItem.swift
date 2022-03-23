0ebd4047082e46a69e5ffee906ccc8737a11e309
Grumpy <sewoong2890@gmail.com>
2022-03-23 12:11:42 +0900

    fix: 잘못 제거한 Character Extension 다시 생성

--- Calculator/Calculator/CalculatorItemQueue/CalculateItem.swift
+++ Calculator/Calculator/CalculatorItemQueue/CalculateItem.swift
@@ -6,7 +6,9 @@ protocol CalculateItem {
 extension Double: CalculateItem {
     
 }
-
+extension Character: CalculateItem {
+    
+}
 extension String {
     public func split(with target: Character) -> [String] {
         return self.components(separatedBy: String(target))
