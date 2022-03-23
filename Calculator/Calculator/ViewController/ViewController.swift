c8b808102d52c38189331e2a4bb30ee17ffe02e5
Taeangel <wngks2050@gmail.com>
2022-03-23 19:22:16 +0900

    feat: operator,number,ACButton을 StackView에 연결

--- Calculator/Calculator/ViewController/ViewController.swift
+++ Calculator/Calculator/ViewController/ViewController.swift
@@ -7,12 +7,76 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+  
+  @IBOutlet weak var stackNumber: UILabel!
+  @IBOutlet weak var `operator`: UILabel!
+  
+  var numberString = ""{
+    didSet{
+      self.stackNumber.text = numberString
     }
-
-
+  }
+  
+  var operatorString = ""{
+    didSet{
+      self.`operator`.text = operatorString
+    }
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+  
+  @IBAction func tapNumber(_ sender: UIButton) {
+    guard let inputString = sender.titleLabel?.text else {
+      return
+    }
+    if numberString.first == "0" {
+      numberString.removeAll()
+      numberString.append(inputString)
+    } else {
+    numberString.append(inputString)
+    }
+    
+    if numberString == "00" {
+      numberString = "0"
+    }
+  }
+  
+  @IBAction func tapPlusMinus(_ sender: UIButton) {
+    if numberString.first == "-" {
+      numberString = numberString.trimmingCharacters(in: ["-"])
+    } else {
+      numberString = String(numberString.reversed())
+      numberString.append("-")
+      numberString = String(numberString.reversed())
+    }
+  }
+  
+  @IBAction func tapDot(_ sender: UIButton) {
+    if numberString.contains(".") == false {
+      numberString.append(".")
+    }
+  }
+  
+  @IBAction func tapOperator(_ sender: UIButton) {
+    guard let inputOperator = sender.titleLabel?.text else {
+      return
+    }
+    operatorString.removeAll()
+    operatorString.append(inputOperator)
+  }
+  
+  @IBAction func tapAC(_ sender: UIButton) {
+    operatorString.removeAll()
+    numberString.removeAll()
+    numberString.append("0")
+  }
+  
+  @IBAction func tapCE(_ sender: UIButton) {
+  }
+  
+  @IBAction func tapResult(_ sender: UIButton) {
+  }
 }
-
