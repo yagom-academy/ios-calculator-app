aa1f742ace9d764fd463ea1ad3ec7661c8b8d7e0
dnwhd0112 <dnwhd0112@naver.com>
2022-03-23 19:36:14 +0900

    feat: 버튼 클래스 추가
    NumberButton, OperandButton 클래스 추가

---
+++ Calculator/Modal/NumberButton.swift
@@ -0,0 +1,12 @@
+//
+//  NumberButton.swift
+//  Calculator
+//
+//  Created by 우롱차 on 2022/03/23.
+//
+
+import UIKit
+
+class NumberButton: UIButton {
+    var value: String?
+}
---
+++ Calculator/Modal/OperandButton.swift
@@ -0,0 +1,12 @@
+//
+//  OperandButton.swift
+//  Calculator
+//
+//  Created by 우롱차 on 2022/03/23.
+//
+
+import UIKit
+
+class OperandButton: UIButton {
+    var value: String?
+}
