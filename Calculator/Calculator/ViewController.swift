ef2e83605b5746ea6f253cda6ff86805f6ead7dd
Quokkaa <rktrkdml@gmail.com>
2022-03-24 21:00:13 +0900

    feat: IBOutlet생성
    * inputFormulaLabel
    * operandsBtns
    * operatorsBtns
    * allClearBtn
    * clearEntryBtn
    * plusAndMinusBtn
    * calculationBtn

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -4,8 +4,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var inputFormulaLabel: UILabel!
+    @IBOutlet var operandBtns: [UIButton]!
+    @IBOutlet var operatorBtns: [UIButton]!
+    
+    @IBOutlet weak var allClearBtn: UIButton!
+    @IBOutlet weak var clearEntryBtn: UIButton!
+    @IBOutlet weak var plusAndMinusBtn: UIButton!
+    @IBOutlet weak var calculationBtn: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
     }
+    
+    
 }
 
