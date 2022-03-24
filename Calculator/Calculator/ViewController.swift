0ed3f9b2cdb02842a2e0867204b56a17e54e3038
saafaaari <111dltldnjs@naver.com>
2022-03-24 21:19:51 +0900

    feat : 피연산자 라벨을 수정해주는 updateOperandLabel 함수 추가
    touchUpNumberButton 함수 내부 로직 updateOperandLabel로 대채
    touchUpDoubleZeroButton, touchUpDotButton 내부 updateOperandLabel 호출

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -15,25 +15,28 @@ class ViewController: UIViewController {
     @IBAction func touchUpNumberButton(_ sender: UIButton) {
         if operandLabel.text == "0" { operandLabel.text = "" }
         
-        guard let text = operandLabel.text, let inputText = sender.currentTitle else { return }
-        
-        operandLabel.text = text + inputText
+        updateOperandLabel(sender)
     }
     
     @IBAction func touchUpDoubleZeroButton(_ sender: UIButton) {
         if operandLabel.text == "0" && sender.currentTitle == "00" { return }
         
+        updateOperandLabel(sender)
     }
     
     @IBAction func touchUpDotButton(_ sender: UIButton) {
-        guard let operandText = operandLabel.text else { return }
+        if (operandLabel.text?.contains(".") == true) && sender.currentTitle == "." { return }
         
-        if operandText.contains(".") && sender.currentTitle == "." { return }
-        
+        updateOperandLabel(sender)
     }
     
     // MARK: - Method
     
+    func updateOperandLabel(_ button: UIButton) {
+        guard let text = operandLabel.text, let inputText = button.currentTitle else { return }
+        
+        operandLabel.text = text + inputText
+    }
     
     
     
