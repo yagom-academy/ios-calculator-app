8e658bd936c4e7c1c226b5f271d3b6b7faed93f7
saafaaari <111dltldnjs@naver.com>
2022-03-24 18:45:44 +0900

    refactor : inputOperand함수 네이밍 변겅
    inputOperand -> touchUpNumberButton 변경

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -10,9 +10,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var operandLabel: UILabel!
     @IBOutlet weak var operatorLabel: UILabel!
     
-    @IBAction func inputOperand(_ sender: UIButton) {
+    @IBAction func touchUpNumberButton(_ sender: UIButton) {
         if operandLabel.text == "0" && sender.currentTitle != "." { operandLabel.text = "" }
+        
         guard let text = operandLabel.text, let inputText = sender.currentTitle else { return }
+        
         operandLabel.text = text + inputText
     }
     override func viewDidLoad() {
