54848d91506454e73c0aa1f43b8d86ec1c4ae723
Donnie <seo_17@naver.com>
2022-03-24 18:19:50 +0900

    feat: Button IBOulet, Label IBOulet 연결

--- Calculator/Calculator/Controller/CalculatorViewController.swift
+++ Calculator/Calculator/Controller/CalculatorViewController.swift
@@ -8,6 +8,31 @@ import UIKit
 
 class CalculatorViewController: UIViewController {
 
+    @IBOutlet weak var singleZeroButton: UIButton!
+    @IBOutlet weak var doubleZeroButton: UIButton!
+    @IBOutlet weak var singleDotButton: UIButton!
+    @IBOutlet weak var numberOneButton: UIButton!
+    @IBOutlet weak var numberTwoButton: UIButton!
+    @IBOutlet weak var numberThreeButton: UIButton!
+    @IBOutlet weak var numberFourButton: UIButton!
+    @IBOutlet weak var numberFiveButton: UIButton!
+    @IBOutlet weak var numberSixButton: UIButton!
+    @IBOutlet weak var numberSevenButton: UIButton!
+    @IBOutlet weak var numberEightButton: UIButton!
+    @IBOutlet weak var numberNineButton: UIButton!
+    
+    @IBOutlet weak var equalSignButton: UIButton!
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var subtractButton: UIButton!
+    @IBOutlet weak var multiplyButton: UIButton!
+    @IBOutlet weak var divideButton: UIButton!
+    @IBOutlet weak var allClearButton: UIButton!
+    @IBOutlet weak var clearElementButton: UIButton!
+    @IBOutlet weak var positiveNegativeConversionButton: UIButton!
+    
+    @IBOutlet weak var operatorComeInLabel: UILabel!
+    @IBOutlet weak var operandComeInLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
