cfdafd183d182e23b8ec628ee0116c1d1fb6a875
saafaaari <111dltldnjs@naver.com>
2022-03-24 19:27:23 +0900

    feat : "00"버튼에 대한 액션과 "."버튼에 대한 액션 추가
    "00"과 "." 버튼을 touchUpNumberButton액션에서 각 액션으로 수정

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -10,13 +10,34 @@ class ViewController: UIViewController {
     @IBOutlet weak var operandLabel: UILabel!
     @IBOutlet weak var operatorLabel: UILabel!
     
+    // MARK: - IBAction
+    
     @IBAction func touchUpNumberButton(_ sender: UIButton) {
+
         if operandLabel.text == "0" && sender.currentTitle != "." { operandLabel.text = "" }
         
         guard let text = operandLabel.text, let inputText = sender.currentTitle else { return }
         
         operandLabel.text = text + inputText
     }
+    
+    @IBAction func touchUpDoubleZeroButton(_ sender: UIButton) {
+        if operandLabel.text == "0" && sender.currentTitle == "00" { return }
+        
+    }
+    
+    @IBAction func touchUpDotButton(_ sender: UIButton) {
+        guard let operandText = operandLabel.text else { return }
+        
+        if operandText.contains(".") && sender.currentTitle == "." { return }
+        
+    }
+    
+    // MARK: - Method
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         operandLabel.text = "0"
