8e8c1d53eaa57dd8f3a4909302e5750e3698c2b1
doogie97 <chris_97@naver.com>
2022-03-27 11:46:21 +0900

    refactor: 자주 사용되는 문자열 상수로 선언

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
     private var isNoneNumber: Bool = true
     private var isResult: Bool = false
     private var formulaToSend = ""
+    private let stringZero = "0"
+    private let minusSign = "-"
+    private let stringNaN = "NaN"
+    private let stringDot = "."
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,24 +29,24 @@ class ViewController: UIViewController {
     }
     
     @IBAction func touchCEButton(_ sender: UIButton) {
-        self.operandLabel.text = "0"
+        self.operandLabel.text = stringZero
         self.isNoneNumber = true
     }
     
     @IBAction func touchChangeSignButton(_ sender: UIButton) {
         guard let operandText = self.operandLabel.text else { return }
-        if operandText == "0" { return }
-        if operandText.contains("-") {
-            self.operandLabel.text = operandText.replacingOccurrences(of: "-", with: "")
+        if operandText == stringZero { return }
+        if operandText.contains(minusSign) {
+            self.operandLabel.text = operandText.replacingOccurrences(of: minusSign, with: "")
         } else {
-            self.operandLabel.text = "-" + operandText
+            self.operandLabel.text = minusSign + operandText
         }
     }
     
     @IBAction func touchNumberButton(_ sender: UIButton) {
         guard let operandText = self.operandLabel.text, operandText.count < 20 else { return }
         guard let inputNumber = sender.titleLabel?.text else { return }
-        if self.operandLabel.text == "NaN" {
+        if operandText == stringNaN {
             removeFormula()
         }
         if isResult == true {
@@ -57,15 +61,15 @@ class ViewController: UIViewController {
     
     @IBAction func touchDotButton(_ sender: UIButton) {
         guard let operandText = self.operandLabel.text else { return }
-        if operandText.contains(".") { return }
-        self.operandLabel.text = operandText + "."
+        if operandText.contains(stringDot) { return }
+        self.operandLabel.text = operandText + stringDot
     }
     
     @IBAction func touchOperationButton(_ sender: UIButton) {
         guard let inputOperation = sender.titleLabel?.text else { return }
         guard let operandText = self.operandLabel.text else { return }
         guard let operationText = self.operationLabel.text else { return }
-        if self.formulaListStackView.subviews.isEmpty && operandText == "0" || operandText == "NaN" { return }
+        if self.formulaListStackView.subviews.isEmpty && operandText == stringZero || operandText == stringNaN { return }
         if isResult == true {
             removeFormula()
             self.isResult = false
@@ -73,7 +77,7 @@ class ViewController: UIViewController {
         self.operationLabel.text = inputOperation
         if isNoneNumber == true { return }
         addFormula(operation: operationText, operand: operandText)
-        self.operandLabel.text = "0"
+        self.operandLabel.text = stringZero
         self.isNoneNumber = true
     }
     
@@ -93,14 +97,14 @@ class ViewController: UIViewController {
     //MARK: - Functions
     private func initializeCalculator() {
         removeFormula()
-        self.operandLabel.text = "0"
+        self.operandLabel.text = stringZero
         self.operationLabel.text = ""
         self.formulaToSend = ""
         self.isNoneNumber = true
     }
     
     private func showZeroAfterDot(number: String) -> String {
-        if number.contains(".") && number.last == "0" { return number }
+        if number.contains(stringDot) && number.last == Character(stringZero) { return number }
         return changeNumberFormat(number: number)
     }
     
@@ -114,7 +118,7 @@ class ViewController: UIViewController {
         numberFormatter.numberStyle = .decimal
         numberFormatter.maximumFractionDigits = -2
         guard let changedNumber = numberFormatter.string(from: number as NSNumber) else { return "" }
-        if changedNumber == "-0" { return "0"}
+        if changedNumber == "-0" { return stringZero}
         return changedNumber
     }
     
