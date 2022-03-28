034c2a29a3a69e666cd11aad6e355159a9eec8ee
malrang <ruehd7924@gmail.com>
2022-03-28 23:11:28 +0900

    ✨[feat]: enum ExpressionParser 내부에 parse() 메서드 구현 및 componentsByOperators() 메서드에 static 키워드 추가

--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -8,7 +8,24 @@
 import Foundation
 
 enum ExpressionParser {
-    private func componentsByOperators(input: String) -> [String] {
+    static func parse(from input: String) -> Formula {
+        let splitSentence = componentsByOperators(from: input)
+        let operandQueue = CalculatorQueue<Double>()
+        let operatorQueue = CalculatorQueue<Operator>()
+        
+        for element in splitSentence {
+            if let operandElement = Double(element) {
+                operandQueue.enqueue(operandElement)
+                continue
+            }
+            if let operatorElement = Operator(rawValue: Character(element)) {
+                operatorQueue.enqueue(operatorElement)
+            }
+        }
+        return Formula(operandQueue: operandQueue, opratorQueue: operatorQueue)
+    }
+    
+    private static func componentsByOperators(from input: String) -> [String] {
         return input.split(with: " ")
     }
 }
