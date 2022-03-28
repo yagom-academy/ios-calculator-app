3a81c04fd99bc308101662b6c67e342c118abf83
Eddy <kimkyunghun3@naver.com>
2022-03-28 21:02:50 +0900

    refactor: CalculateItem의 split 함수 고차함수로 개선
    Double extension, String extension 파일로 구분

---
+++ Calculator/Calculator/Utils/Extension/Double+extension.swift
@@ -0,0 +1,12 @@
+//
+//  Double+extension.swift
+//  Calculator
+//
+//  Created by Eddy on 2022/03/28.
+//
+
+import Foundation
+
+extension Double: CalculateItem {
+   // empty
+}
---
+++ Calculator/Calculator/Utils/Extension/String+extension.swift
@@ -0,0 +1,14 @@
+//
+//  String+extension.swift
+//  Calculator
+//
+//  Created by Eddy on 2022/03/28.
+//
+
+import Foundation
+
+extension String: CalculateItem {
+    func split(with target: Character) -> [String] {
+        return split(separator: target).map { String($0) }
+    }
+}
