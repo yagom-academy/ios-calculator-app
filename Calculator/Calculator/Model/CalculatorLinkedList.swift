00bb60f97bf093cded63f4d8394abe8b92240cee
malrang <ruehd7924@gmail.com>
2022-03-28 21:24:29 +0900

    ✨[feat]: CalculateItem protocol 구현 및 CalculatorQueue<T: CalculateItem> 구현

--- Calculator/Calculator/Model/CalculatorLinkedList.swift
+++ Calculator/Calculator/Model/CalculatorLinkedList.swift
@@ -45,4 +45,3 @@ final class CalculatorLinkedList<T> {
         tail = nil
     }
 }
-
---
+++ Calculator/Calculator/Model/CalculatorQueue.swift
@@ -0,0 +1,12 @@
+//
+//  CalculatorQueue.swift
+//  Calculator
+//
+//  Created by 김동욱 on 2022/03/28.
+//
+
+import Foundation
+
+struct CalculatorQueue<T: CalculateItem> {
+    
+}
