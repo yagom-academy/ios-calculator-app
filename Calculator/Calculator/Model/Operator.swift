3902d5fe9277a5c7269fedddcbbd9d9a65d18c5f
Donnie <seo_17@naver.com>
2022-03-28 22:05:02 +0900

    ✨[feat]: enum Operator 타입 정의

---
+++ Calculator/Calculator/Model/Operator.swift
@@ -0,0 +1,15 @@
+//
+//  Operator.swift
+//  Calculator
+//
+//  Created by SeoDongyeon on 2022/03/28.
+//
+
+import Foundation
+
+enum Operator: Character, CaseIterable, CalculateItem {
+    case add = "+"
+    case subtract = "-"
+    case divide = "÷"
+    case multiply = "×"
+}
