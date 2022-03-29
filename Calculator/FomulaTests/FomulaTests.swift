c6dce5a940ae240f19165f0774dedd00b193bf09
xogns8696@gmail.com <onetool>
2022-03-29 16:05:26 +0900

    ✨[FEAT] : FomulaTests에 test case 추가

--- Calculator/FomulaTests/FomulaTests.swift
+++ Calculator/FomulaTests/FomulaTests.swift
@@ -75,4 +75,32 @@ class FomulaTests: XCTestCase {
             XCTAssertEqual(error, CalculatorError.divisionByZero)
         }
     }
+    
+    func test_result_호출시_앞에_공백이_있는_계산식을_넣으면_올바른_결과를_반환하는지() {
+        // given
+        let fomula = "              1  + 3"
+        // when
+        var result = ExpressionParser.parse(frome: fomula)
+        // then
+        switch result.result() {
+        case .success(let value):
+            XCTAssertEqual(value, 4.0)
+        case .failure(let error):
+            XCTAssertEqual(error, CalculatorError.divisionByZero)
+        }
+    }
+    
+    func test_result_호출시_뒤에_공백이_있는_계산식을_넣으면_올바른_결과를_반환하는지() {
+        // given
+        let fomula = "1 + 3          "
+        // when
+        var result = ExpressionParser.parse(frome: fomula)
+        // then
+        switch result.result() {
+        case .success(let value):
+            XCTAssertEqual(value, 4.0)
+        case .failure(let error):
+            XCTAssertEqual(error, CalculatorError.divisionByZero)
+        }
+    }
 }
