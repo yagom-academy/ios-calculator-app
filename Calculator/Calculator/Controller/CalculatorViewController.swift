2246ea37d059bfeb6f40c01dca914988d4784edc
Donnie <seo_17@naver.com>
2022-03-29 11:55:54 +0900

    ✨[feat]: enum CalculatorEtc 하드코딩 방지용 name space 구현

--- Calculator/Calculator/Controller/CalculatorViewController.swift
+++ Calculator/Calculator/Controller/CalculatorViewController.swift
@@ -1,13 +1,13 @@
 //
-//  ViewController.swift
+//  CalculatorViewController.swift
 //  Created by yagom. 
 //  Copyright © yagom. All rights reserved.
-// /
+//
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class CalculatorViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
---
+++ Calculator/Calculator/Model/CalculatorEtc.swift
@@ -0,0 +1,16 @@
+//
+//  CalculatorEtc.swift
+//  Calculator
+//
+//  Created by SeoDongyeon on 2022/03/29.
+//
+
+import Foundation
+
+enum CalculatorEtc {
+    static let singleZero = "0"
+    static let doubleZero = "00"
+    static let singleDot = "."
+    static let singleZeroAndDot = "0."
+    static let emptyStateString = ""
+}
