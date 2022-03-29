c84441b6a43591b245efa2fbc0ef8a637909a2b6
Donnie <seo_17@naver.com>
2022-03-29 14:58:41 +0900

    ✨[feat]: isValidZeroForTemporaryOperandText() 메서드 구현 및 didTapOperandButtons() 액션함수 연결

--- Calculator/Calculator/Controller/CalculatorViewController.swift
+++ Calculator/Calculator/Controller/CalculatorViewController.swift
@@ -11,6 +11,8 @@ class CalculatorViewController: UIViewController {
     private var temporaryOperandText: String = ""
     private var confirmedFormula: [String] = []
     
+    @IBOutlet weak var operandsLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -20,6 +22,26 @@ class CalculatorViewController: UIViewController {
             return false
         }
         return true
+    }
+        
+    private func isValidZeroForTemporaryOperandText(inputText: String) -> Bool {
+        guard operandsLabel.text == CalculatorEtc.singleZero else { return true }
+        if inputText == CalculatorEtc.singleZero || inputText == CalculatorEtc.doubleZero {
+            return false
+        }
+        return true
+    }
+    
+    @IBAction func didTapOperandButtons(_ sender: UIButton) {
+        guard let operandButtonLabelText = sender.titleLabel?.text else {
+            return
+        }
+        guard isValidTemporaryOperandTextDigitsLessFifteenCount() else {
+            return
+        }
+        guard isValidZeroForTemporaryOperandText(inputText: operandButtonLabelText) else {
+            return
+        }
     }
 }
 
