c957b1600a614d071e393c23e4ca26b6092e73f4
GoMyungju171 <mooooongdu171@gmail.com>
2022-03-29 15:40:28 +0900

    refactor: ExpressionParser 내 componentsByOperators, parse 메서드 로직 수정

--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -14,40 +14,23 @@ extension String {
 }
 
 enum ExpressionParser {
-    
-    private static func compomnentsByOperators(from input: String) throws -> [String] {
-        var value: [String] = [input]
-        let operatorList = Operator.allCases
+    private static func componentsByOperators(from input: String) -> [String] {
+        var splitInput = input.split(with: " ")
+        let operatorList = ["+","−","÷","×"]
         
-        for element in operatorList {
-            value = value.flatMap { $0.split(with: element.rawValue) }
-        }
+        splitInput.removeAll(where: { operatorList.contains($0) })
         
-        value = value.filter { !$0.isEmpty }
-        
-        guard value.count == value.compactMap({ Double($0) }).count else {
-            throw CalauletorError.invalidInputValue
-        }
-        
-        return value
+        return splitInput
     }
     
     static func parse(from input: String) throws -> Formula {
         let operandQueue = CalculatorItemQueue<LinkdeList<Double>>(.init())
         let operatorQueue = CalculatorItemQueue<LinkdeList<Operator>>(.init())
-        let allElement = input.trimmingCharacters(in: .whitespaces).components(separatedBy: " ")
-        let operands = try compomnentsByOperators(from: input).compactMap { Double($0) }
-        let operators = allElement.filter { $0.count == 1 && Operator(rawValue: Character($0)) != nil }
+        let operands = componentsByOperators(from: input)
         
-        if allElement[0].count == 1 && Operator(rawValue: Character(allElement[0])) != nil {
-            operandQueue.enqueue(0.0)
-        }
+        operands.compactMap{Double($0)}.forEach{operandQueue.enqueue($0)}
+        input.split(with: " ").filter{$0.count == 1 && Operator(rawValue: Character($0))}.forEach{operatorQueue.enqueue($0)}
         
-        operands.forEach(operandQueue.enqueue(_:))
-        
-        operators.compactMap ({ Operator(rawValue: Character($0)) })
-            .forEach { operatorQueue.enqueue($0) }
-
         return Formula(operands: operandQueue, operators: operatorQueue)
     }
 }
