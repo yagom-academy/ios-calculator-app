37caf567554ec384eda93bba9747b0aa1723314a
xogns8696@gmail.com <onetool>
2022-03-29 13:58:31 +0900

    ✨[FEAT] : ExpressionParser의 test case 작성

---
+++ Calculator/ExpressionParserTests/ExpressionParserTests.swift
@@ -0,0 +1,31 @@
+//
+//  ExpressionParserTests.swift
+//  ExpressionParserTests
+//
+//  Created by 김태훈 on 2022/03/29.
+//
+
+import XCTest
+@testable import Calculator
+
+class ExpressionParserTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        
+    }
+
+    override func tearDownWithError() throws {
+      
+    }
+    
+    func test_parser_함수를_호출했을때_숫자와_기호가_분리되는지() {
+        // given
+        let someString = "1 + 2"
+        // when
+        let result = ExpressionParser.parse(frome: someString)
+        // then
+        XCTAssertEqual(result.operands.first, 1)
+        XCTAssertEqual(result.operands.last, 2)
+        XCTAssertEqual(result.operators.first, Operator.add)
+    }
+}
