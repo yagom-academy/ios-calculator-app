975653f27c68ff6e44dfed8f42eb78d198e7902f
xogns8696@gmail.com <onetool>
2022-03-29 15:05:13 +0900

    ✨[FEAT] : FomulaTests 파일 생성 및 test case 생성

---
+++ Calculator/FomulaTests/FomulaTests.swift
@@ -0,0 +1,78 @@
+//
+//  FomulaTests.swift
+//  FomulaTests
+//
+//  Created by 김태훈 on 2022/03/29.
+//
+
+import XCTest
+@testable import Calculator
+
+class FomulaTests: XCTestCase {
+    var sut: Fomula!
+    
+    override func setUpWithError() throws {
+        sut = Fomula()
+    }
+
+    override func tearDownWithError() throws {
+        sut = nil
+    }
+
+    func test_result_함수를_호출하면_1더하기1은_2가되는지() {
+        // given
+        let sum = "1 + 1"
+        // when
+        var result = ExpressionParser.parse(frome: sum)
+        // then
+        switch result.result() {
+        case .success(let value):
+            XCTAssertEqual(value, 2.0)
+        case .failure(let error):
+            XCTAssertEqual(error, CalculatorError.nonOperator)
+        }
+        
+    }
+    
+    func test_result_함수_호출시_nonOperator_error가_발생하는지() {
+        // given
+        let sum = "1 + 1 1"
+        // when
+        var result = ExpressionParser.parse(frome: sum)
+        // then
+        switch result.result() {
+        case .success(let value):
+            XCTAssertEqual(value, 2.0)
+        case .failure(let error):
+            XCTAssertEqual(error, CalculatorError.nonOperator)
+        }
+    }
+    
+    func test_result_함수_호출시_nonOperand_error가_발생하는지() {
+        // given
+        let sum = "1 + 1 +"
+        // when
+        var result = ExpressionParser.parse(frome: sum)
+        // then
+        switch result.result() {
+        case .success(let value):
+            XCTAssertEqual(value, 2.0)
+        case .failure(let error):
+            XCTAssertEqual(error, CalculatorError.nonOperand)
+        }
+    }
+    
+    func test_result_함수_호출시_divisionByZero_error가_발생하는지() {
+        // given
+        let sum = "2 ÷ 0"
+        // when
+        var result = ExpressionParser.parse(frome: sum)
+        // then
+        switch result.result() {
+        case .success(let value):
+            XCTAssertEqual(value, Double.infinity)
+        case .failure(let error):
+            XCTAssertEqual(error, CalculatorError.divisionByZero)
+        }
+    }
+}
