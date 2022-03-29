92606260a7dca5a7526afdc762165ba9d9a56100
xogns8696@gmail.com <onetool>
2022-03-29 16:51:06 +0900

    ✨[FEAT] : 숫자 버튼 액션 선언 및 기능 구현, 컨벤션 수정

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -7,11 +7,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private let stringZero = "0"
+    
     @IBOutlet private weak var operatorLabel: UILabel!
     @IBOutlet private weak var operandLabel: UILabel!
     @IBOutlet private weak var fomulaListStackView: UIStackView!
-    private let stringZero = "0"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,13 +22,24 @@ class ViewController: UIViewController {
         removeFomulaList()
         resetCaculator()
     }
+    
     @IBAction func touchCEButton(_ sender: UIButton) {
         self.operandLabel.text = stringZero
     }
     
+    @IBAction func touchNumberButton(_ sender: UIButton) {
+        guard let inputNumber = sender.titleLabel?.text else {
+            return
+        }
+        guard let operandsText = self.operandLabel.text, operandsText.count < 20 else {
+            return
+        }
+        self.operandLabel.text = operandsText + inputNumber
+    }
+    
     private func resetCaculator() {
         self.operatorLabel.text = ""
-        self.operandLabel.text = ""
+        self.operandLabel.text = stringZero
     }
     
     private func removeFomulaList() {
