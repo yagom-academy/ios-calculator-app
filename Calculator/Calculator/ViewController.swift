ddd9e33b0d45bb95f3601a3519fba2dab7a443f7
Red <seryprk@gmail.com>
2022-03-31 04:24:07 +0900

    ♻️refactor : final 키워드 추가

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -6,7 +6,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
     @IBOutlet weak var currentNumberLabbel: UILabel!
     @IBOutlet weak var currentOperatorLabel: UILabel!
     @IBOutlet weak var operationRecord: UIStackView!
