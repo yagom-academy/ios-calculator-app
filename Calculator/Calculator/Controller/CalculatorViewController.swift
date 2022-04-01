78b8d6b4e6599d6aeabfb80c7c0f6218c46a5893
papriOS <bluecrab30@naver.com>
2022-04-01 12:16:40 +0900

    refactor: 불필요한 IBOutlet변수 제거

--- Calculator/Calculator/Controller/CalculatorViewController.swift
+++ Calculator/Calculator/Controller/CalculatorViewController.swift
@@ -2,24 +2,6 @@ import UIKit
 
 final class CalculatorViewController: UIViewController {
     
-    @IBOutlet private weak var numberZeroButton: UIButton!
-    @IBOutlet private weak var numberDoubleZeroButton: UIButton!
-    @IBOutlet private weak var numberDotButton: UIButton!
-    @IBOutlet private weak var numberOneButton: UIButton!
-    @IBOutlet private weak var numberTwoButton: UIButton!
-    @IBOutlet private weak var numberThreeButton: UIButton!
-    @IBOutlet private weak var numberFourButton: UIButton!
-    @IBOutlet private weak var numberFiveButton: UIButton!
-    @IBOutlet private weak var numberSixButton: UIButton!
-    @IBOutlet private weak var numberSevenButton: UIButton!
-    @IBOutlet private weak var numberEightButton: UIButton!
-    @IBOutlet private weak var numberNineButton: UIButton!
-    
-    @IBOutlet private weak var addButton: UIButton!
-    @IBOutlet private weak var subtractButton: UIButton!
-    @IBOutlet private weak var multiplyButton: UIButton!
-    @IBOutlet private weak var divideButton: UIButton!
-    
     @IBOutlet private weak var numberLabel: UILabel!
     @IBOutlet private weak var operatorLabel: UILabel!
     
