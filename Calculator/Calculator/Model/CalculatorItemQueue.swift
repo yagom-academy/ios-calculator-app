c980ae68adc7a5491c9d1c5cf15b3aeb239dba61
Judy <joo00ee@naver.com>
2022-05-16 17:42:13 +0900

    feat: dequeue 메서드 요소 반환 테스트
    - 요소가 하나일 때
    - 요소가 여러개 일 때

--- Calculator/Calculator/Model/CalculatorItemQueue.swift
+++ Calculator/Calculator/Model/CalculatorItemQueue.swift
@@ -1,7 +1,14 @@
 struct CalculatorItemQueue {
     var queue: [Int] = []
+    var count: Int {
+        return queue.count
+    }
     
     mutating func enqueue(element: Int) {
         queue.append(element)
+    }
+    
+    func dequeue() -> Int {
+        return queue[queue.count-1]
     }
 }
--- Calculator/CalculatorTest/CalculatorTest.swift
+++ Calculator/CalculatorTest/CalculatorTest.swift
@@ -14,28 +14,55 @@ class CalculatorTest: XCTestCase {
         sut = nil
     }
 
-    func test_enqueue_0을추가하면_배열에0이들어가는지(){
-        //given
+    func test_enqueue_0을추가하면_배열에0이들어가는지() {
+        // Given
         let input = 0
         let expectation = [0]
         
-        // when
+        // When
         sut.enqueue(element: input)
         let result = sut.queue
+        
         // Then
         XCTAssertEqual(expectation, result)
     }
     
-    func test_enqueue_0과1을추가하면_0과1이들어간배열로변경하는지(){
-        //given
-        let firstTnput = 0
-        let secondTnput = 1
+    func test_enqueue_0과1을추가하면_0과1이들어간배열로변경하는지() {
+        // Given
+        let firstInput = 0
+        let secondInput = 1
         let expectation = [0, 1]
         
-        // when
-        sut.enqueue(element: firstTnput)
-        sut.enqueue(element: secondTnput)
+        // When
+        sut.enqueue(element: firstInput)
+        sut.enqueue(element: secondInput)
         let result = sut.queue
+        
+        // Then
+        XCTAssertEqual(expectation, result)
+    }
+    
+    func test_dequeue_queue가3일때_3을반환하는지() {
+        // Given
+        let currentQueue = [3]
+        let expectation = 3
+        
+        // When
+        sut.queue = currentQueue
+        let result = sut.dequeue()
+        
+        // Then
+        XCTAssertEqual(expectation, result)
+    }
+    
+    func test_dequeue_마지막요소가3일때_3을반환하는지() {
+        // Given
+        let currentQueue = [1, 2, 3, 4, 5]
+        let expectation = 5
+        
+        // When
+        sut.queue = currentQueue
+        let result = sut.dequeue()
         
         // Then
         XCTAssertEqual(expectation, result)
