954c86e6946deea6712a44b9df05455291b169ef
bard <schy2503@naver.com>
2022-05-17 06:36:07 +0900

    feat: 중복된 파일 제거 및 폴더 재정렬

--- Calculator/Calculator/Model/CalculateItem.swift
+++
@@ -1,8 +0,0 @@
-//
-//  CalculatorItem.swift
-//  Calculator
-//
-//  Created by 김동용 on 2022/05/17.
-//
-
-protocol CalculateItem { }
--- Calculator/Calculator/Model/Protocol/CalculateItem.swift
+++ Calculator/Calculator/Model/Protocol/CalculateItem.swift
@@ -1,6 +1,8 @@
 //
-//  CalculateItem.swift
+//  CalculatorItem.swift
 //  Calculator
 //
-//  Created by 김동용 on 2022/05/16.
+//  Created by 김동용 on 2022/05/17.
 //
+
+protocol CalculateItem { }
