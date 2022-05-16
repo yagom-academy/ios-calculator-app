2cfa35e16bba4b656ef3d044622a6459ef78d6a1
OldStarKR <tyr2293@naver.com>
2022-05-17 06:21:49 +0900

    feat: queue가 갖춰야 할 기능을 프로토콜로 정의

---
+++ Calculator/Calculator/Application/Domain/Protocols/Queue.swift
@@ -0,0 +1,13 @@
+//
+//  Queue.swift
+//  Calculator
+//
+//  Created by Gordon Choi on 2022/05/17.
+//
+protocol Queue {
+    associatedtype Element
+    var isEmpty: Bool { get }
+    var peek: Element? { get }
+    mutating func enqueue(_ element: Element) -> Bool
+    mutating func dequeue() -> Element?
+}
