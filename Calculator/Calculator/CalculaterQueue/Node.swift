c1453a51f902803ee9a640160bba2a0e5b8cde19
derrickkim0109 <derrickkim0109@naver.com>
2022-05-17 15:36:43 +0900

    feat : TDD를 활용하여 "node" class 구현

---
+++ Calculator/Calculator/CalculaterQueue/Node.swift
@@ -0,0 +1,18 @@
+//
+//  Node.swift
+//  Calculator
+//
+//  Created by Derrick kim on 2022/05/17.
+//
+
+import Foundation
+
+class Node<T> {
+    var value: T
+    var previous: Node<T>?
+    var next: Node<T>?
+    
+    init(value: T) {
+        self.value = value
+    }
+}
