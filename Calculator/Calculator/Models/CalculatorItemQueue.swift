72ddd1c8d1cfa99632b8e6a215d7d3a9b75150bf
finnn1 <tgss3535@gmail.com>
2022-05-17 15:32:34 +0900

    feat: CalculatorItemQueue 에 기본적인 선입선출 기능 구현
    - linkedList 프로퍼티
    - push(element:)
    - pop() -> T?

--- Calculator/Calculator/Models/CalculatorItemQueue.swift
+++ Calculator/Calculator/Models/CalculatorItemQueue.swift
@@ -1,3 +1,13 @@
 struct CalculatorItemQueue<T>: CalculateItem {
     
+    private var linkedList = LinkedList<T>()
+    
+    mutating func push(element: T) {
+        linkedList.pushAfterTail(element: element)
+    }
+    
+    mutating func pop() -> T? {
+        return linkedList.popHead()
+    }
+
 }
--- Calculator/Calculator/Models/UnitTest/CalculatorItemQueueTDD.swift
+++ Calculator/Calculator/Models/UnitTest/CalculatorItemQueueTDD.swift
@@ -15,7 +15,26 @@ class CalculatorItemQueueTDD: XCTestCase {
     }
     
     // MARK: - First in fisrt out Tests
-
+    func test_선입선출인지_확인() throws {
+        // given
+        let inputValue1: Int = 1
+        let inputValue2: Int = 2
+        let inputValue3: Int = 3
+        let expectation1: Int = 1
+        let expectation2: Int = 2
+        let expectation3: Int = 3
+        
+        // what
+        intQueue.push(element: inputValue1)
+        intQueue.push(element: inputValue2)
+        intQueue.push(element: inputValue3)
+        
+        // then
+        XCTAssertEqual(intQueue.pop(), expectation1)
+        XCTAssertEqual(intQueue.pop(), expectation2)
+        XCTAssertEqual(intQueue.pop(), expectation3)
+        
+    }
     
 
 }
