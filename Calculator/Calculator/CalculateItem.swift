515eaa24dee9b357d0b030dab8d6f080992d4ff7
웡빙 <wb277@naver.com>
2022-05-17 09:23:05 +0900

    feat : "CalculatorItem" 빈 프로토콜 생성

---
+++ Calculator/Calculator/CalculateItem.swift
@@ -0,0 +1,12 @@
+//
+//  CalculateItem.swift
+//  Calculator
+//
+//  Created by 이원빈 on 2022/05/16.
+//
+
+import Foundation
+
+protocol CalculateItem {
+    
+}
---
+++ Calculator/Calculator/CalculatorItemQueue.swift
@@ -0,0 +1,8 @@
+//
+//  CalculatorItemQueue.swift
+//  Calculator
+//
+//  Created by 이원빈 on 2022/05/16.
+//
+
+import Foundation
