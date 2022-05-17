0a071b92fe4cf2e3d9501ccf62be07bfb72431c2
derrickkim0109 <derrickkim0109@naver.com>
2022-05-17 18:08:35 +0900

    docs: "CalculateItemProtocol" 기능 문서화

--- Calculator/Calculator/CalculaterQueue/CalculateItemProtocol.swift
+++ Calculator/Calculator/CalculaterQueue/CalculateItemProtocol.swift
@@ -10,13 +10,17 @@ import Foundation
 protocol CalculateItemProtocol {
     associatedtype Element
     
+    /// 큐 끝에 새로운 큐를 추가하고 성공여부 반환
     mutating func enQueue(_ element: Element) -> Bool
 
+    /// 가장 먼저 들어온 앞의 큐 제거하고 제거한 큐 반환
     mutating func deQueue() -> Element?
     
     func removeAll()
     
+    /// 큐가 비어있는지 알려주는 읽기전용 프로퍼티
     var isEmpty: Bool { get }
     
+    /// 큐의 제일 처음 값을 가짐
     var peek: Element? { get }
 }
