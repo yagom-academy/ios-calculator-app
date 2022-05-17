931ebef867576969a8eb480d271b45eb5cc5456c
unchain123 <unchain123@kakao.com>
2022-05-17 15:53:37 +0900

    refactor: remove prev instance tail instance

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -22,28 +22,14 @@ protocol CalculateItem {
 
 struct CalculatorItemQueue<D>: CalculateItem {
     var head: List<D>?
-    var tail: List<D>?
-    
-    mutating func enQueue(data: D?) {
-        if head == nil || tail == nil {
-            head = List.init(data: data)
-            tail = head
-            return
-        }
-        let newList = List.init(data: data)
-        tail?.next = newList
-        newList.prev = tail
-        tail = newList
-    }
+ 
 }
 
 class List<D> {
-    var prev: List?
     var data: D?
     var next: List?
     
-    init (prev: List? = nil, data: D?, next: List? = nil) {
-        self.prev = prev
+    init (data: D?, next: List? = nil) {
         self.data = data
         self.next = next
     }
