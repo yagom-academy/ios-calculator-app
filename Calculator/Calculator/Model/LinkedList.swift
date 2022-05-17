2f581b148338e75db2658562c3fd7376968dcaa8
yeeton37 <lyeeun37@naver.com>
2022-05-17 19:34:02 +0900

    chore: 파일 생성 및 Model,View,Controller 파일 분리

---
+++ Calculator/Calculator/Model/LinkedList.swift
@@ -0,0 +1,8 @@
+//
+//  LinkedList.swift
+//  Calculator
+//
+//  Created by 이예은 on 2022/05/17.
+//
+
+import Foundation
---
+++ Calculator/Calculator/Model/Node.swift
@@ -0,0 +1,8 @@
+//
+//  Node.swift
+//  Calculator
+//
+//  Created by 이예은 on 2022/05/17.
+//
+
+import Foundation
