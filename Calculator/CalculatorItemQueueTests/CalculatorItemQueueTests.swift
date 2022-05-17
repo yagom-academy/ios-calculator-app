04657d0ae7cfd45f2fcab6337f78634d1fe462b6
starting1231 <starting1231@naver.com>
2022-05-17 17:00:37 +0900

    test: Refactoring test_enQueue_큐에값이여러개추가되는지 test function

--- Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
+++ Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
@@ -31,20 +31,6 @@ class CalculatorItemQueueTests: XCTestCase {
         XCTAssertEqual(result, 1)
     }
     
-    func test_enQueue_큐에값이여러개추가되는지() {
-        // given
-        sut.enQueue(data: 1)
-        sut.enQueue(data: 2)
-        
-        // when
-        let result1 = sut.list.takeOutFirst()
-        let result2 = sut.list.takeOutFirst()
-        
-        // then
-        XCTAssertEqual(result1, 1)
-        XCTAssertEqual(result2, 2)
-    }
-    
     func test_deQueue_큐에서첫번째값을반환하는지() {
         // given
         sut.enQueue(data: 1)
@@ -54,6 +40,20 @@ class CalculatorItemQueueTests: XCTestCase {
         
         // then
         XCTAssertEqual(result, 1)
+    }
+    
+    func test_enQueue_큐에값을여러개추가하고_삭제했을때_순서대로가져오는지() {
+        // given
+        sut.enQueue(data: 1)
+        sut.enQueue(data: 2)
+        
+        // when
+        let result1 = sut.deQueue()
+        let result2 = sut.deQueue()
+        
+        // then
+        XCTAssertEqual(result1, 1)
+        XCTAssertEqual(result2, 2)
     }
     
     func test_isEmpty_큐에값이있을때_false를반환하는지() {
