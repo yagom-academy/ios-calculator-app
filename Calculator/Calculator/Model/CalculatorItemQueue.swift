1c802e60675dfae1c63a443a04d6a9966e007ee8
seohyeon2 <seo_hyeon418@naver.com>
2022-05-17 11:14:48 +0900

    feat: CalculatorItemQueue에 요소를 뻬는 메서드 구현

--- Calculator/Calculator/Model/CalculatorItemQueue.swift
+++ Calculator/Calculator/Model/CalculatorItemQueue.swift
@@ -1,7 +1,15 @@
 struct CalculatorItemQueue<T>: CalculateItem {
     var calculateItems = [T]()
     
-    mutating func enqueu(_ element: T) {
+    mutating func enqueue(_ element: T) {
         calculateItems.append(element)
+    }
+    
+    mutating func dequeue() -> T? {
+        
+        if calculateItems.isEmpty {
+            return nil
+        }
+        return calculateItems.removeFirst()
     }
 }
--- Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
+++ Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
@@ -21,13 +21,39 @@ class CalculatorTests: XCTestCase {
         sut = nil
     }
     
-    func calculateItems에_요소가_잘_추가되는지() {
+    func testCalculateItems에_요소가_잘_추가되는지() {
         // given
-        let number = 3.0
-        sut.enqueu(number)
+        sut.enqueue(3.0)
         // when
         let result = sut.calculateItems[0]
         // then
         XCTAssertEqual(result, 3.0)
+    }
+    
+    func testCalculateItems의_요소가_잘_빠지는지() {
+        // given
+        sut.enqueue(3.0)
+        // when
+        let result = sut.dequeue()
+        // then
+        XCTAssertEqual(result, 3.0)
+    }
+    
+    func testCalculateItem의_첫번째_요소가_잘_빠지는지() {
+        // given
+        sut.enqueue(3.0)
+        sut.enqueue(4.0)
+        sut.enqueue(5.0)
+        // when
+        let result = sut.dequeue()
+        // then
+        XCTAssertEqual(result, 3.0)
+    }
+    
+    func testCalculateItem이_비어있을_때_nil이_반환되는지() {
+        // given when
+        let result = sut.dequeue()
+        // then
+        XCTAssertEqual(result, nil)
     }
 }
