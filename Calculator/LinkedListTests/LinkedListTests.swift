8abee2641d849d2fe88e630799c0a7f306a7680d
starting1231 <starting1231@naver.com>
2022-05-17 11:29:23 +0900

    test: Add unit test bundle

---
+++ Calculator/LinkedListTests/LinkedListTests.swift
@@ -0,0 +1,20 @@
+//
+//  LinkedListTests.swift
+//  LinkedListTests
+//
+//  Created by NAMU on 2022/05/17.
+//
+
+import XCTest
+@testable import Calculator
+
+class LinkedListTests: XCTestCase {
+    var sut: LinkedList<Int>!
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+}
