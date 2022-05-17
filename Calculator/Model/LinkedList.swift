80bd087abe0872453bc44ae7d3efe1b315ca208b
starting1231 <starting1231@naver.com>
2022-05-17 11:17:35 +0900

    add: Add LinkedList for queue

---
+++ Calculator/Model/LinkedList.swift
@@ -0,0 +1,22 @@
+//
+//  LinkedList.swift
+//  Calculator
+//
+//  Created by NAMU on 2022/05/17.
+//
+
+struct LinkedList<T> {
+    var head: Node<T>?
+    
+    func append(data: T){
+        
+    }
+    
+    func removeAndReturnFirst() -> T? {
+        return nil
+    }
+    
+    func isEmpty() -> Bool {
+        return false
+    }
+}
