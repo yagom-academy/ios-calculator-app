30f90fc2a5bf032bf0474f2f5657e281170b9a8d
MinsuJeon <chso294@naver.com>
2022-05-18 15:00:27 +0900

    refactor: enqueue 메서드의 반환 타입 수정
    - Queue 구조체 내 enqueue 메서드의 기존 반환 타입인 [Element]를 Void로 수정
    - 위의 수정에 따른 테스트 코드 수정 완료

--- Calculator/QueueTests/QueueTests.swift
+++ Calculator/QueueTests/QueueTests.swift
@@ -20,142 +20,135 @@ class QueueTests: XCTestCase {
         sut = nil
     }
 
-    func test_enqueue_enqueueStack에_자연수가_순서대로_들어가는지_테스트() throws {
+    func test_enqueue_양의_실수가_순서대로_들어가는지_테스트() throws {
+        // given
         let input1: Double = 1
         let input2: Double = 9999
-        let expectation: [Double] = [1, 9999]
+        sut.enqueue(element: input1)
+        sut.enqueue(element: input2)
         
-        var result = sut.enqueue(element: input1)
-        result = sut.enqueue(element: input2)
+        // when
+        let result1 = sut.dequeue()
+        let result2 = sut.dequeue()
         
-        XCTAssertEqual(result, expectation)
+        // then
+        XCTAssertEqual(result1, input1)
+        XCTAssertEqual(result2, input2)
     }
-    
-    func test_enqueue_enqueueStack에_0과_음의정수가_순서대로_들어가는지_테스트() throws {
+
+
+    func test_enqueue_0과_음의실수가_순서대로_들어가는지_테스트() throws {
+        // given
         let input1: Double = 0
         let input2: Double = -9999
-        let expectation: [Double] = [0, -9999]
-
-        var result = sut.enqueue(element: input1)
-        result = sut.enqueue(element: input2)
-
-        XCTAssertEqual(result, expectation)
+        sut.enqueue(element: input1)
+        sut.enqueue(element: input2)
+        
+        // when
+        let result1 = sut.dequeue()
+        let result2 = sut.dequeue()
+        
+        // then
+        XCTAssertEqual(result1, input1)
+        XCTAssertEqual(result2, input2)
     }
-    
-    func test_enqueue_enqueueStack에_실수가_순서대로_들어가는지_테스트() throws {
+
+    func test_enqueue_중복된_실수가_제대로_들어가는지_테스트() throws {
+        // given
         let input1 = 1.0
         let input2 = 2.0
-        let expectation = [1.0, 2.0]
-        
-        var result = sut.enqueue(element: input1)
-        result = sut.enqueue(element: input2)
-        
-        XCTAssertEqual(result, expectation)
+        let input3 = 2.0
+        sut.enqueue(element: input1)
+        sut.enqueue(element: input2)
+        sut.enqueue(element: input3)
+
+        // when
+        let result1 = sut.dequeue()
+        let result2 = sut.dequeue()
+        let result3 = sut.dequeue()
+
+        // then
+        XCTAssertEqual(result1, input1)
+        XCTAssertEqual(result2, input2)
+        XCTAssertEqual(result3, input3)
     }
-    
-    func test_enqueue_enqueueStack에_중복된_실수가_제대로_들어가는지_테스트() throws {
-        let input1 = 1.0
-        let input2 = 1.0
-        let expectation = [1.0, 1.0]
-        
-        var result = sut.enqueue(element: input1)
-        result = sut.enqueue(element: input2)
-        
-        XCTAssertEqual(result, expectation)
-    }
-    
-    func test_enqueue_enqueueStack에_100개의_실수가_순서대로_들어가는지_테스트() throws {
+
+    func test_enqueue_1000개의_실수가_순서대로_들어가는지_테스트() throws {
+        // given
         var expectation: [Double] = []
         
-        for input in 0...99 {
+        for input in 0...999 {
             expectation.append(Double(input))
-            _ = sut.enqueue(element: Double(input))
+            sut.enqueue(element: Double(input))
         }
-
-        let result = sut.enqueueStack
         
-        XCTAssertEqual(result, expectation)
+        // when
+        var result: Double?
+        
+        // then
+        for i in 0...999 {
+            result = sut.dequeue()
+            XCTAssertEqual(result, expectation[i])
+        }
     }
-    
-    func test_isEmpty_enqueueStack에_값이_없을때_빈_배열로_인지하는지_테스트() throws {
-        let result = sut.isEmpty
+
+    func test_isEmpty_빈_큐임을_정상적으로_인지하는지_테스트() throws {
+        // given
+        sut.clearQueue()
         
+        // when
+        let result = sut.isEmpty
+
+        // then
         XCTAssertTrue(result)
     }
-    
-    func test_isEmpty_enqueueStack에_값이_있을때_비어있지_않은_배열로_인지하는지_테스트() throws {
-        let enqueStack = sut.enqueue(element: 1.0)
-        
-        let result = enqueStack.isEmpty
-        
+
+    func test_isEmpty_enqueue_메서드가_호출될때_비어있지_않은_배열로_인지하는지_테스트() throws {
+        // given
+        sut.enqueue(element: 1.0)
+
+        // when
+        let result = sut.isEmpty
+
+        // then
         XCTAssertFalse(result)
     }
     
-    func test_dequeue_dequeueStack에_실수가_순서대로_제거되는지_테스트() throws {
-        _ = sut.enqueue(element: 1.0)
-        _ = sut.enqueue(element: 2.0)
-        _ = sut.enqueue(element: 3.0)
-        let expectation = 1.0
-        
-        let result = sut.dequeue()
-        
-        XCTAssertEqual(result, expectation)
+    func test_dequeue_실수가_순서대로_제거되는지_테스트() throws {
+        // given
+        sut.enqueue(element: 1.0)
+        sut.enqueue(element: 2.0)
+        let expectation1 = 1.0
+        let expectation2 = 2.0
+
+        // when
+        let result1 = sut.dequeue()
+        let result2 = sut.dequeue()
+
+        // then
+        XCTAssertEqual(result1, expectation1)
+        XCTAssertEqual(result2, expectation2)
     }
 
-    func test_dequeue_dequeue가_진행될때_enqueueStck이_빈_배열이_되는지_테스트() throws {
-        _ = sut.enqueue(element: 1.0)
-        _ = sut.enqueue(element: 2.0)
-        _ = sut.dequeue()
-        
-        let result = sut.enqueueStack.isEmpty
-        
-        XCTAssertTrue(result)
-    }
-    
-    func test_dequeue_dequeueStack이_빈_배열일_때_dequeue가_되는_값이_없는지_테스트() throws {
-        let result = sut.dequeue()
-        
-        XCTAssertEqual(result, nil)
-    }
-    
-    func test_clearQueue_enqueueStack이_빈_배열이_아닐때_clearQueue를_통해_빈_배열이_되는지_테스트() throws {
-        _ = sut.enqueue(element: 1.0)
-        _ = sut.enqueue(element: 2.0)
-        sut.clearQueue()
-        
-        let result = sut.isEmpty
-        
-        XCTAssertTrue(result)
-    }
-    
-    func test_clearQueue_dequeueStack이_빈_배열이_아닐때_clearQueue를_통해_빈_배열이_되는지_테스트() throws {
-        _ = sut.enqueue(element: 1.0)
-        _ = sut.enqueue(element: 2.0)
-        _ = sut.dequeue()
-        sut.clearQueue()
-        
-        let result = sut.isEmpty
-        
-        XCTAssertTrue(result)
-    }
-    
-    func test_peek_enqueueStack이_빈_배열일때_peek를_정상적으로_반환하는지_테스트() throws {
-        _ = sut.enqueue(element: 1.0)
-        _ = sut.enqueue(element: 2.0)
+    func test_peek_peek를_정상적으로_반환하는지_테스트() throws {
+        sut.enqueue(element: 1.0)
+        sut.enqueue(element: 2.0)
         let expectaion = 1.0
-        
+
         let result = sut.peek
-        
+
         XCTAssertEqual(result, expectaion)
     }
-    
-    func test_peek_enqueueStack이_빈_배열이_아닐때_peek를_정상적으로_반환하는지_테스트() throws {
-        _ = sut.enqueue(element: 1.0)
-        _ = sut.enqueue(element: 2.0)
-        let expectaion = 1.0
-        
+
+
+    func test_peek_dequeue_메서드_호출시_peek를_정상적으로_반환하는지_테스트() throws {
+        sut.enqueue(element: 1.0)
+        sut.enqueue(element: 2.0)
+        _ = sut.dequeue()
+        let expectaion = 2.0
+
         let result = sut.peek
-        
+
         XCTAssertEqual(result, expectaion)
     }
 }
