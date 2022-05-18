80fb4d265b71a39f8b6aca03164e702b102f1420
unchain123 <unchain123@kakao.com>
2022-05-18 23:33:22 +0900

    refactor: change test fucntion name(test_숫자5를_넣었을때_head에_5가_들어있는지확인)

--- Calculator/CalculatorItemQueueTest/CalculatorItemQueueTest.swift
+++ Calculator/CalculatorItemQueueTest/CalculatorItemQueueTest.swift
@@ -21,7 +21,7 @@ class CalculatorItemQueueTest: XCTestCase {
         sut = nil
     }
     
-    func test_숫자5를_넣었을때_5가_들어있는지확인() {
+    func test_숫자5를_넣었을때_head에_5가_들어있는지확인() {
         sut.linkedList.append(data: 5)
         
         XCTAssertEqual(sut.linkedList.head?.data, 5)
