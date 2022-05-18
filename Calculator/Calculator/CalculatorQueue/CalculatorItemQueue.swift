6d4b3680f6f015f642c7d4c74154a7ddcf00ff95
yusw10 <yusw10@naver.com>
2022-05-18 14:06:07 +0900

    refactor: Refacotring CalculatorItemQueue to inherit CalculatorItem protocol
    - modify CalculatorList's type to use CalculatorItem protocol

--- Calculator/Calculator/CalculatorQueue/CalculatorItemQueue.swift
+++ Calculator/Calculator/CalculatorQueue/CalculatorItemQueue.swift
@@ -5,17 +5,17 @@
 //  Created by 유한석 on 2022/05/16.
 //
 class CalculatorItemQueue: CalculatorItem {
-    private var queue: CalculatorList<String> = CalculatorList()
+    private var queue: CalculatorList<CalculatorItem> = CalculatorList()
     var count: Int {
         return queue.size()
     }
     
-    func enQueue(_ input: String) {
+    func enQueue(_ input: CalculatorItem) {
         let newNode = Node(value: input)
         self.queue.append(newNode)
     }
     
-    func deQueue() -> Node<String>? {
+    func deQueue() -> Node<CalculatorItem>? {
         if queue.isEmpty() {
             return nil
         }
