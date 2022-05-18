e1b2326b7d9e8ac79849340293a9e2c702cdd6c9
yusw10 <yusw10@naver.com>
2022-05-18 20:39:54 +0900

    feat: Add Operator Enum type
    - add Operator enum
    - define case of Operator enum
    - define function of Operator enum

---
+++ Calculator/Calculator/CalculatorQueue/Operator.swift
@@ -0,0 +1,28 @@
+//
+//  Operator.swift
+//  Calculator
+//
+//  Created by 유한석 on 2022/05/18.
+//
+enum Operator: Character, CaseIterable {
+    case add
+    case substractk
+    case divide
+    case multiply
+    
+    func calculate(lhs: Double, rhs: Double) -> Double {
+        return Double()
+    }
+    private func add(lhs: Double, rhs: Double) -> Double {
+        return Double()
+    }
+    private func substract(lhs: Double, rhs: Double) -> Double {
+        return Double()
+    }
+    private func divide(lhs: Double, rhs: Double) -> Double {
+        return Double()
+    }
+    private func multiply(lhs: Double, rhs: Double) -> Double {
+        return Double()
+    }
+}
