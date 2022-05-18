f4d259b6cf22ab88c05d73236bd45d3dd1dc1922
OldStarKR <tyr2293@naver.com>
2022-05-18 14:42:43 +0900

    refactor: 큐에 대한 테스트 코드가 기능명세서의 역할을 더 잘 수행하도록 수정

--- Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
+++ Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
@@ -18,11 +18,7 @@ class CalculatorItemQueueTests: XCTestCase {
     override func setUpWithError() throws {
         try super.setUpWithError()
         
-        sut = CalculatorItemQueue<CalculateItemTester>(
-            array: [CalculateItemTester(value: 1),
-                    CalculateItemTester(value: 2),
-                    CalculateItemTester(value: 3)]
-        )
+        sut = CalculatorItemQueue<CalculateItemTester>(array: [CalculateItemTester(value: 5)])
     }
     
     override func tearDownWithError() throws {
@@ -31,32 +27,67 @@ class CalculatorItemQueueTests: XCTestCase {
         sut = nil
     }
     
-    func test_isEmpty가_제대로_작동하는지() {
+    func test_큐에_원소가있을때_isEmpty가_false를_반환() {
+        // given
+        
+        // when
         let result = sut.isEmpty
         
+        // then
         XCTAssertFalse(result)
     }
     
-    func test_peek가_첫번째원소를_보여주는지() {
+    func test_peek은_첫번째원소인_5를_보여줌() {
+        // given
+        
+        // when
         let result = sut.peek?.value
         
-        XCTAssertEqual(result, 1)
+        // then
+        XCTAssertEqual(result, 5)
     }
     
-    func test_dequeue로_큐의첫번째원소를_뺄수있는지() {
+    func test_peek은_원소가없을경우_nil을_반환() {
+        // given
+        sut.dequeue()
+        
+        // when
+        let result = sut.peek?.value
+        
+        // then
+        XCTAssertNil(result)
+    }
+    
+    func test_dequeue로_큐의첫번째원소인_5를_제거하고_그값을반환() {
+        // given
+        
+        // when
         let result = sut.dequeue()?.value
         
-        XCTAssertEqual(result, 1)
+        // then
+        XCTAssertEqual(result, 5)
     }
     
-    func test_enqueue로_큐에_원소를_더할수있는지() {
-        sut.enqueue(CalculateItemTester(value: 5))
-        for _ in 0...2 {
-            sut.dequeue()
-        }
+    func test_큐가비어있을때_dequeue는_nil을_반환() {
+        // given
+        sut.dequeue()
         
+        // when
+        let result = sut.dequeue()?.value
+        
+        // then
+        XCTAssertNil(result)
+    }
+    
+    func test_enqueue로_큐에_4를삽입하고_확인() {
+        // given
+        sut.enqueue(CalculateItemTester(value: 4))
+        sut.dequeue()
+        
+        // when
         let result = sut.peek?.value
         
-        XCTAssertEqual(result, 5)
+        // then
+        XCTAssertEqual(result, 4)
     }
 }
