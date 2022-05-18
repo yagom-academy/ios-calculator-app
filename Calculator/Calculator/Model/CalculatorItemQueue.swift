0a669239daebfa690fd60f35e7d07570c12d0b27
seohyeon2 <seo_hyeon418@naver.com>
2022-05-18 22:29:17 +0900

    feat: CalculatorItemQueue에 링크드 리스트로 요소를 추가하는 메서드 구현

--- Calculator/Calculator/Model/CalculatorItemQueue.swift
+++ Calculator/Calculator/Model/CalculatorItemQueue.swift
@@ -1,3 +1,13 @@
 struct CalculatorItemQueue<T>: CalculateItem {
     private var calculateItems = LinkedList<T>()
+    
+    mutating func enqueue(_ element: T) {
+        calculateItems.insert(data: element)
+    }
+    
+    mutating func showHead() -> (data: T?, next: Node<T>?) {
+        let headData = calculateItems.head?.data
+        let headNext = calculateItems.head?.next
+        return (headData, headNext)
+    }
 }
--- Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
+++ Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
@@ -14,4 +14,14 @@ class CalculatorItemQueueTests: XCTestCase {
         try super.tearDownWithError()
         sut = nil
     }
+    
+    // MARK: Test_enqueue()
+    func testCalculateItems에_요소가_잘_추가되는지() {
+        //given
+        sut.enqueue(1.0)
+        //when
+        let result = sut.showHead().data
+        //then
+        XCTAssertEqual(result, 1.0)
+    }
 }
