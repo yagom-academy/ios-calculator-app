3a7482a7a67c9dfb7bf7d92c0a4e3c5d78eadd02
minsson <mskaang@gmail.com>
2022-05-19 01:15:28 +0900

    style: CalculateItem 빈 프로토콜 추가
    CalculatorItemQueue에서 다루는 요소인 Double형이 해당 프로토콜을 준수하도록 지정

---
+++ Calculator/Calculator/Model/Double+CalculateItem.swift
@@ -0,0 +1,14 @@
+//
+//  Double+CalculateItem.swift
+//  Calculator
+//
+//  Created by Minseong Kang on 2022/05/19.
+//
+
+protocol CalculateItem {
+    
+}
+
+extension Double: CalculateItem {
+    
+}
