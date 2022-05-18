9e14625e7ddeeaa88e9513f606c8443c5fbc1195
seohyeon2 <seo_hyeon418@naver.com>
2022-05-18 18:47:18 +0900

    refactor: moveToPreviousness()메서드 반복문을 고차함수로 수정

--- Calculator/Calculator/Node/LinkedList.swift
+++ Calculator/Calculator/Node/LinkedList.swift
@@ -13,8 +13,10 @@ class LinkedList<T> {
     var head: Node<T>?
     var tail: Node<T>?
     var previousness: Node<T>?
+    var count = 0
 
     func insert(data: T) {
+        count += 1
         if head == nil {
             head = Node(data: data, next: nil)
             previousness = head
@@ -28,6 +30,7 @@ class LinkedList<T> {
     }
     
     func delete() {
+        count -= 1
         previousness?.next = nil
         tail = previousness
         moveToPreviousness()
@@ -36,7 +39,7 @@ class LinkedList<T> {
     func moveToPreviousness() {
         var current = head
         var before:Node<T>?
-        while current?.next != nil {
+        (0...count-2).forEach { _ in
             before = current
             current = current?.next
         }
