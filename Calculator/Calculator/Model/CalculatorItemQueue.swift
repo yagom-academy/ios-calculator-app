d6b120f1ee8ddf1a9705d6e374a522abf804d76d
MinsuJeon <chso294@naver.com>
2022-05-18 15:04:01 +0900

    refactor: clearQueue 메서드의 네이밍을 clear로 수정

--- Calculator/Calculator/Model/CalculatorItemQueue.swift
+++ Calculator/Calculator/Model/CalculatorItemQueue.swift
@@ -4,31 +4,6 @@
 //
 //  Created by 전민수 on 2022/05/16.
 //
-struct CalculatorItemQueue<Element> {
-    private(set) var enqueueStack: [Element] = []
-    private(set) var dequeueStack: [Element] = []
-    var isEmpty: Bool {
-        return enqueueStack.isEmpty && dequeueStack.isEmpty
-    }
-    var peek: Element? {
-        return dequeueStack.isEmpty ? enqueueStack.first : dequeueStack.last
-    }
-    
-    mutating func enqueue(element: Element) -> [Element] {
-        enqueueStack.append(element)
-        return enqueueStack
-    }
-    
-    mutating func dequeue() -> Element? {
-        if dequeueStack.isEmpty {
-            dequeueStack = enqueueStack.reversed()
-            enqueueStack.removeAll()
-        }
-        return dequeueStack.popLast()
-    }
-    
-    mutating func clearQueue() {
-        enqueueStack.removeAll()
-        dequeueStack.removeAll()
-    }
+class CalculatorItemQueue<Element> {
+    private var queue: Queue<Element> = Queue()
 }
