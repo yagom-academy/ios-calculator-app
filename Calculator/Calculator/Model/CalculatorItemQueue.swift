36ceb9f1c2906d82eeada664ee410f76ae958adc
Judy <joo00ee@naver.com>
2022-05-18 12:50:04 +0900

    refactor: 구현한 스택을 이용하지 않고 Array만으로 더블 스택 큐 구현

--- Calculator/Calculator/Model/CalculatorItemQueue.swift
+++ Calculator/Calculator/Model/CalculatorItemQueue.swift
@@ -1,28 +1,29 @@
 struct CalculatorItemQueue<Item: CalculatorItem> {
-    private(set) var enqueueStack = Stack<Item>()
-    private(set) var dequeueStack = Stack<Item>()
+    private(set) var enqueueStack: [Item] = []
+    private(set) var dequeueStack: [Item] = []
     var calculatorItems: [Item] {
         get {
-            return dequeueStack.stack.reversed() + enqueueStack.stack 
+            return dequeueStack.reversed() + enqueueStack
         }
     }
     
     func isEmpty() -> Bool {
-        return enqueueStack.isEmpty() && dequeueStack.isEmpty()
+        return enqueueStack.isEmpty && dequeueStack.isEmpty
     }
     
     mutating func enqueue(_ element: Item) {
-        enqueueStack.enqueue(element)
+        enqueueStack.append(element)
     }
     
     mutating func dequeue() -> Item? {
         guard !isEmpty() else { return nil }
         
-        if dequeueStack.isEmpty() {
-            dequeueStack.stack = enqueueStack.stack.reversed()
-            enqueueStack.stack.removeAll()
+        if dequeueStack.isEmpty {
+            dequeueStack = enqueueStack.reversed()
+            enqueueStack.removeAll()
         }
 
-        return dequeueStack.dequeue()
+        return dequeueStack.popLast()
     }
 }
+
