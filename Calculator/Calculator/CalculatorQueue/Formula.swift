c91631021d4090ca6879bc29edac747c8fd9c22c
yusw10 <yusw10@naver.com>
2022-05-19 16:59:04 +0900

    feat: Add formula struct
    - add operands property
    - add operators property
    - add result function to return Double

---
+++ Calculator/Calculator/CalculatorQueue/Formula.swift
@@ -0,0 +1,18 @@
+//
+//  Formula.swift
+//  Calculator
+//
+//  Created by 유한석 on 2022/05/19.
+//
+struct Formula {
+    var operands: CalculatorItemQueue
+    var operators: CalculatorItemQueue
+    
+    func result() -> Double {
+        return 0.0
+    }
+}
+
+
+
+
