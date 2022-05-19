b8caeeeb5986fe58a9c52f57872844524057aea7
yusw10 <yusw10@naver.com>
2022-05-19 18:57:50 +0900

    test: Add test class for Formula struct
    - add FormulaTests file for Formula struct testing

---
+++ Calculator/CalculatorTests/FormulaTests.swift
@@ -0,0 +1,19 @@
+//
+//  FormulaTests.swift
+//  Calculator
+//
+//  Created by 유한석 on 2022/05/19.
+//
+
+import XCTest
+
+class FormulaTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+}
