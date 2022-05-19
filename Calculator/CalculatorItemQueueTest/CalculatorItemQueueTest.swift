a2de726125d82d47d35de1c12bf357faf7646462
Kiwi <toskw@naver.com>
2022-05-19 18:52:42 +0900

    refactor: 테스트 코드 옵셔널 언랩핑

--- Calculator/CalculatorItemQueueTest/CalculatorItemQueueTest.swift
+++ Calculator/CalculatorItemQueueTest/CalculatorItemQueueTest.swift
@@ -8,7 +8,7 @@
 import XCTest
 @testable import Calculator
 class CalculatorItemQueueTest: XCTestCase {
-    var sut: CalculatorItemQueue<Int>?
+    var sut: CalculatorItemQueue<Int>!
     
     override func setUpWithError() throws {
         sut = CalculatorItemQueue<Int>()
@@ -27,30 +27,30 @@ class CalculatorItemQueueTest: XCTestCase {
     
     func test_1을_넣어주고_enqueue를_실행하면_isEmpty값이_false여야한다() {
         //given
-        sut?.enqueue(data: 1)
+        sut.enqueue(data: 1)
         //when,then
-        XCTAssertFalse(sut!.isEmpty)
+        XCTAssertFalse(sut.isEmpty)
     }
     
     func test_1_2_3을_넣어주고_dequeue를_실행하면_1이_나와야한다() {
         //given
-        sut?.enqueue(data: 1)
-        sut?.enqueue(data: 2)
-        sut?.enqueue(data: 3)
+        sut.enqueue(data: 1)
+        sut.enqueue(data: 2)
+        sut.enqueue(data: 3)
         //when
-        let firstElement = sut?.dequeue()
+        let firstElement = sut.dequeue()
         //then
         XCTAssertEqual(firstElement, 1)
     }
     
     func test_1_2_3을_입력_후_removeAll을_하면_isEmpty값이_true여야한다() {
         //given
-        sut?.enqueue(data: 1)
-        sut?.enqueue(data: 2)
-        sut?.enqueue(data: 3)
+        sut.enqueue(data: 1)
+        sut.enqueue(data: 2)
+        sut.enqueue(data: 3)
         //when
-        sut?.removeAll()
+        sut.removeAll()
         //then
-        XCTAssertTrue(sut!.isEmpty)
+        XCTAssertTrue(sut.isEmpty)
     }
 }
