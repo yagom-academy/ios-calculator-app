c776d0c85fed4c241e53f7934ad43e0604f2ebbf
gjrjs31@naver.com <gjrjs31@naver.com>
2022-05-19 18:37:36 +0900

    feat:  Formula 생성
    struct 타입으로 Formula 생성

---
+++ Calculator/Calculator/Model/Calculator/Formula.swift
@@ -0,0 +1,17 @@
+//
+//  Formula.swift
+//  Calculator
+//
+//  Created by 허건 on 2022/05/19.
+//
+
+struct Formula {
+    var operands: CalculatorItemQueue<Any>
+    var operators: CalculatorItemQueue<Any>
+    
+    func result() -> Double {
+        var resultValue: Double?
+        
+        return resultValue!
+    }
+}
