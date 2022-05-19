d1256d2842a54d01988e8cee9484e3342f584b31
seohyeon2 <seo_hyeon418@naver.com>
2022-05-19 23:08:04 +0900

    docs: UML 코드 작성을 위한 Operator, ExpressionParser, Doble, String, Formula 파일 생성

---
+++ Calculator/Calculator/Model/Formula.swift
@@ -0,0 +1,8 @@
+struct Formula {
+    var operands: CalculateItem
+    var operators: CalculateItem
+    
+    func result() -> Double {
+        return 0.0
+    }
+}
---
+++ Calculator/Calculator/Model/Operator.swift
@@ -0,0 +1,26 @@
+enum Operator: Character, CaseIterable {
+    case add = "+"
+    case subtract = "-"
+    case divide = "/"
+    case multiply = "*"
+    
+    func calculate(lhs: Double, rhs: Double) -> Double {
+        return 0.0
+    }
+    
+    func add(lhs: Double, rhs: Double) -> Double {
+        return 0.0
+    }
+    
+    func subtract(lhs: Double, rhs: Double) -> Double {
+        return 0.0
+    }
+    
+    func divide(lhs: Double, rhs: Double) -> Double {
+        return 0.0
+    }
+    
+    func multiply(lhs: Double, rhs: Double) -> Double {
+        return 0.0
+    }
+}
