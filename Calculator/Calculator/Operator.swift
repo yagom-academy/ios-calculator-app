556d9a119ca103ff15957e9d5b3935be1a2891a4
unchain123 <unchain123@kakao.com>
2022-05-19 23:05:10 +0900

    feat: make enum case

--- Calculator/Calculator/Operator.swift
+++ Calculator/Calculator/Operator.swift
@@ -2,7 +2,43 @@
 //  Operator.swift
 //  Calculator
 //
-//  Created by 오경식 on 2022/05/19.
+//  Created by unchain123 on 2022/05/19.
 //
 
 import Foundation
+
+enum Operator: Character, CaseIterable, CalculateItem {
+    case add = "+"
+    case subtract = "-"
+    case divide = "/"
+    case multiply = "*"
+    
+    func calculate(lhs: Double, rhs: Double) -> Double {
+        switch self {
+        case .add:
+            return add(lhs: lhs, rhs: rhs)
+        case .subtract:
+            return subtract(lhs: lhs, rhs: rhs)
+        case .divide:
+            return divide(lhs: lhs, rhs: rhs)
+        case .multiply:
+            return multiply(lhs: lhs, rhs: rhs)
+        }
+    }
+    
+    func add(lhs: Double, rhs: Double) -> Double {
+        return  lhs+rhs
+    }
+    
+    func subtract(lhs: Double, rhs: Double) -> Double {
+        return lhs-rhs
+    }
+    
+    func divide(lhs: Double, rhs: Double) -> Double {
+        return lhs/rhs
+    }
+    
+    func multiply(lhs: Double, rhs: Double) -> Double {
+        return lhs*rhs
+    }
+}
