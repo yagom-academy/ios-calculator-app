3bd0540f75aaf144ec57b19f793cf603ee576e4c
yusw10 <yusw10@naver.com>
2022-05-19 16:30:20 +0900

    feat: Add Operator Enum
    - add Operator case (+,-,/,*)
    - add Operator private method for Operator.calculate fuinction
    - add calculate fuinction of Operator

--- Calculator/Calculator/CalculatorQueue/Operator.swift
+++ Calculator/Calculator/CalculatorQueue/Operator.swift
@@ -6,23 +6,32 @@
 //
 enum Operator: Character, CaseIterable {
     case add = "+"
-    case substractk = "-"
+    case substract = "-"
     case divide = "/"
     case multiply = "*"
     
     func calculate(lhs: Double, rhs: Double) -> Double {
-        return lhs + rhs
+        switch self {
+        case .add:
+            return self.add(lhs: lhs, rhs: rhs)
+        case .substract:
+            return self.substract(lhs: lhs, rhs: rhs)
+        case .divide:
+            return self.divide(lhs: lhs, rhs: rhs)
+        case .multiply:
+            return self.multiply(lhs: lhs, rhs: rhs)
+        }
     }
     private func add(lhs: Double, rhs: Double) -> Double {
-        return Double()
+        return lhs + rhs
     }
     private func substract(lhs: Double, rhs: Double) -> Double {
-        return Double()
+        return lhs - rhs
     }
     private func divide(lhs: Double, rhs: Double) -> Double {
-        return Double()
+        return lhs / rhs
     }
     private func multiply(lhs: Double, rhs: Double) -> Double {
-        return Double()
+        return lhs * rhs
     }
 }
---
+++ Calculator/CalculatorTests/OperatorTests.swift
@@ -0,0 +1,69 @@
+//
+//  OperatorTests.swift
+//  CalculatorTests
+//
+//  Created by 유한석 on 2022/05/18.
+//
+
+import XCTest
+
+class OperatorTests: XCTestCase {
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+    }
+
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+    }
+    
+//MARK: - Operator.calculate
+    func test_calculate메서드_덧셈테스트_15더하기9는_24() {
+        //given
+        let testLhs = 15.0
+        let testRhs = 9.0
+        let add = Operator.add
+        
+        //when
+        let result = add.calculate(lhs: testLhs, rhs: testRhs)
+        
+        //then
+        XCTAssertEqual(result, 24)
+    }
+    
+    func test_calculate메서드_뺄셈테스트() {
+        //given
+        let testLhs = 15.0
+        let testRhs = 9.0
+        let add = Operator.substract
+        
+        //when
+        let result = add.calculate(lhs: testLhs, rhs: testRhs)
+        
+        //then
+        XCTAssertEqual(result, 6)
+    }
+    func test_calculate메서드_나눗셈테스트() {
+        //given
+        let testLhs = 15.0
+        let testRhs = 3.0
+        let add = Operator.divide
+        
+        //when
+        let result = add.calculate(lhs: testLhs, rhs: testRhs)
+        
+        //then
+        XCTAssertEqual(result, 5.0)
+    }
+    func test_calculate메서드_곱셈테스트() {
+        //given
+        let testLhs = 15.0
+        let testRhs = 9.0
+        let add = Operator.multiply
+        
+        //when
+        let result = add.calculate(lhs: testLhs, rhs: testRhs)
+        
+        //then
+        XCTAssertEqual(result, 135)
+    }
+}
