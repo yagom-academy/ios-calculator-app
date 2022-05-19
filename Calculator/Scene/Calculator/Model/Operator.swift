0241062f79e1c4855cc0c5cfdd742c1d2b5a104e
apwierk2451 <apwierk2451@gmail.com>
2022-05-20 00:36:46 +0900

    feat: 요구사항에 맞게 Operator 은닉화된 메서드 구현 및 계산 메서드 구현

--- Calculator/Scene/Calculator/Model/Operator.swift
+++ Calculator/Scene/Calculator/Model/Operator.swift
@@ -4,13 +4,38 @@
 //
 //  Created by 이은찬 on 2022/05/17.
 //
-enum Operator: Character, CalculateItem {
+enum Operator: Character, CalculateItem, CaseIterable {
     case add = "+"
     case subtract = "-"
     case multiply = "*"
     case devide = "/"
-}
-
-extension Double: CalculateItem {
     
+    func calculate(lhs: Double, rhs: Double) -> Double {
+        switch self {
+        case .add:
+            return add(lhs: lhs, rhs: rhs)
+        case .subtract:
+            return subtract(lhs: lhs, rhs: rhs)
+        case .multiply:
+            return multiply(lhs: lhs, rhs: rhs)
+        case .devide:
+            return devide(lhs: lhs, rhs: rhs)
+        }
+    }
+    
+    private func add(lhs: Double, rhs: Double) -> Double {
+        return lhs + rhs
+    }
+    
+    private func subtract(lhs: Double, rhs: Double) -> Double {
+        return lhs - rhs
+    }
+    
+    private func multiply(lhs: Double, rhs: Double) -> Double {
+        return lhs * rhs
+    }
+    
+    private func devide(lhs: Double, rhs: Double) -> Double {
+        return lhs / rhs
+    }
 }
