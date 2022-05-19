0dc20edd471f449cbf4e829f13d29752922f74cb
yusw10 <yusw10@naver.com>
2022-05-20 03:51:16 +0900

    test: Modify testCode because of error handling
    - modify OperatorTests code
    - modify CalculatorItemQueueTests code

--- Calculator/CalculatorTests/CalculatorItemQueueTests.swift
+++ Calculator/CalculatorTests/CalculatorItemQueueTests.swift
@@ -19,76 +19,74 @@ class CalculatorItemQueueTests: XCTestCase {
         sut = nil
     }
     
-//// MARK: - CalculatorItemQueue.count
-//    func test_queue에1개일때_큐의길이1반환() {
-//        //given
-//        sut.enQueue("3")
-//        
-//        //when
-//        let result = sut.count
-//        
-//        //then
-//        XCTAssertEqual(result, 1)
-//    }
-//    
-//    func test_queue에요소가없으면_0반환() {
-//        //when
-//        let result = sut.count
-//        
-//        //then
-//        XCTAssertEqual(result, 0)
-//    }
-//    
-//// MARK: - CalculatorItemQueue.enQueue
-//    func test_queue에_값_한개_삽입() {
-//        //given
-//        let input = "3"
-//        
-//        //when
-//        sut.enQueue(input)
-//        guard let result = sut.deQueue() else {
-//            return
-//        }
-//        
-//        //then
-//        XCTAssertEqual(result.value, "3")
-//    }
-//    
-//    func test_queue에_값_3개_삽입시_크기는3() {
-//        //given
-//        let input1 = "3"
-//        let input2 = "6"
-//        let input3 = "9"
-//        
-//        //when
-//        sut.enQueue(input1)
-//        sut.enQueue(input2)
-//        sut.enQueue(input3)
-//        let result = sut.count
-//        
-//        //then
-//        XCTAssertEqual(result, 3)
-//    }
-//    
-////MARK: - Calculator.deQueue
-//    func test_아무것도없을때_pop하면_nil() {
-//        //when
-//        let result = sut.deQueue()
-//        
-//        //then
-//        XCTAssertNil(result)
-//    }
-//    
-//    func test_값이_있을때_deQueue시_값반환() {
-//        //given
-//        sut.enQueue("3")
-//        sut.enQueue("4")
-//        sut.enQueue("6")
-//        
-//        //when
-//        let result = sut.deQueue()
-//        
-//        //then
-//        XCTAssertEqual(result?.value, "3")
-//    }
+// MARK: - CalculatorItemQueue.count
+    func test_queue에1개일때_큐의길이1반환() {
+        //given
+        sut.enQueue(3.0)
+        
+        //when
+        let result = sut.count
+        
+        //then
+        XCTAssertEqual(result, 1)
+    }
+    
+    func test_queue에요소가없으면_0반환() {
+        //when
+        let result = sut.count
+        
+        //then
+        XCTAssertEqual(result, 0)
+    }
+    
+// MARK: - CalculatorItemQueue.enQueue
+    func test_queue에_값_한개_삽입() {
+        //given
+        let input = 3.0
+        sut.enQueue(input)
+        //when
+        
+        let result = sut.count
+        
+        //then
+        XCTAssertEqual(result, 1)
+    }
+    
+    func test_queue에_값_3개_삽입시_크기는3() {
+        //given
+        let input1 = 3.0
+        let input2 = 6.0
+        let input3 = 9.0
+        
+        //when
+        sut.enQueue(input1)
+        sut.enQueue(input2)
+        sut.enQueue(input3)
+        let result = sut.count
+        
+        //then
+        XCTAssertEqual(result, 3)
+    }
+    
+//MARK: - Calculator.deQueue
+    func test_아무것도없을때_pop하면_nil() {
+        //when
+        let result = sut.deQueue()
+        
+        //then
+        XCTAssertNil(result)
+    }
+    
+    func test_값이_있을때_deQueue시_값반환() {
+        //given
+        sut.enQueue(3.0)
+        sut.enQueue(4.0)
+        sut.enQueue(6.0)
+        
+        //when
+        guard let result = sut.deQueue() as? Node<Double> else { return }
+        
+        //then
+        XCTAssertEqual(result.value, 3.0)
+    }
 }
--- Calculator/CalculatorTests/OperatorTests.swift
+++ Calculator/CalculatorTests/OperatorTests.swift
@@ -17,51 +17,51 @@ class OperatorTests: XCTestCase {
     }
     
 //MARK: - Operator.calculate
-    func test_calculate메서드_덧셈테스트_15더하기9는_24() {
+    func test_calculate메서드_덧셈테스트_15더하기9는_24() throws {
         //given
         let testLhs = 15.0
         let testRhs = 9.0
         let add = Operator.add
         
         //when
-        let result = add.calculate(lhs: testLhs, rhs: testRhs)
+        let result = try add.calculate(lhs: testLhs, rhs: testRhs)
         
         //then
         XCTAssertEqual(result, 24)
     }
     
-    func test_calculate메서드_뺄셈테스트() {
+    func test_calculate메서드_뺄셈테스트() throws {
         //given
         let testLhs = 15.0
         let testRhs = 9.0
         let add = Operator.substract
         
         //when
-        let result = add.calculate(lhs: testLhs, rhs: testRhs)
+        let result = try add.calculate(lhs: testLhs, rhs: testRhs)
         
         //then
         XCTAssertEqual(result, 6)
     }
-    func test_calculate메서드_나눗셈테스트() {
+    func test_calculate메서드_나눗셈테스트() throws {
         //given
         let testLhs = 15.0
         let testRhs = 3.0
         let add = Operator.divide
         
         //when
-        let result = add.calculate(lhs: testLhs, rhs: testRhs)
+        let result = try add.calculate(lhs: testLhs, rhs: testRhs)
         
         //then
         XCTAssertEqual(result, 5.0)
     }
-    func test_calculate메서드_곱셈테스트() {
+    func test_calculate메서드_곱셈테스트() throws{
         //given
         let testLhs = 15.0
         let testRhs = 9.0
         let add = Operator.multiply
         
         //when
-        let result = add.calculate(lhs: testLhs, rhs: testRhs)
+        let result = try add.calculate(lhs: testLhs, rhs: testRhs)
         
         //then
         XCTAssertEqual(result, 135)
