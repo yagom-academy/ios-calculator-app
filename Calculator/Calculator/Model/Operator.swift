60fb15226e3964dacfefd6783b9d9f3f596c353e
Kiwi <toskw@naver.com>
2022-05-19 21:42:33 +0900

    feat: 연산자 열거형 구현

---
+++ Calculator/Calculator/Model/Operator.swift
@@ -0,0 +1,46 @@
+//
+//  Operator.swift
+//  Calculator
+//
+//  Created by Kiwi on 2022/05/19.
+//
+
+import Foundation
+
+enum Operator: Character, CaseIterable {
+    case add = "+"
+    case substract = "-"
+    case divide = "÷"
+    case multiply = "×"
+    
+    func calculate(lhs: Double, rhs: Double) -> Double {
+        switch self {
+        case .add:
+            return add(lhs: lhs, rhs: rhs)
+        case .substract:
+            return substract(lhs: lhs, rhs: rhs)
+        case .divide:
+            return divide(lhs: lhs, rhs: rhs)
+        case .multiply:
+            return multiply(lhs: lhs, rhs: rhs)
+        }
+    }
+    
+    private func add(lhs: Double, rhs: Double) -> Double {
+        return lhs + rhs
+    }
+    
+    private func substract(lhs: Double, rhs: Double) -> Double {
+        return lhs - rhs
+    }
+    
+    private func divide(lhs: Double, rhs: Double) -> Double {
+        return lhs / rhs
+    }
+    
+    private func multiply(lhs: Double, rhs: Double) -> Double {
+        return lhs * rhs
+    }
+}
+
+
