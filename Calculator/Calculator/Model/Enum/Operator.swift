95a8f39e73ae99e654898956c904d6181da929f0
bard <schy2503@naver.com>
2022-05-19 14:38:55 +0900

    refactor: Operator 내 함수 내부 구현

--- Calculator/Calculator/Model/Enum/Operator.swift
+++ Calculator/Calculator/Model/Enum/Operator.swift
@@ -12,22 +12,31 @@ enum Operator: Character, CaseIterable {
     case multiply = "*"
     
     func calculate(lhs: Double, rhs: Double) -> Double {
-        return 0.0
+        switch self {
+        case .add:
+            return add(lhs: lhs, rhs: rhs)
+        case .subtract:
+            return subtract(lhs: lhs, rhs: rhs)
+        case .divide:
+            return divide(lhs: lhs, rhs: rhs)
+        case .multiply:
+            return multiply(lhs: lhs, rhs: rhs)
+        }
     }
     
     func add(lhs: Double, rhs: Double) -> Double {
-        return 0.0
+        return lhs + rhs
     }
     
     func subtract(lhs: Double, rhs: Double) -> Double {
-        return 0.0
+        return lhs - rhs
     }
     
     func divide(lhs: Double, rhs: Double) -> Double {
-        return 0.0
+        return lhs / rhs
     }
     
     func multiply(lhs: Double, rhs: Double) -> Double {
-        return 0.0
+        return lhs * rhs
     }
 }
