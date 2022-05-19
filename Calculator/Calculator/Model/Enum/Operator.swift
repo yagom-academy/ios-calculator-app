641bf1c5ac7992f16636870ecb37ce710dd60800
gjrjs31@naver.com <gjrjs31@naver.com>
2022-05-19 18:47:52 +0900

    feat: Operator enum 생성

--- Calculator/Calculator/Model/Enum/Operator.swift
+++ Calculator/Calculator/Model/Enum/Operator.swift
@@ -5,9 +5,29 @@
 //  Created by 허건 on 2022/05/16.
 //
 
-enum Operator: String {
-    case addition = "+"
-    case subtraction = "-"
-    case multiplication = "*"
-    case division = "/"
+enum Operator: Character, CaseIterable {
+    case add = "+"
+    case subtract = "-"
+    case divide = "/"
+    case multiply = "*"
+    
+    func calculate(Ihs: Double, rhs: Double) -> Double {
+        
+    }
+    
+    private func add(Ihs: Double, rhs: Double) -> Double {
+        
+    }
+    
+    private func subtract(Ihs: Double, rhs: Double) -> Double {
+        
+    }
+    
+    private func divide(Ihs: Double, rhs: Double) -> Double {
+        
+    }
+    
+    private func multiply(Ihs: Double, rhs: Double) -> Double {
+        
+    }
 }
