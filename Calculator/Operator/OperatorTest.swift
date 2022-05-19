cbece41540207446c32f3a6f561efe2fd8bf8012
Kiwi <toskw@naver.com>
2022-05-19 23:00:39 +0900

    feat: 빼기 연산자 테스트

--- Calculator/Operator/OperatorTest.swift
+++ Calculator/Operator/OperatorTest.swift
@@ -29,5 +29,15 @@ class OperatorTest: XCTestCase {
         XCTAssertEqual(result, 3)
     }
     
+    func test_두수를_subtract로_연산했을때_빼기가_되어야한다() {
+        //given
+        sut = .substract
+        
+        //when
+        let result = sut.calculate(lhs: 2, rhs: 1)
+        
+        //then
+        XCTAssertEqual(result, 1)
+    }
     
 }
