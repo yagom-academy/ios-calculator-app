47fef646457c5fc8ff6fdd3dc4d07c2970648f1e
bard <schy2503@naver.com>
2022-05-19 15:34:27 +0900

    refactor: UML에 따른 접근제한자 설정

--- Calculator/Calculator/Model/Enum/Operator.swift
+++ Calculator/Calculator/Model/Enum/Operator.swift
@@ -24,19 +24,19 @@ enum Operator: Character, CaseIterable {
         }
     }
     
-    func add(lhs: Double, rhs: Double) -> Double {
+    private func add(lhs: Double, rhs: Double) -> Double {
         return lhs + rhs
     }
     
-    func subtract(lhs: Double, rhs: Double) -> Double {
+    private func subtract(lhs: Double, rhs: Double) -> Double {
         return lhs - rhs
     }
     
-    func divide(lhs: Double, rhs: Double) -> Double {
+    private func divide(lhs: Double, rhs: Double) -> Double {
         return lhs / rhs
     }
     
-    func multiply(lhs: Double, rhs: Double) -> Double {
+    private func multiply(lhs: Double, rhs: Double) -> Double {
         return lhs * rhs
     }
 }
