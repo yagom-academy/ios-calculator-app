441c0981de9c89337979996898dfae42d89583d7
derrickkim0109 <derrickkim0109@naver.com>
2022-05-19 14:47:38 +0900

    chore : 요구사항에서 불필요한 부분 제거

--- Calculator/Calculator/CalculaterQueue/CalculateItemProtocol.swift
+++ Calculator/Calculator/CalculaterQueue/CalculateItemProtocol.swift
@@ -8,20 +8,5 @@
 import Foundation
 
 protocol CalculateItemProtocol {
-    associatedtype Element
     
-    /// 큐 끝에 새로운 큐를 추가하고 성공여부 반환
-    mutating func enQueue(_ element: Element) -> Bool
-
-    /// 가장 먼저 들어온 앞의 큐 제거하고 제거한 큐 반환
-    mutating func deQueue() throws -> Element
-    
-    /// 큐가 비우는 함수
-    mutating func removeAll() throws
-    
-    /// 큐가 비어있는지 알려주는 읽기전용 프로퍼티
-    var isEmpty: Bool { get }
-    
-    /// 큐의 제일 처음 값을 가짐
-    var peek: Element? { get }
 }
