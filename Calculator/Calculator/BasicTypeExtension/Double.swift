3203ba0a18fdb61a545da4b3f0bb878d2d66401d
yusw10 <yusw10@naver.com>
2022-05-19 22:38:59 +0900

    feat: Add extension Double, String
    - add Double for inherit CalculatorItem
    - add extention for override split method

---
+++ Calculator/Calculator/BasicTypeExtension/Double.swift
@@ -0,0 +1,9 @@
+//
+//  Double.swift
+//  Calculator
+//
+//  Created by 유한석 on 2022/05/19.
+//
+extension Double: CalculatorItem {
+    
+}
---
+++ Calculator/Calculator/BasicTypeExtension/String.swift
@@ -0,0 +1,11 @@
+//
+//  String.swift
+//  Calculator
+//
+//  Created by 유한석 on 2022/05/19.
+//
+extension String: CalculatorItem {
+    func split(with target: Character) -> [String] {
+        return [String()]
+    }
+}
