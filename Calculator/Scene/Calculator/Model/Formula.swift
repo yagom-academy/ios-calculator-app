369042d151b75e4fa98701361a975a156e4d55bd
apwierk2451 <apwierk2451@gmail.com>
2022-05-20 00:38:15 +0900

    feat: Formular 구조체 생성
    1. operands 는 Double로 operators는 Operator 타입으로 프로퍼티 생성
    2. result()에 결과 값 계산 메서드 구현

--- Calculator/Scene/Calculator/Model/Formula.swift
+++ Calculator/Scene/Calculator/Model/Formula.swift
@@ -4,5 +4,20 @@
 //
 //  Created by 이은찬 on 2022/05/19.
 //
-
-import Foundation
+struct Formula {
+    var operands = Queue<Double>()
+    var operators = Queue<Operator>()
+    
+    func result() -> Double {
+        var lhs: Double = 0.0
+        while operands.isEmpty == false {
+            if lhs == 0.0 {
+                lhs = operands.dequeue() ?? 0.0
+            }
+            let rhs = operands.dequeue() ?? 0.0
+            let `operator` = operators.dequeue()
+            lhs = (`operator`?.calculate(lhs: lhs, rhs: rhs)) ?? 0.0
+        }
+        return lhs
+    }
+}
