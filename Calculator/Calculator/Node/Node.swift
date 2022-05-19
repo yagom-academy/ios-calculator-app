4fe342aa788fef0185eab96b5eb3cdff008740ff
seohyeon2 <seo_hyeon418@naver.com>
2022-05-19 15:34:27 +0900

    feat: Node에 prev 변수를 추가하여 양방향 링크드 리스트로 구현

--- Calculator/Calculator/Node/Node.swift
+++ Calculator/Calculator/Node/Node.swift
@@ -1,9 +1,11 @@
 class Node<T> {
     var data: T
     var next: Node?
+    var prev: Node<T>?
     
-    init(data: T, next: Node? = nil) {
+    init(data: T, next: Node? = nil, prev: Node? = nil) {
         self.data = data
         self.next = next
+        self.prev = prev
     }
 }
