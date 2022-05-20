64a217b6a6cb8f1fba653129fd8db31f444ae0c9
derrickkim0109 <derrickkim0109@naver.com>
2022-05-20 16:58:23 +0900

    feat : "Formula" 기능 구현

---
+++ Calculator/Calculator/Model/Fomular/Formula.swift
@@ -0,0 +1,37 @@
+//
+//  Fomular.swift
+//  Calculator
+//
+//  Created by Derrick kim on 2022/05/18.
+//
+
+import Foundation
+
+struct Formula {
+    var operands: CalculatorItemQueue<Double>
+    var operators: CalculatorItemQueue<Operator>
+}
+
+extension Formula {
+    func result() -> Double {
+        return isCalculatedValue
+    }
+}
+
+extension Formula {
+    private var isCalculatedValue: Double {
+        do {
+            var headOperand = try operands.dequeue()
+            
+            while !operands.isEmpty {
+                let operand = try operands.dequeue()
+                let arithmeticOperator = try operators.dequeue()
+                
+                headOperand = try arithmeticOperator.calculate(lhs: headOperand, rhs: operand)
+            }
+            return headOperand
+        } catch _ {
+            return .nan
+        }
+    }
+}
