770fc982a6b29f873acaf3ea3f41eab02e7c5e4a
ic_6_user_28 <toskw@naver.com>
2022-05-20 13:12:34 +0900

    feat: Formula 구조체 생성

---
+++ Calculator/Calculator/Model/Formula.swift
@@ -0,0 +1,13 @@
+//
+//  Formula.swift
+//  Calculator
+//
+//  Created by Kiwi on 2022/05/20.
+//
+
+struct Formula {
+    var operands: CalculatorItemQueue<Double>
+    var operators: CalculatorItemQueue<Operator>
+    
+    
+}
