06e539ee14b5bc82855698f05be4508333582239
BaekGom <jihunmm02@gmail.com>
2022-05-20 19:34:54 +0900

    feat: Formula 구현

--- Calculator/Calculator/Formula.swift
+++ Calculator/Calculator/Formula.swift
@@ -7,4 +7,16 @@
 
 import Foundation
 
-
+struct Formula {
+    var operands: CalculatorItemQueue<Double>
+    var operators: CalculatorItemQueue<Operator>
+    
+    mutating func result() -> Double {
+        var result = operands.dequeue() ?? 0
+        
+        while let operands = operands.dequeue(), let operators = operators.dequeue() {
+            result = operators.calculate(lhs: result, rhs: operands)
+        }
+        return result
+    }
+}
