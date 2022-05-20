30dc9c1af5044e78d1bbf40aeebfcea3b3c047e0
Hugh-github <tnghk4822@gmail.com>
2022-05-20 17:21:21 +0900

    feat: Formula 타입 생성
    - property(operands, operators) 생성

---
+++ Calculator/Calculator/Model/Formula.swift
@@ -0,0 +1,11 @@
+//
+//  Formula.swift
+//  Calculator
+//
+//  Created by dhoney96 on 2022/05/20.
+//
+
+struct Formula {
+    var operands: CalculatorItemQueue<Double>
+    var operators: CalculatorItemQueue<String>
+}
