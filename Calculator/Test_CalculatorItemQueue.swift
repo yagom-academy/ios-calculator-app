430d8737a1fc0a6b403101f34efe4afdcf956707
gjrjs31@naver.com <gjrjs31@naver.com>
2022-05-20 21:22:34 +0900

    feat: TDD enqueue추가
    Test_CalculatorItemQueue TDD enqueue func 추가

---
+++ Calculator/Test_CalculatorItemQueue.swift
@@ -0,0 +1,8 @@
+//
+//  Test_CalculatorItemQueue.swift
+//  Calculator
+//
+//  Created by 허건 on 2022/05/20.
+//
+
+import Foundation
---
+++ Calculator/Test_CalculatorItemQueue/Test_CalculatorItemQueue.swift
@@ -0,0 +1,38 @@
+//
+//  Test_CalculatorItemQueue.swift
+//  Test_CalculatorItemQueue
+//
+//  Created by 허건 on 2022/05/20.
+//
+
+import XCTest
+@testable import Calculator
+
+class Test_CalculatorItemQueue: XCTestCase {
+    var sut: CalculatorItemQueue<[Int]>!
+    
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        sut = CalculatorItemQueue()
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
+    }
+    
+    func test_enqueue_실행시_123456_를넣었을때_123456_를반환하는지() {
+        // given
+        let input: [Int] = [1, 2, 3, 4, 5, 6]
+        let output: [Int] = [1]
+        
+        // when
+        input.forEach {
+            sut.enqueue([$0])
+        }
+        let result = sut.linkedList.head?.data
+        
+        // then
+        XCTAssertEqual(result, output)
+    }
+}
