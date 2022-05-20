b360ed9d572d22ac0db85a0b23d27013fa0d6af3
KayAhnDS <dkseotn123@gmail.com>
2022-05-20 15:10:21 +0900

    refactor: Formula 구조체 구현

--- Calculator/Calculator/Model/Formula.swift
+++ Calculator/Calculator/Model/Formula.swift
@@ -5,4 +5,33 @@
 //  Created by Kay on 2022/05/20.
 //
 
-import Foundation
+struct Formula {
+    var operands: CalculatorItemQueue<String>
+    var operators: CalculatorItemQueue<String>
+
+    mutating func result() -> Double {
+        var operandsNumberInDoubleFormat = Double(operands.dequeue() ?? "Wrong") ?? 0.0
+        var calculationResult: Double = operandsNumberInDoubleFormat
+        
+        while operators.count != 0 {
+            let operatorLetter = operators.dequeue() ?? "Error_Handling"
+            let currentOperator = Character(operatorLetter)
+
+            operandsNumberInDoubleFormat = Double(operands.dequeue() ?? "Wrong") ?? 0.0
+            
+            switch currentOperator {
+            case "+":
+                calculationResult = Operator.add.calculate(lhs: calculationResult, rhs: operandsNumberInDoubleFormat)
+            case "-":
+                calculationResult = Operator.subtract.calculate(lhs: calculationResult, rhs: operandsNumberInDoubleFormat)
+            case "*":
+                calculationResult = Operator.multiply.calculate(lhs: calculationResult, rhs: operandsNumberInDoubleFormat)
+            case "/":
+                calculationResult = Operator.divide.calculate(lhs: calculationResult, rhs: operandsNumberInDoubleFormat)
+            default:
+                calculationResult = 0.0
+            }
+        }
+        return calculationResult
+    }
+}
