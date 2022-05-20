a24546546bce5b3a967d93309765c6a6d5dba985
ic_6_user_28 <toskw@naver.com>
2022-05-20 17:10:28 +0900

    feat: result함수 구현 및 테스트

--- Calculator/Calculator/Model/Formula.swift
+++ Calculator/Calculator/Model/Formula.swift
@@ -6,8 +6,20 @@
 //
 
 struct Formula {
-    var operands: CalculatorItemQueue<Double>
-    var operators: CalculatorItemQueue<Operator>
+    var operands = CalculatorItemQueue<Double>()
+    var operators = CalculatorItemQueue<Operator>()
     
-
+    mutating func result() throws -> Double {
+        guard var result = operands.dequeue() else { throw OperatorError.emptyList }
+        
+        while operands.isEmpty == false {
+            if let number = operands.dequeue(),
+               let operatorSign = operators.dequeue() {
+                result = try operatorSign.calculate(lhs: result, rhs: number)
+            } else {
+                throw OperatorError.emptyList
+            }
+        }
+        return result
+    }
 }
---
+++ Calculator/FormulaTest/FormulaTest.swift
@@ -0,0 +1,33 @@
+//
+//  FormulaTest.swift
+//  FormulaTest
+//
+//  Created by Kiwon Song on 2022/05/20.
+//
+
+import XCTest
+@testable import Calculator
+class FormulaTest: XCTestCase {
+    var sut: Formula!
+    override func setUpWithError() throws {
+        sut = Formula()
+    }
+
+    override func tearDownWithError() throws {
+        sut = nil
+    }
+    
+    func test_숫자를_집어넣었을때_연산이_정상적으로_작동하는지() {
+        //given
+        sut.operands.enqueue(data: 1.0)
+        sut.operands.enqueue(data: 2.0)
+        sut.operands.enqueue(data: 3.0)
+        
+        sut.operators.enqueue(data: .add)
+        sut.operators.enqueue(data: .substract)
+        //when
+        let result = try? sut.result()
+        //then
+        XCTAssertEqual(result, 0)
+    }
+}
