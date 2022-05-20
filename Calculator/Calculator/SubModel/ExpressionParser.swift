489bbce5c76ec26f0aea915fc4a7cac101698e87
KayAhnDS <dkseotn123@gmail.com>
2022-05-20 15:08:39 +0900

    refactor: ExpressionParser파일 내부 구현

--- Calculator/Calculator/SubModel/ExpressionParser.swift
+++ Calculator/Calculator/SubModel/ExpressionParser.swift
@@ -5,4 +5,26 @@
 //  Created by Kay on 2022/05/20.
 //
 
-import Foundation
+enum ExpressionParser {
+    static func parse(from input: String) -> Formula {
+        var retrievedOperators = CalculatorItemQueue<String>()
+        var retrievedOperands = CalculatorItemQueue<String>()
+
+        var operators = componentsByOperators(from: input)
+        var operands = input.split { ["+", "-", "*", "/"].contains(String($0)) }
+
+        while !(operators.isEmpty && operands.isEmpty) {
+            if !operators.isEmpty {
+                retrievedOperators.enqueue(operators.removeFirst())
+            }
+            if !operands.isEmpty {
+                retrievedOperands.enqueue(String(operands.removeFirst()))
+            }
+        }
+        return Formula(operands: retrievedOperands, operators: retrievedOperators)
+    }
+    
+    private static func componentsByOperators(from input: String) -> [String] {
+        return input.filter {$0 == "+" || $0 == "-" || $0 == "/" || $0 == "*" }.map { String($0) }
+    }
+}
