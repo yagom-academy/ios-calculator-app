3413a580daa85b0f87fc67c16322c3c4abd9d4f2
KayAhnDS <dkseotn123@gmail.com>
2022-05-20 15:07:22 +0900

    refactor: SplitExtension 및 DoubleExtension 파일 수정

--- Calculator/Calculator/SubModel/DoubleExtension.swift
+++ Calculator/Calculator/SubModel/DoubleExtension.swift
@@ -5,4 +5,6 @@
 //  Created by Kay on 2022/05/20.
 //
 
-import Foundation
+extension Double {
+    
+}
--- Calculator/Calculator/SubModel/SplitExtension.swift
+++ Calculator/Calculator/SubModel/SplitExtension.swift
@@ -5,4 +5,8 @@
 //  Created by Kay on 2022/05/20.
 //
 
-import Foundation
+extension String {
+    func split(with target: Character) -> [String] {
+        return self.split(separator: target).map{ String($0) }
+    }
+}
