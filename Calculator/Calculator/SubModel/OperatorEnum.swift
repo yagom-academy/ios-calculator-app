2755da636a3e94ccbb1ad57744197af99c72c435
KayAhnDS <dkseotn123@gmail.com>
2022-05-20 15:06:13 +0900

    refactor: OperatorEnum파일 내부 구현 추가

--- Calculator/Calculator/SubModel/OperatorEnum.swift
+++ Calculator/Calculator/SubModel/OperatorEnum.swift
@@ -6,3 +6,52 @@
 //
 
 import Foundation
+
+enum Operator: CalculateItem, CaseIterable {
+    case add
+    case subtract
+    case divide
+    case multiply
+    
+    var ownValue: Character {
+        switch self {
+        case .add:
+            return "+"
+        case .subtract:
+            return "-"
+        case .divide:
+            return "/"
+        case .multiply:
+            return "*"
+        }
+    }
+
+    public func calculate(lhs: Double, rhs: Double) -> Double {
+        switch self {
+        case .add:
+            return add(lhs: lhs, rhs: rhs)
+        case .subtract:
+            return subtract(lhs: lhs, rhs: rhs)
+        case .divide:
+            return divide(lhs: lhs, rhs: rhs)
+        case .multiply:
+            return multiply(lhs: lhs, rhs: rhs)
+        }
+    }
+
+    private func add(lhs: Double, rhs: Double) -> Double {
+        return lhs + rhs
+    }
+
+    private func subtract(lhs: Double, rhs: Double) -> Double {
+        return lhs - rhs
+    }
+
+    private func divide(lhs: Double, rhs: Double) -> Double {
+        return lhs / rhs
+    }
+
+    private func multiply(lhs: Double, rhs: Double) -> Double {
+        return lhs * rhs
+    }
+}
