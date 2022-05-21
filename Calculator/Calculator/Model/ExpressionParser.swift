9c621ed3c73441b456a574695da47ed961449393
yeeton37 <lyeeun37@naver.com>
2022-05-21 16:14:23 +0900

    test: ExpressionParser 메서드(parse) tdd 작성

--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -8,6 +8,24 @@
 import Foundation
 
 enum ExpressionParser {
+    static func parse(from input: String) -> Formula {
+        let separatedInputValue = componentsByOperators(from: input)
+        var operandQueue = CalculatorItemQueue<Double>()
+        var operatorQueue = CalculatorItemQueue<Operator>()
+        
+        separatedInputValue.forEach {
+            if let doubleTypeValue = Double($0) {
+                operandQueue.enqueue(data: doubleTypeValue)
+                return
+            }
+            
+            if let operaterTypeValue = Operator(rawValue: Character($0)) {
+                operatorQueue.enqueue(data: operaterTypeValue)
+            }
+        }
+        return Formula(operands: operandQueue, operators: operatorQueue)
+    }
+    
     private static func componentsByOperators(from input: String) -> [String] {
         return input.split(with: " ")
     }
--- Calculator/MyCalculatorTests/ExpressionParserTests.swift
+++ Calculator/MyCalculatorTests/ExpressionParserTests.swift
@@ -5,4 +5,47 @@
 //  Created by 이예은 on 2022/05/20.
 //
 
-import Foundation
+import XCTest
+@testable import Calculator
+
+class ExpressionParserTests: XCTestCase {
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+    }
+    
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+    }
+    
+    func test_ExpressionParser에서_input으로_값을넣으면_operands와operators가_올바르게나온다_1() {
+        // given
+        let input = "34 + 5 + 7"
+        
+        // when
+        var formula = ExpressionParser.parse(from: input)
+        // then
+        XCTAssertEqual(try formula.result(), 46)
+    }
+    
+    func test_ExpressionParser에서_input으로_값을넣으면_operands와operators가_올바르게나온다_2() throws {
+        // given
+        let input = "30 / 1 + 7"
+        
+        // when
+        var formula = ExpressionParser.parse(from: input)
+        
+        // then
+        XCTAssertEqual(try formula.result(), 37)
+    }
+    
+    func test_ExpressionParser에서_input으로_음수값을넣어도_operands와operators가_올바르게나온다() throws {
+        // given
+        let input = "30 / 1 + -7"
+        
+        // when
+        var formula = ExpressionParser.parse(from: input)
+        
+        // then
+        XCTAssertEqual(try formula.result(), 23)
+    }
+}
