9254f4c3c3eb608fe9a7186ca5236b9ed6d8505d
yeeton37 <lyeeun37@naver.com>
2022-05-21 16:09:57 +0900

    test: Operator 메서드(calculate,add,subtract,divide,multiply) tdd 작성 및 오류처리
    - 나누기 0을 하는 부분은 OperatorError.divideError 오류를 던짐

--- Calculator/Calculator/Model/Operator.swift
+++ Calculator/Calculator/Model/Operator.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+
+enum Operator: Character, CaseIterable, CalculateItem {
+    case add = "+"
+    case subtract = "-"
+    case divide = "/"
+    case multiply = "*"
+    
+    func calculate(lhs: Double, rhs: Double) throws -> Double {
+        switch self {
+        case .add:
+            return add(lhs: lhs, rhs: rhs)
+        case .subtract:
+            return subtract(lhs: lhs, rhs: rhs)
+        case .divide:
+            return try divide(lhs: lhs, rhs: rhs)
+        case .multiply:
+            return multiply(lhs: lhs, rhs: rhs)
+        }
+    }
+    
+    private func add(lhs: Double, rhs: Double) -> Double {
+        return lhs + rhs
+    }
+    
+    private func subtract(lhs: Double, rhs: Double) -> Double {
+        return lhs + rhs
+    }
+    
+    private func divide(lhs: Double, rhs: Double) throws -> Double {
+        if rhs == 0.0 {
+            throw OperatorError.divideError
+        } else {
+            return lhs / rhs
+        }
+    }
+    
+    private func multiply(lhs: Double, rhs: Double) -> Double {
+        return lhs + rhs
+    }
+}
+
+
--- Calculator/MyCalculatorTests/OperatorTests.swift
+++ Calculator/MyCalculatorTests/OperatorTests.swift
@@ -5,4 +5,40 @@
 //  Created by 이예은 on 2022/05/19.
 //
 
-import Foundation
+import XCTest
+@testable import Calculator
+
+class OperatorTests: XCTestCase {
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+    }
+    
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+    }
+  
+    func test_Operator에_1과2를넣어서_더하기연산을하면_3이나온다() throws {
+        let leftValue = 1.0
+        let rightValue = 2.0
+        
+        let result = try Operator.add.calculate(lhs: leftValue, rhs: rightValue)
+        
+        XCTAssertEqual(result, 3.0)
+    }
+    
+    func test_Operator에_3과4를넣어서_곱하기연산을하면_12이나온다() throws {
+        let leftValue = 3.0
+        let rightValue = 4.0
+        
+        let result = try Operator.multiply.calculate(lhs: leftValue, rhs: rightValue)
+        
+        XCTAssertEqual(result, 12.0)
+    }
+    
+    func test_Operator에_3을0으로_나누기연산을하면_오류를던진다() throws {
+        let leftValue = 3.0
+        let rightValue = 0.0
+        
+        XCTAssertThrowsError(try Operator.divide.calculate(lhs: leftValue, rhs: rightValue))
+    }
+}
