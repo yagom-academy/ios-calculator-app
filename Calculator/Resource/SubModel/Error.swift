45bc951149ab74c206747472a1c38b30c45491ff
KayAhnDS <dkseotn123@gmail.com>
2022-05-23 02:57:06 +0900

    feat: error enum 추가

---
+++ Calculator/Resource/SubModel/Error.swift
@@ -0,0 +1,13 @@
+//
+//  Error.swift
+//  Calculator
+//
+//  Created by Kay on 2022/05/23.
+//
+
+import Foundation
+
+enum valueError: Error {
+    case operandEmptyError
+    case operatorEmptyError
+}
