fa0578f7d740acd524018bea03a299d6d0a2637b
unchain123 <unchain123@kakao.com>
2022-05-23 00:10:13 +0900

    feat: ExpressionPaserTest file
    add test_parse에_정상적인_식이_들어왔을_때_계산을_해주는지

---
+++ Calculator/ExpressionPaserTest/ExpressionPaserTest.swift
@@ -0,0 +1,33 @@
+//
+//  ExpressionPaserTest.swift
+//  ExpressionPaserTest
+//
+//  Created by 오경식 on 2022/05/22.
+//
+
+import XCTest
+
+@testable import Calculator
+
+class ExpressionPaserTest: XCTestCase {
+
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+    }
+
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+    }
+    
+    func test_parse에_정상적인_식이_들어왔을_때_계산을_해주는지() {
+        //given
+        let inputExpresion = "1 + 2 * -3"
+        
+        // when
+        let result = ExpressionParser.parse(from: inputExpresion).result()
+        
+        // then
+        XCTAssertEqual(result, -9.0)
+    }
+
+}
