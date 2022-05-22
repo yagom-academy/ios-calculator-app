d6d3f81ebb253ebd4833cd77594b40ea6bb20c97
KayAhnDS <dkseotn123@gmail.com>
2022-05-23 02:53:40 +0900

    refactor: result메서드 구현부 수정

--- Calculator/Source/Model/Formula.swift
+++ Calculator/Source/Model/Formula.swift
@@ -6,31 +6,15 @@
 //
 
 struct Formula {
-    private var operands: CalculatorItemQueue<String>
-    private var operators: CalculatorItemQueue<String>
+    private var operands: CalculatorItemQueue<Double>
+    private var operators: CalculatorItemQueue<Operator>
 
     mutating func result() -> Double {
-        var operandsNumberInDoubleFormat = Double(operands.dequeue() ?? "Wrong") ?? 0.0
-        var calculationResult: Double = operandsNumberInDoubleFormat
+        var calculationResult: Double = operands.dequeue() ?? 0.0
         
         while operators.count != 0 {
-            let operatorLetter = operators.dequeue() ?? "Error_Handling"
-            let currentOperator = Character(operatorLetter)
-
-            operandsNumberInDoubleFormat = Double(operands.dequeue() ?? "Wrong") ?? 0.0
-            
-            switch currentOperator {
-            case "+":
-                calculationResult = Operator.add.calculate(lhs: calculationResult, rhs: operandsNumberInDoubleFormat)
-            case "-":
-                calculationResult = Operator.subtract.calculate(lhs: calculationResult, rhs: operandsNumberInDoubleFormat)
-            case "*":
-                calculationResult = Operator.multiply.calculate(lhs: calculationResult, rhs: operandsNumberInDoubleFormat)
-            case "/":
-                calculationResult = Operator.divide.calculate(lhs: calculationResult, rhs: operandsNumberInDoubleFormat)
-            default:
-                calculationResult = 0.0
-            }
+            guard let currentOperator = operators.dequeue() else { return 0.0}
+            calculationResult = currentOperator.calculate(lhs: calculationResult, rhs: operands.dequeue() ?? 0.0)
         }
         return calculationResult
     }
