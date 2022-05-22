5f807db4d455edef4714d3d6ae06d2c62c50d7f9
bard <schy2503@naver.com>
2022-05-22 15:23:31 +0900

    style: Formular 구조체 내 개행처리 변경

--- Calculator/Calculator/Model/Formula.swift
+++ Calculator/Calculator/Model/Formula.swift
@@ -13,14 +13,17 @@ struct Formula {
         guard var lastResult = try? operands.deQueue() else { throw QueueError.wrongOperands }
         
         while operands.joinedQueue.isEmpty != true {
-            guard let inputNumber = try? operands.deQueue() else
-            { throw QueueError.wrongOperands }
+            guard let inputNumber = try? operands.deQueue() else {
+                throw QueueError.wrongOperands
+            }
             
             guard let inputOperator = try? operators.deQueue() else
             { throw QueueError.wrongOperators }
             
-            lastResult = try inputOperator.calculate(lhs: lastResult,
-                                                     rhs: inputNumber)
+            lastResult = try inputOperator.calculate(
+                lhs: lastResult,
+                rhs: inputNumber
+            )
         }
         return lastResult
     }
