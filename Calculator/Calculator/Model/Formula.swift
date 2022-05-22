93c1ea6b873ee874f87a1a0a9cf2711bde5015fb
Hugh-github <tnghk4822@gmail.com>
2022-05-22 11:05:46 +0900

    feat: Formula타입의 메서드(result) 구현 및 테스트 코드 작성

--- Calculator/Calculator/Model/Formula.swift
+++ Calculator/Calculator/Model/Formula.swift
@@ -8,4 +8,34 @@
 struct Formula {
     var operands: CalculatorItemQueue<Double>
     var operators: CalculatorItemQueue<String>
+    
+    mutating func result() -> Double {
+        guard !operands.items.isEmpty,
+              !operators.items.isEmpty,
+              operands.items.count > operators.items.count,
+              var total = operands.dequeue() else
+        {
+            return 0.0
+        }
+        
+        for _ in 1...operators.items.count {
+            guard let number = operands.dequeue() else { return 0.0 }
+            
+            let symbol = operators.dequeue()
+            
+            switch symbol {
+            case "+":
+                total = Operator.add.calculate(lhs: total, rhs: number)
+            case "-":
+                total = Operator.subtract.calculate(lhs: total, rhs: number)
+            case "/":
+                total = Operator.divide.calculate(lhs: total, rhs: number)
+            case "*":
+                total = Operator.multiply.calculate(lhs: total, rhs: number)
+            default:
+                break
+            }
+        }
+        return total
+    }
 }
