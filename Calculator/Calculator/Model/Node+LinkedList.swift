951fea8d73c2c98c4bc265e06d723e790e4c173e
minsson <mskaang@gmail.com>
2022-05-22 20:35:03 +0900

    test: Node 테스트 및 구현을 위한 테스트 파일 추가

---
+++ Calculator/Calculator/Model/Node+LinkedList.swift
@@ -0,0 +1,12 @@
+//
+//  Node+LinkedList.swift
+//  Calculator
+//
+//  Created by Minseong Kang on 2022/05/22.
+//
+
+import Foundation
+
+final class Node<T> {
+    
+}
---
+++ Calculator/NodeTests/NodeTests.swift
@@ -0,0 +1,20 @@
+//
+//  NodeTests.swift
+//  NodeTests
+//
+//  Created by Minseong Kang on 2022/05/22.
+//
+
+import XCTest
+@testable import Calculator
+
+class NodeTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+}
