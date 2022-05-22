4bd28e0a9be1e11e4a4954e38a7336d509e932c3
KayAhnDS <dkseotn123@gmail.com>
2022-05-23 03:12:55 +0900

    refactor: 각 error에 따른 문구 추가

--- Calculator/Resource/SubModel/Error.swift
+++ Calculator/Resource/SubModel/Error.swift
@@ -10,4 +10,16 @@ import Foundation
 enum valueError: Error {
     case operandEmptyError
     case operatorEmptyError
+    case divideByZeroError
+    
+    var message: String {
+        switch self {
+        case .operandEmptyError:
+            return "숫자가 없습니다!"
+        case .operatorEmptyError:
+            return "연산자가 없습니다!"
+        case .divideByZeroError:
+            return "0으로 나눌수 없습니다!"
+        }
+    }
 }
