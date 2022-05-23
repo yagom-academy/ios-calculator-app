504300fc52b1572e9efe9cebcaf756afbb66ea79
KayAhnDS <dkseotn123@gmail.com>
2022-05-23 18:36:56 +0900

    refactor: result 메서드 수정

--- Calculator/Source/Model/Formula.swift
+++ Calculator/Source/Model/Formula.swift
@@ -9,12 +9,14 @@ struct Formula {
     private var operands: CalculatorItemQueue<Double>
     private var operators: CalculatorItemQueue<Operator>
 
-    mutating func result() -> Double {
-        var calculationResult: Double = operands.dequeue() ?? 0.0
-        
+    mutating func result() throws -> Double {
+        var calculationResult: Double
+        guard let operand = operands.dequeue()  else { throw valueError.operandEmptyError }
+        calculationResult = operand
         while operators.count != 0 {
-            guard let currentOperator = operators.dequeue() else { return 0.0}
-            calculationResult = currentOperator.calculate(lhs: calculationResult, rhs: operands.dequeue() ?? 0.0)
+            guard let currentOperator = operators.dequeue() else { throw valueError.operatorEmptyError }
+            guard let currentOperand = operands.dequeue() else { throw valueError.operandEmptyError }
+            calculationResult = currentOperator.calculate(lhs: calculationResult, rhs: currentOperand)
         }
         return calculationResult
     }
