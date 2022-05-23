fce755c9739f60707d7bd755c833a130c9875b84
Judy <joo00ee@naver.com>
2022-05-23 14:54:17 +0900

    refactor: parse 메서드 로직 변경

--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -1,16 +1,15 @@
 enum ExpressionParser {
     static func parse(from input: String) -> Formula {
         var formula = Formula()
-        let splitedInput = componentByOperators(from: input).enumerated()
-        
-        splitedInput.filter { $0.0 % 2 == 0 }
-            .compactMap { Double($0.1) }
+        let splitedInput = componentByOperators(from: input)
+       
+        splitedInput.compactMap { Double($0) }
             .forEach { formula.operands.enqueue($0) }
         
-        splitedInput.filter { $0.0 % 2 == 1 }
-            .compactMap { Operator(rawValue: Character($0.1)) }
+        splitedInput.filter { $0.count == 1 }
+            .compactMap { Operator(rawValue: Character($0)) }
             .forEach { formula.operators.enqueue($0) }
-       
+        
         return formula
     }
     
