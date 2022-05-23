0f7dab41b2d400229cd774b15d875496dfad5f92
Judy <joo00ee@naver.com>
2022-05-23 14:55:18 +0900

    test: ExpressionParser의 테스트 추가

--- Calculator/ExpressionParserTests/ExpressionParserTests.swift
+++ Calculator/ExpressionParserTests/ExpressionParserTests.swift
@@ -36,4 +36,55 @@ class ExpressionParserTests: XCTestCase {
         // then
         XCTAssertEqual(result, expectation)
     }
+    
+    func test_parse_숫자와기호가무작위로들어왔을때_연산자를분리하는지() {
+        // givwn
+        let input = "2*+2857//24-"
+        let expectation: [Operator] = [.multiply, .add, .divide, .divide, .subtract]
+        
+        // when
+        let formula = ExpressionParser.parse(from: input)
+        let result = formula.operators.calculatorItems
+        
+        // then
+        XCTAssertEqual(result, expectation)
+    }
+    
+    func test_parse_숫자와기호가무작위로들어왔을때_피연산자를분리하는지() {
+        // givwn
+        let input = "2*+2857//24-3"
+        let expectation: [Double] = [2, 2857, 24, 3]
+        
+        // when
+        let formula = ExpressionParser.parse(from: input)
+        let result = formula.operands.calculatorItems
+        
+        // then
+        XCTAssertEqual(result, expectation)
+    }
+    
+    func test_parse_연산자만들어있을때_연산자만분리되어들어가는지() {
+        // givwn
+        let input = "*+//-"
+        let expectation: [Operator] = [.multiply, .add, .divide, .divide, .subtract]
+        
+        // when
+        let formula = ExpressionParser.parse(from: input)
+        let result = formula.operators.calculatorItems
+        
+        // then
+        XCTAssertEqual(result, expectation)
+    }
+    
+    func test_parse_연산자만들어있을때_피연산자큐는nil인지() {
+        // givwn
+        let input = "*+//-"
+
+        // when
+        let formula = ExpressionParser.parse(from: input)
+        let result = formula.operands.calculatorItems
+        
+        // then
+        XCTAssertNil(result)
+    }
 }
