08384dea30ec0b19e4a351c4409cb7b185f939de
KayAhnDS <dkseotn123@gmail.com>
2022-05-24 01:05:37 +0900

    refactor: 프로토콜을 채택하는 부분 수정

--- Calculator/Source/Model/CalculatorItemQueue.swift
+++ Calculator/Source/Model/CalculatorItemQueue.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct CalculatorItemQueue<T: CalculateItem> {
+struct CalculatorItemQueue<T>: CalculateItem {
     private var queue: [T?] = []
     private var head: Int = 0
     
