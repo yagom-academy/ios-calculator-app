e2560766b87450ac99a1a7ddc613537078f74bb7
OldStarKR <tyr2293@naver.com>
2022-05-23 23:17:37 +0900

    test: Operator 테스트 코드 재작성

--- Calculator/OperatorTests/OperatorTests.swift
+++ Calculator/OperatorTests/OperatorTests.swift
@@ -26,9 +26,18 @@ class OperatorTests: XCTestCase {
     func test_calculate에_1과2를_넣고_더해줬을때_3반환() {
         // when
         let result = sut.calculate(lhs: 1.0, rhs: 2.0)
+        var value = Double.nan
+        var error: CalculatorError? = nil
+        
+        switch result {
+        case .success(let newValue):
+            value = newValue
+        case .failure(let newError):
+            error = newError
+        }
         
         // then
-        XCTAssertEqual(result, 3.0)
+        XCTAssertEqual(value, 3.0)
     }
     
     func test_calculate에_2와1을_넣고_빼줬을때_1반환() {
@@ -37,9 +46,18 @@ class OperatorTests: XCTestCase {
         
         // when
         let result = sut.calculate(lhs: 2.0, rhs: 1.0)
+        var value = Double.nan
+        var error: CalculatorError? = nil
+        
+        switch result {
+        case .success(let newValue):
+            value = newValue
+        case .failure(let newError):
+            error = newError
+        }
         
         // then
-        XCTAssertEqual(result, 1.0)
+        XCTAssertEqual(value, 1.0)
     }
     
     func test_calculate에_4와2를_넣고_나누어줬을때_2반환() {
@@ -48,20 +66,38 @@ class OperatorTests: XCTestCase {
         
         // when
         let result = sut.calculate(lhs: 4.0, rhs: 2.0)
+        var value = Double.nan
+        var error: CalculatorError? = nil
+        
+        switch result {
+        case .success(let newValue):
+            value = newValue
+        case .failure(let newError):
+            error = newError
+        }
         
         // then
-        XCTAssertEqual(result, 2.0)
+        XCTAssertEqual(value, 2.0)
     }
     
-    func test_calculate에서_0으로나눌경우_오류와함께_나누기이전수를_반환() {
+    func test_calculate에서_0으로나눌경우_dividedByZero_오류반환() {
         // given
         sut = .divide
         
         // when
         let result = sut.calculate(lhs: 4.0, rhs: 0.0)
+        var value = Double.nan
+        var error: CalculatorError? = nil
+        
+        switch result {
+        case .success(let newValue):
+            value = newValue
+        case .failure(let newError):
+            error = newError
+        }
         
         // then
-        XCTAssertEqual(result, 4.0)
+        XCTAssertEqual(error, CalculatorError.dividedByZero)
     }
     
     func test_calculate에_4와2를_넣고_곱해줬을때_8반환() {
@@ -70,8 +106,17 @@ class OperatorTests: XCTestCase {
         
         // when
         let result = sut.calculate(lhs: 4.0, rhs: 2.0)
+        var value = Double.nan
+        var error: CalculatorError? = nil
+        
+        switch result {
+        case .success(let newValue):
+            value = newValue
+        case .failure(let newError):
+            error = newError
+        }
         
         // then
-        XCTAssertEqual(result, 8.0)
+        XCTAssertEqual(value, 8.0)
     }
 }
