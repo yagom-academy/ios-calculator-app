e7db0b5ae0f90809951f0c25fa90f6d51ab9c4b2
bard <schy2503@naver.com>
2022-05-23 19:41:06 +0900

    add: UILabel 및 UIButton 추가 - 연산자에 해당하는 UIButton 추가 - 1부터9까지 해당하는 UIButton 추가 - 숫자를 입력하는 UILabel 추가 - 연산자를 입력하는 UILabel 추가 - 숫자 및 연산자의 textLabel을 초기화하는 UIButton 추가 - 소수점을 입력하는 UIButton 추가 - 0 및 00을 입력하는 UIButton 추가 - 연산자 기호를 변경하는 UIButton 추가 - 현재 입력된 TextLabel만 초기화하는 UIButton 추가

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -10,9 +10,66 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        operandsTextLabel.text = "0"
     }
-
-
+    
+    @IBOutlet weak var operandsTextLabel: UILabel!
+    @IBOutlet weak var operatorTextLabel: UILabel!
+    
+    @IBAction func changeOperandSignButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func allClearButtonTapped(_ sender: UIButton) {
+        operatorTextLabel.text = ""
+        operandsTextLabel.text = "0"
+    }
+    
+    @IBAction func clearEntryButtonTapped(_ sender: UIButton) {
+        operatorTextLabel.text = ""
+        operandsTextLabel.text = "0"
+    }
+    
+    @IBAction func operandButtonsTapped(_ sender: UIButton) {
+        eraseZero()
+        operandsTextLabel.text! += sender.titleLabel?.text ?? ""
+    }
+    
+    @IBAction func calculateButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func operatorButtonsTapped(_ sender: UIButton) {
+        operatorTextLabel.text = sender.titleLabel?.text
+    }
+    
+    @IBAction func zeroButtonTapped(_ sender: UIButton) {
+        if operandsTextLabel.text == "0" {
+            operandsTextLabel.text = operandsTextLabel.text
+        } else {
+            operandsTextLabel.text! += sender.titleLabel?.text ?? ""
+        }
+    }
+    
+    @IBAction func doubleZeroButtonTapped(_ sender: UIButton) {
+        if operandsTextLabel.text?.count == 1,
+           operandsTextLabel.text == "0" {
+            operandsTextLabel.text = "0"
+        } else {
+            operandsTextLabel.text! += sender.titleLabel?.text ?? ""
+        }
+    }
+    
+    @IBAction func decimalPointButtonTapped(_ sender: UIButton) {
+        if !(operandsTextLabel.text?.count == 0) {
+            operandsTextLabel.text! += sender.titleLabel?.text ?? ""
+        }
+    }
+    
+    func eraseZero() {
+        if operandsTextLabel.text == "0" {
+            operandsTextLabel.text = ""
+        }
+    }
 }
 
