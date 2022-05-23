3b041fc3572cadf396c5916dbe48072fabcd7444
BaekGom <jihunmm02@gmail.com>
2022-05-23 19:44:39 +0900

    feat: ExpressParser 구현

---
+++ Calculator/Calculator/Enum/ExpressionParse.swift
@@ -0,0 +1,33 @@
+//
+//  ExpressionParse.swift
+//  Calculator
+//
+//  Created by Baek on 2022/05/23.
+//
+
+import Foundation
+import UIKit
+
+enum ExpressParser {
+    func parse(form input: String) -> Formula {
+        var operands = CalculatorItemQueue<Double>()
+        var operators =  CalculatorItemQueue<Operator>()
+        let divideString = componetsByOperators(from: input)
+        
+        for parseString in divideString {
+            
+            if let parseNumber = Double(parseString) {
+                operands.enqueue(parseNumber)
+            }
+            
+            if let parseArithmetic = Operator(rawValue: Character(parseString)) {
+                operators.enqueue(parseArithmetic)
+            }
+        }
+        return Formula(operands: operands, operators: operators)
+    }
+    
+    private func componetsByOperators(from input: String) ->[String] {
+        return input.split(with: " ")
+    }
+}
