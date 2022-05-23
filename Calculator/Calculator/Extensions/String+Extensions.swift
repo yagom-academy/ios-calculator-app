6e23855e0633aa12fde2713b53a3a1809270e1f0
OldStarKR <tyr2293@naver.com>
2022-05-23 21:42:31 +0900

    refactor: 자체 정의 split 메서드 전면 수정

--- Calculator/Calculator/Extensions/String+Extensions.swift
+++ Calculator/Calculator/Extensions/String+Extensions.swift
@@ -7,6 +7,29 @@
 
 extension String {
     func split(with target: Character) -> [String] {
-        return self.split(separator: target).map { String($0) }
+        let data = self.map { String($0) }
+        let target = String(target)
+        var result = [String]()
+        var storage = ""
+        
+        data.forEach {
+            if $0 == target {
+                result.append(storage)
+                result.append(String($0))
+                storage = ""
+            } else {
+                storage += $0
+            }
+        }
+        
+        if storage.isNotEmpty {
+            result.append(storage)
+        }
+        
+        return result
+    }
+    
+    var isNotEmpty: Bool {
+        return !self.isEmpty
     }
 }
