c38d67723ed4a848e718983b33c7d64af006164d
bard <schy2503@naver.com>
2022-05-24 22:55:18 +0900

    refactor: ExpressionParser함수 및 테스트 코드리팩토링

--- Calculator/ExpressionParser/ExpressionParserTests.swift
+++ Calculator/ExpressionParser/ExpressionParserTests.swift
@@ -47,7 +47,7 @@ class parseTests: XCTestCase {
     
     func test_문자열에서_componentsByOperators를사용하여_operaters를_character로_형변환을하여집어넣는가2() {
         //when
-        let input = "1 + 2 - 3 + 4 / 2"
+        let input = "1 + 2 − 3 + 4 ÷ 2"
         
         //given
         let result = input.split(with: " ")
@@ -62,7 +62,7 @@ class parseTests: XCTestCase {
     
     func test_문자열에서_componentsByOperators메소드를사용하면_숫자를순서대로반환하는가() {
         //given
-        let input = "1 + 2 - 3 + 4 / 2"
+        let input = "1 + 2 − 3 + 4 ÷ 2"
         
         //when
         let result = input.compactMap { Double(String($0)) }
@@ -73,7 +73,7 @@ class parseTests: XCTestCase {
    
     func test_문자열에서_componentsByOperators메소드를사용하면_숫자를뒤집어서반환하지는않는가() {
         //given
-        let input = "1 + 2 - 3 + 4 / 2"
+        let input = "1 + 2 − 3 + 4 ÷ 2"
         
         //when
         let result = input.compactMap { Double(String($0)) }
@@ -84,7 +84,7 @@ class parseTests: XCTestCase {
     
     func test_operandsQueue에_operands값이순서대로_들어가는가() {
         //given
-        let input = "1 + 2 - 3 + 4 / 2"
+        let input = "1 + 2 − 3 + 4 ÷ 2"
         var operandsQueue = CalculatorItemQueue<Double>()
         let operands = input.compactMap { Double(String($0)) }
         
@@ -99,7 +99,7 @@ class parseTests: XCTestCase {
     
     func test_operatorQueue에_operators값이순서대로_들어가는가() {
         //given
-        let input = "1 + 2 - 3 + 4 / 2"
+        let input = "1 + 2 − 3 + 4 ÷ 2"
         var operatorQueue = CalculatorItemQueue<Operator>()
         let operators = input.split(with: " ")
                         .filter { $0.count == 1 }
@@ -119,7 +119,7 @@ class parseTests: XCTestCase {
     
     func test_ExpressionParser에서_input을집어넣어_parse를호출하고_result함수를_호출하면_계산이잘되는가() {
         //given
-        let input = "1 + 2 - 3 + 4 / 2"
+        let input = "1 + 2 − 3 + 4 ÷ 2"
         
         //when
         var test: Formula
@@ -135,7 +135,7 @@ class parseTests: XCTestCase {
     
     func test_ExpressionParser에서_input을집어넣어_parse를호출하고_result함수를_호출하였을때마지막숫자가0이면_에러를던지는가() {
         //given
-        let input = "1 + 2 - 3 + 4 / 0"
+        let input = "1 + 2 − 3 + 4 ÷ 0"
         
         //when
         var test: Formula
@@ -146,19 +146,29 @@ class parseTests: XCTestCase {
         XCTAssertThrowsError(try test.result())
     }
     
-    func test_ExdpressionParser에서_input을집어넣어_parse를호출하고_result함수를_호출하였을때마지막숫자가0이면_에러를던지는가() {
+    func test_ExdpressionParser에서_input을집어넣어_parse를호출하고_result함수를_호출하였을때마지막숫자가1의자릿수가아니어도_계산을잘하는가() {
         //given
-        let input = "1 + 2 - 3 + 4 - 100"
+        let input = "1 + 2 − 3 + 4 − 100"
         
         //when
         var test: Formula
         
         test = ExpressionParser.parse(from: input)
-//        test.operands = input.split(with: " ").compactMap { Double(String($0) }
-        let operands = input.compactMap { Double(String($0)) }
-        print(operands)
-        print(try? test.result())
+
         //then
-//        XCTAssertThrowsError(try test.result())
+        XCTAssertEqual(try test.result(), -96)
+    }
+    
+    func test_ExdpressionParser에서_input을집어넣어_parse를호출하고_result함수를_호출하였을때마지막숫자의1의자릿수에0이들어있어도_에러로던지지않는가() {
+        //given
+        let input = "1 + 2 − 3 + 4 − 100"
+        
+        //when
+        var test: Formula
+        
+        test = ExpressionParser.parse(from: input)
+
+        //then
+        XCTAssertNoThrow(try test.result())
     }
 }
