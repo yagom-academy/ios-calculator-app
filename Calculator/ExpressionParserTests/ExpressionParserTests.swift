30fd8874b0ec10e5a1a254ff7c3d70687bc79d2d
OldStarKR <tyr2293@naver.com>
2022-05-24 16:19:06 +0900

    test: ExpressionParser 테스트 타깃 작성

---
+++ Calculator/ExpressionParserTests/ExpressionParserTests.swift
@@ -0,0 +1,36 @@
+//
+//  ExpressionParserTests.swift
+//  ExpressionParserTests
+//
+//  Created by Gordon Choi on 2022/05/24.
+//
+
+import XCTest
+@testable import Calculator
+
+class ExpressionParserTests: XCTestCase {
+    
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+    }
+
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+    }
+
+    func test_parse메서드에_1더하기2빼기3을_넣었을때_알맞은Formula_반환() {
+        // given
+        let input = "1+2-3"
+        
+        // when
+        var formula = ExpressionParser.parse(from: input)
+        let formulaValue = try? formula.result()
+        
+        var expected = Formula(operands: CalculatorItemQueue(array: [1, 2, 3]),
+                               operators: CalculatorItemQueue(array: [Operator.add, Operator.subtract]))
+        let expectedValue = try? expected.result()
+        
+        // then
+        XCTAssertEqual(formulaValue, expectedValue)
+    }
+}
