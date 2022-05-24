ceb4dc76ae2c3b6d16430c838c8a2aa04bc73456
starting1231 <starting1231@naver.com>
2022-05-24 14:11:50 +0900

    feat: Implement calculateButtonAction function
    - add isOperandValueEmpty property
    - add isCalculateCompleted property
    - add mathematicalExpression property

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -7,6 +7,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var isOperandValueEmpty = false
+    var isCalculateCompleted = false
+    var mathematicalExpression = ""
     
     @IBOutlet weak var mathematicalExpressionStackView: UIStackView!
     @IBOutlet weak var operatorLabel: UILabel!
@@ -20,16 +23,25 @@ class ViewController: UIViewController {
         mathematicalExpressionStackView.subviews.forEach { $0.removeFromSuperview() }
         operandLabel.text = "0"
         operatorLabel.text = ""
+        mathematicalExpression = ""
+        isOperandValueEmpty = false
+        isCalculateCompleted = false
     }
     
     @IBAction func ceButtonAction(_ sender: UIButton) {
-        operandLabel.text = ""
-        if operatorLabel.text == "" && !mathematicalExpressionStackView.subviews.isEmpty {
+        if isCalculateCompleted {
             mathematicalExpressionStackView.subviews.forEach { $0.removeFromSuperview() }
+            mathematicalExpression = ""
+            isCalculateCompleted = false
         }
+        operandLabel.text = "0"
+        isOperandValueEmpty = false
     }
     
     @IBAction func switchSignButton(_ sender: UIButton) {
+        guard !isCalculateCompleted else {
+            return
+        }
         guard let operandLabelText = operandLabel.text else {
             return
         }
@@ -40,11 +52,70 @@ class ViewController: UIViewController {
             return
         }
         operandLabel.text = String(-Int(operand))
-        
     }
     
     @IBAction func operatorsButtonAction(_ sender: UIButton) {
-        guard !mathematicalExpressionStackView.subviews.isEmpty || operandLabel.text != "0" else {
+        guard !isCalculateCompleted else {
+            return
+        }
+        guard isOperandValueEmpty else {
+            operatorLabel.text = sender.currentTitle
+            return
+        }
+        
+        let operatorOfSignLabel: UILabel = {
+            let label = UILabel()
+            label.text = operandLabel.text
+            label.font = UIFont.systemFont(ofSize: 25)
+            label.textColor = .white
+            return label
+        }()
+        
+        let operandOfSignLabel: UILabel = {
+            let label = UILabel()
+            label.text = operatorLabel.text
+            label.font = UIFont.systemFont(ofSize: 25)
+            label.textColor = .white
+            return label
+        }()
+        
+        let signStackView: UIStackView = {
+            let stackView = UIStackView(arrangedSubviews: [operandOfSignLabel, operatorOfSignLabel])
+            stackView.axis = .horizontal
+            stackView.distribution = .fill
+            stackView.alignment = .fill
+            stackView.spacing = 8
+            return stackView
+        }()
+        
+        operatorLabel.text = sender.currentTitle
+        mathematicalExpressionStackView.addArrangedSubview(signStackView)
+        mathematicalExpression += "\(operandLabel.text ?? "") \(operatorLabel.text ?? "") "
+        operandLabel.text = "0"
+        isOperandValueEmpty = false
+    }
+    
+    @IBAction func operandsButtonAction(_ sender: UIButton) {
+        guard !isCalculateCompleted else {
+            return
+        }
+        if !isOperandValueEmpty {
+            operandLabel.text = sender.currentTitle
+            isOperandValueEmpty = true
+        } else {
+            operandLabel.text = (operandLabel.text ?? "") + (sender.currentTitle ?? "")
+            isOperandValueEmpty = true
+        }
+    }
+    
+    @IBAction func calculateButtonAction(_ sender: UIButton) {
+        guard let operandString = operandLabel.text else {
+            return
+        }
+        guard isOperandValueEmpty else {
+            return
+        }
+        guard operatorLabel.text != "" else {
             return
         }
         
@@ -74,21 +145,12 @@ class ViewController: UIViewController {
         }()
         
         mathematicalExpressionStackView.addArrangedSubview(signStackView)
-        
-        operatorLabel.text = sender.currentTitle
-        operandLabel.text = "0"
+        print(mathematicalExpression + operandString)
+        mathematicalExpression = ""
+        operatorLabel.text = ""
+        operandLabel.text = ""
+        isOperandValueEmpty = false
+        isCalculateCompleted = true
     }
-    
-    @IBAction func operandsButtonAction(_ sender: UIButton) {
-        if mathematicalExpressionStackView.subviews.isEmpty {
-            operandLabel.text = sender.currentTitle
-        } else {
-            operandLabel.text = (operandLabel.text ?? "") + (sender.currentTitle ?? "")
-        }
-    }
-    
-    @IBAction func calculateButtonAction(_ sender: Any) {
-    }
-    
 }
 
