6f9d4c58bc887fa0a8e913cd93af65b42241661c
unchain123 <unchain123@kakao.com>
2022-05-24 13:39:49 +0900

    refactor: changed to the basic type of higher-orderfucntion

--- Calculator/Enum/ExpressionParser.swift
+++ Calculator/Enum/ExpressionParser.swift
@@ -10,7 +10,10 @@ import Foundation
 extension String {
     func split(with target: Character) -> [String] {
         let results = self.split(separator: target)
-        let result = results.map { String($0) }
+        let result = results.map { (element: String.SubSequence) -> String in
+            let result = String(element)
+            return result
+        }
         
         return  result
     }
@@ -28,15 +31,21 @@ enum ExpressionPaser {
         var operatorsQueue = CalculatorItemQueue<Operator>()
         
         let paser = componentsByOperator(from: input)
-        let allOperator = Operator.allCases.map { String($0.rawValue) }
-        
-        let operands = paser.compactMap{ Double($0) }
-        let operators = paser.filter {
-            allOperator.contains($0)
+        let allOperator = Operator.allCases.map { (symbol: Operator) -> String in
+            let allOperator = String(symbol.rawValue)
+            return allOperator
         }
         
-        operators.forEach {
-            operatorsQueue.enQueue(Operator(rawValue: Character($0)) ?? .add)
+        let operands = paser.compactMap { (element: String) -> Double? in
+            return Double(element)
+        }
+        
+        let operators = paser.filter { element -> Bool in
+            allOperator.contains(element)
+        }
+        
+        operators.forEach { element in
+            operatorsQueue.enQueue(Operator(rawValue: Character(element)) ?? .add)
         }
         
         operands.forEach(operandsQueue.enQueue(_:))
