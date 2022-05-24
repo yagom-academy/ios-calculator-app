8533ba906876d3b847d7d02bdd3ab3510a9023e7
starting1231 <starting1231@naver.com>
2022-05-24 10:38:23 +0900

    feat: Connect UIView to ViewController

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -8,11 +8,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mathematicalExpressionStackView: UIStackView!
+    @IBOutlet weak var operatorLabel: UILabel!
+    @IBOutlet weak var operandLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func acButtonAction(_ sender: UIButton) {
+    }
+    
+    @IBAction func ceButtonAction(_ sender: Any) {
+    }
+    
+    @IBAction func switchSignButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func operatorsButtonAction(_ sender: Any) {
+    }
+    
+    @IBAction func operandsButtonAction(_ sender: Any) {
+    }
+    
+    @IBAction func calculateButtonAction(_ sender: Any) {
+    }
+    
 }
 
