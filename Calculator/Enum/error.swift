55cbe5488a7ab903eea96d406fc4b37a11589057
unchain123 <unchain123@kakao.com>
2022-05-24 09:57:33 +0900

    feat: make error enum type

---
+++ Calculator/Enum/error.swift
@@ -0,0 +1,13 @@
+//
+//  error.swift
+//  ExpressionPaserTest
+//
+//  Created by unchain123 on 2022/05/24.
+//
+
+import Foundation
+
+enum CalculationError: Error {
+    case noneValue
+    case divideByZero
+}
