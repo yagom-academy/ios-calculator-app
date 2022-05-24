5f5944ecece8ad380186746fdef7415d19faaaeb
starting1231 <starting1231@naver.com>
2022-05-24 12:50:53 +0900

    feat: Implement operatorsButtonAction function

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -7,7 +7,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var mathematicalExpressionStackView: UIStackView!
     @IBOutlet weak var operatorLabel: UILabel!
     @IBOutlet weak var operandLabel: UILabel!
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
     @IBAction func acButtonAction(_ sender: UIButton) {
         mathematicalExpressionStackView.subviews.forEach { $0.removeFromSuperview() }
         operandLabel.text = "0"
@@ -40,17 +40,50 @@ class ViewController: UIViewController {
             return
         }
         operandLabel.text = String(-Int(operand))
-    
+        
     }
     
-    @IBAction func operatorsButtonAction(_ sender: Any) {
+    @IBAction func operatorsButtonAction(_ sender: UIButton) {
+        guard !mathematicalExpressionStackView.subviews.isEmpty || operandLabel.text != "0" else {
+            return
+        }
+        
+        let operatorOfSignLabel: UILabel = {
+            let label = UILabel()
+            label.text = operandLabel.text
+            label.font = UIFont.systemFont(ofSize: 25)
+            label.textColor = .white
+            return label
+        }()
+        
+        let operandOfSignLabel: UILabel = {
+            let label = UILabel()
+            label.text = operatorLabel.text
+            label.font = UIFont.systemFont(ofSize: 25)
+            label.textColor = .white
+            return label
+        }()
+        
+        let signStackView: UIStackView = {
+            let stackView = UIStackView(arrangedSubviews: [operandOfSignLabel, operatorOfSignLabel])
+            stackView.axis = .horizontal
+            stackView.distribution = .fill
+            stackView.alignment = .fill
+            stackView.spacing = 8
+            return stackView
+        }()
+        
+        mathematicalExpressionStackView.addArrangedSubview(signStackView)
+        
+        operatorLabel.text = sender.currentTitle
+        operandLabel.text = "0"
     }
     
     @IBAction func operandsButtonAction(_ sender: UIButton) {
         if mathematicalExpressionStackView.subviews.isEmpty {
             operandLabel.text = sender.currentTitle
         } else {
-            operandLabel.text = (operandLabel.text ?? "") + (sender.currentTitle ?? "") 
+            operandLabel.text = (operandLabel.text ?? "") + (sender.currentTitle ?? "")
         }
     }
     
