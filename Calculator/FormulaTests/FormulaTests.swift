1ac83806214c49043e819a1cadfed822208d0818
MinsuJeon <chso294@naver.com>
2022-05-24 12:09:29 +0900

    refactor: 에러 핸들링 테스트 코드의 결과값 비교를 String 타입이 아닌 CalculatorError 타입으로 변경

--- Calculator/FormulaTests/FormulaTests.swift
+++ Calculator/FormulaTests/FormulaTests.swift
@@ -23,7 +23,7 @@ class FormulaTests: XCTestCase {
         let input = "1+2+3"
         let expectation: Double = 6
         
-        //when
+        // when
         var parser = ExpressionParser.parse(from: input)
         let result = try parser.result()
 
@@ -36,7 +36,7 @@ class FormulaTests: XCTestCase {
         let input = "1-2-3"
         let expectation: Double = -4
         
-        //when
+        // when
         var parser = ExpressionParser.parse(from: input)
         let result = try parser.result()
 
@@ -49,7 +49,7 @@ class FormulaTests: XCTestCase {
         let input = "1*2*3"
         let expectation: Double = 6
         
-        //when
+        // when
         var parser = ExpressionParser.parse(from: input)
         let result = try parser.result()
 
@@ -62,7 +62,7 @@ class FormulaTests: XCTestCase {
         let input = "10/2/5"
         let expectation: Double = 1
 
-        //when
+        // when
         var parser = ExpressionParser.parse(from: input)
         let result = try parser.result()
 
@@ -75,7 +75,7 @@ class FormulaTests: XCTestCase {
         let input = "10+2-3*4/6"
         let expectation: Double = 6
         
-        //when
+        // when
         var parser = ExpressionParser.parse(from: input)
         let result = try parser.result()
 
@@ -86,137 +86,93 @@ class FormulaTests: XCTestCase {
     func test_result_0으로_나눌때_오류처리가_정상적으로_이루어져_NaN의_값을_반환하는지_테스트() throws {
         // given
         let input = "100/0"
-        let expectation = "NaN"
-        var result = "NaN이 아닙니다"
         
-        //when
+        // when
         var parser = ExpressionParser.parse(from: input)
-        
-        do {
-            _ = try parser.result()
-        } catch CalculatorError.dividedByZero {
-            result = "NaN"
-        }
-        
+
         // then
-        XCTAssertEqual(result, expectation)
+        XCTAssertThrowsError(try parser.result(), "divided by zero") { error in
+            XCTAssertEqual(error as? CalculatorError, CalculatorError.dividedByZero)
+        }
     }
     
     func test_result_사칙연산자가_아닌_문자열이_입력될때_오류처리가_정상적으로_이루어지는지_테스트() throws {
         // given
         let input = "100%0#21@"
-        let expectation = "사칙연산자가 아닙니다"
-        var result = ""
         
-        //when
+        // when
         var parser = ExpressionParser.parse(from: input)
-        
-        do {
-            _ = try parser.result()
-        } catch CalculatorError.invalidOperator {
-            result = "사칙연산자가 아닙니다"
-        }
-        
+
         // then
-        XCTAssertEqual(result, expectation)
+        XCTAssertThrowsError(try parser.result(), "invalid operator") { error in
+            XCTAssertEqual(error as? CalculatorError, CalculatorError.invalidOperator)
+        }
     }
     
     func test_result_피연산수도_연산자도_없는_빈_문자열일때_에러핸들링이_정상적으로_작동하는지_테스트() throws {
         // given
-        let input: String = ""
-        let expectation = "큐가 비었습니다"
-        var result = ""
+        let input = ""
         
-        //when
+        // when
         var parser = ExpressionParser.parse(from: input)
-        
-        do {
-            _ = try parser.result()
-        } catch CalculatorError.emptyQueues {
-            result = "큐가 비었습니다"
-        }
-        
+
         // then
-        XCTAssertEqual(result, expectation)
+        XCTAssertThrowsError(try parser.result(), "empty queues") { error in
+            XCTAssertEqual(error as? CalculatorError, CalculatorError.emptyQueues)
+        }
     }
     
     
     func test_result_사칙연산자만으로_구성된_문자열이_입력될때_오류처리가_정상적으로_이루어지는지_테스트() throws {
         // given
         let input = "+-*/"
-        let expectation = "피연산수가 부족합니다"
-        var result = ""
         
-        //when
+        // when
         var parser = ExpressionParser.parse(from: input)
-        
-        do {
-            _ = try parser.result()
-        } catch CalculatorError.notEnoughOperands {
-            result = "피연산수가 부족합니다"
-        }
-        
+
         // then
-        XCTAssertEqual(result, expectation)
+        XCTAssertThrowsError(try parser.result(), "not enough operands") { error in
+            XCTAssertEqual(error as? CalculatorError, CalculatorError.notEnoughOperands)
+        }
     }
     
     func test_result_피연산수만으로_구성된_문자열이_입력될때_오류처리가_정상적으로_이루어지는지_테스트() throws {
         // given
         let input = "101231240010281"
-        let expectation = "연산자가 부족합니다"
-        var result = ""
         
-        //when
+        // when
         var parser = ExpressionParser.parse(from: input)
-        
-        do {
-            _ = try parser.result()
-        } catch CalculatorError.notEnoughOperators {
-            result = "연산자가 부족합니다"
-        }
-        
+
         // then
-        XCTAssertEqual(result, expectation)
+        XCTAssertThrowsError(try parser.result(), "not enough operators") { error in
+            XCTAssertEqual(error as? CalculatorError, CalculatorError.notEnoughOperators)
+        }
     }
     
     func test_result_연산자와_피연산수가_올바른_순서를_따르지_않을때_오류처리가_정상적으로_이루어지는지_테스트() throws {
         // given
-        let input = "+-*/123"
-        let expectation = "피연산수가 부족합니다"
-        var result = ""
+        let input = "+-*1/123"
         
-        //when
+        // when
         var parser = ExpressionParser.parse(from: input)
-        
-        do {
-            _ = try parser.result()
-        } catch CalculatorError.notEnoughOperands {
-            result = "피연산수가 부족합니다"
-        } catch CalculatorError.notEnoughOperators {
-            result = "연산자가 부족합니다"
-        }
-        
+
         // then
-        XCTAssertEqual(result, expectation)
+        XCTAssertThrowsError(try parser.result(), "not enough operands") { error in
+            XCTAssertEqual(error as? CalculatorError, CalculatorError.notEnoughOperands)
+        }
     }
     
-    func test_result_피연산수와_연산자가_아닌_값이_혼합된_문자열값에_대하여_에러처리가_정상적으로_이루어지는지_테스트() throws {
+    func test_result_피연산수와_연산자가_아닌_값이_혼합된_문자열값에_대하여_에러처리가_정상적으로_이루어지는지_테스트() throws {    
         // given
         let input = "1223#1231555^!2!12231"
-        let expectation = "사칙연산자가 아닙니다"
-        var result = ""
-
-        //when
+        
+        // when
         var parser = ExpressionParser.parse(from: input)
 
-        do {
-            _ = try parser.result()
-        } catch CalculatorError.invalidOperator {
-            result = "사칙연산자가 아닙니다"
-        }
-
         // then
-        XCTAssertEqual(result, expectation)
+        XCTAssertThrowsError(try parser.result(), "invalid operator") { error in
+            XCTAssertEqual(error as? CalculatorError, CalculatorError.invalidOperator)
+        }
     }
     
     func test_result_소수점_아래_자리수가_큰_피연산수를_입력받았을때_정상적으로_값이_출력되는지_테스트() throws {
@@ -224,7 +180,7 @@ class FormulaTests: XCTestCase {
         let input = "0.000001*0.000002"
         let expectation = 0.000001 * 0.000002
        
-        //when
+        // when
         var parser = ExpressionParser.parse(from: input)
         let result = try parser.result()
 
@@ -237,7 +193,7 @@ class FormulaTests: XCTestCase {
         let input = "4.3+1/5*10.2"
         let expectation = ((4.3 + 1) / 5) * 10.2
        
-        //when
+        // when
         var parser = ExpressionParser.parse(from: input)
         let result = try parser.result()
 
@@ -250,7 +206,7 @@ class FormulaTests: XCTestCase {
         let input = "-4.3+1/5*10.2"
         let expectation = ((-4.3 + 1) / 5) * 10.2
        
-        //when
+        // when
         var parser = ExpressionParser.parse(from: input)
         let result = try parser.result()
 
