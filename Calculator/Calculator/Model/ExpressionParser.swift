4cc38981a18d07de2b4b677fbd818bda40227768
yeeton37 <lyeeun37@naver.com>
2022-05-24 15:55:50 +0900

    refactor: enum(ExpressionParser)의 componentsByOperators 수정 및 메서드(parse) 수정
    - 숫자를 반환하는 componentsByOperators 선언
    - 연산자를 반환하는 componentsByOperands 선언
    - parse 메서드는 위의 메서드를 사용해 값을 반환하는 것으로 수정

--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -9,25 +9,37 @@ import Foundation
 
 enum ExpressionParser {
     static func parse(from input: String) -> Formula {
-        let separatedInputValue = componentsByOperators(from: input)
+        let inputOperands = componentsByOperators(from: input)
+        let inputOperators = componentsByOperands(from: input)
         var operandQueue = CalculatorItemQueue<Double>()
         var operatorQueue = CalculatorItemQueue<Operator>()
         
-        separatedInputValue.forEach {
-            if let doubleTypeValue = Double($0) {
-                operandQueue.enqueue(data: doubleTypeValue)
-                return
-            }
-            
-            if let operaterTypeValue = Operator(rawValue: Character($0)) {
-                operatorQueue.enqueue(data: operaterTypeValue)
+        inputOperands.forEach {
+            if let doubleValue = Double($0) {
+                operandQueue.enqueue(data: doubleValue)
             }
         }
+        
+        inputOperators.forEach {
+            if let operatorValue = Operator(rawValue: Character($0)) {
+                operatorQueue.enqueue(data: operatorValue)
+            }
+        }
+        
         return Formula(operands: operandQueue, operators: operatorQueue)
     }
     
     private static func componentsByOperators(from input: String) -> [String] {
-        return input.split(with: " ")
+        let operands = input.components(separatedBy: ["+","-","/","*"])
+        
+        return operands
+    }
+    
+    private static func componentsByOperands(from input: String) -> [String] {
+        let operators = input.filter { !$0.isNumber && $0 != "." }.map { String($0) }
+        
+        return operators
     }
 }
+
 
