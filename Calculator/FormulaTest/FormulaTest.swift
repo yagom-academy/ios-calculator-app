4915886f616f59e46c1fc8af2e3df096b96905ea
KayAhnDS <dkseotn123@gmail.com>
2022-05-24 14:59:29 +0900

    refactor: test function 추가

--- Calculator/FormulaTest/FormulaTest.swift
+++ Calculator/FormulaTest/FormulaTest.swift
@@ -6,30 +6,33 @@
 //
 
 import XCTest
+@testable import Calculator
 
 class FormulaTest: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        try super.setUpWithError()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        try super.tearDownWithError()
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func test_resultReturnsEqualWhenItHasProperEquation() throws {
+        // given
+        let equation = "1000+200-300*4/5"
+        // when
+        var formula1 = ExpressionParser.parse(from: equation)
+        // then
+        XCTAssertEqual(try formula1.result(), 720.0)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func test_resultThrowsErrorWhenItHasNotProperEquation() throws {
+        // given
+        let equation = "1000+200-300*4/5+"
+        // when
+        var formula1 = ExpressionParser.parse(from: equation)
+        // then
+        XCTAssertThrowsError(try formula1.result())
     }
-
 }
