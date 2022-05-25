1a401fa2479f3a0e87f2854e87fe585ef758e4ee
ZZBAE <jebyun2@gmail.com>
2022-05-25 19:14:39 +0900

    feat: Formula 파일 생성 및 추가
    - ExpressionParser에서 나눠준 연산자와 피연산자를 사용해 계산해주는 Formula 구조체 생성

--- Calculator/Models/Formula.swift
+++ Calculator/Models/Formula.swift
@@ -7,3 +7,30 @@
 
 import Foundation
 
+struct Formula {
+    var operands: CalculatorItemQueue<Double>
+    var operators: CalculatorItemQueue<Character>
+    
+    mutating func result() throws -> Double {
+        guard var result: Double = operands.dequeue() else { throw FormulaError.noOperandsInQueue }
+        while operators.isEmpty == false {
+            guard let eachOperator = operators.dequeue()
+            else {
+                throw FormulaError.emptyError
+            }
+            guard let operandsItem = operands.dequeue()
+            else {
+                throw FormulaError.emptyError
+            }
+            let operatorsItem = OperatorItem(rawValue: eachOperator)
+            result = (operatorsItem?.calculate(lhs: result, rhs: operandsItem))!
+        }
+        return result
+    }
+}
+
+
+enum FormulaError: Error {
+    case emptyError
+    case noOperandsInQueue
+}
