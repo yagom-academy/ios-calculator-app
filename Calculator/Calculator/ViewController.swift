4c9fc017f53af92361ac04f73b4ea145f9b65ef8
BaekGom <jihunmm02@gmail.com>
2022-05-26 02:15:11 +0900

    feat: 계산식 및 기호 초기화, 숫자패드 및 사칙연산 기호 버튼입력 구현

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -7,12 +7,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var mathSign: UILabel!
+    @IBOutlet weak var mathFomula: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        clearFormula()
     }
-
-
+    
+    func clearFormula() {
+        mathFomula.text = "0"
+        mathSign.text = ""
+    }
+    
+    @IBAction func InputNumber(buttonNumber: UIButton) {
+        guard let formulaNumber = buttonNumber.currentTitle else { return }
+        mathFomula.text = formulaNumber
+    }
+    
+    @IBAction func InputSign(buttonSign: UIButton) {
+        guard let signFormula = buttonSign.currentTitle else { return }
+        mathSign.text = signFormula
+    }
 }
 
