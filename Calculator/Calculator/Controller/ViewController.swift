8673f5900cff7e95aa09f9caff16719699724d42
웡빙 <wb277@naver.com>
2022-05-25 22:12:43 +0900

    feat : 계산기화면 UI "ViewController"에 연결

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -7,11 +7,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var screenLabel: UILabel?
+    @IBOutlet weak var currentOperatorLabel: UILabel?
+    @IBOutlet weak var historyStackView: UIStackView?
+    @IBOutlet weak var scrollView: UIScrollView?
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+}
 
+extension ViewController {
+    
+    @IBAction func operandButtonDidTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func operatorButtonDidTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func equalButtonDidTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func functionButtonDidTapped(_ sender: UIButton) {
+    }
+}
 
+private extension ViewController {
 }
