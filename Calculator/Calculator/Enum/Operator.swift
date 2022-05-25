c84aa23e16d1ac4ebcde3c1dc7e3792351de7da4
BaekGom <jihunmm02@gmail.com>
2022-05-26 02:16:37 +0900

    refactor: Operator 열거형 타입 divide,multiply 케이스 수정

--- Calculator/Calculator/Enum/Operator.swift
+++ Calculator/Calculator/Enum/Operator.swift
@@ -10,8 +10,8 @@ import Foundation
 enum Operator: Character, CaseIterable, CalculateItem {
     case add = "+"
     case subtract = "-"
-    case divide = "/"
-    case multiply = "*"
+    case divide = "÷"
+    case multiply = "×"
     
     func calculate(lhs: Double, rhs: Double) throws -> Double {
         switch self {
