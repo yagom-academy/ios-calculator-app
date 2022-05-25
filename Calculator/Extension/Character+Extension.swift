9d036f18fcaf35ac350d20f862c9754396ee677a
ZZBAE <jebyun2@gmail.com>
2022-05-25 19:00:14 +0900

    feat: Character extension 추가

--- Calculator/Extension/Character+Extension.swift
+++ Calculator/Extension/Character+Extension.swift
@@ -2,7 +2,12 @@
 //  Character+Extension.swift
 //  Calculator
 //
-//  Created by 변재은 on 2022/05/25.
+//  Created by 재재 on 2022/05/25.
 //
 
 import Foundation
+
+
+extension Character: CalculateItem {
+    
+}
