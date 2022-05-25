64bae3834980e3f86c0fc5c585370fee1c847090
seohyeon2 <seo_hyeon418@naver.com>
2022-05-25 17:59:48 +0900

    refactor: result메서드에서 큐안에 든 모든 값을 계산하도록 수정

--- Calculator/Calculator/Model/Formula.swift
+++ Calculator/Calculator/Model/Formula.swift
@@ -1,23 +1,36 @@
 struct Formula {
-    var operands = CalculatorItemQueue<Double>()
-    var operators = CalculatorItemQueue<String>()
+    private(set) var operands: CalculatorItemQueue<Double>
+    private(set) var operators: CalculatorItemQueue<String>
     
-    @discardableResult
+    init(){
+        operands = CalculatorItemQueue<Double>()
+        operators = CalculatorItemQueue<String>()
+    }
+    
     func result() throws -> Double {
-        let lhs = operands.dequeue() ?? 0
-        let rhs = operands.dequeue() ?? 0
-        let operatorSymbol = Character(operators.dequeue() ?? "")
-        var result = 0.0
-        
-        let `operator` = Operator.allCases.filter {
-            $0.rawValue == operatorSymbol
+        guard let start = operands.displayItems().first else {
+            return 0.0
         }
         
-        do {
-            result = try `operator`[0].calculate(lhs: lhs, rhs: rhs)
-        } catch CalculatorError.dividedByZero {
-            throw CalculatorError.dividedByZero
+        let operandItems = operands.displayItems().filter {
+            $0 != operands.displayItems().first
         }
+
+        let errorNumber = CalculatorError.dividedByZero.errorCaseNumber
+        let result = try operandItems.reduce(start) { reuslt, partialResult in
+            let operatorSymbol = operators.dequeue() ?? ""
+            let `operator` = Operator.allCases.filter {
+                $0.rawValue == Character(operatorSymbol)
+            }
+            let calculatedValue = `operator`[0].calculate(lhs: reuslt, rhs: partialResult)
+            
+            if operatorSymbol == "/" && (partialResult == errorNumber || reuslt == errorNumber) {
+                throw CalculatorError.dividedByZero
+            }
+            return calculatedValue
+        }
+        
+        operands.reset()
         return result
     }
 }
--- Calculator/Test/FormulaTests/FormulaTests.swift
+++ Calculator/Test/FormulaTests/FormulaTests.swift
@@ -17,21 +17,31 @@ class LinkedListTests: XCTestCase {
     // MARK: Test_result()
     func test_operands의_더한_값이_반환되는지() {
         // given
-        sut.operands.enqueue(1)
+        sut.operands.enqueue(5)
+        sut.operands.enqueue(4)
         sut.operands.enqueue(2)
+        sut.operands.enqueue(3)
+        sut.operands.enqueue(1)
+        sut.operators.enqueue("*")
+        sut.operators.enqueue("/")
+        sut.operators.enqueue("-")
         sut.operators.enqueue("+")
         // when
-        var result = 0.0
-        do {
-            result = try sut.result()
-        } catch {
-            print(CalculatorError.unknownError.errorMessage)
-        }
+        let result = try? sut.result()
         // then
-        XCTAssertEqual(result, 3)
+        XCTAssertEqual(result, 8)
     }
     
-    func test_0으로_나눌_때_에러가_반환되는지() {
+    func test_0으로_나눌_때_에러넘버가_반환되는지() {
+        // given
+        sut.operands.enqueue(0)
+        sut.operands.enqueue(2)
+        sut.operators.enqueue("/")
+        // when // given
+        XCTAssertThrowsError(try sut.result())
+    }
+    
+    func test_0으로_나눌_때_에러메세지가_출력되는지() {
         // given
         sut.operands.enqueue(0)
         sut.operands.enqueue(2)
@@ -39,11 +49,38 @@ class LinkedListTests: XCTestCase {
         // when
         var result = ""
         do {
-            try sut.result()
-        } catch {
+            var _ = try sut.result()
+        } catch CalculatorError.dividedByZero {
             result = CalculatorError.dividedByZero.errorMessage
+        } catch {
+            result = CalculatorError.unknownError.errorMessage
         }
-        // then
+        // given
         XCTAssertEqual(result, "NaN")
+    }
+    
+    func test_0으로_나눌_때_에러메세지가_출력되는지2() {
+        // given
+        sut.operands.enqueue(5)
+        sut.operands.enqueue(4)
+        sut.operands.enqueue(0)
+        sut.operands.enqueue(3)
+        sut.operands.enqueue(1)
+        sut.operators.enqueue("*")
+        sut.operators.enqueue("/")
+        sut.operators.enqueue("-")
+        sut.operators.enqueue("+")
+        // when
+        var result = 0.0
+        
+        do {
+            result = try sut.result()
+        } catch CalculatorError.dividedByZero {
+            _ = CalculatorError.dividedByZero.errorMessage
+        } catch {
+            _ = CalculatorError.unknownError.errorMessage
+        }
+        // given
+        XCTAssertNotEqual(result, 20.0)
     }
 }
