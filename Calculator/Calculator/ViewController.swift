4cdb1d9b4e28bb7f0fc409438b06a39ab37392ba
yusw10 <yusw10@naver.com>
2022-05-27 06:25:57 +0900

    feat: Add pressOperatorButton
    - add pressOperatordButton function

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -12,36 +12,44 @@ class ViewController: UIViewController {
     @IBOutlet weak var currendOperand: UILabel!
     @IBOutlet weak var currentOperator: UILabel!
     private var operationQueue: String = ""
-    
+    private let ZERO = "0"
     override func viewDidLoad() {
         super.viewDidLoad()
         clearScrollviewContent()
+        clearCurrentOperand()
+        clearCurrentOperator()
         // Do any additional setup after loading the view.
     }
     // MARK: - IBAction
     @IBAction func pressOperandButton(_ sender: UIButton) {
-        guard currendOperand.text != "0" else {
-            return
+        if currendOperand.text == "0" {
+            currendOperand.text = ""
         }
-        currendOperand?.text = currendOperand?.text ?? "" + (sender.currentTitle ?? "")
+        currendOperand?.text = (currendOperand?.text ?? "") + (sender.currentTitle ?? "")
     }
     
     @IBAction func pressOperatorButton(_ sender: UIButton) {
-        
+        guard currendOperand.text != ZERO else {
+            return
+        }
+        currentOperator.text = sender.currentTitle
+        operationQueue += "\(currendOperand?.text) \(sender.currentTitle) "
+        //create Scrollview Content
+        print(operationQueue)
+        clearCurrentOperand()
     }
     
     @IBAction func pressAllClearButton(_ sender: UIButton) {
         clearScrollviewContent()
         clearCurrentOperand()
         clearCurrentOperator()
-        
     }
     @IBAction func pressClearEntryButton(_ sender: UIButton) {
         clearCurrentOperand()
     }
     //MARK: - ViewController Method
     private func clearCurrentOperand() {
-        currendOperand.text = "0"
+        currendOperand.text = ZERO
     }
     private func clearCurrentOperator() {
         currentOperator.text = ""
