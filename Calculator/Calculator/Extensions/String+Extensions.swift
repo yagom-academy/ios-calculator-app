faeaed0f7c8cb73902f6d4cb901470d219618cb6
웡빙 <wb277@naver.com>
2022-05-26 22:18:55 +0900

    feat : "String+Extensions" removeEntire함수 주가
    문자열에서 특정 character를 모두 지워주는 기능

--- Calculator/Calculator/Extensions/String+Extensions.swift
+++ Calculator/Calculator/Extensions/String+Extensions.swift
@@ -10,6 +10,14 @@ import Foundation
 extension String {
     
     func split(with target: Character) -> [String] {
-        return self.split(separator: target).map{ String($0) }
+        return self.split(separator: target).map { String($0) }
+    }
+    
+    mutating func removeEntire(character: Character) -> String {
+        
+        while self.contains(character) {
+            self.remove(at: firstIndex(of: character) ?? self.endIndex)
+        }
+        return self
     }
 }
