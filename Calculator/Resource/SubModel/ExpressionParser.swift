81692b8dfd7c11f57e885b702bce13753671d77f
KayAhnDS <dkseotn123@gmail.com>
2022-05-26 19:57:04 +0900

    refactor: 매핑방식 수정

--- Calculator/Resource/SubModel/ExpressionParser.swift
+++ Calculator/Resource/SubModel/ExpressionParser.swift
@@ -8,7 +8,7 @@
 enum ExpressionParser {
     static func parse(from input: String) -> Formula {
         let operators = componentsByOperators(from: input)
-        let operands = input.split { Operator.allCases.map({ String($0.rawValue) }).contains(String($0)) }.map(String.init)
+        let operands = input.split { Operator.allCases.map({ String($0.rawValue) }).contains(String($0)) }.map { String($0) }
         
         let compactOperators = operators.compactMap({ $0 })
         let compactOperands = operands.compactMap({ $0 })
@@ -16,8 +16,8 @@ enum ExpressionParser {
         let optionalOperators = compactOperators.map { Operator(rawValue: Character($0))}
         let optionalOperands = compactOperands.map { Double($0) }
         
-        let retrievedOperators = CalculatorItemQueue<Operator>(queue: optionalOperators, head: 0)
-        let retrievedOperands = CalculatorItemQueue<Double>(queue: optionalOperands, head: 0)
+        let retrievedOperators = CalculatorItemQueue<Operator>(queue: optionalOperators)
+        let retrievedOperands = CalculatorItemQueue<Double>(queue: optionalOperands)
 
         return Formula(operands: retrievedOperands, operators: retrievedOperators)
     }
