cd61b02d2df1da9137b8b9995647b2511695c299
MinsuJeon <chso294@naver.com>
2022-05-26 16:39:15 +0900

    feat: CalculatorError extension 추가
    - CalculatorError extension의 LocalizedError 프로토콜 채택
    - 각각의 케이스에 대한 NSLocalizedString 값 할당

--- Calculator/Calculator/Model/CalculatorError.swift
+++ Calculator/Calculator/Model/CalculatorError.swift
@@ -4,10 +4,29 @@
 //
 //  Created by 전민수 on 2022/05/19.
 //
+import Foundation
+
 enum CalculatorError: Error {
     case dividedByZero
     case emptyQueues
     case notEnoughOperators
     case notEnoughOperands
     case invalidOperator
+}
+
+extension CalculatorError: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .dividedByZero:
+            return NSLocalizedString("NaN", comment: "Error: Divided By Zero")
+        case .emptyQueues:
+            return NSLocalizedString("empty queues", comment: "Error: Empty Queues")
+        case .notEnoughOperators:
+            return NSLocalizedString("not enough operators", comment: "Error: Not Enough Operators")
+        case .notEnoughOperands:
+            return NSLocalizedString("not enough operands", comment: "Error: Not Enough Operands")
+        case .invalidOperator:
+            return NSLocalizedString("invalid operator", comment: "Error: Invalid Operator")
+        }
+    }
 }
