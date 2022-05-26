06056cb91206a5c5a15d7697dd6123e7958e5f6a
ic_6_user_28 <toskw@naver.com>
2022-05-26 18:11:37 +0900

    feat: 숫자 버튼 액션 함수 구현

---
+++ Calculator/Calculator/Controller/CalculatorViewController.swift
@@ -0,0 +1,46 @@
+//
+//  Calculator - ViewController.swift
+//  Created by Kiwi. 
+//  Copyright © yagom. All rights reserved.
+// 
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+    private var userNumberTapped = false
+    
+    @IBOutlet weak var operatorLabel: UILabel!
+    @IBOutlet weak var operandLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+    }
+    
+    func setupViews() {
+        operandLabel.text = "0"
+        operatorLabel.text = ""
+    }
+    
+    @IBAction func didTapNumberButton(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        
+        if operandLabel.text == "0", digit == "0" || digit == "00" {
+            return
+        }
+        
+        if operandLabel.text!.count >= 20 {
+            return
+        }
+        
+        if userNumberTapped {
+            let textCurrentlyInDisply = operandLabel.text!
+            operandLabel.text! = textCurrentlyInDisply + digit
+        } else {
+            operandLabel.text! = digit
+        }
+        userNumberTapped = true
+    }
+}
+
+
