02f8e107b29a1b08a9de001532529695bc5316f0
KayAhnDS <dkseotn123@gmail.com>
2022-05-26 23:17:28 +0900

    refactor: ViewController에 UI연결

--- Calculator/Source/Controller/ViewController.swift
+++ Calculator/Source/Controller/ViewController.swift
@@ -7,10 +7,49 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // Buttons - Numbers
+    @IBOutlet weak var zeroButton: UIButton!
+    @IBOutlet weak var doubleZeroButton: UIButton!
+    @IBOutlet weak var oneButton: UIButton!
+    @IBOutlet weak var twoButton: UIButton!
+    @IBOutlet weak var threeButton: UIButton!
+    @IBOutlet weak var fourButton: UIButton!
+    @IBOutlet weak var fiveButton: UIButton!
+    @IBOutlet weak var sixButton: UIButton!
+    @IBOutlet weak var sevenButton: UIButton!
+    @IBOutlet weak var eightButton: UIButton!
+    @IBOutlet weak var nineButton: UIButton!
+    
+    
+    // Buttons - Symbols
+    @IBOutlet weak var dotButton: UIButton!
+    @IBOutlet weak var equalButton: UIButton!
+    @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var multiplyButton: UIButton!
+    @IBOutlet weak var divideButton: UIButton!
+    
+    // Buttons - Functions
+    @IBOutlet weak var acButton: UIButton!
+    @IBOutlet weak var ceButton: UIButton!
+    @IBOutlet weak var plusMinusButton: UIButton!
+    
+    
+    // Labels - Associate with Result
+    @IBOutlet weak var signLabel: UILabel!
+    @IBOutlet weak var resultLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
+    
+    @IBAction func clearCurrentCalculation(_ sender: UIButton) {
+        resultLabel.text = "0"
+    }
+    
 }
 
