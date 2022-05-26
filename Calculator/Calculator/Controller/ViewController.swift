30e779b7e33adaa79de3197c855496f4caeaa295
seohyeon2 <seo_hyeon418@naver.com>
2022-05-26 23:52:59 +0900

    feat: 레이블 및 키패트 버튼 아울렛 생성, 키패드버튼을 눌렀을 때 발생하는 메서드 구현

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -7,12 +7,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var inputNumberLabel: UILabel!
+    @IBOutlet weak var signLabel: UILabel!
+    @IBOutlet var numberButtons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func tapKeypadButton(_ sender: UIButton) {
+       let number = numberButtons.firstIndex(of: sender)
+    }
 }
 
