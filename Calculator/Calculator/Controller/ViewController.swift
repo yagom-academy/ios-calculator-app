73dd1a298124945bb917225c6b81ce40e620b5ed
MinsuJeon <chso294@naver.com>
2022-05-26 14:25:02 +0900

    feat: IBOutlet 변수 추가
    - IBOutlet 변수 numberInput 추가
    - IBOutlet 변수 operatorInput 추가
    - IBOutlet 변수 scrollView 추가
    - IBOutlet 변수 lastInput 추가

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -7,6 +7,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var numberInput: UILabel!
+    @IBOutlet weak var operatorInput: UILabel!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var lastInput: UIStackView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
