a96462863708236286108c165b9c723cec09d564
MinsuJeon <chso294@naver.com>
2022-05-27 12:11:39 +0900

    refactor: FormulaTests 내 operator 리팩토링

--- Calculator/FormulaTests/FormulaTests.swift
+++ Calculator/FormulaTests/FormulaTests.swift
@@ -33,7 +33,7 @@ class FormulaTests: XCTestCase {
     
     func test_result_피연산수가_뺄셈을_정상적으로_계산하는지_테스트() throws {
         // given
-        let input = "1-2-3"
+        let input = "1−2−3"
         let expectation: Double = -4
         
         // when
@@ -46,7 +46,7 @@ class FormulaTests: XCTestCase {
     
     func test_result_피연산수가_곱셈을_정상적으로_계산하는지_테스트() throws {
         // given
-        let input = "1*2*3"
+        let input = "1×2×3"
         let expectation: Double = 6
         
         // when
@@ -59,7 +59,7 @@ class FormulaTests: XCTestCase {
     
     func test_result_피연산수가_나눗셈을_정상적으로_계산하는지_테스트() throws {
         // given
-        let input = "10/2/5"
+        let input = "10÷2÷5"
         let expectation: Double = 1
 
         // when
@@ -72,7 +72,7 @@ class FormulaTests: XCTestCase {
     
     func test_result_복수의_연산자와_피연산수가_혼합되어_있는_문자열값을_정상적으로_계산하는지_테스트() throws {
         // given
-        let input = "10+2-3*4/6"
+        let input = "10+2−3×4÷6"
         let expectation: Double = 6
         
         // when
@@ -85,7 +85,7 @@ class FormulaTests: XCTestCase {
     
     func test_result_0으로_나눌때_오류처리가_정상적으로_이루어져_NaN의_값을_반환하는지_테스트() throws {
         // given
-        let input = "100/0"
+        let input = "100÷0"
         
         // when
         var parser = ExpressionParser.parse(from: input)
@@ -125,7 +125,7 @@ class FormulaTests: XCTestCase {
     
     func test_result_사칙연산자만으로_구성된_문자열이_입력될때_오류처리가_정상적으로_이루어지는지_테스트() throws {
         // given
-        let input = "+-*/"
+        let input = "+−×÷"
         
         // when
         var parser = ExpressionParser.parse(from: input)
@@ -151,7 +151,7 @@ class FormulaTests: XCTestCase {
     
     func test_result_연산자와_피연산수가_올바른_순서를_따르지_않을때_오류처리가_정상적으로_이루어지는지_테스트() throws {
         // given
-        let input = "+-*1/123"
+        let input = "+−×1÷123"
         
         // when
         var parser = ExpressionParser.parse(from: input)
@@ -177,7 +177,7 @@ class FormulaTests: XCTestCase {
     
     func test_result_소수점_아래_자리수가_큰_피연산수를_입력받았을때_정상적으로_값이_출력되는지_테스트() throws {
         // given
-        let input = "0.000001*0.000002"
+        let input = "0.000001×0.000002"
         let expectation = 0.000001 * 0.000002
        
         // when
@@ -190,7 +190,7 @@ class FormulaTests: XCTestCase {
     
     func test_result_실수인_수가_있는_피연산수를_입력받았을때_정상적으로_값이_출력되는지_테스트() throws {
         // given
-        let input = "4.3+1/5*10.2"
+        let input = "4.3+1÷5×10.2"
         let expectation = ((4.3 + 1) / 5) * 10.2
        
         // when
@@ -203,7 +203,7 @@ class FormulaTests: XCTestCase {
     
     func test_result_음수로_시작하는_피연산수를_입력받았을때_정상적으로_값이_출력되는지_테스트() throws {
         // given
-        let input = "-4.3+1/5*10.2"
+        let input = "-4.3+1÷5×10.2"
         let expectation = ((-4.3 + 1) / 5) * 10.2
        
         // when
