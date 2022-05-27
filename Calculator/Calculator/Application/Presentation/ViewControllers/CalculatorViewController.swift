b78978e05531990b79942b5267063078f0081255
OldStarKR <tyr2293@naver.com>
2022-05-27 16:57:03 +0900

    feat: 사용자 입력을 받아서 숫자 및 소수점을 표시

--- Calculator/Calculator/Application/Presentation/ViewControllers/CalculatorViewController.swift
+++ Calculator/Calculator/Application/Presentation/ViewControllers/CalculatorViewController.swift
@@ -7,8 +7,30 @@
 import UIKit
 
 class CalculatorViewController: UIViewController {
+    
+    @IBOutlet private weak var currentNumberLabel: UILabel!
+    
+    private var currentNumber: String = "0"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    @IBAction func pressNumbers(_ sender: UIButton) {
+        guard let number = sender.titleLabel?.text else {
+            return
+        }
+        
+        switch currentNumber {
+        case "0":
+            currentNumber = number
+        default:
+            currentNumber += number
+        }
+        
+        DispatchQueue.main.async {
+            self.currentNumberLabel.text = self.currentNumber
+        }
     }
 }
 
