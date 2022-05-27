48abbf3d256b6f73b73075b9503502965776edd5
OldStarKR <tyr2293@naver.com>
2022-05-27 19:55:16 +0900

    feat: 사용자 입력에 따라 현재 연산자 표시

--- Calculator/Calculator/Application/Presentation/ViewControllers/CalculatorViewController.swift
+++ Calculator/Calculator/Application/Presentation/ViewControllers/CalculatorViewController.swift
@@ -9,14 +9,31 @@ import UIKit
 class CalculatorViewController: UIViewController {
     
     @IBOutlet private weak var currentNumberLabel: UILabel!
+    @IBOutlet private weak var currentOperatorLabel: UILabel!
     
     private var currentNumber: String = "0"
+    private var currentOperator: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        refreshNumberLabel()
+        refreshOperatorLabel()
     }
     
-    @IBAction func pressNumbers(_ sender: UIButton) {
+    func refreshNumberLabel() {
+        DispatchQueue.main.async {
+            self.currentNumberLabel.text = self.currentNumber
+        }
+    }
+    
+    func refreshOperatorLabel() {
+        DispatchQueue.main.async {
+            self.currentOperatorLabel.text = self.currentOperator
+        }
+    }
+    
+    @IBAction func pressNumberButton(_ sender: UIButton) {
         guard let number = sender.titleLabel?.text else {
             return
         }
@@ -28,9 +45,19 @@ class CalculatorViewController: UIViewController {
             currentNumber += number
         }
         
-        DispatchQueue.main.async {
-            self.currentNumberLabel.text = self.currentNumber
+        refreshNumberLabel()
+    }
+    
+    
+    @IBAction func pressOperatorButton(_ sender: UIButton) {
+        guard let `operator` = sender.titleLabel?.text else {
+            return
         }
+        // TODO: 오퍼레이터가 없는 초기 상태일 경우 숫자를 입력하고 오퍼레이터를 입력하면 숫자가 올라가게끔
+        
+        currentOperator = `operator`
+        
+        refreshOperatorLabel()
     }
 }
 
