f67825913a5ba2f6f4814384e807badf0f4c8974
yeeton37 <lyeeun37@naver.com>
2022-05-27 23:42:55 +0900

    feat: ViewController 내부 IBOutlet 연결, numberFormatter 프로퍼티 선언 및 메서드(removeAllStackView) 구현

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -7,10 +7,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var displayNumber: UILabel!
+    @IBOutlet weak var displayOperator: UILabel!
+    @IBOutlet weak var recentInputStackView: UIStackView!
+    @IBOutlet weak var historyScrollView: UIScrollView!
+    
+    private let numberFormatter = NumberFormatter()
+    private let zeroString = "0"
+    private let emptyString = ""
+    var calculateItems = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        removeAllStackView(recentInputStackView)
+        displayNumber.text = zeroString
+        displayOperator.text = emptyString
+        
+        numberFormatter.numberStyle = .decimal
+        numberFormatter.minimumIntegerDigits = 1
+        numberFormatter.maximumIntegerDigits = 20
+        numberFormatter.maximumFractionDigits = 20
+    }
+    
+    func removeAllStackView(_ stack: UIStackView) {
+        stack.arrangedSubviews.forEach {
+            stack.removeArrangedSubview($0)
+            $0.removeFromSuperview()
+        }
+    }
+    
     }
 
 
