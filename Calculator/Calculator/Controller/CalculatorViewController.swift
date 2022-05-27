2caf95afb95a79f804fc4261ce677a57c40cfc9f
ic_6_user_28 <toskw@naver.com>
2022-05-27 12:19:08 +0900

    feat: 스크롤 뷰 구현 함수 생성

--- Calculator/Calculator/Controller/CalculatorViewController.swift
+++ Calculator/Calculator/Controller/CalculatorViewController.swift
@@ -6,7 +6,9 @@
 
 import UIKit
 
-class CalculatorViewController: UIViewController {
+final class CalculatorViewController: UIViewController {
+    var userInput: String = ""
+    
     private var userNumberTapped = false
     
     @IBOutlet weak var inputStackView: UIStackView!
@@ -42,11 +44,12 @@ class CalculatorViewController: UIViewController {
         }
         else if operandLabel.text!.contains(".") {
             userNumberTapped = true
-        }
-        else {
+        } else {
             operandLabel.text! = digit
         }
         userNumberTapped = true
+        userInput.append(digit)
+        print(userInput)
     }
     
     @IBAction func didTapDot(_ sender: UIButton) {
@@ -58,11 +61,70 @@ class CalculatorViewController: UIViewController {
         } else {
             operandLabel.text! = textCurrentlyInDisply + dot
         }
+        userInput.append(dot)
     }
     
     @IBAction func didTapOperatorButton(_ sender: UIButton) {
         let operators = sender.currentTitle!
+        addInputStack()
+        
         operatorLabel.text = operators
+        operandLabel.text = "0"
+        userNumberTapped = false
+        userInput.append(operators)
+        
+    }
+    
+    @IBAction func didTapPlusMinusSignButton(_ sender: UIButton) {
+        
+    }
+    
+    private func generateStackLabels() -> (UILabel, UILabel)? {
+        guard let `operator` = operatorLabel.text,
+              let number = operandLabel.text else {
+            return nil
+        }
+        let operatorStackLabel = UILabel()
+        let numberStackLabel = UILabel()
+        
+        operatorStackLabel.textColor = .white
+        operatorStackLabel.text = `operator`
+        
+        numberStackLabel.textColor = .white
+        numberStackLabel.text = number
+        
+        return (operatorStackLabel, numberStackLabel)
+    }
+    
+    private func generateStack() -> UIStackView? {
+        guard let (operatorStackLabel, numberStackLabel) = generateStackLabels() else {
+            return nil
+        }
+        let stack = UIStackView()
+        
+        stack.axis = .horizontal
+        stack.spacing = 8
+        stack.distribution = .fill
+        stack.alignment = .fill
+        
+        stack.addArrangedSubview(operatorStackLabel)
+        stack.addArrangedSubview(numberStackLabel)
+        
+        return stack
+    }
+    
+    private func addInputStack() {
+        guard let stack = generateStack() else {
+            return
+        }
+        
+        inputStackView.addArrangedSubview(stack)
+        scrollToBottom(scrollView)
+    }
+    
+    private func scrollToBottom(_ scrollView: UIScrollView) {
+        scrollView.layoutIfNeeded()
+        scrollView.setContentOffset(CGPoint(x: 0, y: scrollView.contentSize.height - scrollView.frame.height), animated: false)
     }
 }
 
