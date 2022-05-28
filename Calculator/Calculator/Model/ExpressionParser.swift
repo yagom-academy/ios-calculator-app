96f6688e618ebdedf0f5fc26063c68a1af57daf5
minsson <mskaang@gmail.com>
2022-05-28 15:35:03 +0900

    test: ExpressionParser 열거형 테스트 및 구현 완료

---
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -0,0 +1,37 @@
+//
+//  ExpressionParser.swift
+//  Calculator
+//
+//  Created by Minseong Kang on 2022/05/27.
+//
+
+import Foundation
+
+enum ExpressionParser {
+    static func parse(from input: String) -> Formula {
+        
+        var resultOperands = CalculatorItemQueue<Double>()
+        var resultOperators = CalculatorItemQueue<Operator>()
+        
+        let numbers: [String] = componentsByOperators(from: input)
+        let calculatorOperands = numbers.compactMap( { Double($0) } )
+
+        calculatorOperands.forEach( { resultOperands.enqueue($0) } )
+        
+        for char in input {
+            switch char {
+            case "+", "-", "*", "/":
+                resultOperators.enqueue(Operator(rawValue: char)!)
+            default:
+                continue
+            }
+        }
+ 
+        return Formula(operands: resultOperands, operators: resultOperators)
+    }
+    
+    static private func componentsByOperators(from input: String) -> [String] {
+        let numbers: [String] = input.components(separatedBy: ["+", "-", "*", "/"])
+        return numbers
+    }
+}
---
+++ Calculator/ExpressionParserTests/ExpressionParserTests.swift
@@ -0,0 +1,37 @@
+//
+//  ExpressionParserTests.swift
+//  ExpressionParserTests
+//
+//  Created by Minseong Kang on 2022/05/27.
+//
+
+import XCTest
+@testable import Calculator
+
+class ExpressionParserTests: XCTestCase {
+
+    // MARK: - Properties
+
+    var sut: ExpressionParser!
+    
+    // MARK: - Actions
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    // MARK: - parse(from:)
+    
+    func test_parse메서드_input계산식을_계산한_결과가_정확하다() {
+        // given
+        let input = "100+200-150*3/2"
+        
+        // when
+        let parsedFormula = ExpressionParser.parse(from: input)
+        
+        // then
+        XCTAssertEqual(parsedFormula.result(), Double(225.0))
+    }
+}
