c51881ec9e2c91163df6783fdf0ec24255e9cd6d
OldStarKR <tyr2293@naver.com>
2022-05-30 00:45:00 +0900

    fix: 계산이 끝난 이후에도 숫자를 입력할 수 있는 문제 수정

--- Calculator/Calculator/Application/Presentation/ViewControllers/CalculatorViewController.swift
+++ Calculator/Calculator/Application/Presentation/ViewControllers/CalculatorViewController.swift
@@ -101,6 +101,10 @@ extension CalculatorViewController {
 // MARK: - 각 버튼을 눌렀을 때의 동작을 위한 메서드
 extension CalculatorViewController {
     @IBAction private func pressNumberButton(_ sender: UIButton) {
+        if snippets.isNotEmpty && currentOperator.isEmpty {
+            return
+        }
+        
         guard let number = sender.titleLabel?.text else {
             return
         }
