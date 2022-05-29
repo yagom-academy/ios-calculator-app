d28835baa483eb34b5bb39629a96f6a47272e23c
derrickkim0109 <derrickkim0109@naver.com>
2022-05-29 17:53:38 +0900

    feat : CalculatorViewController 피연산자 & 연산자 VIew 연결 및 기능 구현

--- Calculator/Calculator/View/CalculatorViewController.swift
+++ Calculator/Calculator/View/CalculatorViewController.swift
@@ -6,8 +6,91 @@
 
 import UIKit
 
-class CalculatorViewController: UIViewController {
+//- MARK: View Properties
+
+final class CalculatorViewController: UIViewController {
+    @IBOutlet private weak var operandsLabel: UILabel?
+    @IBOutlet private weak var operatorsLabel: UILabel?
+    @IBOutlet private weak var formulaStackView: UIStackView?
+    @IBOutlet private weak var savedValueScrollView: UIScrollView?
+    
+
+//- MARK: View LifeCycle
+
+extension CalculatorViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setDefalut()
+    }
+}
+
+//- MARK: IBAction
+
+private extension CalculatorViewController {
+    @IBAction func operandsDidTapped(_ operands: UIButton) {
+        guard let value = operands.currentTitle else {
+            return
+        }
+        
+        self.fetchOperand(value)
+    }
+    
+    @IBAction func operatorsDidTapped(_ operators: UIButton) {
+        guard let value = operators.currentTitle,
+              isDefaultState == false else {
+            operatorsLabel?.text = operators.currentTitle
+            return
+        }
+        
+        self.fetchOperator(value)
+    }
+    
+    @IBAction func calculatorSignTapped(_ signs: UIButton) {
+    }
+}
+
+//- MARK: private funtions
+
+private extension CalculatorViewController {
+    func setDefalut() {
+        operandsLabel?.text = CalculatorState.zero.value
+        operatorsLabel?.text = CalculatorState.empty.value
+    }
+    
+    func fetchOperand(_ newValue: String) {
+        if isCalculated {
+            operandsLabel?.text = CalculatorState.empty.value
+        }
+    
+        guard let oldValue = operandsLabel?.text,
+              check(oldValue) else {
+            
+            operandsLabel?.text = newValue
+            return
+        }
+        
+        let result = oldValue + newValue
+        
+        if(isContainedDot) {
+            operandsLabel?.text = result
+            return
+        }
+        
+        if let number = numberFormatter.number(from: result.replacingOccurrences(of: ",", with: CalculatorState.empty.value)) {
+            operandsLabel?.text = numberFormatter.string(from: number)
+        }
+        
+        isTapped = false
+    }
+    
+    func fetchOperator(_ value: String) {
+        if (!isTapped) {
+            operatorsLabel?.text = value
+            operandsLabel?.text = CalculatorState.zero.value
+            isTapped = true
+        }
+    }
+    
     }
 }
