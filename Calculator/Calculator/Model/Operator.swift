f5176f160611fef007bbdf8f98088cff50cf98ef
웡빙 <wb277@naver.com>
2022-05-31 11:23:36 +0900

    refactor : 에러타입명 변경에 따른 네이밍 수정

--- Calculator/Calculator/Model/Operator.swift
+++ Calculator/Calculator/Model/Operator.swift
@@ -35,7 +35,7 @@ enum Operator: Character, CaseIterable, CalculateItem {
     }
     
     private func divide(lhs: Double, rhs: Double) throws -> Double {
-        guard rhs != 0 else { throw OperatorError.divideZero }
+        guard rhs != 0 else { throw CalculatorError.divideZero }
         return lhs / rhs
     }
     
