2e07307f7b679c29a912f075bcff496cb990c9d0
derrickkim0109 <derrickkim0109@naver.com>
2022-06-01 22:30:25 +0900

    chore : 불필요한 테스트 제거

--- Calculator/ExpressionParserTests/ExpressionParserTests.swift
+++ Calculator/ExpressionParserTests/ExpressionParserTests.swift
@@ -24,22 +24,6 @@ class ExpressionParserTests: XCTestCase {
         operands = nil
     }
     
-    func test_문자열로_들어온_값을_분리하여_연산자를_반환하다() {
-        // given
-        let str = "3.2 - 4.5 + 5 − 1 +"
-        let expectation = Operator.subtract
-        
-        // when
-        let char = str.split(with: " ")
-            .filter{ Double($0) == nil }
-            .compactMap{ Character($0) }
-        
-        let result = Operator(rawValue: char[0])
-        
-        // then
-        XCTAssertEqual(result, expectation)
-    }
-    
     func test_문자열로_들어온_값을_분리하여_Double_숫자를_반환하다() {
         // given
         let str = "3.2 4.5 5 - 1 +"
@@ -53,21 +37,7 @@ class ExpressionParserTests: XCTestCase {
         // then
         XCTAssertEqual(result, expectation)
     }
-    
-    func test_문자열로_들어온_값을_분리하여_연산자를_반환하다_Version2() {
-        // given
-        let str = "3.2 - 4.5 - 5 - 1 +"
-        let expectation = Operator.subtract
-        
-        // when
-        let formula = ExpressionParser.parse(from: str)
-        
-        let result = formula.operators.peekFirst
-        
-        // then
-        XCTAssertEqual(result, expectation)
-    }
-    
+  
     func test_문자열로_들어온_값을_분리하여_Double_숫자를_반환하다_Version2() {
         // given
         let str = "3.2 4.5 5 - 1 + -5"
@@ -87,20 +57,6 @@ class ExpressionParserTests: XCTestCase {
         XCTAssertEqual(result, expectation)
     }
     
-    func test_문자열로_들어온_값을_분리하여_operator를_fomular에_넣어서_첫째값을_반환하다() {
-        // given
-        let str = "3.2 4.5 5 - 1 +"
-        let expectation = Operator.subtract
-        
-        // when
-        let formula = ExpressionParser.parse(from: str)
-        
-        let result = formula.operators.peekFirst
-        
-        // then
-        XCTAssertEqual(result, expectation)
-    }
-    
     func test_문자열로_들어온_값을_분리하여_operand_를_fomular에_넣어서_첫째값을_반환하다() {
         // given
         let str = "3.2 - 4.5 + 5 - 1"
@@ -109,34 +65,6 @@ class ExpressionParserTests: XCTestCase {
         let formula = ExpressionParser.parse(from: str)
         let result = formula.operands.peekFirst
         
-        // then
-        XCTAssertEqual(result, expectation)
-    }
-    
-    func test_문자열로_들어온_값을_더하기_빼기_연산자를_사용하여_마이너스_12_반환하다() {
-        // given
-        let str = "3.0 - 4.0 - 5 - 1 + -5 "
-        let expectation: Double = -12.0
-        
-        // when
-        let formula = ExpressionParser.parse(from: str)
-
-        let result = formula.result()
-        
-        // then
-        XCTAssertEqual(result, expectation)
-    }
-    
-    func test_문자열로_들어온_값을_더하기_빼기_연산자를_사용하여_마이너스_121_반환하다() {
-        // given
-        let str = "3.0 × 4.3 - 5 - 1 ÷ -5 "
-        let expectation: Double = -1.3799999999999997
-    
-        // when
-        let formula = ExpressionParser.parse(from: str)
-        
-        let result = formula.result()
-
         // then
         XCTAssertEqual(result, expectation)
     }
