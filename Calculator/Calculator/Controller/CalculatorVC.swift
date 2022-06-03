3fcd65371704bb76eceae674be7485650da32a02
minsson <mskaang@gmail.com>
2022-06-04 00:32:49 +0900

    feat: 현재 입력내용을 화면 상단 입력 내역에 반영해주는 insertCurrentItemIntoHistory() 메서드 구현

--- Calculator/Calculator/Controller/CalculatorVC.swift
+++ Calculator/Calculator/Controller/CalculatorVC.swift
@@ -11,6 +11,8 @@ final class CalculatorVC: UIViewController {
     @IBOutlet private weak var operatorInputLabel: UILabel!
     @IBOutlet private weak var numberInputLabel: UILabel!
     
+    @IBOutlet private weak var inputHistoryStackView: UIStackView!
+    
     // MARK: - Life Cycle
         
     override func viewDidLoad() {
@@ -24,6 +26,39 @@ final class CalculatorVC: UIViewController {
 // MARK: - Actions
 
 extension CalculatorVC {
+    @objc func insertCurrentItemIntoHistory() {
+        let inputItemStackView = UIStackView()
+        inputItemStackView.translatesAutoresizingMaskIntoConstraints = false
+        inputItemStackView.axis = .horizontal
+        inputItemStackView.alignment = .fill
+        inputItemStackView.distribution = .fill
+        inputItemStackView.spacing = 8
+        
+        let operatorSignLabel = UILabel()
+        operatorSignLabel.translatesAutoresizingMaskIntoConstraints = false
+        operatorSignLabel.textColor = .white
+        operatorSignLabel.backgroundColor = .black
+        operatorSignLabel.text = "operator"
+        operatorSignLabel.font = UIFont.preferredFont(forTextStyle: .title3)
+        
+        let numberLabel = UILabel()
+        numberLabel.translatesAutoresizingMaskIntoConstraints = false
+        numberLabel.textColor = .white
+        numberLabel.backgroundColor = .black
+        numberLabel.text = "number"
+        numberLabel.font = UIFont.preferredFont(forTextStyle: .title3)
+        
+        inputItemStackView.addArrangedSubview(operatorSignLabel)
+        inputItemStackView.addArrangedSubview(numberLabel)
+        
+        inputItemStackView.isHidden = true
+        inputHistoryStackView.addArrangedSubview(inputItemStackView)
+        
+        UIStackView.animate(withDuration: 0.3) {
+            inputItemStackView.isHidden = false
+        }
+    }
+    
     @IBAction private func numberButtonTapped(_ sender: UIButton) {
         if numberInputLabel.text == "0" {
             numberInputLabel.text = ""
@@ -34,6 +69,7 @@ extension CalculatorVC {
     
     @IBAction private func operatorButtonTapped(_ sender: UIButton) {
         operatorInputLabel.text = sender.currentTitle
+        insertCurrentItemIntoHistory()
     }
 }
 
