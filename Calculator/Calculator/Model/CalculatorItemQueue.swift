58c227ea597f9c977faa4f27b20f4413af15c6e5
oneStar92 <onestar7984@gmail.com>
2022-09-19 22:02:33 +0900

    feat: CalculatorItemQueue 정의.
    UML에 적힌대로 CalculatorItemQueue를 디자인함.

--- Calculator/Calculator/Model/CalculatorItemQueue.swift
+++ Calculator/Calculator/Model/CalculatorItemQueue.swift
@@ -1,1 +1,26 @@
 protocol CalculateItem { }
+
+struct CalculatorItemQueue<T>: CalculateItem {
+    private var head: Node<T>? = nil
+    private var tail: Node<T>? = nil
+    private(set) count: Int = 0
+    var isEmpty: Bool {
+        return count == 0
+    }
+    
+    func enqueue(_ element: T) {
+        
+    }
+    
+    func dequeue() -> T? {
+        
+    }
+    
+    func peak() -> T {
+        
+    }
+    
+    func clear() {
+        
+    }
+}
