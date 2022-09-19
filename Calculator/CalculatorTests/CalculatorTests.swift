7906f4bf7cfc00981cd091b468836d21684a7605
sunnyCookie <107384230+sunny-maeng@users.noreply.github.com>
2022-09-19 21:08:55 +0900

    feat: unit test 파일추가

---
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -0,0 +1,24 @@
+//
+//  CalculatorTests.swift
+//  CalculatorTests
+//
+//  Created by 맹선아 on 2022/09/19.
+//
+
+import XCTest
+@testable import Calculator
+
+class CalculatorTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        super .setUpWithError()
+    }
+
+    override func tearDownWithError() throws {
+        super .tearDownWithError()
+    }
+
+    func testExample() throws {
+
+    }
+}
