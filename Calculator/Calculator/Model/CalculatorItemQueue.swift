f2ec33a2a6b19d73ee3c13ba6b7b568158f5d561
sunnyCookie <107384230+sunny-maeng@users.noreply.github.com>
2022-09-19 21:46:05 +0900

    feat: CalculatorItemQueue구조체 구현( 프로퍼티itemQueue,index와 메서드 enquene(), dequeue() 구현)

---
+++ Calculator/Calculator/Model/CalculatorItemQueue.swift
@@ -0,0 +1,33 @@
+//
+//  CalculatorItemQueue.swift
+//  Calculator
+//
+//  Created by 맹선아 on 2022/09/19.
+//
+
+import Foundation
+
+struct CalculatorItemQueue {
+    var itemQueue: [(arithmetics: String, number: Int)?] = []
+    var index:Int = 0
+    
+    mutating func enqueue(_ data: (String, Int)) {
+        itemQueue.append(data)
+    }
+    
+    mutating func dequeue() -> (String, Int)? {
+        guard let data = itemQueue[index] else {
+            return nil
+        }
+        
+        itemQueue[index] = nil
+        index += 1
+        
+        return data
+    }
+    
+    
+    
+    
+    
+}
