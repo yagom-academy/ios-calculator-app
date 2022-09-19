013f15e170b9d3a75cd844ab56dd13cfa812fb28
wonbi92 <bin9239@gmail.com>
2022-09-19 21:19:33 +0900

    feat:Unit Test 추가

---
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -0,0 +1,20 @@
+//
+//  CalculatorTests.swift
+//  CalculatorTests
+//
+//  Created by Wonbi on 2022/09/19.
+//
+
+import XCTest
+@testable import Calculator
+
+class CalculatorTests: XCTestCase {
+    
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+    }
+
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+    }
+}
