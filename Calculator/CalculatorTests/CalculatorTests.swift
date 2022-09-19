2541c34381c1d26cf75e1fdad077b05019ad173f
lnhk98 <hozzang666@gmai.com>
2022-09-19 19:39:14 +0900

    feat: unit Test를 위한 CalculatorTests 생성

---
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -0,0 +1,19 @@
+//
+//  CalculatorTests.swift
+//  CalculatorTests
+//
+//  Created by 김인호 on 2022/09/19.
+//
+
+import XCTest
+
+class CalculatorTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+}
