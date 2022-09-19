2dbb880013bc181f915b7b7e7b92da8dc0ca4261
wonbi92 <bin9239@gmail.com>
2022-09-19 23:25:44 +0900

    feat: CalculatorItemQueue타입 구현, isEmpty프로퍼티 구현

---
+++ Calculator/Calculator/CalculatorItemQueue.swift
@@ -0,0 +1,16 @@
+//
+//  CalculatorItemQueue.swift
+//  Calculator
+//
+//  Created by Wonbi on 2022/09/19.
+//
+
+import Foundation
+
+struct CalculatorItemQueue {
+    var data: [Double] = []
+    
+    var isEmpty: Bool {
+        data.isEmpty
+    }
+}
--- Calculator/CalculatorTests/CalculatorTests.swift
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -9,12 +9,27 @@ import XCTest
 @testable import Calculator
 
 class CalculatorTests: XCTestCase {
+    var sut: CalculatorItemQueue!
     
     override func setUpWithError() throws {
         try super.setUpWithError()
+        sut = CalculatorItemQueue()
     }
 
     override func tearDownWithError() throws {
         try super.tearDownWithError()
+        sut = nil
+    }
+    
+    func test_Queue가빈값일때_true를반환하는지() {
+        // given
+        let input: [Double] = []
+        
+        // when
+        sut.data = input
+        let result = sut.isEmpty
+        
+        // then
+        XCTAssertTrue(result)
     }
 }
