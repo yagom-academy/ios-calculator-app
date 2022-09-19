d31b5008b51b09123d772910504443cb46d2b620
oneStar92 <onestar7984@gmail.com>
2022-09-19 23:02:09 +0900

    test: CalculatorItemQueue dequeue Test Case 추가.
    1. 빈 queue의 dequeue Test case
    2. 입력된 1개의 값의 dequeue에 대한 Test case
    3. 여러개의 입력값 중 dequeue에 대한 Test case
    3. 여러개의 입력값이 dequeue로 순서대로 나오는지 Test case

--- Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
+++ Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
@@ -49,4 +49,54 @@ class CalculatorItemQueueTests: XCTestCase {
         XCTAssertEqual(input[0], queue.head?.data)
         XCTAssertEqual(input[2], queue.tail?.data)
     }
+    
+    func test_빈_queue의_dequeue의_값이_nil인가() {
+        //when
+        let queue: CalculatorItemQueue<Int> = CalculatorItemQueue<Int>()
+        
+        //then
+        XCTAssertEqual(nil, queue.dequeue())
+    }
+    
+    func test_입력값_1_이_enqueue되어서_queue의_dequeue의_값이_input과_같은가() {
+        //given
+        let input = 1
+        
+        //when
+        var queue: CalculatorItemQueue<Int> = CalculatorItemQueue<Int>()
+        queue.enqueue(input)
+        
+        //then
+        XCTAssertEqual(input, queue.dequeue())
+    }
+    
+    func test_입력값_1_2_3_이_순서대로_enqueue되어서_queue의_dequeue의_값이_첫번째_input값과_같은가() {
+        //given
+        let input = [1,2,3]
+        
+        //when
+        var queue: CalculatorItemQueue<Int> = CalculatorItemQueue<Int>()
+        queue.enqueue(input[0])
+        queue.enqueue(input[1])
+        queue.enqueue(input[2])
+        
+        //then
+        XCTAssertEqual(input[0], queue.dequeue())
+    }
+    
+    func test_입력값_1_2_3_이_순서대로_enqueue되어서_queue의_dequeue의_값이_input값과_순서가_같은가() {
+        //given
+        let input = [1,2,3]
+        
+        //when
+        var queue: CalculatorItemQueue<Int> = CalculatorItemQueue<Int>()
+        queue.enqueue(input[0])
+        queue.enqueue(input[1])
+        queue.enqueue(input[2])
+        
+        //then
+        XCTAssertEqual(input[0], queue.dequeue())
+        XCTAssertEqual(input[1], queue.dequeue())
+        XCTAssertEqual(input[2], queue.dequeue())
+    }
 }
