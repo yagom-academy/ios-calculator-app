a06eeff867573610f8488fa39a2317442098de43
oneStar92 <onestar7984@gmail.com>
2022-09-19 22:19:34 +0900

    test: Node Unit Test Case 추가.
    1. Node의 인스턴스화가 제대로 이루어 지는지 확인하는 Test Case
    2. 두개이상의 Node가 연결되는지 확인하는 Test Case

---
+++ Calculator/NodeTests/NodeTests.swift
@@ -0,0 +1,35 @@
+//
+//  NodeTests.swift
+//  NodeTests
+//
+//  Created by Ayaan on 2022/09/19.
+//
+
+import XCTest
+
+class NodeTests: XCTestCase {
+
+    func test_input값으로_Node를_생성하면_생성된_Node의_data이랑_Input값이_같은가() {
+        //given
+        let input = "test"
+        
+        //when
+        let node = Node(data: input)
+        
+        //then
+        XCTAssertEqual(input, node.data)
+    }
+    
+    func test_입력된_두개의_값으로_Node를_생성하고_첫번째_Node의_next를_두번째_Node로_지정하면_첫번째_Node의_next의_data와_두번째_Node의_data가_같은가() {
+        //given
+        let input = ["first", "second"]
+        
+        //when
+        let firstNode = Node(data: input[0])
+        let secondNode = Node(data: input[1])
+        firstNode.next = secondNode
+        
+        //then
+        XCTAssertEqual(firstNode.next?.data, secondNode.data)
+    }
+}
