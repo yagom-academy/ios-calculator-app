6bb8473078e16a80105b825b0d023f571340c89f
sunnyCookie <107384230+sunny-maeng@users.noreply.github.com>
2022-09-19 22:30:03 +0900

    feat: Node 클래스 구현

---
+++ Calculator/Calculator/Model/LinkedList.swift
@@ -0,0 +1,22 @@
+//
+//  Node.swift
+//  Calculator
+//
+//  Created by 맹선아 on 2022/09/19.
+//
+
+import Foundation
+
+class Node <T> {
+    var data: T?
+    var next: Node?
+    
+    init (data: T?, next: Node? = nil) {
+        self .data = data
+        self .next = next
+    }
+}
+
+
+
+
