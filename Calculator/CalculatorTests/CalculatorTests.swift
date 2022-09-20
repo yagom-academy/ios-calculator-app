c6f71907b7c1854b8c21a6b993cd56dd608903f7
LJ <swtw72@gmail.com>
2022-09-20 23:12:15 +0900

    feat: 테스트 추가

--- Calculator/CalculatorTests/CalculatorTests.swift
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -24,6 +24,13 @@ class CalculatorTests: XCTestCase {
         measure {
         }
     }
+    func test_3개의값이_큐에_정상적으로_추가되는지() {
+        sut.enqueue(element: 10)
+        sut.enqueue(element: 100)
+        sut.enqueue(element: 200)
+        let result: [Double] = sut.queue
+        XCTAssertEqual(result, [10, 100, 200])
+    }
     func test_더하기연산_성공하는지() {
         let queue: [Double] = [1,2]
         let compute: Operator = .plus
@@ -38,7 +45,7 @@ class CalculatorTests: XCTestCase {
         XCTAssertEqual(result, 2.0)
     }
     func test_곱하기연산_성공하는지() {
-        let queue: [Double] = [1500,3000]
+        let queue: [Double] = [1500, 3000]
         let compute: Operator = .multiplication
         let value: Double = compute.operate(lhs: queue[0], rhs: queue[1])
         let numberFormatter = NumberFormatter()
@@ -51,7 +58,7 @@ class CalculatorTests: XCTestCase {
         XCTAssertEqual(result, "4,500,000")
     }
     func test_나누기연산_성공하는지() {
-        let queue: [Double] = [10000,3]
+        let queue: [Double] = [10000, 3]
         let compute: Operator = .division
         let value: Double = compute.operate(lhs: queue[0], rhs: queue[1])
         let numberFormatter = NumberFormatter()
