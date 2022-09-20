251b0017a3996be11b3d8d3ffa44b27ec9557fc6
woong <workplayhard1@naver.com>
2022-09-20 17:36:24 +0900

    feat: 불필요주석제거

--- Calculator/Calculator/CalculatorItemQueue.swift
+++ Calculator/Calculator/CalculatorItemQueue.swift
@@ -21,17 +21,14 @@ struct CalculatorItemQueue: CalculateItem {
         return queue.isEmpty
     }
     
-    // queue의 맨뒤에 새로운 요소를 추가하는 메서드
     public mutating func enqueue(_ element: String) {
         queue.append(element)
     }
     
-    // queue의 첫번째 요소를 반환하고 제거하는 메서드
     public mutating func dequeue() -> String? {
         return isEmpty ? nil : queue.removeFirst()
     }
     
-    // AC버튼 초기화
     public mutating func clear() {
         queue.removeAll()
     }
