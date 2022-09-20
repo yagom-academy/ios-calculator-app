cce679cc2faab376f9f48e4c0d5f1850c76a9fc0
ash-youu <ash.you.dev@gmail.com>
2022-09-20 20:56:01 +0900

    feat: 기능 구현을 위해 연결리스트와 큐에서 가져야할 프로퍼티 및 메서드 명시

--- Calculator/Calculator/Model/CalculatorItemQueue.swift
+++ Calculator/Calculator/Model/CalculatorItemQueue.swift
@@ -15,4 +15,42 @@ struct LinkedList<T> {
             self.next = next
         }
     }
+    
+    var head: Node<T>?
+    var tail: Node<T>?
+    var nodeCount: Int = 0
+    var isEmpty: Bool {
+        if nodeCount == 0 {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func append(node: Node<T>) {
+    }
+    
+    func removeLast(node: Node<T>) {
+    }
+    
+    func removeAll() {
+    }
+}
+
+struct CalculatorItemQueue<T> {
+    private var list: LinkedList<T> = LinkedList<T>()
+    
+    var count: Int {
+        return list.nodeCount
+    }
+    
+    var isEmpty: Bool {
+        return list.isEmpty
+    }
+    
+    func enqueue(_ element: T) {
+    }
+    
+    func dequeue() {
+    }
 }
