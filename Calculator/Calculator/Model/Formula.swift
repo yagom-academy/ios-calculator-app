a20664b7035e5ac0f9c01a1c6fd2ee23587a2c99
lws2269 <lws2269@kakao.com>
2022-09-21 01:48:25 +0900

    refactor: Formula 구조체 result 함수 리팩토링, Formula 1+1=2 값 테스트

--- Calculator/Calculator/Model/Formula.swift
+++ Calculator/Calculator/Model/Formula.swift
@@ -11,8 +11,18 @@ struct Formula {
     var operands = CalculatorItemQueue()
     var operators = CalculatorItemQueue()
     
-    public func result() -> Double {
+    public mutating func result() -> Double {
+        // 여기서 숫자를 하나 빼고 시작하면 operands와 operators 의 길이가 같아짐
+        guard var totalNumber = operands.dequeue() as? Double else { return 0.0 }
         
-        return 0.0
+        for _ in 1...operands.count {
+            // 연산값
+            guard let number = self.operands.dequeue() as? Double else { return 0.0 }
+            // 연산자
+            guard let operatorSymbol = self.operators.dequeue() as? Operator else{ return 0.0 }
+            
+            totalNumber = operatorSymbol.calculate(lhs: totalNumber, rhs: number)
+        }
+        return totalNumber
     }
 }
--- Calculator/FormulaTests/FormulaTests.swift
+++ Calculator/FormulaTests/FormulaTests.swift
@@ -21,4 +21,13 @@ class FormulaTests: XCTestCase {
         sut = nil
     }
     
+    func test_1더하기_1을했을때_2이_되는지(){
+        let result:Double = 2
+        
+        sut.operands.enqueue(1)
+        sut.operands.enqueue(1)
+        sut.operators.enqueue(Operator.add)
+        
+        XCTAssertEqual(result, sut.result())
+    }
 }
