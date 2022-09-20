aee98543d6063c0c69e7532b6f517bc1a6ada9e9
woong <workplayhard1@naver.com>
2022-09-20 16:02:00 +0900

    feat: clear 메서드 추가

--- Calculator/Calculator/CalculatorItemQueue.swift
+++ Calculator/Calculator/CalculatorItemQueue.swift
@@ -10,8 +10,8 @@ import Foundation
 protocol CalculateItem {
 }
 
-struct CalculatorItemQueue<T>: CalculateItem {
-    private var queue: [T] = []
+struct CalculatorItemQueue: CalculateItem {
+    private var queue: [String] = []
     
     public var count: Int {
         return queue.count
@@ -22,11 +22,17 @@ struct CalculatorItemQueue<T>: CalculateItem {
     }
     
     // queue의 맨뒤에 새로운 요소를 추가하는 메서드
-    public mutating func enqueue(_ element: T) {
+    public mutating func enqueue(_ element: String) {
         queue.append(element)
     }
     
     // queue의 첫번째 요소를 반환하고 제거하는 메서드
-    public mutating func dequeue() -> T? {
+    public mutating func dequeue() -> String {
         return isEmpty ? nil : queue.removeFirst()
     }
+    
+    // AC버튼 초기화
+    public mutating func clear() {
+        queue.removeAll()
+    }
+}
