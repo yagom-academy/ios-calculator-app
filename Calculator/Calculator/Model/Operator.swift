95ff899f3bf26d93d124c3e8c2bfa9a0215873e2
lws2269 <lws2269@kakao.com>
2022-09-20 23:37:35 +0900

    feat: Operator enum 타입 추가

---
+++ Calculator/Calculator/Model/Operator.swift
@@ -0,0 +1,36 @@
+//
+//  Operator.swift
+//  Calculator
+//
+//  Created by leewonseok on 2022/09/20.
+//
+
+import Foundation
+
+enum Operator: Character, CaseIterable {
+    case add = "+"
+    case subtract = "-"
+    case divide = "/"
+    case multiply = "*"
+    
+    public func calculate(lhs: Double, rhs: Double) -> Double {
+        return 0.0
+    }
+    
+    private func add(lhs: Double, rhs: Double) -> Double {
+        return lhs + rhs
+    }
+    
+    private func subtract(lhs: Double, rhs: Double) -> Double {
+        return lhs - rhs
+    }
+    
+    private func divide(lhs: Double, rhs: Double) -> Double {
+        return lhs / rhs
+    }
+    
+    private func multiply(lhs: Double, rhs: Double) -> Double {
+        return lhs * rhs
+    }
+    
+}
