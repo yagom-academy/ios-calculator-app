1fff88808f166683a83c253bc840f219559d0216
chacha <happyhy0429@gmail.com>
2022-09-20 09:58:02 +0900

    test:CalculatorItemQueue타입내 count메서드를 위한 테스트코드작성

--- Calculator/Calculator/CalculatorItemQueue.swift
+++ Calculator/Calculator/CalculatorItemQueue.swift
@@ -1,13 +1,21 @@
 //  Created by bella on 2022/09/19.
 
 struct CalculatorItemQueue<T> {
-    var queue: [Int] = []
+    var queue: [T?] = []
     // 제네릭을 넣어주어 테스트를 진행했으나, 두개의 값이 비교가 되지 않아, 비교가 가능한 타입으로 바꾸어주었다.
+    // 다시 제네릭으로 돌아옴..ㅋㅋㅋ
     private var head: Int = 0
     
     // 반환타입 없으니까, 변수에 값 할당 안됨!!!!
     mutating func enqueue(element: T) {
-        guard let newElement = element as? Int else { return }
-        queue.append(newElement)
+        queue.append(element)
+    }
+    
+    mutating func count() -> Int {
+        return queue.count
     }
 }
+
+
+// 포문돌려서 안에잇는 녀석들이 Int인지...하나씩 담아주자
+// 근데 그것을 위해서는 본래 메서드에서 해주는것인가!
--- Calculator/CalculatorTests/CalculatorTests.swift
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -6,7 +6,7 @@ import XCTest
 @testable import Calculator
 
 class CalculatorTests: XCTestCase {
-    var sut: CalculatorItemQueue<Any>!
+    var sut: CalculatorItemQueue<String>!
     //var sup: CalculatorItemQueue<String>! 예시처럼 하려고 했으나!
     //처음에 몽땅 스트링으로 받아와서, 인트 필요시 인트로 변환해서 비교
     
@@ -25,21 +25,26 @@ class CalculatorTests: XCTestCase {
         //given
         let input = 2
         //when
-        sut.enqueue(element: input)
+        sut.enqueue(element: String(input))
         //then
-        XCTAssertEqual(sut.queue, [2])
+        XCTAssertEqual(sut.queue, ["2"])
     }
     
-    func test_enqueue호출시여러개를넣었을때처음입력값과첫번째인덱스가일치하는지() {
+    func test_enqueue호출시값을넣었을때입력값과첫번째인덱스가일치하는지() {
         //given
-        let inputArray:[Any] = [2,4,5,"+"]
+        let input:String = "2"
         //when
-        var result:[Int] = []
-        for input in inputArray {
-            sut.enqueue(element: input)
-        }
-        result = sut.queue
+        sut.enqueue(element: input)
         //then
-        XCTAssertEqual(result[0], 2)
+        XCTAssertEqual(sut.queue, ["2"])
+    }
+    
+    func test_count호출시값이잘세지는지() {
+        //given
+        sut.queue = ["1","2"]
+        //when
+        let result = sut.count()
+        //then
+        XCTAssertEqual(result, 2)
     }
 }
