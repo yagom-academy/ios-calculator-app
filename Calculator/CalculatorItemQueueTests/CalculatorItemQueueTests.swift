ef95c7860fbadb1b5337b14ec826fbb5ac5e699f
Gundy <korea1880@naver.com>
2022-09-21 05:54:21 +0900

    refactor: 클래스 CalculatorItemQueueTests의 테스트 케이스 수정
    - CalculatorItemQueueTests 클래스의 sut 프로퍼티 관련 수정
    - CalculatorItemQueueTests 클래스의 테스트 메서드 네이밍 수정
    - CalculatorItemQueueTests 클래스의 테스트 메서드 컨벤션 관련(given-when-then 구분 명확하도록) 수정

--- Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
+++ Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
@@ -9,11 +9,11 @@ import XCTest
 @testable import Calculator
 
 class CalculatorItemQueueTests: XCTestCase {
-    var sut: CalculatorItemQueue<String>!
+    var sut: CalculatorItemQueue!
     
     override func setUpWithError() throws {
         try super.setUpWithError()
-        sut = CalculatorItemQueue<String>()
+        sut = CalculatorItemQueue()
     }
     
     override func tearDownWithError() throws {
@@ -22,189 +22,173 @@ class CalculatorItemQueueTests: XCTestCase {
     }
     
     func test_enqueue호출시_data가비어있는지() {
-        let input = Int.random(in: 1...9)
+        let input = "0"
         
-        sut.enqueue(String(input))
+        sut.enqueue(input)
         
         XCTAssertFalse(sut.data.isEmpty)
     }
     
     func test_enqueue호출시_3을전달하면_data의마지막값과같은지() {
-        let input = 3
-        sut.enqueue(String(input))
+        let input = "3"
         
-        let result = sut.data[sut.data.count-1]
-        
-        XCTAssertEqual(result, "3")
-    }
-    
-    func test_enqueue반복호출시_data의마지막값과_전달값7이같은지() {
-        for _ in 1...5 {
-            sut.enqueue(String(Int.random(in: 0...9)))
+        sut.enqueue(input)
+        if let result = sut.data.last as? String{
+            XCTAssertEqual(result, "3")
         }
-        let input = 7
-        sut.enqueue(String(input))
-        
-        let result = sut.data[sut.data.count-1]
-        
-        XCTAssertEqual(result, "7")
     }
-    
+
+    func test_enqueue반복호출시_data의마지막값과_전달값의마지막값이같은지() {
+        let input = ["1", "3", "5", "7"]
+        
+        input.forEach{ sut.enqueue($0) }
+        if let result = sut.data.last as? String{
+            XCTAssertEqual(result, "7")
+        }
+    }
+
     func test_data가있을때_dequeue호출시_반환값이있는지() {
-        sut.data = [String(Int.random(in: 0...9))]
-        
+        sut.data = ["1"]
+
         let result = sut.dequeue()
-        
+
         XCTAssertNotNil(result)
     }
-    
+
     func test_data가없을때_dequeue호출시_nil을반환하는지() {
         sut.data = []
-        
+
         let result = sut.dequeue()
-        
+
         XCTAssertNil(result)
     }
-    
+
     func test_data가있을때_dequeue호출시_data의카운트가감소하는지() {
-        sut.data = [String(Int.random(in: 0...9))]
-        let originCount: Int = sut.data.count
-        
-        let _ = sut.dequeue()
+        sut.data = ["1", "3", "5", "7"]
+
+        sut.dequeue()
         let result = sut.data.count
-        
-        XCTAssert(result < originCount)
+
+        XCTAssert(result < 4)
     }
-    
+
     func test_data가있을때_peek호출시_반환값이있는지() {
-        sut.data = [String(Int.random(in: 0...9))]
-        
-        let result = sut.peek()
-        
+        sut.data = ["1"]
+
+        let result = sut.peek
+
         XCTAssertNotNil(result)
     }
-    
+
     func test_data가없을때_peek호출시_nil을반환하는지() {
         sut.data = []
-        
-        let result = sut.dequeue()
-        
+
+        let result = sut.peek
+
         XCTAssertNil(result)
     }
-    
-    func test_data가있을때_peek호출시_data의카운트가감소하는지() {
-        sut.data = [String(Int.random(in: 0...9))]
-        let originCount: Int = sut.data.count
-        
-        let _ = sut.dequeue()
+
+    func test_data가있을때_peek호출시_data의카운트가변하는지() {
+        sut.data = ["1", "3", "5", "7"]
+
+        let _ = sut.peek
         let result = sut.data.count
-        
-        XCTAssert(result < originCount)
+
+        XCTAssert(result == 4)
     }
-    
+
     func test_data가있을때_clear호출시_data가빈배열인지() {
-        sut.data = [String(Int.random(in: 0...9))]
+        sut.data = ["1", "3", "5", "7"]
+        
         sut.clear()
-        
         let result = sut.data
-        
+
         XCTAssert(result.isEmpty)
     }
-    
-    func test_data가있을때_count호출시_nil이아닌지() {
-        sut.data = [String(Int.random(in: 0...9))]
-        
+
+    func test_data가있을때_count호출시_0이아닌지() {
+        sut.data = ["1"]
+
         let result = sut.count
-        
-        XCTAssertNotNil(result)
+
+        XCTAssertNotEqual(result, 0)
     }
-    
-    func test_data의요소가5개일때_count호출시_5를반환하는지() {
-        for _ in 1...5 {
-            sut.data += [String(Int.random(in: 0...9))]
-        }
-        
+
+    func test_data의요소가4개일때_count호출시_4를반환하는지() {
+        sut.data = ["1", "3", "5", "7"]
+
         let result = sut.count
-        
-        XCTAssertEqual(result, 5)
+
+        XCTAssertEqual(result, 4)
     }
-    
-    func test_data의요소가1개일때_count호출시_1를반환하는지() {
-        sut.data = [String(Int.random(in: 0...9))]
-        
-        let result = sut.count
-        
-        XCTAssertEqual(result, 1)
-    }
-    
-    func test_data의요소가1개일때_isEmpty호출시_false를반환하는지() {
-        sut.data = [String(Int.random(in: 0...9))]
-        
-        let result = sut.isEmpty()
-        
+
+    func test_data의요소가있을때_isEmpty호출시_false를반환하는지() {
+        sut.data = ["1"]
+
+        let result = sut.isEmpty
+
         XCTAssertFalse(result)
     }
-    
+
     func test_data의요소가없을때_isEmpty호출시_true를반환하는지() {
         sut.data = []
-        
-        let result = sut.isEmpty()
-        
+
+        let result = sut.isEmpty
+
         XCTAssertTrue(result)
     }
-    
-    func test_capacity와count가같을때_isFull호출시_capacity와count의비교와같은지() {
+
+    func test_capacity와count가같을때_isFull호출시_true를반환하는지() {
         sut.data.reserveCapacity(sut.data.count)
-        
-        let result = sut.isFull()
-        
-        XCTAssertEqual(result, sut.data.capacity == sut.data.count)
+
+        let result = sut.isFull
+
+        XCTAssertTrue(result)
     }
-    
-    func test_capacity와count가다를때_isFull호출시_capacity와count의비교와같은지() {
+
+    func test_capacity와count가다를때_isFull호출시_false를반환하는지() {
         sut.data.reserveCapacity(sut.data.count + 1)
-        
-        let result = sut.isFull()
-        
-        XCTAssertEqual(result, sut.data.capacity == sut.data.count)
+
+        let result = sut.isFull
+
+        XCTAssertFalse(result)
     }
-    
+
     func test_capasity_getter호출시_현재용량과같은지() {
         let result = sut.capacity
-        
+
         XCTAssertEqual(result, sut.data.capacity)
     }
-    
-    func test_capasity_setter호출시_기존용량보다큰지() {
+
+    func test_capasity_setter호출시_기존용량과다른지() {
         let originCapacity = sut.capacity
-        
+
         sut.capacity += 1
         let result = sut.capacity
-        
-        XCTAssert(result > originCapacity)
+
+        XCTAssertNotEqual(result, originCapacity)
     }
-    
-    func test_인덱스범위내에서_insert호출시_인덱스2에_입력값9이_위치하는지() {
-        let input = 9
-        for _ in 1...5 {
-            sut.data += [String(Int.random(in: 0...8))]
+
+    func test_인덱스범위내에서_insert호출시_인덱스2에_입력값9가_위치하는지() {
+        let input = "9"
+        sut.data = ["1", "3", "5", "7"]
+        
+        sut.insert(input, at: 2)
+        if let result = sut.data[2] as? String {
+            XCTAssertEqual(result, "9")
         }
-        sut.insert(String(input), at: 2)
-        
-        let result = sut.data[2]
-        
-        XCTAssertEqual(Int(result), 9)
     }
-    
-    func test_인덱스범위내에서_removeAtIndex호출시_인덱스2의_값이변하는지() {
-        sut.data = ["1", "2", "3", "4", "5"]
+
+    func test_인덱스범위내에서_remove호출시_인덱스의_값이변하는지() {
+        sut.data = ["1", "3", "5", "7"]
         let input = 2
-        let originValue = sut.data[input]
+        guard let originValue = sut.data[input] as? String else {
+            return
+        }
         
-        sut.removeAtIndex(input)
-        
-        let result = sut.data[input]
-        
-        XCTAssertNotEqual(result, originValue)
+        sut.remove(at: input)
+        if let result = sut.data[input] as? String {
+            XCTAssertNotEqual(result, originValue)
+        }
     }
 }
