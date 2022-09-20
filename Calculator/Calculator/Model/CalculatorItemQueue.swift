3f9a71f399077c0dc03a701e5c62268c5ef8c4f1
lnhk98 <hozzang666@gmai.com>
2022-09-20 18:16:23 +0900

    feat: Node타입 파일 분리

--- Calculator/Calculator/Model/CalculatorItemQueue.swift
+++ Calculator/Calculator/Model/CalculatorItemQueue.swift
@@ -42,12 +42,3 @@ class CalculatorItemQueue<T>: CalculateItem {
     }
 }
 
-class Node<T> {
-    var data: T?
-    var next: Node?
-    
-    init(data: T?, next: Node? = nil) {
-        self.data = data
-        self.next = next
-    }
-}
