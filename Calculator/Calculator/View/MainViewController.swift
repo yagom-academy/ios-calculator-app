8f1290575bffa857f5e90a65299d11f0c349a785
zhilly11 <zhzh1x4@gmail.com>
2022-09-20 11:08:06 +0900

    refactor: group 생성 및 분류, ViewController -> MainViewController로 네이밍 변경

---
+++ Calculator/Calculator/View/MainViewController.swift
@@ -0,0 +1,12 @@
+//  Calculator - ViewController.swift
+//  Created by zhilly on 2022/09/20.
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
