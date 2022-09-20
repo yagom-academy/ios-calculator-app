dcad93ba3bada2ee739e5140b96caf350cfced0e
dev-summer <summerly812@gmail.com>
2022-09-20 16:15:14 +0900

    test: fail enqueue test

--- Calculator/CalculatorTests/CalculatorTests.swift
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -9,28 +9,35 @@ import XCTest
 @testable import Calculator
 
 final class CalculatorTests: XCTestCase {
+    var sut: CalculatorItemQueue!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        try super.setUpWithError()
+        sut = CalculatorItemQueue()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        try super.tearDownWithError()
+        sut = nil
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func test_enqueue_1개삽입() {
+        // given
+        let input = "1"
+        let expected: [CalculateItem] = ["1"]
+        
+        // when
+        sut.enqueue(item: input)
+        
+        // then
+        XCTAssertEqual(sut.inItems, expected)
     }
+}
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+extension String: CalculateItem {
+    
+}
 
+extension CalculateItem: Equatable {
+    
 }
