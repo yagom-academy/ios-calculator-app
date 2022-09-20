3d527b1aeacda30be6584972fcf326a572af4ea8
ash-youu <ash.you.dev@gmail.com>
2022-09-20 20:21:53 +0900

    feat: List 타입에서 사용할 Node 타입 정의

--- Calculator/Calculator/Model/CalculatorItemQueue.swift
+++ Calculator/Calculator/Model/CalculatorItemQueue.swift
@@ -3,4 +3,16 @@
 //  Calculator
 //
 
+protocol CalculateItem { }
 
+struct LinkedList<T> {
+    class Node<T>: CalculateItem {
+        var value: T
+        var next: Node?
+        
+        init(_ value: T, next: Node? = nil) {
+            self.value = value
+            self.next = next
+        }
+    }
+}
