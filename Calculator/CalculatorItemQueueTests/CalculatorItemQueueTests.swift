5a066708e9f503e63780ec7df1d8337dfdf7b8cf
미니 <cow970814@naver.com>
2022-09-20 12:09:23 +0900

    test : 큐 구현 후 테스트 코드 작성

--- Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
+++ Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
@@ -3,37 +3,47 @@
 //  Created by 미니.
 //
 
+/* 초기 테스트 설정
+ 1. enque를 하면 값이 저장된다.
+ 2. deque를 하면 처음으로 저장한 값이 나온다.
+ 3. enque와 deque의 횟수가 동일하면 isEmpty의 프로퍼티가 true를 반환한다.
+ */
+
+/* 구현 후 테스트 변경
+ 1. enque를 하면 isEmpty의 값이 false를 반환한다.
+ 2. deque를 하면 처음으로 저장한 값이 나온다.
+ 3. enque와 deque의 횟수가 동일하면 isEmpty의 프로퍼티가 true를 반환한다.
+ */
+
 import XCTest
 @testable import Calculator
 
 class CalculatorItemQueueTests: XCTestCase {
-    var sut: CalculatorItemQueue!
+    var sut: CalculatorItemQueue<Double>!
     
     override func setUpWithError() throws {
         try super.setUpWithError()
         sut = CalculatorItemQueue()
     }
-    
+
     override func tearDownWithError() throws {
         try super.tearDownWithError()
-        sut = CalculatorItemQueue()
+        sut = nil
     }
-    /* 초기 테스트 설정
-     1. enque를 하면 값이 저장된다.
-     2. deque를 하면 마지막으로 저장한 값이 나온다.
-     3. enque와 deque의 횟수가 동일하면 isEmpty의 프로퍼티가 true를 반환한다.
-     */
     
-    func test_값을넣으면_값이저장되는가() {
-        let result = sut.enqueue(1)
-        XCTAssertTrue(result)
+    func test_값을넣으면_isEmpty가_true인가() {
+        sut.enqueue(1)
+        XCTAssertFalse(sut.isEmpty)
     }
     
     func test_값을_추출하면_마지막에_저장한_값이_나오는가() {
-        let _ = sut.enqueue(1)
-        let result = sut.dequeue()
+        sut.enqueue(1)
+        sut.enqueue(2)
+        sut.enqueue(3)
         
-        XCTAssertNotNil(result)
+        let result = sut.peek(2)
+        
+        XCTAssertEqual(result, 1)
     }
     
     func test_값을_넣는_횟수와_추출하는_횟수가_같으면_배열이_비어있는가() {
