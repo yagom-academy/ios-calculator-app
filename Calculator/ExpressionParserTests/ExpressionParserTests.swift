4cc49ac625d58b151596ed98c9376647d73e440a
lws2269 <lws2269@kakao.com>
2022-09-21 03:09:27 +0900

    feat: ExpressionParserTests 테스트 추가

---
+++ Calculator/ExpressionParserTests/ExpressionParserTests.swift
@@ -0,0 +1,17 @@
+//
+//  ExpressionParserTests.swift
+//  ExpressionParserTests
+//
+//  Created by leewonseok on 2022/09/21.
+//
+
+import XCTest
+
+class ExpressionParserTests: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+}
