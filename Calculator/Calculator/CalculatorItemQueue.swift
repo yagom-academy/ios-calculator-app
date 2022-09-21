3bbadcce3046130bf557974aec5e039a317bcb36
jonghancha <jonghancha@gmail.com>
2022-09-21 12:37:26 +0900

    feat: CalculateItem 프로토콜 추가, 이를 채택하는 CalculatorItemQueue 구조체 추가

---
+++ Calculator/Calculator/CalculatorItemQueue.swift
@@ -0,0 +1,18 @@
+//  Created by Aejong on 2022/09/21.
+
+struct CalculatorItemQueue: CalculateItem {
+    
+    var linkedList
+    
+    var isEmpty: Bool {
+        
+    }
+    
+    mutating func enqueue() {
+        
+    }
+    
+    mutating func dequeue() {
+        
+    }
+}
