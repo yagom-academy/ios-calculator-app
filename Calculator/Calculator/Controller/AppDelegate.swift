6d0ff60e5e67fc002aa32a0e24e14258bec404fd
DragonYG <kioswe92@gmail.com>
2022-09-21 23:00:39 +0900

    feat: Queue 생성

--- Calculator/Calculator/Controller/AppDelegate.swift
+++ Calculator/Calculator/Controller/AppDelegate.swift
@@ -1,6 +1,6 @@
 //
 //  Calculator - AppDelegate.swift
-//  Created by yagom. 
+//  Created by dragon. 
 //  Copyright © yagom. All rights reserved.
 // 
 
---
+++ Calculator/Calculator/Model/Queue.swift
@@ -0,0 +1,48 @@
+//
+//  Queue.swift
+//  Calculator
+//
+//  Created by dragon on 2022/09/21.
+//
+
+public struct Queue<T> {
+    private var data = [T] ()
+    public init() {}
+    
+    public mutating func dequeue() -> T? {
+        return data.removeFirst()
+    }
+    
+    public mutating func peek() -> T? {
+        return data.first()
+    }
+    
+    public mutating func enqueue(element: T) {
+        return data.append(element)
+    }
+    
+    public mutating func clear() {
+        return data.removeAll()
+    }
+    
+    public var count: Int {
+        return data.count()
+    }
+    
+    public var capacity: Int {
+        get {
+            return data.capacity
+        }
+        set {
+            return data.reserveCapacity(newValue)
+        }
+    }
+    
+    public func isFull() -> Bool? {
+        return count == data.capacity
+    }
+    
+    public func isEmpty() -> Bool? {
+        return data.isEmpty
+    }
+}
