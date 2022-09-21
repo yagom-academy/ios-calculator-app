e753c649e7a45c7ac4ecdc59519e90996bd8d448
jin <pearlisrich@naver.com>
2022-09-21 11:38:51 +0900

    feat: Operator enum 생성  및 케이스 정의

---
+++ Calculator/Calculator/Model/Operator.swift
@@ -0,0 +1,15 @@
+//
+//  Operator.swift
+//  Calculator
+//
+//  Created by jin on 9/21/22.
+//
+
+enum Operator: Character, CaseIterable {
+    
+    public case add = "+"
+    public case subtract = "-"
+    public case divide = "÷"
+    public case multiply = "×"
+    
+}
