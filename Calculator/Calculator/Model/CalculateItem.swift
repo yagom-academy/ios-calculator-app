1f20379427e7daf99e5607de80deea793cf9b84e
jin <pearlisrich@naver.com>
2022-09-21 12:22:02 +0900

    feat: Double 타입 extension에 split() 메서드 구현

--- Calculator/Calculator/Model/CalculateItem.swift
+++ Calculator/Calculator/Model/CalculateItem.swift
@@ -9,6 +9,11 @@ protocol CalculateItem {
     
 }
 
-extension String : CalculateItem {}
+extension String : CalculateItem {
+    
+    func split(with target: Character) -> [String] {
+        return self.components(separatedBy: String(target))
+    }
+}
 
 extension Double : CalculateItem {}
