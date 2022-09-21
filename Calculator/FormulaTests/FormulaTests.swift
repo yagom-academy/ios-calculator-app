27110036808107139f3a54e100e3a282b6daf364
jin <pearlisrich@naver.com>
2022-09-21 23:06:37 +0900

    refactor: 불필요한 FormulaTests 함수 제거

--- Calculator/FormulaTests/FormulaTests.swift
+++ Calculator/FormulaTests/FormulaTests.swift
@@ -39,12 +39,4 @@ class FormulaTests: XCTestCase {
         
         XCTAssertEqual(result, 4)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
