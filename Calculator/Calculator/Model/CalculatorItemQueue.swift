2bd3e16463cc6001d20897b5fd255daa884e7cb9
junho <junhoL1046@gmail.com>
2022-09-21 18:48:34 +0900

    refactor: 더블 스택을 사용하는 방법으로 CalculatorItemQueue 리팩토링

--- Calculator/Calculator/Model/CalculatorItemQueue.swift
+++ Calculator/Calculator/Model/CalculatorItemQueue.swift
@@ -6,47 +6,18 @@
 //
 
 class CalculatorItemQueue<T: CalculateItem> {
-    class LinkedList<T> {
-        class Node<T> {
-            var value: T
-            var next: Node?
-            
-            init(value: T, next: Node? = nil) {
-                self.value = value
-                self.next = next
-            }
-        }
-        
-        private var head: Node<T>?
-        private var tail: Node<T>?
-        
-        func addLast(_ element: T) {
-            guard let last = tail else {
-                tail = Node(value: element)
-                head = tail
-                return
-            }
-            let newNode = Node(value: element)
-            last.next = newNode
-            tail = newNode
-        }
-        
-        func removeFirst() -> T? {
-            guard let first = head else {
-                return nil
-            }
-            head = first.next
-            return first.value
-        }
-    }
-    
-    private var queue: LinkedList<T> = LinkedList()
+    private var enqueueStack: Array<T> = []
+    private var dequeueStack: Array<T> = []
     
     func enqueue(_ element: T) {
-        queue.addLast(element)
+        enqueueStack.append(element)
     }
     
     func dequeue() -> T? {
-        return queue.removeFirst()
+        if dequeueStack.isEmpty {
+            dequeueStack = enqueueStack.reversed()
+            enqueueStack.removeAll()
+        }
+        return dequeueStack.popLast()
     }
 }
