6e85cb05926b61d5563a406a0536ee25658fc73b
oneStar92 <onestar7984@gmail.com>
2022-09-21 17:04:20 +0900

    refactor: Unit test시 sut를 사용하여서 테스트를 진행하도록 수정.

--- Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
+++ Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
@@ -7,18 +7,30 @@
 
 import XCTest
 
+extension Int: CalculateItem { }
+
 class CalculatorItemQueueTests: XCTestCase {
+    var sut: CalculatorItemQueue<Int>!
+    
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        sut = CalculatorItemQueue()
+    }
+    
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+        sut = nil
+    }
 
     func test_입력값_1_이_enqueue되어서_queue의_head의_data가_input과_같은가() {
         //given
         let input = 1
         
         //when
-        var queue: CalculatorItemQueue<Int> = CalculatorItemQueue<Int>()
-        queue.enqueue(input)
+        sut.enqueue(input)
         
         //then
-        XCTAssertEqual(input, queue.head?.data)
+        XCTAssertEqual(input, sut.head?.data)
     }
     
     func test_입력값_1_2_이_순서대로_enqueue되어서_queue의_head의_data가_1이고_tail의_data가_2인가() {
@@ -26,13 +38,12 @@ class CalculatorItemQueueTests: XCTestCase {
         let input = [1,2]
         
         //when
-        var queue: CalculatorItemQueue<Int> = CalculatorItemQueue<Int>()
-        queue.enqueue(input[0])
-        queue.enqueue(input[1])
+        sut.enqueue(input[0])
+        sut.enqueue(input[1])
         
         //then
-        XCTAssertEqual(input[0], queue.head?.data)
-        XCTAssertEqual(input[1], queue.tail?.data)
+        XCTAssertEqual(input[0], sut.head?.data)
+        XCTAssertEqual(input[1], sut.tail?.data)
     }
     
     func test_입력값_1_2_3_이_순서대로_enqueue되어서_queue의_head의_data가_1이고_tail의_data가_3인가() {
@@ -40,22 +51,21 @@ class CalculatorItemQueueTests: XCTestCase {
         let input = [1,2,3]
         
         //when
-        var queue: CalculatorItemQueue<Int> = CalculatorItemQueue<Int>()
-        queue.enqueue(input[0])
-        queue.enqueue(input[1])
-        queue.enqueue(input[2])
+        sut.enqueue(input[0])
+        sut.enqueue(input[1])
+        sut.enqueue(input[2])
         
         //then
-        XCTAssertEqual(input[0], queue.head?.data)
-        XCTAssertEqual(input[2], queue.tail?.data)
+        XCTAssertEqual(input[0], sut.head?.data)
+        XCTAssertEqual(input[2], sut.tail?.data)
     }
     
     func test_빈_queue의_dequeue의_값이_nil인가() {
         //when
-        var queue: CalculatorItemQueue<Int> = CalculatorItemQueue<Int>()
+        sut = CalculatorItemQueue<Int>()
         
         //then
-        XCTAssertEqual(nil, queue.dequeue())
+        XCTAssertEqual(nil, sut.dequeue())
     }
     
     func test_입력값_1_이_enqueue되어서_queue의_dequeue의_값이_input과_같은가() {
@@ -63,11 +73,10 @@ class CalculatorItemQueueTests: XCTestCase {
         let input = 1
         
         //when
-        var queue: CalculatorItemQueue<Int> = CalculatorItemQueue<Int>()
-        queue.enqueue(input)
+        sut.enqueue(input)
         
         //then
-        XCTAssertEqual(input, queue.dequeue())
+        XCTAssertEqual(input, sut.dequeue())
     }
     
     func test_입력값_1_2_3_이_순서대로_enqueue되어서_queue의_dequeue의_값이_첫번째_input값과_같은가() {
@@ -75,13 +84,12 @@ class CalculatorItemQueueTests: XCTestCase {
         let input = [1,2,3]
         
         //when
-        var queue: CalculatorItemQueue<Int> = CalculatorItemQueue<Int>()
-        queue.enqueue(input[0])
-        queue.enqueue(input[1])
-        queue.enqueue(input[2])
+        sut.enqueue(input[0])
+        sut.enqueue(input[1])
+        sut.enqueue(input[2])
         
         //then
-        XCTAssertEqual(input[0], queue.dequeue())
+        XCTAssertEqual(input[0], sut.dequeue())
     }
     
     func test_입력값_1_2_3_이_순서대로_enqueue되어서_queue의_dequeue의_값이_input값과_순서가_같은가() {
@@ -89,25 +97,24 @@ class CalculatorItemQueueTests: XCTestCase {
         let input = [1,2,3]
         
         //when
-        var queue: CalculatorItemQueue<Int> = CalculatorItemQueue<Int>()
-        queue.enqueue(input[0])
-        queue.enqueue(input[1])
-        queue.enqueue(input[2])
+        sut.enqueue(input[0])
+        sut.enqueue(input[1])
+        sut.enqueue(input[2])
         
         //then
-        XCTAssertEqual(input[0], queue.dequeue())
-        XCTAssertEqual(input[1], queue.dequeue())
-        XCTAssertEqual(input[2], queue.dequeue())
+        XCTAssertEqual(input[0], sut.dequeue())
+        XCTAssertEqual(input[1], sut.dequeue())
+        XCTAssertEqual(input[2], sut.dequeue())
     }
     
     func test_빈_queue의_peak의_값이_nil인가() {
         //given
         
         //when
-        var queue: CalculatorItemQueue<Int> = CalculatorItemQueue<Int>()
+        sut = CalculatorItemQueue<Int>()
 
         //then
-        XCTAssertEqual(nil, queue.peak())
+        XCTAssertEqual(nil, sut.peek())
     }
     
     func test_여러개의_입력값중_가장_첫번째_입력된_값을_출력하는_peak이_첫번째_입력된_값과_같은가() {
@@ -115,12 +122,11 @@ class CalculatorItemQueueTests: XCTestCase {
         let input = [1,2,3,4,5]
         
         //when
-        var queue: CalculatorItemQueue<Int> = CalculatorItemQueue<Int>()
         input.forEach {
-            queue.enqueue($0)
+            sut.enqueue($0)
         }
         
         //then
-        XCTAssertEqual(input[0], queue.peak())
+        XCTAssertEqual(input[0], sut.peek())
     }
 }
