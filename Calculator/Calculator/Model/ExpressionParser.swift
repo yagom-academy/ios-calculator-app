2227c6370ef825cb7c4ef0c99e6487ff5a79661f
lws2269 <lws2269@kakao.com>
2022-09-21 12:50:56 +0900

    feat: ExpressionParserTests 테스트케이스 추가

--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -7,10 +7,17 @@
 
 enum ExpressionParser {
     static func parse(from input: String) -> Formula {
-        return Formula()
+        var formula = Formula()
+        
+        componentsByOperator(from: input).compactMap{ Double($0) }.forEach {
+            formula.operands.enqueue($0)
+        }
+        input.compactMap { Double(String($0)) == nil ? $0 : nil }.forEach { formula.operators.enqueue(Operator(rawValue: $0)!)}
+
+        return formula
     }
     
     static private func componentsByOperator(from input: String) -> [String] {
-        return [""]
+        return input.compactMap { Double(String($0)) != nil ? String($0) : " " }
     }
 }
--- Calculator/ExpressionParserTests/ExpressionParserTests.swift
+++ Calculator/ExpressionParserTests/ExpressionParserTests.swift
@@ -6,12 +6,21 @@
 //
 
 import XCTest
+@testable import Calculator
 
 class ExpressionParserTests: XCTestCase {
-
+    
     override func setUpWithError() throws {
     }
 
     override func tearDownWithError() throws {
+        
+    }
+    func test_정상적으로parse되는지(){
+        var a = ExpressionParser.parse(from: "123+233")
+        print("---------------")
+        print(a.operands)
+        print(a.operators)
+        print("---------------")
     }
 }
