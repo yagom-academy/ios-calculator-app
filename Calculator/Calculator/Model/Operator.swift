8dddad2e2a6ec83357fed46b04a1f4d4f21f49f8
jin <pearlisrich@naver.com>
2022-09-21 11:56:15 +0900

    feat: Operator enum에 calculate() 메서드 추가

--- Calculator/Calculator/Model/Operator.swift
+++ Calculator/Calculator/Model/Operator.swift
@@ -12,6 +12,19 @@ enum Operator: Character, CaseIterable {
     public case divide = "÷"
     public case multiply = "×"
     
+    public calculate(lhs: Double, rhs: Double) -> Double {
+        switch self {
+        case .add:
+            return add(lhs: lhs, rhs: rhs)
+        case .subtract:
+            return subtract(lhs: lhs, rhs: rhs)
+        case .divide:
+            return divide(lhs: lhs, rhs: rhs)
+        case .multiply:
+            return multiply(lhs: lhs, rhs: rhs)
+        }
+    }
+    
     private add(lhs: Double, rhs: Double) -> Double {
         return lhs + rhs
     }
