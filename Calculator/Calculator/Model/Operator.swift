62dfe7e3069e9780ee8db25da63fa0dfd9c2e178
Jae Hyung Kim <smfc.mene@gmail.com>
2022-09-22 22:09:23 +0800

    fix: Operator calulate 메서드 수정

--- Calculator/Calculator/Model/Operator.swift
+++ Calculator/Calculator/Model/Operator.swift
@@ -16,11 +16,11 @@ enum Operator: Character, CaseIterable, CalculateItem {
         case .add:
             return add(lhs: lhs, rhs: rhs)
         case .subtract:
-            return add(lhs: lhs, rhs: rhs)
+            return subtract(lhs: lhs, rhs: rhs)
         case .divide:
-            return add(lhs: lhs, rhs: rhs)
+            return divide(lhs: lhs, rhs: rhs)
         case .multiply:
-            return add(lhs: lhs, rhs: rhs)
+            return multiply(lhs: lhs, rhs: rhs)
         }
     }
 
