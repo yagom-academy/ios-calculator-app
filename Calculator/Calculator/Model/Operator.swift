3053fc03a6ce2eff23b78d992062e475c340e816
미니 <cow970814@naver.com>
2022-09-22 16:21:35 +0900

    feat : 연산자 열거형 생성 및 연산 함수 생성

---
+++ Calculator/Calculator/Model/Operator.swift
@@ -0,0 +1,33 @@
+//
+//  Operator.swift
+//  Calculator
+//
+//  Created by 이경민 on 2022/09/22.
+//
+
+enum Operator: Character, CaseIterable {
+    case add = "+"
+    case subtract = "-"
+    case divide = "/"
+    case multiply = "*"
+    
+    func calculate(lhs: Double, rhs: Double) -> Double {
+        return 0
+    }
+    
+    private func add(lhs: Double, rhs: Double) -> Double {
+        return 0
+    }
+    
+    private func subtract(lhs: Double, rhs: Double) -> Double {
+        return 0
+    }
+    
+    private func divide(lhs: Double, rhs: Double) -> Double {
+        return 0
+    }
+    
+    private func multiply(lhs: Double, rhs: Double) -> Double {
+        return 0
+    }
+}
