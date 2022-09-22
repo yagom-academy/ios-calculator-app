0004ef558b072a6b4762dd546e5ce379a6771d49
chacha <happyhy0429@gmail.com>
2022-09-22 20:59:36 +0900

    refactor: Formula타입 내부 메서드의 옵셔널바인딩네이밍 수정

--- Calculator/Calculator/Formula.swift
+++ Calculator/Calculator/Formula.swift
@@ -9,12 +9,12 @@ struct Formula {
         guard let firstNumber = operands.dequeue(),
               let operatorSymbol = operators.dequeue(),
               let secondNumber = operands.dequeue() else { return 0 }
-        
         result = operatorSymbol.calculate(lhs: firstNumber, rhs: secondNumber)
+        
         while operands.count() > 0 && operators.count() > 0 {
-            guard let a = operands.dequeue() else { return 0 }
-            if let operatoraa = operators.dequeue() {
-                result = operatoraa.calculate(lhs: result, rhs: a)
+            guard let extratNumber = operands.dequeue() else { return 0 }
+            if let extraOperatorSymbol = operators.dequeue() {
+                result = extraOperatorSymbol.calculate(lhs: result, rhs: extratNumber)
             }
         }
         return result
