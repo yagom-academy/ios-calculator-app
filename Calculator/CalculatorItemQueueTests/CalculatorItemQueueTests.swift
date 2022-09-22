4c7037fa7706d398f8dc9091dc15bfb89b470450
jin <pearlisrich@naver.com>
2022-09-22 17:37:25 +0900

    test: test Double 타입에 바꿔서 수정

--- Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
+++ Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
@@ -9,7 +9,7 @@ import XCTest
 @testable import Calculator
 
 class CalculatorItemQueueTests: XCTestCase {
-    var sut: CalculatorItemQueue<String>!
+    var sut: CalculatorItemQueue<Double>!
 
     override func setUpWithError() throws {
         try super.setUpWithError()
@@ -24,34 +24,34 @@ class CalculatorItemQueueTests: XCTestCase {
     
     func test_when_enqueue_3_then_linkedlist_has_3() {
 
-        let input = "3"
+        let input: Double = 3
         
         sut.enqueue(item: input)
         
         let result = sut.dequeue()
         
-        XCTAssertEqual(result, "3")
+        XCTAssertEqual(result, 3)
     }
 
     func test_when_dequeue_3_when_linkedlist_has_3_4_5() {
         
-        let input = "3"
-        let input2 = "4"
-        let input3 = "5"
+        let input: Double = 4
+        let input2: Double = 5
+        let input3: Double = 6
         
         sut.enqueue(item: input)
         sut.enqueue(item: input2)
         sut.enqueue(item: input3)
 
         let result = sut.dequeue()
-        XCTAssertEqual(result, "3")
+        XCTAssertEqual(result, 4)
     }
     
     func test_when_linkedlist_has_3_4_5_removeAll() {
         
-        let input = "3"
-        let input2 = "4"
-        let input3 = "5"
+        let input: Double = 4
+        let input2: Double = 5
+        let input3: Double = 6
         
         sut.enqueue(item: input)
         sut.enqueue(item: input2)
