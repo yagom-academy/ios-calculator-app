1074069a1dad2e898523ff0d6a2679fdebf4f6b9
woong <workplayhard1@naver.com>
2022-09-22 20:25:06 +0900

    feat: UML 요구사항 - Formula 타입 생성

---
+++ Calculator/Calculator/Model/Formula.swift
@@ -0,0 +1,17 @@
+//
+//  Formula.swift
+//  Calculator
+//
+//  Created by 서현웅 on 2022/09/22.
+//
+
+import Foundation
+
+struct Formula {
+    let operands: CalculatorItemQueue
+    let operators: CalculatorItemQueue
+    
+    func result() -> Double {
+        return 0
+    }
+}
