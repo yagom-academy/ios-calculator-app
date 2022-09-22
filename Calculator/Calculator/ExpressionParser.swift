97863a8a16d456585aa6ebcba6ba203da1ed503b
chacha <happyhy0429@gmail.com>
2022-09-22 21:10:15 +0900

    test:FormulaTests파일내 추가 테스트 작성

--- Calculator/Calculator/ExpressionParser.swift
+++ Calculator/Calculator/ExpressionParser.swift
@@ -4,29 +4,30 @@ import Foundation
 
 enum ExpressionParser {
     
-    static func parse(from input: String) -> Formula {
-        let components = componentsByOperators(from: input)
-        let operands : CalculatorItemQueue<Double>
-        let operators : CalculatorItemQueue<Operator>
+//    static func parse(from input: String) -> Formula {
+//        let components = componentsByOperators(from: input)
+//        let operands : CalculatorItemQueue<Double>
+//        let operators : CalculatorItemQueue<Operator>
         
-        var filtered = components.filter{ $0 == Operator.RawValue }
+        //        var filtered = components.filter{ $0 == Operator.RawValue }
         
         
-// ["1", "+", "2", "*", "4"]
-//        for component in components {
-            
-//            if let b = Operator.self.RawValue {
-//                operators.enqueue(element: b)
-//            } else {
-//                if let a = Double(component) {
-//                    operands.enqueue(element: a)
-//                }
-//        }
-    return Formula(operands: operands, operators: operators)
-}
-
-private static func componentsByOperators(from input: String) -> [String] {
-    let components = input.map{String($0)}
-    return components
-}
+        // ["1", "+", "2", "*", "4"]
+        //        for component in components {
+        
+        //            if let b = Operator.self.RawValue {
+        //                operators.enqueue(element: b)
+        //            } else {
+        //                if let a = Double(component) {
+        //                    operands.enqueue(element: a)
+        //                }
+        //        }
+        //    return Formula(operands: operands, operators: operators)
+        //}
+//    }
+    
+    private static func componentsByOperators(from input: String) -> [String] {
+        let components = input.map{String($0)}
+        return components
+    }
 }
--- Calculator/CalculatorProjectTests/FormulaTests.swift
+++ Calculator/CalculatorProjectTests/FormulaTests.swift
@@ -16,31 +16,44 @@ class FormulaTests: XCTestCase {
         sut = nil
     }
     
-    func test_result메서드호출시_연산결과가잘나오는지() {
+    func test_숫자와연산기호_모두값이있을때_result호출시_연산결과가잘나오는지() {
         //given
-        sut = Formula(operands: CalculatorItemQueue(enqueueStack: [5,3,1]), operators: CalculatorItemQueue(enqueueStack: [Operator.multiply,Operator.add]))
+        sut = Formula(operands: CalculatorItemQueue(enqueueStack: [5,3,1]),
+                      operators: CalculatorItemQueue(enqueueStack: [Operator.multiply,Operator.add]))
         //when
         let result: Double = sut.result()
         //then
         XCTAssertEqual(result, 16)
     }
     
-    func test_숫자와연산기호가_각각1개씩만있을때_reult메서드호출시_반환값이0인지() {
+    func test_숫자와연산기호가_각각1개씩만있을때_reult호출시_반환값이0인지() {
         //given
-        sut = Formula(operands: CalculatorItemQueue(enqueueStack: [5]), operators: CalculatorItemQueue(enqueueStack: [Operator.multiply]))
+        sut = Formula(operands: CalculatorItemQueue(enqueueStack: [5]),
+                      operators: CalculatorItemQueue(enqueueStack: [Operator.multiply]))
         //when
         let result: Double = sut.result()
         //then
         XCTAssertEqual(result, 0)
     }
     
-    func test_연산기호가_숫자보다많을경우_숫자만큼만연산결과가나오는지() {
+    func test_연산기호가_숫자보다많을경우_result호출시_숫자만큼만연산결과가나오는지() {
         //given
-        sut = Formula(operands: CalculatorItemQueue(enqueueStack: [5,3]), operators: CalculatorItemQueue(enqueueStack: [Operator.multiply, Operator.add, Operator.multiply]))
+        sut = Formula(operands: CalculatorItemQueue(enqueueStack: [5,3]),
+                      operators: CalculatorItemQueue(enqueueStack:
+                                [Operator.multiply, Operator.add, Operator.multiply]))
         //when
         let result:Double = sut.result()
         //then
         XCTAssertEqual(result, 15)
+    }
+    func test_연산기호만있을경우_result호출시_0이반환되는지() {
+        sut = Formula(operands: CalculatorItemQueue(enqueueStack: []),
+                      operators: CalculatorItemQueue(enqueueStack:
+                                [Operator.multiply, Operator.add, Operator.multiply]))
+        //when
+        let result:Double = sut.result()
+        //then
+        XCTAssertEqual(result, 0)
     }
 }
 
