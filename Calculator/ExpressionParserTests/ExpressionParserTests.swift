4f5884058dc8e8f7645c9c266194ac6e3c06d3e2
jin <pearlisrich@naver.com>
2022-09-22 11:28:17 +0900

    test: test_split_string_to_components_by_operators()으로 테스트 명 변경

--- Calculator/ExpressionParserTests/ExpressionParserTests.swift
+++ Calculator/ExpressionParserTests/ExpressionParserTests.swift
@@ -10,7 +10,7 @@ import XCTest
 
 class ExpressionParserTests: XCTestCase {
 
-    func testExample() throws {
+    func test_split_string_to_components_by_operators() throws {
         let input = "3+42-7÷32"
         
         let result = ExpressionParser.componentsByOperators(from: input)
