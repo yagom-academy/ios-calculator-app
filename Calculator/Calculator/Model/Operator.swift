00fbb7e3a685f9f85147749e2558e2633ed29f68
Jae Hyung Kim <smfc.mene@gmail.com>
2022-09-22 12:58:18 +0800

    feat: Operator enum 파일 생성

---
+++ Calculator/Calculator/Model/Operator.swift
@@ -0,0 +1,30 @@
+//
+//  Operator.swift
+//  Calculator
+//
+//  Created by mene on 22/9/2022.
+//
+
+enum Operator: Character, CaseIterable, CalculateItem {
+    case add = "+"
+    case subtract = "-"
+    case divide = "÷"
+    case multiply = "✕"
+    
+    func calculate(lhs: Double, rhs: Double) -> Double {
+        
+    }
+    
+    func add(lhs: Double, rhs: Double) -> {
+        
+    }
+    
+    func subtract(lhs: Double, rhs: Double) -> Double {
+        
+    }
+    
+    func multiply(lhs: Double, rhs: Double) -> Double {
+        
+    }
+}
+
