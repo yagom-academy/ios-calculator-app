09e63b93b4507cffd8f85629f0c35f57eff67adc
sunnyCookie <107384230+sunny-maeng@users.noreply.github.com>
2022-09-22 19:22:13 +0900

    feat: StringExtension 메서드 구현

--- Calculator/Calculator/Model/Extension/StringExtension.swift
+++ Calculator/Calculator/Model/Extension/StringExtension.swift
@@ -5,11 +5,8 @@
 //  Created by 써니쿠키 on 2022/09/22.
 //
 
-import Foundation
-
 extension String {
     func split(with target: Character) -> [String] {
-        //코드구현예정
-        return ["0.0"]
+        return self.components(separatedBy: String(target))
     }
 }
