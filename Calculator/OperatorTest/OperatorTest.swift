c5b93f0929ca9862a0347b6779a7ead3ce4ab222
HyosungP <hyosing92@gmail.com>
2022-09-22 21:15:18 +0900

    feat: OperatorTest 추가
    - UnitTest를 위한 OperatorTest 추가

---
+++ Calculator/OperatorTest/OperatorTest.swift
@@ -0,0 +1,58 @@
+//
+//  OperatorTest.swift
+//  OperatorTest
+//
+//  Created by parkhyo on 2022/09/22.
+//
+
+import XCTest
+
+@testable import Calculator
+class OperatorTest: XCTestCase {
+    
+    var sut: Operator!
+
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+        sut = nil
+    }
+
+    func test_add() {
+        sut = Operator(rawValue: "+")
+        let result = sut.calculator(lhs: 10.0, rhs: 20.5)
+        XCTAssertEqual(result, 30.5)
+    }
+    
+    func test_subtract() {
+        sut = Operator(rawValue: "-")
+        let result = sut.calculator(lhs: 20.0, rhs: 10.0)
+        XCTAssertEqual(result, 10.0)
+    }
+    
+    func test_subtract_result_is_minus() {
+        sut = Operator(rawValue: "-")
+        let result = sut.calculator(lhs: 10.0, rhs: 20.0)
+        XCTAssertEqual(result, -10.0)
+    }
+    
+    func test_divide() {
+        sut = Operator(rawValue: "/")
+        let result = sut.calculator(lhs: 20.0, rhs: 10.0)
+        XCTAssertEqual(result, 2.0)
+    }
+    
+    func test_divide_result_is_under_zero() {
+        sut = Operator(rawValue: "/")
+        let result = sut.calculator(lhs: 10.0, rhs: 20.0)
+        XCTAssertEqual(result, 0.5 )
+    }
+    
+    func test_multifly() {
+        sut = Operator(rawValue: "*")
+        let result = sut.calculator(lhs: 10.0, rhs: 10.5)
+        XCTAssertEqual(result, 105.0)
+    }
+    
+    
+
+}
