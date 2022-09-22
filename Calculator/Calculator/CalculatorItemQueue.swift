1c07dd773af66a580f2b3458f990c69f9db517dc
woong <workplayhard1@naver.com>
2022-09-22 15:42:49 +0900

    refactor: CalculatorItemQueue 큐타입 내부의 접근제어자 수정

--- Calculator/Calculator/CalculatorItemQueue.swift
+++ Calculator/Calculator/CalculatorItemQueue.swift
@@ -11,25 +11,25 @@ protocol CalculateItem {
 }
 
 struct CalculatorItemQueue: CalculateItem {
-    var queue: [String] = []
+    private var queue: [String] = []
     
-    public var count: Int {
+    var count: Int {
         return queue.count
     }
     
-    public var isEmpty: Bool {
+    var isEmpty: Bool {
         return queue.isEmpty
     }
     
-    public mutating func enqueue(_ element: String) {
+    mutating func enqueue(_ element: String) {
         queue.append(element)
     }
     
-    public mutating func dequeue() -> String? {
+    mutating func dequeue() -> String? {
         return isEmpty ? nil : queue.removeFirst()
     }
     
-    public mutating func clear() {
+    mutating func clear() {
         queue.removeAll()
     }
 }
