1df10f4ed179516fcdcbaab72aad017bbd346a67
lnhk98 <hozzang666@gmai.com>
2022-09-22 15:08:23 +0900

    feat: Formula 타입 생성

---
+++ Calculator/Calculator/Model/Formula.swift
@@ -0,0 +1,17 @@
+//
+//  Formula.swift
+//  Calculator
+//
+//  Created by 김인호 on 2022/09/22.
+//
+
+import Foundation
+
+struct Formula {
+    var operands: CalculatorItemQueue<Int>
+    var operators: CalculatorItemQueue<Operator>
+    
+    func resut() -> Double {
+        
+    }
+}
