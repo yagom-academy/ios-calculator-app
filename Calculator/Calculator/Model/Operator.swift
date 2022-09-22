e9964b5448c53891126e96c6079ee596a4fb8488
Jae Hyung Kim <smfc.mene@gmail.com>
2022-09-22 13:43:04 +0800

    feat: Operator파일 메서드 구현

--- Calculator/Calculator/Model/Operator.swift
+++ Calculator/Calculator/Model/Operator.swift
@@ -11,24 +11,33 @@ enum Operator: Character, CaseIterable, CalculateItem {
     case divide = "÷"
     case multiply = "✕"
     
-//    func calculate(lhs: Double, rhs: Double) -> Double {
-//        
-//    }
-//    
-//    func add(lhs: Double, rhs: Double) -> {
-//        
-//    }
-//    
-//    func subtract(lhs: Double, rhs: Double) -> Double {
-//        
-//    }
-//    
-//    func divide(lhs: Double, rhs: Double) -> Double {
-//        
-//    }
-//    
-//    func multiply(lhs: Double, rhs: Double) -> Double {
-//        
-//    }
+    func calculate(lhs: Double, rhs: Double) -> Double {
+        switch self {
+        case .add:
+            return add(lhs: lhs, rhs: rhs)
+        case .subtract:
+            return add(lhs: lhs, rhs: rhs)
+        case .divide:
+            return add(lhs: lhs, rhs: rhs)
+        case .multiply:
+            return add(lhs: lhs, rhs: rhs)
+        }
+    }
+
+    private func add(lhs: Double, rhs: Double) -> Double {
+        return lhs + rhs
+    }
+
+    private func subtract(lhs: Double, rhs: Double) -> Double {
+        return lhs - rhs
+    }
+
+    private func divide(lhs: Double, rhs: Double) -> Double {
+        return lhs / rhs
+    }
+
+    private func multiply(lhs: Double, rhs: Double) -> Double {
+        return lhs * rhs
+    }
 }
 
