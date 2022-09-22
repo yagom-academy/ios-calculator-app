d97a855e06c92ca8ec95f5a5477c1793a1b195bc
jin <pearlisrich@naver.com>
2022-09-22 15:17:45 +0900

    refactor: 컨벤션 수정

--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -11,12 +11,12 @@ enum ExpressionParser {
     
     static func parse(from input: String) -> Formula {
         
-        let operands = CalculatorItemQueue<Double>()
-        let operators = CalculatorItemQueue<Operator>()
+        let operands: CalculatorItemQueue<Double> = .init()
+        let operators: CalculatorItemQueue<Operator> = .init()
         let components = componentsByOperators(from: input)
         
         components.compactMap{ Double($0) }.forEach{ operands.enqueue(item: $0) }
-        input.map{ $0 }.compactMap{Operator(rawValue: $0)}.forEach{ operators.enqueue(item: $0) }
+        input.map{ $0 }.compactMap{ Operator(rawValue: $0) }.forEach{ operators.enqueue(item: $0) }
         
         return Formula(operands: operands, operators: operators)
     }
@@ -24,6 +24,7 @@ enum ExpressionParser {
     private static func componentsByOperators(from input: String) -> [String] {
         
         let operators = CharacterSet(charactersIn: Operator.allCases.map{ String($0.rawValue) }.joined())
+        
         return input.components(separatedBy: operators)
     }
 }
