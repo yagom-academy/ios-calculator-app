da7578f9a44a0de62aa2f345130d257b6f6f2ca9
미니 <cow970814@naver.com>
2022-09-22 18:02:05 +0900

    test : 테스트 코드 불필요한 메서드 삭제 및 throws 대응

--- Calculator/CalculatorItemQueueTests/OperatorTests.swift
+++ Calculator/CalculatorItemQueueTests/OperatorTests.swift
@@ -10,14 +10,6 @@ import XCTest
 
 final class OperatorTests: XCTestCase {
     
-    override func setUpWithError() throws {
-        try super.setUpWithError()
-    }
-    
-    override func tearDownWithError() throws {
-        try super.tearDownWithError()
-    }
-    
     func test_연산자가add일때에러없이연산을수행하는지() {
         // driven
         let targetOperator = Operator.add
@@ -31,30 +23,46 @@ final class OperatorTests: XCTestCase {
     }
     
     func test_연산자가subtract일때연산을잘수행하는지() {
+        // driven
         let targetOperator = Operator.subtract
         
+        // when
         let result = try? targetOperator.calculate(lhs: 2, rhs: 1)
         
+        // then
         XCTAssertNoThrow(result)
         XCTAssertEqual(result, 1)
     }
     
     func test_연산자가multiply일때연산을잘수행하는지() {
+        // driven
         let targetOperator = Operator.multiply
         
+        // when
         let result = try? targetOperator.calculate(lhs: 2, rhs: 3)
         
+        // then
         XCTAssertNoThrow(result)
         XCTAssertEqual(result, 6)
     }
     
-    func test_연산자가divide일때연산을잘수행하는지() {
+    func test_연산자가divide일때0이아닌수로연산하였을때에러없이연산을잘수행하는지() {
+        // driven
         let targetOperator = Operator.divide
         
+        // when
         let result = try? targetOperator.calculate(lhs: 10, rhs: 2)
         
+        // then
         XCTAssertNoThrow(result)
         XCTAssertEqual(result, 5)
     }
     
+    func test_연산자가divide이고0인수로연산을수행하였을때에러가발생하는지() {
+        // driven
+        let targetOperator = Operator.divide
+        
+        // when then
+        XCTAssertThrowsError(try targetOperator.calculate(lhs: 10, rhs: 0))
+    }
 }
