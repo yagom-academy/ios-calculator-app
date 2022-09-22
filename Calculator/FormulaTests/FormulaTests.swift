03666a44002a9ed4db729f5c9c5b18282d561c06
yuvinrho <yvrho@naver.com>
2022-09-22 11:07:52 +0900

    test: FormulaTests 타깃생성

---
+++ Calculator/FormulaTests/FormulaTests.swift
@@ -0,0 +1,24 @@
+//
+//  FormulaTests.swift
+//  FormulaTests
+//
+//  Created by 노유빈 on 2022/09/22.
+//
+
+import XCTest
+
+@testable import Calculator
+final class FormulaTests: XCTestCase {
+    var sut: Formula!
+    
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        sut = Formula()
+    }
+    
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+        sut = nil
+    }
+
+}
