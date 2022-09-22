7ed321ffdbf4b5543461ce93890699982e5700ad
jin <pearlisrich@naver.com>
2022-09-22 15:46:45 +0900

    refactor: 주석 삭제

--- Calculator/ExpressionParserTests/ExpressionParserTests.swift
+++ Calculator/ExpressionParserTests/ExpressionParserTests.swift
@@ -9,15 +9,6 @@ import XCTest
 @testable import Calculator
 
 class ExpressionParserTests: XCTestCase {
-
-//    func test_split_string_to_components_by_operators() throws {
-//        let input = "3+42-7÷32"
-//        
-//        let result = ExpressionParser.componentsByOperators(from: input)
-//        
-//        
-//        XCTAssertEqual(result, ["3", "+", "42", "-", "7", "÷", "32"])
-//    }
     
     func test_if_formula_is_made() {
         
