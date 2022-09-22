b7a22003eeca355b72f6d370b5a1cf2f44cca998
Mangdi <lkw4670@naver.com>
2022-09-23 00:00:15 +0900

    feat: ExpressionParser 열거형과 Operator 열거형 생성

---
+++ Calculator/Calculator/ExpressionParser.swift
@@ -0,0 +1,18 @@
+//
+//  ExpressionParser.swift
+//  Calculator
+//
+//  Created by Mangdi on 2022/09/22.
+//
+
+import Foundation
+
+enum ExpressionParser {
+//    static func parse(from input: String) -> Formula {
+//        
+//    }
+//    
+//    static private func componentsByOperators(from input: String) -> [String] {
+//        
+//    }
+}
