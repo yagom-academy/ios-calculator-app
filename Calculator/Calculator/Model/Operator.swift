3be670ae3b4407eeb4f3186eca981168bf9e493e
sunnyCookie <107384230+sunny-maeng@users.noreply.github.com>
2022-09-22 18:23:47 +0900

    chore: Operator 열거형 깃헙업데이트 오류로인한 삭제후 재 생성

---
+++ Calculator/Calculator/Model/Operator.swift
@@ -0,0 +1,45 @@
+//
+//  Operator.swift
+//  Calculator
+//
+//  Created by 써니쿠키 on 2022/09/22.
+//
+
+enum Operator: Character, CaseIterable, CalculateItem {
+    case add = "+"
+    case subtract = "-"
+    case divide = "/"
+    case multiply = "*"
+    
+    func calculate(lhs: Double, rhs: Double) throws -> Double {
+        switch self {
+        case .add:
+            return add(lhs: lhs, rhs: rhs)
+        case .subtract:
+            return subtract(lhs: lhs, rhs: rhs)
+        case .divide:
+            guard rhs != 0.0 else {
+                throw CalculatorError.divideByZeroError
+            }
+            return divide(lhs: lhs, rhs: rhs)
+        case .multiply:
+            return multiply(lhs: lhs, rhs: rhs)
+        }
+    }
+    
+    private func add(lhs: Double, rhs: Double) -> Double {
+        return lhs + rhs
+    }
+    
+    private func subtract(lhs: Double, rhs: Double) -> Double {
+        return lhs - rhs
+    }
+    
+    private func divide(lhs: Double, rhs: Double) -> Double {
+        return lhs / rhs
+    }
+    
+    private func multiply(lhs: Double, rhs: Double) -> Double {
+        return lhs * rhs
+    }
+}
