078d8c5a27cd32730b9ae312ccd796870a319ade
미니 <cow970814@naver.com>
2022-09-22 17:24:46 +0900

    feat : Operator 함수 구현

--- Calculator/Calculator/Model/Operator.swift
+++ Calculator/Calculator/Model/Operator.swift
@@ -5,29 +5,34 @@
 //  Created by 이경민 on 2022/09/22.
 //
 
-enum Operator: Character, CaseIterable, CalculateItem {
+enum `Operator`: Character, CaseIterable, CalculateItem {
     case add = "+"
     case subtract = "-"
     case divide = "/"
     case multiply = "*"
     
     func calculate(lhs: Double, rhs: Double) -> Double {
-        return 0
+        switch self {
+        case .add: return add(lhs: lhs, rhs: rhs)
+        case .subtract: return subtract(lhs: lhs, rhs: rhs)
+        case .divide: return divide(lhs: lhs, rhs: rhs)
+        case .multiply: return multiply(lhs: lhs, rhs: rhs)
+        }
     }
     
     private func add(lhs: Double, rhs: Double) -> Double {
-        return 0
+        return lhs + rhs
     }
     
     private func subtract(lhs: Double, rhs: Double) -> Double {
-        return 0
+        return lhs - rhs
     }
     
     private func divide(lhs: Double, rhs: Double) -> Double {
-        return 0
+        return lhs / rhs
     }
     
     private func multiply(lhs: Double, rhs: Double) -> Double {
-        return 0
+        return lhs * rhs
     }
 }
