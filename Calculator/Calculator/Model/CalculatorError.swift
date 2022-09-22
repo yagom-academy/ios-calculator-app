f56ef20dadb4bd87ac04a814a2034d1594af0bde
sunnyCookie <107384230+sunny-maeng@users.noreply.github.com>
2022-09-22 18:18:15 +0900

    feat: Operator 열거형 내부 메서드 구현, CalculatorError 타입생성, Operatoe 유닛테스트코드수 정

---
+++ Calculator/Calculator/Model/CalculatorError.swift
@@ -0,0 +1,13 @@
+//
+//  CalculatorError.swift
+//  Calculator
+//
+//  Created by 써니쿠키 on 2022/09/22.
+//
+
+import Foundation
+
+enum CalculatorError: Error {
+    case divideByZeroError
+    case otherError
+}
--- Calculator/OperatorTests/OperatorTests.swift
+++ Calculator/OperatorTests/OperatorTests.swift
@@ -23,69 +23,43 @@ class OperatorTests: XCTestCase {
     
     func test_calculate_add_더하기값확인() throws {
         //expection 1111.11111 + 2222.22222
-        do {
-            result = try Operator.calculate(.add)(lhs: 1111.11111, rhs: 2222.22222)
-        } catch {
-            throw Calculator.otherError
-        }
+        result = try Operator.calculate(.add)(lhs: 1111.11111, rhs: 2222.22222)
         
-        //result는 3333.33333이고 그 외의값은 XCTAssertNotEqual이다.
+        //result는 1111.11111 + 2222.22222(=3333.33333)이고 그 외의값은 NotEqual이다.
         XCTAssertEqual(1111.11111 + 2222.22222, result)
-        XCTAssertEqual(3333.33333, result)
         XCTAssertNotEqual(4444.44444, result)
     }
     
     func test_calculate_subtract_빼기값확인() throws {
         //expection 4444.44444 - 3333.33333
-        do {
-            result = try Operator.calculate(.subtract)(lhs: 4444.44444, rhs: 3333.33333)
-        } catch {
-            throw Calculator.otherError
-        }
+        result = try Operator.calculate(.subtract)(lhs: 4444.44444, rhs: 3333.33333)
         
-        //result는 1111.11111이고 그 외의값은 XCTAssertNotEqual이다.
+        //result는 4444.44444 - 3333.33333(=1111.11111)이고 그 외의값은 NotEqual이다.
         XCTAssertEqual(4444.44444 - 3333.33333, result)
-        XCTAssertEqual(1111.11111, result)
         XCTAssertNotEqual(2222.22222, result)
     }
     
     func test_calculate_divide_0이아닌수로나누기값확인() throws {
         //expection 4444.44444 / 2.2
-        do {
-            result = try Operator.calculate(.divide)(lhs: 4444.44444, rhs: 2.2)
-        } catch {
-            throw Calculator.otherError
-        }
+        result = try Operator.calculate(.divide)(lhs: 4444.44444, rhs: 2.2)
         
-        //result는 2020.20202이고 그 외의값은 XCTAssertNotEqual이다.
+        //result는 4444.44444 / 2.2 (=2020.20202)이고 그 외의값은 NotEqual이다.
         XCTAssertEqual(4444.44444 / 2.2, result)
-        XCTAssertEqual(2020.20202, result)
         XCTAssertNotEqual(2222.22222, result)
     }
     
     func test_calculate_divide_0으로나누기할때_에러던지기확인() throws {
-        //expection 4444.44444 / 0
-        do {
-            let result = try Operator.calculate(.divide)(lhs: 4444.44444, rhs: 0)
-        } catch {
-            throw Calculator.otherError
-        }
-        
+        //expection 4444.44444 / 0 하면 에러를 던진다
         //result 에러를 던진다.
-        XCTAssertThrowsError(result)
+        XCTAssertThrowsError(try Operator.calculate(.divide)(lhs: 4444.44444, rhs: 0))
     }
     
     func test_calculate_multiply() throws {
         //expection 3333.33333 * 2.2
-        do {
-            let result = try Operator.calculate(.multiply)(lhs: 3333.33333, rhs: 2.2)
-        } catch {
-            throw Calculator.otherError
-        }
-        
-        //result는 73333.33333이고 그 외의값은 XCTAssertNotEqual이다.
+        let result = try Operator.calculate(.multiply)(lhs: 3333.33333, rhs: 2.2)
+
+        //result는 3333.33333 * 2.2 (=73333.33333)이고 그 외의값은 NotEqual이다.
         XCTAssertEqual(3333.33333 * 2.2, result)
-        XCTAssertEqual(7333.33333, result)
         XCTAssertNotEqual(2222.22222, result)
     }
 }
