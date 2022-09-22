3330538d057dae5a45b3d83f64792deccd832d03
미니 <cow970814@naver.com>
2022-09-22 15:03:19 +0900

    test : 테스트 코드 명세서의 역할을 할 수 있도록 리팩토링

--- Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
+++ Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
@@ -8,165 +8,82 @@ import XCTest
 
 class CalculatorItemQueueTests: XCTestCase {
     var sut: CalculatorItemQueue<Double>!
+    var testNumber: Double!
     
     override func setUpWithError() throws {
         try super.setUpWithError()
         sut = CalculatorItemQueue()
+        testNumber = Double.random(in: 1...100)
     }
     
     override func tearDownWithError() throws {
         try super.tearDownWithError()
         sut = nil
+        testNumber = nil
     }
     
-    func test_init을통해서_값을_넣을수있는가() {
-        let queue = CalculatorItemQueue([1,2,3])
+    // MARK: - init no array
+    func test_초기화를통해서_빈큐를_초기화할수있는가() {
+        // given
+        let queue = CalculatorItemQueue<Double>()
+        
+        // when
         let result = queue.isEmpty
-        XCTAssertFalse(result)
+        
+        // then
+        XCTAssertTrue(result)
     }
     
-    func test_값을넣으면_isEmpty가_false인가() {
-        let randomNumber = Double.random(in: 1...100)
-        sut.enqueue(randomNumber)
-        XCTAssertFalse(sut.isEmpty)
+    // MARK: - init with array
+    func test_초기화와_배열을_통해서_채워져_있는_큐를_생성할_수_있는가() {
+        // given
+        let initArr: [Double] = [1,2,3,4]
+        let queue = CalculatorItemQueue(elements: initArr)
+        
+        // when
+        let result = !queue.isEmpty
+        
+        // then
+        XCTAssertTrue(result)
     }
     
-    func test_값을넣지않으면_값이없는가() {
-        XCTAssertTrue(sut.isEmpty)
-    }
-    
-    func test_값을1개넣으면_front의값과동일한가() {
-        let randomNumber = Double.random(in: 1...100)
-        sut.enqueue(randomNumber)
-        let result = sut.front
-        XCTAssertEqual(result, randomNumber)
-    }
-    
-    func test_값을여러개넣은후_front의_값이_처음들어간_값인가() {
-        let count: Int = Int.random(in: 1...9)
+    // MARK: - enqeue + front
+    func test_enqueue로_요소을_추가하면_제일_처음에_나올_값이_추가한_요소와_동일한가() {
+        // given
+        sut.enqueue(testNumber)
         
-        for value in 1...count {
-            sut.enqueue(Double(value))
-        }
-        
-        let result = sut.front
-        XCTAssertEqual(result, 1)
-    }
-    
-    func test_n번의_enqueue와_n_minus_1_dequeue를_수행후_front값이_마지막에_넣은값과동일한가() {
-        let count: Int = Int.random(in: 1...9)
-        let removeCount = count - 1
-
-        for value in 0...count {
-            let inputValue: Double = Double((2 * value) + 1)
-            sut.enqueue(inputValue)
-        }
-        
-        for _ in 0...removeCount {
-            sut.dequeue()
-        }
-        
-        let result = sut.front
-        let targetValue = Double((2 * count) + 1)
-        XCTAssertEqual(result, targetValue)
-    }
-    
-    func test_n번의_enqueue와_m번의_dequeue를_수행후_front값이_예상값과동일한가() {
-        let count: Int = 5
-        let removeCount: Int = Int.random(in: 1...count)
-        
-        for value in 0...count {
-            let inputValue: Double = Double((2 * value) + 1)
-            sut.enqueue(inputValue)
-        }
-        
-        for _ in 1...removeCount {
-            sut.dequeue()
-        }
-        
-        let result = sut.front
-        let targetNumber = Double(2 * count + 1)
-        XCTAssertEqual(result, targetNumber)
-    }
-    
-    func test_n번의_enqueue와_n번의_dequeue를_수행후_front값이_nil인지() {
-        let count: Int = Int.random(in: 1...9)
-        
-        for value in 0...count {
-            let inputValue: Double = Double((2 * value) + 1)
-            sut.enqueue(inputValue)
-        }
-        
-        for _ in 0...count {
-            sut.dequeue()
-        }
-        
+        // when
         let result = sut.front
         
-        XCTAssertNil(result)
+        // then
+        XCTAssertEqual(testNumber, result)
     }
     
-    func test_n번의_enque를_한후_마지막의값이_마지막에_위치하는가() {
-        let count: Int = Int.random(in: 1...9)
-
-        for index in 1...count {
-            sut.enqueue(Double(index))
-        }
-        
-        let result = sut.peek(count - 1)
-        let targetNumber = Double(count)
-        XCTAssertEqual(result, targetNumber)
-    }
-    
-    func test_값을_넣는_횟수와_추출하는_횟수가_같으면_배열이_비어있는가() {
-        let count: Int = Int.random(in: 1...9)
-        
-        for value in 1...count {
-            sut.enqueue(Double(value))
-        }
-        
-        for _ in 1...count {
-            sut.dequeue()
-        }
-        
-        XCTAssertTrue(sut.isEmpty)
-    }
-    
-    func test_초기화로_값을_할당하였을때_초기배열의_수와_큐에_count변수의값이_동일한가() {
-        let initArr: [Double] = Array(repeating: 1, count: Int.random(in: 1...9))
-        let queue = CalculatorItemQueue<Double>(initArr)
-        
-        let result = queue.count
-        
-        XCTAssertEqual(result, initArr.count)
-    }
-    
-    func test_enque를_한횟수와_한후_큐에_남은_요소의_개수가_동일한가() {
-        let count: Int = Int.random(in: 1...9)
-        
+    func test_여러번의_enqueue로_요소를_추가한후_큐의_요소개수와_동일한가() {
+        // given
+        let count = Int(testNumber)
         for value in 0..<count {
-            sut.enqueue(Double(value))
+            let enqueueNumber = Double(value)
+            sut.enqueue(enqueueNumber)
         }
         
+        // when
         let result = sut.count
         
+        // then
         XCTAssertEqual(result, count)
     }
     
-    func test_n번의_enque와_m번의_deque를_한후_큐에_남아있는_요소의_개수가_n빼기m과_동일한가() {
-        let count: Int = Int.random(in: 1...9)
-        let removeCount: Int = Int.random(in: 1...count)
+    // MARK: - enqeue + deqeue
+    func test_enqueue로_요소를_추가하고_dequeue실행으로_나오는값이_추가한요소와_동일한가() {
+        // given
+        sut.enqueue(testNumber)
         
-        for value in 0..<count {
-            sut.enqueue(Double(value))
-        }
+        // when
+        let result = sut.dequeue()
         
-        for _ in 0..<removeCount {
-            sut.dequeue()
-        }
-        
-        let result = sut.count
-        let targetNumber = count - removeCount
-        XCTAssertEqual(result, targetNumber)
+        // then
+        XCTAssertEqual(testNumber, result)
     }
+    
 }
