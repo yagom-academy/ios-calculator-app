f78012c36b1c54c0ff59bfd1e874fd10278cff22
lnhk98 <hozzang666@gmai.com>
2022-09-22 15:10:29 +0900

    feat: ExpressionParser 타입 생성

---
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -0,0 +1,18 @@
+//
+//  ExpressionParser.swift
+//  Calculator
+//
+//  Created by 김인호 on 2022/09/22.
+//
+
+import Foundation
+
+enum ExpressionParser {
+    static func parse(from input: String) -> Formula {
+        
+    }
+    
+    private static func componentsByOperators(from input: String) -> [String] {
+        
+    }
+}
