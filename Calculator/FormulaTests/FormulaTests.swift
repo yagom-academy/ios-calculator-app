e97dac060413ab778ac75c948e903b03cbee0279
Jae Hyung Kim <smfc.mene@gmail.com>
2022-09-22 22:05:00 +0800

    test: Formula 구조체의 result메서드 add부분 테스트케이스 작성 및 테스트

---
+++ Calculator/FormulaTests/FormulaTests.swift
@@ -0,0 +1,57 @@
+//
+//  FormulaTests.swift
+//  FormulaTests
+//
+//  Created by smfc on 22/9/2022.
+//
+
+import XCTest
+@testable import Calculator
+
+class FormulaTests: XCTestCase {
+    var sut: Formula!
+
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        sut = Formula(operands: CalculatorItemQueue(), operators: CalculatorItemQueue())
+    }
+
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+        sut = nil
+    }
+    
+    func test_operands에한개의숫자만존재할때_return을호출하여도계산되지않는지확인() {
+        // given
+        sut = Formula(operands: CalculatorItemQueue(enqueueStack: [1]), operators: CalculatorItemQueue(enqueueStack: [.add]))
+
+        // when
+        let result = sut.result()
+        
+        // then
+       XCTAssertEqual(result, 0)
+    }
+    
+    func test_1과2를더하면_3을반환하는지확인() {
+        // given
+        sut = Formula(operands: CalculatorItemQueue(enqueueStack: [1, 2]), operators: CalculatorItemQueue(enqueueStack: [.add]))
+
+        // when
+        let result = sut.result()
+        
+        // then
+       XCTAssertEqual(result, 3)
+    }
+    
+    func test_1과2와3을더하면_6을반환하는지확인() {
+        // given
+        sut = Formula(operands: CalculatorItemQueue(enqueueStack: [1, 2, 3]), operators: CalculatorItemQueue(enqueueStack: [.add, .add]))
+
+        // when
+        let result = sut.result()
+        
+        // then
+       XCTAssertEqual(result, 6)
+    }
+
+}
