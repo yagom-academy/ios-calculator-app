bb94ae7aeb160315302c2135333cb18cd48fd38d
yuvinrho <yvrho@naver.com>
2022-09-23 02:43:24 +0900

    refactor: 불필요한 import 삭제

--- Calculator/Calculator/Models/Extensions/String+Extension.swift
+++ Calculator/Calculator/Models/Extensions/String+Extension.swift
@@ -5,8 +5,6 @@
 //  Created by 노유빈 on 2022/09/22.
 //
 
-import Foundation
-
 extension String {
     func split(with target: Character) -> [String] {
         return self.components(separatedBy: String(target))
