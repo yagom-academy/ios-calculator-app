f3d5224ec8aafc77cedde684335955b6a64667b4
lws2269 <lws2269@kakao.com>
2022-09-22 22:59:31 +0900

    refactor: 불필요한 개행삭제

--- Calculator/ExpressionParserTests/ExpressionParserTests.swift
+++ Calculator/ExpressionParserTests/ExpressionParserTests.swift
@@ -10,12 +10,9 @@ import XCTest
 
 class ExpressionParserTests: XCTestCase {
     
-    override func setUpWithError() throws {
-    }
+    override func setUpWithError() throws { }
 
-    override func tearDownWithError() throws {
-        
-    }
+    override func tearDownWithError() throws { }
     
     func test_string값의_식의_결과값이_356일때_string내부의값을_parse하여_연산한_결과값이_356와일치하는지() {
         let result: Double = 356
