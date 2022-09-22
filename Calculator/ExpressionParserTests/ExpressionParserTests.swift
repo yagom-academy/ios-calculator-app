ccdd9ec2129a538239839c6167392fc3cdb8eda4
jin <pearlisrich@naver.com>
2022-09-22 11:41:01 +0900

    fix: test_split_string_to_components_by_operators 함수 주석 처리 (private 접근 제어자로 더이상 테스트가 불가함)

--- Calculator/ExpressionParserTests/ExpressionParserTests.swift
+++ Calculator/ExpressionParserTests/ExpressionParserTests.swift
@@ -10,14 +10,14 @@ import XCTest
 
 class ExpressionParserTests: XCTestCase {
 
-    func test_split_string_to_components_by_operators() throws {
-        let input = "3+42-7÷32"
-        
-        let result = ExpressionParser.componentsByOperators(from: input)
-        
-        
-        XCTAssertEqual(result, ["3", "+", "42", "-", "7", "÷", "32"])
-    }
+//    func test_split_string_to_components_by_operators() throws {
+//        let input = "3+42-7÷32"
+//        
+//        let result = ExpressionParser.componentsByOperators(from: input)
+//        
+//        
+//        XCTAssertEqual(result, ["3", "+", "42", "-", "7", "÷", "32"])
+//    }
     
     func test_if_formula_is_made() {
         
