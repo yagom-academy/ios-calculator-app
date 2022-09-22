05e6764cd2ec99d07a158f1b264cb92901063aef
lnhk98 <hozzang666@gmai.com>
2022-09-22 16:57:40 +0900

    feat: Extension파일 생성

---
+++ Calculator/Calculator/Model/Extension.swift
@@ -0,0 +1,18 @@
+//
+//  Extension.swift
+//  Calculator
+//
+//  Created by 김인호 on 2022/09/22.
+//
+
+import Foundation
+
+extension String {
+    func split(with target: Character) -> [String] {
+        
+    }
+}
+
+extension Double: CalculateItem {
+    
+}
