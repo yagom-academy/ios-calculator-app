3b11ca05d3bb1bd49c52c05c8d8b2b32aa702445
jsa0224 <94514250+jsa0224@users.noreply.github.com>
2022-09-23 15:03:00 +0900

    feat: parse 메서드 추가

--- Calculator/Calculator/ExpressionParser.swift
+++ Calculator/Calculator/ExpressionParser.swift
@@ -8,6 +8,20 @@
 import Foundation
 
 enum ExpressionParser {
+    static func parse(from input: String) -> Formula {
+        let divided = ExpressionParser.componentsByOperators(from: input)
+        let operands = divided.compactMap { Double($0) }
+        let operators = divided.compactMap { Operator(rawValue: Character($0))}.map { $0.rawValue}
+        
+        var operandsToQueue = CalculatorItemQueue<Double>()
+        operands.forEach { operandsToQueue.enQueue(element: $0)}
+        
+        var operatorsToQueue = CalculatorItemQueue<Character>()
+        operators.forEach{ operatorsToQueue.enQueue(element: $0)}
+        
+        return Formula(operands: operandsToQueue, operators: operatorsToQueue)
+    }
+    
     private static func componentsByOperators(from input: String) -> [String] {
         var arithmeticExpressionArray = [input]
         var tempArray: [String] = []
