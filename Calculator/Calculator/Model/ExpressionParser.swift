5fbc22112db5ca86fbb0c00a3501dafcf46ed8ce
sunnyCookie <107384230+sunny-maeng@users.noreply.github.com>
2022-09-23 21:50:14 +0900

    refactor: 클로저 컨벤션 수정

--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -11,16 +11,16 @@ enum ExpressionParser {
     static func parse(from input: String) throws -> Formula {
         var formula = Formula()
         let operands = componentsByOperators(from: input)
-        let operators = input.map{ $0 }.compactMap{ Operator.init(rawValue: $0) }
+        let operators = input.map { $0 }.compactMap { Operator.init(rawValue: $0) }
         
-        operands.compactMap{ Double($0) }.forEach{ formula.operands.enqueue($0) }
-        operators.forEach{ formula.operators.enqueue($0) }
+        operands.compactMap { Double($0) }.forEach { formula.operands.enqueue($0) }
+        operators.forEach { formula.operators.enqueue($0) }
     
         return formula
     }
 
     static func componentsByOperators(from input: String) -> [String] {
-        let operators = CharacterSet(charactersIn: Operator.allCases.map{ String($0.rawValue) }.joined())
+        let operators = CharacterSet(charactersIn: Operator.allCases.map { String($0.rawValue) }.joined())
         return input.components(separatedBy: operators)
     }
 }
