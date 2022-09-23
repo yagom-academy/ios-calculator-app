656ce73e6f534d146f9705d4179dd70b6bcb25b9
junho <junhoL1046@gmail.com>
2022-09-23 16:24:00 +0900

    feat: parse, componentsByOperators 메서드 기능 구현

--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -5,12 +5,43 @@
 //  Created by junho lee on 2022/09/23.
 //
 
+import Foundation
+
 enum ExpressionParser {
     static func parse(from input: String) -> Formula {
-        return Formula()
+        var result: Formula = Formula()
+        
+        var filteredInput: String
+        filteredInput = input.replacingOccurrences(of: "+-", with: "+")
+        filteredInput = filteredInput.replacingOccurrences(of: "--", with: "-")
+        filteredInput = filteredInput.replacingOccurrences(of: "*-", with: "*")
+        filteredInput = filteredInput.replacingOccurrences(of: "/-", with: "/")
+        filteredInput = filteredInput.filter("+-*/".contains)
+        for operatorRawValue in filteredInput {
+            if let operatorToEnqueue: Operator = Operator.init(rawValue: operatorRawValue) {
+                result.operators.enqueue(operatorToEnqueue)
+            }
+        }
+        
+        let operands = componentsByOperators(from: input)
+        var nextOperandisMinus: Bool = false
+        for operand in operands {
+            if let operand: Double = Double(operand) {
+                let operandToEnqueue: Double = nextOperandisMinus ? -operand : operand
+                result.operands.enqueue(operandToEnqueue)
+                nextOperandisMinus = false
+            } else if operand == "" {
+                nextOperandisMinus = true
+            }
+        }
+        return result
     }
     
     private static func componentsByOperators(from input: String) -> [String] {
-        return []
+        var result: Array<String> = [input]
+        for operatorCase in Operator.allCases {
+            result = result.flatMap { $0.split(with: operatorCase.rawValue) }
+        }
+        return result
     }
 }
