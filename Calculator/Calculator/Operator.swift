83e05d7281bc83a4bcf19635fdb00f32bc3f4610
jsa0224 <94514250+jsa0224@users.noreply.github.com>
2022-09-23 14:37:27 +0900

    feat: Operator 소스 파일 생성 & enum 타입 선언

---
+++ Calculator/Calculator/Operator.swift
@@ -0,0 +1,15 @@
+//
+//  Operator.swift
+//  Calculator
+//
+//  Created by 정선아 on 2022/09/23.
+//
+
+import Foundation
+
+enum Operator: Character, CaseIterable {
+    case add = "+"
+    case subtract = "-"
+    case divide = "/"
+    case multiply = "*"
+}
