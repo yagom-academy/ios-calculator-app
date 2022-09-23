5a3d486428843f7642ed40bdf32e55b0f8c2ef93
DragonYG <kioswe92@gmail.com>
2022-09-23 18:09:12 +0900

    feat: extension-String파일 생성

---
+++ Calculator/Calculator/Model/String.swift
@@ -0,0 +1,13 @@
+//
+//  String.swift
+//  Calculator
+//
+//  Created by dragon on 2022/09/23.
+//
+
+extension String {
+    func split(with target: Character) -> [String] {
+        let result = [String]()
+        return result
+    }
+}
