55394c00ec94b6b51ce92c107e2a1ea484411947
junho <junhoL1046@gmail.com>
2022-09-23 14:47:33 +0900

    feat: 열거형 ExpressionParser 추가

---
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -0,0 +1,16 @@
+//
+//  ExpressionParser.swift
+//  Calculator
+//
+//  Created by junho lee on 2022/09/23.
+//
+
+enum ExpressionParser {
+    static func parse(from input: String) -> Formula {
+        return Formula()
+    }
+    
+    private static func componentsByOperators(from input: String) -> [String] {
+        return []
+    }
+}
