1a3da31b70acf578063f6f10711c6a7639be26a4
zhilly11 <zhzh1x4@gmail.com>
2022-09-23 22:00:59 +0900

    refactor: 바뀐 CalculatorItemQueue에 맞춰 네이밍 수정 및 test case 수정

--- Calculator/CalculatorTests/CalculatorItemQueueTests.swift
+++ Calculator/CalculatorTests/CalculatorItemQueueTests.swift
@@ -18,109 +18,100 @@ final class CalculatorItemQueueTests: XCTestCase {
         sut = nil
     }
     
-    func test_queue가비어있을때_isEmpty가true인지() {
-        //given
-        
-        //when
-        
-        //then
-        XCTAssertTrue(sut.isEmpty)
-    }
-    
     func test_queue를_Double타입으로_빈queue생성할때_Error가안나고_isEmpty가true인지(){
         //given
+        let queue = CalculatorItemQueue<Double>()
         
         //when
-        let queue = CalculatorItemQueue<Double>()
         
         //then
         XCTAssertTrue(queue.isEmpty)
     }
     
-    func test_queue에_3을push했을때_sut가에러가안나는지() {
+    func test_queue에_3을enqueue했을때_isEmpty가False인지() {
         //given
         let element: String = "3"
         
         //when
-        sut.push(element)
-        
-        //then
-        XCTAssertNoThrow(sut)
-    }
-    
-    func test_queue에_3을push했을때_isEmpty가false인지() {
-        //given
-        let element: String = "3"
-        
-        //when
-        sut.push(element)
+        sut.enqueue(element)
         
         //then
         XCTAssertFalse(sut.isEmpty)
     }
     
-    func test_queue에_3_6_9를push했을때_isEmpty가False인지() {
+    func test_queue에_3을enqueue했을때_isEmpty가false인지() {
+        //given
+        let element: String = "3"
+        
+        //when
+        sut.enqueue(element)
+        
+        //then
+        XCTAssertFalse(sut.isEmpty)
+    }
+    
+    func test_queue에_3_6_9를enqueue했을때_isEmpty가False인지() {
         //given
         let elementArray: [String] = ["3", "6", "9"]
         
         //when
         for element in elementArray {
-            sut.push(element)
+            sut.enqueue(element)
         }
         
         //then
         XCTAssertFalse(sut.isEmpty)
     }
     
-    func test_queue에_3_6_9를push하고pop을한번했을때_pop된요소가맨앞의값_3과같은값인지() {
+    func test_queue에_3_6_9를enqueue하고dequeue을한번했을때_dequeue된요소가맨앞의값_3과같은값인지() {
         //given
         let elementArray: [String] = ["3", "6", "9"]
         
         //when
         for element in elementArray {
-            sut.push(element)
+            sut.enqueue(element)
         }
-        let result = sut.pop()
+        let result = sut.dequeue()
         
         //then
         XCTAssertEqual(result, "3")
     }
     
-    func test_queue에pop할요소가없을때_nil값을반환하는지() {
+    func test_queue에dequeue할요소가없을때_nil값을반환하는지() {
         //given
         
         //when
-        let result = sut.pop()
+        let result = sut.dequeue()
         
         //then
         XCTAssertNil(result)
     }
     
-    func test_queue에3_6_9를push하고queue의count보다많은pop을했을때_결과가nil이나오는지() {
+    func test_queue에3_6_9를enqueue하고queue의count보다많은dequeue을했을때_결과가nil이나오는지() {
         //given
         let elementArray: [String] = ["3", "6", "9"]
         
         //when
         for element in elementArray {
-            sut.push(element)
+            sut.enqueue(element)
         }
         
         var result: String?
         for _ in 1...sut.count + 1 {
-            result = sut.pop()
+            result = sut.dequeue()
         }
         
         //then
         XCTAssertNil(result)
     }
     
-    func test_queue에1_2_3_4_5를push했을때count값이5인지() {
+    func test_queue에1_2_3_4_5를enqueue했을때count값이5인지() {
         //given
         let elementArray: [String] = ["1", "2", "3", "4", "5"]
         
         //when
         for element in elementArray {
-            sut.push(element)
+            sut.enqueue(element)
         }
         let result = sut.count
     
