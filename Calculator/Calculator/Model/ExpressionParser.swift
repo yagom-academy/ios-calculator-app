af122767f2d318253be15663ede753391871f76d
Jae Hyung Kim <smfc.mene@gmail.com>
2022-09-23 10:31:13 +0800

    test: ExpressionParser 구조체의 componentsByOperators 메서드 테스트 및 구현로직 수정

--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -5,12 +5,31 @@
 //  Created by mene on 22/9/2022.
 //
 
+import Foundation
+
 enum ExpressionParser {
-    static func parse(from input: String) -> Formula {
-        
-    }
+        static func parse(from input: String) -> Formula {
+            let components = componentsByOperators(from: input)
+            let operandsQueue = components.compactMap{
+                Double($0)
+            }
+            let operatorsQueue = components.compactMap{
+                Operator(rawValue: Character($0))
+            }
+            let formula = Formula(operands: CalculatorItemQueue(enqueueStack: operandsQueue), operators: CalculatorItemQueue(enqueueStack: operatorsQueue))
+    
+            return formula
+        }
     
     private static func componentsByOperators(from input: String) -> [String] {
+        var separators: CharacterSet = CharacterSet()
         
+        Operator.allCases.forEach {
+            separators.insert(charactersIn: String($0.rawValue))
+        }
+        
+        let result = input.components(separatedBy: separators)
+   
+        return result
     }
 }
--- Calculator/CalculatorTests/ExpressionParserTests.swift
+++ Calculator/CalculatorTests/ExpressionParserTests.swift
@@ -2,7 +2,7 @@
 //  ExpressionParserTests.swift
 //  ExpressionParserTests
 //
-//  Created by smfc on 22/9/2022.
+//  Created by mene on 22/9/2022.
 //
 
 import XCTest
@@ -13,7 +13,6 @@ class ExpressionParserTests: XCTestCase {
     
     override func setUpWithError() throws {
         try super.setUpWithError()
-        sut = ExpressionParser()
     }
 
     override func tearDownWithError() throws {
@@ -21,5 +20,70 @@ class ExpressionParserTests: XCTestCase {
         sut = nil
     }
 
+    func test_1더하기2를입력했을때_숫자배열이순서대로반환되는지확인() {
+        // given
+        let input = "1+2"
+        
+        // when
+        let result = ExpressionParser.componentsByOperators(from: input)
+        
+        // then
+       XCTAssertEqual(result, ["1", "2"])
+    }
+    
+    func test_1더하기2더하기3을입력했을때_숫자배열이순서대로반환되는지확인() {
+        // given
+        let input = "1+2+3"
+        
+        // when
+        let result = ExpressionParser.componentsByOperators(from: input)
+        
+        // then
+       XCTAssertEqual(result, ["1", "2", "3"])
+    }
+    
+    func test_1뺴기2빼기3을입력했을때_숫자배열이순서대로반환되는지확인() {
+        // given
+        let input = "1-2-3"
+        
+        // when
+        let result = ExpressionParser.componentsByOperators(from: input)
+        
+        // then
+       XCTAssertEqual(result, ["1", "2", "3"])
+    }
+    
+    func test_1곱하기2곱하기3곱하기를입력했을때_숫자배열이순서대로반환되는지확인() {
+        // given
+        let input = "1✕2✕3"
+        
+        // when
+        let result = ExpressionParser.componentsByOperators(from: input)
+        
+        // then
+       XCTAssertEqual(result, ["1", "2", "3"])
+    }
+    
+    func test_10나누기5나누기2를입력했을때_숫자배열이순서대로반환되는지확인() {
+        // given
+        let input = "10÷5÷2"
+        
+        // when
+        let result = ExpressionParser.componentsByOperators(from: input)
+        
+        // then
+       XCTAssertEqual(result, ["10", "5", "2"])
+    }
+    
+    func test_1곱하기10빼기5나누기2를입력했을때_숫자배열이순서대로반환되는지확인() {
+        // given
+        let input = "1✕10-5÷2"
+        
+        // when
+        let result = ExpressionParser.componentsByOperators(from: input)
+        
+        // then
+       XCTAssertEqual(result, ["1", "10", "5", "2"])
+    }
 
 }
