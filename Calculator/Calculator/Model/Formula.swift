20d57b4f6214a352b149995f66f7a187de8fd606
미니 <cow970814@naver.com>
2022-09-23 18:39:18 +0900

    feat : Formula 데이터 설정 메서드 추가

--- Calculator/Calculator/Model/Formula.swift
+++ Calculator/Calculator/Model/Formula.swift
@@ -6,12 +6,15 @@
 //
 
 struct Formula {
-    private var operands: CalculatorItemQueue<Double>
-    private var operators: CalculatorItemQueue<Operator>
+    private var operands = CalculatorItemQueue<Double>()
+    private var operators = CalculatorItemQueue<Operator>()
     
-    init(operands: CalculatorItemQueue<Double>, operators: CalculatorItemQueue<Operator>) {
-        self.operands = operands
-        self.operators = operators
+    mutating func setUpOperands(operands: [Double]) {
+        self.operands = CalculatorItemQueue(elements: operands)
+    }
+    
+    mutating func setUpOperators(operators: [Operator]) {
+        self.operators = CalculatorItemQueue(elements: operators)
     }
     
     func result() -> Double {
