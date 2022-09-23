03b1ef84c813d61862f73d63dd85c8977ffdb39a
DragonYG <kioswe92@gmail.com>
2022-09-23 18:32:22 +0900

    feat: enum-ExpressionParser 생성

---
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -0,0 +1,17 @@
+//
+//  ExpressionParser.swift
+//  Calculator
+//
+//  Created by dragon on 2022/09/23.
+//
+
+enum ExpressionParser {
+//    public func parse(from input: String) -> Formula {
+//        let result = Formula(operands: , operators: CalculatorItemQueue)
+//    }
+    
+    private func componentsByOperators(from input: String) -> [String] {
+        let result = [String]()
+        return result
+    }
+}
