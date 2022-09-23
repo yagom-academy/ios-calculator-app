e8047969861613a22f46c7517e1e5ed26b981d84
DragonYG <kioswe92@gmail.com>
2022-09-23 17:32:17 +0900

    feat: enum-Operator파일 생성

---
+++ Calculator/Calculator/Model/Operator.swift
@@ -0,0 +1,34 @@
+//
+//  Operator.swift
+//  Calculator
+//
+//  Created by dragon on 2022/09/23.
+//
+
+enum Operator: Character, CaseIterable {
+    case add = "+"
+    case subtract = "-"
+    case divide = "/"
+    case multiply = "*"
+    
+    public func calculate(lhs: Double, rhs: Double) -> Double {
+        let result = 0.0
+        return result
+    }
+    
+    private func add(lhs: Double, rhs: Double) -> Double {
+        return (lhs+rhs)
+    }
+    
+    private func subtract(lhs: Double, rhs: Double) -> Double {
+        return (lhs-rhs)
+    }
+    
+    private func divide(lhs: Double, rhs: Double) -> Double {
+        return (lhs/rhs)
+    }
+    
+    private func multiply(lhs: Double, rhs: Double) -> Double {
+       return (lhs*rhs)
+    }
+}
