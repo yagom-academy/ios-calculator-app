e54c76d0e4a5f559eb68b8ccbf197f480538570c
jsa0224 <94514250+jsa0224@users.noreply.github.com>
2022-09-23 14:53:14 +0900

    feat: Formula 소스 파일 생성 & struct 타입 선언

---
+++ Calculator/Calculator/Formula.swift
@@ -0,0 +1,13 @@
+//
+//  Formula.swift
+//  Calculator
+//
+//  Created by 정선아 on 2022/09/23.
+//
+
+import Foundation
+
+struct Formula {
+    var operands: CalculatorItemQueue<Double>
+    var operators: CalculatorItemQueue<Character>
+}
