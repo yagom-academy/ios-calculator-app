e57c697cf23923e0c2c59794c26c16fbb4cac51a
junho <junhoL1046@gmail.com>
2022-09-23 19:18:10 +0900

    refactor: 음수 체크 기능을 parse 메서드에서 componentsByOperators 메서드로 이동, 연산자 filter 코드 제거

--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -11,14 +11,8 @@ enum ExpressionParser {
     static func parse(from input: String) -> Formula {
         var result: Formula = Formula()
         
-        var isNegative: Bool = false
-        componentsByOperators(from: input).forEach {
-            if let operand: Double = Double($0) {
-                result.operands.enqueue(isNegative ? -operand : operand)
-                isNegative = false
-            } else if $0 == "-" {
-                isNegative = true
-            }
+        componentsByOperators(from: input).compactMap { Double($0) }.forEach {
+            result.operands.enqueue($0)
         }
         
         var inputRemovedNegativeSignal: String = input
@@ -28,8 +22,7 @@ enum ExpressionParser {
         Operator.allCases.forEach {
             inputRemovedNegativeSignal = inputRemovedNegativeSignal.replacingOccurrences(of: "\($0.rawValue)-", with: "\($0.rawValue)")
         }
-        let filteredOperators: String = inputRemovedNegativeSignal.filter("+-×÷".contains)
-        filteredOperators.compactMap { Operator.init(rawValue: $0) }.forEach {
+        inputRemovedNegativeSignal.compactMap { Operator.init(rawValue: $0) }.forEach {
             result.operators.enqueue($0)
         }
         
@@ -41,6 +34,17 @@ enum ExpressionParser {
         for operatorCase in Operator.allCases {
             result = result.flatMap { $0.split(with: operatorCase.rawValue) }
         }
-        return result.map { $0 == "" ? "-" : $0 }
+        var isNegative: Bool = false
+        result = result.compactMap {
+            if $0 == "" {
+                isNegative = true
+                return nil
+            } else {
+                let value: String = isNegative ? "-\($0)" : $0
+                isNegative = false
+                return value
+            }
+        }
+        return result
     }
 }
