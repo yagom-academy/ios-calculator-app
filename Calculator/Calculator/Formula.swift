871c5f353ad6e0da1ce932973a48a17a7917e0d8
jsa0224 <94514250+jsa0224@users.noreply.github.com>
2022-09-23 14:59:07 +0900

    feat: result 메서드 추가

--- Calculator/Calculator/Formula.swift
+++ Calculator/Calculator/Formula.swift
@@ -15,4 +15,25 @@ struct Formula {
         case emptyOperandsQueue
         case emptyOperatorsQueue
     }
+    
+    mutating func result() throws -> Double {
+        guard var result: Double = operands.deQueue() else {
+            throw FormulaError.emptyOperandsQueue
+        }
+        
+        while operators.isEmpty() == false {
+            guard let inputOperator = operators.deQueue() else {
+                throw FormulaError.emptyOperatorsQueue
+            }
+            
+            guard let inputOperands = operands.deQueue() else {
+                throw FormulaError.emptyOperandsQueue
+            }
+            
+            let eachOperator = Operator(rawValue: inputOperator)
+            result = (eachOperator?.calculate(lhs: result, rhs: inputOperands)) ?? 0
+        }
+        
+        return result
+    }
 }
