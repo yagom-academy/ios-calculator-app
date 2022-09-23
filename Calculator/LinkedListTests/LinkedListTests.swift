1be7181eaa11c042457d0d46a062317913dc2a86
lxodud <xodud1998223@naver.com>
2022-09-23 14:00:40 +0900

    refactor: LinkedListTests 메서드 네이밍 수정

--- Calculator/LinkedListTests/LinkedListTests.swift
+++ Calculator/LinkedListTests/LinkedListTests.swift
@@ -19,7 +19,7 @@ class LinkedListTests: XCTestCase {
         sut = nil
     }
     
-    func test_HI를_append했을때잘들어가는지() {
+    func testLinkedList_문자열을_append했을때_해당문자열이잘들어가야한다() {
         sut.append("HI")
         
         guard let result = sut.removeFirst() as? String else {
@@ -29,24 +29,7 @@ class LinkedListTests: XCTestCase {
         XCTAssertEqual(result, "HI")
     }
     
-    func test_HI랑HO를_append했을때잘들어가는지() {
-        sut.append("HI")
-        sut.append("HO")
-        
-        guard let result = sut.removeFirst() as? String else {
-            return
-        }
-        
-        XCTAssertEqual(result, "HI")
-        
-        guard let result = sut.removeFirst() as? String else {
-            return
-        }
-        
-        XCTAssertEqual(result, "HO")
-    }
-    
-    func test_HI랑HO랑HU를_append한후removeAll을이용하여전부삭제가가능한지() {
+    func testLinkedList_문자열3개를_append한후_removeAll을이용하여전부삭제돼야한다() {
         sut.append("HI")
         sut.append("HO")
         sut.append("HU")
@@ -55,31 +38,12 @@ class LinkedListTests: XCTestCase {
         XCTAssertEqual(sut.bringHead() == nil && sut.bringTail() == nil, true)
     }
     
-    func test_HI랑HO랑HU를_append한후_제일마지막요소가HI인지() {
-        sut.append("HI")
-        sut.append("HO")
-        sut.append("HU")
+    func testLinkedList_LinkedList_가비었을때_removeFirst를하면_nil이나와야한다() {
         
-        guard let result = sut.bringTail()?.bringData() as? String else {
-            return
-        }
-        
-        XCTAssertEqual(result, "HU")
     }
     
-    func test_HI랑HO를append하고HI를remove하고_HU를다시append했을때_head가HO인지() {
-        sut.append("HI")
-        sut.append("HO")
+    func testLinkedList_removeFirst를했을때_제일앞에요소가나와야한다() {
         
-        let _ = sut.removeFirst()
-        
-        sut.append("HU")
-        
-        guard let result = sut.bringHead()?.bringData() as? String else {
-            return
-        }
-        
-        XCTAssertEqual(result, "HO")
     }
 }
 
