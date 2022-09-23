24fdf8c018256d57a52aa8bde8c8e3a0d8814f85
jsa0224 <94514250+jsa0224@users.noreply.github.com>
2022-09-23 17:36:01 +0900

    test: 1+3-2*10/2은 10인가 에 대한 테스트 케이스 추가

--- Calculator/FormulaTests/FormulaTests.swift
+++ Calculator/FormulaTests/FormulaTests.swift
@@ -64,4 +64,21 @@ class FormulaTests: XCTestCase {
         
         XCTAssertEqual(result, try sut.result())
     }
+    
+    func test_1_더하기_3_빼기_2_곱하기_10_나누기_2은_10로_나오는가() {
+        let result = 10.0
+        
+        sut.operands.enQueue(element: 1.0)
+        sut.operands.enQueue(element: 3.0)
+        sut.operands.enQueue(element: 2.0)
+        sut.operands.enQueue(element: 10.0)
+        sut.operands.enQueue(element: 2.0)
+        
+        sut.operators.enQueue(element: Operator.add.rawValue)
+        sut.operators.enQueue(element: Operator.subtract.rawValue)
+        sut.operators.enQueue(element: Operator.multiply.rawValue)
+        sut.operators.enQueue(element: Operator.divide.rawValue)
+        
+        XCTAssertEqual(result, try sut.result())
+    }
 }
