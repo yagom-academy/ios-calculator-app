18dc73134a3ec3ad36ef0f7b5ff7c6e3e26240ea
DragonYG <kioswe92@gmail.com>
2022-09-23 18:15:49 +0900

    feat: struct-Formula파일 생성

---
+++ Calculator/Calculator/Model/Formula.swift
@@ -0,0 +1,16 @@
+//
+//  Formula.swift
+//  Calculator
+//
+//  Created by dragon on 2022/09/23.
+//
+
+struct Formula {
+    public let operands: CalculatorItemQueue
+    public let operators: CalculatorItemQueue
+    
+    public func result() -> Double {
+        let result: Double = 0.0
+        return result
+    }
+}
