dbb50645af7d6c25e8018540d4ec3b19e4fe5005
미니 <cow970814@naver.com>
2022-09-23 19:40:02 +0900

    test : Formula Test 코드 변경 및 테스트 수행

--- Calculator/CalculatorItemQueueTests/FormulaTests.swift
+++ Calculator/CalculatorItemQueueTests/FormulaTests.swift
@@ -12,7 +12,7 @@ final class FormulaTests: XCTestCase {
     var sut: Formula!
     override func setUpWithError() throws {
         try super.setUpWithError()
-        sut = Formula()
+        sut = Formula(operands: [], operators: [])
     }
 
     override func tearDownWithError() throws {
@@ -21,16 +21,37 @@ final class FormulaTests: XCTestCase {
     }
 
     func test_피연산자의개수가연산자의수보다1만큼큰경우결과가잘나오는가() {
-        sut.setUpOperands(operands: [1,2,3,4,5])
-        sut.setUpOperators(operators: [.multiply,.add,.subtract,.add])
+        // driven
+        sut = Formula(operands: [1, 2, 3, 4, 5], operators: [.multiply, .add, .subtract, .add])
+
+        // when
         let result = sut.result()
         
-        XCTAssertEqual(result, 25)
+        // then
+        XCTAssertEqual(result, 6)
     }
     
-    func test_피연산자의개수가연산자의수보다작은경우결과가잘나오는가() {
+    func test_연산자의개수가피연산자의수보다많은경우제곱을하여서결과를반환하는가() {
+        // driven
+        sut = Formula(
+            operands: [1,2,3,4,5],
+            operators: [.multiply,.add,.add,.subtract,.multiply, .multiply]
+        )
+        
+        // when
         let result = sut.result()
         
-        XCTAssertEqual(result, 20)
+        // then
+        XCTAssertEqual(result, 256)
+    }
+    
+    func test_연산자의개수가피연산자의수보다적은경우피연산자가있는경우까지계산하여서결과를반환하는가() {
+        // driven
+        sut = Formula(operands: [1, 2, 3, 4, 5], operators: [.multiply, .add])
+        // when
+        let result = sut.result()
+        
+        // then
+        XCTAssertEqual(result, 5)
     }
 }
