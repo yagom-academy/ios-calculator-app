76356781903494dec9e2bc0fe5f857c98cc0e3ca
oneStar92 <onestar7984@gmail.com>
2022-09-24 21:52:43 +0900

    feat: ExpressionParser Type Static parse 메소드 구현.

--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -1,4 +1,17 @@
-enum ExpreesionParser {
+enum ExpressionParser {
+    static func parse(from input: String) -> Formula {
+        var formula: Formula = Formula()
+        let componentsResult: [String] = componentsByOperators(from: input)
+        componentsResult.forEach {
+            if let number: Double = Double($0) {
+                formula.operands.enqueue(number)
+            } else if let currentOperator: Operator = Operator.init(rawValue: Character($0)) {
+                formula.operators.enqueue(currentOperator)
+            }
+        }
+        return Formula()
+    }
+    
     private static func componentsByOperators(from input: String) -> [String] {
         var componentsResult: [String] = []
         var calculateData: String = ""
