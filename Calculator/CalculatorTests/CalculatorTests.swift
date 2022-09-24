8d064d2c9f8cd4d1691e75ac583ebe4f1abaf859
DragonYG <kioswe92@gmail.com>
2022-09-24 21:07:30 +0900

    refactor: UnitTest-테스트메서스명 수정

--- Calculator/CalculatorTests/CalculatorTests.swift
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -20,7 +20,7 @@ final class CalculatorTests: XCTestCase {
         try super.tearDownWithError()
     }
     
-    func test_enqueue를_A번_했을때_count가_A와_같아야함() {
+    func test_enqueue를_A번_할때_count가_A와_같아야함() {
         // Given
         sut.enqueue(element: "1")
         sut.enqueue(element: "+")
@@ -33,7 +33,7 @@ final class CalculatorTests: XCTestCase {
         XCTAssertEqual(count, 3)
     }
     
-    func test_dequeue했을때_배열에값이있을경우_배열의첫번째요소가져오고_배열에서값삭제후Count1감소되어야함_배열에값이없을경우_nil가져와야함() {
+    func test_dequeue할때_배열에값이있을경우_배열의첫번째요소가져와야함() {
         // Given
         sut.enqueue(element: "1")
         sut.enqueue(element: "+")
@@ -46,7 +46,17 @@ final class CalculatorTests: XCTestCase {
         XCTAssertEqual(result, "1")
     }
     
-    func test_peek했을때_배열에값이있을경우_배열의첫번째요소가져오고_배열에서값유지되어Count동일해야함_배열에값이없을경우_nil가져와야함() {
+    func test_dequeue할때_배열에값이없을경우_nil가져와야함() {
+        // Given
+        
+        // When
+        let result = sut.dequeue()
+        
+        // Then
+        XCTAssertEqual(result, nil)
+    }
+    
+    func test_peek할때_배열에값이있을경우_배열의첫번째요소가져와야함() {
         // Given
         sut.enqueue(element: "1")
         sut.enqueue(element: "+")
@@ -59,7 +69,17 @@ final class CalculatorTests: XCTestCase {
         XCTAssertEqual(result, "1")
     }
     
-    func test_clear했을때_배열에있는모든값이삭제되어_count가0이면서_isEmpty가True여야함() {
+    func test_peek할때_배열에값이없을경우_nil가져와야함() {
+        // Given
+        
+        // When
+        let result = sut.peek()
+        
+        // Then
+        XCTAssertEqual(result, nil)
+    }
+    
+    func test_clear할때_배열에있는모든값이삭제되어_isEmpty가True여야함() {
         // Given
         sut.enqueue(element: "1")
         sut.enqueue(element: "+")
