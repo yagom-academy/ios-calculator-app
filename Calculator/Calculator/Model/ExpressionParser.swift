425fd7b9704ff52070dfccd61e6dcb3eef995d85
LJ <swtw72@gmail.com>
2022-09-24 18:47:51 +0900

    refactor: componentsByOperators 수정

--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -7,10 +7,12 @@ import Foundation
 enum ExpressionParser {
     static func parse(from input: String) -> Formula {
         var formula: Formula = Formula()
-        let separatedInputOperands: Array<String> = input.components(separatedBy: ["+","-","*","/"])
+        let separatedOperands: Array<String> = input.components(separatedBy: ["+","-","*","/"])
         var convertArray: [String] = componentsByOperators(from: "10.0+20.0*3.0/2.0+3.0-2.0*9.0")
         
-        let doubleNumbersOperands = separatedInputOperands.map({Double($0) ?? 99.999})
+        let doubleNumbersOperands = separatedOperands.map{
+            Double($0) ?? 99.999
+        }
         for i in doubleNumbersOperands {
             formula.operands.enqueue(element: i)
         }
@@ -18,16 +20,15 @@ enum ExpressionParser {
             let value: Operator = Operator.init(rawValue: j)
             formula.operators.enqueue(element: value)
         }
-        print("convertArray : \(convertArray)")
-        print(formula.operators.enqueueStack)
         return formula
     }
     
     static func componentsByOperators(from input: String) -> [String] {
-        let separatedInputOperator =
-        input.filter{ ($0 == "+") || ($0 == "-") || ($0 == "*") || ($0 == "/") }
-        let convertedToStringArrayOperator = separatedInputOperator.map{String($0)}
-        return convertedToStringArrayOperator
+        return input.filter {
+            ($0 == "+") || ($0 == "-") || ($0 == "*") || ($0 == "/")
+        }.map {
+            String($0)
+        }
     }
 }
 
