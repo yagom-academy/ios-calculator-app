d73a445cd7b9ab1e5ae3e01eff206b31f99bbc66
wonbi92 <bin9239@gmail.com>
2022-09-24 23:09:56 +0900

    test: parse메서드에 랜덤한 피연산자와 연산자를 넣었을때 파싱이 정상적으로 이루어지는지 확인하는 케이스 추가

--- Calculator/CalculatorTests/ExpressionParserTests.swift
+++ Calculator/CalculatorTests/ExpressionParserTests.swift
@@ -7,7 +7,7 @@ import XCTest
 @testable import Calculator
 
 final class ExpressionParserTests: XCTestCase {
-    func test_componentsByOperators메서드에1더하기2곱하기3을넣었을때_1과2와3이나오는지() {
+    func test_parse메서드에1더하기2곱하기3을넣었을때_연산자와피연산자가제대로파싱되는지() {
         let input = "1 + 2 × 3"
         
         let result = ExpressionParser.parse(from: input)
@@ -21,5 +21,40 @@ final class ExpressionParserTests: XCTestCase {
         
         XCTAssertEqual(expectedOperands, resultOperands)
         XCTAssertEqual(expectedOperators, resultOperators)
+    }
+    
+    func test_parse메서드에랜덤한피연산자와연산자를넣었을때_파싱이정상적으로이루어지는지() {
+        // given
+        let routine = Int.random(in: 2...50)
+        let operators = ["+", "-", "÷", "×"]
+        var inputOperands = [Double]()
+        var inputOperators = [Operator]()
+        var input: String = ""
+        
+        for _ in 1...routine {
+            inputOperands.append(Double.random(in: -100.0...100.0))
+        }
+        input = String(inputOperands[0])
+        
+        for _ in 1..<routine {
+            inputOperators.append(Operator(rawValue: Character(operators[Int.random(in: 0...3)])) ?? Operator.add)
+        }
+        
+        for index in 1..<routine {
+            input.append(" \(String(inputOperators[index - 1].identifier))")
+            input.append(" \(String(inputOperands[index]))")
+        }
+        
+        // when
+        let result = ExpressionParser.parse(from: input)
+        guard let resultOperands = result.operands.statusQueue as? [Double],
+              let resultOperators = result.operators.statusQueue as? [Operator]
+        else {
+            return
+        }
+        
+        // then
+        XCTAssertEqual(inputOperands, resultOperands)
+        XCTAssertEqual(inputOperators, resultOperators)
     }
 }
