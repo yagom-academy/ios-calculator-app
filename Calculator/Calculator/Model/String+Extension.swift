5376ce88950250916bd0bd109bbbb5a8604402b9
dylan-yoon <d.dylany21@gmail.com>
2022-09-24 18:13:28 +0900

    feat: String에split 함수 Extension

---
+++ Calculator/Calculator/Model/String+Extension.swift
@@ -0,0 +1,12 @@
+//
+//  String+Extension.swift
+//  Calculator
+//
+//  Created by Baem on 2022/09/24.
+//
+
+extension String {
+    func split(with target: Character) -> [String] {
+        return [String(target)]
+    }
+}
