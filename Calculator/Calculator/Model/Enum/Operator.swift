c279b0a4e565e85fe7e8a7d004833d2bbed3cbfa
DragonYG <kioswe92@gmail.com>
2022-09-24 21:56:33 +0900

    feat: Operator-calculate메서드 로직 구현

--- Calculator/Calculator/Model/Enum/Operator.swift
+++ Calculator/Calculator/Model/Enum/Operator.swift
@@ -12,8 +12,16 @@ enum Operator: Character, CaseIterable, CalculateItem {
     case multiply = "*"
     
     public func calculate(lhs: Double, rhs: Double) -> Double {
-        let result = 0.0
-        return result
+        switch self {
+        case .add:
+            return self.add(lhs: lhs, rhs: rhs)
+        case .subtract:
+            return self.subtract(lhs: lhs, rhs: rhs)
+        case .divide:
+            return self.divide(lhs: lhs, rhs: rhs)
+        case .multiply:
+            return self.multiply(lhs: lhs, rhs: rhs)
+        }
     }
     
     private func add(lhs: Double, rhs: Double) -> Double {
