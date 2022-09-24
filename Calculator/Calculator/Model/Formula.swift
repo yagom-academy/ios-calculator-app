0ec21e6fac90331a5acd2ea1649fd2f2231a249d
dylan-yoon <d.dylany21@gmail.com>
2022-09-24 18:03:09 +0900

    feat: Formula 구현

---
+++ Calculator/Calculator/Model/Formula.swift
@@ -0,0 +1,15 @@
+//
+//  Formula.swift
+//  Calculator
+//
+//  Created by Baem on 2022/09/24.
+//
+
+struct Formula {
+    let operands: CalculatorItemQueue
+    let operators: CalculatorItemQueue
+    
+    func result() -> Double {
+        return 1
+    }
+}
