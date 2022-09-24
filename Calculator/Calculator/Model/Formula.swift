8fff5a9bcc65b99c280a3b4730d1790f0874fbc9
dev-summer <summerly812@gmail.com>
2022-09-24 20:32:36 +0900

    feat: create Formula struct

---
+++ Calculator/Calculator/Model/Formula.swift
@@ -0,0 +1,15 @@
+//
+//  Formula.swift
+//  Calculator
+//
+//  Created by Jiyoung Lee on 2022/09/24.
+//
+
+struct Formula {
+    let operands: CalculatorItemQueue
+    let operators: CalculateItem
+    
+    func result() -> Double {
+        
+    }
+}
