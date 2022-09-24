9942a92cf8f9bfbd7208b1bdbf7e64f049ed69fe
dev-summer <summerly812@gmail.com>
2022-09-24 19:01:01 +0900

    feat: create Operator enum

---
+++ Calculator/Calculator/Model/Operator.swift
@@ -0,0 +1,12 @@
+//
+//  Operator.swift
+//  Calculator
+//
+//  Created by Jiyoung Lee on 2022/09/24.
+//
+
+import Foundation
+
+enum Operator: Character, CaseIterable {
+    case add = "+", subtract = "-", divide = "/" , multiply = "*"
+}
