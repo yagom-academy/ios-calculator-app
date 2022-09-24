04f5dc9cf1a983c275c2f227b66896600bf97507
zhilly11 <zhzh1x4@gmail.com>
2022-09-24 18:18:24 +0900

    feat: OperatorTests 파일 생성

---
+++ Calculator/CalculatorTests/OperatorTests.swift
@@ -0,0 +1,16 @@
+//  OperatorTests.swift
+//  Created by zhilly on 2022/09/24.
+
+import XCTest
+@testable import Calculator
+
+final class OperatorTests: XCTestCase {
+
+    override func setUpWithError() throws {
+
+    }
+
+    override func tearDownWithError() throws {
+
+    }
+}
