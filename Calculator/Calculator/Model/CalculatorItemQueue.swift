b0f5e5e4ed010ecc9238b506e5b5bdd6d40dc437
ash-youu <ash.you.dev@gmail.com>
2022-09-26 01:59:51 +0900

    refactor: 타입별 파일 구분 및 구분에 따른 접근 제어 수정

--- Calculator/Calculator/Model/CalculatorItemQueue.swift
+++ Calculator/Calculator/Model/CalculatorItemQueue.swift
@@ -3,68 +3,6 @@
 //  Calculator
 //
 
-struct LinkedList<T> {
-    class Node<T>: CalculateItem {
-        var value: T
-        var next: Node?
-        
-        fileprivate init(_ value: T, next: Node? = nil) {
-            self.value = value
-            self.next = next
-        }
-    }
-    
-    fileprivate(set) var head: Node<T>?
-    fileprivate var nodeCount: Int = 0
-    fileprivate var isEmpty: Bool {
-        if nodeCount == 0 {
-            return true
-        } else {
-            return false
-        }
-    }
-    
-    fileprivate mutating func append(_ element: T) {
-        guard isEmpty == false else {
-            head = Node(element)
-            nodeCount += 1
-            return
-        }
-
-        var node = head
-        while node?.next != nil {
-            node = node?.next
-        }
-        
-        node?.next = Node(element)
-        nodeCount += 1
-    }
-    
-    fileprivate mutating func removeFirst() {
-        head = head?.next
-        nodeCount -= 1
-    }
-    
-    fileprivate mutating func removeLast() {
-        if head == nil { return }
-        
-        var node = head
-        while node?.next != nil {
-            node = node?.next
-        }
-        
-        node = nil
-        nodeCount -= 1
-    }
-    
-    fileprivate mutating func removeAll() {
-        head = nil
-        nodeCount = 0
-    }
-    
-    fileprivate init() {}
-}
-
 struct CalculatorItemQueue<T> {
     var list: LinkedList<T> = LinkedList<T>()
     
