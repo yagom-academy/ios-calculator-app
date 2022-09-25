5ebc10d2e1965b5697d3d646dec6439bc91dc89c
jin <pearlisrich@naver.com>
2022-09-26 00:19:05 +0900

    feat: number 1~9 및 0, 소수점, 00 @IBAction 메서드 추가

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -17,8 +17,53 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func numberZeroButtonPressed(_ sender: UIButton) {
+        updateOperandNumber(with: "0")
+    }
+    
+    @IBAction func numberDoubleZeroButtonPressed(_ sender: UIButton) {
+        updateOperandNumber(with: "00")
+    }
+    
+    @IBAction func decimalPointButtonPressed(_ sender: UIButton) {
+        updateOperandNumber(with: ".")
+    }
+    
     @IBAction func numberOnePressed(_ sender: UIButton) {
         updateOperandNumber(with: "1")
+    }
+    
+    @IBAction func numberTwoButtonPressed(_ sender: UIButton) {
+        updateOperandNumber(with: "2")
+    }
+    
+    @IBAction func numberThreeButtonPressed(_ sender: UIButton) {
+        updateOperandNumber(with: "3")
+    }
+    
+    @IBAction func numberFourButtonPressed(_ sender: UIButton) {
+        updateOperandNumber(with: "4")
+    }
+    
+    @IBAction func numberFiveButtonPressed(_ sender: UIButton) {
+        updateOperandNumber(with: "5")
+    }
+    
+    @IBAction func numberSixButtonPressed(_ sender: UIButton) {
+        updateOperandNumber(with: "6")
+    }
+    
+    @IBAction func numberSevenButtonPressed(_ sender: UIButton) {
+        updateOperandNumber(with: "7")
+    }
+    
+    @IBAction func numberEightButtonPressed(_ sender: UIButton) {
+        updateOperandNumber(with: "8")
+    }
+    
+    @IBAction func numberNineButtonPressed(_ sender: UIButton) {
+        updateOperandNumber(with: "9")
     }
     
     func updateOperandNumber(with number: String) {
