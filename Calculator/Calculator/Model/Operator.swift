355b594c9e52e25bd6e14d4f189e719043fa04f8
lxodud <xodud1998223@naver.com>
2022-09-26 00:10:57 +0900

    feat: Operator 열거형 생성

---
+++ Calculator/Calculator/Model/Operator.swift
@@ -0,0 +1,13 @@
+//
+//  Operator.swift
+//  Calculator
+//
+//  Created by Hamo on 2022/09/26.
+//
+
+enum Operator: Character, CaseIterable {
+    case add = "+"
+    case subtract = "-"
+    case divide = "%"
+    case multiply = "*"
+}
