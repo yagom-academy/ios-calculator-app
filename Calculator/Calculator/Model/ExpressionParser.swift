ec744b3c99cef534390e254bd35c7f47675d816f
미니 <cow970814@naver.com>
2022-09-25 09:48:21 +0900

    feat : 연산자 및 피 연산자 구분하여 구문 생성하는 매서드 생성

--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -8,7 +8,10 @@
 
 enum ExpressionParser {
     static func parse(from input: String) -> Formula {
-        return Formula()
+        let operators: [Operator] = Array(input.compactMap { Operator(rawValue: $0) })
+        let operands: [Double] = componenetsByOperators(from: input).compactMap { Double($0) }
+        
+        return Formula(operands: operands, operators: operators)
     }
     
     static func componenetsByOperators(from input: String) -> [String] {
