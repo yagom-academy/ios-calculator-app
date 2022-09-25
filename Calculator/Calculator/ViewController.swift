efe569a0734a24bb44ffd948025331432d637799
jin <pearlisrich@naver.com>
2022-09-25 23:57:56 +0900

    feat: ViewController에 expression, operand(@IBOutlet), `operator`(@IBOutlet) 변수 선언

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -8,6 +8,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var expression: String = ""
+    
+    @IBOutlet weak var operand: UILabel!
+    @IBOutlet weak var `operator`: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
