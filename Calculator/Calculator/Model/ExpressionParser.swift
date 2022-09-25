f4aedf7922f6ffcd69edb80b97db36f9f4ce0fe5
미니 <cow970814@naver.com>
2022-09-25 09:03:27 +0900

    feat : ExpressionParser 타입 생성

---
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -0,0 +1,19 @@
+//
+//  ExpressionParser.swift
+//  Calculator
+//
+//  Created by 이경민 on 2022/09/25.
+//
+
+
+enum ExpressionParser {
+    static func parse(from input: String) -> Formula {
+    
+        return Formula()
+    }
+    
+    static func componenetsByOperators(from input: String) -> [String] {
+        
+        return []
+    }
+}
