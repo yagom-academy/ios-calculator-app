666418548145485333592612ac0b56b73e934aa7
lxodud <xodud1998223@naver.com>
2022-09-26 00:59:47 +0900

    test: testOperator_Double타입의값2개가_calculator메서드의_인자로들어갔을때_두값의차가반환되어야한다() 테스트 메서드 구현

--- Calculator/OperatorTests/OperatorTests.swift
+++ Calculator/OperatorTests/OperatorTests.swift
@@ -24,4 +24,15 @@ class OperatorTests: XCTestCase {
         // then
         XCTAssertEqual(result, 3.0)
     }
+    
+    func testOperator_Double타입의값2개가_calculator메서드의_인자로들어갔을때_두값의차가반환되어야한다() {
+        // given
+        sut = Operator.add
+        let lhs = 2.0
+        let rhs = 1.0
+        // when
+        let result = sut.calculate(lhs: lhs, rhs: rhs)
+        // then
+        XCTAssertEqual(result, 1.0)
+    }
 }
