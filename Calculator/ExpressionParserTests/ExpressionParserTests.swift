3e7cd0d3e6e41aae4de8eb563af5daa243ea628c
yuvinrho <yvrho@naver.com>
2022-09-26 00:04:44 +0900

    test: ExpressionParserTests 테스트 코드 추가
    # <type>(<scope>) : <subject>
    # 제목은 50자 이내 / 변경사항이 "무엇"인지 명확히 작성 / 끝에 마침표 X
    # 타입과 제목은 필수 / 범위는 선택
    # 예) feat : 로그인 기능 추가
    
    # 바로 아래 공백 유지 (제목과 본문의 분리를 위함)
    
    ################
    # Body Message (선택사항)
    # 본문(구체적인 내용)을 아랫줄에 작성
    # 여러 줄의 메시지를 작성할 땐 "-"로 구분 (한 줄은 72자 이내)
    
    ################
    # 꼬릿말(footer)을 아랫줄에 작성 (현재 커밋과 관련된 이슈 번호 추가 등)
    # 예) Close #7
    
    # Issue Tracker Number or URL
    
    # --- COMMIT END ---
    # <type> list
    #   feat    : 새로운 기능
    #   fix     : 버그
    #   refactor: 코드 리팩토링
    #   style   : 코드 의미에 영향을 주지 않는 변경사항 (형식 지정, 세미콜론 누락 등)
    #   docs    : 문서의 추가, 수정, 삭제
    #   test    : 테스트 추가, 수정, 삭제 (비즈니스 로직에 변경 없음)
    #   chore   : 기타 변경사항 (빌드 부분 혹은 패키지 매니저 수정사항)
    # ------------------
    # Remember me ~
    #   Capitalize the subject line
    #     제목 첫 글자를 대문자로
    #   Use the imperative mood in the subject line
    #     제목은 명령문 사용 (과거형 X)
    #   Do not end the subject line with a period
    #     제목 끝에 마침표(.) 금지
    #   Separate subject from body with a blank line
    #     제목과 본문을 한 줄 띄워 분리하기 (빈 행으로 구분)
    #   Use the body to explain what and why vs. how
    #     본문은 "어떻게" 보다 "무엇을", "왜"를 설명한다.
    #   Can use multiple lines with "-" for bullet points in body
    #     본문에 여러줄의 메시지(목록)를 작성할 땐 "-"로 구분
    # ------------------

--- Calculator/ExpressionParserTests/ExpressionParserTests.swift
+++ Calculator/ExpressionParserTests/ExpressionParserTests.swift
@@ -18,7 +18,45 @@ final class ExpressionParserTests: XCTestCase {
         try super.tearDownWithError()
     }
     
-    func test_string타입_식이_주어지면_파싱하여_결과가_나오는지_확인() {
+    func test_더하기_string타입_식이_주어지면_파싱하여_결과가_나오는지_확인() {
+        let stringFormula = "123+25+30"
+
+        var structFormula = ExpressionParser.parser(from: stringFormula)
+        let result = try! structFormula.result()
+
+        XCTAssertEqual(result, 178)
+    }
+    
+    func test_빼기_string타입_식이_주어지면_파싱하여_결과가_나오는지_확인() {
+        let stringFormula = "153-78-15"
+        
+        var structFormula = ExpressionParser.parser(from: stringFormula)
+        let result = try! structFormula.result()
+       
+        XCTAssertEqual(result, 60)
+    }
+    
+    
+    
+    func test_곱하기_string타입_식이_주어지면_파싱하여_결과가_나오는지_확인() {
+        let stringFormula = "12×25×78×4"
+        
+        var structFormula = ExpressionParser.parser(from: stringFormula)
+        let result = try! structFormula.result()
+       
+        XCTAssertEqual(result, 93600)
+    }
+    
+    func test_나누기_string타입_식이_주어지면_파싱하여_결과가_나오는지_확인() {
+        let stringFormula = "78÷4÷3"
+        
+        var structFormula = ExpressionParser.parser(from: stringFormula)
+        let result = try! structFormula.result()
+       
+        XCTAssertEqual(result, 6.5)
+    }
+    
+    func test_혼합_string타입_식이_주어지면_파싱하여_결과가_나오는지_확인() {
         let stringFormula = "123+25-78÷4×5"
         
         var structFormula = ExpressionParser.parser(from: stringFormula)
