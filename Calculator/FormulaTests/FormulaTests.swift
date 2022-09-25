d68a0552360d370e814a8535f6cd34310156ce08
dylan-yoon <d.dylany21@gmail.com>
2022-09-26 03:18:40 +0900

    test: result 실행시 정상 작동 하는 케이스생성

--- Calculator/FormulaTests/FormulaTests.swift
+++ Calculator/FormulaTests/FormulaTests.swift
@@ -15,6 +15,8 @@ final class FormulaTests: XCTestCase {
     
     override func setUpWithError() throws {
         try super.setUpWithError()
+        operandsQueue = CalculatorItemQueue()
+        operatorsQueue = CalculatorItemQueue()
         sut = Formula(
             operands: operandsQueue,
             operators: operatorsQueue
@@ -24,5 +26,23 @@ final class FormulaTests: XCTestCase {
     override func tearDownWithError() throws {
         try super.tearDownWithError()
         sut = nil
+    }
+    
+    func test_입력후_result실행시_순서대로_정상작동이_되는지() {
+        // given
+        let givenResult = (13+50.6)*11.1/10
+        operandsQueue.enqueue(13)
+        operatorsQueue.enqueue(Operator.add)
+        operandsQueue.enqueue(50.6)
+        operatorsQueue.enqueue(Operator.multiply)
+        operandsQueue.enqueue(11.1)
+        operatorsQueue.enqueue(Operator.divide)
+        operatorsQueue.enqueue(10)
+        
+        // when
+        let result = sut.result()
+        
+        // then
+        XCTAssertEqual(result, givenResult)
     }
 }
