5b26c4b23ec5a1378671498ee247d8927ad21ebb
미니 <cow970814@naver.com>
2022-09-25 16:48:46 +0900

    test : ExpressionParser 테스트 엣지 케이스 추가 및 테스트 수행

--- Calculator/CalculatorItemQueueTests/ExpressionParserTests.swift
+++ Calculator/CalculatorItemQueueTests/ExpressionParserTests.swift
@@ -9,14 +9,37 @@ import XCTest
 @testable import Calculator
 
 final class ExpressionParserTests: XCTestCase {
-    func test_parse를후결과가정상적으로도출되는지() {
+    func test_연산자와피연산자의문자가모드정상적인경우결과가예측치와동일한지() {
         // driven
-        let formula = ExpressionParser.parse(from: "1+2")
-        
+        let input: String = "10+9*8-7/6"
         // when
-        let calResult = formula.result()
+        let form: Formula = ExpressionParser.parse(from: input)
+        let result = form.result()
         
         // then
-        XCTAssertEqual(calResult, 3)
+        XCTAssertEqual(result, 24.166666666666667)
+    }
+    
+    func test_연산자가될수없는연산자가포함되어있는경우결과가예측치와동일한지() {
+        // driven
+        let input: String = "10+9*8-7^6"
+        
+        // when
+        let form: Formula = ExpressionParser.parse(from: input)
+        let result = form.result()
+        
+        // then
+        XCTAssertEqual(result, 76)
+    }
+    
+    func test_피연산자가될수없는문자열이포함되어있는경우결과가예측치와동일한지() {
+        // driven
+        let input: String = "10d+9*8-76"
+        // when
+        let form: Formula = ExpressionParser.parse(from: input)
+        let result = form.result()
+        
+        // then
+        XCTAssertEqual(result, 76)
     }
 }
