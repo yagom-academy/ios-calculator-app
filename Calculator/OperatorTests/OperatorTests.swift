ee0001352ea6bc0d2fcf1ae227f045d5ab940918
lxodud <xodud1998223@naver.com>
2022-09-26 00:19:02 +0900

    feat: OperatorTests 테스트 클래스 생성

---
+++ Calculator/OperatorTests/OperatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  OperatorTests.swift
+//  OperatorTests
+//
+//  Created by Hamo on 2022/09/26.
+//
+
+import XCTest
+@testable import Calculator
+class OperatorTests: XCTestCase {
+    var sut: Operator!
+
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+    }
+    
+}
