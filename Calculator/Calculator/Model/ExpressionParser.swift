3afa53449a5c16337f719d071b7e2eb05975ca95
lxodud <xodud1998223@naver.com>
2022-09-26 23:23:32 +0900

    feat: ExpressionParser 열거형 정의

---
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -0,0 +1,15 @@
+//
+//  ExpressionParser.swift
+//  Calculator
+//
+//  Created by Hamo on 2022/09/26.
+//
+
+enum ExpressionParser {
+    static func parse(from input: String) -> Formula {
+        let arithmeticOperator = CalculatorItemQueue<Operator>()
+        let operands = CalculatorItemQueue<Double>()
+        
+        return Formula(operands: operands, operators: arithmeticOperator)
+    }
+}
