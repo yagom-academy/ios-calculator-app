1b952e209e78fe77c5b878a169baa38e16545b01
ash-youu <ash.you.dev@gmail.com>
2022-09-27 00:01:20 +0900

    feat: ExpressionParser 타입 내 componentsByOperators 메서드 구현

--- Calculator/Calculator/Model/ExtensionParser.swift
+++ Calculator/Calculator/Model/ExtensionParser.swift
@@ -3,12 +3,15 @@
 //  Calculator
 //
 
+import Foundation
+
 enum ExpressionParser {
     static func parse(from input: String) -> Formula {
         fatalError()
     }
     
     static private func componentsByOperators(from input: String) -> [String] {
-        fatalError()
+        let stringOperators = Operator.allCases.map{ String($0.rawValue) }.joined()
+        return input.components(separatedBy: CharacterSet(charactersIn: stringOperators))
     }
 }
