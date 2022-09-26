3f91224846dea3c5b8c5d0f427bb3b1b2a03e283
lxodud <xodud1998223@naver.com>
2022-09-26 13:12:14 +0900

    test: testFormula_result를호출하면_operands값두개와operators값한개가입력되었을때_합한결과가반환되어야한다() 테스트 메서드 구현

--- Calculator/FormulaTests/FormulaTests.swift
+++ Calculator/FormulaTests/FormulaTests.swift
@@ -10,8 +10,24 @@ import XCTest
 
 class FormulaTests: XCTestCase {
     var sut: Formula!
+    var operands: CalculatorItemQueue<Double>!
+    var operators: CalculatorItemQueue<Operator>!
 
     override func setUpWithError() throws {
-        
+        try super.setUpWithError()
+        operands = CalculatorItemQueue<Double>()
+        operators = CalculatorItemQueue<Operator>()
+        sut = Formula(operands: operands, operators: operators)
+    }
+    
+    func testFormula_result를호출하면_operands값두개와operators값한개가입력되었을때_합한결과가반환되어야한다() {
+        // given
+        operands.enqueue(1.0)
+        operands.enqueue(2.0)
+        operators.enqueue(.add)
+        // when
+        let result = sut.result()
+        // then
+        XCTAssertEqual(result, 3.0)
     }
 }
