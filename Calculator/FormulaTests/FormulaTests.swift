a0e7e8319e70c07ee92455778af8d3a54f01e25f
lxodud <xodud1998223@naver.com>
2022-09-26 11:18:29 +0900

    feat: FormulaTests 클래스 정의

---
+++ Calculator/FormulaTests/FormulaTests.swift
@@ -0,0 +1,17 @@
+//
+//  FormulaTests.swift
+//  FormulaTests
+//
+//  Created by Hamo on 2022/09/26.
+//
+
+import XCTest
+@testable import Calculator
+
+class FormulaTests: XCTestCase {
+    var sut: Formula!
+
+    override func setUpWithError() throws {
+        
+    }
+}
