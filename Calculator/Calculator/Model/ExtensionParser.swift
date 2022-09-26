1ae6ae8a81231dc4bf4ad3e3c7f055cc6c34cf0b
ash-youu <ash.you.dev@gmail.com>
2022-09-27 00:04:23 +0900

    feat: ExpressionParser 타입 내 parse 메서드 구현

--- Calculator/Calculator/Model/ExtensionParser.swift
+++ Calculator/Calculator/Model/ExtensionParser.swift
@@ -7,7 +7,22 @@ import Foundation
 
 enum ExpressionParser {
     static func parse(from input: String) -> Formula {
-        fatalError()
+        var operands = CalculatorItemQueue<Double>()
+        var operators = CalculatorItemQueue<String>()
+        
+        componentsByOperators(from: input).forEach {
+            if let doubleOperand = Double($0) {
+                operands.enqueue(doubleOperand)
+            }
+        }
+        
+        input.forEach { character in
+            if Operator.allCases.map({ $0.rawValue }).contains(character) {
+                operators.enqueue(String(character))
+            }
+        }
+        
+        return Formula(operands: operands, operators: operators)
     }
     
     static private func componentsByOperators(from input: String) -> [String] {
