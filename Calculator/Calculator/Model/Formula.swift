6ca771b68af066e4fe87fc6f9828904d040cef3a
zhilly11 <zhzh1x4@gmail.com>
2022-09-26 20:25:55 +0900

    feat: Formula, FormulaTests 파일 생성

---
+++ Calculator/Calculator/Model/Formula.swift
@@ -0,0 +1,6 @@
+//  Formula.swift
+//  Created by zhilly on 2022/09/26.
+
+struct Formula {
+    
+}
---
+++ Calculator/CalculatorTests/FormulaTests.swift
@@ -0,0 +1,14 @@
+//  FormulaTests.swift
+//  Created by zhilly on 2022/09/26.
+
+import XCTest
+@testable import Calculator
+
+final class FormulaTests: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+}
