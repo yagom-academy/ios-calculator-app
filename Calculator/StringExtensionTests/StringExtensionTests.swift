c2a5a4b360557950a0cb7abc92f308d1962868aa
lxodud <xodud1998223@naver.com>
2022-09-27 00:54:00 +0900

    test: StringExtensionTests 테스트 메서드 네이밍 수정

--- Calculator/StringExtensionTests/StringExtensionTests.swift
+++ Calculator/StringExtensionTests/StringExtensionTests.swift
@@ -9,7 +9,7 @@ import XCTest
 @testable import Calculator
 
 class StringExtensionTests: XCTestCase {
-    func testStringExtension_문자하나를입력받고_해당문자를기준으로쪼갠문자열을요소로가진배열을반환해야한다() {
+    func testSplit_문자하나를입력받고_해당문자를기준으로쪼갠문자열을요소로가진배열을반환해야한다() {
         // given
         let input = "1+2+3"
         // when
