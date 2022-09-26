5924c438291a4cdc4dcabf6e3d6508a26156a42b
DragonYG <kioswe92@gmail.com>
2022-09-27 01:26:49 +0900

    style: Operator 코드컨벤션 수정

--- Calculator/Calculator/Model/Enum/Operator.swift
+++ Calculator/Calculator/Model/Enum/Operator.swift
@@ -25,18 +25,18 @@ enum Operator: Character, CaseIterable, CalculateItem {
     }
     
     private func add(lhs: Double, rhs: Double) -> Double {
-        return (lhs+rhs)
+        return (lhs + rhs)
     }
     
     private func subtract(lhs: Double, rhs: Double) -> Double {
-        return (lhs-rhs)
+        return (lhs - rhs)
     }
     
     private func divide(lhs: Double, rhs: Double) -> Double {
-        return (lhs/rhs)
+        return (lhs / rhs)
     }
     
     private func multiply(lhs: Double, rhs: Double) -> Double {
-       return (lhs*rhs)
+       return (lhs * rhs)
     }
 }
