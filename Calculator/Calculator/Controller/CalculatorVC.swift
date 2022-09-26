92118e788fd1797bfa4c1c80ca487ef59498aa4d
Jae Hyung Kim <smfc.mene@gmail.com>
2022-09-26 11:59:26 +0800

    feat: 뷰컨트롤러 이름 CalculatorVC로 변경하고 UI초기화 하는 resetAllUI 함수 구현

---
+++ Calculator/Calculator/Controller/CalculatorVC.swift
@@ -0,0 +1,30 @@
+//
+//  Calculator - ViewController.swift
+//  Created by yagom. 
+//  Copyright © yagom. All rights reserved.
+// 
+
+import UIKit
+
+class CalculatorVC: UIViewController {
+
+    
+    @IBOutlet private weak var calculationFormulaScroll: UIScrollView!
+    @IBOutlet private weak var operatorLabel: UILabel!
+    @IBOutlet private weak var operandLabel: UILabel!
+    @IBOutlet private weak var mainStackView: UIStackView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        resetAllUI()
+    }
+    
+    private func resetAllUI() {
+        mainStackView.superview?.removeFromSuperview()
+        operatorLabel.text = ""
+        operandLabel.text = ""
+    }
+
+
+}
+
