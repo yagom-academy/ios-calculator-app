9f0290c2911c0bf05dadb2aea7cd345de7f5561d
zhilly11 <zhzh1x4@gmail.com>
2022-09-26 18:31:42 +0900

    feat: ExtensionString, StringSplitTests 파일 생성

---
+++ Calculator/Calculator/Extension/ExtensionString.swift
@@ -0,0 +1,8 @@
+//  ExtensionString.swift
+//  Created by zhilly on 2022/09/26.
+
+extension String {
+    func split(with target: Character) -> [String] {
+        return []
+    }
+}
---
+++ Calculator/CalculatorTests/StringSplitTests.swift
@@ -0,0 +1,13 @@
+//  StringSplitTests.swift
+//  Created by zhilly on 2022/09/26.
+
+import XCTest
+
+final class StringSplitTests: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+}
