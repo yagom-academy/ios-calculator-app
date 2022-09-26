e77e2eb99f69823be219760fa8ecb761edd95e15
Gundy <korea1880@naver.com>
2022-09-27 07:31:59 +0900

    feat: '=' 버튼을 누르면 수식 스택뷰를 추가하는 touchUpEqualButton 메서드 추가 및 내부 호출 메서드 작성
    - 연산자 레이블을 반환하는 makeOperatorLabel 메서드 추가
    - 숫자 레이블을 반환하는 makeOperandLabel 메서드 추가
    - 연산자 레이블과 숫자 레이블로 스택뷰를 만들어 반환하는 makeFormulaStackView 메서드 추가
    - 매직넘버를 표현할 열거형 CalculatorNumber 추가

---
+++ Calculator/Calculator/CalculatorNumber.swift
@@ -0,0 +1,10 @@
+//
+//  CalculatorNumber.swift
+//  Calculator
+//
+//  Created by Gundy on 2022/09/27.
+//
+
+enum CalculatorNumber {
+    static let formulaStackViewSpacing: Int = 8
+}
--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -10,12 +10,13 @@ class ViewController: UIViewController {
     
     var formula: Formula?
     
-    @IBOutlet weak private var operatorLabel: UILabel!
+    @IBOutlet weak private var enteredOperatorLabel: UILabel!
     @IBOutlet weak private var enteredNumberLabel: CalculatorNumberLabel!
-    @IBOutlet private var operators: [UIButton]!
+    @IBOutlet private var operatorButtons: [UIButton]!
     @IBOutlet weak private var zeroButton: UIButton!
     @IBOutlet weak private var doubleZeroButton: UIButton!
     @IBOutlet weak private var dotButton: UIButton!
+    @IBOutlet weak var verticalFormulasStackView: UIStackView!
     
     
     override func viewDidLoad() {
@@ -44,24 +45,65 @@ class ViewController: UIViewController {
                     entered: number)
     }
     
-    private func enterNumber(sender: UIButton, current text: String, entered number: String) {
+    private func enterNumber(sender: UIButton, current text: String, entered operand: String) {
         if enteredNumberLabel.isZero {
             switch sender {
             case zeroButton, doubleZeroButton:
                 return
             case dotButton:
-                enteredNumberLabel.text = text + number
+                enteredNumberLabel.text = text + operand
             default:
-                enteredNumberLabel.text = number
+                enteredNumberLabel.text = operand
             }
         } else {
             if sender == dotButton,
                text.contains(CalculatorText.dot) {
                 return
             } else {
-                enteredNumberLabel.text = text + number
+                enteredNumberLabel.text = text + operand
             }
         }
+    }
+    
+    @IBAction private func touchUpEqualButton(sender: UIButton) {
+        guard enteredNumberLabel.isZero == false else {
+            return
+        }
+        makeFormulaStackView()
+    }
+    
+    private func makeFormulaStackView() {
+        guard let operatorLabel = makeOperatorLabel(),
+              let operandLabel = makeOperandLabel() else {
+                  return
+              }
+        enteredOperatorLabel.text = nil
+        enteredNumberLabel.resetToZero()
+        let formulaStackView: UIStackView = .init(arrangedSubviews: [operatorLabel, operandLabel])
+        formulaStackView.spacing = CGFloat(CalculatorNumber.formulaStackViewSpacing)
+        verticalFormulasStackView.addArrangedSubview(formulaStackView)
+    }
+    
+    private func makeOperatorLabel() -> UILabel? {
+        guard let text = enteredOperatorLabel.text else {
+            return nil
+        }
+        let operatorLabel: UILabel = .init()
+        operatorLabel.text = text
+        operatorLabel.textColor = .white
+        operatorLabel.font = .preferredFont(forTextStyle: .title3)
+        return operatorLabel
+    }
+    
+    private func makeOperandLabel() -> UILabel? {
+        guard let number = enteredNumberLabel.text else {
+            return nil
+        }
+        let operandLabel: UILabel = .init()
+        operandLabel.text = number
+        operandLabel.textColor = .white
+        operandLabel.font = .preferredFont(forTextStyle: .title3)
+        return operandLabel
     }
 }
 
