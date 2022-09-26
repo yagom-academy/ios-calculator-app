efe8b40d7da753eb6394f33e2ed7346734554abb
ash-youu <ash.you.dev@gmail.com>
2022-09-26 23:55:22 +0900

    feat: String을 Character 기준으로 구분 후 String 배열을 반환하는 split 메서드 구현

--- Calculator/Calculator/Model/Extension.swift
+++ Calculator/Calculator/Model/Extension.swift
@@ -7,6 +7,10 @@ extension Double: CalculateItem {}
 
 extension String {
     func split(with target: Character) -> [String] {
-        fatalError()
+        let subStrings = self.split(separator: target)
+        var strings: [String] = []
+        
+        subStrings.forEach { strings.append(String($0)) }
+        return strings
     }
 }
