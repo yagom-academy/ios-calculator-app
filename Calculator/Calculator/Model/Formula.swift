ca6726f6fa8ec91770fa844759d24c84a4abfbc9
woong <workplayhard1@naver.com>
2022-09-27 18:34:56 +0900

    feat: 계산하고 값을 Double로 반환해주는 result 함수구현 하지만, 값의 체크를 위해 추가적으로 테스트코드를 작성해봐야함

--- Calculator/Calculator/Model/Formula.swift
+++ Calculator/Calculator/Model/Formula.swift
@@ -11,7 +11,20 @@ struct Formula {
     var operands: CalculatorItemQueue<Double> = CalculatorItemQueue<Double>.init()
     var operators: CalculatorItemQueue<Operator> = CalculatorItemQueue<Operator>.init()
     
-    func result() -> Double {
-        return 0
+    mutating func result() -> Double {
+        var result: Double = 0.0
+        let lhs = operands.dequeue()
+        let rhs = operands.dequeue()
+        let opt = operators.dequeue()
+        
+        guard let checkLhs = lhs else { return 9999.9999 }
+        guard let checkRhs = rhs else { return 9999.9999 }
+        guard let checkOpt = opt else { return 9999.9999 }
+        
+        while !operands.isEmpty && !operators.isEmpty {
+            result = checkOpt.calculate(lhs: checkLhs, rhs: checkRhs)
+        }
+        return result
     }
 }
+
