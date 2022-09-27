85e051da5d0281cab5697ae82ef91982d4df0973
dylan-yoon <d.dylany21@gmail.com>
2022-09-27 13:18:14 +0900

    test: 입력에 따른 결과값이 같은지에대한 테스트함수 생성

--- Calculator/ExpressionParserTests/ExpressionParserTests.swift
+++ Calculator/ExpressionParserTests/ExpressionParserTests.swift
@@ -19,4 +19,29 @@ final class ExpressionParserTests: XCTestCase {
         try super.tearDownWithError()
         sut = nil
     }
+    
+    func test_parse실행시_정상적인값을_입력했을_때_입력개수와_result_값이_같은지() {
+        // given
+        let given = "132+14.5/112.3*13"
+        let expression2Result = (132.0+14.5)/10.0*13.0
+        var operands: CalculatorItemQueue!
+        var operators: CalculatorItemQueue!
+        
+        operands.enqueue(132.0)
+        operands.enqueue(14.5)
+        operands.enqueue(10.0)
+        operands.enqueue(13.0)
+        
+        operators.enqueue(Operator.add)
+        operators.enqueue(Operator.divide)
+        operators.enqueue(Operator.multiply)
+        
+        // when
+        var result = ExpressionParser.parse(from: given)
+        
+        // then
+        XCTAssertEqual(result.operators.count, operands.count)
+        XCTAssertEqual(result.operators.count, operators.count)
+        XCTAssertEqual(try result.result(), expression2Result)
+    }
 }
