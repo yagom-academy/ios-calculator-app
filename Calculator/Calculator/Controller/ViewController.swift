322d148c61bde272c058edcac5493d9dc10a21b3
lws2269 <lws2269@kakao.com>
2022-09-27 16:36:55 +0900

    feat: stackview 하위 속성 계산속성 추가, 접근제어자 추가 및 addScrollSubView function 추가

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -13,11 +13,40 @@ class ViewController: UIViewController {
     @IBOutlet weak var formulaScrollView: UIScrollView!
     @IBOutlet weak var enterdFormulaStackView: UIStackView!
     
-    var formula: Formula?
-    var inputNumber: String?
-    var inputOperator: String?
-    var isCalculated: Bool?
-     
+    private var formula: Formula?
+    private var inputNumber: String?
+    private var inputOperator: String?
+    private var isCalculated: Bool?
+   
+
+    private var formulaStackView: UIStackView {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.alignment = .fill
+        stackView.distribution = .fill
+        stackView.spacing = 8
+        return stackView
+    }
+    
+    private var operandLabel: UILabel {
+        let label = UILabel()
+        label.font = UIFont.preferredFont(forTextStyle: .title3)
+//        label.text = inputNumberLabel.text
+        label.text = "123"
+        label.textAlignment = .right
+        
+        return label
+    }
+    
+    private var operatorLabel: UILabel {
+        let label = UILabel()
+        label.font = UIFont.preferredFont(forTextStyle: .title3)
+        label.text = "+"
+//        label.text = inputOperatorLabel.text
+        label.textAlignment = .right
+        return label
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
@@ -47,6 +76,18 @@ class ViewController: UIViewController {
     func resetInputOperator() {
         inputOperator = ""
         inputOperatorLabel.text = inputOperator
+    }
+    
+    func addScrollSubView(){
+        print("asd")
+        formulaStackView.addSubview(operandLabel)
+        formulaStackView.addSubview(operatorLabel)
+        print(formulaStackView.subviews)
+        print(operandLabel)
+        print(operatorLabel)
+        
+        
+        formulaScrollView.addSubview(formulaStackView)
     }
     
     @IBAction func signChangeButtonTapped(_ sender: UIButton) {
@@ -95,18 +136,23 @@ class ViewController: UIViewController {
     
     @IBAction func operatorsInputButtonTapped(_ sender: UIButton) {
         guard let value = sender.currentTitle else { return }
-        
-        // 연산 후에 한번 더 "="를 누르면 아무 반응 없도록 = 플래그 변수?
-        if value == "=" {
-            // inputNumber 가 0이고,
-            // 연산 과정
-            
-            return
-        }
-        resetInputNumber()
-        
-        inputOperator = value
-        inputOperatorLabel.text = inputOperator
+        addScrollSubView()
+//
+//        // 연산 후에 한번 더 "="를 누르면 아무 반응 없도록 = 플래그 변수?
+//        // 분리?
+//        if value == "=" {
+//            // inputNumber 가 0이고,
+//            // 연산 과정
+//
+//            return
+//        }
+//        // 연산 버튼이 눌릴 때 마다,, 스택 뷰 만들어서,, 레이블 두개 넣고,, 오토레이아웃 잡아주고 ,, 헝헝,,,헝헝,,,
+//
+//
+//        resetInputNumber()
+//
+//        inputOperator = value
+//        inputOperatorLabel.text = inputOperator
     }
     
 }
