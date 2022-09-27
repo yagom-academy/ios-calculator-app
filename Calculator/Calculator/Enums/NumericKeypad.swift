c9f6432ed1308b655a92fa724e3bf6f37b45d6e7
yuvinrho <yvrho@naver.com>
2022-09-27 17:53:20 +0900

    feat: NumericKeypad 정의

---
+++ Calculator/Calculator/Enums/NumericKeypad.swift
@@ -0,0 +1,31 @@
+//
+//  NumericKeypad.swift
+//  Calculator
+//
+//  Created by 노유빈 on 2022/09/27.
+//
+
+enum NumericKeypad: String {
+    case num1 = "1"
+    case num2 = "2"
+    case num3 = "3"
+    case num4 = "4"
+    case num5 = "5"
+    case num6 = "6"
+    case num7 = "7"
+    case num8 = "8"
+    case num9 = "9"
+    case num0 = "0"
+    case num00 = "00"
+    case decimalPoint = "."
+    
+    case plus = "+"
+    case minus = "-"
+    case division = "÷"
+    case multiplication = "×"
+    
+    case equal = "="
+    case ac = "AC"
+    case ce = "CE"
+    case plusMinus = "⁺⁄₋"
+}
