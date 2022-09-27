20d99dfd32150ebcea436e858dd34df94dea540e
HyosungP <hyosing92@gmail.com>
2022-09-28 01:31:59 +0900

    feat: 메서드 정의 및 숫자패드 터치 시 처리 메서드 구현
    - 숫자패드 클릭 시 처리 내용 구현

--- Calculator/Calculator/Controllers/ViewController.swift
+++ Calculator/Calculator/Controllers/ViewController.swift
@@ -7,12 +7,69 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var mainOperatorLabel: UILabel!
+    @IBOutlet weak var mainResultLabel: UILabel!
+    
+    @IBOutlet weak var historyScrollView: UIScrollView!
+    @IBOutlet weak var historyStackView: UIStackView!
+    
+    var currentOperand: String = Constant.defaultZero
+    var currentOperator: String = Constant.empty
+    var calculateHistory: [String] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUILabel()
+    }
+    
+    func setUILabel() {
+        mainOperatorLabel.text = Constant.empty
+        mainResultLabel.text = Constant.zero
+    }
+    
+    func setDefaultOperand() {
+        mainResultLabel.text = Constant.zero
+        currentOperand = Constant.defaultZero
     }
 
+    @IBAction func tappedNumberPads(_ sender: UIButton) {
+        let tappedNumber = sender.currentTitle
+        guard let number = tappedNumber else { return }
+        
+        if currentOperand == Constant.defaultZero || currentOperand == Constant.zero {
+            if number == Constant.doubleZero {
+                return
+            }
+            currentOperand = number
+        } else {
+            currentOperand += number
+        }
+    }
+    
+    @IBAction func tappedOperatorPads(_ sender: UIButton) {
 
+    }
+    
+    @IBAction func tappedCalculate(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func tappedDotPads(_ sender: UIButton) {
+
+    }
+    
+    @IBAction func tappedChangePositiveNegative(_ sender: UIButton) {
+
+    }
+    
+    @IBAction func tappedAllClear(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func tappedClearEntry(_ sender: UIButton) {
+
+    }
 }
 
