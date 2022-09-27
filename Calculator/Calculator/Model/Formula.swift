f642809d8ee383611e273611616a0856713e480e
jin <pearlisrich@naver.com>
2022-09-27 15:12:10 +0900

    chore: result()에서 에러를 던지도록 수정

--- Calculator/Calculator/Model/Formula.swift
+++ Calculator/Calculator/Model/Formula.swift
@@ -9,14 +9,14 @@ struct Formula {
     var operands: CalculatorItemQueue<Double>
     var operators: CalculatorItemQueue<Operator>
     
-    func result() -> Double {
+    func result() throws -> Double {
         
         guard let initialValue = operands.dequeue() else { return 0 }
         var result = initialValue
         
         while !operands.isEmpty() {
             guard let newValue = operands.dequeue() else { return 0 }
-            result = operators.dequeue()?.calculate(lhs: result, rhs: newValue) ?? 0
+            result = try operators.dequeue()?.calculate(lhs: result, rhs: newValue) ?? 0
         }
         
         return result
