e0248837c7b4b8eb46eaefbf3de3cfb027720c01
ash-youu <ash.you.dev@gmail.com>
2022-09-27 13:36:17 +0900

    feat: Formula 타입 내 result 메서드 구현

--- Calculator/Calculator/Model/Formula.swift
+++ Calculator/Calculator/Model/Formula.swift
@@ -8,6 +8,36 @@ struct Formula {
     var operators: CalculatorItemQueue<String>
     
     func result() -> Double {
-        fatalError()
+        var result: Double = operands.list.head?.value ?? 0
+        var currentOperand = operands.list.head
+        var currentOperator = operators.list.head
+        var count: Int = 0
+        
+        while count != operators.count {
+            guard let nextOperand = currentOperand?.next,
+                  let calculatedOperator = currentOperator else {
+                return result
+            }
+            
+            count += 1
+            
+            switch Character(calculatedOperator.value) {
+            case Operator.add.rawValue:
+                result = Operator.add.calculate(lhs: result, rhs: nextOperand.value)
+            case Operator.subtract.rawValue:
+                result = Operator.subtract.calculate(lhs: result, rhs: nextOperand.value)
+            case Operator.divide.rawValue:
+                result = Operator.divide.calculate(lhs: result, rhs: nextOperand.value)
+            case Operator.multiply.rawValue:
+                result = Operator.multiply.calculate(lhs: result, rhs: nextOperand.value)
+            default:
+                print("잘못된 연산자")
+            }
+            
+            currentOperand = currentOperand?.next
+            currentOperator = currentOperator?.next
+        }
+        
+        return result
     }
 }
