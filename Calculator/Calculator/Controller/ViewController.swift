35d03bcda92faadcc147fc3c393b2dab2d99e693
lws2269 <lws2269@kakao.com>
2022-09-27 11:41:10 +0900

    feat: 키패드 입력 구현

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -8,10 +8,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var inputNumberLabel: UILabel!
+    @IBOutlet weak var inputOperatorLabel: UILabel!
+    
+    var inputNumber: String?
+    var inputOperator: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setup()
+    }
+    
+    func setup() {
+        inputNumberLabel.text = "0"
+        inputOperatorLabel.text = ""
+        inputNumber = "0"
+        inputOperator = ""
     }
 
+    @IBAction func operandsInputButtonTapped(_ sender: UIButton) {
+        guard let value = sender.currentTitle else { return }
+        // 가독성 너무 문제인데..
+        if inputNumber == "0", value == "0" {
+            return
+        } else if inputNumber == "0", value == "00" {
+            return
+        } else if inputNumber == "0", value == "." {
+            inputNumber?.append(value)
+        } else if inputNumber == "0" {
+            inputNumber = value
+        } else if inputNumber?.contains(".") == true, value == "."{
+            return
+        } else {
+            inputNumber?.append(value)
+        }
+        
+        inputNumberLabel.text = inputNumber
+        
+    }
+    
+    @IBAction func operatorsInputButtonTapped(_ sender: UIButton) {
+        inputOperatorLabel.text = sender.currentTitle
+    }
+    
 }
 
