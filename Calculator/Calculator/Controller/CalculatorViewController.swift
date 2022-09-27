5b5a8f5bdd034dfd8fc7df69c78d07260bb39b5a
미니 <cow970814@naver.com>
2022-09-27 22:28:53 +0900

    feat : 초기설정 메서드 추가

--- Calculator/Calculator/Controller/CalculatorViewController.swift
+++ Calculator/Calculator/Controller/CalculatorViewController.swift
@@ -8,63 +8,22 @@ import UIKit
 class CalculatorViewController: UIViewController {
     @IBOutlet weak var currentOperatorLabel: UILabel!
     @IBOutlet weak var currentNumberLabel: UILabel!
-    @IBOutlet weak var calculateScrollView: UIScrollView!
+    @IBOutlet weak var recordedCalculateStackView: UIStackView!
     
-    var prevNumber: String = ""
-    var operatorValue: String = ""
-    var nextNumber: String = ""
+    var userInputExpression: String = ""
+    var selectedNumbers: String = ""
+    var selectedOperator: String = ""
+    
+    var isNotEmptyExpression: Bool {
+        return !userInputExpression.isEmpty
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        calculateScrollView.delegate = self
-        
-        changeNumberLabel(value: "0")
+        currentOperatorLabel.text = ""
+        currentNumberLabel.text = "0"
     }
     
-    @IBAction func didTappedNumberButton(_ sender: UIButton) {
-        let input = sender.titleLabel?.text
-        guard let input else {
-            return
-        }
-        setUserInputNumber(value: input)
-        
-    }
-    
-    @IBAction func didTappedOperator(_ sender: UIButton) {
-        let input = sender.titleLabel?.text
-        
-        guard let input else {
-            return
-        }
-        
-        operatorValue = input
-        changeOperatorLabel(value: input)
-        
-        let label = UILabel()
-        label.text = input
-        calculateScrollView.addSubview(label)
-    }
-    
-    private func setUserInputNumber(value: String) {
-        if operatorValue.isEmpty {
-            prevNumber.append(value)
-            changeNumberLabel(value: prevNumber)
-        } else {
-            nextNumber.append(value)
-            changeNumberLabel(value: nextNumber)
-        }
-    }
-    
-    private func changeNumberLabel(value: String) {
-        currentNumberLabel.text = value.calNumber
-    }
-    
-    private func changeOperatorLabel(value: String) {
-        currentOperatorLabel.text = value
-    }
-}
-
-extension CalculatorViewController: UIScrollViewDelegate {
     
 }
