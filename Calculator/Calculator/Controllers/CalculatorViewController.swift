eee65da3b7cceb75750ba98f5375ab0bff9f3a59
yuvinrho <yvrho@naver.com>
2022-09-27 18:28:31 +0900

    feat: operatorLabel, operandLabel IBOutlet연결

--- Calculator/Calculator/Controllers/CalculatorViewController.swift
+++ Calculator/Calculator/Controllers/CalculatorViewController.swift
@@ -1,7 +1,9 @@
 import UIKit
 
 class CalculatorViewController: UIViewController {
-
+    @IBOutlet private weak var operatorLabel: UILabel!
+    @IBOutlet private weak var operandLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
