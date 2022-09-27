3b4e79aaf47bde84119d70f7435eeeba358b73b3
lws2269 <lws2269@kakao.com>
2022-09-27 12:26:20 +0900

    feat: ViewController 함수 구현

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -11,12 +11,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var inputNumberLabel: UILabel!
     @IBOutlet weak var inputOperatorLabel: UILabel!
     
+    var formula: Formula?
+    
     var inputNumber: String?
     var inputOperator: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
+        setupFomula()
     }
     
     func setup() {
@@ -25,7 +28,27 @@ class ViewController: UIViewController {
         inputNumber = "0"
         inputOperator = ""
     }
-
+    
+    func setupFomula() {
+        formula = Formula()
+    }
+    
+    @IBAction func signChangeButtonTapped(_ sender: UIButton) {
+        // +/- 버튼 클릭시
+        // - 부호 일 경우 삭제
+    }
+    
+    @IBAction func allClearButtonTapped(_ sender: UIButton) {
+        // inputNumber,inputNumberLabel
+        // inputOperator, inputOperatorLabel
+        // 스택뷰,,~~ 삭제,,
+    }
+    
+    @IBAction func clearEntryButtonTapped(_ sender: UIButton) {
+        // inputNumber,inputNumberLabel
+        // inputOperator, inputOperatorLabel
+    }
+    
     @IBAction func operandsInputButtonTapped(_ sender: UIButton) {
         guard let value = sender.currentTitle else { return }
         // 가독성 너무 문제인데..
@@ -48,7 +71,24 @@ class ViewController: UIViewController {
     }
     
     @IBAction func operatorsInputButtonTapped(_ sender: UIButton) {
-        inputOperatorLabel.text = sender.currentTitle
+        guard let value = sender.currentTitle else { return }
+        
+        // 연산 후에 한번 더 "="를 누르면 아무 반응 없도록 = 플래그 변수?
+        if value == "=" {
+            // inputNumber 가 0이고,
+            // 연산 과정
+            
+            return
+        }
+        // inputNumber의 값이 0이면 연산자만 교체
+        // 연산이 이루어진 후 = inputNumber 값이 0이 아니라면
+        // inputNumber, inputNumberLabel 초기화
+        // inputOperator, inputOperatorLabel 초기화
+        inputNumber = "0"
+        inputNumberLabel.text = inputNumber
+        
+        inputOperator = value
+        inputOperatorLabel.text = inputOperator
     }
     
 }
