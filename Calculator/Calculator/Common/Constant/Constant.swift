bafc92696b2cc15db8f5c275dc8518c9115753cc
HyosungP <hyosing92@gmail.com>
2022-09-27 23:59:00 +0900

    feat: Constant 구조체 생성 및 내부에 String Literal 값 기재

---
+++ Calculator/Calculator/Common/Constant/Constant.swift
@@ -0,0 +1,17 @@
+//
+//  Constant.swift
+//  Calculator
+//
+//  Created by Kyo on 2022/09/27.
+//
+
+struct Constant {
+    private init() { }
+    
+    static let zero = "0"
+    static let defaultZero = "D0"
+    static let doubleZero = "00"
+    static let negative = "-"
+    static let empty = ""
+    static let dot = "."
+}
