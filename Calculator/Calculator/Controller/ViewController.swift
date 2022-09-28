72495e37469523df6554ae596933a8a40461591d
wonbi92 <bin9239@gmail.com>
2022-09-28 17:46:01 +0900

    feat: 피연산자 버튼을 눌렀을 때 누른 값이 피연산자 라벨에 전달되는 로직 구현

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -6,7 +6,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var expression: String = ""
+    private var currentOperand: String = "0"
+    
     @IBOutlet weak var operandLabel: UILabel!
     @IBOutlet weak var operatorLabel: UILabel!
     
@@ -17,6 +18,19 @@ class ViewController: UIViewController {
     }
     
     @IBAction func tapOperandButton(_ sender: UIButton) {
+        guard let tappedOperand = sender.currentTitle else { return }
+        
+        switch tappedOperand {
+        case "⁺⁄₋":
+            handleSignButton()
+        case ".":
+            handleDotButton()
+        case "0", "00":
+            handleZeroButtons(from: tappedOperand)
+        default:
+            appendOperands(from: tappedOperand)
+        }
+        operandLabel.text = currentOperand
     }
     
     @IBAction func tapOperatorButton(_ sender: UIButton) {
@@ -32,3 +46,33 @@ class ViewController: UIViewController {
     }
 }
 
+// handling Operands
+extension ViewController {
+    private func handleSignButton() {
+        if currentOperand.first == "-" {
+            currentOperand.removeFirst()
+        } else {
+            currentOperand = "-" + currentOperand
+        }
+    }
+    
+    private func handleDotButton() {
+        if currentOperand.contains(".") { return }
+        currentOperand.append(".")
+    }
+    
+    private func handleZeroButtons(from operand: String) {
+        if Double(currentOperand) == .zero { return }
+        currentOperand.append(operand)
+    }
+    
+    private func appendOperands(from operand: String) {
+        if currentOperand == "0"{
+            currentOperand = operand
+        } else if currentOperand == "-0" {
+            currentOperand = "-" + operand
+        } else {
+            currentOperand.append(operand)
+        }
+    }
+}
