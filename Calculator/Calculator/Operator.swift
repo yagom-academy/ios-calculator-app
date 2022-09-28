5a1e837da566c7943fa3f443a97bc5e5dc2ab216
yjjem <marksim0606@gmail.com>
2022-09-28 16:39:41 +0900

    feat: add Operator functions - calculate, add, substract, divide, multiply

--- Calculator/Calculator/Operator.swift
+++ Calculator/Calculator/Operator.swift
@@ -12,4 +12,33 @@ enum Operator: Character, CaseIterable {
     case subtract = "-"
     case divide = "/"
     case multiply = "*"
+    
+    func calculate(lhs: Double, rhs: Double) -> Double {
+        switch self {
+        case .add:
+            return add(lhs: lhs, rhs: rhs)
+        case .subtract:
+            return add(lhs: lhs, rhs: rhs)
+        case .divide:
+            return add(lhs: lhs, rhs: rhs)
+        case .multiply:
+            return add(lhs: lhs, rhs: rhs)
+        }
+    }
+    
+    func add(lhs: Double, rhs: Double) -> Double {
+        return lhs + rhs
+    }
+    
+    func substract(lhs: Double, rhs: Double) -> Double {
+        return lhs - rhs
+    }
+    
+    func divide(lhs: Double, rhs: Double) -> Double {
+        return lhs / rhs
+    }
+    
+    func multiply(lhs: Double, rhs: Double) -> Double {
+        return lhs * rhs
+    }
 }
