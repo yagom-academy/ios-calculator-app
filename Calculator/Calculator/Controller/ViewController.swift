8790268da9d667054c5471c25bcb9016990b2b4f
wonbi92 <bin9239@gmail.com>
2022-09-28 15:48:47 +0900

    feat: 레이블 및 버튼의 아울렛과 액션 추가

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -6,10 +6,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var expression: String = ""
+    @IBOutlet weak var operandLabel: UILabel!
+    @IBOutlet weak var operatorLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        operandLabel.text = "0"
+        operatorLabel.text = ""
+    }
+    
+    @IBAction func tapOperandButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func tapOperatorButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func tapEqualsButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func tapCEButton(_ sender: UIButton) {
+    }
 
+    @IBAction func tapACButton(_ sender: UIButton) {
     }
 }
 
