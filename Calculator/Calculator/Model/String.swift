3ba02627bfe6d580b941cb8cf2cb0cba1b837be0
hashswim <ss991117@naver.com>
2022-09-28 21:18:01 +0900

    feat: String split 함수 구현

--- Calculator/Calculator/Model/String.swift
+++ Calculator/Calculator/Model/String.swift
@@ -2,9 +2,10 @@
 //  String.swift
 //  Calculator
 //
+import Foundation
 
 extension String: CalculateItem {
     func split(with target: Character) -> [String] {
-        
+        return self.components(separatedBy: String(target))
     }
 }
