3460fd6fb2d0e90fd5b5824a43e00bca325d9bcf
lnhk98 <hozzang666@gmai.com>
2022-09-28 16:50:34 +0900

    feat: resetCurrentEntry메서드 구현

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -20,9 +20,13 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        resetCurrentEntry()
     }
 
+    func resetCurrentEntry() {
+        currentEntryLabel.text = "0"
+    }
 
 }
 
