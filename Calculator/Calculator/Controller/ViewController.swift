964df7ce321559bbdbca54dbd47b54939256dc53
lnhk98 <hozzang666@gmai.com>
2022-09-28 15:25:39 +0900

    feat: 버튼과 레이블의 Outlet 변수 추가

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -7,7 +7,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var resultButton: UIButton!
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var subtractButton: UIButton!
+    @IBOutlet weak var multiplyButton: UIButton!
+    @IBOutlet weak var divideButton: UIButton!
+    @IBOutlet weak var allClearButton: UIButton!
+    @IBOutlet weak var clearEntryButton: UIButton!
+    @IBOutlet weak var signChangeButton: UIButton!
+    @IBOutlet weak var currentEntryLabel: UILabel!
+    @IBOutlet weak var operatorLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
