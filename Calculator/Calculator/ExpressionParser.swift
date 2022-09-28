8bcdc431d46bec30703da35dfb03fc4a6d5a965a
Mangdi <lkw4670@naver.com>
2022-09-28 15:57:48 +0900

    refactor: ExpressionParser의 parse메서드 연산자 넣는기능 추가 및 테스트

--- Calculator/Calculator/ExpressionParser.swift
+++ Calculator/Calculator/ExpressionParser.swift
@@ -8,20 +8,28 @@
 import Foundation
 
 enum ExpressionParser {
-    // 연산자배열 추가하기
     static func parse(from input: String) -> Formula {
         let numberArray = componentsByOperators(from: input).compactMap { Double($0) }
         let operands = CalculatorItemQueue<Double>()
-        var formula = Formula(operands: operands)
+        let operators = CalculatorItemQueue<Operator>()
+        var formula = Formula(operands: operands, operators: operators)
         
         for num in numberArray {
             formula.operands?.enqueue(num)
         }
         
+        let allOperators = Operator.allCases.map { $0.rawValue }
+        let operatorArray = input.filter { allOperators.contains($0) }.map { Operator.init(rawValue: $0) }
+        
+        for op in operatorArray {
+            if let op = op {
+                formula.operators?.enqueue(op)
+            }
+        }
+
         return formula
     }
     
-    //이곳 체크하기
     static private func componentsByOperators(from input: String) -> [String] {
         let operators = Operator.allCases.reduce("") { $0 + String($1.rawValue) }
         let separators = CharacterSet(charactersIn: operators)
