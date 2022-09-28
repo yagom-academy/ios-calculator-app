eebbd9beb17fda7ef7e1acc7610336e0ee691bbf
hashswim <ss991117@naver.com>
2022-09-28 20:57:28 +0900

    feat: Operator 열거형 내부구현

--- Calculator/Calculator/Model/Operator.swift
+++ Calculator/Calculator/Model/Operator.swift
@@ -10,20 +10,29 @@ enum Operator: Character, CaseIterable {
     case multiply = "*"
     
     private func add(lhs: Double, rhs: Double) -> Double {
-        
+        return lhs + rhs
     }
     private func substract(lhs: Double, rhs: Double) -> Double {
-        
+        return lhs - rhs
     }
     private func divide(lhs: Double, rhs: Double) -> Double {
-        
+        return lhs / rhs
     }
     private func muliply(lhs: Double, rhs: Double) -> Double {
-        
+        return lhs * rhs
     }
     
     func calculate(lhs: Double, rhs: Double) -> Double {
-        
+        switch self {
+        case .add:
+            return add(lhs: lhs, rhs: rhs)
+        case .substract:
+            return substract(lhs: lhs, rhs: rhs)
+        case .divide:
+            return divide(lhs: lhs, rhs: rhs)
+        case .multiply:
+            return muliply(lhs: lhs, rhs: rhs)
+        }
     }
 }
 
