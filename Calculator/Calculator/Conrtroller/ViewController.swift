7b964ea2fca918e525bbc4fb9ea18e50b9392c6c
sunnyCookie <107384230+sunny-maeng@users.noreply.github.com>
2022-09-28 10:41:08 +0900

    feat:  숫자버튼, 연산자버튼 touchUp시 라벨에 표시하는 touchUpNumberButton, touchUpOperatorButton 메서드 구현

--- Calculator/Calculator/Conrtroller/ViewController.swift
+++ Calculator/Calculator/Conrtroller/ViewController.swift
@@ -7,10 +7,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var operandLabel: UILabel!
+    @IBOutlet weak var operatorLabel: UILabel!
 
+    var operandItem: String = "" {
+        willSet {
+            guard newValue != "" else {
+                operandLabel.text = "0"
+                return
+            }
+
+            operandLabel.text = newValue
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func touchUpNumberButton(_ sender: UIButton) {
+        guard let number = sender.titleLabel?.text else { return }
+        
+        switch number {
+        case "0", "00":
+            guard operandItem != "0" else { return }
+        default:
+            if operandItem == "0" {
+                operandItem.removeFirst()
+            }
+            
+            operandItem += number
+        }
+    }
+    
+    @IBAction func touchUpOperatorButton(_ sender: UIButton) {
+        operatorLabel.text = sender.titleLabel?.text
     }
 }
 
