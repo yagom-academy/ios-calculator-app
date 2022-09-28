4af3e49131af03194d89872f596561b53374451e
dylan-yoon <d.dylany21@gmail.com>
2022-09-29 02:38:55 +0900

    refactor: ExpressionParser, parse및 componentsByOperator 기능 재 구현

--- Calculator/Calculator/Model/ExpressionParserEntity/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParserEntity/ExpressionParser.swift
@@ -9,40 +9,29 @@ enum ExpressionParser {
     static func parse(from input: String) throws -> Formula {
         var operands = CalculatorItemQueue()
         var operators = CalculatorItemQueue()
-        var beforeDoubleValue = ""
         
-        func enqueueOperand(and `operator`: Operator) throws {
-            guard let changingDouble = Double(beforeDoubleValue) else {
-                throw ExpressionParserError.canNotChangeDouble
-            }
-            operands.enqueue(changingDouble)
-            operators.enqueue(`operator`)
-            beforeDoubleValue = ""
-        }
-        try input.forEach { char in
-            switch char {
-            case "+" :
-                try enqueueOperand(and: .add)
-            case "-" :
-                try enqueueOperand(and: .subtract)
-            case "*" :
-                try enqueueOperand(and: .multiply)
-            case "/" :
-                try enqueueOperand(and: .divide)
+        try componentsByOperator(from: input).forEach {
+            switch $0 {
+            case "+":
+                operators.enqueue(Operator.add)
+            case "-":
+                operators.enqueue(Operator.subtract)
+            case "*":
+                operators.enqueue(Operator.multiply)
+            case "/":
+                operators.enqueue(Operator.divide)
             default:
-                beforeDoubleValue += String(char)
+                guard let double = Double($0) else {
+                    throw ExpressionParserError.canNotChangeDouble
+                }
+                operands.enqueue(double)
             }
         }
-        
-        guard let changingDouble = Double(beforeDoubleValue) else {
-            throw ExpressionParserError.canNotChangeDouble
-        }
-        operands.enqueue(changingDouble)
         
         return Formula(operands: operands, operators: operators)
     }
     
     private static func componentsByOperator(from input: String) -> [String] {
-        return [input]
+        return input.split(with: " ")
     }
 }
