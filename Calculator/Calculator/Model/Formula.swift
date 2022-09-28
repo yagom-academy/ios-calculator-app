c8505e06bda2a801ceade21517acf896a788bde5
hashswim <ss991117@naver.com>
2022-09-28 13:22:55 +0900

    feat: Formula 생성자 구현

--- Calculator/Calculator/Model/Formula.swift
+++ Calculator/Calculator/Model/Formula.swift
@@ -2,12 +2,17 @@
 //  Formula.swift
 //  Calculator
 
-
 import Foundation
+import UIKit
 
 struct Formula {
     var operands: CalculatorItemQueue
     var operators: CalculatorItemQueue
+    
+    init(operrands: CalculatorItemQueue, operators: CalculatorItemQueue) {
+        self.operands = operrands
+        self.operators = operators
+    }
     
     func result() -> Double {
         return 0
