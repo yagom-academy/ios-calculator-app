59f6473e5301622463d94e090c4ffd407a1876bf
chacha <happyhy0429@gmail.com>
2022-09-28 21:21:07 +0900

    feat: ViewController내부 숫자버튼과 숫자레이블을 위한 touchUpNumberButton, touchUpDoubleZeroButton메서드와 operandsLable 생성

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -7,11 +7,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var stringNumbers: String = ""
+    
+    @IBOutlet weak var operandsLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         var formula: Formula = Formula(operands: CalculatorItemQueue(), operators: CalculatorItemQueue())
         print(formula.result())
+    }
+    
+    @IBAction func touchUpNumberButton(_ sender: UIButton) {
+        if operandsLabel.text == "0" {
+            stringNumbers += "\(String(sender.tag))"
+            operandsLabel.text = "\(String(sender.tag))"
+        } else {
+            stringNumbers += "\(String(sender.tag))"
+            operandsLabel.text = stringNumbers
+        }
+    }
+    
+    @IBAction func touchUpDoubleZeroButton(_ sender: UIButton) {
+        if operandsLabel.text != "0" {
+            stringNumbers += "00"
+            operandsLabel.text = stringNumbers
+        } else if operandsLabel.text == "0" {
+            operandsLabel.text = "0"
+        }
     }
 }
 
