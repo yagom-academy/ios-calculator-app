6f5b31ea7dfe2319adfc17e30eb6d13521de2635
hashswim <ss991117@naver.com>
2022-09-29 00:18:18 +0900

    refactor: String CalculateItem 프로토콜 제거

--- Calculator/Calculator/Model/String.swift
+++ Calculator/Calculator/Model/String.swift
@@ -4,7 +4,7 @@
 //
 import Foundation
 
-extension String: CalculateItem {
+extension String {
     func split(with target: Character) -> [String] {
         return self.components(separatedBy: String(target))
     }
