340dfb525cd9a301d864da922c3e3fdeaed8229c
jonghancha <jonghancha@gmail.com>
2022-09-28 17:29:30 +0900

    chore: ExpressionParserTests 메서드의 Input 문자열 수정

--- Calculator/CalculatorModelTests/ExpressionParserTests.swift
+++ Calculator/CalculatorModelTests/ExpressionParserTests.swift
@@ -7,7 +7,7 @@ class ExpressionParserTests: XCTestCase {
     
     func test_문자열로수식을입력받았을때_parse호출및result호출시_피연산자와연산자가분리돼계산되어야한다() {
         // given
-        let input = "1+2-3*0"
+        let input = "1 + 2 - 3 * 0"
         
         // when
         var formula = ExpressionParser.parse(from: input)
