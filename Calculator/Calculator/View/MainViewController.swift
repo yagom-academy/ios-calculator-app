7bfd337ddb818898d0b8143182579e154955f20a
zhilly11 <zhzh1x4@gmail.com>
2022-09-29 20:31:13 +0900

    refactor: Button들을 Collection타입으로 수정

--- Calculator/Calculator/View/MainViewController.swift
+++ Calculator/Calculator/View/MainViewController.swift
@@ -5,26 +5,12 @@ import UIKit
 
 class MainViewController: UIViewController {
     
-    @IBOutlet weak var doubleZeroButton: UIButton!
-    @IBOutlet weak var zeroButton: UIButton!
-    @IBOutlet weak var oneButton: UIButton!
-    @IBOutlet weak var twoButton: UIButton!
-    @IBOutlet weak var threeButton: UIButton!
-    @IBOutlet weak var fourButton: UIButton!
-    @IBOutlet weak var fiveButton: UIButton!
-    @IBOutlet weak var sixButton: UIButton!
-    @IBOutlet weak var sevenButton: UIButton!
-    @IBOutlet weak var eightButton: UIButton!
-    @IBOutlet weak var nineButton: UIButton!
+    @IBOutlet var numberButton: [UIButton]!
+    @IBOutlet var operatorButton: [UIButton]!
     
     @IBOutlet weak var dotButton: UIButton!
     @IBOutlet weak var equalButton: UIButton!
-    
-    @IBOutlet weak var addButton: UIButton!
-    @IBOutlet weak var subtractButton: UIButton!
-    @IBOutlet weak var devideButton: UIButton!
-    @IBOutlet weak var multiplyButton: UIButton!
-    
+
     @IBOutlet weak var ceButton: UIButton!
     @IBOutlet weak var acButton: UIButton!
     @IBOutlet weak var reverseSignButton: UIButton!
