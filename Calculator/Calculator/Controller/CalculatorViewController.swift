9502329992d361583dd14c02e02b32c36ef5844f
미니 <cow970814@naver.com>
2022-09-29 10:13:03 +0900

    feat : CalculateViewController 사용자 입력 메서드 추가

--- Calculator/Calculator/Controller/CalculatorViewController.swift
+++ Calculator/Calculator/Controller/CalculatorViewController.swift
@@ -6,7 +6,46 @@
 import UIKit
 
 class CalculatorViewController: UIViewController {
+    private var mathExpression: String = ""
+    private var selectedNumbers: String = ""
+    private var selectedOperator: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    @IBAction func didTappedNumberButton(_ sender: UIButton) {
+        if selectedNumbers.isEmpty {
+            mathExpression.append(selectedOperator)
+        }
+        
+        guard let inputNumber = sender.titleLabel?.text else {
+            return
+        }
+        
+        selectedNumbers.append(inputNumber)
+        
+    }
+    
+    @IBAction func didTappedOperatorButton(_ sender: UIButton) {
+        mathExpression.append(selectedNumbers)
+        selectedNumbers = ""
+        
+        guard let inputedOperator = sender.titleLabel?.text else {
+            return
+        }
+        
+        selectedOperator = inputedOperator
+    }
+    @IBAction func didTappedEqualButton(_ sender: UIButton) {
+        
+        guard let lastElement = mathExpression.last else {
+            return
+        }
+        
+        if lastElement.shouldConvertOperator {
+            mathExpression.append(selectedNumbers)
+        }
+        
     }
 }
