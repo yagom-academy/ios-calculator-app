4646c1cbf94903f40b244c2ce281ed15ab071f51
Ayaan <onestar7984@gmail.com>
2022-09-30 04:34:35 +0900

    feat: FormulaStackView 구현

---
+++ Calculator/Calculator/StackView/FormulaStackView.swift
@@ -0,0 +1,56 @@
+import UIKit
+
+class FormulaStackView: UIStackView {
+    private(set) var formula: String = ""
+    
+    func appendFormula(combining operatorLabel: OperatorLabel, to numberLabel: NumberLabel) {
+        guard let number = numberLabel.text else {
+            return
+        }
+        if formula.isEmpty == true {
+            formula.append(number)
+            addSubStackView(including: number)
+        } else {
+            guard let operatorSign = operatorLabel.text else {
+                return
+            }
+            formula.append(operatorSign + number)
+            addSubStackView(including: number, and: operatorSign)
+        }
+    }
+    
+    private func addSubStackView(including number: String, and operatorSign: String = "") {
+        let subStackView: UIStackView = makeSubStackView()
+        
+        let numberLabel: UILabel = UILabel()
+        let operatorLabel: UILabel = UILabel()
+        numberLabel.text = number
+        numberLabel.textColor = .white
+        operatorLabel.text = operatorSign
+        operatorLabel.textColor = .white
+        
+        subStackView.addArrangedSubview(operatorLabel)
+        subStackView.addArrangedSubview(numberLabel)
+
+        self.addArrangedSubview(subStackView)
+    }
+    
+    private func makeSubStackView() -> UIStackView {
+        let subStackView: UIStackView = UIStackView()
+        subStackView.axis = .horizontal
+        subStackView.alignment = .fill
+        subStackView.distribution = .fill
+        subStackView.spacing = 8.0
+        
+        return subStackView
+    }
+}
+
+extension FormulaStackView: InitializationProtocol {
+    func Initialization() {
+        formula = ""
+        self.arrangedSubviews.forEach {
+            $0.removeFromSuperview()
+        }
+    }
+}
