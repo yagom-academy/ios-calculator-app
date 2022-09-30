56cee69df65f5a0e5e591e954c5e407936bb57b2
ash-youu <ash.you.dev@gmail.com>
2022-09-30 16:35:59 +0900

    feat: 버튼 touch시 label값이 수정되도록 버튼 tag 수정 및 메서드 선언부 작성

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -8,11 +8,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainOperandLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func touchUpNumberButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func touchUpZeroButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func touchUpZeroZeroButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func touchUpDotButton(_ sender: UIButton) {
+    }
+    
 }
 
