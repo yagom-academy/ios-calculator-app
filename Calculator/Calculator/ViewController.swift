1dd90db0c9abd613947eb0fd5801647521de9ec0
chacha <happyhy0429@gmail.com>
2022-09-30 12:43:00 +0900

    refactor:touchUpNumberButton메서드 내부 숫자 자리수 기준 구현

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -7,11 +7,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let doubleZero = "00"
     let zero = "0"
     let empty = ""
     var stringNumbers: String = ""
     var stringOperators: String = ""
+    let numberFormatter = NumberFormatter()
+    
     @IBOutlet weak var recentNumbersStackView: UIStackView!
+    @IBOutlet weak var recentNumbersScrollView: UIScrollView!
     
     @IBOutlet weak var operandsLabel: UILabel!
     @IBOutlet weak var operatorLabel: UILabel!
@@ -19,8 +23,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        stringNumbers = zero
-        operandsLabel.text = stringNumbers
+        operandsLabel.text = zero
         operatorLabel.text = stringOperators
         recentNumbersStackView.arrangedSubviews.forEach {
             $0.isHidden = true
@@ -43,6 +46,7 @@ class ViewController: UIViewController {
     }
     
     @IBAction func touchUpPositiveNegativeButton(_ sender: UIButton) {
+        guard operandsLabel.text != zero else { return }
         if !stringNumbers.contains("-") {
             stringNumbers = "-\(stringNumbers)"
             operandsLabel.text = stringNumbers
@@ -67,29 +71,44 @@ class ViewController: UIViewController {
         let stackView: UIStackView = .init(arrangedSubviews: [recentOperatorLabel, recentOperandsLabel])
         
         recentNumbersStackView.addArrangedSubview(stackView)
+        recentNumbersScrollView.layoutIfNeeded()
+        recentNumbersScrollView.setContentOffset(CGPoint(x: 0, y: recentNumbersScrollView.contentSize.height - recentNumbersScrollView.bounds.height), animated: false)
     }
     
     @IBAction func touchUpNumberButton(_ sender: UIButton) {
+        guard stringNumbers.count < 20 else { return }
+        
+        numberFormatter.numberStyle = .decimal
+        numberFormatter.maximumSignificantDigits = 20
+        
         if operandsLabel.text == zero {
             stringNumbers = sender.titleLabel?.text ?? ""
             operandsLabel.text = stringNumbers
         } else {
             stringNumbers += sender.titleLabel?.text ?? ""
-            operandsLabel.text = stringNumbers
+            guard let stringNumber = Double(stringNumbers),
+                  let number = numberFormatter.string(from: NSNumber(value: stringNumber)) else { return }
+            operandsLabel.text = number
         }
     }
     
     @IBAction func touchUpDoubleZeroButton(_ sender: UIButton) {
         if operandsLabel.text != zero {
-            stringNumbers += "00"
+            stringNumbers += doubleZero
             operandsLabel.text = stringNumbers
         }
     }
+    
     @IBAction func touchUpDotButton(_ sender: UIButton) {
-        if stringNumbers.last != "." && !stringNumbers.contains(".") && stringNumbers == zero {
-            stringNumbers += "."
-            operandsLabel.text = stringNumbers
-        }
+        guard !stringNumbers.contains(".") else { return }
+        stringNumbers += "."
+        operandsLabel.text = stringNumbers
+    }
+    
+    @IBAction func touchUpEqualButton(_ sender: UIButton) {
+        
+        // 숫자 개수가 3개씩 될때마다 . 찍어줘
+        
     }
 }
 
