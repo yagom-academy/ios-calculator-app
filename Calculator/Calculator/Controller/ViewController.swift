0cf13c768edfc8f11e96e32e608e0fca9e5ba5e8
ash-youu <ash.you.dev@gmail.com>
2022-09-30 17:10:15 +0900

    feat: 숫자버튼을 누르면 NumberFormatter가 적용돼 UI에 나타나도록 구현

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -7,15 +7,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let numberFormatter = NumberFormatter()
     @IBOutlet weak var mainOperandLabel: UILabel!
+    var operandLabelText: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        mainOperandLabel.text = applyNumberFormatter(to: mainOperandLabel.text ?? "")
     }
-
+    
     @IBAction func touchUpNumberButton(_ sender: UIButton) {
+        operandLabelText = removeComma(mainOperandLabel.text)
+        guard operandLabelText != "0" else {
+            mainOperandLabel.text = sender.tag.description
+            return
+        }
+        
+        mainOperandLabel.text = applyNumberFormatter(to: operandLabelText + sender.tag.description)
     }
     
     @IBAction func touchUpZeroButton(_ sender: UIButton) {
@@ -27,5 +36,32 @@ class ViewController: UIViewController {
     @IBAction func touchUpDotButton(_ sender: UIButton) {
     }
     
+    func removeComma(_ string: String?) -> String {
+        guard let returnValue = string?.components(separatedBy: ",").joined() else {
+            return ""
+        }
+        return returnValue
+    }
+    
+    func applyNumberFormatter(to string: String) -> String {
+        numberFormatter.numberStyle = .decimal // 3자리마다 , 를 찍어주는 역할
+        numberFormatter.usesSignificantDigits = true
+        numberFormatter.maximumSignificantDigits = 20 // 유효자리수를 설정해주는 역할
+        
+        guard string.contains(".") else {
+            guard let number: NSNumber = numberFormatter.number(from: string),
+                  let returnValue = numberFormatter.string(from: number) else {
+                return ""
+            }
+            return returnValue
+        }
+        
+        let stringsSplitedByDot: [String] = string.split(with: ".")
+        guard let numberBeforeDot: NSNumber = numberFormatter.number(from: stringsSplitedByDot[0]),
+              let returnValue = numberFormatter.string(from: numberBeforeDot) else {
+            return ""
+        }
+        return returnValue + "." + stringsSplitedByDot[1]
+    }
 }
 
