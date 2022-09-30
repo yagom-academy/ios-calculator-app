74b4937fecd2569c413a0e2195b1eb2602631994
jsa0224 <94514250+jsa0224@users.noreply.github.com>
2022-09-30 16:30:42 +0900

    feat: CalculatorNameSpace 소스 파일 생성 & enum 선언

---
+++ Calculator/Calculator/Model/CalculatorNameSpace.swift
@@ -0,0 +1,28 @@
+//
+//  CalculatorNameSpace.swift
+//  Calculator
+//
+//  Created by 정선아 on 2022/09/30.
+//
+
+import Foundation
+
+enum CalculatorNameSpace: String {
+    case zero = "0"
+    case doubleZero = "00"
+    case one = "1"
+    case two = "2"
+    case three = "3"
+    case four = "4"
+    case five = "5"
+    case six = "6"
+    case seven = "7"
+    case eight = "8"
+    case nine = "9"
+    
+    case dot = "."
+    case minusSign = "-"
+    
+    case exclamationMark = "!"
+    case questionMark = "?"
+}
