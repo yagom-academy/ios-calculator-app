f30c77126bb238346aca16ec924e163b5f52db5b
yuvinrho <yvrho@naver.com>
2022-09-30 16:34:59 +0900

    feat: MathSymbol 타입 정의, NumericKeypad 타입 numKeys 리스트 수정

---
+++ Calculator/Calculator/Enums/MathSymbol.swift
@@ -0,0 +1,17 @@
+//
+//  MathSymbol.swift
+//  Calculator
+//
+//  Created by 노유빈 on 2022/09/30.
+//
+
+enum MathSymbol {
+    static let plus = "+"
+    static let minus = "-"
+    static let division = "÷"
+    static let multiplication = "×"
+    static let decimalPoint = "."
+    static let nagative = "−"
+}
+
+
