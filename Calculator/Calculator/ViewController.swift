e3479ed8bb3ace171c229a91040f15e69278030d
jin <pearlisrich@naver.com>
2022-10-02 11:41:09 +0900

    feat: Constant enum 추가로 네임스페이스로 스트링 분리 작업

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -7,7 +7,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private enum Constant {
+        static let one = "1"
+        static let two = "2"
+        static let three = "3"
+        static let four = "4"
+        static let five = "5"
+        static let six = "6"
+        static let seven = "7"
+        static let eight = "8"
+        static let nine = "9"
+        static let add = "+"
+        static let substract = "−"
+        static let multiply = "×"
+        static let divide = "÷"
+        static let minus = "-"
+        static let doubleZero = "00"
+        static let dot = "."
+        static let zero = "0"
+    }
+    
     private var expression: String = ""
     let numberFormatter: NumberFormatter = NumberFormatter()
     
