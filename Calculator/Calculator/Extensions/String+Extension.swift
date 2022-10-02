750c0032c1dbb02ebbe42b051e8b316882dcd950
yuvinrho <yvrho@naver.com>
2022-10-02 22:25:54 +0900

    feat: String타입에 convertNSNumber() 정의

--- Calculator/Calculator/Extensions/String+Extension.swift
+++ Calculator/Calculator/Extensions/String+Extension.swift
@@ -5,8 +5,16 @@
 //  Created by 노유빈 on 2022/09/22.
 //
 
+import Foundation
+
 extension String {
     func split(with target: Character) -> [String] {
         return self.components(separatedBy: String(target))
+    }
+    
+    func convertNSNumber() -> NSNumber {
+        let string = self.replacingOccurrences(of: MathSymbol.negative, with: MathSymbol.minus)
+        
+        return (Double(string) ?? 0) as NSNumber
     }
 }
