9ec493f7908f6c51d811a9ba1aa98d58c1447ab6
yuvinrho <yvrho@naver.com>
2022-10-03 16:16:12 +0900

    feat: ExpressionParser 열거형 추가

---
+++ Calculator/Calculator/Enums/ExpressionParser.swift
@@ -0,0 +1,32 @@
+//
+//  ExpressionParser.swift
+//  Calculator
+//
+//  Created by 노유빈 on 2022/10/03.
+//
+
+import Foundation
+
+enum ExpressionParser {
+    static func parser(from input: String) -> Formula {
+        let operands = ExpressionParser.componentsByOperators(from: input).compactMap { Double($0) }
+        let operators = input.compactMap { Operator(rawValue: $0) }
+        
+        var operandsQueue = CalculatorItemQueue<Double>()
+        var operatorsQueue = CalculatorItemQueue<Operator>()
+        operandsQueue.push(operands)
+        operatorsQueue.push(operators)
+        
+        return Formula(operands: operandsQueue, operators: operatorsQueue)
+    }
+    
+    static private func componentsByOperators(from input: String) -> [String] {
+        var operators: CharacterSet = CharacterSet()
+
+        Operator.allCases.forEach {
+            operators.insert(charactersIn: String($0.rawValue))
+        }
+
+        return input.components(separatedBy: operators)
+    }
+}
