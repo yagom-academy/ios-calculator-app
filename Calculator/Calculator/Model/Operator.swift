8e28071192a15211a721a8dedb6ea9f2d89fa681
lj-7-77 <swtw72@gmail.com>
2022-10-03 15:41:39 +0900

    feat: Operator 열거형 추가

---
+++ Calculator/Calculator/Model/Operator.swift
@@ -0,0 +1,42 @@
+//
+//  Operator.swift
+//  Calculator
+//
+//  Created by 임지연 on 2022/10/03.
+//
+
+enum Operator: Character, CaseIterable, CalculateItem {
+    case add = "+"
+    case subtract = "-"
+    case multiply = "*"
+    case divide = "/"
+    
+    func calculate(lhs: Double, rhs: Double) -> Double {
+        switch self {
+        case .add:
+            return add(lhs, rhs)
+        case .subtract:
+            return subtract(lhs, rhs)
+        case .multiply:
+            return multiply(lhs, rhs)
+        case .divide:
+            return divide(lhs, rhs)
+        }
+    }
+    
+    private func add(_ lhs: Double, _ rhs: Double) -> Double {
+        return lhs + rhs
+    }
+    
+    private func subtract(_ lhs: Double, _ rhs: Double) -> Double {
+        return lhs - rhs
+    }
+    
+    private func multiply(_ lhs: Double, _ rhs: Double) -> Double {
+        return lhs * rhs
+    }
+    
+    private func divide(_ lhs: Double, _ rhs: Double) -> Double {
+        return lhs / rhs
+    }
+}
