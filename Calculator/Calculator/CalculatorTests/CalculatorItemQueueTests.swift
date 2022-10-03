45aef187622b8fbe89c78e2eeefcf34826bd8406
hashswim <ss991117@naver.com>
2022-10-03 16:08:52 +0900

    test: CalculatorQueue 테스트 코드 구현

--- Calculator/Calculator/CalculatorTests/CalculatorItemQueueTests.swift
+++ Calculator/Calculator/CalculatorTests/CalculatorItemQueueTests.swift
@@ -4,119 +4,87 @@ import XCTest
 
 private class CalculatorItemQueueTests: XCTestCase {
 
-    var sut: CalculatorItemQueue!
+    var sut: CalculatorItemQueue<Double>!
     var testCase1: Double = Double.random(in: (0...100))
     var testCase2: Double = Double.random(in: (0...100))
     var testCase3: Double = Double.random(in: (0...100))
     
     override func setUpWithError() throws {
+        try super.setUpWithError()
         sut = CalculatorItemQueue([])
     }
 
     override func tearDownWithError() throws {
+        try super.tearDownWithError()
         sut = nil
     }
 
     func test_CalculatorItemQueue생성시_빈배열전달하면_isEmpty가true리턴() {
-        let deque = CalculatorItemQueue([])
-        
-        let result = deque.isEmpty
+        let result = sut.isEmpty
         
         XCTAssertTrue(result)
     }
     
-    func test_CalculatorItemQueue에서_enque와deque에_아이템이_모두있을때_popLast시_마지막아이템추출() {
-        sut.enqueue = [testCase1]
-        sut.dequeue = [testCase2, testCase3]
-        
-        let result = sut.popLast()
-        
-        guard let result = result as? Double else{
-            XCTAssert(false)
-            return
-        }
+    func test_queue에_데이터가없고_enqueue를_했을때_enqueueStack에_들어가는지() {
+        sut.enqueue(testCase1)
+        let result = sut.enqueueStack[0]
         
         XCTAssertEqual(testCase1, result)
     }
     
-    func test_CalculatorItemQueue에서_enque에만_아이템이있을때_popLast시_마지막아이템추출() {
-        sut.enqueue = [testCase1, testCase2, testCase3]
-        
-        let result = sut.popLast()
-        
-        guard let result = result as? Double else{
-            XCTAssert(false)
-            return
-        }
-        
-        XCTAssertEqual(testCase3, result)
-    }
-    
-    func test_CalculatorItemQueue에서_deque에만_아이템이있을때_popLast시_마지막아이템추출() {
-        sut.dequeue = [testCase1, testCase2, testCase3]
-        let result = sut.popLast()
-        
-        guard let result = result as? Double else{
-            XCTAssert(false)
-            return
-        }
-        
-        XCTAssertEqual(testCase1, result)
-    }
-    
-    func test_CalculatorItemQueue에서_enque와deque에_아이템이_모두있을때_popFirst시_첫번째아이템추출() {
-        sut.enqueue = [testCase1, testCase3]
-        sut.dequeue = [testCase2]
-        
-        let result = sut.popFirst()
-        
-        guard let result = result as? Double else{
-            XCTAssert(false)
-            return
-        }
+    func test_queue에_데이터가있고_enqueue를_했을때_enqueueStack의_뒤로_들어가는지() {
+        sut.enqueue(testCase1)
+        sut.enqueue(testCase2)
+        let result = sut.enqueueStack[1]
         
         XCTAssertEqual(testCase2, result)
     }
     
-    func test_CalculatorItemQueue에서_enque에만_아이템이있을때_popFirst시_첫번째아이템추출() {
-        sut.enqueue = [testCase1, testCase3]
+    func test_queue에_데이터가없고_dequeue를_했을떄_nil이_반환되는지() {
+        let result = sut.dequeue()
         
-        let result = sut.popFirst()
-        
-        guard let result = result as? Double else{
-            XCTAssert(false)
-            return
-        }
+        XCTAssertNil(result)
+    }
+    
+    
+    func test_queue에_데이터가있고_dequeue를_했을때_데이터가_반환되는지() {
+        sut.enqueue(testCase1)
+        let result = sut.dequeue()
         
         XCTAssertEqual(testCase1, result)
     }
 
-    func test_CalculatorItemQueue에서_deque에만_아이템이있을때_popFirst시_첫번째아이템추출() {
-        sut.dequeue = [testCase1, testCase3]
-        
-        let result = sut.popFirst()
-        
-        guard let result = result as? Double else{
-            XCTAssert(false)
-            return
-        }
-        
-        XCTAssertEqual(testCase3, result)
+    func test_enqueue를_여러번하고_dequeue를_했을때_앞의_데이터가_반환되는지() {
+        sut.enqueue(testCase1)
+        sut.enqueue(testCase2)
+        sut.enqueue(testCase3)
+        let result = sut.dequeue()
+
+        XCTAssertEqual(testCase1, result)
     }
 
-    func test_CalculatorItemQueue에서_pushLast시_마지막아이템할당() {
-        sut.enqueue = [testCase3]
-        sut.dequeue = [testCase1]
-
-        sut.pushLast(testCase1)
+    func test_queue에_데이터가있고_isEmpty를_했을때_true가_리턴되는지() {
+        sut.enqueue(testCase1)
+        sut.enqueue(testCase2)
+        let result = sut.isEmpty
         
-        let result = sut.enqueue.last
+        XCTAssertTrue(result)
+    }
+    
+    func test_queue에_데이터가없고_isEmpty를_했을때_false가_리턴되는지() {
+        let result = sut.isEmpty
         
-        guard let result = result as? Double else{
-            XCTAssert(false)
-            return
-        }
+        XCTAssertFalse(result)
+    }
+    
+    func test_queue에_데이터가있고_removeAll을_했을때_queue가_비워지는지() {
+        sut.enqueue(testCase1)
+        sut.enqueue(testCase2)
+        sut.enqueue(testCase3)
         
-        XCTAssertEqual(testCase1, result)
+        sut.removeAll()
+        let result = sut.isEmpty
+        
+        XCTAssertTrue(result)
     }
 }
