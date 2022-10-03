74ee2bf7baf5de6c46d4fc93238e969ac66c7872
jsa0224 <94514250+jsa0224@users.noreply.github.com>
2022-10-03 21:35:41 +0900

    refactor: parse 메서드 내부 로직 변경

--- Calculator/Calculator/Models/ExpressionParser.swift
+++ Calculator/Calculator/Models/ExpressionParser.swift
@@ -9,17 +9,17 @@ import Foundation
 
 enum ExpressionParser {
     static func parse(from input: String) -> Formula {
-        let operands = CalculatorItemQueue<Double>()
-        let operators = CalculatorItemQueue<Operator>()
-        let operatorTypes = Operator.allCases.map { String($0.rawValue) }
-
-        let operandComponents = self.componentsByOperators(from: input)
-        operandComponents.compactMap { Double($0) }.forEach { operands.enqueue($0) }
+        let divided = ExpressionParser.componentsByOperators(from: input)
+        let operands = divided.compactMap { Double($0) }
+        let operators = input.compactMap { Operator(rawValue: $0) }
         
-        let operatorComponents = input.map { String($0) }.filter { operatorTypes.contains($0) }
-        operatorComponents.compactMap { Operator(rawValue: Character($0)) }.forEach { operators.enqueue($0) }
+        let operandsToQueue = CalculatorItemQueue<Double>()
+        operands.forEach { operandsToQueue.enqueue($0)}
         
-        return Formula(operands: operands, operators: operators)
+        let operatorsToQueue = CalculatorItemQueue<Operator>()
+        operators.forEach{ operatorsToQueue.enqueue($0)}
+        
+        return Formula(operands: operandsToQueue, operators: operatorsToQueue)
     }
 
     private static func componentsByOperators(from input: String) -> [String] {
