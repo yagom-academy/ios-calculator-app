6049897600804dc1f4d70cea7a4f4ca28562767b
chacha <happyhy0429@gmail.com>
2022-10-04 15:58:45 +0900

    feat: VC내부 viewDidLoad() 메서드 내부 초기화상태 완성

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -24,7 +24,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        resetSubviewsIfNeeded()
     }
     
     @IBAction private func numberButtonPressed(_ sender: UIButton) {
