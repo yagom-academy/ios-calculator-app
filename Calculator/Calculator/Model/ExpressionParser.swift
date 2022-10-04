013670393ac4bbaebf2202696a1b5f0bf6de3013
Jae Hyung Kim <smfc.mene@gmail.com>
2022-10-04 16:11:10 +0800

    refactor: ExpressionParser에서 rawValue대신 operators.description 사용하도록 수정

--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -1,32 +1,32 @@
-////
-////  ExpressionParser.swift
-////  Calculator
 //
-//
-//import Foundation
-//
-//enum ExpressionParser {
-//    static func parse(from input: String) -> Formula {
-//        let operators = input.filter {
-//            "0123456789".contains($0) == false }.compactMap {
-//                Operator(rawValue: $0) }
-//        
-//        let operands = componentsByOperators(from: input).compactMap {
-//            Double($0)
-//        }
-//        
-//        return Formula(operands: CalculatorItemQueue(operands),
-//                       operators: CalculatorItemQueue(operators))
-//    }
-//    
-//    private static func componentsByOperators(from input: String) -> [String] {
-//        var result: [String]
-//        
-//        result = input.split(with: " ")
-//        Operator.allCases.forEach { operators in
-//            result = result.flatMap {
-//                $0.split(with: Character(operators.description))}
-//        }
-//        return result
-//    }
-//}
+//  ExpressionParser.swift
+//  Calculator
+
+import Foundation
+
+enum ExpressionParser {
+    static func parse(from input: String) -> Formula {
+        let operandsQueue = componentsByOperators(from: input).compactMap{
+            Double($0)
+        }
+        let operatorsQueue: [Operator] = input.compactMap{
+            Operator(rawValue: $0)
+        }
+        let formula = Formula(operands: CalculatorItemQueue(enqueueStack: operandsQueue),
+                              operators: CalculatorItemQueue(enqueueStack: operatorsQueue))
+        
+        return formula
+    }
+    
+    private static func componentsByOperators(from input: String) -> [String] {
+        var separators: CharacterSet = CharacterSet()
+        
+        Operator.allCases.forEach {
+            separators.insert(charactersIn: Character(operators.description))
+        }
+        
+        let result = input.components(separatedBy: separators)
+        
+        return result
+    }
+}
