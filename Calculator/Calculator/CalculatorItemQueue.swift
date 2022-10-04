1333236b19e6261c26fcbf3fcc873074034cf7fd
chacha <happyhy0429@gmail.com>
2022-10-04 11:17:07 +0900

    feat:CalculatorItemQueue 클래스 추가

---
+++ Calculator/Calculator/CalculatorItemQueue.swift
@@ -0,0 +1,29 @@
+//
+//  CalculatorItemQueue.swift
+//  Calculator
+//
+//  Created by hy on 2022/10/04.
+//
+
+import Foundation
+
+final class CalculatorItemQueue<T> {
+    
+    private var queue: LinkedList = LinkedList<T>()
+    
+    func enqueue(item: T) {
+        queue.append(item)
+    }
+    
+    func dequeue() -> T? {
+        return queue.pop()
+    }
+    
+    func removeAll() {
+        queue.removeAll()
+    }
+    
+    func isEmpty() -> Bool {
+        return queue.isEmpty
+    }
+}
