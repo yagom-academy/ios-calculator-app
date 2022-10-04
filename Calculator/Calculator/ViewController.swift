cca9977f5f5a2b573c6e1a69957a2c6bf081ce79
chacha <happyhy0429@gmail.com>
2022-10-04 13:47:21 +0900

    feat:VC내부 operandsLabel, operatorLabel @IBOutlet 프로퍼티 추가

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -7,7 +7,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var operandsLabel: UILabel!
+    @IBOutlet weak var operatorLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
