4004d484429d683cf91270f5828c79cbe042c6d2
jin <pearlisrich@naver.com>
2022-10-04 13:56:45 +0900

    feat: VC내부 updateOperandLabel() 메서드 추가, 피연산자의 레이블 업데이트 메서드

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -7,14 +7,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var operandsLabel: UILabel!
+    @IBOutlet weak var operandLabel: UILabel!
     @IBOutlet weak var operatorLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    private func updateOperandLabel(with number: String) {
+        
+        if operandLabel.isZero {
+            operandLabel.text = number
+        } else {
+            if let operandText = operandLabel.text {
+                operandLabel.text = operandText + number
+            }
+        }
+    }
 
 }
 
