6239db254e1aef562d432aae4dda8e9ac9375db2
woong <workplayhard1@naver.com>
2022-10-04 13:51:51 +0900

    feat: UILabel extension에 isZero 프로퍼티 추가

---
+++ Calculator/Calculator/UILabel+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  UILabel+Extension.swift
+//  Calculator
+//
+//  Created by 서현웅 on 2022/10/04.
+//
+
+import UIKit
+
+extension UILabel {
+    var isZero: Bool {
+        self.text == "0"
+    }
+}
