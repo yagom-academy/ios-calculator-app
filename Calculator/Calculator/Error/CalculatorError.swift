19406a109de4bde8861e01f6c39cf6bcc9f690c8
yuvinrho <yvrho@naver.com>
2022-10-04 16:50:33 +0900

    refactor: 에러케이스 수정

--- Calculator/Calculator/Error/CalculatorError.swift
+++ Calculator/Calculator/Error/CalculatorError.swift
@@ -8,18 +8,18 @@
 enum CalculatorError: Error {
     case queueIsEmpty
     case divisionByZero
-    case wrongFormula
+    case unknownError
 }
 
-extension CalculatorError: CustomDebugStringConvertible {
-    var debugDescription: String {
+extension CalculatorError: CustomStringConvertible {
+    var description: String {
         switch self {
         case .queueIsEmpty:
             return "큐가 비어있습니다."
         case .divisionByZero:
             return "0으로 나눌 수 없습니다."
-        case .wrongFormula:
-            return "잘못된 수식입니다."
+        case .unknownError:
+            return "예상치 못한 에러입니다."
         }
     }
 }
