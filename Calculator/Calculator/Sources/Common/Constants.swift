f8396dbb371eafd5d9b241335f86dfd07f831d5e
미니 <cow970814@naver.com>
2022-10-04 17:11:49 +0900

    refactor : merge CalculatorViewController

--- Calculator/Calculator/Sources/Common/Constants.swift
+++ Calculator/Calculator/Sources/Common/Constants.swift
@@ -8,10 +8,14 @@
 enum Constant {
     enum Calculator {
         static let defaultNumber: String = "0"
-        static let defaultOperator: String = ""
-        static let defaultInput: String = ""
-        static let defaultPoint: String = "."
-        static let minusOperator: Character = "-"
-        static let errorNumber: String = "Nan"
+        static let doubleZero: String = "00"
+        static let empty: String = ""
+        static let decimalPoint: String = "."
+        static let nan: String = "NaN"
+        
+        static let plusSymbol: String = "+"
+        static let minusSymbol: String = "-"
+        static let divideSymbol: String = "÷"
+        static let multiplySymbol: String = "×"
     }
 }
