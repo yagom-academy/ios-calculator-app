1d45b2786a7a7021fef8b3e12d851681ae7d448e
ash-youu <ash.you.dev@gmail.com>
2022-10-05 11:49:49 +0900

    test: StringExtensionTests addComma와 addCommaInteger에 해당하는 유닛테스트 작성

--- Calculator/CalculatorTests/StringExtensionTests.swift
+++ Calculator/CalculatorTests/StringExtensionTests.swift
@@ -9,11 +9,35 @@ import XCTest
 @testable import Calculator
 
 class StringExtensionTests: XCTestCase {
-    func test_소숫점이있는숫자에numberFormatter를적용하였을시_정수부분만numberFormatter가적용되는지() {
+    func test_소숫점이있는숫자에addCommaInteger를적용하였을시_정수부분만numberFormatter가적용되는지() {
         let input: String = "178264.001"
         
         let result: String = input.addCommaInteger()
         
         XCTAssertEqual("178,264.001", result)
+    }
+    
+    func test_소수점이있는숫자가정수형일때_addCommaInteger를적용하였을시_정수부분만numberFormatter가적용되는지() {
+        let input: String = "178264.000000"
+        
+        let result: String = input.addCommaInteger()
+        
+        XCTAssertEqual("178,264.000000", result)
+    }
+    
+    func test_소숫점만있는정수형에addComma를적용하였을시_numberFormatter가적용되는지() {
+        let input: String = "178264."
+        
+        let result: String = input.addComma()
+        
+        XCTAssertEqual("178,264", result)
+    }
+    
+    func test_소수점이없는숫자에addComma를적용하였을시_numberFormatter가적용되는지() {
+        let input: String = "178264"
+        
+        let result: String = input.addComma()
+        
+        XCTAssertEqual("178,264", result)
     }
 }
