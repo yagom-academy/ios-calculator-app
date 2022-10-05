22e40189a5daea1ec9519e393ffe3509a273313c
yuvinrho <yvrho@naver.com>
2022-10-05 11:00:26 +0900

    feat: CalculatorViewController 레이블, 스크롤뷰, 스택뷰 IBOutlet연결

---
+++ Calculator/Calculator/Controller/CalculatorViewControllerViewController.swift
@@ -0,0 +1,22 @@
+//
+//  Calculator - ViewController.swift
+//  Created by yagom. 
+//  Copyright © yagom. All rights reserved.
+// 
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+    @IBOutlet private weak var operatorLabel: UILabel!
+    @IBOutlet private weak var numberLabel: UILabel!
+    @IBOutlet private weak var historyStackView: UIStackView!
+    @IBOutlet private weak var historyScrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
