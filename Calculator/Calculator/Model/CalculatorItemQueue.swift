d6087c1bb8d73417dcc994b36ba36fbd89128930
wonbi92 <bin9239@gmail.com>
2022-10-05 11:56:55 +0900

    feat: 유닛 테스트 진행을 위한 CalculatorItemQueue타입에 statusQueue 연산 프로퍼티 구현

--- Calculator/Calculator/Model/CalculatorItemQueue.swift
+++ Calculator/Calculator/Model/CalculatorItemQueue.swift
@@ -14,6 +14,18 @@ struct CalculatorItemQueue<T: CalculateItem> {
         return list.isEmpty
     }
     
+    var statusQueue: [T] {
+        var result: [T] = [T]()
+        var queue = self
+        
+        while !queue.isEmpty {
+            if let component = queue.dequeue() {
+                result.append(component)
+            }
+        }
+        return result
+    }
+    
     mutating func enqueue(_ element: T) {
         list.append(element)
     }
