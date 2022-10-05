2f66d3ebaafa8a994994666b667421aa89a8b29d
lj-7-77 <swtw72@gmail.com>
2022-10-05 12:25:09 +0900

    feat: CalculatorKeypad 열거형 추가, touchCalculatorButton 액션메서드 추가

--- Calculator/Calculator/Controller/CalculatorViewControllerViewController.swift
+++ Calculator/Calculator/Controller/CalculatorViewControllerViewController.swift
@@ -11,12 +11,39 @@ class CalculatorViewController: UIViewController {
     @IBOutlet private weak var numberLabel: UILabel!
     @IBOutlet private weak var historyStackView: UIStackView!
     @IBOutlet private weak var historyScrollView: UIScrollView!
+    var currentInputNumber: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func touchCalculatorButton(_ sender: UIButton) {
+        
+        guard let key: CalculatorKeypad = CalculatorKeypad(rawValue: sender.currentTitle ?? "") else {
+            return
+        }
+        
+        switch key {
+        case .one, .two, .three, .four, .five, .six, .seven, .eight, .nine, .zero, .doubleZero:
+            inputNumber(key.rawValue)
+            print(currentInputNumber)
+        case .dot:
+            print(key.rawValue)
+        case .add, .subtract, .multiply, .divide:
+            print(key.rawValue)
+        case .ac:
+            print(key.rawValue)
+        case .ce:
+            print(key.rawValue)
+        case .plusMinus:
+            print(key.rawValue)
+        case .equal:
+            print(key.rawValue)
+        }
+    }
+    
+    private func inputNumber(_ number: String) {
+        currentInputNumber += number
+    }
 }
 
---
+++ Calculator/Calculator/Enums/CalculatorKeypad.swift
@@ -0,0 +1,31 @@
+//
+//  CalculatorKeypad.swift
+//  Calculator
+//
+//  Created by 임지연 on 2022/10/05.
+//
+
+enum CalculatorKeypad: String {
+    case one = "1"
+    case two = "2"
+    case three = "3"
+    case four = "4"
+    case five = "5"
+    case six = "6"
+    case seven = "7"
+    case eight = "8"
+    case nine = "9"
+    case zero = "0"
+    case doubleZero = "00"
+    case dot = "."
+    
+    case add = "+"
+    case subtract = "-"
+    case multiply = "×"
+    case divide = "÷"
+
+    case ac = "AC"
+    case ce = "CE"
+    case plusMinus = "⁺⁄₋"
+    case equal = "="
+}
