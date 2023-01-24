bc36948e3fd847666c946867723cf6643f68d599
kaki <ycg02109@naver.com>
2023-01-24 17:29:08 +0900

    feat: CalculateItem 프로토콜 생성 및 채택

---
+++ Calculator/Calculator/Model/CalculateItem.swift
@@ -0,0 +1,14 @@
+//
+//  CalculateItem.swift
+//  Calculator
+//
+//  Created by kaki on 2023/01/24.
+//
+
+protocol CalculateItem { }
+
+extension Double: CalculateItem { }
+
+extension Int: CalculateItem { }
+
+extension String: CalculateItem { }
