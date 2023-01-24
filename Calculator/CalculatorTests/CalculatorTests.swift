1414e4b0a06a2a21ae87edc82d2baa854aa3e331
Jun <sa9343@naver.com>
2023-01-25 00:06:45 +0900

    feat : 테스트 케이스 추가

--- Calculator/CalculatorTests/CalculatorTests.swift
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -18,7 +18,7 @@ final class CalculatorTests: XCTestCase {
         sut = nil
     }
 
-    //MARK: - isEmpty 메서드 테스트케이스
+    //MARK: - isEmpty 메서드 테스트 케이스
     func test_queue에_값이없을때_isEmpty를호출시_true가나온다() {
         //given
         sut?.clear()
@@ -29,7 +29,7 @@ final class CalculatorTests: XCTestCase {
         XCTAssertEqual(result, expectation)
     }
     
-    //MARK: - peek 메서드 테스트케이스
+    //MARK: - peek 메서드 테스트 케이스
     func test_peek을_실행할때_queue가_비어있다면_nil이_반환된다() {
         //given
         sut?.clear()
@@ -56,7 +56,7 @@ final class CalculatorTests: XCTestCase {
         XCTAssertEqual(result, expectation)
     }
     
-    //MARK: - enqueue 메서드 테스트케이스
+    //MARK: - enqueue 메서드 테스트 케이스
     func test_enqueue를_실행할때_파라미터로_준값이_queue에들어간다() {
         //given
         let element = 3
@@ -68,7 +68,7 @@ final class CalculatorTests: XCTestCase {
         XCTAssertEqual(result, expectation)
     }
     
-    //MARK: - size 메서드 테스트케이스
+    //MARK: - size 메서드 테스트 케이스
     func test_size를_실행할때_queue의_갯수가_반환된다() {
         //given
         let firstElement = 1
@@ -85,7 +85,7 @@ final class CalculatorTests: XCTestCase {
         XCTAssertEqual(result, expectation)
     }
     
-    //MARK: - dequeue 메서드 테스트케이스
+    //MARK: - dequeue 메서드 테스트 케이스
     func test_dequeue를_실행할때_첫번째요소가_삭제된다() {
         //given
         let firstElement = 1
@@ -129,5 +129,22 @@ final class CalculatorTests: XCTestCase {
         XCTAssertEqual(result, expectation)
     }
     
+    func test_dequeue를_실행할때_count가_1만큼_줄어든다() {
+        //given
+        let firstElement = 1
+        let secondElement = 2
+        let thirdElement = 3
+        //when
+        sut?.enqueue(firstElement)
+        sut?.enqueue(secondElement)
+        sut?.enqueue(thirdElement)
+        let previousCount = sut?.count
+        
+        sut?.dequeue()
+        let result = sut?.count
+        let expectation = previousCount! - 1
+        //then
+        XCTAssertEqual(result, expectation)
+    }
     
 }
