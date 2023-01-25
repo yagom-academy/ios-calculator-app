09936ab673a92f1e45b0500ee120a3817b6fe243
yijiye <jiyee4013@naver.com>
2023-01-25 17:19:44 +0900

    feat: CalculatorItemQueue 파일로 변경 및 UnitTest작성

--- Calculator/Calculator/Model/CalculatorItemQueue.swift
+++ Calculator/Calculator/Model/CalculatorItemQueue.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+struct CalculatorItemQueue<T: Equatable> {
+    var input: [T] = []
+    var output: [T] = []
+    
+    var isEmpty: Bool {
+        return input.isEmpty && output.isEmpty
+    }
+    
+    var count: Int {
+        return input.count + output.count
+    }
+    
+    mutating func enqueue(_ value: T) {
+        input.append(value)
+    }
+    
+    mutating func dequeue() -> T? {
+        if output.isEmpty {
+            output = input.reversed()
+            input.removeAll()
+        }
+        return output.removeLast()
+    }
+}
---
+++ Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
@@ -0,0 +1,95 @@
+//
+//  CalculatorItemQueueTests.swift
+//  CalculatorItemQueueTests
+//
+//  Created by jiye Yi on 2023/01/25.
+//
+
+import XCTest
+@testable import Calculator
+final class CalculatorItemQueueTests: XCTestCase {
+    var sut: CalculatorItemQueue<String>!
+
+    override func setUpWithError() throws {
+      try super.setUpWithError()
+        sut = CalculatorItemQueue()
+    }
+
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+        sut = nil
+    }
+    func test_배열이비어있을때_true를반환한다() {
+        sut.input = []
+        sut.output = []
+        let expectation = true
+        
+        let result = sut.isEmpty
+        
+        XCTAssertEqual(result, expectation)
+    }
+    func test_input_output_배열의수를반환한다() {
+        sut.input = ["1", "2"]
+        sut.output = ["2", "3"]
+        let expectation = 4
+        
+        let result = sut.count
+        XCTAssertEqual(result, expectation)
+    }
+    
+    func test_enqueue_를실행하면_input배열에값이입력된다() {
+        let input = "1"
+        let expectation = ["1"]
+        
+        let _ = sut.enqueue(input)
+        let inputArray = sut.input
+        
+        XCTAssertEqual(inputArray, expectation)
+    }
+    
+    func test_dequeue_를실행하면_output에값이있으면_output의_마지막값을_반환한다() {
+        sut.output = ["1"]
+        let result = sut.dequeue()
+        let expectation = "1"
+        
+        XCTAssertEqual(result, expectation)
+    }
+    
+    func test_dequeue_를실행하면_ouput에값이없으면_nil을반환한다() {
+        sut.output = []
+        let result = sut.dequeue()
+        
+        XCTAssertEqual(result, nil)
+    }
+    
+    func test_dequeue_output에값이없으면_input을reversed해서_output에넣어준다() {
+        sut.output = []
+        sut.input = ["1"]
+        let _ = sut.dequeue()
+        let result = sut.output
+        let expectation = sut.input.reversed() as [String]
+        
+        XCTAssertEqual(result, expectation)
+    }
+    
+    func test_dequeue_output에값이없으면_input을reversed해서넣어주고_마지막값을제거한다() {
+        sut.output = []
+        sut.input = ["1", "2"]
+        let _ = sut.dequeue()
+        let result = sut.output
+        let expectation = ["2"]
+        
+        XCTAssertEqual(result, expectation)
+    }
+    
+    func test_dequeue_실행시_output가빈배열이라면_기존의input의값을제거한다() {
+        sut.output = []
+        sut.input = ["1", "2", "="]
+        let _ = sut.dequeue()
+        let result = sut.input
+        
+        XCTAssertEqual(result, [])
+    }
+
+    
+}
