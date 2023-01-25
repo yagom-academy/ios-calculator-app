de00ee2833182faccd50f3b15b78b17715f26164
vetto <gzzjk159@naver.com>
2023-01-25 16:51:49 +0900

    feat: isEmpty 연산프로퍼티 구현 및 테스트

--- Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
+++ Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
@@ -14,4 +14,20 @@ final class CalculatorItemQueueTests: XCTestCase {
         sut = nil
     }
     
+    // isEmpty property test
+    func test_head가_nil일_때_isQueueEmpty호출하면_true이다() {
+        let result = sut.isEmpty
+        let expectation = true
+        
+        XCTAssertEqual(result, expectation)
+    }
+    
+    func test_CalculatorItemQueue에_enqueue를_하고_isQueueEmpty호출하면_false이다() {
+        sut.head = Node(data: "head")
+        
+        let result = sut.isEmpty
+        let expectation = false
+        
+        XCTAssertEqual(result, expectation)
+    }
 }
