ecdb41cd2018c553473f9e34b85c0aab58b62746
Andrew-0411 <whk1140@naver.com>
2023-01-25 19:00:12 +0900

    feat: testCase 생성 및 작성

--- Calculator/CalculatorTests/CalculatorTests.swift
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -2,24 +2,64 @@
 //  CalculatorTests.swift
 //  CalculatorTests
 //
-//  Created by Andrew on 2023/01/24.
+//  Created by Andrew on 2023/01/25.
 //
 
 import XCTest
 
 final class CalculatorTests: XCTestCase {
-
-    var sut: CalculatorItemQueue!
+    var sut: CalculatorItemQueue<Any>!
     
     override func setUpWithError() throws {
-        
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        sut = CalculatorItemQueue()
     }
 
     override func tearDownWithError() throws {
+        sut = nil
+    }
+    
+    func test_빈배열일경우_isEmpty에서_true값을반환하는함수() {
+        // given
+        sut.enqueue(element: 9)
+        sut.enqueue(element: 3)
+        sut.enqueue(element: 4)
+        sut.enqueue(element: 11)
+        // when
+        sut.clearQueue()
+        // then
+        XCTAssertTrue(sut.isEmpty())
+    }
+    
+    func test_빈배열에서_요소를삭제를시도할경우_nil값반환하는함수() {
+        // given
+        sut.enqueue(element: 9)
+        // when
+        sut.clearQueue()
+        // then
+        XCTAssertNil(sut.dequeue())
+    }
+    
+    func test_요소를삭제할때_첫번째요소가삭제되는지_확인하는함수() {
+        sut.enqueue(element: 9)
+        sut.enqueue(element: 3)
+        sut.enqueue(element: 4)
+        sut.enqueue(element: 11)
+        sut.dequeue()
+        let result = [3,4,11]
         
     }
     
-    
+    func test_queue에서_첫번째요소를찾는함수() {
+        // given
+        sut.enqueue(element: 9)
+        sut.enqueue(element: 3)
+        sut.enqueue(element: 4)
+        sut.enqueue(element: 11)
+        sut.dequeue()
+        // when
+        let result = 9
+        // then
+        XCTAssertEqual(result, sut.searchQueue())
+    }
 
 }
