85266d3ca9addb0baf158ee34889af45da11c961
kaki <ycg02109@naver.com>
2023-01-25 13:36:10 +0900

    feat: enqueue 메서드 추가 및 기능 테스트

--- Calculator/Calculator/Model/CalculatorItemQueue.swift
+++ Calculator/Calculator/Model/CalculatorItemQueue.swift
@@ -6,5 +6,33 @@
 //
 
 struct CalculatorItemQueue: CalculateItem {
+    private var head: Node?
+    private var tail: Node?
     
+    var isEmpty: Bool {
+        return head == nil
+    }
+    
+    var size: Int {
+        var count = 0
+        var current = head
+        while (current != nil) {
+            current = current?.next
+            count += 1
+        }
+        return count
+    }
+    
+    mutating func enqueue(data: CalculateItem) {
+        if head == nil || tail == nil {
+            head = Node.init(data)
+            tail = head
+            return
+        }
+        
+        let newNode = Node.init(data)
+        tail?.next = newNode
+        newNode.prev = tail
+        tail = newNode
+    }
 }
--- Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
+++ Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
@@ -9,11 +9,56 @@ import XCTest
 @testable import Calculator
 
 final class CalculatorItemQueueTests: XCTestCase {
+    var sut: CalculatorItemQueue!
 
     override func setUpWithError() throws {
+        try super.setUpWithError()
+        sut = CalculatorItemQueue()
     }
 
     override func tearDownWithError() throws {
+        try super.tearDownWithError()
+        sut = nil
+    }
+    
+    func test_비었을경우_isEmpty가_true이다() {
+        let result = sut.isEmpty
+        let expectation = true
+        
+        XCTAssertEqual(result, expectation)
+    }
+    
+    func test_비었을경우_size가_0이다() {
+        let result = sut.size
+        let expectation = 0
+        
+        XCTAssertEqual(result, expectation)
+    }
+    
+    func test_enqueue_됐을경우_isEmpty가_false이다() {
+        sut.enqueue(data: "111")
+        let result = sut.isEmpty
+        let expectation = false
+        
+        XCTAssertEqual(result, expectation)
+    }
+    
+    func test_한번_enqueue_됐을경우_size가_1이다() {
+        sut.enqueue(data: "111")
+        let result = sut.size
+        let expectation = 1
+        
+        XCTAssertEqual(result, expectation)
+    }
+    
+    func test_세번_enqueue_됐을경우_size가_3이다() {
+        sut.enqueue(data: "111")
+        sut.enqueue(data: "222")
+        sut.enqueue(data: "333")
+        let result = sut.size
+        let expectation = 3
+        
+        XCTAssertEqual(result, expectation)
     }
     
 }
