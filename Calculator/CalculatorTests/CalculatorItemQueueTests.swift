8c12f05122e10131b2f6f700e34f395743baee3f
HarryHyeon <hharry0728@gmail.com>
2023-01-25 17:36:46 +0900

    refactor: CalculatorItemQueueTests에서 CalculaterItem 프로토콜을 준수하는 타입으로 생성해주도록 수정

--- Calculator/CalculatorTests/CalculatorItemQueueTests.swift
+++ Calculator/CalculatorTests/CalculatorItemQueueTests.swift
@@ -8,10 +8,10 @@
 import XCTest
 
 final class CalculatorItemQueueTests: XCTestCase {
-    var sut: CalculaterItemQueue<CalculateItem>!
+    var sut: CalculaterItemQueue<Int>!
     
     override func setUpWithError() throws {
-        sut = CalculaterItemQueue<CalculateItem>(queue: LinkedList<CalculateItem>())
+        sut = CalculaterItemQueue<Int>(queue: LinkedList<Int>())
     }
 
     override func tearDownWithError() throws {
@@ -20,7 +20,7 @@ final class CalculatorItemQueueTests: XCTestCase {
     // MARK: - enqueue Test
     func test_enqueue호출시_queue의_count가_1증가한다() {
         // given
-        let element = "+"
+        let element = 1
         let expectation = sut.queue.count + 1
 
         // when
@@ -33,11 +33,11 @@ final class CalculatorItemQueueTests: XCTestCase {
     
     func test_enqueue호출시_파라미터로_전달한값과_queue에추가된value가같다() {
         // given
-        let expectation = "+"
+        let expectation = 1
 
         // when
-        sut.enqueue("+")
-        let result = sut.queue.tail?.value as! String
+        sut.enqueue(1)
+        let result = sut.queue.tail?.value
         
         // then
         XCTAssertEqual(result, expectation)
@@ -46,10 +46,10 @@ final class CalculatorItemQueueTests: XCTestCase {
     // MARK: - dequeue Test
     func test_dequeue호출시_queue의_count가0이면_nil반환한다() {
         let countExpectation = 0
-        let dequeueExpectation: String? = nil
+        let dequeueExpectation: Int? = nil
 
         let countResult = sut.queue.count
-        let dequeueResult = sut.dequeue() as? String
+        let dequeueResult = sut.dequeue()
 
         XCTAssertEqual(countResult, countExpectation)
         XCTAssertEqual(dequeueResult, dequeueExpectation)
@@ -57,13 +57,13 @@ final class CalculatorItemQueueTests: XCTestCase {
 
     func test_dequeue호출시_queue의_첫번쨰element를_제거하고_반환한다() {
         sut.queue.tail = Node(value: 3)
-        sut.queue.head = Node(value: 1, next: Node(value: "+", next: sut.queue.tail))
+        sut.queue.head = Node(value: 1, next: Node(value: 2, next: sut.queue.tail))
         sut.queue.count = 3
         
         let returnExpectation = 1
         let queueCountExpectation = 2
 
-        let dequeueReturn = sut.dequeue() as? Int
+        let dequeueReturn = sut.dequeue()
         let queueCount = sut.queue.count
 
         XCTAssertEqual(dequeueReturn, returnExpectation)
@@ -73,13 +73,13 @@ final class CalculatorItemQueueTests: XCTestCase {
     // MARK: - removeAll Test
     func test_removeAll호출시_queue의_head와_tail은_nil이다() {
         sut.queue.tail = Node(value: 3)
-        sut.queue.head = Node(value: 1, next: Node(value: "+", next: sut.queue.tail))
+        sut.queue.head = Node(value: 1, next: Node(value: 2, next: sut.queue.tail))
         let headExpectation: Int? = nil
         let tailExpectation: Int? = nil
         
         sut.removeAll()
-        let headValueResult = sut.queue.head?.value as? Int
-        let tailValueResult = sut.queue.tail?.value as? Int
+        let headValueResult = sut.queue.head?.value
+        let tailValueResult = sut.queue.tail?.value
         
         XCTAssertEqual(headValueResult, headExpectation)
         XCTAssertEqual(tailValueResult, tailExpectation)
