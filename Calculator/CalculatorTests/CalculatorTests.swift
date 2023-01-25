f756e8df5fa216bf5247529e1428dbcd222cd40d
parkmuri <smxhmu@naver.com>
2023-01-25 14:43:20 +0900

    feat: clearAll메서드 추가, test코드 메서드명 변경 및 순서변경

--- Calculator/CalculatorTests/CalculatorTests.swift
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -9,16 +9,17 @@ import XCTest
 @testable import Calculator
 
 final class CalculatorTests: XCTestCase {
-    var sut: CalculatorItemQueue<Any>!
+    var sut: CalculatorItemQueue<Int>!
     
     override func setUpWithError() throws {
-        sut = CalculatorItemQueue<Any>()
+        sut = CalculatorItemQueue<Int>()
     }
 
     override func tearDownWithError() throws {
         sut = nil
     }
     
+    //MARK: - enqueue test code
     func test_enqueue호출시_isEmpty는_False이다() {
         // given
         let input = 1
@@ -29,7 +30,6 @@ final class CalculatorTests: XCTestCase {
         
         // then
         XCTAssertFalse(expectation)
-        
     }
     
     func test_enqueue호출시_count가증가된다() {
@@ -45,6 +45,33 @@ final class CalculatorTests: XCTestCase {
         XCTAssertEqual(result, expectation)
     }
     
+    //MARK: - dequeue test code
+    func test_data배열에_값이하나인상태에서_dequeue호출시_count는_0이다() {
+        // given
+        sut.data = [1]
+        let expectation = 0
+        
+        // when
+        sut.dequeue()
+        let result = sut.count
+        
+        // then
+        XCTAssertEqual(result, expectation)
+    }
+    
+    func test_data배열에_값이하나인상태에서_dequeue호출시_빈배열을반환한다() {
+        // given
+        sut.data = [1]
+        let expectation: [Int] = []
+        
+        // when
+        sut.dequeue()
+        let result = sut.data
+        
+        // then
+        XCTAssertEqual(expectation, result)
+    }
+    
     func test_값이없는상태에서_dequeue호출시_반환값은_nil이다() {
         // when
         let result = sut.dequeue()
@@ -53,7 +80,7 @@ final class CalculatorTests: XCTestCase {
         XCTAssertNil(result)
     }
     
-    func test_data배열이존재하는상태에서_dequeue호출시_첫번째로들어온값이_삭제된다(){
+    func test_data배열값이_여러개존재하는상태에서_dequeue호출시_첫번째로들어온값이_삭제된다(){
         // given
         sut.data = [11, 22, 33, 44]
         let expectation = 11
@@ -62,9 +89,10 @@ final class CalculatorTests: XCTestCase {
         let result = sut.dequeue()
         
         // then
-        XCTAssertEqual(result as? Int, expectation)
+        XCTAssertEqual(result, expectation)
     }
     
+    //MARK: - clearAll test code
     func test_clearAll호출시_data배열값이_모두삭제되어_count가0이다(){
         // given
         sut.data = [1, 2, 3, 4, 5]
