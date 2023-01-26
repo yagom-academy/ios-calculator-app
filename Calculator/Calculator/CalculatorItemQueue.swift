471110baea228349861049f52bbac609fe3d6289
sehong <tjsgh0801@naver.com>
2023-01-26 18:54:06 +0900

    refactor: 1. 코드에 대한 간단한 설명 주석 추가 2. class, struct 첫번째 줄 마지막줄 띄우기 feat: Queue 타입 구현하기 1. 해당 리스트의 첫번째(head), 마지막(last)값을 알려줄 변수 선언 2. 해당 리스트의 빈 배열 여부 판단 할 수 있는 isEmpty 구현 3. 해당 리스트 마지막 부분에 값을 추가 해주는 enqueue 함수구현 4. 해당 리스트의 첫번째 값을 삭제 해주는 dequeue 함수구현 5. 해당 리스트의 모든 값을 삭제할 수 있는 removeall 함수구현

--- Calculator/Calculator/CalculatorItemQueue.swift
+++ Calculator/Calculator/CalculatorItemQueue.swift
@@ -4,13 +4,67 @@
 //
 //  Created by Seoyeon Hong on 2023/01/26.
 //
-
-class CalculatorItemQueue<T>: CalculateItem {
-    private var queue: Node<T>? = nil
+//MARK: Queue 타입 구현
+struct CalculatorItemQueue<T>: CalculateItem {
+    
+    private var head: Node<T>?
+    
+    var last: Node<T>? {
+        if head == nil {
+            return nil
+        }
+        var node = head
+        while node?.nextData != nil {
+            node = node?.nextData
+        }
+        return node
+    }
+    
+    var isEmpty: Bool {
+        if head == nil {
+            return true
+        }else {
+            return false
+        }
+    }
+    
+    mutating func enqueue(_ element: T) {
+        if let lastNode = last{
+            lastNode.nextData = Node(data: element)
+        }else {
+            head = Node(data: element)
+        }
+    }
+    
+    mutating func dequeue() -> T? {
+        if head == nil { return nil }
+        
+        if head?.nextData == nil {
+            head = nil
+            return head?.data
+        }
+        
+        var node = head
+        while node?.nextData?.nextData != nil {
+            node = node?.nextData
+        }
+        
+        return node?.data
+    }
+    
+    mutating func removeAll() {
+        guard head != nil else {
+            return
+        }
+        
+        head = nil
+    }
+    
 }
 
 //MARK: Node 타입 생성
 class Node<T> {
+    
     var data: T
     var nextData: Node?
     
@@ -18,4 +72,5 @@ class Node<T> {
         self.data = data
         self.nextData = nextData
     }
+    
 }
