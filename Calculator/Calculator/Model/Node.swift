0cec353bb28ad03648c136996f0d1ff672ca2cc7
minsu3007 <minsu3007@gmail.com>
2023-01-26 17:15:40 +0900

    Revert "refactor: Node타입 수정"
    This reverts commit 975683184009f78b47f6269032b362923928344f.

--- Calculator/Calculator/Model/Node.swift
+++ Calculator/Calculator/Model/Node.swift
@@ -1,11 +1,11 @@
 //  Node.swift
 //  Created by 레옹아범 on 2023/01/24.
 
-final class Node {
+final class Node<Element: CalculateItem> {
     var next: Node?
-    var value: CalculateItem
+    var value: Element
     
-    init(next: Node? = nil, value: CalculateItem) {
+    init(next: Node? = nil, value: Element) {
         self.next = next
         self.value = value
     }
