defd70e4ca3ccbdbf9f85cf308a4c0b8731e205a
seunghyunCheon <csh7215@naver.com>
2023-01-26 16:40:11 +0900

    chore: 테스트케이스 파일 dequeueCurrentElement -> dequeue 변수명 수정

--- Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
+++ Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
@@ -101,7 +101,7 @@ final class CalculatorItemQueueTests: XCTestCase {
         XCTAssertEqual(result, expectation)
     }
     
-    func test_dequeueCurrentItem호출시_dequeueStack이비어있다면_reversed된enqueueStack이들어간다() {
+    func test_dequeue호출시_dequeueStack이비어있다면_reversed된enqueueStack이들어간다() {
         // given
         let input = ["1", "+", "3"]
         let expectation = ["3", "+"]
@@ -116,7 +116,7 @@ final class CalculatorItemQueueTests: XCTestCase {
         XCTAssertEqual(result, expectation)
     }
     
-    func test_dequeueCurrentItem호출시_dequeueStack이비어있다면_enqueueStack이비워진다() {
+    func test_dequeue호출시_dequeueStack이비어있다면_enqueueStack이비워진다() {
         // given
         let input = ["1", "2", "3"]
         let expectation = true
@@ -130,7 +130,7 @@ final class CalculatorItemQueueTests: XCTestCase {
         XCTAssertEqual(result, expectation)
     }
     
-    func test_dequeueCurrentItem호출시_dequeueStack이비어있지않다면_dequeueStack의마지막값을반환한다() {
+    func test_dequeue호출시_dequeueStack이비어있지않다면_dequeueStack의마지막값을반환한다() {
         // given
         let input = ["1", "2", "3", "4", "5"]
         let expectation = "2"
