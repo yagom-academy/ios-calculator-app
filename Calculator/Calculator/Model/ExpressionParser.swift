5afdc17c4425a149b09432c36c9de97fff33ab7d
minsu3007 <minsu3007@gmail.com>
2023-01-26 21:59:18 +0900

    init: ExpressionParser 메소드 전역변수로 생성

--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -2,10 +2,10 @@
 //  Created by 레옹아범 on 2023/01/26.
 
 enum ExpressionParser {
-    func parse(from input: String) -> Formula {
+    static func parse(from input: String) -> Formula {
     }
     
-    private func componentsByOperators(from input: String) -> [String] {
+    private static func componentsByOperators(from input: String) -> [String] {
         
     }
 }
