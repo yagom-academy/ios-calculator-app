9b316f3dc97abe8ba5de3e345c087cb47aa7bc23
sehong <tjsgh0801@naver.com>
2023-01-26 17:28:15 +0900

    feat: CalculatorItemQueue에서 사용할 Node타입 생성

--- Calculator/Calculator/CalculatorItemQueue.swift
+++ Calculator/Calculator/CalculatorItemQueue.swift
@@ -4,3 +4,18 @@
 //
 //  Created by Seoyeon Hong on 2023/01/26.
 //
+
+class CalculatorItemQueue<T>: CalculateItem {
+    private var queue: Node<T>? = nil
+}
+
+//MARK: Node 타입 생성
+class Node<T> {
+    var data: T
+    var nextData: Node?
+    
+    init(data: T, nextData: Node? = nil) {
+        self.data = data
+        self.nextData = nextData
+    }
+}
