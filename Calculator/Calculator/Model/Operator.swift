e1b962613f7292ed44f7758e67156b4d972852e3
yijiye <jiyee4013@naver.com>
2023-01-27 10:34:50 +0900

    feat: enum Operator 구현

---
+++ Calculator/Calculator/Model/Operator.swift
@@ -0,0 +1,43 @@
+//
+//  Operator.swift
+//  Calculator
+//
+//  Created by jiye Yi on 2023/01/27.
+//
+
+enum Operator: Character, CaseIterable, CalculateItemProtocol {
+    case add = "+"
+    case substract = "-"
+    case divide = "%"
+    case multiply = "x"
+    
+    func calculate(lhs: Double, rhs: Double) -> Double {
+        switch self {
+        case .add:
+            return add(lhs: lhs, rhs: rhs)
+        case .substract:
+            return subtract(lhs: lhs, rhs: rhs)
+        case .divide:
+            return divide(lhs: lhs, rhs: rhs)
+        case .multiply:
+            return multiply(lhs: lhs, rhs: rhs)
+        }
+    }
+    
+    private func add(lhs: Double, rhs: Double) -> Double {
+        return lhs + rhs
+    }
+    
+    private func subtract(lhs: Double, rhs: Double) -> Double {
+        return lhs - rhs
+    }
+    
+    private func divide(lhs: Double, rhs: Double) -> Double {
+        return lhs / rhs
+    }
+    
+    private func multiply(lhs: Double, rhs: Double) -> Double {
+        return lhs * rhs
+    }
+}
+
