22db4b9cb2815335449426896cad9cb0af2df93e
vetto <gzzjk159@naver.com>
2023-01-27 15:26:00 +0900

    feat: StringExtension 구현 및 Operator 구현

---
+++ Calculator/Calculator/Model/Operator.swift
@@ -0,0 +1,30 @@
+//  Calculator - Operator.swift
+//  created by vetto on 2023/01/27
+
+enum Operator: Character, CaseIterable, CalculateItem {
+    case add = "+"
+    case subtract = "-"
+    case divide = "/"
+    case multiply = "*"
+    
+    func calculate(lhs: Double, rhs: Double) -> Double {
+        return 1
+    }
+    
+    func add(lhs: Double, rhs: Double) -> Double {
+        return lhs + rhs
+    }
+    
+    func subtract(lhs: Double, rhs: Double) -> Double {
+        return lhs - rhs
+    }
+    
+    func divide(lhs: Double, rhs: Double) -> Double? {
+        guard rhs != 0 else { return nil }
+        return lhs / rhs
+    }
+    
+    func multiply(lhs: Double, rhs: Double) -> Double {
+        return lhs * rhs
+    }
+}
