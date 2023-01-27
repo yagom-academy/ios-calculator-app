c0c43f730f171d5e1bce37ddc09af3f55fbbef5f
Rhode-park <jinah.park.16@gmail.com>
2023-01-27 17:45:18 +0900

    feat: Operator 열거형 구현

---
+++ Calculator/Calculator/Model/CalculatorError.swift
@@ -0,0 +1,12 @@
+//
+//  Error.swift
+//  Calculator
+//
+//  Created by Jinah Park on 2023/01/27.
+//
+
+import Foundation
+
+enum CalculatorError: String, Error {
+    case cannotDevideByZero = "You can not devide Double with 0.0"
+}
--- Calculator/Calculator/Model/Operator.swift
+++ Calculator/Calculator/Model/Operator.swift
@@ -13,33 +13,36 @@ enum Operator: Character, CaseIterable, CalculateItem {
     case devide = "/"
     case multiply = "*"
     
-    func calculate(lhs: Double, rhs: Double) -> Double {
+    func calculate(lhs: Double, rhs: Double) throws -> Double {
         switch self {
         case .add:
             return add(lhs: lhs, rhs: rhs)
         case .subtract:
             return subtract(lhs: lhs, rhs: rhs)
         case .devide:
-            return devide(lhs: lhs, rhs: rhs)
+            return try devide(lhs: lhs, rhs: rhs)
         case .multiply:
             return multiply(lhs: lhs, rhs: rhs)
         }
     }
     
     private func add(lhs: Double, rhs: Double) -> Double {
-        return 0.0
+        return lhs + rhs
     }
     
     private func subtract(lhs: Double, rhs: Double) -> Double {
-        return 0.0
+        return lhs - rhs
     }
     
-    private func devide(lhs: Double, rhs: Double) -> Double {
-        return 0.0
+    private func devide(lhs: Double, rhs: Double) throws -> Double {
+        guard rhs != 0 else {
+            throw CalculatorError.cannotDevideByZero
+        }
+        return lhs/rhs
     }
     
     private func multiply(lhs: Double, rhs: Double) -> Double {
-        return 0.0
+        return lhs * rhs
     }
     
 }
