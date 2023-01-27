46b975673e1f89d3c36fdf7b65880738cad8c039
ROWAN <kimkj93@naver.com>
2023-01-27 11:17:49 +0900

    feat: Operator 열거형 정의

--- Calculator/Calculator/Model/Operator.swift
+++ Calculator/Calculator/Model/Operator.swift
@@ -5,4 +5,46 @@
 //  Created by Rowan on 2023/01/27.
 //
 
-import Foundation
+enum Operator: Character, CaseIterable {
+    case add = "+"
+    case subtract = "-"
+    case divide = "/"
+    case multiply = "*"
+    
+    func calculate(lhs: Double, rhs: Double) -> Double {
+        switch self {
+        case .add:
+            return add(lhs: lhs, rhs: rhs)
+        case .subtract:
+            return subtract(lhs: lhs, rhs: rhs)
+        case .divide:
+            return divide(lhs: lhs, rhs: rhs)
+        case .multiply:
+            return multiply(lhs: lhs, rhs: rhs)
+        }
+    }
+    
+    private func add(lhs: Double, rhs: Double) -> Double {
+        let result = lhs + rhs
+        
+        return result
+    }
+    
+    private func subtract(lhs: Double, rhs: Double) -> Double {
+        let result = lhs - rhs
+        
+        return result
+    }
+    
+    private func divide(lhs: Double, rhs: Double) -> Double {
+        let result = lhs / rhs
+        
+        return result
+    }
+    
+    private func multiply(lhs: Double, rhs: Double) -> Double {
+        let result = lhs * rhs
+        
+        return result
+    }
+}
