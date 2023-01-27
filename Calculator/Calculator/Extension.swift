3d292c6d8505da20194c81757e02df62175ea09a
seunghyunCheon <csh7215@naver.com>
2023-01-27 16:28:36 +0900

    feat: String extension정의

---
+++ Calculator/Calculator/Extension.swift
@@ -0,0 +1,14 @@
+//
+//  Extension.swift
+//  Calculator
+//
+//  Created by 천승현 on 2023/01/27.
+//
+
+import Foundation
+
+extension String {
+    func split(with target: Character) -> [String] {
+        return [""]
+    }
+}
