6bf50a95320c4a3ab29368e4ac4a02fc4ed2376e
yijiye <jiyee4013@naver.com>
2023-01-27 12:29:16 +0900

    feat: CalculatorError 구현 및 나누기 오류 처리, 파일분리

---
+++ Calculator/Calculator/Model/Enumeration/CalculatorError.swift
@@ -0,0 +1,10 @@
+//
+//  CalculatorError.swift
+//  Calculator
+//
+//  Created by jiye Yi on 2023/01/27.
+//
+
+enum CalculatorError: Error {
+    case divideError
+}
--- Calculator/Calculator/Model/Enumeration/Operator.swift
+++ Calculator/Calculator/Model/Enumeration/Operator.swift
@@ -12,16 +12,24 @@ enum Operator: Character, CaseIterable, CalculateItemProtocol {
     case multiply = "x"
     
     func calculate(lhs: Double, rhs: Double) -> Double {
+        var result: Double = lhs
         switch self {
         case .add:
             return add(lhs: lhs, rhs: rhs)
         case .subtract:
             return subtract(lhs: lhs, rhs: rhs)
         case .divide:
-            return divide(lhs: lhs, rhs: rhs)
+            do {
+                return try divide(lhs: lhs, rhs: rhs)
+            } catch CalculatorError.divideError {
+                print("NaN")
+            } catch {
+                print(error)
+            }
         case .multiply:
             return multiply(lhs: lhs, rhs: rhs)
         }
+        return result
     }
     
     private func add(lhs: Double, rhs: Double) -> Double {
@@ -32,8 +40,11 @@ enum Operator: Character, CaseIterable, CalculateItemProtocol {
         return lhs - rhs
     }
     
-    private func divide(lhs: Double, rhs: Double) -> Double {
-            return lhs / rhs
+    private func divide(lhs: Double, rhs: Double) throws -> Double {
+        if rhs == Double.zero {
+            throw CalculatorError.divideError
+        }
+        return lhs / rhs
     }
     
     private func multiply(lhs: Double, rhs: Double) -> Double {
