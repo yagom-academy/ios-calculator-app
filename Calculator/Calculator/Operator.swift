fd8c0760294664b7ff186b5ad88c5df2ce87cbd1
seunghyunCheon <csh7215@naver.com>
2023-01-27 16:27:35 +0900

    feat: Operater enum정의

---
+++ Calculator/Calculator/Operator.swift
@@ -0,0 +1,35 @@
+//
+//  Operator.swift
+//  Calculator
+//
+//  Created by 천승현 on 2023/01/27.
+//
+
+import Foundation
+
+enum Operator: Character, CaseIterable {
+    case add = "+"
+    case subtract = "-"
+    case divide = "/"
+    case multiply = "*"
+    
+    func calculate(lhs: Double, rhs: Double) -> Double {
+        return 0.0
+    }
+    
+    private func add(lhs: Double, rhs: Double) -> Double {
+        return 0.0
+    }
+    
+    private func subtract(lhs: Double, rhs: Double) -> Double {
+        return 0.0
+    }
+    
+    private func divide(lhs: Double, rhs: Double) -> Double {
+        return 0.0
+    }
+    
+    private func multiply(lhs: Double, rhs: Double) -> Double {
+        return 0.0
+    }
+}
