cdcd67f56a3eff79276db90db31e0c079e8d62c3
Jun <59365211+juun97@users.noreply.github.com>
2023-01-27 19:26:16 +0900

    feat: Operator 내부 구현 및 테스트 진행

---
+++ Calculator/Calculator/Test/OperatorTest/OperatorTest.swift
@@ -0,0 +1,76 @@
+//
+//  OperatorTest.swift
+//  OperatorTest
+//
+//  Created by 김성준 on 2023/01/27.
+//
+
+import XCTest
+
+final class OperatorTest: XCTestCase {
+
+    override func setUpWithError() throws {
+     
+    }
+
+    override func tearDownWithError() throws {
+        
+    }
+
+    func test_operator가_add일때_calculatet를_실행할시_덧셈의연산을한다() {
+        //give
+        let a = 10.0
+        let b = 5.0
+        let operators = Operator.add
+        
+        //when
+        let result = operators.calculate(lhs: a, rhs: b)
+        let expectation = 15.0
+       
+        //then
+        XCTAssertEqual(result, expectation)
+    }
+    
+    func test_operator가_subtract일때_calculatet를_실행할시_뺄셈의연산을한다() {
+        //give
+        let a = 10.0
+        let b = 5.0
+        let operators = Operator.subtract
+        
+        //when
+        let result = operators.calculate(lhs: a, rhs: b)
+        let expectation = 5.0
+        
+        //then
+        XCTAssertEqual(result, expectation)
+    }
+    
+    func test_operator가_divide일때_calculatet를_실행할시_나눗셈의연산을한다() {
+        //give
+        let a = 10.0
+        let b = 5.0
+        let operators = Operator.divide
+        
+        //when
+        let result = operators.calculate(lhs: a, rhs: b)
+        let expectation = 2.0
+  
+        //then
+        XCTAssertEqual(result, expectation)
+    }
+    
+    func test_operator가_multiply일때_calculatet를_실행할시_곱셈의연산을한다() {
+        //give
+        let a = 10.0
+        let b = 5.0
+        let operators = Operator.multiply
+        
+        //when
+        let result = operators.calculate(lhs: a, rhs: b)
+        let expectation = 50.0
+   
+        //then
+        XCTAssertEqual(result, expectation)
+    }
+
+}
