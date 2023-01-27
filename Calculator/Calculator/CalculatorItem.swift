1b2bb298bff5753b2c31b11bcebaca2a37d58fba
sehong <tjsgh0801@naver.com>
2023-01-28 02:02:32 +0900

    feat: 1. CalculatorTest 파일 생성 및 extenstion int 추가 2. DoubleStack Queue타입으로 변경 3. Queue타입 내에 queue가 비어있는지 확인 할 수 있는 isEmpty 프로퍼티 생성 및 isEmpty & isNotEmpty 테스트 진행 4. Queue타입 내에 queue가 가지고 있는 첫번째 값을 출력하는 peek 프로퍼티 생성 5. Queue타입 내에 queue에 값을 추가할 수 있는 enqueue 함수 생성 및 enqueue함수 테스트 진행 6. Queue타입 내에 queue에 값을 삭제할 수 있는 dequeue 함수 생성 및 dequeue함수 테스트 진행
    chore:
    node파일 삭제(queue 구현방식 변경)
    코드 컨벤션 수정
    test파일 각각 given,when,then 주석추가

--- Calculator/Calculator/CalculatorItem.swift
+++ Calculator/Calculator/CalculatorItem.swift
@@ -5,5 +5,4 @@
 //  Created by Seoyeon Hong on 2023/01/26.
 //
 //MARK: 빈 프로토콜 생성
-protocol CalculateItem {
-}
+protocol CalculateItem {}
--- Calculator/CalculatorTests/CalculatorTests.swift
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -21,102 +21,70 @@ final class CalculatorTests: XCTestCase {
         sut = nil
     }
     
-    func test_head접근시_첫번째노드를반환한다() {
+    func test_enqueue실행시_처음_값을_넣어주면_처음넣은값이_출력된다() {
+        //given
+        let fisrt = sut.peek
+
+        //when
+        sut.enqueue(1)
+        
+        //then
+        XCTAssertEqual(fisrt, 1)
+    }
+    
+    func test_enqueue실행시_값을_여러번_넣었을때_첫번째값이_출력된다() {
+        //given
+        let fisrt = sut.peek
+        
+        //when
         sut.enqueue(1)
         sut.enqueue(2)
         
-        let expectation = 1
-        let result = sut.head?.data
-        
-        XCTAssertEqual(expectation, result)
+        //then
+        XCTAssertEqual(fisrt, 1)
     }
     
-    func test_last접근할때_마지막노드를_반환한다() {
+    func test_dequeue실행시_첫번째로넣어줬던값이_삭제된다() {
+        //given
+        let item = sut.dequeue()
+        
+        //when
+        sut.enqueue(1)
+        
+        //then
+        XCTAssertEqual(item, 1)
+    }
+    
+    func test_값을_여러번넣고_dequeue실행시_첫번째로넣었던값이_삭제되고_두번째가남는다() {
+        //given
+        let first = sut.peek
+        
+        //when
         sut.enqueue(1)
         sut.enqueue(2)
-        
-        let expectation = 2
-        let result = sut.last?.data
-        
-        XCTAssertEqual(expectation, result)
-    }
-    
-    func test_isEmpty접근할때_요소가_있으면_Bool을반환한다() {
-        sut.enqueue(1)
-        
-        let result = sut.isEmpty
-        
-        XCTAssertFalse(result)
-    }
-    
-    func test_isEmpty접근할때_요소가_없으면_true를반환한다() {
-        let result = sut.isEmpty
-        
-        XCTAssertTrue(result)
-    }
-    
-    func test_enqueue실행시_입력된_값이_첫번째노드_값으로_생성이된다() {
-        let input = 1
-        let expectation = 1
-        
-        sut.enqueue(input)
-        
-        XCTAssertEqual(input, expectation)
-    }
-    
-    func test_enqueue실행시_첫번째값_뒤에_두번째노드가_생성이된다() {
-        let expectation = 3
-        sut.enqueue(1)
-        sut.enqueue(2)
-        sut.enqueue(3)
-        
-        let result = sut.last?.data
-        
-        XCTAssertEqual(expectation, result)
-    }
-    
-    func test_dequeue실행시_값이없으면_nil이_반환된다() {
-        let result = sut.dequeue()
-        XCTAssertEqual(nil, result)
-    }
-    
-    func test_dequeue실행시_첫번째값이_삭제된다() {
-        sut.enqueue(1)
-        sut.enqueue(2)
-        sut.enqueue(3)
-        
-        let expectation = 1
-        let result = sut.dequeue()
-        
-        XCTAssertEqual(expectation, result)
-    }
-    
-    func test_dequeue실행후_헤드값이_다음노드로_변경된다() {
-        let expectation = 2
-        
-        sut.enqueue(1)
-        sut.enqueue(2)
-        sut.enqueue(3)
         sut.dequeue()
         
-        let result = sut.head?.data
-        
-        XCTAssertEqual(expectation, result)
+        //then
+        XCTAssertEqual(first, 2)
     }
     
-    func test_removeAll실행시_모든값이_비워진다() {
+    func test_IsEmpty() {
+        //when
         sut.enqueue(1)
-        sut.enqueue(2)
-        sut.enqueue(3)
+        sut.dequeue()
         
-        sut.removeAll()
+        //then
         XCTAssertTrue(sut.isEmpty)
     }
     
-    func test_removeAll실행시_값이_없으면_종료된다() {
-        sut.removeAll()
+    func test_IsNotEmpty() {
+        //when
+        sut.enqueue(1)
         
-        XCTAssert(sut.isEmpty)
+        //then
+        XCTAssertFalse(sut.isEmpty)
     }
-
+    
 }
+
+extension Int: CalculateItem {}
