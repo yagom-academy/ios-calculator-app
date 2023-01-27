1766634ca92cedd2e295cc833429afdbc9266627
vetto <gzzjk159@naver.com>
2023-01-27 15:30:59 +0900

    refactor: operator 함수 접근 제어자 설정

--- Calculator/Calculator/Model/Operator.swift
+++ Calculator/Calculator/Model/Operator.swift
@@ -11,20 +11,20 @@ enum Operator: Character, CaseIterable, CalculateItem {
         return 1
     }
     
-    func add(lhs: Double, rhs: Double) -> Double {
+    private func add(lhs: Double, rhs: Double) -> Double {
         return lhs + rhs
     }
     
-    func subtract(lhs: Double, rhs: Double) -> Double {
+    private func subtract(lhs: Double, rhs: Double) -> Double {
         return lhs - rhs
     }
     
-    func divide(lhs: Double, rhs: Double) -> Double? {
+    private func divide(lhs: Double, rhs: Double) -> Double? {
         guard rhs != 0 else { return nil }
         return lhs / rhs
     }
     
-    func multiply(lhs: Double, rhs: Double) -> Double {
+    private func multiply(lhs: Double, rhs: Double) -> Double {
         return lhs * rhs
     }
 }
