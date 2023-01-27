1f0e9d8dc6c43c854093f5e830ea83ae64548f6f
hyemory <cjstksla73@gmail.com>
2023-01-27 19:21:58 +0900

    feat: Operator 기본 기능 구현

--- Calculator/Calculator/Model/extensions.swift
+++ Calculator/Calculator/Model/extensions.swift
@@ -1,10 +1,14 @@
 //
-//  String+CalculatorItem.swift
+//  extensions.swift
 //  Calculator
 //
 //  Created by 혜모리 on 2023/01/27.
 //
 
 extension String: CalculateItem {
+    
+}
+
+extension Double: CalculateItem {
     
 }
