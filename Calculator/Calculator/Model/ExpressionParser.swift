7413abb3ddca7b5be966c2af8a38b41dfb0edbb2
ROWAN <kimkj93@naver.com>
2023-01-28 23:28:45 +0900

    chore: currentFormula 변수 명 formula로 변경

--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -8,17 +8,17 @@
 enum ExpressionParser {
     static func parse(from input: String) -> Formula {
         let components = self.componentsByOperators(from: input)
-        var currentFormula = Formula()
+        var formula = Formula()
         
         for member in components {
             if let operand = Double(member) {
-                currentFormula.operands.enqueue(operand)
+                formula.operands.enqueue(operand)
             } else if let `operator` = Operator(rawValue: Character(member)) {
-                currentFormula.operators.enqueue(`operator`)
+                formula.operators.enqueue(`operator`)
             }
         }
 
-        return currentFormula
+        return formula
     }
 
     static private func componentsByOperators(from input: String) -> [String] {
