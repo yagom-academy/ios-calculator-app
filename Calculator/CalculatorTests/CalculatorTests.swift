34ecd990368b7405f4b98a33906ca6bbfc79603f
Andrew-0411 <whk1140@naver.com>
2023-01-30 08:11:46 +0900

    feat: testCase clearQueue, dequeue 테스트 추가

--- Calculator/CalculatorTests/CalculatorTests.swift
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -31,6 +31,22 @@ final class CalculatorTests: XCTestCase {
         XCTAssertEqual(input, sut.enqueueStack)
     }
     
+    func test_처음Queue를생성하고_isEmpty를호출할경우_true를확인하는함수() {
+        // then
+        XCTAssertTrue(sut.isEmpty())
+    }
+    
+    func test_clearQueue를호출해서값을다지우고_isEmpty를호출할경우_true를확인하는함수() {
+        // given
+        sut.enqueue(element: 4)
+        sut.enqueue(element: 11)
+        
+        // when
+        sut.clearQueue()
+        
+        //then
+        XCTAssertTrue(sut.isEmpty())
+    }
 
     
     func test_queue에요소가들어있는상태에서_isEmpty를호출할경우_false를확인하는함수() {
@@ -43,6 +59,31 @@ final class CalculatorTests: XCTestCase {
         XCTAssertFalse(sut.isEmpty())
     }
     
+    func test_dequeue호출시_Queue에값이존재한다면_값중에첫번째값을반환하는지_확인하는함수() {
+        // given
+        sut.enqueue(element: 9)
+        sut.enqueue(element: 3)
+        sut.enqueue(element: 4)
+        sut.enqueue(element: 11)
+        
+        // when
+        let result = sut.dequeue()
+        let firstElement = 9
+        
+        // then
+        XCTAssertEqual(result, firstElement)
+    }
+    
+    func test_dequeue호출시_Queue에값이존재하지않는다면_nil을반환하는지_확인하는함수() {
+        // given
+        sut.clearQueue()
+        
+        // when
+        let result = sut.dequeue()
+        
+        // then
+        XCTAssertNil(result)
+    }
 
 
 }
