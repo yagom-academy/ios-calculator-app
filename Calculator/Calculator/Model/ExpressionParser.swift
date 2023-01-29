f24c2631fa9137b7f915a151e6c28268529c8d94
kimseongj <ksj8277@naver.com>
2023-01-29 22:24:48 +0900

    feat: ExpressionParser에서 parse메서드 생성"

--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -9,17 +9,30 @@ import Foundation
 
 enum ExpressionParser {
     func parse(from input: String) -> Formula {
-        
         var operandsQueue = CalculatorItemQueue<Double>()
         var operatorsQueue = CalculatorItemQueue<Operator>()
         
-        return Formula(operands: operandsQueue, operators: operatorsQueue)
+        
+        let separatedOperators = input.compactMap { Operator(rawValue: $0) }
+       separatedOperators.forEach { `operator` in
+            operatorsQueue.enqueue(item: `operator`)
+        }
+       
+        let separatedOperands = componentsByOperators(from: input).compactMap { Double($0) }
+            separatedOperands.forEach { operand in
+            operandsQueue.enqueue(item: operand)
+        }
+        
+        var currentFomula = Formula()
+        currentFomula.operands = operandsQueue
+        currentFomula.operators = operatorsQueue
+        return currentFomula
     }
-//    
+    
     func componentsByOperators(from input: String) -> [String] {
         var numbers: [String] = [input]
-        Operator.allCases.forEach { oper in
-            numbers = numbers.map { $0.split(with: oper.rawValue) }.flatMap { $0 }
+        Operator.allCases.forEach { `operator` in
+            numbers = numbers.map { $0.split(with: `operator`.rawValue) }.flatMap { $0 }
         }
         return numbers
     }
