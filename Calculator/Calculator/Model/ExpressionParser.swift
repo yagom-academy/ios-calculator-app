70777797f8dbfa1092d8af27f0d9a7a80dbeee42
vetto <gzzjk159@naver.com>
2023-01-29 15:44:51 +0900

    refactor: closure style convention 수정

--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -6,18 +6,18 @@ enum ExpressionParser {
     static func parse(from input: String) -> Formula {
         var operandQueue = CalculatorItemQueue<Double>()
         var operatorQueue = CalculatorItemQueue<Operator>()
-        let operandsArray = componentsByOperators(from: input).compactMap{ Double($0) }
+        let operandsArray = componentsByOperators(from: input).compactMap { Double($0) }
         let operatorsArray = input.split(with: " ")
-                                .filter{ $0.count == 1 }
-                                .compactMap{ Operator(rawValue: Character($0)) }
+                                .filter { $0.count == 1 }
+                                .compactMap { Operator(rawValue: Character($0)) }
         
-        operandsArray.forEach{ operandQueue.enqueue($0) }
-        operatorsArray.forEach{ operatorQueue.enqueue($0) }
+        operandsArray.forEach { operandQueue.enqueue($0) }
+        operatorsArray.forEach { operatorQueue.enqueue($0) }
         
         return Formula(operands: operandQueue, operators: operatorQueue)
     }
     
     private static func componentsByOperators(from input: String) -> [String] {
-        return input.split(with: " ").compactMap { Double($0) }.map{ String($0) }
+        return input.split(with: " ").compactMap { Double($0) }.map { String($0) }
     }
 }
