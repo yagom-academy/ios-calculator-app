38c2fa1a2318626c2ca6824cafad3c4a88cc8995
vetto <gzzjk159@naver.com>
2023-01-31 03:48:46 +0900

    refactor: Formula init 추가

--- Calculator/Calculator/Model/Formula.swift
+++ Calculator/Calculator/Model/Formula.swift
@@ -5,6 +5,11 @@ struct Formula {
     private var operands: CalculatorItemQueue<Double>
     private var operators: CalculatorItemQueue<Operator>
     
+    init(operands: CalculatorItemQueue<Double>, operators: CalculatorItemQueue<Operator>) {
+        self.operands = operands
+        self.operators = operators
+    }
+    
     mutating func result() -> Double {
         
         guard var result = operands.dequeue() else { return 0 }
