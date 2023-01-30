70442892661da36f89486914599eeef9e790c649
ROWAN <kimkj93@naver.com>
2023-01-31 01:11:36 +0900

    refactor: delete unnecessary IBOutlets

--- Calculator/Calculator/Controller/MainViewController.swift
+++ Calculator/Calculator/Controller/MainViewController.swift
@@ -10,18 +10,6 @@ final class MainViewController: UIViewController {
     @IBOutlet weak var operatorLabel: UILabel!
     @IBOutlet weak var operandLabel: UILabel!
     
-    @IBOutlet weak var zero: UIButton!
-    @IBOutlet weak var zeroDouble: UIButton!
-    @IBOutlet weak var one: UIButton!
-    @IBOutlet weak var two: UIButton!
-    @IBOutlet weak var three: UIButton!
-    @IBOutlet weak var four: UIButton!
-    @IBOutlet weak var five: UIButton!
-    @IBOutlet weak var six: UIButton!
-    @IBOutlet weak var seven: UIButton!
-    @IBOutlet weak var eight: UIButton!
-    @IBOutlet weak var nine: UIButton!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         initializeLabels()
