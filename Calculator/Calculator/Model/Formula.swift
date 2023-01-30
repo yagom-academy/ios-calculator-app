86f534cc8f32316a5e466640ce8b4b0c70cef188
kkuldak1@naver.com <kkuldak1@naver.com>
2023-01-30 13:06:46 +0900

    feat: Formula 구현

--- Calculator/Calculator/Model/Formula.swift
+++ Calculator/Calculator/Model/Formula.swift
@@ -9,4 +9,26 @@ struct Formula<T: CalculateItem> {
     var operands = CalculatorItemQueue<T>()
     var operators = CalculatorItemQueue<T>()
     
+    mutating func result() -> Double {
+        
+        guard let firstData = operands.dequeueCalculateItems()?.data as? String,
+              var result = Double(firstData) else {
+            return 0.0
+        }
+        
+        while let dequeuedOperand = operands.dequeueCalculateItems()?.data as? String,
+              let operandData = Double(dequeuedOperand),
+              let dequeuedOperator = operators.dequeueCalculateItems()?.data as? String,
+              let operatorData = dequeuedOperator.first {
+
+            let operatorInstance = Operator(rawValue: operatorData)
+            do {
+                result = try operatorInstance!.calculate(lhs: result, rhs: operandData)
+            } catch {
+
+            }
+        }
+
+        return result
+    }
 }
