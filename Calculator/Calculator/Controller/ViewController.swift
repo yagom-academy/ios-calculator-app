2dc0e33a9e619f91ba3c0bb5e58a222bcffade99
minsu3007 <minsu3007@gmail.com>
2023-01-31 23:16:13 +0900

    init: 버튼별 @IBAction메소드 생성

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -6,44 +6,59 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    private var isPositive: Bool = true
-    
+    private let numberFormatter = NumberFormatter()
     private var currentNumber: String = "" {
-        didSet {
-            currentNumberLabel.text = currentNumber
-        }
         willSet {
-            if currentNumber.count > 20 {
-            }
+            guard let value = Double(newValue),
+                  let labelText = self.numberFormatter.string(from: NSNumber(floatLiteral: value)) else { return }
+            
+            self.currentNumberLabel.text = labelText
         }
     }
     
-
     @IBOutlet private weak var currentNumberLabel: UILabel!
     @IBOutlet private weak var currentOperatorLabel: UILabel!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setNumberFormatter()
+    }
+    
+    func setNumberFormatter() {
+        self.numberFormatter.numberStyle = .decimal
+    }
+    
+    func resetCurrentNumberLabel() {
+        self.currentNumber = ""
+        self.currentNumberLabel.text = "0"
     }
 
     @IBAction private func didTapNumberButton(sender: UIButton) {
         guard let buttonTitle = sender.currentTitle else { return }
         
-        switch buttonTitle {
-        case "⁺⁄₋":
-            if isPositive {
-                self.currentNumber = "-" + self.currentNumber
-                isPositive.toggle()
-            } else {
-                self.currentNumber = self.currentNumber.replacingOccurrences(of: "-", with: "")
-                isPositive.toggle()
-            }
-        case ".":
-            self.currentNumber += "."
-        default:
-            self.currentNumber += buttonTitle
-        }
+        self.currentNumber += buttonTitle
+    }
+    
+    @IBAction private func didTapOperatorButton(sender: UIButton) {
+        guard let buttonTitle = sender.currentTitle else { return }
+        
+        self.currentOperatorLabel.text = buttonTitle
+        
+        resetCurrentNumberLabel()
+    }
+    
+    @IBAction private func didTapClearButton(sender: UIButton) {
+        resetCurrentNumberLabel()
+    }
+    
+    @IBAction private func didTapAllClearButton(sender: UIButton) {
+        
+    }
+    
+    @IBAction private func didTapCalculateButton(sender: UIButton) {
+        
     }
 }
 
