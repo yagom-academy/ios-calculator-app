ef2aed322870d4e19d1dfbc212e801ee054ecd92
whdghks8654 <62684851+whdghks8654@users.noreply.github.com>
2023-02-01 02:25:12 +0900

    feat: getInputtedNumber, getInputtedOperator 매서드 구현

--- Calculator/Calculator/Contorller/ViewController.swift
+++ Calculator/Calculator/Contorller/ViewController.swift
@@ -8,9 +8,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    //입력하고있는 상태의 필드숫자들
-    var currentInputNumber: String = ""
-    var currentInputOperator: String = ""
+    var scrollviewComponentsArray: [String] = []
     
     var inputtedComponents: String = "" // 데이터를 저장할 스트링
     
@@ -19,6 +17,9 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var currentOperatorOnField: UILabel!
         
+    @IBOutlet weak var firstScrollViewLabel: UILabel!
+    
+    @IBOutlet weak var secondScrollViewLabel: UILabel!
     
     //    ⁺⁄₋ 버튼은 현재 입력한 숫자의 부호를 변환합니다.
 //
@@ -27,20 +28,22 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        currentInputtedNumberOnField.text = "0" //시작 넘버
+        currentOperatorOnField.text = "+" //시작 오퍼레이터
         setUpComponentsOnField()
     }
     
     
     func setUpComponentsOnField() {
-        currentOperatorOnField.text = "+" //시작 오퍼레이터
-        currentInputtedNumberOnField.text = currentInputNumber
+        
+       
     }
     //AC는 모든 연산내역을 초기화합니다.
     //    2+3-4+5 를 입력하고 AC를 누르면 모든 수식이 초기화되고 0의 상태가 됩니다.
     //    2+3-4+5= 을 입력하고 AC를 누르면 모든 수식이 초기화되고 0의 상태가 됩니다.
     
     @IBAction func allClearButtonTapped(_ sender: Any) {
-        self.currentInputNumber = "0"
         self.currentInputtedNumberOnField.text = "0"
         var queueStack = CalculatorItemQueue<String>()
         queueStack.removeAll()
@@ -51,7 +54,7 @@ class ViewController: UIViewController {
     //    2+3-4+ 를 입력하고 CE를 누르면 2+3-4+의 상태가 됩니다.
     //    2+3-4+5= 을 입력하고 CE를 누르면 0의 상태가 됩니다.
     @IBAction func clearEntryButtonTapped(_ sender: Any) {
-        self.currentInputNumber = ""
+
     }
    
     @IBAction func changeOperatorButtonTapped(_ sender: Any) {
@@ -62,43 +65,86 @@ class ViewController: UIViewController {
         ExpressionParser.parse(from: inputtedComponents)
     }
     
-    @IBAction func numberButtonTapped(_ sender: UIButton) {
-        
-        switch sender.tag {
-        case 1: currentInputNumber = "1"
-        case 2: currentInputNumber = "2"
-        case 3: currentInputNumber = "3"
-        case 4: currentInputNumber = "4"
-        case 5: currentInputNumber  = "5"
-        case 6: currentInputNumber  = "6"
-        case 7: currentInputNumber  = "7"
-        case 8: currentInputNumber  = "8"
-        case 9: currentInputNumber  = "9"
-        case 0: currentInputNumber  = "0"
-        case 100: currentInputNumber  = "00"
-        default:
-            break
-        }
-        inputtedComponents += currentInputNumber //현재 데이터에 저장
-        setUpComponentsOnField()
-    }
     
-    @IBAction func operatorButtonTapped(_ sender: UIButton) {
+    
+    //버트탭하면 가져온 입력값을 바탕으로 배열에 넣어주기
+    @IBAction func NumAndOperButtonTapped(_ sender: UIButton) {
+        currentOperatorOnField.text = ""
+        
+        var currentNum = getInputtedOperator(sender)
+        var currentOper = getInputtedNumber(sender)
+        
+        //현재 오퍼레이터를 스크롤뷰어레이에 넣음
+        scrollviewComponentsArray.append(currentOper + currentNum)
+        }
+    
+    //숫자 입력값가져오기
+    func getInputtedNumber(_ sender: UIButton) -> String {
+        
+        var currentOperator: String = ""
         
         switch sender.titleLabel?.text {
-        case "+": currentInputOperator = "+"
-        case "−": currentInputOperator = "-" // -값 다름 case부분에 적용된건 가짜 minus
-        case "÷": currentInputOperator = "/"
-        case "×": currentInputOperator = "*"
+        case "+": currentOperator = "+"
+        case "−": currentOperator = "−"
+        case "÷": currentOperator = "÷"
+        case "×": currentOperator = "×"
         default:
             break
         }
-        inputtedComponents += currentInputOperator  // 현재 데이터값에 저장
-        currentOperatorOnField.text = currentInputOperator // 오퍼레이터 현재상태 표시
-        setUpComponentsOnField()
+        
+        currentOperatorOnField.text = currentOperator
+        inputtedComponents += currentOperator  // 데이터값에 저장
+        
+        return currentOperator
     }
     
+    //연산자 입력값 가져오기
+    func getInputtedOperator(_ sender: UIButton) -> String {
+        
+        var currentInputNumber: String = ""
 
+        switch sender.tag {
+        case 1: currentInputNumber += "1"
+        case 2: currentInputNumber += "2"
+        case 3: currentInputNumber += "3"
+        case 4: currentInputNumber += "4"
+        case 5: currentInputNumber += "5"
+        case 6: currentInputNumber += "6"
+        case 7: currentInputNumber += "7"
+        case 8: currentInputNumber += "8"
+        case 9: currentInputNumber += "9"
+        case 0: currentInputNumber += "0"
+        case 100: currentInputNumber += "00"
+        default: return ""
+        }
+        
+        return currentInputNumber
+    }
+        
+    
+    func convertOperator(_ sender: UIButton) -> String {
+        
+        guard let titleLabel = sender.titleLabel?.text else {
+            return ""
+        }
+        switch titleLabel {
+        case "+":
+            return "+"
+        case "−":
+            return "-" // -값 다름 case부분에 적용된건 가짜 minus
+        case "÷":
+            return "/"
+        case "×":
+            return "*"
+        default:
+            return ""
+        }
+    }
+    
+    func makeScrollViewData() -> String {
+        
+        return ""
+    }
     
 }
 
