57d0ef1ed3ac20da5506f1cae8d2389602277763
parkmuri <smxhmu@naver.com>
2023-01-31 15:00:08 +0900

    feat: Formula result 구현

--- Calculator/Calculator/Model/Formula.swift
+++ Calculator/Calculator/Model/Formula.swift
@@ -7,10 +7,41 @@
 
 struct Formula {
     var operands: CalculatorItemQueue<Double>
-    
     var operators: CalculatorItemQueue<Operator>
     
-    func result() -> Double {
+    mutating func result() -> Double {
+        print(operands)
+        print(operators)
+        var result1: Double = 0
+        var result2: Double = 0
+        
+        while operands.isEmpty == false {
+            if operands.head == 0 {
+                guard let lhsOperand = operands.dequeue() else {
+                    return .nan
+                }
+                guard let operatorSign = operators.dequeue() else {
+                    return .nan
+                }
+                guard let rhsOperand = operands.dequeue() else {
+                    return .nan
+                }
+                
+                result1 = operatorSign.calculate(lhs: lhsOperand, rhs: rhsOperand)
+            } else {
+                var lhsOperand = result1
+
+                guard let operatorSign = operators.dequeue() else {
+                    return 0
+                }
+                guard let rhsOperand = operands.dequeue() else {
+                    return .nan
+                }
+  
+                result2 = operatorSign.calculate(lhs: lhsOperand, rhs: rhsOperand)
+                result1 = result2
+            }
+        }
         
         return Double()
     }
