1dafe9ea25e04b3995e6c4767a4b9cad5b747fc5
yijiye <jiyee4013@naver.com>
2023-01-31 12:17:44 +0900

    refactor: ExpressionParser 로직 수정

--- Calculator/Calculator/Model/Enumeration/ExpressionParser.swift
+++ Calculator/Calculator/Model/Enumeration/ExpressionParser.swift
@@ -8,17 +8,15 @@
 enum ExpressionParser {
     static func parse(from input: String) -> Formula {
         let components = componentsByOperator(from: input)
-        var operands = CalculatorItemQueue<Double>()
-        var operators = CalculatorItemQueue<Operator>()
         
-        components.compactMap{ Double($0) }
-            .forEach{ operands.enqueue($0) }
-        
-        components.filter{ $0.count == 1 }
+        let operand: [Double] = components.compactMap{ Double($0) }
+        let operators: [Operator] = components.filter{ $0.count == 1 }
             .compactMap{ Operator(rawValue: Character($0)) }
-            .forEach{ operators.enqueue($0) }
-
-        let result: Formula = Formula(operands: operands, operators: operators)
+        
+        let operandValue = CalculatorItemQueue<Double>(items: operand)
+        let operatorValue = CalculatorItemQueue<Operator>(items: operators)
+        
+        let result: Formula = Formula(operands: operandValue, operators: operatorValue)
         
         return result
     }
