b275b04e926b8c96759b02d527ee5964600f3030
yijiye <jiyee4013@naver.com>
2023-02-01 00:05:15 +0900

    feat: 숫자버튼, 연산자버튼 @IBAction 구현

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -7,12 +7,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak private var currentOperatorLabel: UILabel!
+    @IBOutlet weak private var currentInputLabel: UILabel!
+    @IBOutlet weak var enteredOperandLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    private var isFinishedEnteringOperands: Bool = false
+    
+    @IBAction func operatorButtonTapped(_ sender: UIButton) {
+        
+        if let senderSign = sender.currentTitle {
+            currentOperatorLabel.text = senderSign
+            currentInputLabel.text = "0"
+        }
+
+    }
+    
+    @IBAction func numberButtonTapped(_ sender: UIButton) {
+        
+        guard let number = sender.currentTitle else { return }
+        checkFirstDigit()
+        
+        if isFinishedEnteringOperands {
+            guard let currentNumber = currentInputLabel.text else  { return }
+            currentInputLabel.text = currentNumber + number
+        } else {
+            currentInputLabel.text = number
+        }
+        isFinishedEnteringOperands = true
+    }
+    
+    private func checkFirstDigit() {
+        if currentInputLabel.text == "0" || currentInputLabel.text == "00" {
+            currentInputLabel.text = ""
+        }
+    }
 
 }
-
