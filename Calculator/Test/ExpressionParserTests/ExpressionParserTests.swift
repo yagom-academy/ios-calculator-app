154fe6a2c5f04c84ae500b1f2a6d5e6846eecbe5
kaki <ycg02109@naver.com>
2023-01-31 12:59:56 +0900

    feat: Parser 로직 수정 및 Test 반영

--- Calculator/Test/ExpressionParserTests/ExpressionParserTests.swift
+++ Calculator/Test/ExpressionParserTests/ExpressionParserTests.swift
@@ -11,7 +11,7 @@ import XCTest
 final class ExpressionParserTests: XCTestCase {
     func test_5_더하기_10_곱하기_4_나누기_6_빼기_15는_마이너스5인가() {
         // given
-        let input = "5 + 10 × 4 ÷ 6 − 15"
+        let input = "5+10×4÷6−15"
         let expectation = -5.0
         // when
         var parseTest = ExpressionParser.parse(from: input)
@@ -22,7 +22,7 @@ final class ExpressionParserTests: XCTestCase {
     
     func test_5_빼기_마이너스8은_13인가() {
         // given
-        let input = "5 − -8"
+        let input = "5−-8"
         let expectation = 13.0
         // when
         var parseTest = ExpressionParser.parse(from: input)
