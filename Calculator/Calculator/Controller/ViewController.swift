b02ed20b5e14827dd66d5e8d9478fde51fab7e35
parkmuri <smxhmu@naver.com>
2023-01-31 17:02:10 +0900

    refactor: 불필요한 코드제거, 공백제거

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -10,9 +10,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        var result = ExpressionParser.parse(from: "9 + 5 - 2 / 3 * 5")
-        var result2 = result.result()
-        // Do any additional setup after loading the view.
+
     }
 }
-
--- Calculator/Calculator/Model/Formula.swift
+++ Calculator/Calculator/Model/Formula.swift
@@ -33,5 +33,3 @@ struct Formula {
         return result
     }
 }
-
-
