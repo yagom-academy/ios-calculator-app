f038993779015319be85814a3ee9ed90dbe206e6
Rhode-park <jinah.park.16@gmail.com>
2023-01-31 13:39:34 +0900

    feat: ViewController UI연결

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -7,12 +7,58 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var presentOperatorLabel: UILabel!
+    @IBOutlet weak var presentNumberLabel: UILabel!
+    
+    @IBOutlet weak var acButton: UIButton!
+    @IBOutlet weak var ceButton: UIButton!
+    @IBOutlet weak var convertSignButton: UIButton!
+    
+    @IBOutlet weak var divideButton: UIButton!
+    @IBOutlet weak var multiplyButton: UIButton!
+    @IBOutlet weak var subtractButton: UIButton!
+    @IBOutlet weak var addButton: UIButton!
+    
+    @IBOutlet weak var calculateButton: UIButton!
+    
+    @IBOutlet weak var dotButton: UIButton!
+    
+    @IBOutlet weak var doubleZeroButton: UIButton!
+    @IBOutlet weak var singleZeroButton: UIButton!
+    @IBOutlet weak var oneButton: UIButton!
+    @IBOutlet weak var twoButton: UIButton!
+    @IBOutlet weak var threeButton: UIButton!
+    @IBOutlet weak var fourButton: UIButton!
+    @IBOutlet weak var fiveButton: UIButton!
+    @IBOutlet weak var sixButton: UIButton!
+    @IBOutlet weak var sevenButton: UIButton!
+    @IBOutlet weak var eightButton: UIButton!
+    @IBOutlet weak var nineButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func tapACButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func tapCEButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func tapConvertSignButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func tapNumberButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func tapDotButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func tapOperatorButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func tapCalculateButton(_ sender: UIButton) {
+    }
 }
 
