52aca8b6bec2b417b6d079b9781fe0d3257b184e
minsu3007 <minsu3007@gmail.com>
2023-01-31 20:59:14 +0900

    feat: didTapNumberButton 구현

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -5,12 +5,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private var isPositive: Bool = true
+    
+    private var currentNumber: String = "" {
+        didSet {
+            currentNumberLabel.text = currentNumber
+        }
+        willSet {
+            if currentNumber.count > 20 {
+            }
+        }
+    }
+    
 
+    @IBOutlet private weak var currentNumberLabel: UILabel!
+    @IBOutlet private weak var currentOperatorLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction private func didTapNumberButton(sender: UIButton) {
+        guard let buttonTitle = sender.currentTitle else { return }
+        
+        switch buttonTitle {
+        case "⁺⁄₋":
+            if isPositive {
+                self.currentNumber = "-" + self.currentNumber
+                isPositive.toggle()
+            } else {
+                self.currentNumber = self.currentNumber.replacingOccurrences(of: "-", with: "")
+                isPositive.toggle()
+            }
+        case ".":
+            self.currentNumber += "."
+        default:
+            self.currentNumber += buttonTitle
+        }
+    }
 }
 
