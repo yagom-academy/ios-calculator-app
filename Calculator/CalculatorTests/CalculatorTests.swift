19ac730ca2021cccc331866dfa14fe0a97a6bb7c
sehong <tjsgh0801@naver.com>
2023-02-01 19:23:43 +0900

    feat: calculatorItem test failed 발생하는 부분 해결 전체 테스트 파일 setUpWithError() , tearDownWithError() 메서드 삭제

--- Calculator/CalculatorTests/CalculatorTests.swift
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -9,63 +9,45 @@ import XCTest
 
 final class CalculatorTests: XCTestCase {
     
-    var sut: CalculatorItemQueue<Int>!
-    
-    override func setUpWithError() throws {
-        try super.setUpWithError()
-        sut = CalculatorItemQueue<Int>()
-    }
-    
-    override func tearDownWithError() throws {
-        try super.tearDownWithError()
-        sut = nil
-    }
+    var sut: CalculatorItemQueue<Int>! = CalculatorItemQueue<Int>()
     
     func test_enqueue실행시_처음_값을_넣어주면_처음넣은값이_출력된다() {
-        //given
-        let fisrt = sut.peek
-
-        //when
+        //given when
         sut.enqueue(1)
+        let expectation = sut.peek
         
         //then
-        XCTAssertEqual(fisrt, 1)
+        XCTAssertEqual(1, expectation)
     }
     
     func test_enqueue실행시_값을_여러번_넣었을때_첫번째값이_출력된다() {
-        //given
-        let fisrt = sut.peek
-        
-        //when
+        //given when
         sut.enqueue(1)
         sut.enqueue(2)
+        let expectation = sut.peek
         
         //then
-        XCTAssertEqual(fisrt, 1)
+        XCTAssertEqual(1, expectation)
     }
     
     func test_dequeue실행시_첫번째로넣어줬던값이_삭제된다() {
-        //given
-        let item = sut.dequeue()
-        
-        //when
+        //given when
         sut.enqueue(1)
+        let result = sut.dequeue()
         
         //then
-        XCTAssertEqual(item, 1)
+        XCTAssertEqual(result, 1)
     }
     
     func test_값을_여러번넣고_dequeue실행시_첫번째로넣었던값이_삭제되고_두번째가남는다() {
-        //given
-        let first = sut.peek
-        
-        //when
+        //given when
         sut.enqueue(1)
         sut.enqueue(2)
         sut.dequeue()
+        let expectation = sut.peek
         
         //then
-        XCTAssertEqual(first, 2)
+        XCTAssertEqual(2, expectation)
     }
     
     func test_putValueAndRemove_Check_ResultIsEmpty() {
