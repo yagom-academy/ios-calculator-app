19de5aaf91459ef7fd01fc7567753fb5faa935bf
minsu3007 <minsu3007@gmail.com>
2023-02-01 17:22:39 +0900

    feat: 숫자버튼, .버튼, 0,00버튼 메소드 구현

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -8,22 +8,18 @@ class ViewController: UIViewController {
     
     private let numberFormatter = NumberFormatter()
     private var inputs: String = ""
-    private var currentNumber: String = ""
+    private var currentOperand: String = ""
+    private var currentOperator: String = ""
+    private var isFractional: Bool = false
     
     @IBOutlet private weak var scrollView: UIScrollView!
     @IBOutlet private weak var stackViewInScrollView: UIStackView!
-    @IBOutlet private weak var currentNumberLabel: UILabel!
+    @IBOutlet private weak var currentOperandLabel: UILabel!
     @IBOutlet private weak var currentOperatorLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setNumberFormatter()
-        
-    }
-    
-    func setNumberFormatter() {
-        self.numberFormatter.numberStyle = .decimal
-        self.numberFormatter.roundingMode = .halfEven
     }
     
     @IBAction private func didTapButton(sender: UIButton) {
@@ -31,13 +27,73 @@ class ViewController: UIViewController {
         
         switch buttonTitle {
         case "AC":
+            return
         case "CE":
+            return
         case "⁺⁄₋":
+            return
         case "÷", "×", "−", "+":
+            clickedOperator(operatorValue: buttonTitle)
         case "0", "00":
+            addZeroToOperandLabel(operand: buttonTitle)
         case ".":
+            addDotToOperandLabel()
         default:
+            addNumberToOperandLabel(operand: buttonTitle)
         }
+    }
+    
+    func clickedOperator(operatorValue: String) {
+        let operandValue = self.currentOperand
+        let operatorValue = self.currentOperator
+        
+        addStackView(number: operandValue, operatorType: operatorValue)
+        
+        self.inputs = "\(operatorValue) \(operandValue) "
+        
+        self.currentOperand = ""
+        self.currentOperandLabel.text = self.currentOperand
+        self.currentOperator = operatorValue
+        self.currentOperatorLabel.text = self.currentOperator
+    }
+    
+    func addZeroToOperandLabel(operand: String) {
+        if self.currentOperand.count > 0 {
+            if self.isFractional == false {
+                self.currentOperand += operand
+                
+                guard let number = Double(self.currentOperand),
+                      let formattedOperand = self.numberFormatter.string(from: NSNumber(floatLiteral: number))  else { return }
+                
+                self.currentOperand = formattedOperand
+                self.currentOperandLabel.text = self.currentOperand
+            }
+        }
+    }
+    
+    func addDotToOperandLabel() {
+        if self.currentOperand == "" {
+            self.currentOperand = "0."
+        } else {
+            self.currentOperand += "."
+        }
+        
+        self.isFractional = true
+    }
+    
+    func addNumberToOperandLabel(operand: String) {
+        self.currentOperand += operand
+        
+        guard let number = Double(self.currentOperand),
+              let formattedOperand = self.numberFormatter.string(from: NSNumber(floatLiteral: number))  else { return }
+        
+        self.currentOperand = formattedOperand
+        self.currentOperandLabel.text = self.currentOperand
+    }
+        
+    func setNumberFormatter() {
+        self.numberFormatter.numberStyle = .decimal
+        self.numberFormatter.roundingMode = .halfEven
     }
 
 }
@@ -46,7 +102,7 @@ extension ViewController {
     func addStackView(number: String?, operatorType: String?) {
         guard let operandValue = number,
               let operatorValue = operatorType else { return }
-        
+
         let enteredStackView = UIStackView()
         enteredStackView.heightAnchor.constraint(equalToConstant: 24).isActive = true
         enteredStackView.axis = .horizontal
@@ -54,36 +110,36 @@ extension ViewController {
         enteredStackView.alignment = .fill
         enteredStackView.distribution = .fill
         enteredStackView.spacing = 8
-        
+
         let operatorLabel = UILabel()
         operatorLabel.font = .preferredFont(forTextStyle: .title3)
         operatorLabel.text = operatorValue
-        
+
         let operandLabel = UILabel()
         operandLabel.font = .preferredFont(forTextStyle: .title3)
         operandLabel.text = operandValue
-        
+
         enteredStackView.addArrangedSubview(operatorLabel)
         enteredStackView.addArrangedSubview(operandLabel)
-        
+
         operandLabel.trailingAnchor.constraint(equalTo: enteredStackView.trailingAnchor, constant: 0).isActive = true
         operandLabel.topAnchor.constraint(equalTo: enteredStackView.topAnchor, constant: 0).isActive = true
         operandLabel.bottomAnchor.constraint(equalTo: enteredStackView.bottomAnchor, constant: 0).isActive = true
-        
+
         operatorLabel.leadingAnchor.constraint(equalTo: enteredStackView.leadingAnchor, constant: 0).isActive = true
         operatorLabel.topAnchor.constraint(equalTo: enteredStackView.topAnchor, constant: 0).isActive = true
         operatorLabel.bottomAnchor.constraint(equalTo: enteredStackView.bottomAnchor, constant: 0).isActive = true
-        
+
         self.stackViewInScrollView.addArrangedSubview(enteredStackView)
     }
-    
+
     func removeAllStackView() {
         let allSubViewsInStackVIew = self.stackViewInScrollView.arrangedSubviews
         for stackView in allSubViewsInStackVIew {
             self.stackViewInScrollView.removeArrangedSubview(stackView)
         }
     }
-    
+
     func scrollToBottom() {
         if self.scrollView.contentSize.height < self.scrollView.bounds.size.height { return }
         self.scrollView.layoutIfNeeded()
