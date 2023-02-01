bd16aa0e880a440e4d67d4435063e47930996020
sehong <tjsgh0801@naver.com>
2023-02-01 17:46:16 +0900

    refactor: self 키워드 삭제

--- Calculator/FormulaTests/FormulaTests.swift
+++ Calculator/FormulaTests/FormulaTests.swift
@@ -13,7 +13,7 @@ final class FormulaTests: XCTestCase {
     
     override func setUpWithError() throws {
         try super.setUpWithError()
-        self.sut = Formula()
+        sut = Formula()
     }
     
     override func tearDownWithError() throws {
@@ -34,25 +34,25 @@ final class FormulaTests: XCTestCase {
     
     func testResult_if_thirdOperand_isEmpty_returnFirstResult() {
         //given
-        self.sut.operands.enqueue(1.0)
-        self.sut.operators.enqueue(Operator.add)
-        self.sut.operands.enqueue(2.0)
-        self.sut.operators.enqueue(Operator.add)
+        sut.operands.enqueue(1.0)
+        sut.operators.enqueue(Operator.add)
+        sut.operands.enqueue(2.0)
+        sut.operators.enqueue(Operator.add)
         
         // when
-        let result = self.sut.result()
+        let result = sut.result()
         // then
         XCTAssertEqual(result, 3.0)
     }
     
     func testResult_givenZeroOperand_and_devideOperator_returnNan() {
         //given
-        self.sut.operands.enqueue(1.0)
-        self.sut.operators.enqueue(Operator.divide)
-        self.sut.operands.enqueue(.zero)
+        sut.operands.enqueue(1.0)
+        sut.operators.enqueue(Operator.divide)
+        sut.operands.enqueue(.zero)
         
         //when
-        let result = self.sut.result()
+        let result = sut.result()
         
         //then
         XCTAssertTrue(result.isNaN)
@@ -60,13 +60,13 @@ final class FormulaTests: XCTestCase {
     
     func testResult_givenTwoOperands_and_addOperator_return3() {
         //given
-        self.sut.operands.enqueue(1.0)
-        self.sut.operators.enqueue(Operator.add)
-        self.sut.operands.enqueue(2.0)
+        sut.operands.enqueue(1.0)
+        sut.operators.enqueue(Operator.add)
+        sut.operands.enqueue(2.0)
         let expectation: Double = 3.0
         
         //when
-        let result = self.sut.result()
+        let result = sut.result()
         
         //then
         XCTAssertEqual(result, expectation)
@@ -74,13 +74,13 @@ final class FormulaTests: XCTestCase {
     
     func testResult_givenTwoOperands_and_subtractOperator_return1() {
         //given
-        self.sut.operands.enqueue(2.0)
-        self.sut.operators.enqueue(Operator.substact)
-        self.sut.operands.enqueue(1.0)
+        sut.operands.enqueue(2.0)
+        sut.operators.enqueue(Operator.substact)
+        sut.operands.enqueue(1.0)
         let expectation: Double = 1.0
         
         //when
-        let result = self.sut.result()
+        let result = sut.result()
         
         //then
         XCTAssertEqual(result, expectation)
@@ -88,13 +88,13 @@ final class FormulaTests: XCTestCase {
     
     func testResult_givenTwoOperands_and_multiplyOperator_return2() {
         //given
-        self.sut.operands.enqueue(1.0)
-        self.sut.operators.enqueue(Operator.multiply)
-        self.sut.operands.enqueue(2.0)
+        sut.operands.enqueue(1.0)
+        sut.operators.enqueue(Operator.multiply)
+        sut.operands.enqueue(2.0)
         let expectation: Double = 2.0
         
         //when
-        let result = self.sut.result()
+        let result = sut.result()
         
         //then
         XCTAssertEqual(result, expectation)
@@ -102,12 +102,12 @@ final class FormulaTests: XCTestCase {
     
     func testResult_operator_isEmpty_returnFirstOperand() {
         //given
-        self.sut.operands.enqueue(1.0)
-        self.sut.operands.enqueue(2.0)
+        sut.operands.enqueue(1.0)
+        sut.operands.enqueue(2.0)
         let expectation: Double = 1.0
         
         //when
-        let result = self.sut.result()
+        let result = sut.result()
         
         //then
         XCTAssertEqual(result, expectation)
