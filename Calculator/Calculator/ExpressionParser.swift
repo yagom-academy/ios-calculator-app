ceba40ffc7b315576f8bc09ac71c0aad12cea05f
Hanseul Lee <61936306+christy-hs-lee@users.noreply.github.com>
2023-02-01 19:17:50 +0900

    feat: parse, componentsByOperators 구현

--- Calculator/Calculator/ExpressionParser.swift
+++ Calculator/Calculator/ExpressionParser.swift
@@ -8,16 +8,27 @@
 import Foundation
 
 enum ExpressionParser {
-    
-//    static func parse(from input: String) -> Formula {
-//        
-//        return 
-//    }
+    static func parse(from input: String) -> Formula {
+        let components = componentsByOperators(from: input)
+        var operandQueue = CalculatorItemQueue<Double>()
+        var operatorQueue = CalculatorItemQueue<Operator>()
+        
+        for component in components {
+            if let componentAsDouble = Double(component) {
+                operandQueue.enqueue(componentAsDouble)
+            } else if let componentAsOperator = Operator(rawValue: Character(component)) {
+                operatorQueue.enqueue(componentAsOperator)
+            }
+        }
+        return Formula(operands: operandQueue, operators: operatorQueue)
+    }
     
     static private func componentsByOperators(from input: String) -> [String] {
-        
-    // split 연산자 기준으로 숫자 쪼개기
-        
-        return []
+        let inputComponents = [input].flatMap { inputComponent in
+            Operator.allCases.reduce(into: [inputComponent]) { result, operateSymbol in
+                result = result.flatMap { $0.split(with: operateSymbol.rawValue) }
+            }
+        }
+        return inputComponents
     }
 }
