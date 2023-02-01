302bc560ec556dabeb53efb64d9d082d2b86a876
vetto <gzzjk159@naver.com>
2023-02-01 14:34:47 +0900

    feat: didTapChangeSighButton 생성, refactor: Operator rawvalue 수정

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -8,11 +8,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var signLabel: UILabel!
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    @IBOutlet weak var calculateStackView: UIStackView!
+    @IBOutlet weak var calculateScrollView: UIScrollView!
+    
+    private var calculateSign: String = ""
+    private var calculateNumber: String = "0"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    @IBAction func didTapACButton() {
+        
+    }
+    
+    @IBAction func didTapCEButton() {
+        
+    }
+    
+    @IBAction func didTapChangeSignButton() {
+        guard calculateNumber != "0" else { return }
+        
+        guard let calculateNumberFirst = calculateNumber.first else { return }
+        
+        if calculateNumberFirst == "-" {
+            calculateNumber.remove(at: calculateNumber.startIndex)
+        } else {
+            calculateNumber.insert("-", at: calculateNumber.startIndex)
+        }
+        
+        numberLabel.text = calculateNumber
+    }
+    
+    @IBAction func didTapResultButton() {
+        
+    }
+    
+    @IBAction func didTapOperaterButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func didTapNumberButton(_ sender: UIButton) {
+    }
 }
 
