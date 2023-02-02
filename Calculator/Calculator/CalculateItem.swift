4af7f0bcf4f0701002ec900dc1683792c4a50f99
Andrew-0411 <whk1140@naver.com>
2023-02-02 15:55:07 +0900

    feat: ExtensionString 파일 추가, refactor: String split  ExtensionString으로 이동

--- Calculator/Calculator/CalculateItem.swift
+++ Calculator/Calculator/CalculateItem.swift
@@ -4,10 +4,3 @@ protocol CalculateItem {
 
 extension Double: CalculateItem { }
 extension Operator: CalculateItem { }
-
-extension String {
-    func split(with target: Character) -> [String] {
-        return self.split(separator: target).map { String($0) }
-    }
-    
-}
---
+++ Calculator/Calculator/ExtensionString.swift
@@ -0,0 +1,6 @@
+extension String {
+    func split(with target: Character) -> [String] {
+        return self.split(separator: target).map { String($0) }
+    }
+    
+}
