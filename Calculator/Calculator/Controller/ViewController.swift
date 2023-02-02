6819ac43735e27c4082eff96debb3be560df970e
parkmuri <smxhmu@naver.com>
2023-02-02 17:05:34 +0900

    feat: 피연산자, 연산자 버튼 연결

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -7,8 +7,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var operandsButton: [UIButton]!
+    @IBOutlet var operatorsButton: [UIButton]!
+    @IBOutlet weak var inputOperandsLabel: UILabel!
+    @IBOutlet weak var inputOperatorsLabel: UILabel!
+    
+    let operrands = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 00, 000]
+    let operators = ["+", "-", "/", "*", "="]
     override func viewDidLoad() {
         super.viewDidLoad()
+        allocateOperrandsTag()
+        allocateOperatorsTag()
     }
+    
+    func allocateOperrandsTag() {
+        for index in 0..<operandsButton.count {
+            operandsButton[index].tag = index
+        }
+    }
+    
+    func allocateOperatorsTag() {
+        for index in 0..<operatorsButton.count {
+            operatorsButton[index].tag = index
+        }
+    }
+    
+    @IBAction func operandsButtonDidTapped(_ sender: UIButton) {
+        let number = operrands[sender.tag]
+        var screen = inputOperandsLabel.text
+        
+    }
+    
+    @IBAction func operatorsButtonDidTapped(_ sender: UIButton) {
+        print(operators[sender.tag])
+    }
+        
 }
