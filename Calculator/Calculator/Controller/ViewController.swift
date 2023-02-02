32328801b3329e67763e53adf196929f62001874
kkuldak1@naver.com <kkuldak1@naver.com>
2023-02-02 09:05:50 +0900

    feat: ViewController 구현 - IBOutlet 프로퍼티 선언 및 UI요소와 연결, touchUpCalculatorButton 구현, processInput 구현

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -7,12 +7,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var expression: String = ""
+    var constraintsOfLabelView: [NSLayoutConstraint]?
+    @IBOutlet weak var scrollView: UIStackView!
+    @IBOutlet weak var operatorUILabel: UILabel!
+    @IBOutlet weak var operandUILabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func touchUpCalculatorButton(sender: UIButton) {
+        guard let inputFromButton = sender.titleLabel?.text else {
+            return
+        }
+        
+        processInput(from: inputFromButton)
+    }
+    
+    func processInput(from inputFromButton: String) {
+        switch inputFromButton {
+        case "AC":
+            return
+        case "CE":
+            return
+        case "⁺⁄₋":
+            return
+        case "+", "−", "÷", "×":
+            return
+        case "=":
+            return
+        case "0", "00":
+            return
+        case ".":
+            return
+        default:
+            return
+        }
+    }
 }
 
