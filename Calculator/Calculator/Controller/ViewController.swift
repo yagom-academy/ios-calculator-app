55e022e0ef56ba596e0da81289c66cea95ae9d80
Jun <59365211+juun97@users.noreply.github.com>
2023-02-03 15:48:30 +0900

    feat: 연산자 버튼 클릭 액션,  StackView 추가 메서드

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -40,5 +40,55 @@ final class ViewController: UIViewController {
     }
     
 
+    @IBAction func operatorButtonDidTap(_ sender: UIButton) {
+         guard let operands = OperandsLabel.text,
+               let operatorSign = sender.currentTitle else { return }
+         
+         if currentLabelText.isEmpty && operands.isEmpty {
+             OperatorLabel.text = sender.currentTitle
+             return
+         }
+   
+         if let element = inputList.last {
+             guard Operator(rawValue: Character(element)) == nil else {
+                 OperatorLabel.text = operatorSign
+                 return
+             }
+         }
+         
+         updateScrollView()
+         inputList.append(operands)
+             
+         guard let lastInput = inputList.last,
+               let _ = Double(lastInput) else { return }
+         
+         inputList.append(operatorSign)
+         OperatorLabel.text = operatorSign
+         currentLabelText.removeAll()
+         OperandsLabel.text = "0"
+     }
+    
+    private func updateScrollView() {
+        let stack = UIStackView()
+        stack.axis = .horizontal
+        stack.alignment = .trailing
+        stack.distribution = .fill
+        stack.spacing = 8
+        
+        let `operator` = UILabel()
+        `operator`.text = OperatorLabel.text
+        `operator`.textColor = .white
+        
+        let operands = UILabel()
+        operands.text = OperandsLabel.text
+        operands.textColor = .white
+        
+        stack.addArrangedSubview(`operator`)
+        stack.addArrangedSubview(operands)
+        
+        containerStackView.addArrangedSubview(stack)
+        scrollView.setContentOffset(CGPoint(x: 0, y: scrollView.contentSize.height - scrollView.bounds.height), animated: true)
+
+    }
  
 }
