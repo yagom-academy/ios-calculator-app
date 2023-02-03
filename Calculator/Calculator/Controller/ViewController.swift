2f4202e564fffbf1f1379765fe431c113d921f68
Jun <59365211+juun97@users.noreply.github.com>
2023-02-03 15:46:44 +0900

    feat: 숫자버튼 클릭 액션

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -6,13 +6,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    @IBOutlet weak var OperatorLabel: UILabel!
+    @IBOutlet weak var OperandsLabel: UILabel!
+    @IBOutlet var containerStackView: UIStackView!
+    @IBOutlet var scrollView: UIScrollView!
+    
+    private var isDotClicked: Bool = false
+    private var currentLabelText: String = ""
+    private var inputList: [String] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        OperandsLabel.text = "0"
     }
 
+    @IBAction func operandsButtonDidTap(_ sender: UIButton) {
+        guard let inputNumber = sender.currentTitle else { return }
+        
+        switch inputNumber {
+        case "0", "00":
+            if currentLabelText.isEmpty {
+                currentLabelText = "0"
+                OperandsLabel.text = "0"
+                return
+            } else if currentLabelText == "0" { return }
+            currentLabelText += inputNumber
+            
+        default: currentLabelText += inputNumber
+        }
+        OperandsLabel.text = currentLabelText
+    }
+    
 
+ 
 }
-
