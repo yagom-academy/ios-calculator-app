46a2e592eba39edd644e483817beeeee5a060e4c
Jun <59365211+juun97@users.noreply.github.com>
2023-02-05 21:23:12 +0900

    refactor : 네이밍 변경

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -20,17 +20,21 @@ final class ViewController: UIViewController {
         super.viewDidLoad()
         OperandsLabel.text = "0"
     }
-
+    
     @IBAction func operandsButtonDidTap(_ sender: UIButton) {
         guard let inputNumber = sender.currentTitle else { return }
         
         switch inputNumber {
         case "0", "00":
+            
             if currentLabelText.isEmpty {
                 currentLabelText = "0"
                 OperandsLabel.text = "0"
                 return
-            } else if currentLabelText == "0" { return }
+            } else if currentLabelText == "0" {
+                return
+            }
+
             currentLabelText += inputNumber
             
         default: currentLabelText += inputNumber
@@ -39,23 +43,23 @@ final class ViewController: UIViewController {
     }
     
     @IBAction func operatorButtonDidTap(_ sender: UIButton) {
-         guard let formattedOperands = OperandsLabel.text,
-               let operatorSign = sender.currentTitle else { return }
-         
-         if currentLabelText.isEmpty && formattedOperands == "0" {
-             OperatorLabel.text = sender.currentTitle
-             return
-         }
-   
-         let operands = restorationNumber(formattedOperands)
-         updateScrollView()
-         inputList.append(operands)
-         
-         inputList.append(operatorSign)
-         OperatorLabel.text = operatorSign
-         currentLabelText.removeAll()
-         OperandsLabel.text = "0"
-     }
+        guard let formattedOperands = OperandsLabel.text,
+              let operatorSign = sender.currentTitle else { return }
+        
+        if currentLabelText.isEmpty && formattedOperands == "0" {
+            OperatorLabel.text = sender.currentTitle
+            return
+        }
+        
+        let operands = restorationNumber(formattedOperands)
+        updateScrollView()
+        inputList.append(operands)
+        
+        inputList.append(operatorSign)
+        OperatorLabel.text = operatorSign
+        currentLabelText.removeAll()
+        OperandsLabel.text = "0"
+    }
     
     private func updateScrollView() {
         let stack = UIStackView()
@@ -79,44 +83,47 @@ final class ViewController: UIViewController {
         scrollView.setContentOffset(CGPoint(x: 0, y: scrollView.contentSize.height - scrollView.bounds.height), animated: true)
         scrollView.layoutIfNeeded()
     }
- 
+    
     @IBAction func calculateButtonDidTap(_ sender: UIButton) {
-           guard currentLabelText.isEmpty == false,
-                 let operatorSign = OperatorLabel.text  else { return }
-           inputList.removeLast()
-           inputList.append(operatorSign)
-           
-           updateScrollView()
-           inputList.append(currentLabelText)
-           
-           let calculate = inputList.reduce("") { current, next in current + " " + next}
-           
-           var formula = ExpressionParser.parse(from: calculate)
-           guard let result = formula.result(),
-                 result.isNaN == false  else {
-               OperandsLabel.text = Double.nan.description
-               isDotClicked = false
-               
-               inputList.removeAll()
-               currentLabelText.removeAll()
-               OperatorLabel.text = ""
-               return
-           }
-         
-           let intResult = Int(result)
-                
-           inputList.removeAll()
-           currentLabelText.removeAll()
-           OperatorLabel.text = ""
-           
-           if result == Double(intResult) {
-               OperandsLabel.text = formatNumber(intResult.description)
-               isDotClicked = false
-           } else {
-               OperandsLabel.text = formatNumber(result.description)
-               isDotClicked = true
-           }
-       }
+        guard currentLabelText.isEmpty == false,
+              let operatorSign = OperatorLabel.text  else { return }
+        
+        guard inputList.isEmpty == false else { return }
+        inputList.removeLast()
+        inputList.append(operatorSign)
+        
+        updateScrollView()
+        inputList.append(currentLabelText)
+        
+        let calculate = inputList.reduce("") { current, next in current + " " + next}
+        
+        var formula = ExpressionParser.parse(from: calculate)
+        guard let result = formula.result(),
+              result.isNaN == false  else {
+            OperandsLabel.text = Double.nan.description
+            isDotClicked = false
+            
+            inputList.removeAll()
+            currentLabelText.removeAll()
+            OperatorLabel.text = ""
+            return
+        }
+        
+        let intResult = Int(result)
+        
+        inputList.removeAll()
+        currentLabelText.removeAll()
+        OperatorLabel.text = ""
+        
+        
+        if result == Double(intResult) {
+            OperandsLabel.text = formatNumber(intResult.description)
+            isDotClicked = false
+        } else {
+            OperandsLabel.text = formatNumber(result.description)
+            isDotClicked = true
+        }
+    }
     
     @IBAction func allClearButtonDidTap(_ sender: UIButton) {
         inputList.removeAll()
@@ -125,7 +132,7 @@ final class ViewController: UIViewController {
         OperandsLabel.text?.removeAll()
         containerStackView.arrangedSubviews.forEach { $0.removeFromSuperview() }
     }
-
+    
     @IBAction func clearCurrentOperands(_ sender: UIButton) {
         currentLabelText.removeAll()
         OperandsLabel.text = "0"
@@ -145,11 +152,14 @@ final class ViewController: UIViewController {
     }
     
     @IBAction func dotButtonDidTap(_ sender: UIButton) {
-        if currentLabelText.isEmpty {
+        guard currentLabelText.isEmpty == false else {
             currentLabelText = "0."
             OperandsLabel.text = currentLabelText
-        } else if isDotClicked == false {
-            currentLabelText += "."
+            return
+        }
+        
+        if isDotClicked == false {
+            currentLabelText += "0."
             OperandsLabel.text = currentLabelText
             isDotClicked = true
         } else if isDotClicked == true {
@@ -162,13 +172,24 @@ final class ViewController: UIViewController {
         formatter.numberStyle = .decimal
         formatter.maximumFractionDigits = 6
         guard let number = Double(stringNumber) else { return ""}
- 
-        let result = formatter.string(from: NSNumber(value: number)) ?? ""
-        return result
+        
+        let formattedNumber = formatter.string(from: NSNumber(value: number))  ?? ""
+        return formattedNumber
     }
     
     private func restorationNumber(_ formattedNumber: String) -> String {
         let result = formattedNumber.split(with: ",").joined()
         return result
+    }
+    
+    private func checkIsItDecimal(number: Double) -> Bool {
+        let intNumber = Int(number)
+        
+        if number == Double(intNumber) {
+            return false
+        } else {
+            return true
+        }
+        
     }
 }
