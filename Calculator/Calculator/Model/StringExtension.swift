90cdfd4baff8bc937caf5d5e3ca511a1a069ded2
vetto <gzzjk159@naver.com>
2023-02-05 15:28:11 +0900

    refactor: 불필요한 함수 코드 삭제

--- Calculator/Calculator/Model/StringExtension.swift
+++ Calculator/Calculator/Model/StringExtension.swift
@@ -9,30 +9,6 @@ extension String {
     }
     
     func numberFormatting() -> String {
-        var stringArray = self.split(with: ".")
-        stringArray[0] = integerFormatting(stringArray[0])
-        if self.contains(".") {
-            if Double(stringArray[1]) == 0 {
-                stringArray.removeLast()
-            }
-        }
-        
-        return stringArray.joined(separator: ".")
-    }
-    
-    func integerFormatting(_ input: String) -> String {
-        var numberFormatter: NumberFormatter {
-            let numberFormatter = NumberFormatter()
-            numberFormatter.numberStyle = .decimal
-            numberFormatter.maximumFractionDigits = 20
-            numberFormatter.roundingMode = .halfUp
-            
-            return numberFormatter
-        }
-        
-        let number = input.components(separatedBy: ",").joined()
-        guard let numberString = numberFormatter.string(for: Double(number)) else { return "" }
-        
-        return numberString
+        return ""
     }
 }
