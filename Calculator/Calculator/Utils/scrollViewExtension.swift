f7e39086ffda11c7f9ee5db270c836474e402ef8
seunghyunCheon <csh7215@naver.com>
2023-02-06 18:38:14 +0900

    feat: scrollView extension 구현 및 폴더정리

---
+++ Calculator/Calculator/Utils/scrollViewExtension.swift
@@ -0,0 +1,15 @@
+//
+//  scrollViewExtension.swift
+//  Calculator
+//
+//  Created by 천승현 on 2023/02/06.
+//
+
+extension UIScrollView {
+    func scrollToBottom() {
+        if contentSize.height > bounds.size.height {
+            let bottomEdge = contentSize.height - bounds.size.height
+            setContentOffset(CGPoint(x: 0, y: bottomEdge), animated: true)
+        }
+    }
+}
