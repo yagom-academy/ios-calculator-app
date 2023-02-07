6baa326f5cabd4e21d5addc43364e0cfea5ad8e4
whdghks8654 <62684851+whdghks8654@users.noreply.github.com>
2023-02-07 12:53:14 +0900

    feat: ExpressionParser 정의

---
+++ Calculator/Calculator/ExpressionParser.swift
@@ -0,0 +1,54 @@
+//
+//  ExpressionParser.swift
+//  Calculator
+//
+//  Created by Harry,Goat on 2023/02/07.
+//
+
+import Foundation
+
+enum ExpressionParser {
+    
+    static func parse(from input: String) -> Formula {
+        
+        var operatorQueue = CalculatorItemQueue<Operator>()
+        var operandQueue = CalculatorItemQueue<Double>()
+                
+        extractOperators(from: input)
+                .map {Character($0)}
+                .compactMap { Operator(rawValue: $0) }
+                .forEach {operatorQueue.enqueueItem($0) }
+               
+        componentsByOperators(from: input)
+                .compactMap { Double($0) }
+                .forEach{ operandQueue.enqueueItem($0) }
+        
+        return Formula(operators: operatorQueue, operands: operandQueue)
+    }
+    
+    static private func componentsByOperators(from target: String) -> [String] {
+        
+        var resultNumber: [String] = []
+        resultNumber.append(target)
+
+        for operators in Operator.allCases {
+            var curruentNumber: [String] = []
+
+            resultNumber.forEach {
+                curruentNumber += $0.split(someOperator: operators.rawValue)
+            }
+            
+            resultNumber = curruentNumber
+        }
+        return resultNumber
+    }
+    
+    static private func extractOperators(from target: String) -> [String] {
+        
+        return target
+            .split(whereSeparator: {$0.isNumber || $0 == "."})
+            .map{String($0)}
+        
+    }
+    
+}
