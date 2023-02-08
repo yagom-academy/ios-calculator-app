626565a8c364ddf60bf8cd3f20b660e344a8d7bb
kimseongj <ksj8277@naver.com>
2023-02-08 14:30:54 +0900

    refactor: CalculatorItemQueue가 DoubleStack형식으로 바뀜에 따라 Unit Test 수정 / Deployment Target version 16.2 -> 16.0으로 변경

--- Calculator/CalculatorTests/ExpressionParserTests.swift
+++ Calculator/CalculatorTests/ExpressionParserTests.swift
@@ -19,8 +19,8 @@ final class ExpressionParserTests: XCTestCase {
         let result = ExpressionParser.parse(from: input)
         
         //then
-        XCTAssertEqual(expectedOperands, result.operands.data)
-        XCTAssertEqual(expectedOperator, result.operators.data)
+        XCTAssertEqual(expectedOperands, result.operands.enqueueStack)
+        XCTAssertEqual(expectedOperator, result.operators.enqueueStack)
     }
     
     func test_input이1더하기2더하기3일때_parse호출시_1과2와3이들어있는배열과더하기와더하기가들어있는배열의큐를반환한다() {
@@ -33,8 +33,8 @@ final class ExpressionParserTests: XCTestCase {
         let result = ExpressionParser.parse(from: input)
         
         //then
-        XCTAssertEqual(expectedOperands, result.operands.data)
-        XCTAssertEqual(expectedOperator, result.operators.data)
+        XCTAssertEqual(expectedOperands, result.operands.enqueueStack)
+        XCTAssertEqual(expectedOperator, result.operators.enqueueStack)
     }
     
     func test_input이1점1더하기2점2빼기3점3곱하기4점4나누기5점5일때_parse호출시_1점1과2점2와3점3과4점4와5점5가들어있는배열과더하기빼기곱하기나누기가들어있는배열의큐를반환한다() {
@@ -47,8 +47,8 @@ final class ExpressionParserTests: XCTestCase {
         let result = ExpressionParser.parse(from: input)
         
         //then
-        XCTAssertEqual(expectedOperands, result.operands.data)
-        XCTAssertEqual(expectedOperator, result.operators.data)
+        XCTAssertEqual(expectedOperands, result.operands.enqueueStack)
+        XCTAssertEqual(expectedOperator, result.operators.enqueueStack)
     }
     
     func test_input이1점1더하기2점2더하기3점3일때_parse호출시_1점1과2점2와3점3이들어있는배열과더하기와더하기가들어있는배열의큐를반환한다() {
@@ -61,8 +61,8 @@ final class ExpressionParserTests: XCTestCase {
         let result = ExpressionParser.parse(from: input)
         
         //then
-        XCTAssertEqual(expectedOperands, result.operands.data)
-        XCTAssertEqual(expectedOperator, result.operators.data)
+        XCTAssertEqual(expectedOperands, result.operands.enqueueStack)
+        XCTAssertEqual(expectedOperator, result.operators.enqueueStack)
     }
     
     func test_input이1더하기마이너스2빼기3곱하기마이너스4나누기5일때_parse호출시_1과마이너스2와3과마이너스4와5가들어있는배열과더하기빼기곱하기나누기가들어있는배열의큐를반환한다() {
@@ -75,7 +75,7 @@ final class ExpressionParserTests: XCTestCase {
         let result = ExpressionParser.parse(from: input)
         
         //then
-        XCTAssertEqual(expectedOperands, result.operands.data)
-        XCTAssertEqual(expectedOperator, result.operators.data)
+        XCTAssertEqual(expectedOperands, result.operands.enqueueStack)
+        XCTAssertEqual(expectedOperator, result.operators.enqueueStack)
     }
 }
