ddae644b91379ab80eefa9b717552228fc3f9c37
kaki <ycg02109@naver.com>
2023-02-08 14:06:45 +0900

    feat: CalculatorItemQueue 기능 및 Test 복구

--- Calculator/Test/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
+++ Calculator/Test/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
@@ -21,6 +21,56 @@ final class CalculatorItemQueueTests: XCTestCase {
         sut = nil
     }
     
+    func test_큐가_비었을경우_isEmpty가_true이다() {
+        // given
+        let expectation = true
+        // when
+        let result = sut.isEmpty
+        // then
+        XCTAssertEqual(result, expectation)
+    }
+    
+    func test_큐가_비었을경우_size가_0이다() {
+        // given
+        let expectation = 0
+        // when
+        let result = sut.size
+        // then
+        XCTAssertEqual(result, expectation)
+    }
+    
+    func test_enqueue_됐을경우_isEmpty가_false이다() {
+        // given
+        let expectation = false
+        // when
+        sut.enqueue(111)
+        let result = sut.isEmpty
+        // then
+        XCTAssertEqual(result, expectation)
+    }
+    
+    func test_한번_enqueue_됐을경우_size가_1이다() {
+        // given
+        let expectation = 1
+        // when
+        sut.enqueue(111)
+        let result = sut.size
+        // then
+        XCTAssertEqual(result, expectation)
+    }
+    
+    func test_세번_enqueue_됐을경우_size가_3이다() {
+        // given
+        let expectation = 3
+        // when
+        sut.enqueue(111)
+        sut.enqueue(222)
+        sut.enqueue(333)
+        let result = sut.size
+        // then
+        XCTAssertEqual(result, expectation)
+    }
+    
     func test_dequeue_됐을경우_처음에_enqueue_해준_데이터가_반환된다() {
         // given
         sut.enqueue(111)
@@ -33,6 +83,19 @@ final class CalculatorItemQueueTests: XCTestCase {
         XCTAssertEqual(result, expectation)
     }
     
+    func test_dequeue_됐을경우_size가_감소한다() {
+        // given
+        sut.enqueue(111)
+        sut.enqueue(222)
+        sut.enqueue(333)
+        let expectation = 2
+        // when
+        let _ = sut.dequeue()
+        let result = sut.size
+        // then
+        XCTAssertEqual(result, expectation)
+    }
+    
     func test_빈queue를_dequeue_할경우_nil이_반환된다() {
         // given
         let expectation: Double? = nil
@@ -41,4 +104,18 @@ final class CalculatorItemQueueTests: XCTestCase {
         // then
         XCTAssertEqual(result, expectation)
     }
+    
+    func test_3개의_queue에서_clear를_한경우_isEmpty는_true이다() {
+        // given
+        sut.enqueue(111)
+        sut.enqueue(222)
+        sut.enqueue(333)
+        let expectation = true
+        // when
+        sut.clear()
+        let result = sut.isEmpty
+        // then
+        XCTAssertEqual(result, expectation)
+    }
+
 }
