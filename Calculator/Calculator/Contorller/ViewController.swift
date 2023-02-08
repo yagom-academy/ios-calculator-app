c849b7177d6d4d90cb0a7343207708c3342c7c7c
whdghks8654 <62684851+whdghks8654@users.noreply.github.com>
2023-02-08 11:16:09 +0900

    refactor: dotButtonTapped 매서드 추가 및 구현

--- Calculator/Calculator/Contorller/ViewController.swift
+++ Calculator/Calculator/Contorller/ViewController.swift
@@ -76,79 +76,79 @@ class ViewController: UIViewController {
     }
     
     @IBAction func numberButtonTapped(_ sender: UIButton) {
-        
-        guard var currentNumberOnField = numberOnField.text else { return }
         guard let currentTitle = sender.currentTitle else { return }
         
-        
-        if currentNumberOnField != "0" {
-            currentNumberOnField += currentTitle
-            numberOnField.text = currentNumberOnField
+        if inputNumbers != "0" {
+            inputNumbers += currentTitle
+            numberOnField.text = inputNumbers
         } else {
+            inputNumbers = currentTitle
             numberOnField.text = currentTitle
         }
-   
     }
-        
-        @IBAction func onehundredButtonTapped(_ sender: UIButton) {
-            print("")
-        }
-        
-        
-        @IBAction func dotButtonTapped(_ sender: UIButton) {
-            print("")
-        }
-        
-        @IBAction func operatorButtonTapped(_ sender: UIButton) {
-            
-            guard let currentNumber = numberOnField.text else { return }
-            guard let inputtedOperator = sender.titleLabel?.text else { return }
-            guard let currentOper = operatorOnField.text else { return }
-            
-            operatorOnField.text = inputtedOperator
-            if currentNumber == "0", currentNumber.last == "." {
-                return
-            } else if calculateComponents == "" {
-                calculateComponents += currentNumber
-                addHistoryEntry(left: "", right: currentNumber)
-            } else {
-                calculateComponents += currentOper + currentNumber
-                addHistoryEntry(left: currentOper, right: currentNumber)
-                self.numberOnField.text = ""
-                
-            }
-            inputNumbers = ""
-            numberOnField.text = "0"
-            setUpScrollViewToBottom()
-        }
-        
-        private func addHistoryEntry(left: String, right: String) {
-            let historyEntryStackView = HistoryEntryStackView(operatorText: left, operandText: right)
-            
-            historyEntryStackView.isHidden = true
-            
-            historyStackView.addArrangedSubview(historyEntryStackView)
-            
-            UIView.animate(withDuration: 0.3) {
-                historyEntryStackView.isHidden = false
-            }
-            
-            setUpScrollViewToBottom()
-        }
-        
-        private func resetAllStackView() {
-            historyStackView.subviews.forEach { $0.removeFromSuperview() }
-        }
-        
-        private func addCommaToThreeDigit(number: String) -> String {
-            let numberFormatter = NumberFormatter()
-            numberFormatter.numberStyle = .decimal
-            
-            guard let numberMappedToDouble = Double(number) else { return "" }
-            guard let result = numberFormatter.string(from: NSNumber( value:numberMappedToDouble )) else { return "" }
-            
-            return result
-        }
-        
+    
+    @IBAction func onehundredButtonTapped(_ sender: UIButton) {
+        print("")
     }
+    
+    @IBAction func dotButtonTapped(_ sender: UIButton) {
+        guard let dot = sender.currentTitle else { return }
+        guard inputNumbers.contains(".") == false else { return }
+        
+        inputNumbers += dot
+        numberOnField.text = inputNumbers
+    }
+    
+    @IBAction func operatorButtonTapped(_ sender: UIButton) {
+        
+        guard let currentNumber = numberOnField.text else { return }
+        guard let inputtedOperator = sender.titleLabel?.text else { return }
+        guard let currentOper = operatorOnField.text else { return }
+        
+        operatorOnField.text = inputtedOperator
+        if currentNumber == "0", currentNumber.last == "." {
+            return
+        } else if calculateComponents == "" {
+            calculateComponents += currentNumber
+            addHistoryEntry(left: "", right: currentNumber)
+        } else {
+            calculateComponents += currentOper + currentNumber
+            addHistoryEntry(left: currentOper, right: currentNumber)
+            self.numberOnField.text = ""
+            
+        }
+        inputNumbers = ""
+        numberOnField.text = "0"
+        setUpScrollViewToBottom()
+    }
+    
+    private func addHistoryEntry(left: String, right: String) {
+        let historyEntryStackView = HistoryEntryStackView(operatorText: left, operandText: right)
+        
+        historyEntryStackView.isHidden = true
+        
+        historyStackView.addArrangedSubview(historyEntryStackView)
+        
+        UIView.animate(withDuration: 0.3) {
+            historyEntryStackView.isHidden = false
+        }
+        
+        setUpScrollViewToBottom()
+    }
+    
+    private func resetAllStackView() {
+        historyStackView.subviews.forEach { $0.removeFromSuperview() }
+    }
+    
+    private func addCommaToThreeDigit(number: String) -> String {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        
+        guard let numberMappedToDouble = Double(number) else { return "" }
+        guard let result = numberFormatter.string(from: NSNumber( value:numberMappedToDouble )) else { return "" }
+        
+        return result
+    }
+    
+}
 
