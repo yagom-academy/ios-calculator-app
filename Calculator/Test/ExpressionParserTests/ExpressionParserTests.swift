e0f97914ee7b242edfd0e594c395d34bc961bdbf
kaki <ycg02109@naver.com>
2023-02-09 15:23:16 +0900

    feat: ExpressionParserTests 수정

--- Calculator/Test/ExpressionParserTests/ExpressionParserTests.swift
+++ Calculator/Test/ExpressionParserTests/ExpressionParserTests.swift
@@ -9,43 +9,26 @@ import XCTest
 @testable import Calculator
 
 final class ExpressionParserTests: XCTestCase {
-    func test_5_더하기_10_곱하기_4_나누기_6_빼기_15는_마이너스5인가() {
+    func test_parse호출시_operands_큐데이터가_저장된_Formula를_반환한다() {
         // given
-        let input = "5+10×4÷6−15"
-        let expectation = -5.0
-        // when
-        var parseTest = ExpressionParser.parse(from: input)
-        let result = parseTest.result()
-        // then
-        XCTAssertEqual(result, expectation)
-    }
-    
-    func test_5_빼기_마이너스8은_13인가() {
-        // given
-        let input = "5−-8"
-        let expectation = 13.0
-        // when
-        var parseTest = ExpressionParser.parse(from: input)
-        let result = parseTest.result()
-        // then
-        XCTAssertEqual(result, expectation)
-    }
-    
-    func test_parse호출시_연산자와숫자가분리된큐데이터가저장된Formula반환한다() {
-        // given
-        let input = "1+2−3×4+5+10÷6−7"
-        let expectationOperands: [Double] = [ 1, 2, 3, 4, 5, 10, 6, 7]
-        let expectationOperators = [ "+", "−", "×", "+", "+", "÷", "−" ]
-        
+        let input = "1+2−3×4+5+10÷6−-7"
+        let expectationOperands: [Double] = [1, 2, 3, 4, 5, 10, 6, -7]
         // when
         var result = ExpressionParser.parse(from: input)
-        
         // then
         expectationOperands.forEach {
             let resultOperands = result.operands.dequeue()
             XCTAssertEqual($0, resultOperands)
         }
-        
+    }
+    
+    func test_parse호출시_operators_큐데이터가_저장된_Formula를_반환한다() {
+        // given
+        let input = "1+2−3×4+5+10÷6−-7"
+        let expectationOperators = [ "+", "−", "×", "+", "+", "÷", "−" ]
+        // when
+        var result = ExpressionParser.parse(from: input)
+        // then
         expectationOperators.forEach {
             let resultOperator = result.operaters.dequeue()
             XCTAssertEqual($0.first, resultOperator?.rawValue)
