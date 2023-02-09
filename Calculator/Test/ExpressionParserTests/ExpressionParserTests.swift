6a857cb354eb25ec57d8b0f64ef82a4319a05765
kkuldak1@naver.com <kkuldak1@naver.com>
2023-02-09 15:16:34 +0900

    feat: ExpressionParserTests 내 테스트 케이스 추가

--- Calculator/Test/ExpressionParserTests/ExpressionParserTests.swift
+++ Calculator/Test/ExpressionParserTests/ExpressionParserTests.swift
@@ -30,4 +30,25 @@ final class ExpressionParserTests: XCTestCase {
         // then
         XCTAssertEqual(result, expectation)
     }
+    
+    func test_parse호출시_연산자와숫자가분리된큐데이터가저장된Formula반환한다() {
+        // given
+        let input = "1+2−3×4+5+10÷6−7"
+        let expectationOperands: [Double] = [ 1, 2, 3, 4, 5, 10, 6, 7]
+        let expectationOperators = [ "+", "−", "×", "+", "+", "÷", "−" ]
+        
+        // when
+        var result = ExpressionParser.parse(from: input)
+        
+        // then
+        expectationOperands.forEach {
+            let resultOperands = result.operands.dequeue()
+            XCTAssertEqual($0, resultOperands)
+        }
+        
+        expectationOperators.forEach {
+            let resultOperator = result.operaters.dequeue()
+            XCTAssertEqual($0.first, resultOperator?.rawValue)
+        }
+    }
 }
