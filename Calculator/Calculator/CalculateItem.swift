1a9f9742bc64771a14c40be1659daa0a26b52009
Min Hyun <minnimida@gmail.com>
2023-05-29 17:50:45 +0900

    test: CalculatorItemQueue 객체 생성 테스트(RED)

---
+++ Calculator/Calculator/CalculateItem.swift
@@ -0,0 +1,10 @@
+//
+//  CalculateItem.swift
+//  Calculator
+//
+//  Created by Min Hyun on 2023/05/29.
+//
+
+protocol CalculateItem {}
+
+struct CalculateNumberItem: CalculateItem {}
--- Calculator/CalculatorTests/CalculatorTests.swift
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -9,10 +9,13 @@ import XCTest
 @testable import Calculator
 
 final class CalculatorTests: XCTestCase {
+    var sut: CalculatorItemQueue!
 
     override func setUpWithError() throws {
+        sut = CalculatorItemQueue()
     }
 
     override func tearDownWithError() throws {
+        sut = nil
     }
 }
