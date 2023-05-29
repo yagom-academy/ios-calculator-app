a3f161e5f0d17b418e953f8b8324eecafec0267d
idinaloq <idinaloq@icloud.com>
2023-05-29 18:50:33 +0900

    docs:CalculateItem 프로토콜 파일추가, CalculatorItemQueue 파일 생성

---
+++ Calculator/Calculator/CalculateItem.swift
@@ -0,0 +1,10 @@
+//
+//  CalculateItem.swift
+//  Calculator
+//
+//  Created by idinaloq on 2023/05/29.
+//
+
+protocol CalculateItem {
+    
+}
---
+++ Calculator/Calculator/CalculatorItemQueue.swift
@@ -0,0 +1,8 @@
+//
+//  CalculatorItemQueue.swift
+//  Calculator
+//
+//  Created by idinaloqon 2023/05/29.
+//
+
+import Foundation
