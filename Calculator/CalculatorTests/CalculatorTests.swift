a685b7bf74f0f6ad2d0248ea474a2cccc4fa2206
mint3382 <mskim0926@naver.com>
2023-05-29 22:13:57 +0900

    feat: CalculatorTests 추가

---
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -0,0 +1,20 @@
+//
+//  CalculatorTests.swift
+//  CalculatorTests
+//
+//  Created by minsong kim on 2023/05/29.
+//
+
+import XCTest
+
+final class CalculatorTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+}
