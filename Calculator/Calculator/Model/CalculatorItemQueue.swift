3a117a422752af46d1262fb2fffe428fb0101e29
YaRkyungmin <kkgm94@naver.com>
2023-05-30 16:32:48 +0900

    🚚build: CalculatorItemQueue 파일 생성

---
+++ Calculator/Calculator/Model/CalculatorItemQueue.swift
@@ -0,0 +1,13 @@
+//
+//  CalculatorItemQueue.swift
+//  Calculator
+//
+//  Created by kangkyungmin on 2023/05/30.
+//
+protocol CalculateItem {
+    
+}
+
+class CalculatorItemQueue: CalculateItem {
+    
+}
