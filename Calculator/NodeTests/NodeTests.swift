765c5bf6d3c9b4720c51abc1a41a5d05ff9e508a
Hoon94 <dleognsdl1@naver.com>
2023-05-30 22:55:14 +0900

    test: 사용자 지정 타입 Node가 선언되어 있는지 확인한다.

---
+++ Calculator/NodeTests/NodeTests.swift
@@ -0,0 +1,21 @@
+//
+//  NodeTests.swift
+//  NodeTests
+//
+//  Created by Daehoon Lee on 2023/05/30.
+//
+
+import XCTest
+@testable import Calculator
+
+final class NodeTests: XCTestCase {
+    var sut: Node!
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+}
