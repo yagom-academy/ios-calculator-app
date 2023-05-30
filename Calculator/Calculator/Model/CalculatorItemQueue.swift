52c8b19a18622a8a9030cdd5fa3fde1806e00b34
조호준 <johojun@johojun-ui-MacBookAir.local>
2023-05-30 09:26:31 +0900

    test: CalculatorItemQueue 생성 및 단위 테스트 타깃 추가

---
+++ Calculator/Calculator/Model/CalculatorItemQueue.swift
@@ -0,0 +1,10 @@
+//
+//  CalculatorItemQueue.swift
+//  Calculator
+//
+//  Created by EtialMoon on 2023/05/30.
+//
+
+struct CalculatorItemQueue {
+    
+}
---
+++ Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
@@ -0,0 +1,21 @@
+//
+//  CalculatorItemQueueTests.swift
+//  CalculatorItemQueueTests
+//
+//  Created by EtialMoon on 2023/05/30.
+//
+
+import XCTest
+@testable import Calculator
+
+final class CalculatorItemQueueTests: XCTestCase {
+    var sut: CalculatorItemQueue!
+
+    override func setUpWithError() throws {
+        sut = CalculatorItemQueue()
+    }
+
+    override func tearDownWithError() throws {
+        sut = nil
+    }
+}
