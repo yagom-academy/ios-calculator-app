338618774a98219f28d7797804bfe9fca449f868
redmango1446 <redmango1446@redmango1446ui-MacBookAir.local>
2023-05-30 17:23:58 +0900

    feat:Unit Test를 위한 타깃 생성

---
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -0,0 +1,29 @@
+//
+//  CalculatorTests.swift
+//  CalculatorTests
+//
+//  Created by redmango1446 on 2023/05/30.
+//
+
+import XCTest
+@testable import Calculator
+
+final class CalculatorTests: XCTestCase {
+    var sut: CalculatorItemQueue!
+
+    override func setUpWithError() throws {
+        sut = CalculatorItemQueue()
+    }
+
+    override func tearDownWithError() throws {
+        sut = nil
+    }
+    
+    func test() {
+        // given
+        
+        // when
+        
+        // then
+    }
+}
