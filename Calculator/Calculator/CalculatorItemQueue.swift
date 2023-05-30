43313f5f61bf82a84c9247cdfcbdbc4385fe6c23
MaryJo-github <jotjdal0215@gmail.com>
2023-05-30 20:13:45 +0900

    refactor: dequeue 메서드 시간복잡도 감소화 방안 적용

--- Calculator/Calculator/CalculatorItemQueue.swift
+++ Calculator/Calculator/CalculatorItemQueue.swift
@@ -8,14 +8,22 @@
 protocol CalculateItem { }
 
 final class CalculatorItemQueue: CalculateItem {
-    var itemQueue: [String] = []
+    var itemQueue: [String?] = []
+    private var head = 0
+    private var size: Int {
+        return itemQueue.count
+    }
     
     func enqueue(_ element: String) {
         itemQueue.append(element)
     }
     
     func dequeue() -> String? {
-        return itemQueue.isEmpty ? nil : itemQueue.removeFirst()
+        guard head <= size,
+              let element = itemQueue[head] else { return nil }
+        itemQueue[head] = nil
+        head += 1
+        return element
     }
     
     func resetQueue() {
