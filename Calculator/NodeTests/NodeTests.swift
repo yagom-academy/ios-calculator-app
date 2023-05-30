f6821c82afdde46330bba88faa4658204452fca9
Hoon94 <dleognsdl1@naver.com>
2023-05-30 23:58:39 +0900

    test: Node class data property +로 초기화

--- Calculator/NodeTests/NodeTests.swift
+++ Calculator/NodeTests/NodeTests.swift
@@ -12,18 +12,29 @@ final class NodeTests: XCTestCase {
     var sut: Node!
 
     override func setUpWithError() throws {
-        super.setUpWithError()
-        sut = Node()
+        try super.setUpWithError()
+        sut = Node(data: 0)
     }
 
     override func tearDownWithError() throws {
-        super.tearDownWithError()
+        try super.tearDownWithError()
         sut = nil
     }
     
     func test_init_data를0으로초기화한다() {
         // given
         let expectedData = 0
+        
+        // when
+        let testNode = Node(data: expectedData)
+        
+        // then
+        XCTAssertEqual(testNode.data, expectedData)
+    }
+    
+    func test_init_data를더하기연산자로초기화한다() {
+        // given
+        let expectedData = "+"
         
         // when
         let testNode = Node(data: expectedData)
