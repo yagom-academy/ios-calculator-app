ce9d07d9f604c4ebd206b4b817e08161e1de59c0
Kobe <mskang@xingxingmobility.com>
2023-05-30 16:03:33 +0900

    feat: Node 클래스 생성 테스트 구현.

--- Calculator/CalculatorNodeTests/CalculatorNodeTests.swift
+++ Calculator/CalculatorNodeTests/CalculatorNodeTests.swift
@@ -9,14 +9,15 @@ import XCTest
 @testable import Calculator
 
 final class CalculatorNodeTests: XCTestCase {
-
+	var sut: Node<Any>!
+	
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        try super.setUpWithError()
+		sut = Node<Any>()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        try super.tearDownWithError()
+		sut = nil
     }
-
-
 }
