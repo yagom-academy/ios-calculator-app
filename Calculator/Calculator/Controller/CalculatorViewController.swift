7baa0fc312ce985793d048c77c330b5c5a881e65
myungsun <audtjs9987@naver.com>
2023-05-30 17:49:24 +0900

    refactor: CalculatorViewController 클래스에 final 키워드 추가

--- Calculator/Calculator/Controller/CalculatorViewController.swift
+++ Calculator/Calculator/Controller/CalculatorViewController.swift
@@ -6,7 +6,7 @@
 
 import UIKit
 
-class CalculatorViewController: UIViewController {
+final class CalculatorViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
