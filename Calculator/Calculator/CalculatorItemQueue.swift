fc921bbb01a0178fb2c3e81a2cfdf8b3380d0eaf
Zion <lion2212@naver.com>
2023-05-30 17:24:39 +0900

    feat: CalculatorItemQueue 기초 뼈대 작성

---
+++ Calculator/Calculator/CalculatorItemQueue.swift
@@ -0,0 +1,20 @@
+//
+//  CalculatorItemQueue.swift
+//  Calculator
+//
+//  Created by Hyungmin Lee on 2023/05/30.
+//
+
+protocol CalculateItem { }
+
+class CalculatorItemQueue: CalculateItem {
+    
+    func push() {
+        
+    }
+    
+    func pop() {
+        
+    }
+
+}
