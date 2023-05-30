c3b032303f18795e98cf24e9ea1c90b01199a0c1
JusBug <pksgwo123@gmail.com>
2023-05-30 14:24:18 +0900

    feat: 숫자와 연산자를 myQueue에 입력 및 displayLabel을 초기화하는 함수 setText() 생성

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -7,43 +7,84 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var myQueue = CalculatorItemQueue<String>()
 
+    @IBOutlet weak var displayLabel: UILabel!
+    @IBOutlet weak var displayCalculationLabel: UILabel!
+    @IBOutlet weak var firstProcessLabel: UILabel!
+    @IBOutlet weak var secondProcessLabel: UILabel!
+    @IBOutlet weak var firstCalculationLabel: UILabel!
+    @IBOutlet weak var secondCalculationLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        setText()
     }
     
     
     @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        guard let currentText = displayLabel.text else { return }
         
+        displayLabel.text = currentText + digit
+        myQueue.enqueue(digit)
+        print(myQueue)
     }
     
     @IBAction func allClear(_ sender: UIButton) {
-        
+        myQueue.clear()
+        setText()
+        print(myQueue)
     }
     
     @IBAction func clearEntry(_ sender: UIButton) {
-        
+        myQueue.dequeue()
+        print(myQueue)
     }
     
     @IBAction func division(_ sender: UIButton) {
+        let calculation = sender.currentTitle!
         
+        myQueue.enqueue(calculation)
+        print(myQueue)
     }
     
     @IBAction func multiplication(_ sender: UIButton) {
+        let calculation = sender.currentTitle!
         
+        myQueue.enqueue(calculation)
+        print(myQueue)
     }
     
     @IBAction func subtraction(_ sender: UIButton) {
+        let calculation = sender.currentTitle!
         
+        myQueue.enqueue(calculation)
+        print(myQueue)
     }
     
     @IBAction func addtion(_ sender: UIButton) {
+        let calculation = sender.currentTitle!
         
+        myQueue.enqueue(calculation)
+        print(myQueue)
     }
     
     @IBAction func compute(_ sender: UIButton) {
+        let calculation = sender.currentTitle!
         
+        myQueue.enqueue(calculation)
+        print(myQueue)
+    }
+    
+    func setText() {
+        displayLabel.text = ""
+        displayCalculationLabel.text = ""
+        firstProcessLabel.text = ""
+        secondProcessLabel.text = ""
+        firstCalculationLabel.text = ""
+        secondCalculationLabel.text = ""
     }
     
 }
