b6adbf7fddaf138e01dbc02d597de5c740328c60
MaryJo-github <jotjdal0215@gmail.com>
2023-05-30 19:51:00 +0900

    feat: linkedList 구조로 변경 및 unittest 수정

--- Calculator/Calculator/CalculatorItemQueue.swift
+++ Calculator/Calculator/CalculatorItemQueue.swift
@@ -7,18 +7,56 @@
 
 protocol CalculateItem { }
 
+class Item {
+    var data: String
+    var next: Item?
+    var prev: Item?
+    
+    init(data: String, next: Item? = nil, prev: Item? = nil) {
+        self.data = data
+        self.next = next
+        self.prev = prev
+    }
+}
+
 struct CalculatorItemQueue: CalculateItem {
-    var itemQueue: [String] = []
+    var head: Item?
+    var tail: Item?
+    var size: Int = 0
+    var isEmpty: Bool { size == 0 }
     
     mutating func enqueue(_ element: String) {
-        itemQueue.append(element)
+        let newItem = Item(data: element)
+        
+        if isEmpty {
+            head = newItem
+            tail = head
+        } else {
+            tail?.next = newItem
+            newItem.prev = tail
+            tail = newItem
+        }
+        size += 1
     }
     
     mutating func dequeue() -> String? {
-        return itemQueue.isEmpty ? nil : itemQueue.removeFirst()
+        guard let result = head?.data else { return nil }
+        
+        head = head?.next
+        head?.prev = nil
+        size -= 1
+        
+        if isEmpty {
+            head = nil
+            tail = nil
+        }
+        
+        return result
     }
     
     mutating func resetQueue() {
-        itemQueue = []
+        head = nil
+        tail = nil
+        size = 0
     }
 }
--- Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
+++ Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
@@ -19,51 +19,51 @@ final class CalculatorItemQueueTests: XCTestCase {
         sut = nil
     }
     
-    func test_enqueue_itemQueue가비어있고_string을넣었을때_itemQueue에_저장한다() {
+    func test_enqueue_queue가비어있고_string을넣었을때_queue에_저장한다() {
         let input = "string"
         sut.enqueue(input)
         
-        let result = sut.itemQueue
-        let expectation = ["string"]
+        let result = sut.head?.data
+        let expectation = "string"
         
         XCTAssertEqual(result, expectation)
     }
     
-    func test_enqueue_itemQueue가비어있지않고_string을넣었을때_기존itemQueue에_추가한다() {
-        let input = "string"
-        sut.itemQueue = ["문자열"]
-        sut.enqueue(input)
-        
-        let result = sut.itemQueue
-        let expectation = ["문자열", "string"]
-        
-        XCTAssertEqual(result, expectation)
-    }
-    
+//    func test_enqueue_itemQueue가비어있지않고_string을넣었을때_기존itemQueue에_추가한다() {
+//        let input = "string"
+//        sut.itemQueue = ["문자열"]
+//        sut.enqueue(input)
+//
+//        let result = sut.itemQueue
+//        let expectation = ["문자열", "string"]
+//
+//        XCTAssertEqual(result, expectation)
+//    }
+
     func test_dequeue_itemQueue가비어있을때_nil을반환한다() {
         let result = sut.dequeue()
-        
+
         XCTAssertNil(result)
     }
-    
-    func test_dequeue_itemQueue에서_맨앞에있는요소를_반환하고_itemQueue에서_삭제한다() {
-        sut.itemQueue = ["5", "+", "3"]
-        
-        let returnValue = sut.dequeue()
-        let returnValueExpectation = "5"
-        let afterQueue = sut.itemQueue
-        let afterQueueExpectation = ["+", "3"]
-        
-        XCTAssertEqual(returnValue, returnValueExpectation)
-        XCTAssertEqual(afterQueue, afterQueueExpectation)
-    }
-    
-    func test_resetQueue_itemQueue를_빈배열로만든다() {
-        sut.itemQueue = ["5", "+", "3"]
-        
-        sut.resetQueue()
-        
-        XCTAssertTrue(sut.itemQueue.isEmpty)
-    }
+
+//    func test_dequeue_itemQueue에서_맨앞에있는요소를_반환하고_itemQueue에서_삭제한다() {
+//        sut.itemQueue = ["5", "+", "3"]
+//
+//        let returnValue = sut.dequeue()
+//        let returnValueExpectation = "5"
+//        let afterQueue = sut.itemQueue
+//        let afterQueueExpectation = ["+", "3"]
+//
+//        XCTAssertEqual(returnValue, returnValueExpectation)
+//        XCTAssertEqual(afterQueue, afterQueueExpectation)
+//    }
+//
+//    func test_resetQueue_itemQueue를_빈배열로만든다() {
+//        sut.itemQueue = ["5", "+", "3"]
+//
+//        sut.resetQueue()
+//
+//        XCTAssertTrue(sut.itemQueue.isEmpty)
+//    }
     
 }
