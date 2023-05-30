6c9c5072e9f29617f087bb149e608ce698b284f2
JusBug <pksgwo123@gmail.com>
2023-05-30 14:06:03 +0900

    feat: Unit Test Target 추가 및 View와 코드를 연결

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -12,7 +12,39 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func allClear(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func clearEntry(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func division(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func multiplication(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func subtraction(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func addtion(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func compute(_ sender: UIButton) {
+        
+    }
+    
 }
 
