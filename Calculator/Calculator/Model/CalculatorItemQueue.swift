3bc7fe3143e56272b701f0f8dc2ef19897b8aaa9
myungsun <audtjs9987@naver.com>
2023-05-31 20:30:57 +0900

    refactor: CalculatorItemQueue 재구현

--- Calculator/Calculator/Model/CalculatorItemQueue.swift
+++ Calculator/Calculator/Model/CalculatorItemQueue.swift
@@ -5,6 +5,32 @@
 //  Created by myungsun on 2023/05/30.
 //
 
-struct CalculatorItemQueue {
+struct CalculatorItemQueue<T>: CalculateItem {
+    private var inStack: [T] = []
+    private var outStack: [T] = []
+    
+    mutating func enqueue(_ data: T) {
+        inStack.append(data)
+    }
 
+    mutating func dequeue() -> T? {
+        guard !isEmpty() else { return nil }
+        
+        if outStack.isEmpty {
+            while let data = inStack.popLast() {
+                outStack.append(data)
+            }
+        }
+        
+        return outStack.popLast()
+    }
+    
+    func isEmpty() -> Bool {
+        return inStack.isEmpty && outStack.isEmpty
+    }
+    
+    mutating func clear() {
+        inStack = []
+        outStack = []
+    }
 }
