0c040aad177e1c23136892da94f41b0ac0cc26e0
myungsun <audtjs9987@naver.com>
2023-05-31 20:14:14 +0900

    refactor: CalculatorItemQueue 클래스 타입에서 구조체 타입으로 변경

--- Calculator/Calculator/Model/CalculatorItemQueue.swift
+++ Calculator/Calculator/Model/CalculatorItemQueue.swift
@@ -5,24 +5,6 @@
 //  Created by myungsun on 2023/05/30.
 //
 
-final class CalculatorItemQueue {
-    private var items: [CalculateItem] = []
-    
-    func enqueue(item: CalculateItem) {
-        items.append(item)
-    }
-    
-    func dequeue() -> CalculateItem? {
-        guard !isEmpty() else { return nil }
-        
-        return items.removeFirst()
-    }
-    
-    func clear() {
-        items.removeAll()
-    }
-    
-    func isEmpty() -> Bool {
-        return items.isEmpty ? true : false
-    }
+struct CalculatorItemQueue {
+
 }
