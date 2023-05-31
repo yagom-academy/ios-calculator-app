e6dcbf13f0bf4a12d744b3cabb3c8c1c5c20f39f
agilestarskim <agilestars@kakao.com>
2023-06-01 01:49:53 +0900

    feat: ExpressionParser 내부구현

--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -6,14 +6,46 @@
 //
 
 enum ExpressionParser {
+    
     static func parse(from input: String) -> Formula {
-        return Formula(
+        
+        var formula: Formula = Formula(
             operands: CalculatorItemQueue<Double>(),
             operators: CalculatorItemQueue<Operator>()
         )
+        
+        let items = componentsByOperators(from: input)
+        
+        items.forEach { item in
+            if let `operator` = Operator(rawValue: Character(item)) {
+                formula.operators.enqueue(`operator`)
+            } else if let operand = Double(item) {
+                formula.operands.enqueue(operand)
+            } 
+        }
+        
+        return formula
     }
     
-    private func componentsByOperators(from input: String) -> [String] {      
-        return []
+    private static func componentsByOperators(from input: String) -> [String] {
+        
+        var currentNumber: String = ""
+        var tokens: [String] = []
+        
+        input.forEach { char in
+            switch char {
+            case "+", "-", "÷", "×":
+                if !currentNumber.isEmpty {
+                    tokens.append(currentNumber)
+                    currentNumber = ""
+                }
+                tokens.append(String(char))
+                
+            default:
+                currentNumber.append(char)
+            }
+        }
+        
+        return tokens
     }
 }
