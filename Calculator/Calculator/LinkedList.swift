faf6dd0e40ce45e50079a01be18bdf319d024f40
redmango1446 <redmango1446@redmango1446ui-MacBookAir.local>
2023-06-01 00:50:30 +0900

    feat:LinkedList 타입 생성

---
+++ Calculator/Calculator/LinkedList.swift
@@ -0,0 +1,11 @@
+//
+//  LinkedList.swift
+//  Calculator
+//
+//  Created by redmango1446 on 2023/06/01.
+//
+
+class LinkedList<T> {
+    private var head: Node<T>?
+    private var tail: Node<T>?
+}
