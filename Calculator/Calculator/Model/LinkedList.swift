35320689d5be525b821a132233813b43360ec2a1
DasanKim <tempzipp@gmail.com>
2023-05-31 09:44:09 +0900

    ✨feat: 리스트 내 첫번째 노드를 지워주는 함수 및 노드 출력 함수 추가

--- Calculator/Calculator/Model/LinkedList.swift
+++ Calculator/Calculator/Model/LinkedList.swift
@@ -7,7 +7,6 @@
 
 import Foundation
 
-class Node<T> {
 class Node<T: CalculateItem> {
     var data: T
     var next: Node?
@@ -18,37 +17,18 @@ class Node<T: CalculateItem> {
     }
 }
 
-class LinkedList<T> {
-    private var head: Node<T>?
-    var count: Int = Int()
-    var isEmpty: Bool = Bool()
 class LinkedList<T: CalculateItem> {
     private(set) var head: Node<T>?
     private(set) var tail: Node<T>?
     
-    init(head: Node<T>) {
-        self.head = head
-    }
-    
-    func enqueue(_ element: T) {
     var count: Int = 0
     var isEmpty: Bool { (head == nil) }
-        
+    
+    init(head: Node<T>? = nil, tail: Node<T>? = nil) {
+        self.head = head
+        self.tail = tail
     }
-    
-//    func dequeue() -> T? {
-//
-//
-//    }
-    
-    
-    
-    
-    func printAllList() {
-        var node = head
-        while node != nil {
-            print("\(String(describing: node?.data)) -> ", terminator: "")
-            node = node?.next
+        
     func append(_ data: T) {
         let node = Node(data: data)
         
@@ -62,39 +42,32 @@ class LinkedList<T: CalculateItem> {
         count += 1
     }
     
-    func getLastNodeData() -> Node<T>? {
+    func removeFirst() -> T? {
+        let node = head
+        var returnValue: T?
+        
+        if isEmpty {
+            returnValue = nil
+        } else if node?.next == nil {
+            head = nil
+            tail = nil
+            returnValue = node?.data
+            count -= 1
+        } else {
+            head = node?.next
+            returnValue = node?.data
+            count -= 1
+        }
+    
+        return returnValue
+    }
+    
+    func printAllList() {
         var node = head
         while node != nil {
+            print("\(String(describing: node?.data)) -> ", terminator: "")
             node = node?.next
         }
-        return node
     }
     
-    // 삽입할 경우
-    // 1. Adds a new element at the end of the array.
-    func append(data: T) {
-        var node = head
-        while node?.next != nil {
-            node = node?.next
-        }
-        node?.next = Node(data: data)
-    }
-    
-    // 2. Inserts a new element at the specified position.
-    func insert(data: T, at index: Int) {
-        var node = head
-        for _ in 0..<index-1 {
-            if node?.next == nil { break }
-            node = node?.next
-        }
-        
-        let nextNode = node?.next
-        node?.next = Node(data: data)
-        node?.next?.next = nextNode
-    }
-    
-    // 삭제할 경우
-    // 1. remove(at:): Removes and returns the element at the specified position.
-    // 2. removeFirst(): Removes and returns the first element of the collection.
-    // 3. removeLast(): Removes and returns the last element of the collection.
 }
