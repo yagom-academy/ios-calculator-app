a4d47e95a1077cc972f4574dbdbae2ed4f7e9dad
JusBug <pksgwo123@gmail.com>
2023-05-31 15:51:56 +0900

    refactor: Unit Test Target 주석 제거

--- Calculator/CalculatorTests/CalculatorTests.swift
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -22,39 +22,30 @@ final class CalculatorTests: XCTestCase {
     }
     
     func test_enqueue를_호출하여_1과2와3을_넣었을때_개수가_3이_나오는지() {
-        // given
-        
-        // when
         sut.enqueue("1")
         sut.enqueue("-")
         sut.enqueue("2")
         
-        // then
         XCTAssertEqual(sut.count, 3)
     }
+    
     func test_dequeue를_호출하여_1과2와3에서_3을_제거할때_개수가_2개인지() {
-        // given 주어지다 / 무언가 주어졌을 때
         sut.enqueue("3")
         sut.enqueue("+")
         sut.enqueue("4")
         
-        // when 언제 / 어ㄸ 결과는 이거다
         XCTAssertEqual(sut.dequeue(), "3")
-        // then 그렇다면 / 그렇다면 이 케이스는 맞을까?
+        
         XCTAssertEqual(sut.count, 2)
     }
     
     func test_clear이_호출되면_빈배열이_반환되는지() {
-        // given 주어지다 / 무언가 주어졌을 때
-        // 경계
         sut.enqueue("8")
         sut.enqueue("x")
         sut.enqueue("9")
         
-        // when 언제 / 어ㄸ너 결과는 이거다
         sut.clear()
         
-        // then 그렇다면 / 그렇다면 이 케이스는 맞을까?
         XCTAssertTrue(sut.isEmpty)
         
     }
