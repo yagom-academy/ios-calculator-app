117874e93c1fe7741aa1f8179be61db049f07a5b
Kobe <mskang@xingxingmobility.com>
2023-05-31 09:10:19 +0900

    test: 배열에 값이 들어가는것을 확인하기 위한 테스트 생성 및 확인.

--- Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
+++ Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
@@ -9,12 +9,26 @@ import XCTest
 @testable import Calculator
 
 final class CalculatorItemQueueTests: XCTestCase {
-
+	var sut: CalculatorItemQueue!
+	
     override func setUpWithError() throws {
-
+		try super.setUpWithError()
+		sut = CalculatorItemQueue()
     }
 
     override func tearDownWithError() throws {
-	
+		try super.tearDownWithError()
+		sut = nil
     }
+	
+	func tests_배열에_값이_들어간다() {
+		// given
+		let input: () = sut.addElement(1)
+		
+		// when
+		let result: () = input
+		
+		// then
+		XCTAssertNotNil(result)
+	}
 }
