1a647d86abf4c81758cb0712bb3ffbbd8c3aac80
DasanKim <tempzipp@gmail.com>
2023-05-31 09:39:18 +0900

    ✅test: 리스트 내 removeFirst 대한 테스트 추가

--- Calculator/LinkedListTests/LinkedListTests.swift
+++ Calculator/LinkedListTests/LinkedListTests.swift
@@ -42,4 +42,41 @@ final class LinkedListTests: XCTestCase {
         // then
         XCTAssertEqual(sut.tail?.data, 2)
     }
+    
+    func test_노드가없을때_removeFirst를하면_return값은nil이다() {
+        // given
+        // 아무것도 안들어감
+        
+        // when
+        let result = sut.removeFirst()
+        
+        // then
+        XCTAssertNil(result)
+    }
+    
+    func test_노드가하나만있을때_removeFirst를하면_head와tail은_nil이된다() {
+        // given
+        sut.append(1)
+        
+        // when
+        let result = sut.removeFirst()
+        
+        // then
+        XCTAssertNil(sut.head)
+        XCTAssertNil(sut.tail)
+    }
+    
+    func test_removeFirst를하면_처음노드의data를_반환한다() {
+        // given
+        sut.append(1)
+        sut.append(2)
+        sut.append(3)
+        let expectaiton = 1
+        
+        // when
+        let result = sut.removeFirst()
+        
+        // then
+        XCTAssertEqual(result, expectaiton)
+    }
 }
