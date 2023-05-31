6d48bb8c4da78b632cc9a4801f768c4afb4a7d4d
Kobe <mskang@xingxingmobility.com>
2023-05-31 09:31:41 +0900

    refactor: enqueue에 반환값 생성

--- Calculator/Calculator/Model/CalculatorItemQueue.swift
+++ Calculator/Calculator/Model/CalculatorItemQueue.swift
@@ -13,11 +13,12 @@ protocol CalculateItem {
 struct CalculatorItemQueue: CalculateItem {
 	private var elements: [Int] = []
 	
-	mutating func enqueue(_ value: Int) {
+	mutating func enqueue(_ value: Int) -> [Int] {
 		elements.append(value)
+		return elements
 	}
 	
-	mutating func dequeue(_ value: Int) {
-		
+	mutating func dequeue() -> Int? {
+		return elements.removeFirst()
 	}
 }
