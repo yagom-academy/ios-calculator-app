b0531cffea3ba62ee5e7160e21d72a400b4e2f11
redmango1446 <redmango1446@redmango1446ui-MacBookAir.local>
2023-06-01 04:30:50 +0900

    test:불필요한 테스트 코드 삭제

--- Calculator/CalculatorTests/LinkedListTests.swift
+++ Calculator/CalculatorTests/LinkedListTests.swift
@@ -21,48 +21,6 @@ final class LinkedListTests: XCTestCase {
         try super.tearDownWithError()
     }
     
-    func test_LinkedList의_head는_nil이다() {
-        // given
-        // when
-        let result = sut.checkHeadValue()
-        // then
-        XCTAssertNil(result)
-    }
-    
-    func test_LinkedList의_tail은_nil이다() {
-        // given
-        // when
-        let result = sut.checkTailValue()
-        // then
-        XCTAssertNil(result)
-    }
-    
-    func test_append_최초로_append_호출시_head와_tail은_같은_노드_data를_가진다() {
-        // given
-        sut.append(1)
-        // when
-        let headValue = sut.checkHeadValue()?.data
-        let tailValue = sut.checkTailValue()?.data
-        // then
-        XCTAssertEqual(headValue, tailValue)
-    }
-    
-    func test_append_호출시_기존_data가_존재할_경우_head는_최초로_입력된_data를_그대로_가지고_tail은_새로_입력된_data를_가진다() {
-        // given
-        sut.append(1)
-        sut.append(2)
-        sut.append(3)
-        sut.append(4)
-        // when
-        let headValue = sut.checkHeadValue()?.data
-        let tailValue = sut.checkTailValue()?.data
-        let headExpectedValue = 1
-        let tailExpectedValue = 4
-        // then
-        XCTAssertEqual(headValue, headExpectedValue)
-        XCTAssertEqual(tailValue, tailExpectedValue)
-    }
-    
     func test_removeFirst_호출시_head는_다음_data를_가진다() {
         // given
         sut.append(1)
