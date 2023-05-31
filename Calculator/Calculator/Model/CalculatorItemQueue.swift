9e10b830b832d4fb8a954c4047c7c8f7d4459804
Hoon94 <dleognsdl1@naver.com>
2023-05-31 17:13:44 +0900

    refactor: CalculatorItemQueue의 요소는 CalculateItem protocol을 준수하도록 수정

--- Calculator/Calculator/Model/CalculatorItemQueue.swift
+++ Calculator/Calculator/Model/CalculatorItemQueue.swift
@@ -5,9 +5,7 @@
 //  Created by Daehoon Lee on 2023/05/31.
 //
 
-import Foundation
-
-struct CalculatorItemQueue<T> {
+struct CalculatorItemQueue<T: CalculateItem> {
     var linkedList: LinkedList<T> = LinkedList()
     
     mutating func enqueue(_ element: T) {
