48af9349b7b0522d5df50248bef03045a2485d99
redmango1446 <redmango1446@redmango1446ui-MacBookAir.local>
2023-06-01 00:36:54 +0900

    refactor: LinkedList방식의 Queue구현을 위해 기존 코드 삭제

--- Calculator/Calculator/CalculatorItemQueue.swift
+++ Calculator/Calculator/CalculatorItemQueue.swift
@@ -6,28 +6,5 @@
 //
 
 struct CalculatorItemQueue: CalculateItem {
-    private var enqueueStack: [String] = []
-    private var dequeueStack: [String] = []
     
-    var isEmpty: Bool {
-        return enqueueStack.isEmpty && dequeueStack.isEmpty
-    }
-    
-    mutating func enqueue(_ element: String) {
-        enqueueStack.append(element)
-    }
-    
-    mutating func dequeue() -> String? {
-        if dequeueStack.isEmpty {
-            dequeueStack = enqueueStack.reversed()
-            enqueueStack.removeAll()
-        }
-        
-        return dequeueStack.popLast()
-    }
-    
-    mutating func clear() {
-        enqueueStack.removeAll()
-        dequeueStack.removeAll()
-    }
 }
