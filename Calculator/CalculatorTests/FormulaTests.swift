9db9ac47ef585fe7b2a3ed4502a3cbcaac547fb3
agilestarskim <agilestars@kakao.com>
2023-06-01 02:56:47 +0900

    test: 연산자가 여러 개일 때 테스트 추가

--- Calculator/CalculatorTests/FormulaTests.swift
+++ Calculator/CalculatorTests/FormulaTests.swift
@@ -22,7 +22,7 @@ final class FormulaTests: XCTestCase {
     }
 
     func test_lhs_rhs_operator가_하나이면서_정수일_때_result를_호출하면_값을_정상적으로_계산해서_반환한다() throws {
-        //given
+        // given
         let operands1: [Double] = [1, 2]
         let operands2: [Double] = [10, 150]
         let operands3: [Double] = [-10, 20]
@@ -43,13 +43,13 @@ final class FormulaTests: XCTestCase {
         let expectation3 = -0.5
         let expectation4 = 5500.0
         
-        //when
+        // when
         let result1 = try formul1.result()
         let result2 = try formul2.result()
         let result3 = try formul3.result()
         let result4 = try formul4.result()
         
-        //then
+        // then
         XCTAssertEqual(result1, expectation1)
         XCTAssertEqual(result2, expectation2)
         XCTAssertEqual(result3, expectation3)
@@ -57,7 +57,7 @@ final class FormulaTests: XCTestCase {
     }
     
     func test_lhs_rhs_operator가_하나이면서_소수일_때_result를_호출하면_값을_정상적으로_계산해서_반환한다() throws {
-        //given
+        // given
         let operands1: [Double] = [1.3, 2.8]
         let operands2: [Double] = [10.3, -150.39]
         let operands3: [Double] = [-10.48, 20.01]
@@ -77,22 +77,38 @@ final class FormulaTests: XCTestCase {
         let expectation2 = 160.69
         let expectation3 = -0.5237381309345327
         let expectation4 = 5525.5199999999995
-        //when
+        
+        // when
         let result1 = try formul1.result()
         let result2 = try formul2.result()
         let result3 = try formul3.result()
         let result4 = try formul4.result()
         
-        //then
+        // then
         XCTAssertEqual(result1, expectation1)
         XCTAssertEqual(result2, expectation2)
         XCTAssertEqual(result3, expectation3)
         XCTAssertEqual(result4, expectation4)
     }
     
+    func test_lhs_rhs_operator가_여러_개이면_앞에서부터_순서대로_연산한_후_연산_결과를_반환한다() throws {
+        // given
+        let operands = CalculatorItemQueue<Double>([1, 2, 5, 2, 2, 20, 5])
+        let operators = CalculatorItemQueue<Operator>([.add, .add, .divide, .multiply, .add, .subtract])
+        sut = Formula(operands: operands, operators: operators)
+        
+        let expectation = 23.0
+        
+        // when
+        let result = try sut.result()
+        // then
+        XCTAssertEqual(result, expectation)
+    }
+    
     func test_lhs_rhs_operator가_하나이면서_0으로_나누면_DivisionByZero_에러가_발생한다() throws {
         //given
-        let operands = CalculatorItemQueue<Double>([1, 0])
+        let random: Double = Double.random(in: -100...100)
+        let operands = CalculatorItemQueue<Double>([random, 0])
         let operators = CalculatorItemQueue<Operator>([.divide])
         sut = Formula(operands: operands, operators: operators)
         
