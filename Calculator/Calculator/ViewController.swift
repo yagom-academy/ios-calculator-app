cb0d49d506d12c7a0179d8f0e35b6c5d2efb95b7
JusBug <pksgwo123@gmail.com>
2023-05-31 15:48:39 +0900

    refactor: Unit Test Target 수정

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -13,57 +13,5 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     
     }
-    
-    @IBAction func touchDigit(_ sender: UIButton) {
-        let digit = sender.currentTitle!
-        
-        myQueue.enqueue(digit)
-        print(myQueue)
-    }
-    
-    @IBAction func allClear(_ sender: UIButton) {
-        myQueue.clear()
-        print(myQueue)
-    }
-    
-    @IBAction func clearEntry(_ sender: UIButton) {
-        myQueue.dequeue()
-        print(myQueue)
-    }
-    
-    @IBAction func division(_ sender: UIButton) {
-        let calculation = sender.currentTitle!
-        
-        myQueue.enqueue(calculation)
-        print(myQueue)
-    }
-    
-    @IBAction func multiplication(_ sender: UIButton) {
-        let calculation = sender.currentTitle!
-        
-        myQueue.enqueue(calculation)
-        print(myQueue)
-    }
-    
-    @IBAction func subtraction(_ sender: UIButton) {
-        let calculation = sender.currentTitle!
-        
-        myQueue.enqueue(calculation)
-        print(myQueue)
-    }
-    
-    @IBAction func addtion(_ sender: UIButton) {
-        let calculation = sender.currentTitle!
-        
-        myQueue.enqueue(calculation)
-        print(myQueue)
-    }
-    
-    @IBAction func compute(_ sender: UIButton) {
-        let calculation = sender.currentTitle!
-        
-        myQueue.enqueue(calculation)
-        print(myQueue)
-    }
 }
 
--- Calculator/CalculatorTests/CalculatorTests.swift
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -9,28 +9,53 @@ import XCTest
 @testable import Calculator
 
 final class CalculatorTests: XCTestCase {
+    var sut: CalculatorItemQueue<String>!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        try super.setUpWithError()
+        sut = CalculatorItemQueue()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        try super.tearDownWithError()
+        sut = nil
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func test_enqueue를_호출하여_1과2와3을_넣었을때_개수가_3이_나오는지() {
+        // given
+        
+        // when
+        sut.enqueue("1")
+        sut.enqueue("-")
+        sut.enqueue("2")
+        
+        // then
+        XCTAssertEqual(sut.count, 3)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
+    func test_dequeue를_호출하여_1과2와3에서_3을_제거할때_개수가_2개인지() {
+        // given 주어지다 / 무언가 주어졌을 때
+        sut.enqueue("3")
+        sut.enqueue("+")
+        sut.enqueue("4")
+        
+        // when 언제 / 어ㄸ 결과는 이거다
+        XCTAssertEqual(sut.dequeue(), "3")
+        // then 그렇다면 / 그렇다면 이 케이스는 맞을까?
+        XCTAssertEqual(sut.count, 2)
     }
-
+    
+    func test_clear이_호출되면_빈배열이_반환되는지() {
+        // given 주어지다 / 무언가 주어졌을 때
+        // 경계
+        sut.enqueue("8")
+        sut.enqueue("x")
+        sut.enqueue("9")
+        
+        // when 언제 / 어ㄸ너 결과는 이거다
+        sut.clear()
+        
+        // then 그렇다면 / 그렇다면 이 케이스는 맞을까?
+        XCTAssertTrue(sut.isEmpty)
+        
+    }
 }
