e8ad0446461e5b8056d7c5be715cee236cbf898a
agilestarskim <agilestars@kakao.com>
2023-06-01 01:48:15 +0900

    feat: Formula 내부구현

--- Calculator/Calculator/Model/Formula.swift
+++ Calculator/Calculator/Model/Formula.swift
@@ -6,10 +6,21 @@
 //
 
 struct Formula {
-    let operands: CalculatorItemQueue<Double>
-    let operators: CalculatorItemQueue<Operator>
+    var operands = CalculatorItemQueue<Double>()
+    var operators = CalculatorItemQueue<Operator>()
     
-    func result() -> Double {
-        return 0.0
+    mutating func result() throws -> Double {
+        var temp: Double = 0.0
+        
+        let lhs = operands.dequeue() ?? 0.0
+        let rhs = operands.dequeue() ?? 0.0
+        temp = try operators.dequeue()?.calculate(lhs: lhs, rhs: rhs) ?? 0.0
+        
+        while !operands.isEmpty {
+            let rhs = operands.dequeue() ?? 0.0
+            temp = try operators.dequeue()?.calculate(lhs: temp, rhs: rhs) ?? 0.0
+        }
+        
+        return temp
     }
 }
