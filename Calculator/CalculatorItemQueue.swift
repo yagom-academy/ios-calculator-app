ddb6096165101df03e71ac1ea0c8a647d8f4eef6
iOS-Yetti <yechan123456@gmail.net>
2023-06-01 00:20:04 +0900

    Queue 프로토콜 DoubleStackQueue로 네이밍 변경과 기능 수정 및 isEmpty메서드 연산 프로퍼티로 수정

--- Calculator/CalculatorItemQueue.swift
+++ Calculator/CalculatorItemQueue.swift
@@ -4,37 +4,39 @@
 //
 //  Created by Yetti on 2023/05/30.
 //
-protocol Queue {
+protocol DoubleStackQueue {
+    var isEmpty: Bool { get }
     mutating func enQueue(_ data: String)
     mutating func deQueue() -> String?
     func peek() -> String?
-    func isEmpty() -> Bool
     mutating func clear()
 }
 protocol CalculateItem { }
 
-struct CalculatorItemQueue: Queue, CalculateItem {
-    var queue: [String] = []
+struct CalculatorItemQueue: DoubleStackQueue, CalculateItem {
     
-    mutating func enQueue(_ data: String) {
-        queue.append(data)
+    var enQueueStack: [String] = []
+    var deQueueStack: [String] = []
+    
+    var isEmpty: Bool {
+        return enQueueStack.isEmpty && deQueueStack.isEmpty
+    }
+    
+    mutating func enQueue(_ element: String) {
+        enQueueStack.append(element)
     }
     
     mutating func deQueue() -> String? {
-        let frontData = (queue == []) ? nil : queue.removeFirst()
-        return frontData
+        let frontElement = (enQueueStack == []) ? nil : enQueueStack.removeFirst()
+        return frontElement
     }
 
     func peek() -> String? {
-        let frontData = (queue == []) ? nil : queue[0]
+        let frontData = (enQueueStack == []) ? nil : enQueueStack[0]
         return frontData
     }
     
     mutating func clear() {
-        queue.removeAll()
-    }
-    
-    func isEmpty() -> Bool {
-        <#code#>
+        enQueueStack.removeAll()
     }
 }
