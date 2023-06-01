8b6149d76601c881a0ee69f1726479bab27645ba
YaRkyungmin <kkgm94@naver.com>
2023-06-01 22:30:52 +0900

    🚚build: CalculatorItemQueueProtocol 파일 생성

---
+++ Calculator/Calculator/Test Double/CalculatorItemQueueProtocol.swift
@@ -0,0 +1,11 @@
+//
+//  CalculatorItemQueueProtocol.swift
+//  Calculator
+//
+//  Created by kangkyungmin on 2023/06/01.
+//
+
+protocol CalculatorItemQueueProtocol {
+    func enqueue()
+    func dequeue() -> Int
+}
