757ffb8e894a0512e30bcc3fe0543d67b10f04f0
iOS-Yetti <yechan123456@gmail.net>
2023-06-01 10:28:11 +0900

    enqueue메서드 테스트진행 및 기능 구현

--- Calculator/CalculatorItemQueue.swift
+++ Calculator/CalculatorItemQueue.swift
@@ -5,33 +5,34 @@
 //  Created by Yetti on 2023/05/30.
 //
 protocol DoubleStackQueue {
+    associatedtype T
     var isEmpty: Bool { get }
     
-    mutating func enQueue(_ data: String)
-    mutating func deQueue() -> String?
+    mutating func enQueue(_ element: T)
+    mutating func deQueue() -> T?
     mutating func clear()
-    func peek() -> String?
+    func peek() -> T?
 }
 protocol CalculateItem { }
 
-struct CalculatorItemQueue: DoubleStackQueue, CalculateItem {
-    var enQueueStack: [String] = []
-    var deQueueStack: [String] = []
+struct CalculatorItemQueue<T>: DoubleStackQueue, CalculateItem {
+    var enQueueStack: [T] = []
+    var deQueueStack: [T] = []
     
     var isEmpty: Bool {
         return enQueueStack.isEmpty && deQueueStack.isEmpty
     }
     
-    mutating func enQueue(_ element: String) {
-        
+    mutating func enQueue(_ element: T) {
+        enQueueStack.append(element)
     }
     
-    mutating func deQueue() -> String? {
-        return "a"
+    mutating func deQueue() -> T? {
+        return nil
     }
 
-    func peek() -> String? {
-        return "b"
+    func peek() -> T? {
+        return nil
     }
     
     mutating func clear() {
--- Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
+++ Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
@@ -9,7 +9,7 @@ import XCTest
 @testable import Calculator
 
 final class CalculatorItemQueueTests: XCTestCase {
-    var sut: CalculatorItemQueue!
+    var sut: CalculatorItemQueue<Any>!
 
     override func setUpWithError() throws {
         sut = CalculatorItemQueue()
@@ -35,59 +35,13 @@ final class CalculatorItemQueueTests: XCTestCase {
         XCTAssertFalse(result)
     }
     
-    
-    
-    
-   
-//    func test_enQueue에String값Hi를넣었을때_queue에String값Hi를저장한다() {
-//        sut.queue += ["Hi"]
-//        let expectation = "Hi"
-//
-//        let result: String = sut.queue[0]
-//
-//        XCTAssertEqual(result, expectation)
-//    }
-//
-//    func test_deQueue호출시_queue에요소가있다면_0번째인덱스값을반환한다() {
-//        sut.queue = ["1", "2", "3", "4", "5"]
-//        let expectation = "1"
-//
-//        let result = sut.deQueue()
-//
-//        XCTAssertEqual(result, expectation)
-//    }
-//
-//    func test_deQueue호출시_queue가비어있다면_nil을반환한다() {
-//        sut.queue = []
-//
-//        let result = sut.deQueue()
-//
-//        XCTAssertNil(result)
-//    }
-//
-//    func test_peek호출시_queue에0번인덱스값을_삭제하지않고반환한다() {
-//        sut.queue = ["1", "2", "3", "4", "5"]
-//        let expectation = "1"
-//
-//        let result = sut.peek()
-//
-//        XCTAssertEqual(result, expectation)
-//    }
-//
-//    func test_peek호출시_queue가비어있다면_nil을반환한다() {
-//        sut.queue = []
-//
-//        let result = sut.peek()
-//
-//        XCTAssertNil(result)
-//    }
-//
-//    func test_clear호출시_queue의요소들을_모두지운다() {
-//        sut.queue = ["1", "2", "3", "4", "5"]
-//        let expectation: [String] = []
-//
-//        sut.clear()
-//
-//        XCTAssertEqual(sut.queue, expectation)
-//    }
+    func test_enQueue에_문자열3을전달했을때_enQueueStack에문자열3을저장한다() {
+        let input = "3"
+        
+        sut.enQueue(input)
+        
+        XCTAssertEqual(sut.enQueueStack[0] as? String, input)
+    }
+
+
 }
