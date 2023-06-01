864eb83d421cf2aaeed24040948c85e91861c923
redmango1446 <redmango1446@redmango1446ui-MacBookAir.local>
2023-06-01 17:15:41 +0900

    test:enqueue를 하지않고 dequeue메소드를 호출하면 nil반환 테스트(Red)

--- Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
+++ Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
@@ -20,4 +20,13 @@ final class CalculatorItemQueueTests: XCTestCase {
         sut = nil
         try super.tearDownWithError()
     }
+    
+    func test_dequeue_enqueue를_하지_않고_dequeue를_하면_Nil이_반환된다() {
+        // given
+        // when
+        var result = sut.dequeue()
+        
+        // then
+        XCTAssertNil(result)
+    }
 }
