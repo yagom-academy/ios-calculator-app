9180a7a9fe1ca68830b7a78eafb488608050c76c
idinaloq <idinaloq@icloud.com>
2023-06-01 10:54:25 +0900

    refactor: Node의 제네릭타입 네이밍변경 T -> Value

--- Calculator/Calculator/Node.swift
+++ Calculator/Calculator/Node.swift
@@ -5,11 +5,11 @@
 //  Created by idinaloq on 2023/05/29.
 //
 
-final class Node<T> {
-    let data: T
-    var next: Node<T>?
+final class Node<Value> {
+    let data: Value
+    var next: Node<Value>?
     
-    init(data: T, next: Node? = nil) {
+    init(data: Value, next: Node? = nil) {
         self.data = data
         self.next = next
     }
