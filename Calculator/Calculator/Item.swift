edbb1911887eaf121d1758e3f74e0ce542dd6f14
Zion <lion2212@naver.com>
2023-06-02 16:13:07 +0900

    feat: Opearator 정의

--- Calculator/Calculator/Item.swift
+++ Calculator/Calculator/Item.swift
@@ -5,8 +5,17 @@
 //  Created by Hyungmin Lee on 2023/06/01.
 //
 
-final class Item<Element> {
+final class Item<Element: CalculateItem> {
     var next: Item?
+    var element: Element
+    
+    init(_ element: Element) {
+        self.element = element
+    }
+}
+
+final class Item2<Element> {
+    var next: CalculateItem?
     var element: Element
     
     init(_ element: Element) {
