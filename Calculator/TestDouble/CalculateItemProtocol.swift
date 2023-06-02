373e85a4e3862a55224be738d33763fe8bb3e69d
Kobe <mskang@xingxingmobility.com>
2023-06-03 06:09:49 +0900

    :sparkles: Feat: Test Double를 위한 CalculateItemProtocol 구현
    Test Double를 위한 빈 프로토콜인 CalculateItemProtocol을 구현했습니다.

---
+++ Calculator/TestDouble/CalculateItemProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  CalculateItemProtocol.swift
+//  Calculator
+//
+//  Created by Minseong Kang on 2023/06/03.
+//
+
+protocol CalculateItemProtocol { }
---
+++ Calculator/TestDouble/MockCalculatorItemQueue.swift
@@ -0,0 +1,8 @@
+//
+//  MockCalculatorItemQueue.swift
+//  Calculator
+//
+//  Created by Minseong Kang on 2023/06/03.
+//
+
+import Foundation
