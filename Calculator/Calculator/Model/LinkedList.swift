d373579d8dc485ff3011550dbed4fedd9e44c72a
Kobe <mskang@xingxingmobility.com>
2023-06-02 18:20:26 +0900

    :sparkles: Feat: LinkedList 프로토콜, LinkedList 클래스 구현.
    LinkedList 프로토콜을 구현하고 그 프로토콜을 채택하여 LinkedList 클래스를 구현하였습니다.

--- Calculator/Calculator/Model/LinkedList.swift
+++ Calculator/Calculator/Model/LinkedList.swift
@@ -5,3 +5,36 @@
 //  Created by Minseong Kang on 2023/06/02.
 //
 
+protocol LinkedListProtocol {
+	associatedtype T: Any
+	func enqueue(value: T)
+	func dequeue()
+}
+
+class LinkedList<T>: LinkedListProtocol {
+	private var head: Node<T>?
+	private var tail: Node<T>?
+	
+	var isEmpty: Bool {
+		return head == nil
+	}
+	
+	func enqueue(value: T) {
+		let newNode = Node(value: value)
+		if let tailNode = tail {
+			tailNode.next = newNode
+		} else {
+			head = newNode
+		}
+		tail = newNode
+	}
+	
+	func dequeue() {
+		if let nextNode = head?.next {
+			head = nextNode
+		} else {
+			head = nil
+			tail = nil
+		}
+	}
+}
