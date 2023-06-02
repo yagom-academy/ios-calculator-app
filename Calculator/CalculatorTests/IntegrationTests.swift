b67d167e9720671f6c0bdf9e68f5c720e4ad3da9
agilestarskim <agilestars@kakao.com>
2023-06-02 19:17:55 +0900

    test: 배열로 구현했을 시 퍼포먼스 테스트

--- Calculator/CalculatorTests/IntegrationTests.swift
+++ Calculator/CalculatorTests/IntegrationTests.swift
@@ -2,34 +2,172 @@
 //  IntegrationTests.swift
 //  CalculatorTests
 //
-//  Created by 김민성 on 2023/06/02.
+//  Created by Minsup on 2023/06/02.
 //
 
 import XCTest
+@testable import Calculator
 
 final class IntegrationTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func test_정상적인_수식을_parse해서_result를_호출하면_값을_계산해서_반환한다() throws {
+        // given
+        let input1 = "+5+3×10÷2"
+        let input2 = "50+32×+100÷10"
+        let input3 = "50.1+32.5×10.13÷2.5"
+        let input4 = "5.153+3.55×10÷2"
+        let input5 = "5-3×10.00÷2.5123"
+        
+        let expectation1 = 40.0
+        let expectation2 = 820.0
+        let expectation3 = 334.6952
+        let expectation4 = 43.515
+        let expectation5 = 7.960832703100744
+        
+        // when
+        var formula1 = try ExpressionParser.parse(from: input1)
+        var formula2 = try ExpressionParser.parse(from: input2)
+        var formula3 = try ExpressionParser.parse(from: input3)
+        var formula4 = try ExpressionParser.parse(from: input4)
+        var formula5 = try ExpressionParser.parse(from: input5)
+        
+        let result1 = try formula1.result()
+        let result2 = try formula2.result()
+        let result3 = try formula3.result()
+        let result4 = try formula4.result()
+        let result5 = try formula5.result()
+        
+        // then
+        XCTAssertEqual(result1, expectation1)
+        XCTAssertEqual(result2, expectation2)
+        XCTAssertEqual(result3, expectation3)
+        XCTAssertEqual(result4, expectation4)
+        XCTAssertEqual(result5, expectation5)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func test_0으로_나누는_수식을_parse해서_result를_호출하면_divisionByZero_에러가_발생한다() throws {
+        // given
+        let input1 = "5÷0"
+        let input2 = "50+32×100÷0"
+        let input3 = "50.1+32.5×10.13÷0"
+        let input4 = "5.153÷0+3.55×-10"
+        let input5 = "-5×10.00÷0"
+        
+        // when
+        let formula1 = try ExpressionParser.parse(from: input1)
+        let formula2 = try ExpressionParser.parse(from: input2)
+        let formula3 = try ExpressionParser.parse(from: input3)
+        let formula4 = try ExpressionParser.parse(from: input4)
+        let formula5 = try ExpressionParser.parse(from: input5)
+        // then
+        try [formula1, formula2, formula3, formula4, formula5].forEach { formula in
+            var formula = formula
+            XCTAssertThrowsError(try formula.result()) { error in
+                if let error = error as? CalculationError {
+                    XCTAssertEqual(error, .divisionByZero)
+                } else {
+                    XCTFail("예상하지 않은 에러 출력 \(error)")
+                }
+            }
         }
     }
+    
+    func test_연산자와_피연산자의_순서가_맞지않으면_invalidInput_에러가_발생한다() throws {
+        // given
+        let input1 = "÷5÷0"
+        let input2 = "50+32×100÷+5÷"
+        let input3 = "50.1++32.5×10.13--"
+        let input4 = "5.153÷0+3.55×-10-"
+        let input5 = "--5×10.00÷1"
+        
+        // then
+        try [input1, input2, input3, input4, input5].forEach { input in
+            XCTAssertThrowsError(try ExpressionParser.parse(from: input)) { error in
+                if let error = error as? CalculationError {
+                    XCTAssertEqual(error, .invalidInput)
+                } else {
+                    XCTFail("예상하지 않은 에러 출력 \(error)")
+                }
+            }
+        }        
+    }
+    
+    func test_10개의_피연산자_계산_속도_측정() {
+        // given
+        let input = "10+10+10+10+10+10+10+10+10+10"
+        
+        measure {
+            do {
+                var formula = try ExpressionParser.parse(from: input)
+                _ = try formula.result()
+            } catch {
+                
+            }
+        }
+    }
+    
+    func test_2자리수_100개의_피연산자_계산_속도_측정() {
+        // given
+        var input: String = ""
+        for time in 0...99 {
+            input += time != 99 ? "10+" : "10"
+        }
 
+        measure {
+            do {
+                var formula = try ExpressionParser.parse(from: input)
+                _ = try formula.result()
+            } catch {
+                if let error = error as? CalculationError {
+                    print(error)
+                } else {
+                    print("알 수 없는 에러: \(error)")
+                }
+            }
+        }
+    }
+    
+    func test_10자리_수_100개의_피연산자_계산_속도_측정() {
+        // given
+        var input: String = ""
+        for time in 0...99 {
+            input += time != 99 ? "1000000000+" : "1000000000"
+        }
+
+        measure {
+            do {
+                var formula = try ExpressionParser.parse(from: input)
+                _ = try formula.result()
+            } catch {
+                if let error = error as? CalculationError {
+                    print(error)
+                } else {
+                    print("알 수 없는 에러: \(error)")
+                }
+            }
+        }
+    }
+    
+    func test_랜덤한_피연산자_1000개_계산_속도_측정() {
+        var input: String = ""
+        for time in 0...999 {
+            let random: Double = Double.random(in: -1000...1000)
+            let randomString = time != 999 ? String(random) + "+" : String(random)
+            input += randomString
+        }
+        print(input)
+        
+        measure {
+            do {
+                var formula = try ExpressionParser.parse(from: input)
+                _ = try formula.result()
+            } catch {
+                if let error = error as? CalculationError {
+                    print(error)
+                } else {
+                    print("알 수 없는 에러: \(error)")
+                }
+            }
+        }
+    }
 }
