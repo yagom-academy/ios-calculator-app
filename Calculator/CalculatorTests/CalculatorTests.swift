77c6c40c3dcadf4c646f3e4e79ae47978099f6c0
Whales <Whales.w.Jin@gmail.com>
2023-06-03 02:44:17 +0900

    test: 변경에 따른 테스트 함수 수정

--- Calculator/CalculatorTests/CalculatorTests.swift
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -9,49 +9,62 @@ import XCTest
 @testable import Calculator
 
 final class CalculatorTests: XCTestCase {
-    var sut: CalculatorItemQueue<Int>!
-    
+    var sut: CalculatorItemQueue<Double>!
+
     override func setUpWithError() throws {
         sut = CalculatorItemQueue()
     }
-    
+
     override func tearDownWithError() throws {
         sut = nil
     }
-    
-    func test_enqueue함수호출시_배열의원소개수가_늘어나는지() {
+
+    func test_enqueue함수호출시_queue의원소개수가_늘어나는지() {
         // given
-        let input = 3
-        let expectation = 1
+        let expectation: Int = 3
         // when
-        sut.enqueue(input)
+        sut.enqueue(3.25)
+        sut.enqueue(4)
+        sut.enqueue(5)
         // then
         XCTAssertEqual(sut.count, expectation)
     }
-    
-    func test_enqueue함수호출시_배열의원소가_순서대로_늘어나는지() {
+
+    func test_enqueue함수호출시_queue의_마지막원소로_삽입되는지() {
         // given
-        let expectation = [2, 3]
+        let expectation: Double = 3
         // when
         sut.enqueue(2)
         sut.enqueue(3)
         // then
-        XCTAssertEqual(sut.readCalculatorItemQueue(), expectation)
+        XCTAssertEqual(sut.readLastData(), expectation)
     }
     
-    func test_dequeue함수호출시_배열의첫번째원소가_사라지는지() {
+    func test_dequeue함수호출시_queue의원소의개수가_줄어드는지() {
         // given
+        let expectation: Int = 2
         sut.enqueue(2)
         sut.enqueue(3)
         sut.enqueue(4)
-        let expectation = [3, 4]
         // when
         sut.dequeue()
         // then
-        XCTAssertEqual(sut.readCalculatorItemQueue(), expectation)
+        XCTAssertEqual(sut.count, expectation)
     }
-    
-    func test_clearQueue함수호출시_배열의원소가_다삭제되면_true를_반환하는지() {
+
+    func test_dequeue함수호출시_queue의첫번째원소가_사라지는지() {
+        // given
+        let expectation: Double = 3
+        sut.enqueue(2)
+        sut.enqueue(3)
+        sut.enqueue(4)
+        // when
+        sut.dequeue()
+        // then
+        XCTAssertEqual(sut.readFirstData(), expectation)
+    }
+
+    func test_clearQueue함수호출시_queue의isEmpty가_true가되는지() {
         // given
         sut.enqueue(2)
         sut.enqueue(3)
