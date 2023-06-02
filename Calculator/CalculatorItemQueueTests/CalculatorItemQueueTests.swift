1c9d44eb4dc80f27fce5d3f508765cfccb1b1fa9
Kobe <mskang@xingxingmobility.com>
2023-06-03 06:25:21 +0900

    :white_check_mark: Test: Mock Object와 Dummy Data 활용한 테스트 성공[GREEN]

--- Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
+++ Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
@@ -9,98 +9,16 @@ import XCTest
 @testable import Calculator
 
 final class CalculatorItemQueueTests: XCTestCase {
-	var sut: CalculatorItemQueue<Int>!
+	var sut: MockCalculatorItemQueue<Int>!
 	
 	override func setUpWithError() throws {
 		try super.setUpWithError()
-		sut = CalculatorItemQueue()
+		let dummyLinkedList = DummyLinkedList<Int>()
+		sut = MockCalculatorItemQueue(dummyList: dummyLinkedList)
 	}
 	
 	override func tearDownWithError() throws {
 		try super.tearDownWithError()
 		sut = nil
-	}
-	
-	func tests_enqueue함수호출시_elements배열안에_요소가_들어간다() {
-		// given
-		sut.enqueue(1)
-		let input = sut.getElementsForTest()
-		
-		// when
-		let result = [1]
-		
-		// then
-		XCTAssertEqual(input, result)
-	}
-	
-	func tests_enqueue함수를_여러번_호출시_elements배열안에_여러개의_요소가_들어간다() {
-		// given
-		sut.enqueue(1)
-		sut.enqueue(2)
-		sut.enqueue(3)
-		let input = sut.getElementsForTest()
-		
-		// when
-		let result = [1,2,3]
-		
-		// then
-		XCTAssertEqual(input, result)
-	}
-	
-	func tests_dequeue함수를_호출시_elements가_빈배열일경우_nil을_반환한다() {
-		// given
-		let input = sut.dequeue()
-		// then
-		XCTAssertNil(input)
-	}
-	
-	func tests_dequeue함수를_호출시_elements내부의_요소가_하나씩_빠진다() {
-		//given
-		sut.enqueue(1)
-		sut.enqueue(2)
-		sut.enqueue(3)
-		guard let input = sut.dequeue() else { return }
-		
-		// when
-		let result = 1
-		
-		// then
-		XCTAssertEqual(input, result)
-	}
-	
-	func tests_peek함수를_호출시_elements가_빈배열일경우_nil을_반환한다() {
-		// given
-		let input = sut.peek()
-		// when
-		
-		// then
-		XCTAssertNil(input)
-	}
-	
-	func tests_peek함수를_호출시_elements의_첫번쨰_요소를_뽑아낸다() {
-		// given
-		sut.enqueue(1)
-		sut.enqueue(2)
-		sut.enqueue(3)
-		guard let input = sut.peek() else { return }
-		
-		// when
-		let result = 1
-		
-		XCTAssertEqual(input, result)
-	}
-	
-	func tests_removeAll함수를_호출시_모든요소가_지워진다() {
-		// given
-		sut.enqueue(1)
-		sut.enqueue(2)
-		sut.enqueue(3)
-		sut.clear()
-		let expection: [Int] = []
-		
-		// when
-		let result = sut.getElementsForTest()
-		
-		XCTAssertEqual(result, expection)
 	}
 }
