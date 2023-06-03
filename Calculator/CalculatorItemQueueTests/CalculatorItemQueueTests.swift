a131b5e6f3df181a040a4da27cb256fed02c634f
조호준 <johojun@johojun-ui-MacBookAir.local>
2023-06-03 19:11:41 +0900

    test: 테스트 케이스 String에서 Double로 수정
    요구사항 UML에 따라 String에서 CalculateItem을 해제했기 때문에 Double로 변경

--- Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
+++ Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
@@ -9,7 +9,7 @@ import XCTest
 @testable import Calculator
 
 final class CalculatorItemQueueTests: XCTestCase {
-    var sut: CalculatorItemQueue<String>!
+    var sut: CalculatorItemQueue<Double>!
 
     override func setUpWithError() throws {
         sut = CalculatorItemQueue()
@@ -21,47 +21,20 @@ final class CalculatorItemQueueTests: XCTestCase {
     
     func test_init에1_2_3을넣으면_enqueuedCalculatorItems에1_2_3이있다() {
         for input in 1...3 {
-            sut.enqueue("\(input)")
+            sut.enqueue(Double(input))
         }
-        let expectation = ["1", "2", "3"]
+        let expectation = [1.0, 2.0, 3.0]
         
         let result = sut.enqueuedCalculatorItems
         
         XCTAssertEqual(expectation, result)
     }
     
-    func test_enqueue에1을넣으면_enqueuedCalculatorItems에1이있다() {
-        let input = "1"
-        
-        sut.enqueue(input)
-        let result = sut.enqueuedCalculatorItems.first
-        
-        XCTAssertEqual(input, result)
-    }
-    
-    func test_enqueue에더하기기호를넣으면_enqueuedCalculatorItems에더하기기호가있다() {
-        let input = "+"
-        
-        sut.enqueue(input)
-        let result = sut.enqueuedCalculatorItems.first
-        
-        XCTAssertEqual(input, result)
-    }
-    
     func test_enqueuedCalculatorItems에1_2가있으면_dequeue를했을때_1이반환된다() {
-        for item in 1...2 {
-            sut.enqueue("\(item)")
+        for input in 1...2 {
+            sut.enqueue(Double(input))
         }
-        let expectation = "1"
-        
-        let result = sut.dequeue()
-        
-        XCTAssertEqual(result, expectation)
-    }
-    
-    func test_enqueuedCalculatorItems에더하기기호가있으면_dequeue를했을때_더하기기호가반환된다() {
-        sut.enqueue("+")
-        let expectation = "+"
+        let expectation = 1.0
         
         let result = sut.dequeue()
         
@@ -69,7 +42,7 @@ final class CalculatorItemQueueTests: XCTestCase {
     }
     
     func test_enqueuedCalculatorItems에1이있으면_dequeue를했을때_enqueuedCalculatorItems가비어있다() {
-        sut.enqueue("1")
+        sut.enqueue(1.0)
         
         sut.dequeue()
         let result = sut.enqueuedCalculatorItems.isEmpty
