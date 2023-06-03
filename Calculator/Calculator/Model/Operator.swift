fe922a85fc96070d3a94a504f6d36f67623bda80
MaryJo-github <jotjdal0215@gmail.com>
2023-06-03 17:06:19 +0900

    feat: Operator 타입 구현

--- Calculator/Calculator/Model/Operator.swift
+++ Calculator/Calculator/Model/Operator.swift
@@ -6,28 +6,29 @@
 //
 
 enum Operator: Character, CaseIterable {
-    case add
-    case subtract
-    case divide
-    case multiply
+    case add = "+"
+    case subtract = "-"
+    case divide = "/"
+    case multiply = "*"
     
     func calculate(lhs: Double, rhs: Double) -> Double {
-
+        switch self {
+        case .add:
+            return add(lhs: lhs, rhs: rhs)
+        case .subtract:
+            return subtract(lhs: lhs, rhs: rhs)
+        case .divide:
+            return divide(lhs: lhs, rhs: rhs)
+        case .multiply:
+            return multiply(lhs: lhs, rhs: rhs)
+        }
     }
     
-    func add(lhs: Double, rhs: Double) -> Double {
-        
-    }
+    private func add(lhs: Double, rhs: Double) -> Double { lhs + rhs }
     
-    func subtract(lhs: Double, rhs: Double) -> Double {
-        
-    }
+    private func subtract(lhs: Double, rhs: Double) -> Double { lhs - rhs }
     
-    func divide(lhs: Double, rhs: Double) -> Double {
-        
-    }
+    private func divide(lhs: Double, rhs: Double) -> Double { lhs / rhs }
     
-    func multiply(lhs: Double, rhs: Double) -> Double {
-        
-    }
+    private func multiply(lhs: Double, rhs: Double) -> Double { lhs * rhs }
 }
