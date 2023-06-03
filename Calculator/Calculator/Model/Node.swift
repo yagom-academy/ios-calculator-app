a4c766e16fb99c19d6e64013face227d8865189c
Kobe <mskang@xingxingmobility.com>
2023-06-03 14:46:20 +0900

    :recycle: Refactor: Node 클래스와 T 타입 Equatable 채택.
    Node 클래스가 Equatable을 채택함으로써 Equtable을 준수하도록하였습니다. 그래서 대소비교가 가능하게 하였습니다. T 타입 역시 Equatable을 채택했습니다.

--- Calculator/Calculator/Model/Node.swift
+++ Calculator/Calculator/Model/Node.swift
@@ -5,7 +5,11 @@
 //  Created by Minseong Kang on 2023/06/02.
 //
 
-class Node<T: Any> {
+class Node<T: Equatable>: Equatable {
+	static func == (lhs: Node<T>, rhs: Node<T>) -> Bool {
+		return lhs.value == rhs.value
+	}
+	
 	var value: T
 	var next: Node?
 	
