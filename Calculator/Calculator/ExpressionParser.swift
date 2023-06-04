3b90ff08d9f6502511484dd5ec181e0db8a215bc
Zion <lion2212@naver.com>
2023-06-04 14:34:44 +0900

    fix: removeFirst 사용 로직 삭제

--- Calculator/Calculator/ExpressionParser.swift
+++ Calculator/Calculator/ExpressionParser.swift
@@ -7,20 +7,20 @@
 
 enum ExpressionParser {
     static func parse(from input: String) -> Formula {
-        
+        let operatorComponents = input.compactMap { Operator(rawValue: $0) }
         var operandComponents = componentsByOperators(from: input).compactMap { return Double($0) }
-        var operatorComponents = input.compactMap { Operator(rawValue: $0) }
+        var operatorQueue = CalculatorItemQueue<Operator>()
+        var operandQueue = CalculatorItemQueue<Double>()
+        
+        operatorComponents.forEach{ operatorQueue.enqueue(element: $0) }
         
         if operandComponents.count == operatorComponents.count {
             operandComponents[0] = -operandComponents[0]
-            operatorComponents.removeFirst()
+            _ = operatorQueue.dequeue()
         }
         
-        var operandQueue = CalculatorItemQueue<Double>()
-        var operatorQueue = CalculatorItemQueue<Operator>()
-        
         operandComponents.forEach { operandQueue.enqueue(element: $0) }
-        operatorComponents.forEach{ operatorQueue.enqueue(element: $0) }
+        
         
         let formula = Formula(operands: operandQueue, operators: operatorQueue)
         
