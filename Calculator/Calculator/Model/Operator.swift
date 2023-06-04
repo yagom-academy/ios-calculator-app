31751611ad26b86d336c9f26ce9cf17ee187ef4f
Serena <sha0720@naver.com>
2023-06-05 01:42:16 +0900

    feat: Operator 생성

---
+++ Calculator/Calculator/Model/Operator.swift
@@ -0,0 +1,42 @@
+//
+//  Operator.swift
+//  Calculator
+//
+//  Created by Serena on 2023/06/05.
+//
+
+enum Operator: Character, CaseIterable, CalculateItem {
+    case add = "+"
+    case substract = "-"
+    case divide = "/"
+    case multiply = "*"
+    
+    func calculate(lhs: Double, rhs: Double) -> Double {
+        switch self {
+        case .add:
+            return add(lhs: lhs, rhs: rhs)
+        case .substract:
+            return substract(lhs: lhs, rhs: rhs)
+        case .divide:
+            return divide(lhs: lhs, rhs: rhs)
+        case .multiply:
+            return multiply(lhs: lhs, rhs: rhs)
+        }
+    }
+    
+    private func add(lhs: Double, rhs: Double) -> Double {
+        return lhs + rhs
+    }
+    
+    private func substract(lhs: Double, rhs: Double) -> Double {
+        return lhs - rhs
+    }
+    
+    private func divide(lhs: Double, rhs: Double) -> Double {
+        return lhs / rhs
+    }
+    
+    private func multiply(lhs: Double, rhs: Double) -> Double {
+        return lhs * rhs
+    }
+}
