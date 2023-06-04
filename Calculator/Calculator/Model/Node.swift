411c4a3a4e19e949f4e734d483f39c54cc9f7bf8
Hoon94 <dleognsdl1@naver.com>
2023-06-05 03:44:34 +0900

    refactor: Node class test를 위한 deinit 메서드 삭제

--- Calculator/Calculator/Model/Node.swift
+++ Calculator/Calculator/Model/Node.swift
@@ -14,10 +14,6 @@ final class Node<T>: Nodeable {
         self.next = next
     }
     
-    deinit {
-        print("Node deinit")
-    }
-    
     func fetchData() -> T {
         return data
     }
