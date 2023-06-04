99bda0b7823b8d357ee6ff957405d3cfec455ecf
Serena <sha0720@naver.com>
2023-06-05 01:40:47 +0900

    feat: extension Double, String 생성

---
+++ Calculator/Calculator/Extensions/Double+.swift
@@ -0,0 +1,8 @@
+//
+//  Double+.swift
+//  Calculator
+//
+//  Created by Serena on 2023/06/05.
+//
+
+extension Double: CalculateItem {}
---
+++ Calculator/Calculator/Extensions/String+.swift
@@ -0,0 +1,13 @@
+//
+//  String+.swift
+//  Calculator
+//
+//  Created by Serena on 2023/05/31.
+//
+
+extension String: CalculateItem {
+    func split(with target: Character) -> [String] {
+        let targetArray: [String] = []
+        return targetArray
+    }
+}
