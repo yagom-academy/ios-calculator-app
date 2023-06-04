55e2c3c1ac821daafbfa77e26b4b60be87f26c8b
Serena <sha0720@naver.com>
2023-06-05 01:41:58 +0900

    feat: Formula 생성

---
+++ Calculator/Calculator/Model/Formula.swift
@@ -0,0 +1,16 @@
+//
+//  Formula.swift
+//  Calculator
+//
+//  Created by Serena on 2023/06/05.
+//
+
+struct Formula {
+    var operands: CalculatorItemQueue<String>
+    var operators: CalculatorItemQueue<String>
+    
+    func result() -> Double {
+        //string을 받아서 double로 계산을 돌리기
+        return 0.0
+    }
+}
