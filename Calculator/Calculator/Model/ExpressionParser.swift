76fe60eef28ebbb675bf3f7781696e710fcd496a
DasanKim <tempzipp@gmail.com>
2023-06-05 19:48:17 +0900

    ✨feat: ExpressionParser enumeration 생성

--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -5,4 +5,15 @@
 //  Created by Dasan on 2023/06/02.
 //
 
+// 밑줄은 static을 나타냄
 
+enum ExpressionParser {
+    // ExpressionParser는 Formula를 의존함
+//    static func parser(from input: String) -> Formula {
+//        return
+//    }
+    
+    private static func componentsByOperators(from input: String) -> [String] {
+        return []
+    }
+}
