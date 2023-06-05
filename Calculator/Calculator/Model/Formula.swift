bd970c575d2313829b0ecdd4e505a756e1614e72
DasanKim <tempzipp@gmail.com>
2023-06-05 19:46:55 +0900

    ✨feat: Formula 타입 추가 및 result 함수 구현

--- Calculator/Calculator/Model/Formula.swift
+++ Calculator/Calculator/Model/Formula.swift
@@ -5,4 +5,26 @@
 //  Created by Dasan on 2023/06/02.
 //
 
-
+struct Formula {
+    var operands: CalculatorItemQueue<Double>
+    var operators: CalculatorItemQueue<Operator>
+    
+    mutating func result() -> Double {
+        var result: Double = 0.0
+        
+        if operands.count >= 2 && operators.count >= 1 {
+            if let leftValue = operands.dequeue(),
+               let rightValue = operands.dequeue(),
+               let currentOperator = operators.dequeue() {
+                result = currentOperator.calculate(lhs: leftValue, rhs: rightValue)
+                operands.enqueue(result)
+            }
+        } else {
+            if let currentOperand = operands.first {
+                result = currentOperand
+            }
+        }
+    
+        return result
+    }
+}
