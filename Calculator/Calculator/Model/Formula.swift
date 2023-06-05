5557d75c321ffdc7b5bd69dd36a71e6cb2d2e283
Kobe <mskang@xingxingmobility.com>
2023-06-06 08:09:27 +0900

    :label: Add: FormulaProtocol 구현.
    FormulaProtocol을 구현하였습니다.

--- Calculator/Calculator/Model/Formula.swift
+++ Calculator/Calculator/Model/Formula.swift
@@ -5,6 +5,13 @@
 //  Created by Minseong Kang on 2023/06/06.
 //
 
+protocol FormulaProtocol {
+	associatedtype T: CalculateItem
+	var operands: CalculatorItemQueue<T> { get }
+	var operators: CalculatorItemQueue<T> { get }
+	func result() -> Double
+}
+
 struct Formula {
 	
 }
