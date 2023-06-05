03162e856db1cc56b3e9713fad9876a6dbbc87fd
bubblecocoa <bubblecocoa@gmail.com>
2023-06-05 23:57:18 +0900

    faet: Operator 열거형 생성
    Character를 raw value로 지정
    CaseIterable, CalculateItem 프로토콜 채택

---
+++ Calculator/Calculator/Model/Operator.swift
@@ -0,0 +1,13 @@
+//
+//  Operator.swift
+//  Calculator
+//
+//  Created by 비모 on 2023/06/05.
+//
+
+enum Operator: Character, CaseIterable, CalculateItem {
+    case add = "+"
+    case subtract = "-"
+    case divide = "÷"
+    case multiply = "×"
+}
