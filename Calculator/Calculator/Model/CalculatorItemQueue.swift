37f8c5701ed9f1792275a6001c2fa1bf3ce74cfc
YaRkyungmin <kkgm94@naver.com>
2023-06-05 19:15:31 +0900

    ✨feat: CalculatorItemQueue 제네릭타입에 제약추가

--- Calculator/Calculator/Model/CalculatorItemQueue.swift
+++ Calculator/Calculator/Model/CalculatorItemQueue.swift
@@ -5,14 +5,21 @@
 //  Created by kangkyungmin on 2023/05/30.
 //
 
-struct CalculatorItemQueue<T> {
+struct CalculatorItemQueue<T: CalculateItem>: CalculatorItemQueueProtocol {
     private var linkedList = LinkedList<T>()
-
+    
+    var first: Node<T>? {
+        return linkedList.first
+    }
+    
+    var last: Node<T>? {
+        return linkedList.last
+    }
+    
     mutating func enqueue(_ element: T) {
         linkedList.enqueue(element)
     }
     
-    @discardableResult
     mutating func dequeue() -> T? {
         return linkedList.dequeue()
     }
