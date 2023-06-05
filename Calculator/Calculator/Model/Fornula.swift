e05f189c6a9b90e4f77ca797c18942f0783aecf0
mint3382 <mskim0926@naver.com>
2023-06-05 23:39:33 +0900

    refactor: result 내용 수정

--- Calculator/Calculator/Model/Fornula.swift
+++ Calculator/Calculator/Model/Fornula.swift
@@ -8,10 +8,30 @@
 import Foundation
 
 struct Formula {
-    var operators: CalculatorItemQueue<Operator>? //연산자
-    var operands: CalculatorItemQueue<Double>? //피연산자
+    var operators: CalculatorItemQueue<Operator> //연산자
+    var operands: CalculatorItemQueue<Double> //피연산자
     
-    func result() -> Double {
-        return 1.0
+    init(operators: CalculatorItemQueue<Operator>, operands: CalculatorItemQueue<Double>) {
+        self.operators = operators
+        self.operands = operands
+    }
+    
+    mutating func result() -> Double {
+        // operators + operands
+        
+        var calculateByOperators = operators.dequeue()
+        var intermediateResult = operands.dequeue()
+        
+        while calculateByOperators == nil {
+            guard let identifiedOperator = calculateByOperators else { break }
+            guard let lhs = intermediateResult else { break }
+            guard let rhs = operands.dequeue() else { break }
+            
+            intermediateResult = identifiedOperator.calculate(lhs: lhs, rhs: rhs)
+        }
+        
+        guard let result = intermediateResult else { return 0 }
+        
+        return result
     }
 }
