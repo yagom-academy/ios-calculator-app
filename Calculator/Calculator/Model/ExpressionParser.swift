cc562b2fa59c08a50aeebc59e7b660c0a1647de7
Kobe <mskang@xingxingmobility.com>
2023-06-06 08:27:17 +0900

    :poop: Add: ExpressionParser 열거형 임시구현.
    ExpressionParser 열거형을 임시적으로 구현하였습니다.

--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -5,6 +5,18 @@
 //  Created by Minseong Kang on 2023/06/06.
 //
 
-enum ExpressionParser {
+enum ExpressionParser<T: CalculateItem> {
+	static func parse(from input: String) -> Formula<T> {
+		let operandsLinkedList = LinkedList<T>()
+		let operatorsLinkedList = LinkedList<T>()
+		let operands = CalculatorItemQueue(list: operandsLinkedList)
+		let operators = CalculatorItemQueue(list: operatorsLinkedList)
+		let result = Formula(operands: operands, operators: operators)
+		
+		return result
+	}
 	
+	static private func componentsByOperators(from input: String) -> [String] {
+		return [""]
+	}
 }
