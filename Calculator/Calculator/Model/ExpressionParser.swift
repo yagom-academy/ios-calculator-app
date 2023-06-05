b5d4993c9867189f10d27a6d797944c3513ed445
mint3382 <mskim0926@naver.com>
2023-06-05 23:38:57 +0900

    refactor: parse, componentsByOperators 내용 구현

--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -8,13 +8,31 @@
 import Foundation
 
 enum ExpressionParser {
-    static func parse(from input: String) -> Formula {
-        var formula = Formula()
+    static func parse(from input: String) -> Formula { // oper
+        let elements = componentsByOperators(from: input)
+        var operands = CalculatorItemQueue<Double>()
         
-        return formula
+        for index in elements {
+            guard let element = Double(index) else { continue }
+            operands.enqueue(element)
+        }
+        
+        let symbols = input.split(with: " ").filter { Double($0) == nil }
+        var operators = CalculatorItemQueue<Operator>()
+        
+        for index in symbols {
+            guard let symbol = Operator(rawValue: Character(index)) else { continue }
+            operators.enqueue(symbol)
+        }
+        
+        return Formula(operators: operators, operands: operands)
     } //여기서 formula 만들어서 반환
     
     private static func componentsByOperators(from input: String) -> [String] {
-        return []
+//        var options = Operator.allCases
+//            .map { $0.rawValue }
+//            .filter { input.contains($0) }
+        
+        return input.split(with: " ").filter { Double($0) != nil }
     }//private가 붙어서 이 enum 안에서만 사용할 수 있음. 즉 parse에서 호출해 사용.
 }
--- Calculator/Calculator/Model/extension.swift
+++ Calculator/Calculator/Model/extension.swift
@@ -10,7 +10,8 @@ import Foundation
 extension Double: CalculateItem { }
 
 extension String {
-//    func split(with target: Character) -> [String] {
-//    }
+    func split(with target: Character) -> [String] {
+        return self.components(separatedBy: String(target))
+    }
 } // String을 매개변수 target 기준으로 잘라서 [String]으로 반환
 //extension이기에 String type이면 어디서든 사용 가능. -> expressionParser?
