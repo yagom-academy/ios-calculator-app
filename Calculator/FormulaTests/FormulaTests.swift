dc574a223a096c498ba555f66ec44065ae14c28d
hemg2 <hemg2@naver.com>
2023-06-07 00:52:32 +0900

    feat: FormulaTests 값이 3개일때 test메소드 추가진행

--- Calculator/FormulaTests/FormulaTests.swift
+++ Calculator/FormulaTests/FormulaTests.swift
@@ -25,7 +25,7 @@ final class FormulaTests: XCTestCase {
         operators = nil
     }
     
-    func test_enqueue_값입력후_곱하기하고_formula값과같은지확인() {
+    func test_enqueue_5_2입력후_곱하기하고_formula값이_10과같은지확인() {
         //given
         sut.enqueue(item: 5.0)
         sut.enqueue(item: 2.0)
@@ -39,7 +39,7 @@ final class FormulaTests: XCTestCase {
         XCTAssertEqual(formula.result(), 10)
     }
     
-    func test_enqueue_값입력후_더하기하고_formula값과같은지확인() {
+    func test_enqueue_5_2입력후_더하기하고_formula값이_7과같은지확인() {
         //given
         sut.enqueue(item: 5.0)
         sut.enqueue(item: 2.0)
@@ -53,7 +53,7 @@ final class FormulaTests: XCTestCase {
         XCTAssertEqual(formula.result(), 7)
     }
     
-    func test_enqueue_값입력후_빼기하고_formula값과같은지확인() {
+    func test_enqueue_5_2입력후_빼기하고_formula값이_3과같은지확인() {
         //given
         sut.enqueue(item: 5.0)
         sut.enqueue(item: 2.0)
@@ -67,7 +67,7 @@ final class FormulaTests: XCTestCase {
         XCTAssertEqual(formula.result(), 3)
     }
     
-    func test_enqueue_값입력후_나누고하고_formula값과같은지확인() {
+    func test_enqueue_5_2입력후_나누고하고_formula값이_2쩜5와같은지확인() {
         //given
         sut.enqueue(item: 5.0)
         sut.enqueue(item: 2.0)
@@ -81,4 +81,67 @@ final class FormulaTests: XCTestCase {
         XCTAssertEqual(formula.result(), 2.5)
     }
     
+    func test_enqueue_3개의값인_5_2_3_입력후_더하고하고_빼기하고_formula값이4와_같은지확인() {
+        //given
+        sut.enqueue(item: 5.0)
+        sut.enqueue(item: 2.0)
+        sut.enqueue(item: 3.0)
+        
+        //when
+        operators.enqueue(item: .add)
+        operators.enqueue(item: .subtract)
+        
+        let formula = Formula(operands: sut, operators: operators)
+        
+        //then
+        XCTAssertEqual(formula.result(), 4)
+    }
+    
+    func test_enqueue_3개의값인_5_2_3_입력후_더하고하고_더하기하고_formula값이10와_같은지확인() {
+        //given
+        sut.enqueue(item: 5.0)
+        sut.enqueue(item: 2.0)
+        sut.enqueue(item: 3.0)
+        
+        //when
+        operators.enqueue(item: .add)
+        operators.enqueue(item: .add)
+        
+        let formula = Formula(operands: sut, operators: operators)
+        
+        //then
+        XCTAssertEqual(formula.result(), 10)
+    }
+    
+    func test_enqueue_3개의값인_5_2_3_입력후_더하고하고_곱하기하고_formula값이21와_같은지확인() {
+        //given
+        sut.enqueue(item: 5.0)
+        sut.enqueue(item: 2.0)
+        sut.enqueue(item: 3.0)
+        
+        //when
+        operators.enqueue(item: .add)
+        operators.enqueue(item: .multiply)
+        
+        let formula = Formula(operands: sut, operators: operators)
+        
+        //then
+        XCTAssertEqual(formula.result(), 21)
+    }
+    
+    func test_enqueue_3개의값인_5_2_2_입력후_나누기하고_곱하기하고_formula값이21와_같은지확인() {
+        //given
+        sut.enqueue(item: 5.0)
+        sut.enqueue(item: 2.0)
+        sut.enqueue(item: 2.0)
+        
+        //when
+        operators.enqueue(item: .multiply)
+        operators.enqueue(item: .divide)
+        
+        let formula = Formula(operands: sut, operators: operators)
+        
+        //then
+        XCTAssertEqual(formula.result(), 5)
+    }
 }
