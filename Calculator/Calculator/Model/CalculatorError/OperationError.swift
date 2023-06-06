3eebd9aa8b0575fd20d982ceadebb0b880ff368a
YaRkyungmin <kkgm94@naver.com>
2023-06-07 07:23:33 +0900

    ✨feat: OperationError 구현

---
+++ Calculator/Calculator/Model/CalculatorError/OperationError.swift
@@ -0,0 +1,12 @@
+//
+//  OperationError.swift
+//  Calculator
+//
+//  Created by kangkyungmin on 2023/06/07.
+//
+
+enum OperationError: Error {
+    case divideByZeroError
+    case operandNotEnoughError
+    case operatorNotEnoughError
+}
