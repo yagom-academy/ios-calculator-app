7e400978a829309ddd5213af063ba1f9ac0bf942
Kobe <mskang@xingxingmobility.com>
2023-06-07 08:01:36 +0900

    :recycle: Refactor: String Extension 로직 변경.
    String Extension의 함수 split의 내부 로직을 변경하였습니다.

--- Calculator/Calculator/Model/String+Extension.swift
+++ Calculator/Calculator/Model/String+Extension.swift
@@ -5,8 +5,13 @@
 //  Created by Minseong Kang on 2023/06/06.
 //
 
+import Foundation
+
 extension String {
 	func split(with target: Character) -> [String] {
-		return [""]
+		let splitByTarget = self.split(separator: target)
+		let result = splitByTarget.map { String($0) }
+
+		return result
 	}
 }
