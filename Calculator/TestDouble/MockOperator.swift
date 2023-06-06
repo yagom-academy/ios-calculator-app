4b4ecd1d1477ee5870c377b16c004df95f2b1dfa
Kobe <mskang@xingxingmobility.com>
2023-06-06 11:00:58 +0900

    :sparkles: Feat: Operator 테스트를 위한 Mock 객체 생성.
    Operator 열거형 테스트를 위해 MockOperator 객체를 생성했습니다.

--- Calculator/TestDouble/MockOperator.swift
+++ Calculator/TestDouble/MockOperator.swift
@@ -5,4 +5,45 @@
 //  Created by Minseong Kang on 2023/06/06.
 //
 
-import Foundation
+extension MockOperator: CalculateItem { }
+
+enum MockOperator: Character, CaseIterable {
+	case add = "+"
+	case subtract = "-"
+	case divide = "/"
+	case multiply = "*"
+}
+
+extension MockOperator {
+	func calculate(lhs: Double, rhs: Double) -> Double {
+		var operatorsCalculateResult: Double {
+			switch self {
+			case .add:
+				return add(lhs: lhs, rhs: rhs)
+			case .subtract:
+				return subtract(lhs: lhs, rhs: rhs)
+			case .divide:
+				return divide(lhs: lhs, rhs: rhs)
+			case .multiply:
+				return multiply(lhs: lhs, rhs: rhs)
+			}
+		}
+		return operatorsCalculateResult
+	}
+	
+	private func add(lhs: Double, rhs: Double) -> Double {
+		return lhs + rhs
+	}
+	
+	private func subtract(lhs: Double, rhs: Double) -> Double {
+		return lhs - rhs
+	}
+	
+	private func divide(lhs: Double, rhs: Double) -> Double {
+		return lhs / rhs
+	}
+	
+	private func multiply(lhs: Double, rhs: Double) -> Double {
+		return lhs * rhs
+	}
+}
