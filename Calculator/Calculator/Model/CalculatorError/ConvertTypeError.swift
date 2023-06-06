8b316498319370f74225a54ede4abc1ca61050b9
YaRkyungmin <kkgm94@naver.com>
2023-06-07 07:22:29 +0900

    ✨feat: CovertTypeError 구현

---
+++ Calculator/Calculator/Model/CalculatorError/ConvertTypeError.swift
@@ -0,0 +1,11 @@
+//
+//  ConvertTypeError.swift
+//  Calculator
+//
+//  Created by kangkyungmin on 2023/06/07.
+//
+
+enum ConvertTypeError: Error {
+    case invaildStringToDouble
+    case invaildRawValueToCase
+}
