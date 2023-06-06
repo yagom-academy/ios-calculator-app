b680ec88372404fd99131450e26a634363ae098f
mint3382 <mskim0926@naver.com>
2023-06-06 14:44:39 +0900

    test: formula,expressionParser test

--- Calculator/CalculatorTests/FormulaTests.swift
+++ Calculator/CalculatorTests/FormulaTests.swift
@@ -50,4 +50,48 @@ final class FormulaTests: XCTestCase {
         //then
         XCTAssertEqual(result, 3)
     }
+    
+    func test_1더하기2더하기3더하기4문자열을_넣었을때_결과값이_10인지() {
+        //given
+        sut = ExpressionParser.parse(from: "1 + 2 + 3 + 4")
+        
+        //when
+        let result = sut.result()
+        
+        //then
+        XCTAssertEqual(result, 10)
+    }
+    
+    func test_1더하기2빼기3곱하기4나누기5문자열을_넣었을때_결과값이_0인지() {
+        //given
+        sut = ExpressionParser.parse(from: "1 + 2 - 3 x 4 % 5")
+        
+        //when
+        let result = sut.result()
+        
+        //then
+        XCTAssertEqual(result, 0)
+    }
+    
+    func test_1빼기2곱하기3더하기4나누기5문자열을_넣었을때_결과값이_0점2인지() {
+        //given
+        sut = ExpressionParser.parse(from: "1 - 2 x 3 + 4 % 5")
+        
+        //when
+        let result = sut.result()
+        
+        //then
+        XCTAssertEqual(result, 0.2)
+    }
+    
+    func test_10빼기마이너스5문자열을_넣었을때_결과값이_15인지() {
+        //given
+        sut = ExpressionParser.parse(from: "10 - -5")
+        
+        //when
+        let result = sut.result()
+        
+        //then
+        XCTAssertEqual(result, 15)
+    }
 }
