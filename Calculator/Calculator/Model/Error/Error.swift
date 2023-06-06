51ea7e9537ccb18cf39bc05857df934dde9943b3
Serena <sha0720@naver.com>
2023-06-06 15:19:15 +0900

    feat: Error enum case 생성

---
+++ Calculator/Calculator/Model/Error/Error.swift
@@ -0,0 +1,10 @@
+//
+//  Error.swift
+//  Calculator
+//
+//  Created by Serena on 2023/06/06.
+//
+
+enum CalculateError: Error {
+    case invalid
+}
--- Calculator/Calculator/Model/Formula.swift
+++ Calculator/Calculator/Model/Formula.swift
@@ -9,9 +9,10 @@ struct Formula {
     var operands: CalculatorItemQueue<Double>
     var operators: CalculatorItemQueue<String>
     
-    mutating func result() -> Double {
-        var lhs = operands.dequeue()!
-        var rhs = operands.dequeue()!
+    mutating func result() throws -> Double? {
+        guard let lhs = operands.dequeue(), let rhs = operands.dequeue() else {
+            throw CalculateError.invalid
+        }
         var result: Double = 0.0
         
         if operators.peek == String(Operator.add.rawValue) {
