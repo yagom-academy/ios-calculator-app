e1eed6f9e321eb8e0638951522d844fbcc728b20
bubblecocoa <bubblecocoa@gmail.com>
2023-06-06 17:44:22 +0900

    test: Formula의 result 테스트 케이스 추가

--- Calculator/CalculatorTests/FormulaTests.swift
+++ Calculator/CalculatorTests/FormulaTests.swift
@@ -19,4 +19,77 @@ final class FormulaTests: XCTestCase {
     override func tearDownWithError() throws {
         sut = nil
     }
+    
+    func test_result_operands가_비어있는_경우_0을_반환한다() {
+        // given
+        let expectedResult = 0.0
+        
+        // when
+        let result = sut.result()
+        
+        // then
+        XCTAssertEqual(result, expectedResult)
+    }
+    
+    func test_result_operands에_3만_있는_경우_3을_반환한다() {
+        // given
+        sut.operands.enqueue(3.0)
+        let expectedResult = 3.0
+        
+        // when
+        let result = sut.result()
+        
+        // then
+        XCTAssertEqual(result, expectedResult)
+    }
+    
+    func test_result_operands에_3과2_operators에_더하기가_있는경우_5를_반환한다() {
+        // given
+        sut.operands.enqueue(3.0)
+        sut.operators.enqueue(Operator.add)
+        sut.operands.enqueue(2.0)
+        let expectedResult = 5.0
+
+        // when
+        let result = sut.result()
+
+        // then
+        XCTAssertEqual(result, expectedResult)
+    }
+    
+    func test_result_0_더하기_1_빼기_1가_0을_반환한다() {
+        // given
+        sut.operands.enqueue(0.0)
+        sut.operators.enqueue(Operator.add)
+        sut.operands.enqueue(1.0)
+        sut.operators.enqueue(Operator.subtract)
+        sut.operands.enqueue(1.0)
+        let expectedResult = 0.0
+
+        // when
+        let result = sut.result()
+
+        // then
+        XCTAssertEqual(result, expectedResult)
+    }
+    
+    func test_result_2_더하기_8_빼기_5_곱하기_2_나누기_4가_2점5를_반환한다() {
+        // given
+        sut.operands.enqueue(2.0)
+        sut.operands.enqueue(8.0)
+        sut.operands.enqueue(5.0)
+        sut.operands.enqueue(2.0)
+        sut.operands.enqueue(4.0)
+        sut.operators.enqueue(Operator.add)
+        sut.operators.enqueue(Operator.subtract)
+        sut.operators.enqueue(Operator.multiply)
+        sut.operators.enqueue(Operator.divide)
+        let expectedResult = 2.5
+
+        // when
+        let result = sut.result()
+
+        // then
+        XCTAssertEqual(result, expectedResult)
+    }
 }
