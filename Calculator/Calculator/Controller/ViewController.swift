95a38698d34e18275185d9a6ce90407020102b1d
h-suo <phstoo1021@naver.com>
2023-06-06 12:12:55 +0900

    feat: opertorLabel, operandsLabel 뷰컨과 연결 및 버튼도 option, operator, operands에 나눠 액션과 연결 및 간단한 연산만 가능하도록 구현

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -7,12 +7,82 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var formula: String = ""
+    var numberFormatter = NumberFormatter()
+    
+    @IBOutlet weak var operatorLabel: UILabel!
+    @IBOutlet weak var operandsLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        clearLabel()
+        setNumberFormatter()
     }
-
-
+    
+    @IBAction func TapOptionButton(_ sender: UIButton) {
+        guard let title = sender.currentTitle else { return }
+        
+        switch title {
+        case "AC":
+            clearLabel()
+            clearFormula()
+        case "CE":
+            clearLabel()
+        case "⁺⁄₋":
+            break
+        default:
+            break
+        }
+    }
+    
+    @IBAction func TapOperatroButton(_ sender: UIButton) {
+        guard let title = sender.currentTitle else { return }
+        
+        if self.formula == "" {
+            self.formula += "\(operandsLabel.text!) "
+            clearLabel()
+        } else {
+            self.formula += "\(operatorLabel.text!) \(operandsLabel.text!) "
+            clearLabel()
+        }
+        
+        if title == "=" {
+            var formula = ExpressionParser.parse(from: self.formula)
+            clearFormula()
+            clearLabel()
+            operandsLabel.text = numberFormatter.string(for: formula.result())
+            return
+        }
+        
+        operatorLabel.text = title
+    }
+    
+    @IBAction func TapNumberButton(_ sender: UIButton) {
+        guard let inputNumber = sender.currentTitle else { return }
+        guard var operands = operandsLabel.text else { return }
+        
+        operands += inputNumber
+        guard let Number = Double(operands) else { return }
+        
+        operandsLabel.text = numberFormatter.string(for: Number)
+    }
+    
+    func clearLabel() {
+        operandsLabel.text = "0"
+        operatorLabel.text = ""
+    }
+    
+    func clearFormula() {
+        self.formula = ""
+    }
+    
+    func setNumberFormatter() {
+        numberFormatter.maximumFractionDigits = 19
+        numberFormatter.maximumSignificantDigits = 20
+        numberFormatter.numberStyle = .decimal
+    }
+    
 }
 
