f3591f01228dc71d143c069f056d8831736fda8d
Whales <Whales.w.Jin@gmail.com>
2023-06-06 19:52:36 +0900

    feat: 연산자 열거형 타입 생성
    +,-,/,* 프로퍼티 구현

---
+++ Calculator/Calculator/Operator.swift
@@ -0,0 +1,14 @@
+//
+//  Operator.swift
+//  Calculator
+//
+//  Created by Whales on 2023/06/06.
+//
+
+enum Operator: Character, CaseIterable {
+    case add = "+"
+    case subtract = "-"
+    case divide = "/"
+    case multiply = "*"
+}
+
