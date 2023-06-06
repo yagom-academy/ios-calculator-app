c4de613bbd8857df0ae8b869ab0cb149aba32394
DasanKim <tempzipp@gmail.com>
2023-06-06 16:33:40 +0900

    🚚build: String extension 파일 분리

---
+++ Calculator/Calculator/Model/String+split.swift
@@ -0,0 +1,12 @@
+//
+//  String+split.swift
+//  Calculator
+//
+//  Created by Dasan on 2023/06/06.
+//
+
+extension String {
+    mutating func split(with target: Character) -> [String] {
+        return self.split(separator: target).map{ String($0) }
+    }
+}
