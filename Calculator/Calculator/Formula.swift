6a31a666e56a4664a0fac2b00b6d645a4dfc5ccd
idinaloq <idinaloq@icloud.com>
2023-06-06 19:39:10 +0900

    feat: result()메서드 임시구현

--- Calculator/Calculator/Formula.swift
+++ Calculator/Calculator/Formula.swift
@@ -11,7 +11,32 @@ struct Formula {
     var operands = CalculatorItemQueue<Double>()
     var operators = CalculatorItemQueue<Operator>()
     
-    func result() -> Double {
+    mutating func result() -> Double {
+        //1. operand dequeue x 2
+        //2. operator dequeue
+        //3. lhs에 operand 할당
+        //4. rhs에 operand 할당
+        //5. operators 연산
+        //6. operand dequeue
+        //7. operator dequeue
+        //8. 5번의 연산결과 = lhs할당, rhs할당 후 연산
+        //9. 리스트 길이만큼 반복
+        guard let lhsOperand: Double = operands.dequeue() else {
+            return 0
+            //return throw 구문
+        }
         
+        guard let operatorValue: Operator = operators.dequeue() else {
+            return 0
+            //return throw 구문
+        }
+        
+        guard let rhsOperand: Double = operands.dequeue() else {
+            return 0
+            //return throw 구문
+        }
+        
+        var result: Double = operatorValue.calculate(lhs: lhsOperand, rhs: rhsOperand)
+        return result
     }
 }
