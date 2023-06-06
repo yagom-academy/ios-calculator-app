14102e487011691fe718785884b3fbb3290b2cfa
Kobe <mskang@xingxingmobility.com>
2023-06-06 09:42:21 +0900

    :sparkles: Feat: String Extension 파일 생성 및 임시 구현.
    String Extension 파일을 생성하고 임시로 구현하였습니다.

---
+++ Calculator/Calculator/Model/String+Extension.swift
@@ -0,0 +1,12 @@
+//
+//  String+Extension.swift
+//  Calculator
+//
+//  Created by Minseong Kang on 2023/06/06.
+//
+
+extension String {
+	func split(with target: Character) -> [String] {
+		return [""]
+	}
+}
