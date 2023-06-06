750577d79c95e387fa2be77c5903fc71bd149d3c
Whales <Whales.w.Jin@gmail.com>
2023-06-06 20:01:18 +0900

    feat: 연산자 열거형에 연산 메서드 구현

--- Calculator/Calculator/Operator.swift
+++ Calculator/Calculator/Operator.swift
@@ -10,5 +10,18 @@ enum Operator: Character, CaseIterable {
     case subtract = "-"
     case divide = "/"
     case multiply = "*"
+    
+    private func add(lhs: Double, rhs: Double) -> Double {
+        return lhs + rhs
+    }
+    private func subtract(lhs: Double, rhs: Double) -> Double {
+        return lhs - rhs
+    }
+    private func divide(lhs: Double, rhs: Double) -> Double {
+        return lhs / rhs
+    }
+    private func multiply(lhs: Double, rhs: Double) -> Double {
+        return lhs * rhs
+    }
 }
 
