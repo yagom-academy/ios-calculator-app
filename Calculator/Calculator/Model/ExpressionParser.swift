686e5b70c35b5d0a6263365b15de934cd8729f34
hemg2 <hemg2@naver.com>
2023-06-06 11:29:18 +0900

    feat: Formula, ExpressionParser 객체생성

---
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -0,0 +1,19 @@
+//
+//  ExpressionParser.swift
+//  Calculator
+//
+//  Created by Hmeg on 2023/06/06.
+//
+
+enum ExpressionParser {
+    static func parse(from input: String) -> Formula {
+       
+        
+        return
+    }
+    
+    private static func componentsByOperators(from input: String) -> [String] {
+        
+        return ["Asdsa"]
+    }
+}
---
+++ Calculator/Calculator/Model/Formula.swift
@@ -0,0 +1,17 @@
+//
+//  Formula.swift
+//  Calculator
+//
+//  Created by Hemg on 2023/06/06.
+//
+
+struct Formula {
+    let operands: CalculatorItemQueue<Double>
+    let operators: CalculatorItemQueue<Operator>
+    
+    func result() -> Double {
+    
+        
+        return 0.0
+    }
+}
