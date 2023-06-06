ea20456cd899288878deea169224748293c9f8f2
bubblecocoa <bubblecocoa@gmail.com>
2023-06-06 14:26:00 +0900

    feat: Formula에 프로퍼티, 메서드 작성
    operands: 숫자를 담는 큐
    operators: 연산자를 담는 큐
    result: 계산 결과를 반환하는 메서드

--- Calculator/Calculator/Model/Formula.swift
+++ Calculator/Calculator/Model/Formula.swift
@@ -6,5 +6,26 @@
 //
 
 struct Formula {
+    var operands: CalculatorItemQueue<Double>
+    var operators: CalculatorItemQueue<Operator>
     
+    mutating func result() -> Double {
+        var result: Double = 0.0
+        
+        while operands.peek() != nil {
+            let lhs = result
+            
+            guard let `operator` = operators.dequeue() else {
+                return result
+            }
+            
+            guard let rhs = operands.dequeue() else {
+                return result
+            }
+            
+            result = `operator`.calculate(lhs: lhs, rhs: rhs)
+        }
+        
+        return result
+    }
 }
