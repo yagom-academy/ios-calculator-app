c11645bec34150a683ebedd6715c3b9d18b66ec4
Serena <sha0720@naver.com>
2023-06-06 22:04:55 +0900

    refactor: componentsByOperators 함수 수정

--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -5,12 +5,17 @@
 //  Created by Serena on 2023/06/05.
 //
 
+import Foundation
+
 enum ExpressionParser {
     static func parse(from input: String) -> Formula {
         return Formula(operands: CalculatorItemQueue(queue: [0.0]), operators: CalculatorItemQueue(queue: [""]))
     }
     
-    static private func componentsByOperators(from Input: String) -> [String] {
-        return [""]
+    static private func componentsByOperators(from input: String) -> [String] {
+        let componentStandard = CharacterSet(charactersIn: String(Operator.allCases.map({$0.rawValue})))
+        let componentParts: [String] = input.components(separatedBy: componentStandard)
+
+        return componentParts
     }
 }
