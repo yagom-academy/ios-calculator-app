99a7f2d8f0cb7247089e441dc1b279c3ab4aba21
hemg2 <hemg2@naver.com>
2023-06-06 18:50:39 +0900

    feat: ExpressionParser 객체 생성 로직구현

--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -7,13 +7,24 @@
 
 enum ExpressionParser {
     static func parse(from input: String) -> Formula {
-       
+        let operandQueue = CalculatorItemQueue<Double>()
+        let operatorQueue = CalculatorItemQueue<Operator>()
         
-        return
+        let components = componentsByOperators(from: input)
+        
+        for component in components {
+            if let number = Double(component) {
+                operandQueue.enqueue(item: number)
+            } else if let op = Operator(rawValue: Character(component)) {
+                operatorQueue.enqueue(item: op)
+            }
+        }
+        
+        let formula = Formula(operands: operandQueue, operators: operatorQueue)
+        return formula
     }
     
     private static func componentsByOperators(from input: String) -> [String] {
-        
-        return ["Asdsa"]
+        return input.components(separatedBy: ["+", "-", "/", "*"])
     }
 }
