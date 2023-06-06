bf009db50a0ca939ce163be45b24eaaebca4d929
Whales <Whales.w.Jin@gmail.com>
2023-06-07 02:22:07 +0900

    feat: Formula(공식) 구조체 타입 생성
    operands(피연산자), operators(연산자) 프로퍼티 구현

---
+++ Calculator/Calculator/Model/Formula.swift
@@ -0,0 +1,11 @@
+//
+//  Formula.swift
+//  Calculator
+//
+//  Created by Whales on 2023/06/07.
+//
+
+struct Formula {
+    var operands: CalculatorItemQueue<Double>
+    var operators: CalculatorItemQueue<String>
+}
