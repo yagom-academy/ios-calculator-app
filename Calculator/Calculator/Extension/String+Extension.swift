dfa99201b8e0083071bd64e9b7c368d3790acd06
Hoon94 <dleognsdl1@naver.com>
2023-06-07 04:14:47 +0900

    refactor: ExpressionParser parse와 componentsByOperators 메서드 추가

--- Calculator/Calculator/Extension/String+Extension.swift
+++ Calculator/Calculator/Extension/String+Extension.swift
@@ -7,6 +7,6 @@
 
 extension String {
     func split(with target: Character) -> [String] {
-        self.components(separatedBy: String(target))
+        return components(separatedBy: String(target))
     }
 }
--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -6,5 +6,35 @@
 //
 
 enum ExpressionParser {
-    
+    static func parse(from input: String) -> Formula {
+        var formula: Formula = Formula(operands: CalculatorItemQueue<Double>(),
+                                       operators: CalculatorItemQueue<Operator>())
+        var operandCandidates = componentsByOperators(from: input)
+        
+        for index in 0..<operandCandidates.count - 1 {
+            if operandCandidates[index].isEmpty {
+                operandCandidates[index + 1] = "-\(operandCandidates[index + 1])"
+            }
+        }
+            
+        var operands = operandCandidates.compactMap { Double($0) }
+        let operatorCandidates: String = input.hasPrefix("-") ? String(input.suffix(from: input.index(input.startIndex, offsetBy: 1))) : input
+        let operators = operatorCandidates
+            .replacingOccurrences(of: "+-", with: "+")
+            .replacingOccurrences(of: "--", with: "-")
+            .replacingOccurrences(of: "/-", with: "/")
+            .replacingOccurrences(of: "*-", with: "*")
+            .compactMap { Operator(rawValue: $0) }
+        
+        operands.forEach { formula.operands.enqueue($0) }
+        operators.forEach { formula.operators.enqueue($0) }
+
+        return formula
+    }
+        
+    static private func componentsByOperators(from input: String) -> [String] {
+        return Operator.allCases.reduce([input]) { splitedInput, `operator` in
+            splitedInput.map { $0.split(with: `operator`.rawValue) }.flatMap { $0 }
+        }
+    }
 }
