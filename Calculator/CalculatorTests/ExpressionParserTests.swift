f04b27bc242058f5e50a11a66a3b3db21e8d533d
bubblecocoa <bubblecocoa@gmail.com>
2023-06-06 21:23:12 +0900

    test: ExpressionParserTests, ExpressionParserMock 생성

---
+++ Calculator/CalculatorTests/ExpressionParserTests.swift
@@ -0,0 +1,19 @@
+//
+//  ExpressionParserTests.swift
+//  CalculatorTests
+//
+//  Created by 비모 on 2023/06/06.
+//
+
+import XCTest
+@testable import Calculator
+
+final class ExpressionParserTests: XCTestCase {
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+    }
+
+    override func tearDownWithError() throws {
+        
+    }
+}
