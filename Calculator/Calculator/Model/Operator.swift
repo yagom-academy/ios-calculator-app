7813e90a721ac83ca2f6c25dabc8a7ea911b87f9
YaRkyungmin <kkgm94@naver.com>
2023-06-07 07:28:39 +0900

    ✨feat: Operator 기능 구현

---
+++ Calculator/Calculator/Model/Operator.swift
@@ -0,0 +1,46 @@
+//
+//  Operator.swift
+//  Calculator
+//
+//  Created by kangkyungmin on 2023/06/07.
+//
+
+enum Operator: Character, CaseIterable, CalculateItem {
+    case add = "+"
+    case subtract = "−"
+    case divide = "÷"
+    case multiply = "×"
+
+    func calculate(lhs: Double, rhs: Double) throws -> Double {
+        switch self {
+        case .add:
+            return add(lhs: lhs, rhs: rhs)
+        case .subtract:
+            return subtract(lhs: lhs, rhs: rhs)
+        case .divide:
+            if rhs == 0 {
+                throw OperationError.divideByZeroError
+            }
+            
+            return divide(lhs: lhs, rhs: rhs)
+        case .multiply:
+            return multiply(lhs: lhs, rhs: rhs)
+        }
+    }
+    
+    private func add(lhs: Double, rhs: Double) -> Double {
+        return lhs + rhs
+    }
+    
+    private func subtract(lhs: Double, rhs: Double) -> Double {
+        return lhs - rhs
+    }
+    
+    private func divide(lhs: Double, rhs: Double) -> Double {
+        return lhs / rhs
+    }
+    
+    private func multiply(lhs: Double, rhs: Double) -> Double {
+        return lhs * rhs
+    }
+}
