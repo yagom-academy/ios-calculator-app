c34d9eaff1c65c615b894eff3b3f7ea9f8141349
DasanKim <tempzipp@gmail.com>
2023-06-06 15:44:07 +0900

    ✅test: ExpressionParser에 대한 test 추가

---
+++ Calculator/CalculatorTests/ExpressionParserTests/ExpressionParserTests.swift
@@ -0,0 +1,131 @@
+//
+//  ExpressionParserTests.swift
+//  ExpressionParserTests
+//
+//  Created by Yena on 2023/06/06.
+//
+
+import XCTest
+@testable import Calculator
+
+final class ExpressionParserTests: XCTestCase {
+    //var sut: ExpressionParser!
+
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+    }
+
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+    }
+
+    func test_parser_문자열이_1_더하기_2_더하기_3일때_반환값의operands의_count는_3이다() {
+        // given
+        let formula = ExpressionParser.parser(from: "1+2+3")
+        let expectation = 3
+        
+        // when
+        let result = formula.operands.count
+        
+        // then
+        XCTAssertEqual(result, expectation)
+    }
+    
+    func test_parser_문자열이_1_더하기_2_더하기_3일때_반환값의operators의_count는_2이다() {
+        // given
+        let formula = ExpressionParser.parser(from: "1+2+3")
+        let expectation = 2
+        
+        // when
+        let result = formula.operators.count
+        
+        // then
+        XCTAssertEqual(result, expectation)
+    }
+    
+    func test_parser_문자열이_1_더하기_2_빼기_3일때_반환값의operators의_first값은_빼기이다() {
+        // given
+        let formula = ExpressionParser.parser(from: "1+2-3")
+        let expectation = Operator.add
+        
+        // when
+        let result = formula.operators.first
+        
+        // then
+        XCTAssertEqual(result, expectation)
+    }
+    
+    func test_parser_문자열이_1_더하기_2_빼기_3일때_반환값의operators의_last값은_빼기이다() {
+        // given
+        let formula = ExpressionParser.parser(from: "1+2-3")
+        let expectation = Operator.subtract
+        
+        // when
+        let result = formula.operators.last
+        
+        // then
+        XCTAssertEqual(result, expectation)
+    }
+    
+    func test_parser_문자열이_2_곱하기_4_나누기_8일때_반환값의operators의_first값은_곱하기이다() {
+        // given
+        let formula = ExpressionParser.parser(from: "2×4÷8")
+        let expectation = Operator.multiply
+        
+        // when
+        let result = formula.operators.first
+        
+        // then
+        XCTAssertEqual(result, expectation)
+    }
+    
+    func test_parser_문자열이_2_곱하기_4_나누기_8일때_반환값의operators의_last값은_나누기이다() {
+        // given
+        let formula = ExpressionParser.parser(from: "2×4÷8")
+        let expectation = Operator.divide
+        
+        // when
+        let result = formula.operators.last
+        
+        // then
+        XCTAssertEqual(result, expectation)
+    }
+    
+    func test_parser_문자열이_2_곱하기_4_나누기_8일때_반환값의result값은_1이다() {
+        // given
+        var formula = ExpressionParser.parser(from: "2×4÷8")
+        let expectation = 1.0
+        
+        // when
+        let result = try? formula.result()
+        
+        // then
+        XCTAssertEqual(result, expectation)
+    }
+    
+    func test_parser_문자열을_2_더하기_4와_빼기_3으로_나눠서입력했을때_반환값의operators의_count값은_3이다() {
+        // given
+        var formula = ExpressionParser.parser(from: "2+4")
+        formula = ExpressionParser.parser(from: "-3")
+        let expectation = 1.0
+        
+        // when
+        let result = try? formula.result()
+        
+        // then
+        XCTAssertEqual(result, expectation)
+    }
+    
+    func test_parser_문자열에_operator만_있을경우_이다() {
+        // given
+        var formula = ExpressionParser.parser(from: "++×÷-")
+        let expectation = 1.0
+        
+        // when
+        let result = try? formula.result()
+        
+        // then
+        XCTAssertEqual(result, expectation)
+    }
+
+}
