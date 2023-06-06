90bbd699420e6d9161b0561d7687939a9f004825
hemg2 <hemg2@naver.com>
2023-06-06 23:11:12 +0900

    feat: result 함수 로직 구현

--- Calculator/Calculator/Model/Formula.swift
+++ Calculator/Calculator/Model/Formula.swift
@@ -6,25 +6,23 @@
 //
 
 struct Formula {
-    var operands: CalculatorItemQueue<Double>
-    var operators: CalculatorItemQueue<Operator>
+    let operands: CalculatorItemQueue<Double>
+    let operators: CalculatorItemQueue<Operator>
     
     func result() -> Double {
-        if let firstOperand = operands.dequeueItem() {
-            operands.enqueue(item: firstOperand)
+        guard let firstOperand = operands.dequeueItem() else {
+            return 0.0
         }
+        
+        var result = firstOperand
         
         while !operators.isEmpty() {
-            guard let operatorValue = operators.lastItem else { continue }
-            guard let rhs = operands.dequeueItem() else { continue }
-            guard let lhs = operands.dequeueItem() else { continue }
-//            guard let operators = operators.dequeueItem() else { continue }
-            
-            let result = operatorValue.calculate(rhs, lhs)
-            operands.enqueue(item: result)
+            guard let operatorValue = operators.dequeueItem() else { continue }
+            guard let nextOperand = operands.dequeueItem() else { continue }
+
+            result = operatorValue.calculate(result, nextOperand)
         }
         
-        
-        return 0.0
+        return result
     }
 }
