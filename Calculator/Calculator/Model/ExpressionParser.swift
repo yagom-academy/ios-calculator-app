e0cc668a0cabe8364ec6a7ae3fce4c056cfd9dac
DasanKim <tempzipp@gmail.com>
2023-06-06 16:35:17 +0900

    ✨feat: ExpressionParser의 내부 parsing 하는 메서드 구현

--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -5,15 +5,34 @@
 //  Created by Dasan on 2023/06/02.
 //
 
-// 밑줄은 static을 나타냄
+import Foundation
 
 enum ExpressionParser {
-    // ExpressionParser는 Formula를 의존함
-//    static func parser(from input: String) -> Formula {
-//        return
-//    }
+    static func parser(from input: String) -> Formula {
+        var formula = Formula(operands: CalculatorItemQueue(),
+                              operators: CalculatorItemQueue())
+        
+        let operands = componentsByOperators(from: input).compactMap { Double($0) }
+        
+        operands.forEach {
+            formula.operands.enqueue($0)
+        }
+    
+        let operators = input.filter({ $0.isNumber == false }).compactMap({ Operator(rawValue: $0) })
+        
+        operators.forEach {
+            formula.operators.enqueue($0)
+        }
+        
+        return formula
+    }
     
     private static func componentsByOperators(from input: String) -> [String] {
-        return []
+        let operators = String(Operator.allCases.map{ $0.rawValue })
+        
+        let OperatorCharacterSet = CharacterSet(charactersIn: operators)
+        let components = input.components(separatedBy: OperatorCharacterSet)
+        
+        return components
     }
 }
