def96f486249ed0fc893292c96ab6c43e572697b
Min Hyun <minnimida@gmail.com>
2023-06-06 22:32:34 +0900

    feat: Outlet 연결

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -7,7 +7,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var acButton: UIButton!
+    @IBOutlet weak var ceButton: UIButton!
+    @IBOutlet weak var negativeButton: UIButton!
+    @IBOutlet weak var divideButton: UIButton!
+    @IBOutlet weak var multiplyButton: UIButton!
+    @IBOutlet weak var subtractButton: UIButton!
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var equalButton: UIButton!
+    @IBOutlet weak var pointButton: UIButton!
+    @IBOutlet weak var doubleZeroButton: UIButton!
+    @IBOutlet weak var zeroButton: UIButton!
+    @IBOutlet weak var oneButton: UIButton!
+    @IBOutlet weak var twoButton: UIButton!
+    @IBOutlet weak var threeButton: UIButton!
+    @IBOutlet weak var fourButton: UIButton!
+    @IBOutlet weak var fiveButton: UIButton!
+    @IBOutlet weak var sixButton: UIButton!
+    @IBOutlet weak var sevenButton: UIButton!
+    @IBOutlet weak var eightButton: UIButton!
+    @IBOutlet weak var nineButton: UIButton!
+    
+    @IBOutlet weak var currentOperatorLabel: UILabel!
+    @IBOutlet weak var currentNumberLabel: UILabel!
+    
+    @IBOutlet weak var formulaListScrollView: UIScrollView!
+    @IBOutlet weak var formulaListStackView: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
