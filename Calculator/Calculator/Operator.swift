97d61d7ec60de02f230fe711fd849c7b44340106
Whales <Whales.w.Jin@gmail.com>
2023-06-06 20:42:23 +0900

    feat: 연산자 열거형에 계산 메서드 구현
    각 case 별로 계산 들어가도록 구현

--- Calculator/Calculator/Operator.swift
+++ Calculator/Calculator/Operator.swift
@@ -11,6 +11,19 @@ enum Operator: Character, CaseIterable {
     case divide = "/"
     case multiply = "*"
     
+    func calculate(lhs: Double, rhs: Double) -> Double {
+        switch self {
+        case .add :
+            return add(lhs: lhs, rhs: rhs)
+        case .subtract :
+            return subtract(lhs: lhs, rhs: rhs)
+        case .divide :
+            return divide(lhs: lhs, rhs: rhs)
+        case .multiply :
+            return multiply(lhs: lhs, rhs: rhs)
+        }
+    }
+    
     private func add(lhs: Double, rhs: Double) -> Double {
         return lhs + rhs
     }
@@ -25,3 +38,4 @@ enum Operator: Character, CaseIterable {
     }
 }
 
+Operator.calculate(.add)
