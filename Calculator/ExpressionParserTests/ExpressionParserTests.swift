de2aff743bebdbfee9e935877a4a2c7a241594d2
Hoon94 <dleognsdl1@naver.com>
2023-06-07 01:39:50 +0900

    test: 수식이 주어지면 ExpressionParser에서 수식을 받아 처리

--- Calculator/ExpressionParserTests/ExpressionParserTests.swift
+++ Calculator/ExpressionParserTests/ExpressionParserTests.swift
@@ -6,30 +6,19 @@
 //
 
 import XCTest
+@testable import Calculator
 
 final class ExpressionParserTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func test_parse_수식이주어지면_연산자를기준으로문자열을나누고_각각의Queue에맞게입력한다() {
+        // given
+        let expectedString: String = "1+2"
+        
+        // when
+        var result: Formula = ExpressionParser.parse(from: expectedString)
+        
+        // then
+        XCTAssertEqual(result.operands.dequeue(), 1)
+        XCTAssertEqual(result.operands.dequeue(), 2)
+        XCTAssertEqual(result.operators.dequeue(), .add)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
