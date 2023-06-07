1a96109810dd6e6632203aef5764a386d5ab9f05
bubblecocoa <bubblecocoa@gmail.com>
2023-06-07 13:00:52 +0900

    test: Operator의 divide 테스트 케이스 추가

--- Calculator/CalculatorTests/OperatorTests.swift
+++ Calculator/CalculatorTests/OperatorTests.swift
@@ -9,5 +9,25 @@ import XCTest
 @testable import Calculator
 
 final class OperatorTests: XCTestCase {
+    func test_divide_1을_0으로_나누면_divisionError를_잡는다() {
+        // given
+        let `operator` = Operator.divide
+        
+        // when
+        XCTAssertThrowsError(try `operator`.calculate(lhs: 1, rhs: 0)) { error in
+            // then
+            XCTAssertEqual(error as? CalculatorError, CalculatorError.divisionError)
+        }
+    }
     
+    func test_divide_0을_0으로_나누면_divisionError를_잡는다() {
+        // given
+        let `operator` = Operator.divide
+        
+        // when
+        XCTAssertThrowsError(try `operator`.calculate(lhs: 0, rhs: 0)) { error in
+            // then
+            XCTAssertEqual(error as? CalculatorError, CalculatorError.divisionError)
+        }
+    }
 }
