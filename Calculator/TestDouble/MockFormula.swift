28c2e9cfd16a92e1f439f1f60b7a9435e5e3308a
Kobe <mskang@xingxingmobility.com>
2023-06-07 11:27:40 +0900

    :recycle: Refactor: MockFormula result 메서드 내부 코드 수정.
    테스트를 위해 MockFormula의 result 메서드 내부 코드를 수정하였습니다.

--- Calculator/TestDouble/MockFormula.swift
+++ Calculator/TestDouble/MockFormula.swift
@@ -18,13 +18,12 @@ struct MockFormula<T: CalculateItem, U: CalculateItem>: MockFormulaProtocol {
 	var operators: MockCalculatorItemQueue<U>?
 	
 	func result() -> Double {
-		let lhs = operands.flatMap { $0.mockList?.head?.value }
-		guard let lhsOperand = lhs as? Double else { return -1.0 }
+		while let operands = operands.flatMap({ $0.mockList?.head?.value }) {
+			guard let operands = operands as? Double else { return -1.0 }
+			return operands
+		}
 		
-		let rhs = operands.flatMap { $0.mockList?.tail?.value }
-		guard let rhsOperand = rhs as? Double else { return -1.0 }
-		
-		return lhsOperand
+		return -1.0
 	}
 	
 	init(operands: MockCalculatorItemQueue<T>, operators: MockCalculatorItemQueue<U>) {
