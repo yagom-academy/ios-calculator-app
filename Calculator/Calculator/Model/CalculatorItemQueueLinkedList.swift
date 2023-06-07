d3c3e521f40e042d7c97a3f55833c959f1526c4f
karen <karenrodanthe@icloud.com>
2023-06-07 18:13:47 +0900

    🆕feat: CalculatorItemQueueLinkedList 제네릭 타입으로 정의

--- Calculator/Calculator/Model/CalculatorItemQueueLinkedList.swift
+++ Calculator/Calculator/Model/CalculatorItemQueueLinkedList.swift
@@ -14,3 +14,44 @@ final class Node<T> {
         self.data = data
     }
 }
+
+struct CalculatorItemQueueLinkedList<T> {
+    private var head: Node<T>?
+    private var tail: Node<T>?
+    
+    var isEmpty: Bool {
+        return head == nil
+    }
+    
+    var first: T? {
+        return head?.data
+    }
+    
+    var last: T? {
+        return tail?.data
+    }
+    
+    mutating func append(data: T) {
+        let newNode = Node(data: data)
+        
+        guard let tailNode = head else {
+            head = newNode
+            tail = newNode
+            return
+        }
+        newNode.prev = tailNode
+        tailNode.next = newNode
+        tail = newNode
+    }
+    
+    mutating func removeFirst() {
+        guard let firstNode = head else { return }
+        
+        head = firstNode.next
+        if head == nil {
+            tail = nil
+        } else {
+            head?.prev = nil
+        }
+    }
+}
