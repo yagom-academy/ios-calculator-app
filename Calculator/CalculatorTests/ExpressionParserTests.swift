f9b27eed415b4c497f411ef6ced456623f89dc7e
bubblecocoa <bubblecocoa@gmail.com>
2023-06-07 15:21:59 +0900

    test: ExpressionParserMock파일 제거에 따른 ExpressionParserTests 테스트 변경
    ExpressionParser의 private 메서드는 테스트 하지 않음

--- Calculator/CalculatorTests/ExpressionParserTests.swift
+++ Calculator/CalculatorTests/ExpressionParserTests.swift
@@ -9,25 +9,13 @@ import XCTest
 @testable import Calculator
 
 final class ExpressionParserTests: XCTestCase {
-    func test_componentsByOperators_숫자와_연산자가_섞인_문자열을_연산자로_나누고_숫자배열을_반환한다() {
-        // given
-        let numberWithOperators = "1+2−3÷4×5"
-        let expectedResult = ["1", "2", "3", "4", "5"]
-        
-        // when
-        let result = ExpressionParserMock.componentsByOperators(from: numberWithOperators)
-        
-        // then
-        XCTAssertEqual(result, expectedResult)
-    }
-    
     func test_parse_operands에는_입력한_문자열의_숫자만_있다() {
         // given
         let numberWithOperators = "1.0+2.0−3.0÷4.0×5.0"
         let expectedResult = [1.0, 2.0, 3.0, 4.0, 5.0]
         
         // when
-        var operands = ExpressionParserMock.parse(from: numberWithOperators).operands
+        var operands = ExpressionParser.parse(from: numberWithOperators).operands
         var result: [Double] = []
         
         for _ in 0..<operands.count {
@@ -44,7 +32,7 @@ final class ExpressionParserTests: XCTestCase {
         let expectedResult = [Operator.add, Operator.subtract, Operator.divide, Operator.multiply]
         
         // when
-        var operators = ExpressionParserMock.parse(from: numberWithOperators).operators
+        var operators = ExpressionParser.parse(from: numberWithOperators).operators
         var result: [Operator] = []
         
         for _ in 0..<operators.count {
@@ -62,7 +50,7 @@ final class ExpressionParserTests: XCTestCase {
         let expectedOperatorsResult: [Operator] = [Operator.subtract]
         
         // when
-        var formula = ExpressionParserMock.parse(from: numberWithOperators)
+        var formula = ExpressionParser.parse(from: numberWithOperators)
         
         var operandsResult: [Double] = []
         
