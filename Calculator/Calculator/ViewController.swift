119b67a2144aa5fb34af5ea4a2608d3411b10cf2
Zion <lion2212@naver.com>
2023-06-07 13:21:46 +0900

    feat: didTappedNumbers, didTappedMenus 구현완료, Menus 추가

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -10,6 +10,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var currentOperatorLabel: UILabel!
     @IBOutlet weak var currentOperandLabel: UILabel!
     
+    private let initialNumber = 0
+    
+    // MARK: - View LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -23,7 +26,9 @@ class ViewController: UIViewController {
 // MARK: - Button Action
 extension ViewController {
     @IBAction func didTappedOperators(_ sender: UIButton) {
+        // TODO: Insert StackView
         
+        currentOperatorLabel.text = sender.currentTitle
     }
     
     @IBAction func didTappedCalculate(_ sender: UIButton) {
@@ -31,10 +36,36 @@ extension ViewController {
     }
     
     @IBAction func didTappedNumbers(_ sender: UIButton) {
+        guard let insertedNumber = sender.currentTitle else { return }
+        let currentOperand = currentOperandLabel.text ?? "\(initialNumber)"
         
+        currentOperandLabel.text = Int(currentOperand) == initialNumber ? insertedNumber : currentOperand + insertedNumber
     }
     
     @IBAction func didTappedMenus(_ sender: UIButton) {
+        guard let insertedMenu = sender.currentTitle,
+                let menu = Menus(rawValue: insertedMenu) else { return }
         
+        switch menu {
+        case .allCelar:
+            // TODO : All Clear
+            currentOperandLabel.text = ""
+        case .clearElement:
+            currentOperandLabel.text = "\(initialNumber)"
+        case .changeSign:
+            changeOperandSign()
+        }
+    }
+}
+
+// MARK: - Calculate Method
+extension ViewController {
+    private func changeOperandSign() {
+        guard let currentOperandString = currentOperandLabel.text,
+              Int(currentOperandString) != initialNumber else { return }
+        guard var currentOperand = Double(currentOperandString) else { return }
+
+        currentOperand = -currentOperand
+        currentOperandLabel.text = "\(currentOperand)"
     }
 }
