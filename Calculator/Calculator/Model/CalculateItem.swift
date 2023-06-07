399dadaae5c93eb040279f92a990c30e468a277a
Whales <Whales.w.Jin@gmail.com>
2023-06-08 07:17:13 +0900

    chore: String_extension 파일 분리

--- Calculator/Calculator/Model/CalculateItem.swift
+++ Calculator/Calculator/Model/CalculateItem.swift
@@ -5,14 +5,6 @@
 //  Created by Whales on 2023/05/31.
 //
 
-import Foundation
-
 protocol CalculateItem { }
 
 extension Double: CalculateItem { }
-
-extension String {
-    func split(with target: Character) -> [String] {
-        self.components(separatedBy: String(target))
-    }
-}
