a3301717bb1a50fb966fa17c427a3321c5adf3f3
bubblecocoa <bubblecocoa@gmail.com>
2023-06-07 12:59:59 +0900

    test: OperatorTests 생성

---
+++ Calculator/CalculatorTests/OperatorTests.swift
@@ -0,0 +1,13 @@
+//
+//  OperatorTests.swift
+//  CalculatorTests
+//
+//  Created by 비모 on 2023/06/07.
+//
+
+import XCTest
+@testable import Calculator
+
+final class OperatorTests: XCTestCase {
+    
+}
