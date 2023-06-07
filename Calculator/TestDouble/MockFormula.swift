0136e5e5c48c24d132cb0a6f0eec7d175297b615
Kobe <mskang@xingxingmobility.com>
2023-06-07 09:36:00 +0900

    :recycle: Refactor: 테스트를 위하여 프로토콜과 구조체 코드 수정.
    케이스 테스트 비교를 위하여 MockFormulaProtocol의 operands와 operators의 타입을 옵셔널로 변경하였으며 그에 따라 MockFormula 구조체도 변경해주었습니다.

--- Calculator/TestDouble/MockFormula.swift
+++ Calculator/TestDouble/MockFormula.swift
@@ -8,31 +8,19 @@
 protocol MockFormulaProtocol {
 	associatedtype T: CalculateItem
 	associatedtype U: CalculateItem
-	var operands: MockCalculatorItemQueue<T> { get }
-	var operators: MockCalculatorItemQueue<U> { get }
+	var operands: MockCalculatorItemQueue<T>? { get }
+	var operators: MockCalculatorItemQueue<U>? { get }
 	func result() -> Double
 }
 
 struct MockFormula<T: CalculateItem, U: CalculateItem>: MockFormulaProtocol {
-	var operands: MockCalculatorItemQueue<T>
-	var operators: MockCalculatorItemQueue<U>
+	var operands: MockCalculatorItemQueue<T>?
+	var operators: MockCalculatorItemQueue<U>?
+	
 	func result() -> Double {
-		guard let lhsOperand = operands.dequeue() as? Double else { return 0.0 }
-		guard let rhsOperand = operands.dequeue() as? Double else { return 0.0 }
-		
-		guard let `operator` = operators.dequeue() as? MockOperator else { return 0.0 }
-		
-		switch `operator` {
-		case .add:
-			return MockOperator.add.calculate(lhs: lhsOperand, rhs: rhsOperand)
-		case .subtract:
-			return MockOperator.subtract.calculate(lhs: lhsOperand, rhs: rhsOperand)
-		case .divide:
-			return MockOperator.divide.calculate(lhs: lhsOperand, rhs: rhsOperand)
-		case .multiply:
-			return MockOperator.multiply.calculate(lhs: lhsOperand, rhs: rhsOperand)
-		}
+		return 0.0
 	}
+	
 	init(operands: MockCalculatorItemQueue<T>, operators: MockCalculatorItemQueue<U>) {
 		self.operands = operands
 		self.operators = operators
