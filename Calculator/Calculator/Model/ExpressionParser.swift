ed70afc2a09be98c403539cc362cb474a426cf40
Serena <sha0720@naver.com>
2023-06-07 15:11:59 +0900

    refactor: stringParts "" 값 출력 삭제 filter 함수 추가

--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -12,13 +12,14 @@ enum ExpressionParser {
         let componentParts = self.componentsByOperators(from: input)
         let numberParts: [Double] = componentParts.compactMap(Double.init)
         let stringPartsStandard = CharacterSet(charactersIn: componentParts.joined())
-        let stringParts: [String] = input.components(separatedBy: stringPartsStandard).filter {
-            $0 != ""
-        }
+        let stringParts: [String] = input
+            .components(separatedBy: stringPartsStandard)
+            .filter { $0 != "" }
         
         return Formula(operands: CalculatorItemQueue(queue: numberParts),
                        operators: CalculatorItemQueue(queue: stringParts))
     }
+    
     static private func componentsByOperators(from input: String) -> [String] {
         let allOperatorRawValues = Operator.allCases.map{ $0.rawValue }
         let componentStandard = CharacterSet(charactersIn: String(allOperatorRawValues))
