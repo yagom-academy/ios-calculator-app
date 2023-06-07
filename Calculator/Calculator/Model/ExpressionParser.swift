fb8ae08a4f8bae5a08594d2de17704a8afb7244a
Whales <Whales.w.Jin@gmail.com>
2023-06-08 07:45:54 +0900

    feat: ExpressionParser 열거형에 components 메서드 구현
    공백을 기준으로 쪼개져서 문자열 배열로 반환

--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -5,6 +5,10 @@
 //  Created by Whales on 2023/06/07.
 //
 
+import Foundation
+
 enum ExpressionParser {
-    
+    private static func componentsByOperators(from input: String) -> [String] {
+        return input.split(with: " ")
+    }
 }
