788e0913afc791e2e0ef466b1f2ac5b7a379c999
Whales <Whales.w.Jin@gmail.com>
2023-06-08 08:04:22 +0900

    feat: ExpressionParser 열거형에 parse 메서드 구현
    components 중 연산자에 포함되어있는 원소들만 Formula 연산자 큐에 추가

--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -8,6 +8,29 @@
 import Foundation
 
 enum ExpressionParser {
+    static func parse(from input: String) throws -> Formula {
+        var operands: CalculatorItemQueue<Double> = CalculatorItemQueue()
+        var operators: CalculatorItemQueue<Operator> = CalculatorItemQueue()
+        
+        let components = componentsByOperators(from: input)
+        
+        let operatorsArray = components.filter { (element) -> Bool in
+            return ["+", "-", "/", "*"].contains(element)
+        }
+        
+        for element in operatorsArray {
+            let changeToCharacter = Character(element)
+            
+            guard let changeToOperator = Operator(rawValue: changeToCharacter) else {
+                throw CalculatorError.invalidOperator
+            }
+            
+            operators.enqueue(changeToOperator)
+        }
+        
+        return Formula(operands: operands, operators: operators)
+    }
+    
     private static func componentsByOperators(from input: String) -> [String] {
         return input.split(with: " ")
     }
