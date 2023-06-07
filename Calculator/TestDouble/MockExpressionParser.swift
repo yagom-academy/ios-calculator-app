69191fd408083a06bdef7a3c7f0e9b47bd13906b
Kobe <mskang@xingxingmobility.com>
2023-06-07 09:37:29 +0900

    :sparkles: Feat: ExpressionParser의 Mock 객체 생성.
    테스트를 위형 MockExpressionParser 객체를 생성하였습니다.

--- Calculator/TestDouble/MockExpressionParser.swift
+++ Calculator/TestDouble/MockExpressionParser.swift
@@ -5,4 +5,18 @@
 //  Created by Minseong Kang on 2023/06/07.
 //
 
-import Foundation
+enum MockExpressionParser<T: CalculateItem> {
+	static func parse(from input: String) -> Formula<T> {
+		let operandsLinkedList = LinkedList<T>()
+		let operatorsLinkedList = LinkedList<T>()
+		let operands = CalculatorItemQueue(list: operandsLinkedList)
+		let operators = CalculatorItemQueue(list: operatorsLinkedList)
+		let result = Formula(operands: operands, operators: operators)
+		
+		return result
+	}
+	
+	static private func componentsByOperators(from input: String) -> [String] {
+		return [""]
+	}
+}
