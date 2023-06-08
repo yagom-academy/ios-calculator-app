1aea1e4fb196886d63d31a834370bbb8ad406586
Serena <sha0720@naver.com>
2023-06-08 22:57:47 +0900

    refactor: componentsParts 타입 변경

--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -9,19 +9,20 @@ import Foundation
 
 enum ExpressionParser {
     static func parse(from input: String) -> Formula {
-        let componentParts = self.componentsByOperators(from: input)
+        let componentParts: [String] = self.componentsByOperators(from: input)
         let numberParts: [Double] = componentParts.compactMap(Double.init)
         let stringPartsStandard = CharacterSet(charactersIn: componentParts.joined())
-        let stringParts: [String] = input
+        let stringParts: [Operator] = input
             .components(separatedBy: stringPartsStandard)
             .filter { $0 != "" }
+            .compactMap { Operator(rawValue: Character($0)) }
         
         return Formula(operands: CalculatorItemQueue(queue: numberParts),
                        operators: CalculatorItemQueue(queue: stringParts))
     }
     
     static private func componentsByOperators(from input: String) -> [String] {
-        let allOperatorRawValues = Operator.allCases.map{ $0.rawValue }
+        let allOperatorRawValues = Operator.allCases.map { $0.rawValue }
         let componentStandard = CharacterSet(charactersIn: String(allOperatorRawValues))
         let componentParts: [String] = input.components(separatedBy: componentStandard)
         
