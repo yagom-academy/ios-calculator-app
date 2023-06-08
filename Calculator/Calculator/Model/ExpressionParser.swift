02de8147546739b51df79fd4fab4a0f20464407f
DasanKim <tempzipp@gmail.com>
2023-06-08 11:58:24 +0900

    🩹chore: ExpressionParser내 고차함수 개행 수정

--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -12,28 +12,23 @@ enum ExpressionParser {
         var formula = Formula(operands: CalculatorItemQueue(),
                               operators: CalculatorItemQueue())
         
-        let operands = componentsByOperators(from: input).compactMap { Double($0) }
-        
-        operands.forEach {
-            formula.operands.enqueue($0)
-        }
+        componentsByOperators(from: input)
+            .compactMap { Double($0) }
+            .forEach { formula.operands.enqueue($0) }
     
-        let operators = input.filter { $0.isNumber == false }
-                             .compactMap { Operator(rawValue: $0) }
-        
-        operators.forEach {
-            formula.operators.enqueue($0)
-        }
+        input
+            .filter { $0.isNumber == false }
+            .compactMap { Operator(rawValue: $0) }
+            .forEach { formula.operators.enqueue($0) }
         
         return formula
     }
     
     private static func componentsByOperators(from input: String) -> [String] {
-        var operands: [String]
-        
-        operands = Operator.allCases.reduce([input]) { accumulatedArray, operatorItem in
-            accumulatedArray.map { $0.split(with: operatorItem.rawValue) }
-                            .flatMap { $0 }
+        let operands = Operator.allCases.reduce([input]) { accumulatedArray, operatorItem in
+            accumulatedArray
+                .map { $0.split(with: operatorItem.rawValue) }
+                .flatMap { $0 }
         }
 
         return operands
