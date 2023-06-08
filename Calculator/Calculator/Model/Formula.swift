30c2a529f9ecfd16f108a14b2c32e10a1f166dca
Whales <Whales.w.Jin@gmail.com>
2023-06-08 10:18:10 +0900

    feat: Formula 구조체 result 메서드 구현
    피연산자 큐가 비어있으면 0을, 연산자 큐가 비어있으면 피연산자 값을, 그 외 경우에는 피연산자 값들을 연산자로 계산한 최종값을 반환한다.

--- Calculator/Calculator/Model/Formula.swift
+++ Calculator/Calculator/Model/Formula.swift
@@ -8,4 +8,22 @@
 struct Formula {
     var operands: CalculatorItemQueue<Double>
     var operators: CalculatorItemQueue<Operator>
+    
+    mutating func result() throws -> Double {
+        if operands.isEmpty {
+            return 0
+        } else if operators.isEmpty, let result = operands.readFirstData() {
+            return result
+        }
+        
+        var result: Double = try operands.dequeue()
+        
+        result = try operators.dequeue().calculate(lhs: result, rhs: try operands.dequeue())
+        
+        while !operands.isEmpty {
+            result = try operators.dequeue().calculate(lhs: result, rhs: try operands.dequeue())
+        }
+        
+        return result
+    }
 }
