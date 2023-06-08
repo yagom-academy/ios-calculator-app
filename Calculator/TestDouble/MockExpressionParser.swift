b37b1fbab6cf7e0cbc23dc3eec6c569d1249edc3
Kobe <mskang@xingxingmobility.com>
2023-06-08 18:51:54 +0900

    :recycle: Refactor: componentsByOperators 메서드 내부 로직 수정.
    MockExpressionParser의 componentsByOperators의 메서드 내부의 로직을 고차함수와 split을 이용하여 수정하였습니다.

--- Calculator/TestDouble/MockExpressionParser.swift
+++ Calculator/TestDouble/MockExpressionParser.swift
@@ -17,25 +17,12 @@ enum MockExpressionParser<T: CalculateItem> {
 	}
 	
 	static func componentsByOperators(from input: String) -> [String] {
-		var expressions: [String] = []
-		var operatorList: [String] = []
+		let operatorList = Operator.allCases.compactMap { $0.rawValue }
 		
-		let operators = Operator.allCases.compactMap { String($0.rawValue) }
-		for `operator` in operators {
-			operatorList.append(`operator`)
+		let expression = operatorList.reduce([input]) { partialResult, `operator` in
+			return partialResult.map { $0.split(with: `operator`)}.flatMap { $0 }
 		}
 		
-		input.forEach {
-			expressions.append(String($0))
-		}
-		
-		operatorList.forEach {
-			let isContainOperator = expressions.contains($0)
-			guard isContainOperator else { return }
-			guard let operatorIndex = expressions.firstIndex(of: $0) else { return }
-			expressions.remove(at: operatorIndex)
-		}
-		
-		return expressions
+		return expression
 	}
 }
