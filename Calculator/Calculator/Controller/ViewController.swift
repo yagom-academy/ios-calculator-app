38e2b8fe8bbe24def8002e7413f443252fc93586
mint3382 <mskim0926@naver.com>
2023-06-08 19:05:45 +0900

    feat: tappedOperands, tappedOperator 생성

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -7,12 +7,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var operators: UILabel!
+    @IBOutlet weak var operands: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        operands.text = "0"
+        operators.text = ""
     }
 
-
+    @IBAction func tappedOperands(_ sender: UIButton) {
+        guard let operand = sender.currentTitle else {
+            return
+        }
+        
+        operands.text = operand
+    }
+    
+    @IBAction func tappedOperator(_ sender: UIButton) {
+        guard let `operator` = sender.currentTitle else {
+            return
+        }
+        
+        operators.text = `operator`
+        
+        
+    }
+    
+    
 }
 
