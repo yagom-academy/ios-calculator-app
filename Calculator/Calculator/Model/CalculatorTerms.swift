4250d39170004594acdfa7d51a00e608393a3302
h-suo <phstoo1021@naver.com>
2023-06-08 18:25:06 +0900

    refactor: 컨트롤러에서 반복 사용되는 문자들을 정리한 CalculatorTerms 생성

---
+++ Calculator/Calculator/Model/CalculatorTerms.swift
@@ -0,0 +1,13 @@
+//
+//  CalculatorTerms.swift
+//  Calculator
+//
+//  Created by Erick on 2023/06/08.
+//
+
+enum CalculatorTerms: String {
+    case zero = "0"
+    case doubleZero = "00"
+    case minusSign = "-"
+    case decimalPoint = "."
+}
