22c7a18c31618ce358948d50ba7afe8fc5537cc2
Serena <sha0720@naver.com>
2023-06-08 22:55:27 +0900

    refactor: map함수 컨벤션 통일

--- Calculator/Calculator/Model/Extensions/String+.swift
+++ Calculator/Calculator/Model/Extensions/String+.swift
@@ -5,9 +5,9 @@
 //  Created by Serena on 2023/05/31.
 //
 
-extension String: CalculateItem {
+extension String {
     func split(with target: Character) -> [String] {
-        let targetArray = self.split(separator: target).map { String($0) }
+        let targetArray = self.split(separator: target).map(String.init)
         
         return targetArray
     }
