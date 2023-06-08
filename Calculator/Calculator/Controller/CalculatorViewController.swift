34a0695040abc929bce54a242e7bdf0f3c210883
agilestarskim <agilestars@kakao.com>
2023-06-08 22:02:59 +0900

    refactor: 뷰컨트롤러에서 비지니스 로직 분리 및 = 버튼 중복클릭 방지

--- Calculator/Calculator/Controller/CalculatorViewController.swift
+++ Calculator/Calculator/Controller/CalculatorViewController.swift
@@ -8,134 +8,67 @@ import UIKit
 
 class CalculatorViewController: UIViewController {
     
+    let inputProcessor = InputProcessor()
+    
     @IBOutlet weak var displayLabel: UILabel!
     @IBOutlet weak var operatorLabel: UILabel!
     @IBOutlet weak var recordStackView: UIStackView!
     @IBOutlet weak var recordScrollView: UIScrollView!
-    
-    private var currentOperand: String = ""
-    private var currentOperator: String = ""
-    private var formula: String = ""
+    @IBOutlet weak var equalButton: UIButton!
     
     override func viewDidLoad() {
-        self.updateOperandWithDisplay(by: Symbol.empty)
-        self.operatorLabel.text = Symbol.empty
+        self.displayLabel.text = "0"
+        self.equalButton.isEnabled = false
         self.recordScrollView.translatesAutoresizingMaskIntoConstraints = false
         self.recordStackView.translatesAutoresizingMaskIntoConstraints = false
     }
     
     @IBAction private func tapNumberButton(_ sender: UIButton) {
-        self.setFormula(when: .tapNumberButton)
-        self.updateOperandWithDisplay(by: sender.unwrappedTitle)
+        self.displayLabel.text = self.inputProcessor.inputOperand(sender.unwrappedTitle)
     }
     
     @IBAction private func tapDecimalPointButton(_ sender: UIButton) {
-        self.updateOperandWithDisplay(by: Symbol.dot)
+        self.displayLabel.text = self.inputProcessor.inputOperand(Symbol.dot)
     }
     
     
     @IBAction private func tapOperatorButton(_ sender: UIButton) {
-        self.currentOperator = sender.sign
-        self.operatorLabel.text = sender.sign
-        self.recordOnStack()
-        self.setFormula(when: .tapOperatorButton)
+        self.operatorLabel.text = self.inputProcessor.inputOperator(sender.unwrappedTitle) { `operator`, operand in
+            self.displayLabel.text = "0"
+            return self.recordOnStack(operater: `operator`, operand: operand)
+        }
+        self.equalButton.isEnabled = true
     }
 
     
     @IBAction private func tapEqualButton(_ sender: UIButton) {
-        do {
-            self.setFormula(when: .tapEqualButton)
-            var parsedFormula = ExpressionParser.parse(from: self.formula)
-            let result = try parsedFormula.result()
-            self.displayLabel.text = DisplayFormatter.string(from: result)
-        } catch CalculationError.divisionByZero {
-            self.displayLabel.text = Error.nan
-        } catch {
-            
+        self.displayLabel.text = self.inputProcessor.getResult {`operator`, operand in
+            return self.recordOnStack(operater: `operator`, operand: operand)
         }
+        self.operatorLabel.text = ""
+        self.equalButton.isEnabled = false
     }
     
     @IBAction private func tapAllClearButton(_ sender: UIButton) {
-        self.allClear()
+        self.inputProcessor.allClear()
+        self.displayLabel.text = "0"
+        self.operatorLabel.text = ""
         self.clearRecords()
     }
     
-    
     @IBAction private func tapClearEntryButton(_ sender: UIButton) {
-        self.updateOperandWithDisplay(by: Symbol.empty)
+        self.inputProcessor.clearLastOperand()
+        self.displayLabel.text = "0"
     }
     
     @IBAction private func tapSignChangeButton(_ sender: UIButton) {
-        self.toggleSign()
+        self.displayLabel.text = self.inputProcessor.toggleSign()
     }
     
-    private func updateOperandWithDisplay(by newElement: String) {
-        switch newElement {
-        case Symbol.empty:
-            self.currentOperand = Symbol.empty
-            self.displayLabel.text = Number.zero
-            
-        case Number.all:
-            guard currentOperand.count < 20 else { return }
-            self.currentOperand.append(newElement)
-            self.displayLabel.text = DisplayFormatter.string(from: self.currentOperand)
-            
-        case Symbol.dot:
-            guard currentOperand.count < 19 else { return }
-            guard !self.currentOperand.contains(Symbol.dot) else { return }
-            self.currentOperand.append(self.currentOperand.isEmpty ? Number.zero + Symbol.dot : Symbol.dot)
-            self.displayLabel.text = self.displayLabel.unwrappedText + Symbol.dot
-            
-        case Sign.plus:
-            self.currentOperand.remove(at: self.currentOperand.startIndex)
-            self.displayLabel.text = DisplayFormatter.string(from: self.currentOperand)
-        
-        case Sign.minus:
-            self.currentOperand.insert(Character(Sign.minus), at: self.currentOperand.startIndex)
-            self.displayLabel.text = Sign.minus + self.displayLabel.unwrappedText
-        
-        default:
-            return
-        }
-    }
-    
-    private func toggleSign() {
-        guard self.currentOperand.isNotEmpty else { return }
-        if self.currentOperand.contains(Sign.minus) {
-            self.updateOperandWithDisplay(by: Sign.plus)
-        } else {
-            self.updateOperandWithDisplay(by: Sign.minus)
-        }
-    }
-    
-    private func setFormula(when mode: Mode) {
-        switch mode {
-        case .tapNumberButton:
-            guard self.currentOperator.isNotEmpty else { return }
-            self.formula += self.currentOperator
-            self.currentOperator = Symbol.empty
-        case .tapOperatorButton:
-            guard self.currentOperand.isNotEmpty else { return }
-            self.formula += self.currentOperand
-            self.updateOperandWithDisplay(by: Symbol.empty)
-        case .tapEqualButton:
-            guard self.formula.isNotEmpty else { return }
-            self.formula += self.currentOperator + self.currentOperand
-        }
-    }
-    
-    private func allClear() {
-        self.updateOperandWithDisplay(by: Symbol.empty)
-        self.currentOperator = Symbol.empty
-        self.operatorLabel.text = Symbol.empty
-        self.formula = Symbol.empty
-    }
-    
-    private func recordOnStack() {
-        guard self.currentOperand.isNotEmpty && self.currentOperator.isNotEmpty else { return }
+    private func recordOnStack(operater: String, operand: String) {
         let operatorLabel: UILabel = {
             let label = UILabel()
-            label.text = self.currentOperator
+            label.text = operater
             label.textColor = .white
             label.font = UIFont.preferredFont(forTextStyle: .title3)
             
@@ -144,7 +77,7 @@ class CalculatorViewController: UIViewController {
         
         let operandLabel: UILabel = {
             let label = UILabel()
-            label.text = self.displayLabel.unwrappedText
+            label.text = operand
             label.textColor = .white
             label.font = UIFont.preferredFont(forTextStyle: .title3)
             
