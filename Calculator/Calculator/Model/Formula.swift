ef55538c7eb92d6cf0dc967c3da19c3f27fafb9b
Kobe <mskang@xingxingmobility.com>
2023-06-09 10:57:35 +0900

    :recycle: Refactor: Formula의 operators 제네릭 타입 수정[BLUE]
    Formula의 operators의 CalulatorItemQueue<T> 타입에서 CalulatorItemQueue<U> 타입으로 수정하였습니다.

--- Calculator/Calculator/Model/Formula.swift
+++ Calculator/Calculator/Model/Formula.swift
@@ -7,7 +7,7 @@
 
 struct Formula<T: CalculateItem, U: CalculateItem>: FormulaProtocol {
 	var operands: CalculatorItemQueue<T>
-	var operators: CalculatorItemQueue<T>
+	var operators: CalculatorItemQueue<U>
 	func result() -> Double {
 		return 0.0
 	}
