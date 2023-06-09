4df1dc1c264e09de2473e5d53d85c6a45a6e357d
JusBug <pksgwo123@gmail.com>
2023-06-09 19:29:34 +0900

    feat: UI 연동하여 메서드 생성, 부가 기능 함수 추가

--- Calculator/Calculator/ViewController/ViewController.swift
+++ Calculator/Calculator/ViewController/ViewController.swift
@@ -1,16 +1,47 @@
 //
 //  Calculator - ViewController.swift
-//  Created by yagom. 
+//  Created by yagom.
 //  Copyright © yagom. All rights reserved.
-// 
+//
 
 import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var displayOperandLabel: UILabel!
+    @IBOutlet weak var displayOperatorLabel: UILabel!
+    @IBOutlet weak var stackView: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
+    }
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
         
     }
-}
+    
+    @IBAction func touchOperator(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func touchCalculate(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func touchMenu(_ sender: UIButton) {
+        
+    }
+    
+    private func clearOperandLabel() {
 
+    }
+    
+    private func clearOperatorLabel() {
+
+    }
+    
+    private func clearStack() {
+
+    }
+}
