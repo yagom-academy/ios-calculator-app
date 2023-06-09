27b7d0f13129d0e62bcad606a1451752ffd9241d
Hoon94 <dleognsdl1@naver.com>
2023-06-09 13:14:15 +0900

    refactor: NumberFormatter 추가

--- Calculator/Calculator/Extension/Double+Extension.swift
+++ Calculator/Calculator/Extension/Double+Extension.swift
@@ -5,4 +5,16 @@
 //  Created by Daehoon Lee on 2023/06/06.
 //
 
-extension Double: CalculateItem {}
+import Foundation
+
+extension Double: CalculateItem {
+    func formatNumbers() -> String {
+        let numberFormat = NumberFormatter()
+        let number = NSNumber(value: self)
+        numberFormat.numberStyle = .decimal
+        numberFormat.usesSignificantDigits = true
+        numberFormat.maximumSignificantDigits = 20
+        
+        return String(numberFormat.string(from: number) ?? "")
+    }
+}
