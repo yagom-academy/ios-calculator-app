4545cc59e9bb6dedca4fcac557b0ea9c7515d24e
Serena <sha0720@naver.com>
2023-06-10 03:17:01 +0900

    feat: IBOutlet 연결

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -8,6 +8,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var inputNumberLabel: UILabel!
+    @IBOutlet weak var inputOperatorLabel: UILabel!
+    @IBOutlet weak var allStackView: UIStackView!
+    @IBOutlet weak var scrollView: UIScrollView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
