d4fa90d0602b8deb52f9dc4cb3ca2bb2cf00041d
Serena <sha0720@naver.com>
2023-06-10 03:18:06 +0900

    feat: tapNumbersButton 함수 구현

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -12,9 +12,20 @@ class ViewController: UIViewController {
     @IBOutlet weak var inputOperatorLabel: UILabel!
     @IBOutlet weak var allStackView: UIStackView!
     @IBOutlet weak var scrollView: UIScrollView!
+    private var resultString: String = ""
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        inputNumberLabel.text = "0"
+        inputOperatorLabel.text = ""
+        
+    @IBAction func tapNumbersButton(_ sender: UIButton) {
+        guard let inputNumber = sender.titleLabel?.text,
+              let numberLabel = inputNumberLabel.text else { return }
+        
+        inputNumberLabel.text = (numberLabel == "0") ? (inputNumber) : (numberLabel + inputNumber)
+    }
+    
     }
 
 
