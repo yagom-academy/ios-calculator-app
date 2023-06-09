6b897f6986f2b08b852a62725b578bab2c3a9581
Kobe <mskang@xingxingmobility.com>
2023-06-09 10:30:39 +0900

    :recycle: Refactor: FormulaProtocol 코드 수정.
    FormulaProtocol의 내부에 U 제네릭 타입을 추가하고 operators의 CalculatorItemQueue에 U를 제네릭을 채택했습니다.

--- Calculator/Calculator/Model/Protocols/FormulaProtocol.swift
+++ Calculator/Calculator/Model/Protocols/FormulaProtocol.swift
@@ -7,7 +7,8 @@
 
 protocol FormulaProtocol {
 	associatedtype T: CalculateItem
+	associatedtype U: CalculateItem
 	var operands: CalculatorItemQueue<T> { get }
-	var operators: CalculatorItemQueue<T> { get }
+	var operators: CalculatorItemQueue<U> { get }
 	func result() -> Double
 }
