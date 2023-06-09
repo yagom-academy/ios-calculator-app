16b0f4cb7d93d3fa90d35b73e79be852b15bf7c5
Kobe <mskang@xingxingmobility.com>
2023-06-09 11:18:50 +0900

    :recycle: Refactor: Formula의 result 함수 내부 로직 수정[BLUE].
    Formula의 result 함수의 내부 로직을 테스트 성공 케이스에 부합하도록 수정하였습니다.

--- Calculator/Calculator/Model/Formula.swift
+++ Calculator/Calculator/Model/Formula.swift
@@ -8,7 +8,33 @@
 struct Formula<T: CalculateItem, U: CalculateItem>: FormulaProtocol {
 	var operands: CalculatorItemQueue<T>
 	var operators: CalculatorItemQueue<U>
+	
 	func result() -> Double {
-		return 0.0
+		var operandsList: [Double] = []
+		while let operands = operands.dequeue() {
+			guard let operand = operands as? Double else { return -1.0 }
+			operandsList.append(operand)
+		}
+		
+		let lhsOperand = operandsList[operandsList.startIndex]
+		let rhsOperand = operandsList[operandsList.endIndex-1]
+
+		guard let operators = operators.dequeue() as? Operator else { return -3.0 }
+		
+		switch operators {
+		case .add:
+			return lhsOperand + rhsOperand
+		case .subtract:
+			return lhsOperand - rhsOperand
+		case .divide:
+			return lhsOperand / rhsOperand
+		case .multiply:
+			return lhsOperand * rhsOperand
+		}
+	}
+	
+	init(operands: CalculatorItemQueue<T>, operators: CalculatorItemQueue<U>) {
+		self.operands = operands
+		self.operators = operators
 	}
 }
