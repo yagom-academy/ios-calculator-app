036dda5884bd948fb61fe9b783d6ff106c6334f1
조호준 <johojun@johojun-ui-MacBookAir.local>
2023-06-09 12:08:53 +0900

    feat: 숫자 패드를 눌렀을 때 레이블에 숫자가 표시되는 tapNumpad 메서드 추가

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -7,12 +7,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var inputNumberLabel: UILabel!
+    @IBOutlet weak var inputOperatorLabel: UILabel!
+    @IBOutlet weak var inputLabelStackView: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        inputNumberLabel.text = "0"
+        inputOperatorLabel.text = ""
     }
 
-
+    @IBAction func tapNumpad(_ sender: UIButton) {
+        guard let labelText = sender.titleLabel?.text else {
+            return
+        }
+        if inputNumberLabel.text == "0" {
+            inputNumberLabel.text? = labelText
+        } else {
+            inputNumberLabel.text? += labelText
+        }
+    }
 }
 
