dc13323be941fc962a59dbf9e215632a27ab565e
Kobe <mskang@xingxingmobility.com>
2023-06-09 09:26:28 +0900

    :recycle: Refactor: parse 메서드 내부 로직 수정.
    MockExpressionParser의 parse 메서드 내부 로직을 수정하였습니다.

--- Calculator/TestDouble/MockExpressionParser.swift
+++ Calculator/TestDouble/MockExpressionParser.swift
@@ -5,13 +5,27 @@
 //  Created by Minseong Kang on 2023/06/07.
 //
 
-enum MockExpressionParser<T: CalculateItem> {
-	static func parse(from input: String) -> Formula<T> {
-		let operandsLinkedList = LinkedList<T>()
-		let operatorsLinkedList = LinkedList<T>()
-		let operands = CalculatorItemQueue(list: operandsLinkedList)
-		let operators = CalculatorItemQueue(list: operatorsLinkedList)
-		let result = Formula(operands: operands, operators: operators)
+enum MockExpressionParser<T: CalculateItem, U: CalculateItem> {	
+	static func parse(from input: String) -> MockFormula<Double, Operator> {
+		// 1+2
+		let operandsLinkedList = MockLinkedList<Double>()
+		let operatorsLinkedList = MockLinkedList<Operator>()
+		let operandQueue = MockCalculatorItemQueue(dummyList: operandsLinkedList)
+		let operatorQueue = MockCalculatorItemQueue(dummyList: operatorsLinkedList)
+		
+		let operands = componentsByOperators(from: input).compactMap { Double($0) }
+		operands.forEach {
+			operandQueue.enqueue($0)
+		}
+		
+		let operatorList = Operator.allCases.compactMap { $0.rawValue }
+		operatorList.forEach {
+			guard input.contains($0) else { return }
+			guard let `operator` = Operator(rawValue: $0) else { return }
+			operatorQueue.enqueue(`operator`)
+		}
+		
+		let result = MockFormula(operands: operandQueue, operators: operatorQueue)
 		
 		return result
 	}
