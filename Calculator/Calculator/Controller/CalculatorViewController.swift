01ddca937db874ad73c90090b7a54093576f5c39
bubblecocoa <bubblecocoa@gmail.com>
2023-06-09 23:57:16 +0900

    feat: CalculatorViewController의 IBOutlet, IBAction 선언 및 스토리보드와 연결

--- Calculator/Calculator/Controller/CalculatorViewController.swift
+++ Calculator/Calculator/Controller/CalculatorViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 class CalculatorViewController: UIViewController {
     // MARK: - IBOutlet
     
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var calculateItemsStack: UIStackView!
+    @IBOutlet weak var operatorLabel: UILabel!
+    @IBOutlet weak var operandLabel: UILabel!
+    
     // MARK: - Property
     
     // MARK: - View State Method
@@ -19,6 +24,38 @@ class CalculatorViewController: UIViewController {
     
     // MARK: - IBAction
     
+    @IBAction func tapACButton(_ sender: Any) {
+        
+    }
+
+    @IBAction func tapCEButton(_ sender: Any) {
+        
+    }
+
+    @IBAction func tapNEGButton(_ sender: Any) {
+        
+    }
+
+    @IBAction func tapOperatorButton(_ sender: UIButton) {
+    
+    }
+
+    @IBAction func tapNumberButton(_ sender: UIButton) {
+      
+    }
+
+    @IBAction func tapZeroButton(_ sender: UIButton) {
+
+    }
+
+    @IBAction func tapDotButton(_ sender: UIButton) {
+
+    }
+
+    @IBAction func tapEqualsButton(_ sender: Any) {
+
+    }
+
     // MARK: - Method
 }
 
