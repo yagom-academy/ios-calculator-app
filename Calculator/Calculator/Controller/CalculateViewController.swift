38b228eecd6bb8708e8e3c4bfa341a489fea70e0
mint3382 <mskim0926@naver.com>
2023-06-10 23:50:19 +0900

    refactor: 네이밍 수정

--- Calculator/Calculator/Controller/CalculateViewController.swift
+++ Calculator/Calculator/Controller/CalculateViewController.swift
@@ -7,25 +7,16 @@
 import UIKit
 
 class CalculateViewController: UIViewController {
-    //현재 표시되는 연산자 라벨
     @IBOutlet weak var currentOperatorLabel: UILabel!
-    //현재 표시되는 피연산자 라벨
     @IBOutlet weak var currentOperandLabel: UILabel!
-    //스크롤뷰에 쌓이는 스텍뷰, 연산 과정을 저장하는 스텍뷰
-    @IBOutlet weak var saveFormulaStackView: UIStackView!
-    //연산과정을 저장하는 스크롤뷰
-    @IBOutlet weak var saveFormulaScrollView: UIScrollView!
-    //현재까지의 수식들이 담기는 문자열 배열
+    @IBOutlet weak var currentFormulaStackView: UIStackView!
+    @IBOutlet weak var currentFormulaScrollView: UIScrollView!
     private var formulasUntilNow = [String]()
-    //숫자0이 눌려졌었는지
-    private var isZeroButtonTappedBefore = true
-    //결과값이 나왔는지
-    private var isResultValue = false
-    //.(dot)이 사용되었었는지
-    private var isDotUsed = false
-    //연산자버튼이 눌려졌었는지
-    private var isOperatorButtonPushed = false
-    //현재 표시되는 피연산자 라벨 초기화하는지
+    //라벨에 있는 0이 버튼을 누른 값인지
+    private var isZeroButtonTapped = true
+    //라벨에 있는 값이 결과값인지
+    private var isCurrentOperandLabelMadeFromResult = false
+    private var canTapDotButton = true
     private var isInitializeCurrentOperandLabel = false {
         didSet {
             if isInitializeCurrentOperandLabel {
@@ -33,7 +24,6 @@ class CalculateViewController: UIViewController {
             }
         }
     }
-    //현재 표시되는 연산자 라벨 초기화하는지
     private var isInitializeCurrentOperatorLabel = false {
         didSet {
             if isInitializeCurrentOperatorLabel {
@@ -48,7 +38,7 @@ class CalculateViewController: UIViewController {
             return "NaN"
         }
         
-        return "\(operatorText) \(checkFutureOperand(numberText).filter { $0 != "," }) "
+        return "\(operatorText) \(checkOperandForm(numberText).filter { $0 != "," }) "
     }
     
     override func viewDidLoad() {
@@ -62,8 +52,8 @@ class CalculateViewController: UIViewController {
     @IBAction func tappedOperandsButton(_ sender: UIButton) {
         guard let number = sender.currentTitle,
               let operandLabelText = currentOperandLabel.text,
-              isResultValue == false,
-              checkFutureOperand(operandLabelText + number) != "error" else {
+              isCurrentOperandLabelMadeFromResult == false,
+              checkOperandForm(operandLabelText + number) != "error" else {
             return
         }
         
@@ -76,14 +66,13 @@ class CalculateViewController: UIViewController {
     
     //.(dot) 버튼 눌릴 때
     @IBAction func tappedDotButton(_ sender: UIButton) {
-        guard isDotUsed == false,
-              let operandLabelText = currentOperandLabel.text,
-              isResultValue == false,
-              checkFutureOperand(operandLabelText) != "error" else {
+        guard let operandLabelText = currentOperandLabel.text,
+              canTapDotButton == true,
+              isCurrentOperandLabelMadeFromResult == false else {
             return
         }
         
-        isDotUsed = true
+        canTapDotButton = false
         currentOperandLabel.text = operandLabelText + "."
     }
     
@@ -92,12 +81,12 @@ class CalculateViewController: UIViewController {
         guard let operandLabelText = currentOperandLabel.text,
               operandLabelText != "0" else {
             currentOperandLabel.text = "0"
-            isZeroButtonTappedBefore = true
+            isZeroButtonTapped = true
             return
         }
         
-        guard checkFutureOperand(operandLabelText + "0") != "error",
-              isResultValue == false else {
+        guard checkOperandForm(operandLabelText + "0") != "error",
+              isCurrentOperandLabelMadeFromResult == false else {
             return
         }
         
@@ -109,12 +98,12 @@ class CalculateViewController: UIViewController {
         guard let operandLabelText = currentOperandLabel.text,
               operandLabelText != "0" else {
             currentOperandLabel.text = "0"
-            isZeroButtonTappedBefore = true
+            isZeroButtonTapped = true
             return
         }
         
-        guard checkFutureOperand(operandLabelText + "00") != "error",
-              isResultValue == false else {
+        guard checkOperandForm(operandLabelText + "00") != "error",
+              isCurrentOperandLabelMadeFromResult == false else {
             return
         }
         
@@ -123,49 +112,49 @@ class CalculateViewController: UIViewController {
     
     //연산자(+-%*) 버튼 눌릴때
     @IBAction func tappedOperatorButton(_ sender: UIButton) {
-        guard let operand = currentOperandLabel.text,
-              operand != "0" || isZeroButtonTappedBefore,
-              let operandLabelText = currentOperandLabel.text,
-              checkFutureOperand(operandLabelText) != "error" else {
+        guard let operandLabelText = currentOperandLabel.text,
+              operandLabelText != "0" ||  isZeroButtonTapped,
+              checkOperandForm(operandLabelText) != "error" else {
+            currentOperatorLabel.text = sender.currentTitle
             return
         }
         
-        settingFormula()
+        addCurrentFormula()
 
         currentOperatorLabel.text = sender.currentTitle
 
-        isOperatorButtonPushed = true
-        isResultValue = false
-        isZeroButtonTappedBefore = false
-        isDotUsed = false
+
+        isCurrentOperandLabelMadeFromResult = false
+        isZeroButtonTapped = false
+        canTapDotButton = true
         isInitializeCurrentOperandLabel = true
     }
     
     //=(결과) 버튼 눌릴 때
     @IBAction func tappedResultButton(_ sender: Any) {
-        guard isResultValue == false,
-              isOperatorButtonPushed == true else {
+        guard isCurrentOperandLabelMadeFromResult == false,
+              (currentOperandLabel.text?.isEmpty) == nil else {
             return
         }
-        settingFormula()
+        addCurrentFormula()
         
         var formula = ExpressionParser.parse(from: formulasUntilNow.joined())
         
         let result = formula.result()
-        isResultValue = true
-        isZeroButtonTappedBefore = true
+        isCurrentOperandLabelMadeFromResult = true
+        isZeroButtonTapped = true
         
         currentOperandLabel.text = result.formatNumber()
         formulasUntilNow.removeAll()
         isInitializeCurrentOperatorLabel = true
-        isDotUsed = false
+        canTapDotButton = true
     }
     
     //-+ 변환 부호 버튼 눌릴 때
     @IBAction func tappedChangeSignButton(_ sender: Any) {
         guard var operandLabelText = currentOperandLabel.text,
                 operandLabelText != "0",
-                checkFutureOperand(operandLabelText) != "error" else {
+                checkOperandForm(operandLabelText) != "error" else {
             return
         }
         
@@ -181,36 +170,32 @@ class CalculateViewController: UIViewController {
     //CE 버튼 눌릴 때
     @IBAction func tappedClearButton(_ sender: Any) {
         isInitializeCurrentOperandLabel = true
-        isResultValue = false
-        isZeroButtonTappedBefore = false
-        isDotUsed = false
+        isCurrentOperandLabelMadeFromResult = false
+        isZeroButtonTapped = false
+        canTapDotButton = true
     }
     
     //AC 버튼 눌릴 때
     @IBAction func tappedAllClearButton(_ sender: Any) {
-        saveFormulaStackView.arrangedSubviews.forEach { $0.removeFromSuperview() }
+        currentFormulaStackView.arrangedSubviews.forEach { $0.removeFromSuperview() }
         isInitializeCurrentOperandLabel = true
         isInitializeCurrentOperatorLabel = true
-        isResultValue = false
-        isZeroButtonTappedBefore = true
-        isDotUsed = false
-        isOperatorButtonPushed = false
+        isCurrentOperandLabelMadeFromResult = false
+        isZeroButtonTapped = true
+        canTapDotButton = true
     }
 
-    //라벨의 수식을 문자열 배열에 저장하고, 스택뷰에 넘김
-    //다음연산을 위해 이번 수식을 저장하며 지우는 함수
-    private func settingFormula() {
+    private func addCurrentFormula() {
         guard let operatorString = currentOperatorLabel.text,
               let operandString = currentOperandLabel.text else {
             return
         }
         print(currentFormula)
         formulasUntilNow.append(currentFormula)
-        addView(operatorString, checkFutureOperand(operandString))
+        addView(operatorString, checkOperandForm(operandString))
     }
     
-    //들어갈 피연산자가 조건에 맞는지 확인하여 맞으면 형식에 알맞는 문자열을 반환하는 함수 (20자리, 컴마 등)
-    private func checkFutureOperand(_ input: String) -> String {
+    private func checkOperandForm(_ input: String) -> String {
         guard let number = Double(input.filter { $0 != "," })?.formatNumber(),
               input.count <= 20 else {
             return "error"
@@ -257,9 +242,9 @@ extension CalculateViewController {
     //스크롤뷰에 있는 스택뷰에 만든 서브뷰를 추가하고, 자동스크롤 되도록
     // 현재 수식이 보이도록 스택뷰에 추가하는 함수
     func addView(_ `operator`: String, _ operand: String) {
-        saveFormulaStackView.addArrangedSubview(addSubView(`operator`, operand))
+        currentFormulaStackView.addArrangedSubview(addSubView(`operator`, operand))
         
-        saveFormulaScrollView.layoutIfNeeded()
-        saveFormulaScrollView.setContentOffset(CGPoint(x: 0, y: saveFormulaScrollView.contentSize.height - saveFormulaScrollView.bounds.height), animated: true)
+        currentFormulaScrollView.layoutIfNeeded()
+        currentFormulaScrollView.setContentOffset(CGPoint(x: 0, y: currentFormulaScrollView.contentSize.height - currentFormulaScrollView.bounds.height), animated: true)
     }
 }
