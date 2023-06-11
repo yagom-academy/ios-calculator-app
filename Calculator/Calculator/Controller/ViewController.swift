582260484c688fdfe12c88f9b73b3e0bdbdf2fc1
조호준 <johojun@johojun-ui-MacBookAir.local>
2023-06-11 11:16:48 +0900

    refactor: tapAllClear 수정

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -148,6 +148,7 @@ class ViewController: UIViewController {
         resetInputNumberLabel()
         resetInputOperatorLabel()
         resetAllInputStackView()
+        resetFormulaString()
         operationReady = true
     }
     
