5d428cc88edbc3fd0ab5e97b9bda1bf169cb5b13
hemg2 <hemg2@naver.com>
2023-06-11 15:05:59 +0900

    feat: 다중 연산 1차

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -8,38 +8,51 @@ import UIKit
 
 final class ViewController: UIViewController {
     
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet var contentView: UIView!
     @IBOutlet weak var numberInputLabel: UILabel!
     @IBOutlet weak var operatorsLabel: UILabel!
+    @IBOutlet weak var firstNumberLabel: UILabel!
+    @IBOutlet weak var secondNumberLabel: UILabel!
+    @IBOutlet weak var firstOperatorLabel: UILabel!
+    @IBOutlet weak var secondOperatorLabel: UILabel!
+    @IBOutlet weak var operatorStackView: UIStackView!
+    
+    private var isReset: Bool = false
+    
     let operandQueue = CalculatorItemQueue<Double>()
     let operatorQueue = CalculatorItemQueue<Operator>()
     var defaultNumber: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
     
     @IBAction func tapNumberButton(_ sender: UIButton) {
         guard let numberValue = sender.title(for: .normal) else { return }
-        guard defaultNumber.count > 20 else {
-            defaultNumber += numberValue
-            numberInputLabel.text = defaultNumber
-            return
-        }
+        guard defaultNumber.count < 20 else { return }
+        
+        defaultNumber += numberValue
+        numberInputLabel.text = defaultNumber
     }
     
     @IBAction func tapClearButton(_ sender: UIButton) {
         defaultNumber = ""
+        operatorsLabel.text = ""
         numberInputLabel.text = "0"
+        firstNumberLabel.text = ""
+        secondNumberLabel.text = ""
+        secondOperatorLabel.text = ""
+        operandQueue.removeAll()
+        operatorQueue.removeAll()
     }
     
     @IBAction func tapDotButton(_ sender: UIButton) {
-        guard defaultNumber.count > 20,
-              defaultNumber.contains(".") else {
-            defaultNumber += defaultNumber.isEmpty ? "0." : "."
-            numberInputLabel.text = defaultNumber
-            return
-        }
+        guard defaultNumber.count < 20,
+              !defaultNumber.contains(".") else { return }
+        
+        defaultNumber += defaultNumber.isEmpty ? "0." : "."
+        numberInputLabel.text = defaultNumber
     }
     
     @IBAction func tapOperatorButton(_ sender: UIButton) {
@@ -49,12 +62,28 @@ final class ViewController: UIViewController {
         }
         
         operatorsLabel.text = operatorString
-        operatorQueue.enqueue(item: selectedOperator)
+        
         
         if let number = Double(defaultNumber) {
             operandQueue.enqueue(item: number)
             defaultNumber = ""
         }
+        
+        updateDisplayLabels()
+        operatorQueue.enqueue(item: selectedOperator)
+        
+        
+        guard let op = operatorsLabel.text?.count else { return }
+        
+        if  op >= 1 {
+            firstOperatorLabel.text = operatorsLabel.text
+        }
+        firstNumberLabel.text = numberInputLabel.text
+        
+        let operatorLabel = UILabel()
+        operatorLabel.text = operatorString
+        operatorStackView.addArrangedSubview(operatorLabel)
+        operatorStackView.addSubview(operatorLabel)
     }
     
     @IBAction func tapEqualButton(_ sender: UIButton) {
@@ -63,19 +92,23 @@ final class ViewController: UIViewController {
             defaultNumber = ""
         }
         
-        while let operatorItem = operatorQueue.dequeueItem(),
-              let operand1 = operandQueue.dequeueItem(),
-              let operand2 = operandQueue.dequeueItem() {
-            let result = operatorItem.calculate(operand1, operand2)
-            operandQueue.enqueue(item: result)
-        }
-        
-        if let result = operandQueue.lastItem {
+        let formula = Formula(operands: operandQueue, operators: operatorQueue)
+        do {
+            let result = try formula.result()
             numberInputLabel.text = String(result)
+        } catch {
+            print(error)
         }
         
         operandQueue.removeAll()
         operatorQueue.removeAll()
+        firstNumberLabel.text = ""
+        firstOperatorLabel.text = ""
+        
+    }
+    
+    func updateDisplayLabels() {
+        secondNumberLabel.text = firstNumberLabel.text
+        secondOperatorLabel.text = firstOperatorLabel.text
     }
 }
-
