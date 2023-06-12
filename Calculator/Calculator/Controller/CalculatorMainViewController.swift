03ced6ab392c2057e0d072b430a34a2fb24e2735
redmango1446 <redmango1446@redmango1446ui-MacBookAir.local>
2023-06-12 12:04:14 +0900

    feat: ViewController 일부 단순한 버튼 기능 추가

--- Calculator/Calculator/Controller/CalculatorMainViewController.swift
+++ Calculator/Calculator/Controller/CalculatorMainViewController.swift
@@ -8,9 +8,62 @@ import UIKit
 
 class CalculatorMainViewController: UIViewController {
 
+    @IBOutlet var numberView: UILabel!
+    @IBOutlet var operatorView: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+    }
+    
+    @IBAction func touchUpPointButton(_ sender: UIButton) {
+        let currentNumberText = numberView.text ?? "0"
+        let newPointText = sender.titleLabel?.text ?? "."
+        let isPoint = currentNumberText.contains(".")
+        
+        if isPoint == false {
+            numberView.text = currentNumberText + newPointText
+        }
+    }
+    
+    @IBAction func touchUpNumberButton(_ sender: UIButton) {
+        let currentNumberText = numberView.text ?? "0"
+        let newNumberText = sender.titleLabel?.text ?? "0"
+        
+        guard currentNumberText == "0" else {
+            numberView.text = currentNumberText + newNumberText
+            return
+        }
+
+        guard newNumberText == "00" else {
+            numberView.text = newNumberText
+            return
+        }
+    }
+    
+    @IBAction func touchUpOperatorButton(_ sender: UIButton) {
+        
+        guard let number = numberView.text, Double(number) == 0 else {
+            return //스크롤뷰에 옮기기 numberView 초기화 작업
+        }
+        
+        operatorView.text = sender.titleLabel?.text
+    }
+    
+    @IBAction func touchUpClearEntryButton(_ sender: UIButton) {
+        numberView.text = "0"
+    }
+    
+    @IBAction func touchUpChangeSignButton(_ sender: UIButton) {
+        var currentNumberText = numberView.text ?? "0"
+        let isCurrentSign = currentNumberText.contains("-")
+        
+        if isCurrentSign == true {
+            currentNumberText.removeFirst()
+            numberView.text = currentNumberText
+        } else {
+            numberView.text = "-" + currentNumberText
+        }
     }
 }
 
