7041443601101ab70ae332a619fe37fd32730d8b
Whales <Whales.w.Jin@gmail.com>
2023-06-12 21:24:28 +0900

    feat: Queueable 프로토콜 생성

---
+++ Calculator/Calculator/Model/Queueable.swift
@@ -0,0 +1,20 @@
+//
+//  Queueable.swift
+//  Calculator
+//
+//  Created by Whales on 2023/06/12.
+//
+
+protocol Queueable {
+    associatedtype CalculateItem
+    
+    var queue: LinkedList<CalculateItem> { get }
+    var count: Int { get }
+    var isEmpty: Bool { get }
+    
+    mutating func enqueue(_ element: CalculateItem)
+    
+    mutating func dequeue() -> CalculateItem?
+    
+    mutating func clearQueue()
+}
