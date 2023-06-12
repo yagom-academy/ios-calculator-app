8630ee50f51ca07c0c857d607d9543cd930bcf78
YaRkyungmin <kkgm94@naver.com>
2023-06-12 23:48:55 +0900

    🩹chore: Foundation 라이브러리 삭제

--- Calculator/Calculator/Model/ExtenssionPrimitiveDataType/String.swift
+++ Calculator/Calculator/Model/ExtenssionPrimitiveDataType/String.swift
@@ -5,8 +5,6 @@
 //  Created by kangkyungmin on 2023/06/07.
 //
 
-import Foundation
-
 extension String {
     func split(with target: Character) -> [String] {
         return self.components(separatedBy: "\(target)")
