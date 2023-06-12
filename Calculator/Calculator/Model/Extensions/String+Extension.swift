8d9704ac5d3bb29b00d41b0b2cbbe371075767ac
Kobe <mskang@xingxingmobility.com>
2023-06-12 13:01:34 +0900

    :fire: Remove: import Foundation 삭제.
    불필요한 import Foundation 삭제.

--- Calculator/Calculator/Model/Extensions/String+Extension.swift
+++ Calculator/Calculator/Model/Extensions/String+Extension.swift
@@ -4,9 +4,6 @@
 //
 //  Created by Minseong Kang on 2023/06/06.
 //
-
-import Foundation
-
 extension String {
 	func split(with target: Character) -> [String] {
 		let splitByTarget = self.split(separator: target).map { String($0) }
