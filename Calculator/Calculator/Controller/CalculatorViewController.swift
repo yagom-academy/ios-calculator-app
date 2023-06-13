46c637b797e0c97b1587ede01be0c8208843f58c
ysj <yyssjj9999@gmail.com>
2023-06-13 14:42:52 +0900

    feat: IBAction 연결

--- Calculator/Calculator/Controller/CalculatorViewController.swift
+++ Calculator/Calculator/Controller/CalculatorViewController.swift
@@ -15,23 +15,30 @@ class CalculatorViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
     
     @IBAction func tapNubersButton(_ sender: UIButton) {
     }
     
-    
     @IBAction func tapSerialZeroButton(_ sender: UIButton) {
     }
-    
     
     @IBAction func tapPeriodButton(_ sender: UIButton) {
     }
     
+    @IBAction func tapOperatorButton(_ sender: UIButton) {
+    }
     
+    @IBAction func tapChangeSignButton(_ sender: UIButton) {
+    }
     
-
-
+    @IBAction func tapEqualMarkButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func tapAllClearButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func tapClearEntryButton(_ sender: UIButton) {
+    }
 }
 
