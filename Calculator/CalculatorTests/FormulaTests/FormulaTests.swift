328e56f4de4fe9349e850cf1299c906c12be9189
MaryJo-github <jotjdal0215@gmail.com>
2023-06-13 22:18:40 +0900

    :sparkles: Test: 테스트케이스 추가
    CalculatorItemQueue, Formula, ExpressionParser

---
+++ Calculator/CalculatorTests/FormulaTests/FormulaTests.swift
@@ -0,0 +1,107 @@
+//
+//  FormulaTests.swift
+//  FormulaTests
+//
+//  Created by karen, Mary.
+//
+
+import XCTest
+@testable import Calculator
+
+final class FormulaTests: XCTestCase {
+    var sut: Formula!
+    
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        sut = Formula(operands: CalculatorItemQueue<Double>(),
+                      operators: CalculatorItemQueue<Operator>())
+    }
+
+    func test_2더하기3곱하기3빼기1을연산큐와숫자큐에넣고_result를호출하면_14를반환한다() {
+        // given
+        sut.operands.enqueue(2.0)
+        sut.operators.enqueue(.add)
+        sut.operands.enqueue(3.0)
+        sut.operators.enqueue(.multiply)
+        sut.operands.enqueue(3.0)
+        sut.operators.enqueue(.subtract)
+        sut.operands.enqueue(1.0)
+        
+        // when
+        let result = try! sut.result()
+
+        // then
+        XCTAssertEqual(result, 14)
+    }
+    
+    func test_3빼기3곱하기2빼기3나누기6을연산큐와숫자큐에넣고_result를호출하면_음수영쩜오를반환한다() {
+        // given
+        sut.operands.enqueue(3.0)
+        sut.operators.enqueue(.subtract)
+        sut.operands.enqueue(3.0)
+        sut.operators.enqueue(.multiply)
+        sut.operands.enqueue(2.0)
+        sut.operators.enqueue(.subtract)
+        sut.operands.enqueue(3.0)
+        sut.operators.enqueue(.divide)
+        sut.operands.enqueue(6.0)
+        
+        // when
+        let result = try! sut.result()
+
+        // then
+        XCTAssertEqual(result, -0.5)
+    }
+    
+    func test_3빼기음수3곱하기음수2나누기음수6을연산큐와숫자큐에넣고_result를호출하면_2를반환한다() {
+        // given
+        sut.operands.enqueue(3.0)
+        sut.operators.enqueue(.subtract)
+        sut.operands.enqueue(-3.0)
+        sut.operators.enqueue(.multiply)
+        sut.operands.enqueue(-2.0)
+        sut.operators.enqueue(.divide)
+        sut.operands.enqueue(-6.0)
+        
+        // when
+        let result = try! sut.result()
+
+        // then
+        XCTAssertEqual(result, 2)
+    }
+    
+    func test_3을숫자큐에넣고_result를호출하면_invalidFormula오류를반환한다() {
+        // given
+        sut.operands.enqueue(3.0)
+
+        // when
+        let result = try! sut.result()
+
+        // then
+        XCTAssertEqual(result, 3.0)
+    }
+
+    func test_나누기와2를연산큐와숫자큐에넣고_result를호출하면_2를반환한다() {
+        // given
+        sut.operands.enqueue(2.0)
+        sut.operators.enqueue(.divide)
+        
+        // when
+        let result = try! sut.result()
+
+        // then
+        XCTAssertEqual(result, 2.0)
+    }
+    
+    func test_2나누기0을연산큐와숫자큐에넣고_result를호출하면_notDivisibleByZero오류를반환한다() {
+        // given
+        sut.operands.enqueue(2.0)
+        sut.operators.enqueue(.divide)
+        sut.operands.enqueue(0.0)
+        
+        // when, then
+        XCTAssertThrowsError(try sut.result()) { error in
+            XCTAssertEqual(error as? FormulaErrors, FormulaErrors.notDivisibleByZero)
+        }
+    }
+}
