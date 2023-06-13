2979b6e001c2691c9ad866cfeeab6c541e5462e9
MaryJo-github <jotjdal0215@gmail.com>
2023-06-13 21:55:03 +0900

    :recycle: Refactor: Formula result 메서드 리팩토링

--- Calculator/Calculator/Model/Formula.swift
+++ Calculator/Calculator/Model/Formula.swift
@@ -9,18 +9,15 @@ struct Formula {
     var operands: CalculatorItemQueue<Double>
     var operators: CalculatorItemQueue<Operator>
     
-    func result() throws -> Double {
-        var operandsQueue = operands
-        var operatorsQueue = operators
-        
-        while !operatorsQueue.isEmpty {
-            guard let operatorValue = operatorsQueue.dequeue() else { break }
-            guard let lhs = operandsQueue.dequeue(), let rhs = operandsQueue.dequeue() else { break }
-            
-            let result = try operatorValue.calculate(lhs: lhs, rhs: rhs)
-            operandsQueue.enqueue(result)
+    mutating func result() throws -> Double {
+        guard var accumulatedValue = operands.dequeue() else { return 0 }
+
+        while let `operator` = operators.dequeue(),
+              let rhs = operands.dequeue() {
+            accumulatedValue = try `operator`.calculate(lhs: accumulatedValue, rhs: rhs)
         }
         
-        return operandsQueue.dequeue() ?? 0
+        return accumulatedValue
     }
 }
+
