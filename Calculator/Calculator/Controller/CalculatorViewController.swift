6c3cbc6a1783db68bef627921ac7997dca0ae65d
Serena <sha0720@naver.com>
2023-06-13 14:36:35 +0900

    feat: IBOulet, IBAction 연결

---
+++ Calculator/Calculator/Controller/CalculatorViewController.swift
@@ -0,0 +1,37 @@
+//
+//  Calculator - ViewController.swift
+//  Created by yagom. 
+//  Copyright © yagom. All rights reserved.
+// 
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    @IBOutlet weak var inputNumberLabel: UILabel!
+    @IBOutlet weak var inputOperatorLabel: UILabel!
+    @IBOutlet weak var formulaListStackView: UIStackView!
+    @IBOutlet weak var formulaListScrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func tapNubersButton(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func tapSerialZeroButton(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func tapPeriodButton(_ sender: UIButton) {
+    }
+    
+    
+    
+
+
+}
+
