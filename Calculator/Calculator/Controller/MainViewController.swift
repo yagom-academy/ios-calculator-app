60423a3c9fab04eec0cb13fa262907ddd20bd131
MaryJo-github <jotjdal0215@gmail.com>
2023-06-14 16:46:17 +0900

    🆕feat: StackView 삽입 및 초기화 메서드 구현, ScrollView scrollToBottom 메서드 구현

--- Calculator/Calculator/Controller/MainViewController.swift
+++ Calculator/Calculator/Controller/MainViewController.swift
@@ -37,5 +37,53 @@ class MainViewController: UIViewController {
         operatorLabel.text = ""
     }
     
+    private func updateOperands(to value: String) {
+        operandsValue = value
+        operandsLabel.text = value
+    }
+    
+    private func updateOperator(to `operator`: String) {
+        operatorValue = `operator`
+        operatorLabel.text = `operator`
+    }
+
+    private func insertStackView(with strings: String...) {
+        let labels = strings.map { createUILabel(text: $0) }
+        let subStackView = createSubStackView(with: labels)
+        
+        parentStackView.addArrangedSubview(subStackView)
+        expressionScrollView.layoutIfNeeded()
+        expressionScrollView.scrollToBottom()
+    }
+    
+    private func initializeStackView() {
+        guard parentStackView.subviews.count > 0 else { return }
+        
+        parentStackView.subviews.forEach { subview in
+            subview.removeFromSuperview()
+        }
+    }
 }
 
+extension MainViewController {
+    private func createUILabel(text: String) -> UILabel {
+        let label = UILabel()
+        
+        label.font = UIFont.preferredFont(forTextStyle: .title3)
+        label.textColor = .white
+        label.text = text
+        
+        return label
+    }
+    
+    private func createSubStackView(with labels: [UILabel]) -> UIStackView {
+        let stackView = UIStackView()
+        
+        stackView.spacing = 8
+        labels.forEach {
+            stackView.addArrangedSubview($0)
+        }
+        
+        return stackView
+    }
+}
