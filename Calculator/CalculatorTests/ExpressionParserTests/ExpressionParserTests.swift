2a77412faf452558cb73c762574429546dc41f99
DasanKim <tempzipp@gmail.com>
2023-06-15 19:45:53 +0900

    ✅test: 병합한 코드에 대한 CalculatorParserTests 추가

--- Calculator/CalculatorTests/ExpressionParserTests/ExpressionParserTests.swift
+++ Calculator/CalculatorTests/ExpressionParserTests/ExpressionParserTests.swift
@@ -17,74 +17,14 @@ final class ExpressionParserTests: XCTestCase {
     override func tearDownWithError() throws {
         try super.tearDownWithError()
     }
-
-    func test_parser_문자열이_1_더하기_2_더하기_3일때_반환값의operands의_count는_3이다() {
-        // given
-        let formula = ExpressionParser.parser(from: "1+2+3")
-        let expectation = 3
-        
-        // when
-        let result = formula.operands.count
-        
-        // then
-        XCTAssertEqual(result, expectation)
-    }
     
-    func test_parser_문자열이_1_더하기_2_더하기_3일때_반환값의operators의_count는_2이다() {
+    func test_parser_문자열이_2_더하기_3_빼기_4일때_반환값의result값은_1이다() {
         // given
-        let formula = ExpressionParser.parser(from: "1+2+3")
-        let expectation = 2
+        var formula = ExpressionParser<CalculatorItemQueue, CalculatorItemQueue>.parser(from: "2+3−4")
+        let expectation = 1.0
         
         // when
-        let result = formula.operators.count
-        
-        // then
-        XCTAssertEqual(result, expectation)
-    }
-    
-    func test_parser_문자열이_1_더하기_2_빼기_3일때_반환값의operators의_first값은_더하기이다() {
-        // given
-        let formula = ExpressionParser.parser(from: "1+2−3")
-        let expectation = Operator.add
-        
-        // when
-        let result = formula.operators.first
-        
-        // then
-        XCTAssertEqual(result, expectation)
-    }
-    
-    func test_parser_문자열이_1_더하기_2_빼기_3일때_반환값의operators의_last값은_빼기이다() {
-        // given
-        let formula = ExpressionParser.parser(from: "1+2−3")
-        let expectation = Operator.subtract
-        
-        // when
-        let result = formula.operators.last
-        
-        // then
-        XCTAssertEqual(result, expectation)
-    }
-    
-    func test_parser_문자열이_2_곱하기_4_나누기_8일때_반환값의operators의_first값은_곱하기이다() {
-        // given
-        let formula = ExpressionParser.parser(from: "2×4÷8")
-        let expectation = Operator.multiply
-        
-        // when
-        let result = formula.operators.first
-        
-        // then
-        XCTAssertEqual(result, expectation)
-    }
-    
-    func test_parser_문자열이_2_곱하기_4_나누기_8일때_반환값의operators의_last값은_나누기이다() {
-        // given
-        let formula = ExpressionParser.parser(from: "2×4÷8")
-        let expectation = Operator.divide
-        
-        // when
-        let result = formula.operators.last
+        let result = formula.result()
         
         // then
         XCTAssertEqual(result, expectation)
@@ -92,11 +32,11 @@ final class ExpressionParserTests: XCTestCase {
     
     func test_parser_문자열이_2_곱하기_4_나누기_8일때_반환값의result값은_1이다() {
         // given
-        var formula = ExpressionParser.parser(from: "2×4÷8")
+        var formula = ExpressionParser<CalculatorItemQueue, CalculatorItemQueue>.parser(from: "2×4÷8")
         let expectation = 1.0
         
         // when
-        let result = try! formula.result()
+        let result = formula.result()
         
         // then
         XCTAssertEqual(result, expectation)
@@ -104,11 +44,11 @@ final class ExpressionParserTests: XCTestCase {
     
     func test_parser_문자열이_더하기_3일때_반환값의result값은_3이다() {
         // given
-        var formula = ExpressionParser.parser(from: "+3")
+        var formula = ExpressionParser<CalculatorItemQueue, CalculatorItemQueue>.parser(from: "+3")
         let expectation = 3.0
         
         // when
-        let result = try! formula.result()
+        let result = formula.result()
         
         // then
         XCTAssertEqual(result, expectation)
@@ -116,14 +56,13 @@ final class ExpressionParserTests: XCTestCase {
     
     func test_parser_문자열이_3_더하기일때_반환값의result값은_3이다() {
         // given
-        var formula = ExpressionParser.parser(from: "3+")
+        var formula = ExpressionParser<CalculatorItemQueue, CalculatorItemQueue>.parser(from: "3+")
         let expectation = 3.0
         
         // when
-        let result = try! formula.result()
+        let result = formula.result()
         
         // then
         XCTAssertEqual(result, expectation)
     }
-    
 }
