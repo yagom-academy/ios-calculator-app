23ac5a0a3cee382072a2c43140e62ca36defa743
jyubong <nane030@naver.com>
2023-10-03 21:31:47 +0900

    feat: class, protocol, UnitTest 생성
    CalculatorItemQueue, CalculatorItemQueueLinkedList 생성
    CalculatorItem protocol 생성
    CalculatorItemQueueTests 생성

---
+++ Calculator/Calculator/CalculatorItemQueue.swift
@@ -0,0 +1,10 @@
+//
+//  CalculatorItemQueue.swift
+//  Calculator
+//
+//  Created by jyubong on 2023/10/03.
+//
+
+class CalculatorItemQueue {
+    
+}
---
+++ Calculator/Calculator/CalculatorItemQueueLinkedList.swift
@@ -0,0 +1,10 @@
+//
+//  CalculatorItemQueueLinkedList.swift
+//  Calculator
+//
+//  Created by jyubong on 2023/10/03.
+//
+
+class CalculatorItemQueueLinkedList {
+    
+}
