eb0259dc17c2f6e1a46772da36d40ae88e41aa65
woohyeon3382 <woohyeon3382@naver.com>
2023-10-05 01:40:55 +0900

    feat: 불필요한 주석 제거 및 Unit Test를 위한 타깃 추가

---
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -0,0 +1,31 @@
+//
+//  CalculatorTests.swift
+//  CalculatorTests
+//
+//  Created by Toy on 10/5/23.
+//
+
+import XCTest
+
+final class CalculatorTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        
+    }
+
+    override func tearDownWithError() throws {
+        
+    }
+
+    func testExample() throws {
+        
+    }
+
+    func testPerformanceExample() throws {
+        
+        measure {
+           
+        }
+    }
+
+}
