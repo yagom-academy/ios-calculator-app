f543f9b0d9886b1e1706aefd7a1aae51ef59e51a
mireu79 <https://github.com/mireu79>
2023-10-04 09:18:21 +0900

    feat: testable키워드 생성후 테스트케이스 실행전후 호출되는 메서드 구현, sut 프로퍼티생성

--- Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
+++ Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
@@ -6,15 +6,19 @@
 //
 
 import XCTest
+@testable import Calculator
 
-final class CalculatorItemQueueTests: XCTestCase {
-
+class CalculatorItemQueueTests: XCTestCase {
+    var sut: CalculatorItemQueue<String>!
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        try super.setUpWithError()
+        sut = CalculatorItemQueue()
     }
-
+    
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        try super.tearDownWithError()
+        sut = nil
     }
-
+    
 }
