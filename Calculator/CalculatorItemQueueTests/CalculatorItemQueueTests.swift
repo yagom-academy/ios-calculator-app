841fda3f8923a69895319965cc39d5022ea59788
mireu79 <https://github.com/mireu79>
2023-10-04 09:20:35 +0900

    feat: 테스트 케이스 구현

--- Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
+++ Calculator/CalculatorItemQueueTests/CalculatorItemQueueTests.swift
@@ -21,4 +21,59 @@ class CalculatorItemQueueTests: XCTestCase {
         sut = nil
     }
     
+    func test_queue가비어있을때_isEmpty호출시_true를반환하는지() {
+        sut.queue = []
+        
+        let result = sut.isEmpty
+        
+        XCTAssertTrue(result)
+    }
+    
+    func test_1이queue에들어있을때_isEmpty호출시_false를반환하는지() {
+        sut.queue = ["1"]
+        
+        let result = sut.isEmpty
+        
+        XCTAssertFalse(result)
+    }
+    
+    func test_숫자가1과2와3이queue에들어있을떄_count호출시_개수3개가반환되는지() {
+        sut.queue = ["1", "2", "3"]
+        
+        let result = sut.count
+        
+        XCTAssertEqual(result, 3)
+    }
+    
+    func test_빈배열인queue에_enqueue호출시숫자가3이들어오면_개수가1개가되는지() {
+        sut.queue = []
+        
+        sut.enqueue("3")
+        
+        XCTAssertEqual(sut.count, 1)
+    }
+    
+    func test_queue비었을때_dequeue호출시_nil을반환하는지() {
+        sut.queue = []
+        
+        let result = sut.dequeue()
+        
+        XCTAssertNil(result)
+    }
+    
+    func test_4와5와6이queue들었을때_dequeue호출시_4을제거하면_개수가2개가되는지() {
+        sut.queue = ["4", "5", "6"]
+        
+        XCTAssertEqual(sut.dequeue(), "4")
+        
+        XCTAssertEqual(sut.count, 2)
+    }
+    
+    func test_7과8과9가queue에들어있을떄_clear호출시배열이제거되면_ture를반환하는지() {
+        sut.queue = ["7", "8", "9"]
+        
+        sut.clear()
+        
+        XCTAssertTrue(sut.isEmpty)
+    }
 }
