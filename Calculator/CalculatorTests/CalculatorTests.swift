8beb9be94775badce2e0563dc31de8ae7be6da67
woohyeon3382 <woohyeon3382@naver.com>
2023-10-05 17:00:08 +0900

    feat: CalculatorItemQueue에 대한 기본  UnitTest 구현

--- Calculator/CalculatorTests/CalculatorTests.swift
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -9,24 +9,30 @@ import XCTest
 @testable import Calculator
 
 final class CalculatorTests: XCTestCase {
-
+    var sut: CalculatorItemQueue<Int>!
+    
     override func setUpWithError() throws {
-        
+        sut = CalculatorItemQueue()
     }
 
     override func tearDownWithError() throws {
-        
+        sut = nil
     }
 
-    func testExample() throws {
+    func test_Enqueue호출시_요소가_제대로_추가되는지() throws {
+        let _ = sut.enqueue(3)
         
+        let result = sut.peek()
+        
+        XCTAssertEqual(result, 3)
     }
 
-    func testPerformanceExample() throws {
+    func test_Denqueue호출시_가장앞의_요소가_삭제되는지() {
+        let _ = sut.enqueue(1)
+        let _ = sut.enqueue(2)
         
-        measure {
-           
-        }
+        let result = sut.dequeue()
+        
+        XCTAssertEqual(result, 1)
     }
-
 }
