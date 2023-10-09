b61aa23e94779fd8f7ea206b558e21591b94c1ce
ue-mu <ym14170@gmail.com>
2023-10-10 04:12:59 +0900

    comment: Unit Tests에 가독성을 위한 주석 추가

--- Calculator/CalculatorTests/CalculatorTests.swift
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -22,42 +22,54 @@ final class CalculatorTests: XCTestCase {
     }
     
     func test_enqueue로_1과2를_넣고_dequeue를_2번호출한_상태에서_isEmpty가_true를_반환하는지() {
+        // given
         sut.enqueue(value: 1)
         sut.enqueue(value: 2)
         sut.dequeue()
         sut.dequeue()
         
+        // when
         let result = sut.isEmpty
         
+        // then
         XCTAssertTrue(result)
     }
     
     func test_enqueue로_3과4를_넣고_count를_호출하여_2개의인자가있는지() {
+        // given
         sut.enqueue(value: 3)
         sut.enqueue(value: 4)
         
+        // when
         let result = sut.count
         
+        // then
         XCTAssertEqual(result, 2)
         
     }
     
     func test_enqueue로_5와6을_넣고_peek을_호출하여_첫번째_인자가_5가맞는지() {
+        // given
         sut.enqueue(value: 5)
         sut.enqueue(value: 6)
         
+        // when
         let result = sut.peek
         
+        // then
         XCTAssertEqual(result, 5)
     }
     
     func test_enqueue로_1과2를_넣고_clear를_호출하여_isEmpty로_초기화가됐는지_확인() {
+        // given
         sut.enqueue(value: 1)
         sut.enqueue(value: 2)
         sut.clear()
         
+        // when
         let result = sut.isEmpty
         
+        // then
         XCTAssertTrue(result)
     }
 }
