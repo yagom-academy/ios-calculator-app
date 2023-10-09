d6cf3826a1994f9e0af5333bf4c22ea5c071c64d
ue-mu <ym14170@gmail.com>
2023-10-10 03:27:58 +0900

    feat: CalculatorItemQueue의 UnitTests 구현
    1. enqueue로1과2를 넣고 dequeue를 2번 호출한 상태에서 isEmpty가 true를 반환하는 지 테스트
    2. enqueue로 3과 4를 넣고 count를 호출하여 2개의인자가있는지 테스트
    3. enqueue로 5와 6을 넣고 peek을 호출하여 첫번째 인자가 5가 맞는지 테스트
    4. enqueue로 1과 2를 넣고 clear를 호출하여 isEmpty로 초기화가됐는지 확인하는 테스트

--- Calculator/CalculatorTests/CalculatorTests.swift
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -20,4 +20,44 @@ final class CalculatorTests: XCTestCase {
         try super.tearDownWithError()
         sut = nil
     }
+    
+    func test_enqueue로_1과2를_넣고_dequeue를_2번호출한_상태에서_isEmpty가_true를_반환하는지() {
+        sut.enqueue(value: 1)
+        sut.enqueue(value: 2)
+        sut.dequeue()
+        sut.dequeue()
+        
+        let result = sut.isEmpty
+        
+        XCTAssertTrue(result)
+    }
+    
+    func test_enqueue로_3과4를_넣고_count를_호출하여_2개의인자가있는지() {
+        sut.enqueue(value: 3)
+        sut.enqueue(value: 4)
+        
+        let result = sut.count
+        
+        XCTAssertEqual(result, 2)
+        
+    }
+    
+    func test_enqueue로_5와6을_넣고_peek을_호출하여_첫번째_인자가_5가맞는지() {
+        sut.enqueue(value: 5)
+        sut.enqueue(value: 6)
+        
+        let result = sut.peek
+        
+        XCTAssertEqual(result, 5)
+    }
+    
+    func test_enqueue로_1과2를_넣고_clear를_호출하여_isEmpty로_초기화가됐는지_확인() {
+        sut.enqueue(value: 1)
+        sut.enqueue(value: 2)
+        sut.clear()
+        
+        let result = sut.isEmpty
+        
+        XCTAssertTrue(result)
+    }
 }
