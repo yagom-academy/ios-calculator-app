3c6964de995767c644ca65c5278353986ba63d3f
ue-mu <ym14170@gmail.com>
2023-10-09 23:17:41 +0900

    feat: CalculatorItemQueue에 큐에 필요한 메서드 구현

--- Calculator/Calculator/Model/CalculatorItemQueue.swift
+++ Calculator/Calculator/Model/CalculatorItemQueue.swift
@@ -32,4 +32,41 @@ struct CalculatorItemQueue<T: CalculateItem> {
     var peek: T? {
         return head?.value
     }
+    
+    mutating func enqueue(value: T) {
+        let node = Node(value: value)
+        
+        guard isEmpty == false else {
+            head = node
+            tail = node
+            return
+        }
+        
+        tail?.next = node
+        tail = tail?.next
+    }
+    
+    mutating func dequeue() -> T? {
+        var newValue: T? = nil
+        
+        guard isEmpty == false else {
+            return nil
+        }
+        
+        if head === tail {
+            newValue = head?.value
+            head = nil
+            tail = nil
+            return newValue
+        }
+        
+        newValue = head?.value
+        head = head?.next
+        return newValue
+    }
+    
+    mutating func clear() {
+        head = nil
+        tail = nil
+    }
 }
