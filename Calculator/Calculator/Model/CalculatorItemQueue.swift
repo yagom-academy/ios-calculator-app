b383ccad97b0a2df4b03917fa4ad42b9c18ebbfa
ue-mu <ym14170@gmail.com>
2023-10-09 23:15:08 +0900

    feat:CalculatorItemQueue구조체에 head, tail 생성 및 큐 구현에 필요한 계산속성 생성

--- Calculator/Calculator/Model/CalculatorItemQueue.swift
+++ Calculator/Calculator/Model/CalculatorItemQueue.swift
@@ -6,5 +6,30 @@
 //
 
 struct CalculatorItemQueue<T: CalculateItem> {
+    var head: Node<T>?
+    var tail: Node<T>?
     
+    var isEmpty: Bool {
+        if head == nil {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    var count: Int {
+        var current = head
+        var elementCount = 0
+        
+        while current != nil {
+            elementCount += 1
+            current = current?.next
+        }
+        
+        return elementCount
+    }
+    
+    var peek: T? {
+        return head?.value
+    }
 }
