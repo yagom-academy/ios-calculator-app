d828d395cd520d2996c6c8c8037cf132492cbc3b
kiseok <carti1108@naver.com>
2023-10-11 02:41:44 +0900

    test: parse메서드 호출시 queue에 담기는지 테스트

--- Calculator/ExpressionParserTests/ExpressionParserTests.swift
+++ Calculator/ExpressionParserTests/ExpressionParserTests.swift
@@ -15,6 +15,15 @@ final class ExpressionParserTests: XCTestCase {
         sut = nil
     }
 
-    
+    func test_parse호출시_전달된문자열순서대로formula의queue에담기는지() throws {
+        //given
+        let formula = try ExpressionParser.parse(from: "1 + 2 - 3 x 4 / 5")
+        let expectedFormulaOperators: [Operator] = [.add, .subtract, .multiply, .divide]
+        let expectedFormulaOperands: [Double] = [1, 2, 3, 4, 5]
+        
+        //then
+        XCTAssertEqual(expectedFormulaOperands, formula.operands.enqueueBox)
+        XCTAssertEqual(expectedFormulaOperators, formula.operators.enqueueBox)
+    }
 
 }
