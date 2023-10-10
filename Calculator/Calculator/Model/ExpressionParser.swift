097dcf21908417da7761616f56a150df86c07518
jyubong <nane030@naver.com>
2023-10-10 17:18:59 +0900

    refactor: String extension 메서드 수정

--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -11,15 +11,6 @@ enum ExpressionParser {
 
 extension String {
     func split(with target: Character) -> [String] {
-        var splitedString = self.split(separator: target).map{ String($0) }
-        let limitCount = splitedString.count - 1
-        
-        splitedString.enumerated().forEach{(index, _) in
-            guard index < limitCount else { return }
-            
-            splitedString.insert(String(target), at: index+1)
-        }
-        
-        return splitedString
+        return self.split(separator: target).map{ String($0) }
     }
 }
