43ceb30c9f3ace65105f5e57cc3bbce1e45568b4
jyubong <nane030@naver.com>
2023-10-10 09:20:50 +0900

    feat: Operator enum 메서드 구현

--- Calculator/Calculator/Operator.swift
+++ Calculator/Calculator/Operator.swift
@@ -9,5 +9,34 @@ enum Operator: Character, CaseIterable, CalculatorItem {
     case add = "+"
     case subtract = "-"
     case divide = "/"
-    case mutiply = "*"
+    case multiply = "*"
+    
+    func calculate(lhs: Double, rhs: Double) -> Double {
+        switch self {
+        case .add:
+            return add(lhs: lhs, rhs: rhs)
+        case .subtract:
+            return substract(lhs: lhs, rhs: rhs)
+        case .divide:
+            return divide(lhs: lhs, rhs: rhs)
+        case .multiply:
+            return multiply(lhs: lhs, rhs: rhs)
+        }
+    }
+    
+    private func add(lhs: Double, rhs: Double) -> Double {
+        return lhs + rhs
+    }
+    
+    private func substract(lhs: Double, rhs: Double) -> Double {
+        return lhs - rhs
+    }
+    
+    private func divide(lhs: Double, rhs: Double) -> Double {
+        return lhs / rhs
+    }
+    
+    private func multiply(lhs: Double, rhs: Double) -> Double {
+        return lhs * rhs
+    }
 }
