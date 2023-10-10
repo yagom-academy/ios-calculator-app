b4270b82318198ca14311808867389f61e8e553d
kiseok <carti1108@naver.com>
2023-10-11 01:53:14 +0900

    feat: Formula struct구현
    operands, operators 프로퍼티 추가
    result메서드 구현

---
+++ Calculator/Calculator/Model/Formula.swift
@@ -0,0 +1,33 @@
+//
+//  Formula.swift
+//  Calculator
+//
+//  Created by Kiseok on 2023/10/11.
+//
+
+struct Formula {
+    var operands: CalculatorItemQueue<Double>
+    var operators: CalculatorItemQueue<Operator>
+    
+    mutating func result() throws -> Double {
+        guard var lhs = operands.dequeue() else {
+            throw CalculatorError.invalidOperandError
+        }
+        var result: Double = 0
+        
+        while !operators.isEmpty {
+            guard let operators = operators.dequeue() else {
+                throw CalculatorError.invalidOperatorError
+            }
+            
+            guard let rhs = operands.dequeue() else {
+                throw CalculatorError.invalidOperandError
+            }
+            
+            lhs = try operators.calculate(lhs: lhs, rhs: rhs)
+            result = lhs
+        }
+        
+        return result
+    }
+}
