c6ca1d44deaedfd1791643374fa6b0c49fa9d46e
jyubong <nane030@naver.com>
2023-10-10 09:17:49 +0900

    feat: Operator enum 생성, case 구현

---
+++ Calculator/Calculator/Operator.swift
@@ -0,0 +1,13 @@
+//
+//  Operator.swift
+//  Calculator
+//
+//  Created by jyubong on 10/10/23.
+//
+
+enum Operator: Character, CaseIterable, CalculatorItem {
+    case add = "+"
+    case subtract = "-"
+    case divide = "/"
+    case mutiply = "*"
+}
