c9ca5d7e6f72c7519dda3e2605b8d0b7703811e8
kiseok <carti1108@naver.com>
2023-10-11 01:28:28 +0900

    test: add case 테스트 메서드명 수정 및 subtract case 테스트

--- Calculator/OperatorTests/OperatorTests.swift
+++ Calculator/OperatorTests/OperatorTests.swift
@@ -15,7 +15,7 @@ final class OperatorTests: XCTestCase {
         sut = nil
     }
     
-    func test_operator가add케이스이고lhs와rhs가각각1일때_add호출시2가나오는지() throws {
+    func test_operator가add케이스이고lhs와rhs가각각1일때_calculate호출시2가나오는지() throws {
         //given
         sut = Operator.add
         let lhs: Double = 1
@@ -26,6 +26,19 @@ final class OperatorTests: XCTestCase {
         
         //then
         XCTAssertEqual(result, 2)
+    }
+    
+    func test_operator가subtract케이스이고lhs와rhs가각각2와1일때_calculate호출시1이나오는지() throws {
+        //given
+        sut = Operator.subtract
+        let lhs: Double = 2
+        let rhs: Double = 1
+        
+        //when
+        let result = try sut.calculate(lhs: lhs, rhs: rhs)
+        
+        //then
+        XCTAssertEqual(result, 1)
     }
 
     
