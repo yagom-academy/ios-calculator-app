47d14277c5f08b51ba8d96598b60ea93d9362c7b
kiseok <carti1108@naver.com>
2023-10-11 02:15:05 +0900

    test: Formula result메서드 호출 시 test

--- Calculator/FormulaTests/FormulaTests.swift
+++ Calculator/FormulaTests/FormulaTests.swift
@@ -11,13 +11,28 @@ import XCTest
 final class FormulaTests: XCTestCase {
     var sut: Formula!
 
-    override func setUpWithError() throws {
-        sut = Formula(operands: CalculatorItemQueue<Double>(enqueueBox: [], dequeueBox: []),
-                      operators: CalculatorItemQueue<Operator>(enqueueBox: [], dequeueBox: []))
-    }
-
     override func tearDownWithError() throws {
         sut = nil
     }
-
+    
+    func test_Formula의queue에요소가있을때_result호출시_순서대로연산이진행되는지() throws {
+        //given
+        sut = Formula(operands: CalculatorItemQueue<Double>(enqueueBox: [2, 7, 1, 4], dequeueBox: []), operators: CalculatorItemQueue<Operator>(enqueueBox: [.add, .subtract, .multiply], dequeueBox: []))
+        
+        //when
+        let result = try sut.result()
+        
+        //then
+        XCTAssertEqual(result, 32)
+    }
+    
+    func test_Formula의operands의queue가비어있을때_result호출시_invalidOperandError가나오는지() throws {
+        //given
+        sut = Formula(operands: CalculatorItemQueue<Double>(enqueueBox: [], dequeueBox: []), operators: CalculatorItemQueue<Operator>(enqueueBox: [], dequeueBox: []))
+        
+        //then
+        XCTAssertThrowsError(try sut.result()) { error in
+            XCTAssertEqual(error as! CalculatorError, CalculatorError.invalidOperandError)
+        }
+    }
 }
