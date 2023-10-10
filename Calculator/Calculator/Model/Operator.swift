8bbf54a100f6ceebf38a299073919876b66e241d
kiseok <carti1108@naver.com>
2023-10-11 01:00:47 +0900

    feat: Operator enum 생성 및 case 추가
    add, subtract, divide, multiply case생성

---
+++ Calculator/Calculator/Model/Operator.swift
@@ -0,0 +1,13 @@
+//
+//  Operator.swift
+//  Calculator
+//
+//  Created by Kiseok on 2023/10/11.
+//
+
+enum Operator: Character, CaseIterable, CalculateItem {
+    case add = "+"
+    case subtract = "-"
+    case divide = "/"
+    case multiply = "x"
+}
