f008a025099d3cdad8b51689f22ff2a86bb32cae
kiseok <carti1108@naver.com>
2023-10-11 01:02:43 +0900

    feat: case 별 연산 메서드 구현

--- Calculator/Calculator/Model/Operator.swift
+++ Calculator/Calculator/Model/Operator.swift
@@ -10,4 +10,20 @@ enum Operator: Character, CaseIterable, CalculateItem {
     case subtract = "-"
     case divide = "/"
     case multiply = "x"
+    
+    private func add(lhs: Double, rhs: Double) -> Double {
+        return lhs + rhs
+    }
+    
+    private func subtract(lhs: Double, rhs: Double) -> Double {
+        return lhs - rhs
+    }
+    
+    private func divide(lhs: Double, rhs: Double) -> Double {
+        return lhs / rhs
+    }
+    
+    private func multiply(lhs: Double, rhs: Double) -> Double {
+        return lhs * rhs
+    }
 }
