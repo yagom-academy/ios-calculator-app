4e363bfff253b4c1b1800d8956ae237c18e0ca20
kiseok <carti1108@naver.com>
2023-10-11 01:05:01 +0900

    feat: CalculatorError enum 추가

---
+++ Calculator/Calculator/Model/CalculatorError.swift
@@ -0,0 +1,12 @@
+//
+//  CalculatorError.swift
+//  Calculator
+//
+//  Created by Kiseok on 2023/10/11.
+//
+
+enum CalculatorError: Error {
+    case invalidOperatorError
+    case invalidOperandError
+    case divideByZeroError
+}
