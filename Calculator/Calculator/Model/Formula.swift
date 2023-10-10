fa340cfaa01edf40c69ad820e03b218801c99a6f
jyubong <nane030@naver.com>
2023-10-10 14:29:29 +0900

    feat: Formula result 메서드 구현

--- Calculator/Calculator/Model/Formula.swift
+++ Calculator/Calculator/Model/Formula.swift
@@ -9,7 +9,15 @@ struct Formula {
     var operands: CalculatorItemQueue<Double>
     var operators: CalculatorItemQueue<Operator>
     
-    
+    func result() -> Double {
+        guard var result = operands.deQueue() else { return 0.0 } 
+        
+        while let operand = operands.deQueue(), let operation = operators.deQueue() {
+            result = operation.calculate(lhs: result, rhs: operand)
+        }
+        
+        return result
+    }
 }
 
 extension Double: CalculatorItem { }
