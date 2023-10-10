4d5e5fe194dc2e88c87f929bce07c675db4650eb
woohyeon3382 <woohyeon3382@naver.com>
2023-10-11 00:53:53 +0900

    feat: extension 파일 분리

---
+++ Calculator/Calculator/Models/Extension/ExtensionDouble.swift
@@ -0,0 +1,9 @@
+//
+//  ExtensionType.swift
+//  Calculator
+//
+//  Created by Toy on 10/5/23.
+//
+
+
+extension Double: CalculateItem { }
--- Calculator/Calculator/Models/Extension/ExtensionString.swift
+++ Calculator/Calculator/Models/Extension/ExtensionString.swift
@@ -1,13 +1,11 @@
 //
-//  ExtensionType.swift
+//  ExtensionString.swift
 //  Calculator
 //
-//  Created by Toy on 10/5/23.
+//  Created by Toy on 10/11/23.
 //
 
 import Foundation
-
-extension Double: CalculateItem { }
 
 extension String {
     func split(with target: Character) -> [String] {
