638c4034baa57b85d94ff698e4089c7bc5a4ba10
jyubong <nane030@naver.com>
2023-10-12 14:57:13 +0900

    refactor: String 내 변수명 수정

--- Calculator/Calculator/Extension/String+.swift
+++ Calculator/Calculator/Extension/String+.swift
@@ -9,9 +9,9 @@ extension String {
     func split(with target: Character) -> [String] {
         let seperator = String(target)
         var splitedString = components(separatedBy: seperator)
-        let count = splitedString.count - 1
+        let lastIndex = splitedString.count - 1
         
-        for index in 0..<count {
+        for index in 0..<lastIndex {
             let insertIndex = index * 2 + 1
             splitedString.insert(seperator, at: insertIndex)
         }
