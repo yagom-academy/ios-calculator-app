2bd04129fe8bf41009f5602eb7913430bb96987a
woohyeon3382 <woohyeon3382@naver.com>
2023-10-12 18:10:56 +0900

    refactor: ExpressionParserTests의 테스트메서드의 변수명 변경

--- Calculator/ExpressionParserTests/ExpressionParserTests.swift
+++ Calculator/ExpressionParserTests/ExpressionParserTests.swift
@@ -13,25 +13,25 @@ final class ExpressionParserTests: XCTestCase {
     func test_parse메서드호출시_연산자와_피연산자가_분리되어enqueue되는지() {
         let input = "3 + 7 - -10"
         var parse = ExpressionParser.parse(from: input)
-        var numberArray = [Double]()
-        var operatorsArray = [Operator]()
-        let resultNumberArray = [3.0, 7.0, -10.0]
-        let resultOperatorsArray: [Operator] = [.add, .subtract]
+        var numbers = [Double]()
+        var operators = [Operator]()
+        let resultNumbers = [3.0, 7.0, -10.0]
+        let resultOperators: [Operator] = [.add, .subtract]
 
         while let number = parse.operands.dequeue() {
-            numberArray.append(number)
+            numbers.append(number)
         }
         
         while let `operator` = parse.operators.dequeue() {
-            operatorsArray.append(`operator`)
+            operators.append(`operator`)
         }
         
-        for (index, value) in numberArray.enumerated() {
-            XCTAssertEqual(value, resultNumberArray[index])
+        for (index, value) in numbers.enumerated() {
+            XCTAssertEqual(value, resultNumbers[index])
         }
         
-        for (index, value) in operatorsArray.enumerated() {
-            XCTAssertEqual(value, resultOperatorsArray[index])
+        for (index, value) in operators.enumerated() {
+            XCTAssertEqual(value, resultOperators[index])
         }
     }
 }
