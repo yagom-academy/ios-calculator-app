21ea94e5ebbdede0b755c1d3812eaaa9fdef17cc
kiseok <carti1108@naver.com>
2023-10-13 13:19:51 +0900

    feat: componentsByOperators 메서드를 연산자를 기준으로 나누도록 수정, parse메서드 내부에서 연산자와 피연산자를 queue에 넣는 로직을 분리

--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -6,18 +6,22 @@
 //
 
 enum ExpressionParser {
-    static func parse(from input: String) throws -> Formula {
+    static func parse(from input: String) -> Formula {
         let components = componentsByOperators(from: input)
         let operands = CalculatorItemQueue<Double>(enqueueBox: [])
         let operators = CalculatorItemQueue<Operator>(enqueueBox: [])
+        let inputArray = input.map { $0 }
         var formula = Formula(operands: operands, operators: operators)
         
+        inputArray.forEach {
+            if let operators = Operator(rawValue: $0) {
+                formula.operators.enqueue(operators)
+            }
+        }
         
         components.forEach {
             if let operands = Double($0) {
                 formula.operands.enqueue(operands)
-            } else if let operators = Operator(rawValue: Character($0)) {
-                formula.operators.enqueue(operators)
             }
         }
         
@@ -25,6 +29,13 @@ enum ExpressionParser {
     }
     
     static private func componentsByOperators(from input: String) -> [String] {
-        return input.split(with: " ")
+        var result: [String] = [input]
+        
+        Operator.allCases.forEach { operatorSymbol in
+            result = result.flatMap { $0.split(with: operatorSymbol.rawValue)
+            }
+        }
+        
+        return result
     }
 }
