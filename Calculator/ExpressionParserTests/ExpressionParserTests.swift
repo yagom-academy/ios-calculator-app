88a6767ae1c929cb09b1a5d00e24a421938424e0
kiseok <carti1108@naver.com>
2023-10-13 14:29:06 +0900

    test: parse호출 시 음수가 잘 담기는지 테스트

--- Calculator/ExpressionParserTests/ExpressionParserTests.swift
+++ Calculator/ExpressionParserTests/ExpressionParserTests.swift
@@ -26,6 +26,17 @@ final class ExpressionParserTests: XCTestCase {
         XCTAssertEqual(expectedFormulaOperators, formula.operators.enqueueBox)
     }
     
+    func test_parse호출시_전달된문자열에음수가있을시_queue에음수가담기는지() {
+        //given
+        let formula = ExpressionParser.parse(from: "11+22－-33*44/55")
+        let expectedFormulaOperators: [Operator] = [.add, .subtract, .multiply, .divide]
+        let expectedFormulaOperands: [Double] = [11, 22, -33, 44, 55]
+        
+        //then
+        XCTAssertEqual(expectedFormulaOperands, formula.operands.enqueueBox)
+        XCTAssertEqual(expectedFormulaOperators, formula.operators.enqueueBox)
+    }
+    
     func test_parse호출시_공백을포함한문자열을전달했을때_공백없이queue에담기는지() {
         let formula = ExpressionParser.parse(from: "1 1+ 22－33 * 44/ 55")
         let expectedFormulaOperators: [Operator] = [.add, .subtract, .multiply, .divide]
