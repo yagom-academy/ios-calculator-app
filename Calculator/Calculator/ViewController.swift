e5202b56bfd4d794b2ddf33499d04792fc69d865
jyubong <nane030@naver.com>
2023-10-14 23:19:17 +0900

    feat: ViewController IBOutlet, IBAction 구현
    IBOutlet: 현재 입력중인 label인 operandLabel, operatorLabel
    IBAction: 숫자 버튼 클릭 action 구현
    추가적으로 필요한 프로퍼티 및 메서드 구현

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -7,12 +7,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var operandLabel: UILabel!
+    @IBOutlet weak var operatorLabel: UILabel!
+    
+    private let numberFormatter = NumberFormatter()
+    private var digitIsSelecting = false
+    private var dotIsClicked = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configuareUI()
+    }
+    
+    private func configuareUI() {
+        operandLabel.text = "0"
+        
+        numberFormatter.numberStyle = .decimal
     }
 
 
+    @IBAction func digitButtonTapped(_ sender: UIButton) {
+        guard let lhs = operandLabel.text, let rhs = sender.currentTitle else { return }
+        
+        if dotIsClicked {
+            operandLabel.text = lhs + rhs
+        } else if digitIsSelecting {
+            formatInDigitSelecting(lhs: lhs, rhs: rhs)
+        } else {
+            operandLabel.text = rhs
+        }
+        
+        digitIsSelecting = true
+    }
+    
+    private func formatInDigitSelecting(lhs: String, rhs: String) {
+        guard let lhsInt = numberFormatter.number(from: lhs) as? Int, let rhsInt = Int(rhs) else { return }
+        
+        var digit = 0
+        
+        switch rhs {
+        case "0":
+            digit = lhsInt * 10
+        case "00":
+            digit = lhsInt * 100
+        default:
+            digit = lhsInt * 10 + rhsInt
+        }
+        
+        operandLabel.text = numberFormatter.string(for: digit)
+    }
 }
 
