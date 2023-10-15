9ac69060ada29bb95a3a06d6805be10be7e2f79c
kiseok <carti1108@naver.com>
2023-10-15 22:25:51 +0900

    feat: ViewController에 IBOutlet연결, storyboard autolayout 수정

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -8,6 +8,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var formulaScrollView: UIScrollView!
+    @IBOutlet weak var scrollStackView: UIStackView!
+    @IBOutlet weak var operandLabel: UILabel!
+    @IBOutlet weak var operatorLabel: UILabel!
+    @IBOutlet weak var listStackView: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
