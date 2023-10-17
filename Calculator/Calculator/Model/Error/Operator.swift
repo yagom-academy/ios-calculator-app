0f7fbb9411a7b3a8ad7ad6b3a3860e08d9575582
mireu79 <https://github.com/mireu79>
2023-10-17 15:42:39 +0900

    refactor: 오류처리 수정

--- Calculator/Calculator/Model/Error/Operator.swift
+++ Calculator/Calculator/Model/Error/Operator.swift
@@ -11,14 +11,14 @@ enum Operator: Character, CaseIterable, CalculateItem {
     case divide = "/"
     case multiply = "*"
     
-    func calculate(lhs: Double, rhs: Double) throws -> Double {
+    func calculate(lhs: Double, rhs: Double) -> Double {
         switch self {
         case .add:
             return add(lhs: lhs, rhs: rhs)
         case .substract:
             return substract(lhs: lhs, rhs: rhs)
         case .divide:
-            return try divide(lhs: lhs, rhs: rhs)
+            return divide(lhs: lhs, rhs: rhs)
         case .multiply:
             return multiply(lhs: lhs, rhs: rhs)
         }
@@ -32,9 +32,9 @@ enum Operator: Character, CaseIterable, CalculateItem {
         return lhs - rhs
     }
     
-    private func divide(lhs: Double, rhs: Double) throws -> Double {
+    private func divide(lhs: Double, rhs: Double) -> Double {
         if rhs == .zero {
-            throw CalculatorError.divideByZero
+            return .nan
         }
         
         return lhs / rhs
