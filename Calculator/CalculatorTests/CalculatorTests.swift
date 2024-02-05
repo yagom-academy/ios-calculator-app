36ddaefd3ffaaf809aa5c77de48733e979b6a4f6
PrismSpirit <yeolkeut@gmail.com>
2024-02-06 01:34:24 +0900

    Add target for Unit Test

---
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -0,0 +1,19 @@
+//
+//  CalculatorTests.swift
+//  CalculatorTests
+//
+//  Created by Jaehun Lee on 2/6/24.
+//
+
+import XCTest
+@testable import Calculator
+
+final class CalculatorTests: XCTestCase {
+    override func setUpWithError() throws {
+        
+    }
+
+    override func tearDownWithError() throws {
+        
+    }
+}
