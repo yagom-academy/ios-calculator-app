0b4cd76a1e0e1a4c530d834b6020f23c8731eb56
yawoong2 <adamzoong@naver.com>
2024-02-05 20:01:28 +0900

    add: 프로젝트에 Test target 추가

---
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -0,0 +1,26 @@
+//
+//  CalculatorTests.swift
+//  CalculatorTests
+//
+//  Created by JIWOONG on 2024/02/05.
+//
+
+import XCTest
+@testable import Calculator
+
+
+final class CalculatorTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        
+    }
+
+    override func tearDownWithError() throws {
+        
+    }
+
+    func testExample() throws {
+        
+    }
+
+}
