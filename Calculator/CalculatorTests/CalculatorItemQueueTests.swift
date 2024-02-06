13c3f80f04364f51d59d152331a5f39117bf4a0b
PrismSpirit <yeolkeut@gmail.com>
2024-02-06 23:01:55 +0900

    Add Unit Test Case Class for CalculatorItemQueue

---
+++ Calculator/CalculatorTests/CalculatorItemQueueTests.swift
@@ -0,0 +1,19 @@
+//
+//  CalculatorItemQueueTests.swift
+//  CalculatorTests
+//
+//  Created by Jaehun Lee on 2/6/24.
+//
+
+import XCTest
+@testable import Calculator
+
+final class CalculatorItemQueueTests: XCTestCase {
+    override func setUpWithError() throws {
+        
+    }
+
+    override func tearDownWithError() throws {
+        
+    }
+}
