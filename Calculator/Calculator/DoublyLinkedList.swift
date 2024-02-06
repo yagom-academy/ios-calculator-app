0ebb55f43e0c3b61a725a4fa1c7b145a58690421
PrismSpirit <yeolkeut@gmail.com>
2024-02-06 17:48:27 +0900

    Specify requirements that DoublyLinkedList must implement

--- Calculator/Calculator/DoublyLinkedList.swift
+++ Calculator/Calculator/DoublyLinkedList.swift
@@ -6,13 +6,32 @@
 //
 
 protocol DoublyLinkedListProtocol {
-    
+    associatedtype Element
+    var count: Int { get set }
+    var isEmpty: Bool { get }
+    mutating func addFirst(element: Element)
+    mutating func addLast(element: Element)
+    mutating func removeFirst() -> Element?
+    mutating func removeLast() -> Element?
 }
 
 struct DoublyLinkedList<Element>: DoublyLinkedListProtocol {
     private var header: Node<Element>
     private var trailer: Node<Element>
     private var _count: Int = 0
+    
+    var count: Int {
+        get {
+            return 0
+        }
+        set {
+            
+        }
+    }
+    
+    var isEmpty: Bool {
+        return true
+    }
     
     init() {
         self.header = Node(element: nil)
@@ -45,5 +64,21 @@ struct DoublyLinkedList<Element>: DoublyLinkedListProtocol {
         _count -= 1
         
         return node.element
+    }
+    
+    func addFirst(element: Element) {
+        
+    }
+    
+    func addLast(element: Element) {
+        
+    }
+    
+    func removeFirst() -> Element? {
+        return nil
+    }
+    
+    func removeLast() -> Element? {
+        return nil
     }
 }
