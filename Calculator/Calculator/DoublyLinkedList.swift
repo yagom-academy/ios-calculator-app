8c3a33a5630a396ee2cb557388967a9af93a8f2b
PrismSpirit <yeolkeut@gmail.com>
2024-02-06 16:30:08 +0900

    Define initializer for DoublyLinkedList

--- Calculator/Calculator/DoublyLinkedList.swift
+++ Calculator/Calculator/DoublyLinkedList.swift
@@ -10,5 +10,15 @@ protocol DoublyLinkedListProtocol {
 }
 
 struct DoublyLinkedList<Element>: DoublyLinkedListProtocol {
+    private var header: Node<Element>
+    private var trailer: Node<Element>
+    private var _count: Int = 0
     
+    init() {
+        self.header = Node(element: nil)
+        self.trailer = Node(element: nil)
+        
+        header.setNext(node: trailer)
+        trailer.setPrev(node: header)
+    }
 }
