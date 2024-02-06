c6426e9dad380fff9e574c011df6b4b860cb7abd
PrismSpirit <yeolkeut@gmail.com>
2024-02-06 17:35:01 +0900

    Write private methods for DoublyLinkedList

--- Calculator/Calculator/DoublyLinkedList.swift
+++ Calculator/Calculator/DoublyLinkedList.swift
@@ -21,4 +21,29 @@ struct DoublyLinkedList<Element>: DoublyLinkedListProtocol {
         header.setNext(node: trailer)
         trailer.setPrev(node: header)
     }
+    
+    private mutating func addBetween(element: Element, predecessor: Node<Element>, successor: Node<Element>) {
+        let newNode = Node(element: element)
+        
+        newNode.setPrev(node: predecessor)
+        newNode.setNext(node: successor)
+        predecessor.setNext(node: newNode)
+        successor.setPrev(node: newNode)
+        
+        _count += 1
+    }
+    
+    private mutating func remove(node: Node<Element>) -> Element? {
+        guard let predecessor = node.prev,
+              let successor = node.next else {
+            return nil
+        }
+        
+        predecessor.setNext(node: successor)
+        successor.setPrev(node: predecessor)
+        
+        _count -= 1
+        
+        return node.element
+    }
 }
