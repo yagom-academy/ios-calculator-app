b934b7702e25a022db84aa864e4d2968c7b9b824
PrismSpirit <yeolkeut@gmail.com>
2024-02-06 15:49:07 +0900

    Add DoublyLinkedList struct and it conforms DoublyLinkedListProtocol

---
+++ Calculator/Calculator/DoublyLinkedList.swift
@@ -0,0 +1,14 @@
+//
+//  DoublyLinkedList.swift
+//  Calculator
+//
+//  Created by Jaehun Lee on 2/6/24.
+//
+
+protocol DoublyLinkedListProtocol {
+    
+}
+
+struct DoublyLinkedList<Element>: DoublyLinkedListProtocol {
+    
+}
