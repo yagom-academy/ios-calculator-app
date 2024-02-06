6c2e458a3557f0067f8acd2c4eb342d7a6f62254
PrismSpirit <yeolkeut@gmail.com>
2024-02-06 22:58:07 +0900

    Specify requirements that CalculatorItemQueue must implement

--- Calculator/Calculator/CalculatorItemQueue.swift
+++ Calculator/Calculator/CalculatorItemQueue.swift
@@ -9,6 +9,35 @@ protocol CalculateItem {
     
 }
 
-struct CalculatorItemQueue<Element: CalculateItem> {
+protocol QueueProtocol {
+    associatedtype Element
+    var count: Int { get }
+    var isEmpty: Bool { get }
+    var first: Element? { get }
+    mutating func enqueue(element: Element)
+    mutating func dequeue() -> Element?
+}
+
+struct CalculatorItemQueue<Element: CalculateItem>: QueueProtocol {
+    var list = DoublyLinkedList<Element>()
     
+    var count: Int {
+        return 0
+    }
+    
+    var isEmpty: Bool {
+        return true
+    }
+    
+    var first: Element? {
+        return nil
+    }
+    
+    mutating func enqueue(element: Element) {
+        
+    }
+    
+    mutating func dequeue() -> Element? {
+        return nil
+    }
 }
