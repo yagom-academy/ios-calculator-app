8a06cc734948cdd169dfdcd5c41310694caaf429
PrismSpirit <yeolkeut@gmail.com>
2024-02-06 15:45:25 +0900

    Add CalculatorItemQueue struct and its element conforms CalculateItem protocol

---
+++ Calculator/Calculator/CalculatorItemQueue.swift
@@ -0,0 +1,14 @@
+//
+//  CalculatorItemQueue.swift
+//  Calculator
+//
+//  Created by Jaehun Lee on 2/6/24.
+//
+
+protocol CalculateItem {
+    
+}
+
+struct CalculatorItemQueue<Element: CalculateItem> {
+    
+}
