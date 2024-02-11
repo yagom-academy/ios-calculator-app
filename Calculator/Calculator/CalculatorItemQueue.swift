88828fc0475e75f1c30d56dfb30fad3bb55a6da7
forseaest <u3z@naver.com>
2024-02-12 01:37:59 +0900

    Feat: CalculatorItemQueue 구조체 구현

---
+++ Calculator/Calculator/CalculatorItemQueue.swift
@@ -0,0 +1,75 @@
+//
+//  CalculatorItemQueue.swift
+//  Calculator
+//
+//  Created by H on 2/12/24.
+//
+
+import Foundation
+
+struct CalculatorItemQueue {
+    var inbox: [Int] = []
+    var outbox: [Int] = []
+    
+    var size: Int {
+        return inbox.count + outbox.count
+    }
+    
+    var empty: Int {
+        if inbox.isEmpty && outbox.isEmpty {
+            return 1
+        } else {
+            return 0
+        }
+    }
+    
+    mutating func push(_ data: Int) {
+        inbox.append(data)
+    }
+    
+    mutating func pop() -> Int {
+        if self.empty == 1 {
+            return -1
+        }
+        
+        if outbox.isEmpty {
+            outbox = inbox.reversed()
+            inbox.removeAll()
+        }
+        
+        return outbox.removeLast()
+    }
+    
+    mutating func front() -> Int {
+        if self.empty == 1 {
+            return -1
+        }
+        
+        if outbox.isEmpty {
+            outbox = inbox.reversed()
+            inbox.removeAll()
+        }
+        
+        if let lastData = outbox.last {
+            return lastData
+        } else {
+            return -1
+        }
+    }
+    
+    mutating func back() -> Int {
+        if self.empty == 1 {
+            return -1
+        }
+        
+        if inbox.isEmpty {
+            return outbox.first!
+        }
+        
+        if let lastData = inbox.last {
+            return lastData
+        } else {
+            return -1
+        }
+    }
+}
