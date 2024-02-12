a5ad26065c2ea218f992dc6a711502bfd73ae105
danny <minzami1215@gmail.com>
2024-02-13 00:08:16 +0900

    feat: extension String 내부 확장 로직 기능 구현, ExpressionParser 열거형 타입 내부의 parse, componentByOperators 메소드 내부 로직 구현

--- Calculator/Calculator/Model/ExpressionParser.swift
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -10,9 +10,31 @@ import Foundation
 enum ExpressionParser {
     static func parse(from input: String) -> Formula {
         
+        var operands = CalculatorItemQueue()
+        var operators = CalculatorItemQueue()
+        
+        var componentByOperatorsArray = componentByOperators(from: input)
+        
+        operands.enqueue(componentByOperatorsArray[1])
+        operators.enqueue(componentByOperatorsArray[0])
+    
+        let resultOfParse = Formula(operands: operands, operators: operators)
+        
+        return resultOfParse
+        
     }
     
     private static func componentByOperators(from input: String) -> [String] {
+        
+        if input.split(with: "+").count > 1 {
+            return input.split(with: "+")
+        } else if input.split(with: "-").count > 1 {
+            return input.split(with: "-")
+        } else if input.split(with: "*").count > 1 {
+            return input.split(with: "*")
+        } else {
+            return input.split(with: "/")
+        }
         
     }
 }
--- Calculator/Calculator/Model/String.swift
+++ Calculator/Calculator/Model/String.swift
@@ -9,6 +9,19 @@ import Foundation
 
 extension String {
     func split(with target: Character) -> [String] {
+        var splitString: [String] = []
+        var splitOperands: String = ""
         
+        for i in self {
+            if i == target {
+                splitString.append(String(i))
+            } else {
+                splitOperands.append(String(i))
+            }
+        }
+        
+        splitString.append(splitOperands)
+        
+        return splitString
     }
 }
