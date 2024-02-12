e8204333966d5958e57bd99fdabb349316b03997
PrismSpirit <yeolkeut@gmail.com>
2024-02-12 16:33:06 +0900

    Add Unit Test Case Class for Operator

---
+++ Calculator/CalculatorTests/OperatorTests.swift
@@ -0,0 +1,19 @@
+//
+//  OperatorTests.swift
+//  CalculatorTests
+//
+//  Created by Jaehun Lee on 2/12/24.
+//
+
+import XCTest
+@testable import Calculator
+
+final class OperatorTests: XCTestCase {
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+    }
+
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+    }
+}
