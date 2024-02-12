1e2d43f33e893afbbdec408aaaad9d7938f94888
PrismSpirit <yeolkeut@gmail.com>
2024-02-12 23:06:23 +0900

    Add missing super methods call

--- Calculator/CalculatorTests/FormulaTests.swift
+++ Calculator/CalculatorTests/FormulaTests.swift
@@ -12,10 +12,12 @@ final class FormulaTests: XCTestCase {
     var sut: Formula!
     
     override func setUpWithError() throws {
+        try super.setUpWithError()
         sut = Formula(operands: CalculatorItemQueue<Double>(), operators: CalculatorItemQueue<Operator>())
     }
     
     override func tearDownWithError() throws {
+        try super.tearDownWithError()
         sut = nil
     }
     
