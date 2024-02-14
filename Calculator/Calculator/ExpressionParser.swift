4b0a17d9b7d7a8fe71a93a97fb8ebe2c3848bfc6
forseaest <u3z@naver.com>
2024-02-15 04:05:30 +0900

    Feat: ExpressionParser 열거형 구현

---
+++ Calculator/Calculator/ExpressionParser.swift
@@ -0,0 +1,48 @@
+//
+//  ExpressionParser.swift
+//  Calculator
+//
+//  Created by H on 2/14/24.
+//
+
+import Foundation
+
+enum ExpressionParser {
+    static func parse(from input: String) -> Formula {
+        var operands = CalculatorItemQueue<Double>()
+        var operators = CalculatorItemQueue<String>()
+        
+        let parsedExpression = componentsByOperators(from: input)
+        for component in parsedExpression {
+            if let number = Double(component) {
+                operands.push(number)
+            } else {
+                operators.push(component)
+            }
+        }
+        
+        return Formula(operands: operands, operators: operators)
+    }
+    
+    static private func componentsByOperators(from input: String) -> [String] {
+        var components: [String] = []
+        var tempOperand = ""
+        
+        for char in input {
+            let str = String(char)
+            if str == "+" || str == "-" || str == "%" || str == "*" {
+                components.append(tempOperand)
+                components.append(str)
+                tempOperand = ""
+            } else {
+                tempOperand.append(str)
+            }
+        }
+        
+        if tempOperand != "" {
+            components.append(tempOperand)
+        }
+        
+        return components
+    }
+}
