24bdab577dfe0895a16a58aede5640f11a16b608
forseaest <u3z@naver.com>
2024-02-15 04:04:02 +0900

    Feat: Formula 구조체의 result 함수 로직 구현

--- Calculator/Calculator/Formula.swift
+++ Calculator/Calculator/Formula.swift
@@ -8,10 +8,33 @@
 import Foundation
 
 struct Formula {
-    let operands: CalculatorItemQueue
-    let operators: CalculatorItemQueue
+    var operands: CalculatorItemQueue<Double>
+    var operators: CalculatorItemQueue<String>
     
-    func result() -> Double {
-        return 0.0
+    mutating func result() -> Double {
+        var formulaResult: Double? = nil
+        
+        while let operand = operands.pop() {
+            if let result = formulaResult {
+                let operatorCharacter = operators.pop()
+                
+                switch operatorCharacter {
+                case "+":
+                    formulaResult = Operator.calculate(.add)(lhs: result, rhs: operand)
+                case "-":
+                    formulaResult = Operator.calculate(.substract)(lhs: result, rhs: operand)
+                case "%":
+                    formulaResult = Operator.calculate(.divide)(lhs: result, rhs: operand)
+                case "*":
+                    formulaResult = Operator.calculate(.multiply)(lhs: result, rhs: operand)
+                default:
+                    break
+                }
+            } else {
+                formulaResult = operand
+            }
+        }
+        
+        return formulaResult ?? 0.0
     }
 }
