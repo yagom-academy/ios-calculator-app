8856f539d6063ef74a5abb6ec005048ccff36b35
forseaest <u3z@naver.com>
2024-02-14 20:44:50 +0900

    Feat: Operator 열거형 구현

---
+++ Calculator/Calculator/Operator.swift
@@ -0,0 +1,44 @@
+//
+//  Operator.swift
+//  Calculator
+//
+//  Created by H on 2/14/24.
+//
+
+import Foundation
+
+enum Operator: Character, CaseIterable {
+    case add = "+"
+    case substract = "-"
+    case divide = "%"
+    case multiply = "*"
+    
+    func calculate(lhs: Double, rhs: Double) -> Double {
+        switch self {
+        case .add:
+            return add(lhs: lhs, rhs: rhs)
+        case .substract:
+            return substract(lhs: lhs, rhs: rhs)
+        case .divide:
+            return divide(lhs: lhs, rhs: rhs)
+        case .multiply:
+            return multiply(lhs: lhs, rhs: rhs)
+        }
+    }
+    
+    func add(lhs: Double, rhs: Double) -> Double {
+        return lhs + rhs
+    }
+    
+    func substract(lhs: Double, rhs: Double) -> Double {
+        return lhs - rhs
+    }
+    
+    func divide(lhs: Double, rhs: Double) -> Double {
+        return lhs.truncatingRemainder(dividingBy: rhs)
+    }
+    
+    func multiply(lhs: Double, rhs: Double) -> Double {
+        return lhs * rhs
+    }
+}
