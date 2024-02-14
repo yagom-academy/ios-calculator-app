f1e0eff934b1672932f4f3c5efbc24921bfac610
forseaest <u3z@naver.com>
2024-02-14 23:06:47 +0900

    Feat: Formula 구조체 생성

---
+++ Calculator/Calculator/Formula.swift
@@ -0,0 +1,17 @@
+//
+//  Formula.swift
+//  Calculator
+//
+//  Created by H on 2/14/24.
+//
+
+import Foundation
+
+struct Formula {
+    let operands: CalculatorItemQueue
+    let operators: CalculatorItemQueue
+    
+    func result() -> Double {
+        return 0.0
+    }
+}
