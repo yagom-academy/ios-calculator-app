fb704fb92070a08137f32734d0fc56ad85e26b52
danny <minzami1215@gmail.com>
2024-02-16 22:16:01 +0900

    test: OperatorTests 생성 및 테스트케이스 추가

---
+++ Calculator/CalculatorTests/FormulaTests.swift
@@ -0,0 +1,8 @@
+//
+//  FormulaTests.swift
+//  CalculatorTests
+//
+//  Created by Danny on 2/16/24.
+//
+
+import Foundation
---
+++ Calculator/CalculatorTests/OperatorTests.swift
@@ -0,0 +1,72 @@
+//
+//  OperatorTests.swift
+//  CalculatorTests
+//
+//  Created by Danny on 2/16/24.
+//
+
+import XCTest
+@testable import Calculator
+
+final class OperatorTests: XCTestCase {
+    
+    var sutAdd: Operator!
+    var sutSubtract: Operator!
+    var sutDivide: Operator!
+    var sutMultiply: Operator!
+    
+    override func setUpWithError() throws {
+        sutAdd = Operator.add
+        sutSubtract = Operator.subtract
+        sutDivide = Operator.divide
+        sutMultiply = Operator.multiply
+    }
+    
+    override func tearDownWithError() throws {
+        sutAdd = nil
+        sutSubtract = nil
+        sutDivide = nil
+        sutMultiply = nil
+    }
+    
+    func test_addOperator_calculate_결과확인() throws {
+        let lhs: Double = 1
+        let rhs: Double = 1
+        let expectation: Double = 2
+        
+        let result = try sutAdd.calculate(lhs: lhs, rhs: rhs)
+        
+        XCTAssertEqual(expectation, result)
+    }
+    
+    func test_subtractOperator_calculate_결과확인() throws {
+        let lhs: Double = 1
+        let rhs: Double = 3
+        let expectation: Double = -2
+        
+        let result = try sutSubtract.calculate(lhs: lhs, rhs: rhs)
+        
+        XCTAssertEqual(expectation, result)
+    }
+    
+    func test_divideOperator_calculate_결과확인() throws {
+        let lhs: Double = 3
+        let rhs: Double = 3
+        let expectation: Double = 1
+        
+        let result = try sutDivide.calculate(lhs: lhs, rhs: rhs)
+        
+        XCTAssertEqual(expectation, result)
+    }
+    
+    func test_multiplyOperator_calculate_결과확인() throws {
+        let lhs: Double = 3
+        let rhs: Double = 3
+        let expectation: Double = 9
+        
+        let result = try sutMultiply.calculate(lhs: lhs, rhs: rhs)
+        
+        XCTAssertEqual(expectation, result)
+    }
+    
+}
