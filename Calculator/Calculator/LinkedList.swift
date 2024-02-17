9ff3df37fde292e1a31f44ef1419f8e8578fe51f
yawoong2 <adamzoong@naver.com>
2024-02-17 17:10:49 +0900

    feat: LinkedList 의 test 준비

---
+++ Calculator/Calculator/LinkedList.swift
@@ -0,0 +1,12 @@
+//
+//  CustomLinkedList.swift
+//  Calculator
+//
+//  Created by JIWOONG on 2024/02/17.
+//
+
+import UIKit
+
+class LinkedList {
+    
+}
--- Calculator/CalculatorTests/CalculatorTests.swift
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -8,15 +8,21 @@
 import XCTest
 @testable import Calculator
 
+extension LinkedList {
+    
+}
 
 final class CalculatorTests: XCTestCase {
-
+    var sut: LinkedList!
+    
     override func setUpWithError() throws {
-        
+        try super.setUpWithError()
+        sut = LinkedList()
     }
 
     override func tearDownWithError() throws {
-        
+        try super.tearDownWithError()
+        sut = nil
     }
 
     func testExample() throws {
