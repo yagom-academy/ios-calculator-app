3ce57f8940f8c79669c17529cda7a8d537c2ebdc
yawoong2 <adamzoong@naver.com>
2024-02-20 02:46:35 +0900

    test: dequeue() unit test 실시

--- Calculator/CalculatorTests/CalculatorTests.swift
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -33,47 +33,59 @@ final class CalculatorTests: XCTestCase {
         XCTAssertEqual(expectation, result)
     }
     
-    func test_Queue에서_3을_count하는가() throws {
+    func test_dequeue에_성공하는가() throws {
         // given
-        let expectation = 3
+        let expectation = 4.0
         
         // when
-        sut.enqueue(element: 3.0)
         sut.enqueue(element: 4.0)
-        sut.enqueue(element: 24.2)
-        let result = sut.count
+        let result = sut.dequeue()
         
         // then
         XCTAssertEqual(expectation, result)
     }
     
-    func test_Queue가_첫번째요소를_반환하는가() throws {
-        // given
-        let expectation = 3.0
-        
-        // when
-        sut.enqueue(element: 3.0)
-        sut.enqueue(element: 4.0)
-        sut.enqueue(element: 24.2)
-        let result = sut.first()
-        
-        // then
-        XCTAssertEqual(expectation, result)
-    }
-    
-    func test_Queue가_비워지는가() throws {
-        // given
-        let expectation = true
-        
-        // when
-        sut.enqueue(element: 3.0)
-        sut.enqueue(element: 4.0)
-        sut.enqueue(element: 24.2)
-        sut.clear()
-        let result = sut.isEmpty
-        
-        // then
-        XCTAssertEqual(expectation, result)
-    }
+//    func test_Queue에서_3을_count하는가() throws {
+//        // given
+//        let expectation = 3
+//
+//        // when
+//        sut.enqueue(element: 3.0)
+//        sut.enqueue(element: 4.0)
+//        sut.enqueue(element: 24.2)
+//        let result = sut.count
+//
+//        // then
+//        XCTAssertEqual(expectation, result)
+//    }
+//
+//    func test_Queue가_첫번째요소를_반환하는가() throws {
+//        // given
+//        let expectation = 3.0
+//
+//        // when
+//        sut.enqueue(element: 3.0)
+//        sut.enqueue(element: 4.0)
+//        sut.enqueue(element: 24.2)
+//        let result = sut.first()
+//
+//        // then
+//        XCTAssertEqual(expectation, result)
+//    }
+//
+//    func test_Queue가_비워지는가() throws {
+//        // given
+//        let expectation = true
+//
+//        // when
+//        sut.enqueue(element: 3.0)
+//        sut.enqueue(element: 4.0)
+//        sut.enqueue(element: 24.2)
+//        sut.clear()
+//        let result = sut.isEmpty
+//
+//        // then
+//        XCTAssertEqual(expectation, result)
+//    }
 
 }
