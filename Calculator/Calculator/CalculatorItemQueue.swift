210cf7a367f83bdcbea7e273b1bd88238ec7bcb7
yawoong2 <adamzoong@naver.com>
2024-02-20 02:30:19 +0900

    feat: CalculatorItemQueue struct 타입에 property 생성 및 enqueue() 추가

--- Calculator/Calculator/CalculatorItemQueue.swift
+++ Calculator/Calculator/CalculatorItemQueue.swift
@@ -5,6 +5,11 @@
 //  Created by JIWOONG on 2024/02/19.
 //
 
-struct CalculatorItemQueue {
+struct CalculatorItemQueue<E> {
+    var list = LinkedList<E>()
+    var isEmpty: Bool { list.isEmpty }
     
+    mutating func enqueue(element: E) {
+        list.addLast(element: element)
+    }
 }
