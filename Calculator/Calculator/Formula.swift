9122ef8b64e07f01c31e2d406d367120f3bcd40b
danny <minzami1215@gmail.com>
2024-02-20 11:17:18 +0900

    feat: Formula 구조체 타입 구현

---
+++ Calculator/Calculator/Formula.swift
@@ -0,0 +1,34 @@
+//
+//  Formula.swift
+//  Calculator
+//
+//  Created by Danny on 2/20/24.
+//
+
+import Foundation
+
+struct Formula {
+    var operands: CalculatorItemQueue<Double>
+    var operators: CalculatorItemQueue<Operator>
+    
+    mutating func result() throws -> Double {
+        guard operands.count == operators.count + 1 else {
+            throw CalculatorError.unExpectedError
+        }
+        
+        guard var valueOfResult = operands.dequeue() else {
+            throw CalculatorError.unExpectedError
+        }
+        
+        while operands.isEmpty == false {
+            if operators.isEmpty == false {
+                guard let elementOfOperators = operators.dequeue() else {
+                    throw CalculatorError.unExpectedError
+                }
+                try valueOfResult = elementOfOperators.calculate(lhs: valueOfResult, rhs: operands.dequeue() ?? Constant.zero)
+            }
+        }
+        
+        return valueOfResult
+    }
+}
