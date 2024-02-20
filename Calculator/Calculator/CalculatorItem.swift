dd6741413e64aa42c90f988b8d883e8da6cee92a
forseaest <u3z@naver.com>
2024-02-20 10:31:50 +0900

    feat: CalculatorItem과 CalculatorItemQueue 구현

---
+++ Calculator/Calculator/CalculatorItem.swift
@@ -0,0 +1,12 @@
+//
+//  CalculatorItem.swift
+//  Calculator
+//
+//  Created by H on 2/20/24.
+//
+
+import Foundation
+
+protocol CalculateItem {
+    
+}
---
+++ Calculator/Calculator/CalculatorItemQueue.swift
@@ -0,0 +1,55 @@
+//
+//  CalculatorItemQueue.swift
+//  Calculator
+//
+//  Created by H on 2/20/24.
+//
+
+import Foundation
+
+struct CalculatorItemQueue<T>: CalculateItem {
+    private var inbox: [T] = []
+    private var outbox: [T] = []
+    
+    var size: Int {
+        return inbox.count + outbox.count
+    }
+    
+    var isEmpty: Bool {
+        if inbox.isEmpty && outbox.isEmpty {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    mutating func push(_ data: T) {
+        inbox.append(data)
+    }
+    
+    mutating func pop() -> T? {
+        if outbox.isEmpty {
+            outbox = inbox.reversed()
+            inbox.removeAll()
+        }
+        
+        return outbox.popLast()
+    }
+    
+    mutating func front() -> T? {
+        if outbox.isEmpty {
+            outbox = inbox.reversed()
+            inbox.removeAll()
+        }
+        
+        return outbox.last
+    }
+    
+    mutating func rear() -> T? {
+        if inbox.isEmpty {
+            return outbox.first
+        }
+        
+        return inbox.last
+    }
+}
