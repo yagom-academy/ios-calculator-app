168877b9a1e9f961c7f2e672118a5418fa75fc78
forseaest <u3z@naver.com>
2024-02-20 11:49:36 +0900

    feat: CalculatorError 타입 선언 및 Formula의 result() 함수에 에러 처리 로직 추가

---
+++ Calculator/Calculator/CalculatorError.swift
@@ -0,0 +1,13 @@
+//
+//  CalculatorError.swift
+//  Calculator
+//
+//  Created by H on 2/20/24.
+//
+
+import Foundation
+
+enum CalculatorError: Error {
+    case divideByZero
+    case unexpected
+}
--- Calculator/Calculator/Formula.swift
+++ Calculator/Calculator/Formula.swift
@@ -12,20 +12,29 @@ struct Formula {
     var operators: CalculatorItemQueue<Operator>
     
     mutating func result() throws -> Double {
-        guard operands.count == operators.count + 1 else {
-            throw CalculatorError.unExpectedError
+        guard operands.size == operators.size + 1 else {
+            throw CalculatorError.unexpected
         }
         
-        guard var valueOfResult = operands.dequeue() else {
-            throw CalculatorError.unExpectedError
+        guard var valueOfResult = operands.pop() else {
+            throw CalculatorError.unexpected
         }
         
         while operands.isEmpty == false {
             if operators.isEmpty == false {
-                guard let elementOfOperators = operators.dequeue() else {
-                    throw CalculatorError.unExpectedError
+                guard let elementOfOperators = operators.pop() else {
+                    throw CalculatorError.unexpected
                 }
-                try valueOfResult = elementOfOperators.calculate(lhs: valueOfResult, rhs: operands.dequeue() ?? Constant.zero)
+                
+                guard let elementOfOperands = operands.pop() else {
+                    throw CalculatorError.unexpected
+                }
+                
+                if elementOfOperators == Operator.divide && elementOfOperands == 0 {
+                    throw CalculatorError.divideByZero
+                }
+                
+                valueOfResult = elementOfOperators.calculate(lhs: valueOfResult, rhs: elementOfOperands)
             }
         }
         
