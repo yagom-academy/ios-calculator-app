ac9feafbc62d1cf7680cb37847262736510ad413
PrismSpirit <yeolkeut@gmail.com>
2024-02-21 17:45:35 +0900

    feat: 계산을 수행하는 `result()` 메서드 구현

--- Calculator/Calculator/Model/Formula.swift
+++ Calculator/Calculator/Model/Formula.swift
@@ -11,13 +11,21 @@ struct Formula {
     var operands: CalculatorItemQueue<Double>
     var operators: CalculatorItemQueue<Operator>
     
-    mutating func result() -> Double {
-        var result = 0.0
-        while !operands.isEmpty {
-            if let operand = operands.dequeue() {
-                result += operand
-            }
+    mutating func result() throws -> Double {
+        guard operands.count == operators.count + 1,
+              var result = operands.dequeue() else {
+            throw CalculateError.invalidFormula
         }
+        
+        while !operators.isEmpty {
+            guard let `operator` = operators.dequeue(),
+                  let operand = operands.dequeue() else {
+                throw CalculateError.invalidFormula
+            }
+            
+            result = try `operator`.calculate(lhs: result, rhs: operand)
+        }
+        
         return result
     }
 }
