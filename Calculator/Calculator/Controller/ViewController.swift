2a69765ae7facb9ff36a9a364a3f96e4d68f3d10
forseaest <u3z@naver.com>
2024-02-21 15:27:03 +0900

    feat: ViewController에 숫자, 사칙연산, 등호, AC 버튼을 눌렀을 때 호출하는 함수 구현

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -7,12 +7,72 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var formulaResult: String = ""
+    var formulaUserInput: String = "0"
+    var calculationOperator: Operator = Operator.add
+    
+    @IBOutlet weak var formulaLabel: UILabel!
+    @IBOutlet weak var operatorLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        formulaLabel.text = "\(formulaUserInput)"
+        operatorLabel.text = "\(calculationOperator.rawValue)"
     }
-
-
+    
+    @IBAction func operandButtonTapped(_ sender: UIButton) {
+        if let number = sender.titleLabel?.text {
+            if formulaUserInput == "0" {
+                formulaUserInput = number
+            } else {
+                formulaUserInput += number
+            }
+            
+            formulaLabel.text = "\(formulaUserInput)"
+        }
+    }
+    
+    @IBAction func operatorButtonTapped(_ sender: UIButton) {
+        if let currentOperator = sender.titleLabel?.text {
+            switch currentOperator {
+            case "+":
+                calculationOperator = .add
+            case "−":
+                calculationOperator = .subtract
+            case "×":
+                calculationOperator = .multiply
+            case "÷":
+                calculationOperator = .divide
+            default:
+                break
+            }
+            
+            operatorLabel.text = "\(calculationOperator.rawValue)"
+            
+            formulaResult += formulaUserInput + " " + currentOperator + " "
+            
+            formulaUserInput = "0"
+            formulaLabel.text = "\(formulaUserInput)"
+        }
+    }
+    
+    @IBAction func equalSignButtonTapped(_ sender: UIButton) {
+        formulaResult += formulaUserInput
+        formulaUserInput = ""
+        
+        formulaLabel.text = "\(formulaResult)"
+        
+        let parsedExpression = ExpressionParser.parse(from: formulaResult)
+    }
+    
+    
+    @IBAction func acButtonTapped(_ sender: UIButton) {
+        formulaResult = ""
+        formulaUserInput = "0"
+        calculationOperator = .add
+        
+        formulaLabel.text = "\(formulaUserInput)"
+        operatorLabel.text = "\(calculationOperator.rawValue)"
+    }
 }
-
